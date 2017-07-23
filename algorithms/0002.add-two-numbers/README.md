d7670e6d3132cf9e7c817cab0735ded4a44d79a2
aQua <aQua@aQuaYi.com>
2017-07-23 13:05:10 +0800

    added Problem 2

--- algorithms/0002.add-two-numbers/README.md
+++ algorithms/0002.add-two-numbers/README.md
@@ -5,12 +5,24 @@ You are given two non-empty linked lists representing two non-negative integers.
 
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
-Input: + (5 -> 6 -> 4)
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
 
 ## 解题思路
-(2 -> 4 -> 3)  
+(2 -> 4 -> 3)是 342
+
+(5 -> 6 -> 4)是 465
+
+(7 -> 0 -> 8)是 807
+
+342 + 465 = 807
+
+所以，题目的本意是，把整数换了一种表达方式后，实现其加法。
+
+设计程序时候，需要处理的点有
+1. 位上的加法，需要处理进位问题
+1. 如何进入下一位运算
+1. 按位相加结束后，也还需要处理进位问题。
 
 ## 总结
-
-
+读懂题意后，按步骤实现题目要求。
--- algorithms/0002.add-two-numbers/add-two-numbers.go
+++ algorithms/0002.add-two-numbers/add-two-numbers.go
@@ -14,31 +14,36 @@ type ListNode struct {
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	result := &ListNode{}
-	r := result
+	temp := result
 	v, n := 0, 0
 
 	for {
+		// 在当前位上进行加法运算
 		v, n = add(l1, l2, n)
-		r.Val = v
+		temp.Val = v
 
+		// 进入下一位
 		l1 = next(l1)
 		l2 = next(l2)
-
-		if !(l1 != nil || l2 != nil) {
+		// 如果两个数的下一位都为nil，则结束按位相加的运算
+		if l1 == nil && l2 == nil {
 			break
 		}
 
-		r.Next = &ListNode{}
-		r = r.Next
+		// 为下一位运算准备节点
+		temp.Next = &ListNode{}
+		temp = temp.Next
 	}
 
+	// n == 1 说明，最后一次加运算进位了，需要再添加一个节点。
 	if n == 1 {
-		r.Next = &ListNode{Val: n}
+		temp.Next = &ListNode{Val: n}
 	}
 
 	return result
 }
 
+// next 进入l的下一位。
 func next(l *ListNode) *ListNode {
 	if l != nil {
 		return l.Next
@@ -56,9 +61,11 @@ func add(n1, n2 *ListNode, i int) (v, n int) {
 	}
 
 	v += i
+
 	if v > 9 {
 		v -= 10
 		n = 1
 	}
+
 	return
 }
