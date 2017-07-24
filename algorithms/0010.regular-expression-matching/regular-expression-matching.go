f65e1ae579421f028013f4e9b378e4f60dee5ed6
aQua <aQua@aQuaYi.com>
2017-07-24 20:00:59 +0800

    adding Problem 10 在leetcode网站上复制了最快的解答，准备明天研究一下。

--- algorithms/0010.regular-expression-matching/regular-expression-matching.go
+++ algorithms/0010.regular-expression-matching/regular-expression-matching.go
@@ -4,34 +4,42 @@ package Problem0010
 // "*" 不会出现在p的首位
 // "**" 不会出现，但会出现 ".*."" , ".*.." , ".*.*"
 
-func isMatch(s, p string) bool {
-	if p == "" {
-		return s == ""
+func isMatch(s string, p string) bool {
+	dp := make([][]bool, len(p)+1)
+	for i := range dp {
+		dp[i] = make([]bool, len(s)+1)
 	}
 
-	if len(p) == 1 || (len(p) > 1 && p[1] != '*') {
-		if s == "" || (p[0] != '.' && s[0] != p[0]) {
-			return false
-		}
+	dp[0][0] = true
 
-		return isMatch(s[1:], p[1:])
-	}
-
-	slen := len(s)
-	i := -1
-
-	for i < slen && (i < 0 || p[0] == '.' || p[0] == s[i]) {
-		if len(p) >= 2 {
-			if isMatch(s[i+1:], p[2:]) {
-				return true
-			}
+	for i := 2; i < len(dp); i += 2 {
+		if p[i-1] == '*' {
+			dp[i][0] = true
 		} else {
-			if isMatch(s[i+1:], p[1:]) {
-				return true
-			}
+			break
 		}
-		i++
 	}
 
-	return false
+	for i := 1; i < len(dp); i++ {
+		if i < len(p) && p[i] == '*' {
+			continue
+		}
+
+		for j := 1; j < len(dp[0]); j++ {
+
+			if p[i-1] == '*' {
+				if p[i-2] == '.' {
+					dp[i][j] = dp[i-2][j-1] || dp[i][j-1] || dp[i-2][j]
+				} else {
+					dp[i][j] = (dp[i-2][j]) || (p[i-2] == s[j-1] && (dp[i-2][j-1] || dp[i][j-1]))
+				}
+			} else if p[i-1] == '.' {
+				dp[i][j] = dp[i-1][j-1]
+			} else {
+				dp[i][j] = dp[i-1][j-1] && p[i-1] == s[j-1]
+			}
+		}
+	}
+
+	return dp[len(p)][len(s)]
 }
