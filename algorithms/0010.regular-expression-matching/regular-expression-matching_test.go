5fb3564cc2fb94261cecf74d8491801d29baa185
aQua <aQua@aQuaYi.com>
2017-07-24 17:47:18 +0800

    adding Problem 10

--- algorithms/0010.regular-expression-matching/regular-expression-matching_test.go
+++ algorithms/0010.regular-expression-matching/regular-expression-matching_test.go
@@ -13,30 +13,149 @@ type question struct {
 
 type para struct {
 	one string
+	two string
 }
 
 type ans struct {
-	one string
+	one bool
 }
 
 func Test_Problem0010(t *testing.T) {
 	ast := assert.New(t)
 
 	qs := []question{
-		
+
 		question{
 			p: para{
-				one: "",
+				one: "aa",
+				two: "a",
 			},
 			a: ans{
-				one: "",
+				one: false,
+			},
+		},
+		question{
+			p: para{
+				one: "aa",
+				two: "aa",
+			},
+			a: ans{
+				one: true,
+			},
+		},
+		question{
+			p: para{
+				one: "aaa",
+				two: "aa",
+			},
+			a: ans{
+				one: false,
 			},
 		},
 
+		question{
+			p: para{
+				one: "aa",
+				two: "a*",
+			},
+			a: ans{
+				one: true,
+			},
+		},
+		question{
+			p: para{
+				one: "aa",
+				two: ".*",
+			},
+			a: ans{
+				one: true,
+			},
+		},
+		question{
+			p: para{
+				one: "ab",
+				two: ".*",
+			},
+			a: ans{
+				one: true,
+			},
+		},
+		question{
+			p: para{
+				one: "aab",
+				two: "c*a*b",
+			},
+			a: ans{
+				one: true,
+			},
+		},
+		question{
+			p: para{
+				one: "aaaaaaaab",
+				two: "c*a*b",
+			},
+			a: ans{
+				one: true,
+			},
+		},
+		question{
+			p: para{
+				one: "ab",
+				two: "c*a*b",
+			},
+			a: ans{
+				one: true,
+			},
+		},
+		question{
+			p: para{
+				one: "abc",
+				two: ".*",
+			},
+			a: ans{
+				one: true,
+			},
+		},
+		question{
+			p: para{
+				one: "ab",
+				two: ".*b.*",
+			},
+			a: ans{
+				one: true,
+			},
+		},
+		question{
+			p: para{
+				one: "b",
+				two: ".*b.*",
+			},
+			a: ans{
+				one: true,
+			},
+		},
+		question{
+			p: para{
+				one: "b",
+				two: ".*...b",
+			},
+			a: ans{
+				one: false,
+			},
+		},
+		question{
+			p: para{
+				one: "b",
+				two: ".*..*b",
+			},
+			a: ans{
+				one: false,
+			},
+		},
 	}
 
 	for _, q := range qs {
 		a, p := q.a, q.p
-		ast.Equal(a.one, p.one, "输入:%v", p)
+		ast.Equal(a.one, isMatch(p.one, p.two), "输入:%v", p)
 	}
 }
