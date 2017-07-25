4a1e159bfa558ecdeb511e77c30874e8f1077e59
aQua <aQua@aQuaYi.com>
2017-07-25 15:37:35 +0800

    利用穷举法，实现目标，但是超时了。

--- Algorithms/0011.container-with-most-water/container-with-most-water.go
+++ Algorithms/0011.container-with-most-water/container-with-most-water.go
@@ -1,2 +1,35 @@
 package Problem0011
 
+func maxArea(height []int) int {
+	max := 0
+	for i := 0; i < len(height); i++ {
+		for j := 0; j < len(height); j++ {
+			temp := h(height, i, j) * w(i, j)
+			if temp > max {
+				max = temp
+			}
+		}
+	}
+
+	return max
+}
+
+// h 高度
+func h(h []int, i, j int) int {
+	a, b := h[i], h[j]
+
+	if a < b {
+		return a
+	}
+
+	return b
+}
+
+// w 宽度
+func w(a, b int) int {
+	if a < b {
+		return b - a
+	}
+
+	return a - b
+}
--- Algorithms/0011.container-with-most-water/container-with-most-water_test.go
+++ Algorithms/0011.container-with-most-water/container-with-most-water_test.go
@@ -14,30 +14,36 @@ type question struct {
 // para 是参数
 // one 代表第一个参数
 type para struct {
-	one string
+	one []int
 }
 
 // ans 是答案
 // one 代表第一个答案
 type ans struct {
-	one string
+	one int
 }
 
 func Test_Problem0011(t *testing.T) {
 	ast := assert.New(t)
 
 	qs := []question{
-
 		question{
-			para{""},
-			ans{""},
+			para{[]int{1, 2, 3, 1}},
+			ans{3},
 		},
-		
+		question{
+			para{[]int{1, 3, 6, 4, 3, 5, 6, 7, 8, 9, 7, 5, 4, 3, 2, 1}},
+			ans{48},
+		},
+		question{
+			para{[]int{1, 1}},
+			ans{1},
+		},
 	}
 
 	for _, q := range qs {
 		a, p := q.ans, q.para
-		
-		ast.Equal(a.one, p.one, "输入:%v", p)
+
+		ast.Equal(a.one, maxArea(p.one), "输入:%v", p)
 	}
 }
