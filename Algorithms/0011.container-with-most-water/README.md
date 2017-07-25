0103faef3547c6c8fee2e96fd362e5d41f04363d
aQua <aQua@aQuaYi.com>
2017-07-25 16:08:34 +0800

    added Problem 11

--- Algorithms/0011.container-with-most-water/README.md
+++ Algorithms/0011.container-with-most-water/README.md
@@ -1,11 +1,11 @@
 # [11. Container With Most Water](https://leetcode.com/problems/container-with-most-water/)
 
 ## 题目
+Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
 
+就是说，x轴上在1,2,...,n点上有许多垂直的线段，长度依次是a1, a2, ..., an。找出两条线段，使他们和x抽围成的面积最大。面积公式是 Min(ai, aj) X |j - i|
 
 ## 解题思路
+穷举法是O(n^2)的复杂度，会触发leetcode的时间限制。
 
-
-## 总结
-
-
+ O(n)的复杂度的解法是，保持两个指针i,j；分别指向长度数组的首尾。如果ai 小于aj，则移动i向后（i++）。反之，移动j向前（j--）。如果当前的area大于了所记录的area，替换之。这个想法的基础是，如果i的长度小于j，无论如何移动j，短板在i，不可能找到比当前记录的area更大的值了，只能通过移动i来找到新的可能的更大面积。
--- Algorithms/0011.container-with-most-water/container-with-most-water.go
+++ Algorithms/0011.container-with-most-water/container-with-most-water.go
@@ -1,35 +1,33 @@
 package Problem0011
 
 func maxArea(height []int) int {
+	// 从两端开始寻找，至少保证了宽度是最大值
+	i, j := 0, len(height)-1
 	max := 0
-	for i := 0; i < len(height); i++ {
-		for j := 0; j < len(height); j++ {
-			temp := h(height, i, j) * w(i, j)
-			if temp > max {
-				max = temp
-			}
+
+	for i < j {
+		a, b := height[i], height[j]
+		h := min(a, b)
+
+		area := h * (j - i)
+		if max < area {
+			max = area
+		}
+
+		// 朝着area具有变大的可能性方向变化。
+		if a < b {
+			i++
+		} else {
+			j--
 		}
 	}
 
 	return max
 }
 
-// h 高度
-func h(h []int, i, j int) int {
-	a, b := h[i], h[j]
-
-	if a < b {
-		return a
+func min(i, j int) int {
+	if i <= j {
+		return i
 	}
-
-	return b
-}
-
-// w 宽度
-func w(a, b int) int {
-	if a < b {
-		return b - a
-	}
-
-	return a - b
+	return j
 }
