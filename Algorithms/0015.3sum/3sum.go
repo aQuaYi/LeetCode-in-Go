2a1b615e7cf61eabe111fdcdbbf12135fc6b222e
aQua <aQua@aQuaYi.com>
2017-07-28 11:49:28 +0800

    添加了穷举法额解法，但是超时了。

--- Algorithms/0015.3sum/3sum.go
+++ Algorithms/0015.3sum/3sum.go
@@ -1,2 +1,47 @@
 package Problem0015
 
+import (
+	"sort"
+)
+
+func threeSum(nums []int) [][]int {
+	res := [][]int{}
+
+	for i := 0; i < len(nums)-2; i++ {
+		for j := i + 1; j < len(nums)-1; j++ {
+			for k := j + 1; k < len(nums); k++ {
+				if nums[i]+nums[j]+nums[k] == 0 {
+					temp := []int{nums[i], nums[j], nums[k]}
+					if yes := contain(res, temp); !yes {
+						res = append(res, temp)
+
+					}
+				}
+			}
+		}
+	}
+
+	return res
+}
+
+func contain(numss [][]int, nums []int) bool {
+	sort.Ints(nums)
+
+	for _, ns := range numss {
+		sort.Ints(ns)
+		if equal(ns, nums) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func equal(a, b []int) bool {
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
--- Algorithms/0015.3sum/3sum_test.go
+++ Algorithms/0015.3sum/3sum_test.go
@@ -14,13 +14,13 @@ type question struct {
 // para 是参数
 // one 代表第一个参数
 type para struct {
-	one string
+	one []int
 }
 
 // ans 是答案
 // one 代表第一个答案
 type ans struct {
-	one string
+	one [][]int
 }
 
 func Test_Problem0015(t *testing.T) {
@@ -29,16 +29,19 @@ func Test_Problem0015(t *testing.T) {
 	qs := []question{
 
 		question{
-			para{""},
-			ans{""},
+			para{[]int{-1, 0, 1, 2, -1, -4}},
+			ans{[][]int{
+				[]int{-1, 0, 1},
+				[]int{-1, -1, 2},
+			}},
 		},
-	
+
 		// 如需多个测试，可以复制上方元素。
 	}
 
 	for _, q := range qs {
 		a, p := q.ans, q.para
-		
-		ast.Equal(a.one, p.one, "输入:%v", p)
+
+		ast.Equal(a.one, threeSum(p.one), "输入:%v", p)
 	}
 }
