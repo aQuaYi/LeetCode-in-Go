2f2efacf303e0b759df351cdfda6cfd2264d1835
aQua <aQua@aQuaYi.com>
2017-07-29 22:11:18 +0800

    accepted Problem 19

--- Algorithms/0019.remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.go
+++ Algorithms/0019.remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.go
@@ -1,2 +1,36 @@
 package Problem0019
 
+/*
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+// ListNode 是节点
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	nd, n := getNDaddy(head, n)
+	if nd == head && n == 0 {
+		return head.Next
+	}
+	nd.Next = nd.Next.Next
+	return head
+}
+
+func getNDaddy(head *ListNode, n int) (*ListNode, int) {
+	NDaddy := head
+	for head != nil {
+		if n < 0 {
+			NDaddy = NDaddy.Next
+		}
+		n--
+		head = head.Next
+	}
+	return NDaddy, n
+}
--- Algorithms/0019.remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list_test.go
+++ Algorithms/0019.remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list_test.go
@@ -1,8 +1,8 @@
 package Problem0019
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -15,13 +15,14 @@ type question struct {
 // para 是参数
 // one 代表第一个参数
 type para struct {
-	one string
+	one []int
+	two int
 }
 
 // ans 是答案
 // one 代表第一个答案
 type ans struct {
-	one string
+	one []int
 }
 
 func Test_Problem0019(t *testing.T) {
@@ -30,10 +31,21 @@ func Test_Problem0019(t *testing.T) {
 	qs := []question{
 
 		question{
-			para{""},
-			ans{""},
+			para{[]int{1, 2}, 1},
+			ans{[]int{1}},
 		},
-	
+		question{
+			para{[]int{1, 2, 3, 4, 5}, 1},
+			ans{[]int{1, 2, 3, 4}},
+		},
+		question{
+			para{[]int{1, 2, 3, 4, 5}, 2},
+			ans{[]int{1, 2, 3, 5}},
+		},
+		question{
+			para{[]int{1}, 1},
+			ans{[]int{}},
+		},
 		// 如需多个测试，可以复制上方元素。
 	}
 
@@ -41,6 +53,38 @@ func Test_Problem0019(t *testing.T) {
 		a, p := q.ans, q.para
 		fmt.Printf("~~%v~~\n", p)
 
-		ast.Equal(a.one, (p.one), "输入:%v", p)
+		ast.Equal(a.one, l2s(removeNthFromEnd(s2l(p.one), p.two)), "输入:%v", p)
 	}
+}
+
+// convert *ListNode to []int
+func l2s(head *ListNode) []int {
+	res := []int{}
+
+	for head != nil {
+		res = append(res, head.Val)
+		head = head.Next
+	}
+
+	return res
+}
+
+// convert []int to *ListNode
+func s2l(nums []int) *ListNode {
+	if len(nums) == 0 {
+		return nil
+	}
+
+	res := &ListNode{
+		Val: nums[0],
+	}
+	temp := res
+	for i := 1; i < len(nums); i++ {
+		temp.Next = &ListNode{
+			Val: nums[i],
+		}
+		temp = temp.Next
+	}
+
+	return res
 }
