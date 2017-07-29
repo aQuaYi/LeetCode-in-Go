c57f0a8419526a0de4c12bad4ab174d722a37ae0
aQua <aQua@aQuaYi.com>
2017-07-29 18:49:56 +0800

    adding Problem 18

--- Algorithms/0018.4sum/4sum.go
+++ Algorithms/0018.4sum/4sum.go
@@ -9,10 +9,19 @@ func fourSum(nums []int, target int) [][]int {
 	sort.Ints(nums)
 
 	for i := 0; i < len(nums)-3; i++ {
+		// 避免添加重复的结果
+		if i > 1 && nums[i] == nums[i-2] {
+			continue
+		}
+
 		for j := i + 1; j < len(nums)-2; j++ {
+			// 避免添加重复的结果
+			if nums[j] == nums[j-1] {
+				continue
+			}
+
 			l, r := j+1, len(nums)-1
 			for l < r {
-
 				s := nums[i] + nums[j] + nums[l] + nums[r]
 				switch {
 				case s < target:
@@ -20,10 +29,28 @@ func fourSum(nums []int, target int) [][]int {
 				case s > target:
 					r--
 				default:
+					res = append(res, []int{nums[i], nums[j], nums[l], nums[r]})
+					l, r = next(nums, l, r)
 				}
 			}
 		}
 
 	}
 	return res
+}
+
+func next(nums []int, l, r int) (int, int) {
+	for l < r {
+		switch {
+		case nums[l] == nums[l+1]:
+			l++
+		case nums[r] == nums[r-1]:
+			r--
+		default:
+			l++
+			r--
+			break
+		}
+	}
+	return l, r
 }
