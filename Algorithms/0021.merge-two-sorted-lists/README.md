0ce89fa0543822644e5d0dbacae30ff19161547a
aQua <aQua@aQuaYi.com>
2017-07-30 12:50:34 +0800

    finish Problem 21

--- Algorithms/0021.merge-two-sorted-lists/README.md
+++ Algorithms/0021.merge-two-sorted-lists/README.md
@@ -4,8 +4,10 @@
 Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
 
 ## 解题思路
-把两个排序好的链合并，要求合并后依然是排序好的。
-
+把两个排序好的链合并，要求合并后依然是排序好的。结题步骤如下：
+1. 先处理其中一条链为nil的情况，直接返回另一条链，这样可以简化后面的判断条件。
+1. 设置好链接头head和用于移动节点指针node
+1. 利用for循环反复比较，每次选取较小的节点，放在node.Next
+1. 处理l1或l2中剩余的节点
 ## 总结
-
-
+合理地安排步骤，可以有效地减轻后面的判断条件和处理步骤，让整个函数更清晰易懂。
--- Algorithms/0021.merge-two-sorted-lists/merge-two-sorted-lists.go
+++ Algorithms/0021.merge-two-sorted-lists/merge-two-sorted-lists.go
@@ -8,45 +8,50 @@ package Problem0021
  * }
  */
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	// 有一条链为nil，直接返回另一条链
 	if l1 == nil {
 		return l2
 	}
-
 	if l2 == nil {
 		return l1
 	}
 
-	var res, temp *ListNode
-
+	// 此时，两条链都不为nil，可以直接使用l.Val，而不用担心panic
+	// 在l1和l2之间，选择较小的节点作为head，并设置好node
+	var head, node *ListNode
 	if l1.Val < l2.Val {
-		res = l1
-		temp = l1
+		head = l1
+		node = l1
 		l1 = l1.Next
 	} else {
-		res = l2
-		temp = l2
+		head = l2
+		node = l2
 		l2 = l2.Next
 	}
 
+	// 循环比较l1和l2的值，始终选择较小的值连上node
 	for l1 != nil && l2 != nil {
 		if l1.Val < l2.Val {
-			temp.Next = l1
+			node.Next = l1
 			l1 = l1.Next
 		} else {
-			temp.Next = l2
+			node.Next = l2
 			l2 = l2.Next
 		}
 
-		temp = temp.Next
+		// 有了这一步，head才是一个完整的链
+		node = node.Next
 	}
 
 	if l1 != nil {
-		temp.Next = l1
+		// 连上l1剩余的链
+		node.Next = l1
 	}
 
 	if l2 != nil {
-		temp.Next = l2
+		// 连上l2剩余的链
+		node.Next = l2
 	}
 
-	return res
+	return head
 }
