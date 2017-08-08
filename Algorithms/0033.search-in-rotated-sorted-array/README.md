b43c88b9c099eca6b5f973031d50936245f5f609
aQua <aQua@aQuaYi.com>
2017-08-08 20:22:23 +0800

    finish Problem 33 想通了以后，就会觉得题目好简单。

--- Algorithms/0033.search-in-rotated-sorted-array/README.md
+++ Algorithms/0033.search-in-rotated-sorted-array/README.md
@@ -10,8 +10,19 @@ You are given a target value to search. If found in the array return its index, 
 You may assume no duplicate exists in the array.
 
 ## 解题思路
+先假设old = [0, 1, 2, 4, 5, 6, 7]，利用二分查找法，很容易可以`5`的索引号，当old变换成了new = [4, 5, 6, 7, 0, 1, 2]以后，同样可以使用二分查找法，因为old和new中的元素有明确的对应关系
+
+old[i] == new[j]，只要i和j满足关系式
+```go
+j=i+4
+if j > len(old) {
+    j -= len(old)
+}
+```
+其中，4 = old中的最大值在new中的索引号 + 1
+
+所以，如果我们手中只有new，我们可以假装自己还是在对old使用二分查找法，当需要获取old[i]的值进行比较判断的时候，使用new[j]的值替代即可。
 
 
 ## 总结
-
-
+本题是二分查找法的升级版
--- Algorithms/0033.search-in-rotated-sorted-array/search-in-rotated-sorted-array.go
+++ Algorithms/0033.search-in-rotated-sorted-array/search-in-rotated-sorted-array.go
@@ -2,33 +2,33 @@ package Problem0033
 
 func search(nums []int, target int) int {
 	var index, indexOfMax int
-
 	length := len(nums)
 
 	if length == 0 {
 		return -1
 	}
 
+	// 获取最大值的索引号，以便进行索引号变换
 	for indexOfMax+1 < length && nums[indexOfMax] < nums[indexOfMax+1] {
 		indexOfMax++
 	}
 
-	lo, hi, median := 0, length-1, 0
-	for lo <= hi {
-		median = (lo + hi) / 2
+	low, high, median := 0, length-1, 0
+	for low <= high {
+		median = (low + high) / 2
 
-		relativeIndex := median + indexOfMax + 1
-		if relativeIndex < length {
-			index = relativeIndex
-		} else {
-			index = relativeIndex - length
+		// 变换索引号
+		index = median + indexOfMax + 1
+		if index >= length {
+			index -= length
 		}
 
+		// 传统二分查找法的比较判断
 		switch {
 		case nums[index] > target:
-			hi = median - 1
+			high = median - 1
 		case nums[index] < target:
-			lo = median + 1
+			low = median + 1
 		default:
 			return index
 		}
