eabc11afdcf267a2a40416a3adcee981861bbe1f
aQua <aQua@aQuaYi.com>
2017-08-08 17:55:53 +0800

    adding Problem 33 在学习最快的答案

--- Algorithms/0033.search-in-rotated-sorted-array/search-in-rotated-sorted-array.go
+++ Algorithms/0033.search-in-rotated-sorted-array/search-in-rotated-sorted-array.go
@@ -1,58 +1,38 @@
 package Problem0033
 
 func search(nums []int, target int) int {
-	if len(nums) == 0 {
+	var index, indexOfMax int
+
+	length := len(nums)
+
+	if length == 0 {
 		return -1
 	}
 
-	if nums[len(nums)-1] > nums[0] {
-		return searchInt(nums, target, 0)
+	for indexOfMax+1 < length && nums[indexOfMax] < nums[indexOfMax+1] {
+		indexOfMax++
 	}
 
-	base := searchMax(nums) + 1
-	res := searchInt(nums[:base], target, 0)
-	if res != -1 {
-		return res
-	}
-	res = searchInt(nums[base:], target, base)
-	if res != -1 {
-		return res
-	}
-
-	return -1
-}
-
-func searchInt(nums []int, target, base int) (index int) {
-	lo, hi := 0, len(nums)-1
+	lo, hi, median := 0, length-1, 0
 	for lo <= hi {
-		index = (lo + hi) / 2
+		median = (lo + hi) / 2
+
+		relativeIndex := median + indexOfMax + 1
+		if relativeIndex < length {
+			index = relativeIndex
+		} else {
+			index = relativeIndex - length
+		}
+
 		switch {
 		case nums[index] > target:
-			hi = index - 1
+			hi = median - 1
 		case nums[index] < target:
-			lo = index + 1
+			lo = median + 1
 		default:
-			return base + index
+			return index
 		}
 	}
+
 	return -1
-}
-
-func searchMax(nums []int) (index int) {
-	i, j := 0, len(nums)-1
-
-	for {
-		index = (i + j) / 2
-		if index == 0 || index == len(nums)-1 {
-			return
-		}
-		if index > 0 && index < len(nums)-1 && nums[index] > nums[index-1] && nums[index] > nums[index+1] {
-			return
-		}
-		if nums[index] > nums[i] {
-			i = index
-		} else {
-			j = index
-		}
-	}
 }
--- Algorithms/0033.search-in-rotated-sorted-array/search-in-rotated-sorted-array_test.go
+++ Algorithms/0033.search-in-rotated-sorted-array/search-in-rotated-sorted-array_test.go
@@ -31,12 +31,12 @@ func Test_Problem0033(t *testing.T) {
 	qs := []question{
 
 		question{
-			para{[]int{4, 5, 6, 7, 0, 1, 2}, 5},
+			para{[]int{4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0, 1, 2}, 5},
 			ans{1},
 		},
 
 		question{
-			para{[]int{6, 7, 0, 1, 2,3,4,5}, 1},
+			para{[]int{6, 7, 0, 1, 2, 3, 4, 5}, 1},
 			ans{3},
 		},
 
@@ -69,41 +69,4 @@ func Test_Problem0033(t *testing.T) {
 
 		ast.Equal(a.one, search(p.one, p.two), "输入:%v", p)
 	}
-}
-func Test_searchInt(t *testing.T) {
-	var actual, expected int
-	ast := assert.New(t)
-
-	actual = searchInt([]int{3, 4}, 4, 0)
-	expected = 1
-	ast.Equal(expected, actual)
-
-	actual = searchInt([]int{3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}, 4, 0)
-	expected = 1
-	ast.Equal(expected, actual)
-}
-func Test_searchMax(t *testing.T) {
-	var actual, expected int
-	ast := assert.New(t)
-
-	actual = searchMax([]int{9, 1, 2, 3, 4, 5, 6, 7, 8})
-	expected = 0
-	ast.Equal(expected, actual)
-
-	actual = searchMax([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0})
-	expected = 8
-	ast.Equal(expected, actual)
-
-	actual = searchMax([]int{4, 5, 6, 7, 0, 1, 2})
-	expected = 3
-	ast.Equal(expected, actual)
-
-	actual = searchMax([]int{1, 0})
-	expected = 0
-	ast.Equal(expected, actual)
-
-	actual = searchMax([]int{0})
-	expected = 0
-	ast.Equal(expected, actual)
-
 }
