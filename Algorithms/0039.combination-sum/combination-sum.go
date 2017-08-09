d381c9446e35b44bcd3a6f9cbbfe22208f587f8f
aQua <aQua@aQuaYi.com>
2017-08-09 20:10:35 +0800

    improve Problem 39

--- Algorithms/0039.combination-sum/combination-sum.go
+++ Algorithms/0039.combination-sum/combination-sum.go
@@ -20,9 +20,15 @@ func cs(candidates, solution []int, target int, result *[][]int) {
 	}
 
 	if target < 0 || len(candidates) == 0 || target < candidates[0] {
+		// target < candidates[0] 这个是因为candidates是排序好的
+		// 否则，就应该是 target < min(candidates...)
+		// 其实，不要这个判断条件，也能成功
 		return
 	}
 
+	// 这样处理一下的用意是，让切片的容量等于长度，以后append的时候，会分配新的底层数组
+	// 避免多处同时对底层数组进行修改，产生错误的答案。
+	// 可以注释掉以下语句，运行单元测试，查看错误发生。
 	solution = solution[:len(solution):len(solution)]
 
 	cs(candidates, append(solution, candidates[0]), target-candidates[0], result)
