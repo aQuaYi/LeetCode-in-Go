20e0c6ed77966757863a13b947e4ec20abd200b5
aQua <aQua@aQuaYi.com>
2017-08-10 17:57:56 +0800

    adding

--- Algorithms/0036.valid-sudoku/valid-sudoku.go
+++ Algorithms/0036.valid-sudoku/valid-sudoku.go
@@ -1,2 +1,61 @@
 package Problem0036
 
+func isValidSudoku(board [][]byte) bool {
+	row, column := makeIndex()
+
+	for i := range row {
+		if check(board, r[i], c [9]int)
+	}
+	return true
+}
+
+func check(board [][]byte, r, c [9]int) bool {
+	record := make(map[byte]bool, 9)
+	for i := 0; i < 9; i++ {
+		b := board[r[i]][c[i]]
+		if b == '.' {
+			continue
+		}
+		ok := record[b]
+		if ok {
+			return false
+		}
+		record[b] = true
+	}
+
+	return true
+}
+
+func makeIndex() (row, column [][9]int) {
+	for i := 0; i < 9; i++ {
+		r := [9]int{}
+		c := [9]int{}
+		for j := 0; j < 9; j++ {
+			r[j] = i
+			c[j] = j
+		}
+		row = append(row, r, c)
+		column = append(column, c, r)
+	}
+
+	rowBase := [][9]int{
+		[9]int{0, 0, 0, 1, 1, 1, 2, 2, 2},
+		[9]int{3, 3, 3, 4, 4, 4, 5, 5, 5},
+		[9]int{6, 6, 6, 7, 7, 7, 8, 8, 8},
+	}
+
+	columnBase := [][9]int{
+		[9]int{0, 1, 2, 0, 1, 2, 0, 1, 2},
+		[9]int{3, 4, 5, 3, 4, 5, 3, 4, 5},
+		[9]int{6, 7, 8, 6, 7, 8, 6, 7, 8},
+	}
+
+	for i := range rowBase {
+		for j := range columnBase {
+			row = append(row, rowBase[i])
+			column = append(column, columnBase[j])
+		}
+	}
+
+	return
+}
--- Algorithms/0036.valid-sudoku/valid-sudoku_test.go
+++ Algorithms/0036.valid-sudoku/valid-sudoku_test.go
@@ -1,8 +1,8 @@
 package Problem0036
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -15,13 +15,13 @@ type question struct {
 // para 是参数
 // one 代表第一个参数
 type para struct {
-	one string
+	one [][]byte
 }
 
 // ans 是答案
 // one 代表第一个答案
 type ans struct {
-	one string
+	one bool
 }
 
 func Test_Problem0036(t *testing.T) {
@@ -30,10 +30,20 @@ func Test_Problem0036(t *testing.T) {
 	qs := []question{
 
 		question{
-			para{""},
-			ans{""},
+			para{[][]byte{
+				[]byte(".87654321"),
+				[]byte("2........"),
+				[]byte("3........"),
+				[]byte("4........"),
+				[]byte("5........"),
+				[]byte("6........"),
+				[]byte("7........"),
+				[]byte("8........"),
+				[]byte("9........"),
+			}},
+			ans{true},
 		},
-	
+
 		// 如需多个测试，可以复制上方元素。
 	}
 
@@ -43,4 +53,14 @@ func Test_Problem0036(t *testing.T) {
 
 		ast.Equal(a.one, (p.one), "输入:%v", p)
 	}
+}
+func Test_makeIndex(t *testing.T) {
+	ast := assert.New(t)
+
+	ast.True(true)
+	row, column := makeIndex()
+	for i, r := range row {
+		fmt.Println(r, column[i])
+	}
+
 }
