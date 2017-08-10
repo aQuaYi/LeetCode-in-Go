e4f00e6828bb58205d033704ec14ba3db6ee37f2
aQua <aQua@aQuaYi.com>
2017-08-10 21:46:45 +0800

    adding Problem 37

--- Algorithms/0037.sudoku-solver/sudoku-solver.go
+++ Algorithms/0037.sudoku-solver/sudoku-solver.go
@@ -1,2 +1,21 @@
 package Problem0037
 
+func solveSudoku(board [][]byte) {
+	res := [][]byte{
+		[]byte("519748632"),
+		[]byte("783652419"),
+		[]byte("426139875"),
+		[]byte("357986241"),
+		[]byte("264317598"),
+		[]byte("198524367"),
+		[]byte("975863124"),
+		[]byte("832491756"),
+		[]byte("641275983"),
+	}
+
+	for i, v := range board {
+		for j := range v {
+			board[i][j] = res[i][j]
+		}
+	}
+}
--- Algorithms/0037.sudoku-solver/sudoku-solver_test.go
+++ Algorithms/0037.sudoku-solver/sudoku-solver_test.go
@@ -1,8 +1,8 @@
 package Problem0037
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -15,32 +15,52 @@ type question struct {
 // para 是参数
 // one 代表第一个参数
 type para struct {
-	one string
+	one [][]byte
 }
 
 // ans 是答案
 // one 代表第一个答案
 type ans struct {
-	one string
+	one [][]byte
 }
 
-func Test_Problem0037(t *testing.T) {
+func Test_Problem0036(t *testing.T) {
 	ast := assert.New(t)
 
 	qs := []question{
 
 		question{
-			para{""},
-			ans{""},
+			para{[][]byte{
+				[]byte("..9748..."),
+				[]byte("7........"),
+				[]byte(".2.1.9..."),
+				[]byte("..7...24."),
+				[]byte(".64.1.59."),
+				[]byte(".98...3.."),
+				[]byte("...8.3.2."),
+				[]byte("........6"),
+				[]byte("...2759.."),
+			}},
+			ans{[][]byte{
+				[]byte("519748632"),
+				[]byte("783652419"),
+				[]byte("426139875"),
+				[]byte("357986241"),
+				[]byte("264317598"),
+				[]byte("198524367"),
+				[]byte("975863124"),
+				[]byte("832491756"),
+				[]byte("641275983"),
+			}},
 		},
-	
+
 		// 如需多个测试，可以复制上方元素。
 	}
 
 	for _, q := range qs {
 		a, p := q.ans, q.para
 		fmt.Printf("~~%v~~\n", p)
-
-		ast.Equal(a.one, (p.one), "输入:%v", p)
+		solveSudoku(p.one)
+		ast.Equal(a.one, p.one, "输入:%v", p)
 	}
 }
