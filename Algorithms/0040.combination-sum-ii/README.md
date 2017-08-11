9564782f8437a6d84343276ff44f23a8877b3733
aQua <aQua@aQuaYi.com>
2017-08-11 16:07:36 +0800

    adding Problem 40

--- Algorithms/0040.combination-sum-ii/README.md
+++ Algorithms/0040.combination-sum-ii/README.md
@@ -1,8 +1,23 @@
 # [40. Combination Sum II](https://leetcode.com/problems/combination-sum-ii/)
 
 ## 题目
+Given a collection of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
 
+Each number in C may only be used once in the combination.
 
+Note:
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations.
+For example, given candidate set [10, 1, 2, 7, 6, 1, 5] and target 8, 
+A solution set is:
+``` 
+[
+  [1, 7],
+  [1, 2, 5],
+  [2, 6],
+  [1, 1, 6]
+]
+```
 ## 解题思路
 
 
--- Algorithms/0040.combination-sum-ii/combination-sum-ii_test.go
+++ Algorithms/0040.combination-sum-ii/combination-sum-ii_test.go
@@ -1,8 +1,8 @@
 package Problem0040
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -15,25 +15,31 @@ type question struct {
 // para 是参数
 // one 代表第一个参数
 type para struct {
-	one string
+	candidates []int
+	target     int
 }
 
 // ans 是答案
 // one 代表第一个答案
 type ans struct {
-	one string
+	one [][]int
 }
 
-func Test_Problem0040(t *testing.T) {
+func Test_Problem0039(t *testing.T) {
 	ast := assert.New(t)
 
 	qs := []question{
 
 		question{
-			para{""},
-			ans{""},
+			para{[]int{10, 1, 2, 7, 6, 1, 5}, 8},
+			ans{[][]int{
+				[]int{1, 7},
+				[]int{1, 2, 5},
+				[]int{2, 6},
+				[]int{1, 1, 6},
+			}},
 		},
-	
+
 		// 如需多个测试，可以复制上方元素。
 	}
 
@@ -41,6 +47,6 @@ func Test_Problem0040(t *testing.T) {
 		a, p := q.ans, q.para
 		fmt.Printf("~~%v~~\n", p)
 
-		ast.Equal(a.one, (p.one), "输入:%v", p)
+		ast.Equal(a.one, combinationSum2(p.candidates, p.target), "输入:%v", p)
 	}
 }
