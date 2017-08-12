81b0b866328fcae69b2d8a7746d937268f73e4f4
aQua <aQua@aQuaYi.com>
2017-08-12 23:01:40 +0800

    finish Problem 43

--- Algorithms/0043.multiply-strings/README.md
+++ Algorithms/0043.multiply-strings/README.md
@@ -10,8 +10,11 @@ Note:
 1. You must not use any `built-in BigInteger library` or `convert the inputs to integer directly`.
 
 ## 解题思路
+1. 利用[]int{}记录中间计算过程，此时不考虑进位，例如，"123" 和 "65" 的记录为 []int{6,17,28,15}
+1. 统一处理进位，记录变为[]int{7,9,9,5}
+1. 转换为string，返回
 
-
+细节见程序注释。
 ## 总结
 
 
--- Algorithms/0043.multiply-strings/multiply-strings.go
+++ Algorithms/0043.multiply-strings/multiply-strings.go
@@ -5,28 +5,40 @@ func multiply(num1 string, num2 string) string {
 		return "0"
 	}
 
-	base := []byte(num1)
-	count := []byte(num2)
-	temp := make([]int, len(num1)+len(num2))
+	// string转换成[]byte，容易取得相应位上的具体值
+	bsi := []byte(num1)
+	bsj := []byte(num2)
 
-	for i := 0; i < len(base); i++ {
-		for j := 0; j < len(count); j++ {
-			temp[i+j+1] += int(base[i]-'0') * int(count[j]-'0')
+	// temp 的长度只可能为 len(num1)+len(num2) 或 len(num1)+len(num2)-1
+	// 先选长的，免得位不够
+	temp := make([]int, len(num1)+len(num2))
+	for i := 0; i < len(bsi); i++ {
+		for j := 0; j < len(bsj); j++ {
+			// 每个位上的乘积，可以直接存入 temp 中相应的位置
+			temp[i+j+1] += int(bsi[i]-'0') * int(bsj[j]-'0')
 		}
 	}
 
+	// 统一处理进位
 	for i := len(temp) - 1; i > 0; i-- {
 		temp[i-1] += temp[i] / 10
 		temp[i] = temp[i] % 10
 	}
 
+	// num1 和 num2 较小的时候，temp的首位为0
+	// 为避免输出结果以0开头，需要去掉temp的0首位
 	if temp[0] == 0 {
 		temp = temp[1:]
 	}
 
+	// 转换结果
+	// temp 选用为[]int，而不是[]byte，是因为
+	// golang中，byte的基础结构是uint8，最大值为255。
+	// 不考虑进位的话，temp会溢出
 	res := make([]byte, len(temp))
 	for i := 0; i < len(temp); i++ {
 		res[i] = '0' + byte(temp[i])
 	}
+
 	return string(res)
 }
