dc59df3575bd0632ac0ec0ed1d888483feada457
aQua <aQua@aQuaYi.com>
2017-08-12 18:46:19 +0800

    无法通过时长限制，在本机都无法通过。

--- Algorithms/0041.first-missing-positive/first-missing-positive.go
+++ Algorithms/0041.first-missing-positive/first-missing-positive.go
@@ -1,26 +1,34 @@
 package Problem0041
 
-import (
-	"fmt"
-)
-
 func firstMissingPositive(nums []int) int {
+	// 整理 nums ，让 nums[k] == k+1，只要 k+1 存在于 nums 中
 	for i := 0; i < len(nums); i++ {
 
-		fmt.Println(i)
+		// fmt.Println(i)
 
 		for 0 <= nums[i]-1 && nums[i]-1 < len(nums) && nums[i] != nums[nums[i]-1] {
-			//  0 <= nums[i]-1 && nums[i]-1 < len(nums) 是为了防止nums[nums[i]-1]溢出
+			// 当 for 的判断语句成立时，
+			// nums[i]-1 就是 k ，nums[i] 的值是 k+1
+			// nums[i] != nums[nums[i]-1] 即是 k+1 != nums[k] ，这说明
+			// 1. k+1 存在与 nums 中，
+			// 2. k+1 还没有在他该在的 nums[k] 中
+			// 通过互换，让 k+1 到 nums[k] 中去
 			nums[i], nums[nums[i]-1] = nums[nums[i]-1], nums[i]
 
-			fmt.Println(nums)
+			// fmt.Println(nums)
 
+			// 使用 for 而不是 if 是因为
+			// nums[i] 中的新值，有可能是另一个 k+1 ，需要再让其归为
+			// 如果使用 if ，而这个新的 k+1 又只有一个的话，
+			// 这个新的 k+1 不会被处理到，不会被放在 nums[k] 中
 		}
 	}
+	// 循环结束后，所有 1<=k+1<=len(nums) 且 k+1 存在于nums中，都会被存放于 nums[k] 中
 
-	for i := range nums {
-		if nums[i] != i+1 {
-			return i + 1
+	// 整理后，第一个不存在的 k+1 就是答案
+	for k := range nums {
+		if nums[k] != k+1 {
+			return k + 1
 		}
 	}
 	return len(nums) + 1
--- Algorithms/0042.trapping-rain-water/trapping-rain-water.go
+++ Algorithms/0042.trapping-rain-water/trapping-rain-water.go
@@ -1,2 +1,69 @@
 package Problem0042
 
+func trap(height []int) int {
+	height = trim(height)
+	res := 0
+
+	for len(height) > 2 {
+		for i := 1; i < len(height)-1; i++ {
+			if hasHisherBefore(height, i) && hasHisherAfter(height, i) {
+				height[i]++
+				res++
+			}
+		}
+		height = down(height)
+	}
+
+	return res
+}
+
+func hasHisherBefore(nums []int, index int) bool {
+	for i := index - 1; i >= 0; i-- {
+		if nums[i] > nums[index] {
+			return true
+		}
+	}
+	return false
+}
+func hasHisherAfter(nums []int, index int) bool {
+	for i := index + 1; i < len(nums); i++ {
+		if nums[i] > nums[index] {
+			return true
+		}
+	}
+	return false
+}
+
+func trim(nums []int) []int {
+	i := 0
+	length := len(nums)
+	for i < len(nums) {
+		if nums[i] != 0 {
+			nums = nums[i:]
+			break
+		}
+		i++
+	}
+
+	if i == length {
+		return []int{}
+	}
+
+	for i := len(nums) - 1; i >= 0; i-- {
+		if nums[i] != 0 {
+			nums = nums[:i+1]
+			break
+		}
+	}
+	return nums
+}
+
+func down(nums []int) []int {
+	for i := 0; i < len(nums); i++ {
+		if nums[i] > 0 {
+			nums[i]--
+		}
+	}
+
+	return trim(nums)
+}
