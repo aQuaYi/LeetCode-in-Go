e08f0cfde66964edab95b23e6584a6cf2a8e8b9b
aQua <aQua@aQuaYi.com>
2017-08-14 15:55:12 +0800

    problem 46 通过单元测试

--- Algorithms/0046.permutations/permutations.go
+++ Algorithms/0046.permutations/permutations.go
@@ -1,2 +1,23 @@
 package Problem0046
 
+func permute(nums []int) [][]int {
+	ress := [][]int{}
+
+	makePermute(nums, []int{}, &ress)
+
+	return ress
+}
+
+func makePermute(nums, res []int, ress *[][]int) {
+	if len(nums) == 0 {
+		*ress = append(*ress, res)
+	}
+
+	for i, n := range nums {
+		temp := make([]int, 0, len(nums))
+		temp = append(temp, nums[:i]...)
+		temp = append(temp, nums[i+1:]...)
+
+		makePermute(temp, append(res[:len(res):len(res)], n), ress)
+	}
+}
--- Algorithms/0046.permutations/permutations_test.go
+++ Algorithms/0046.permutations/permutations_test.go
@@ -1,8 +1,8 @@
 package Problem0046
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -15,13 +15,13 @@ type question struct {
 // para 是参数
 // one 代表第一个参数
 type para struct {
-	one string
+	one []int
 }
 
 // ans 是答案
 // one 代表第一个答案
 type ans struct {
-	one string
+	one [][]int
 }
 
 func Test_Problem0046(t *testing.T) {
@@ -30,10 +30,17 @@ func Test_Problem0046(t *testing.T) {
 	qs := []question{
 
 		question{
-			para{""},
-			ans{""},
+			para{[]int{1, 2, 3}},
+			ans{[][]int{
+				[]int{1, 2, 3},
+				[]int{1, 3, 2},
+				[]int{2, 1, 3},
+				[]int{2, 3, 1},
+				[]int{3, 1, 2},
+				[]int{3, 2, 1},
+			}},
 		},
-	
+
 		// 如需多个测试，可以复制上方元素。
 	}
 
@@ -41,6 +48,6 @@ func Test_Problem0046(t *testing.T) {
 		a, p := q.ans, q.para
 		fmt.Printf("~~%v~~\n", p)
 
-		ast.Equal(a.one, (p.one), "输入:%v", p)
+		ast.Equal(a.one, permute(p.one), "输入:%v", p)
 	}
 }
