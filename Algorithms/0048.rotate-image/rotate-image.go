f8df45f2794cf46c6afbb86a306eacc79fea59dd
aQua <aQua@aQuaYi.com>
2017-08-14 17:54:16 +0800

    adding Problem 48

--- Algorithms/0048.rotate-image/rotate-image.go
+++ Algorithms/0048.rotate-image/rotate-image.go
@@ -1,7 +1,10 @@
 package Problem0048
 
-func rotate(matrix [][]int) {
-	for i := 0; i < len(matrix)/2; i++ {
-
+func rotate(m [][]int) {
+	n := len(m)
+	for i := 0; i < n/2; i++ {
+		for j := i; j < n-i-1; j++ {
+			m[j+i][i]
+		}
 	}
 }
