716fb77e8e0f9cb5755391046f4e2f185f034702
aQua <aQua@aQuaYi.com>
2017-08-14 16:37:14 +0800

    finish Problem 46

--- Algorithms/0046.permutations/README.md
+++ Algorithms/0046.permutations/README.md
@@ -1,11 +1,22 @@
 # [46. Permutations](https://leetcode.com/problems/permutations/)
 
 ## 题目
+Given a collection of distinct numbers, return all possible permutations.
 
+For example,
+`[1,2,3]` have the following permutations:
+```
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+```
 
 ## 解题思路
-
+按照手写排列的思路，编写递归程序即可。
 
 ## 总结
-
-
--- Algorithms/0046.permutations/permutations.go
+++ Algorithms/0046.permutations/permutations.go
@@ -1,23 +1,44 @@
 package Problem0046
 
 func permute(nums []int) [][]int {
-	ans := [][]int{}
+	n := len(nums)
+	// vector 是一组可能的解答
+	vector := make([]int, n)
+	// taken[i] == true 表示 nums[i] 已经出现在了 vector 中
+	taken := make([]bool, n)
 
-	makePermute(nums, []int{}, &ans)
+	var ans [][]int
+
+	makePermutation(0, n, nums, vector, taken, &ans)
 
 	return ans
 }
 
-func makePermute(nums, res []int, ans *[][]int) {
-	if len(nums) == 0 {
-		*ans = append(*ans, res)
+// 这个方式和我原来的方式相比，
+// 增加了比较的次数
+// 但是，减少了切片生成的次数。
+// 所以，速度会快一些。
+func makePermutation(cur, n int, nums, vector []int, taken []bool, ans *[][]int) {
+	if cur == n {
+		tmp := make([]int, n)
+		copy(tmp, vector)
+		*ans = append(*ans, tmp)
+		return
 	}
 
-	for i, n := range nums {
-		temp := make([]int, 0, len(nums))
-		temp = append(temp, nums[:i]...)
-		temp = append(temp, nums[i+1:]...)
+	for i := 0; i < n; i++ {
+		if !taken[i] {
+			// 准备使用 nums[i]，所以，taken[i] == true
+			taken[i] = true
+			// NOTICE: 是 vector[cur]
+			vector[cur] = nums[i]
 
-		makePermute(temp, append(res[:len(res):len(res)], n), ans)
+			makePermutation(cur+1, n, nums, vector, taken, ans)
+
+			// 下一个循环中
+			// vector[cur] = nums[i+1]
+			// 所以，在这个循环中，恢复 nums[i] 自由
+			taken[i] = false
+		}
 	}
 }
