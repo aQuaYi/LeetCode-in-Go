69109d4d051022a676fcd5d3d9db9d4f13ac69f0
aQua <aQua@aQuaYi.com>
2017-08-14 10:28:29 +0800

    problem 45 通过了测试

--- Algorithms/0045.jump-game-ii/jump-game-ii.go
+++ Algorithms/0045.jump-game-ii/jump-game-ii.go
@@ -1,26 +1,27 @@
 package Problem0045
 
 func jump(nums []int) int {
-	res := len(nums)
+	// 虽然题目已经假设了 len(nums) > 0
+	// 但是，我还是做了长度检查，因为后面使用了切片的索引号，检查一下，养成好习惯。
+	if len(nums) == 0 {
+		panic("输入nums的长度为0")
+	}
 
-	jumpToNext(nums, &res, 0, 0)
-
-	return res
+	return jumpToNext(nums[:len(nums)-1], 0)
 }
 
-func jumpToNext(nums []int, res *int, i, temp int) {
-	if temp > *res {
-		return
+func jumpToNext(nums []int, res int) int {
+	if len(nums) == 0 {
+		return res
 	}
 
-	if i >= len(nums)-1 {
-		if *res > temp {
-			*res = temp
+	end := 0
+
+	for i := len(nums) - 1; i >= 0; i-- {
+		if nums[i]+i >= len(nums) {
+			end = i
 		}
-		return
 	}
 
-	for j := nums[i]; j > 0; j-- {
-		jumpToNext(nums, res, i+j, temp+1)
-	}
+	return jumpToNext(nums[:end], res+1)
 }
