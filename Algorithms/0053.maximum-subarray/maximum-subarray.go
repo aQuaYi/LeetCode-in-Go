d1ab1519aa4e52602b2ea1fe7dbb1156d4ef4dd6
aQua <aQua@aQuaYi.com>
2017-08-15 23:47:25 +0800

    53 通过单元测试

--- Algorithms/0053.maximum-subarray/maximum-subarray.go
+++ Algorithms/0053.maximum-subarray/maximum-subarray.go
@@ -1,5 +1,16 @@
 package Problem0053
 
 func maxSubArray(nums []int) int {
+	max := 0
+	for i := 0; i < len(nums); i++ {
+		temp := 0
+		for j := i; j < len(nums); j++ {
+			temp += nums[j]
+			if temp > max {
+				max = temp
+			}
+		}
+	}
 
+	return max
 }
