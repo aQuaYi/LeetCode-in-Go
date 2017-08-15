9971a01e8c9118a5b164435e6110e54092ccef4f
aQua <aQua@aQuaYi.com>
2017-08-15 11:51:08 +0800

    problem 51 没有通过leetcode的测试，因为我没有考虑第一行相同，而后面不同的情况。

--- Algorithms/0051.n-queens/README.md
+++ Algorithms/0051.n-queens/README.md
@@ -1,10 +1,34 @@
 # [51. N-Queens](https://leetcode.com/problems/n-queens/)
 
 ## 题目
+The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
 
+![8-queens](8-queens.png)
 
+Given an integer n, return all distinct solutions to the n-queens puzzle.
+
+Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space respectively.
+
+For example,
+There exist two distinct solutions to the 4-queens puzzle:
+```
+[
+ [".Q..",  // Solution 1
+  "...Q",
+  "Q...",
+  "..Q."],
+
+ ["..Q.",  // Solution 2
+  "Q...",
+  "...Q",
+  ".Q.."]
+]
+```
 ## 解题思路
-
+题目要求，在以下地方都没有其他的Queen
+1. 所在的行
+1. 所在的列
+1. 周围的8个空格中
 
 ## 总结
 
--- Algorithms/0051.n-queens/n-queens.go
+++ Algorithms/0051.n-queens/n-queens.go
@@ -1,2 +1,75 @@
 package Problem0051
 
+func solveNQueens(n int) [][]string {
+	res := [][]string{}
+	col := 1
+	ok := false
+	for col <= n {
+		ans := makeTemp(n)
+		col, ok = solve(ans, 1, col)
+		if !ok {
+			return res
+		}
+
+		temp := make([]string, n)
+		for i := 1; i <= n; i++ {
+			for j := 1; j <= n; j++ {
+				if ans[i][j] != 'Q' {
+					ans[i][j] = '.'
+				}
+			}
+			temp[i-1] = string(ans[i][1 : n+1])
+		}
+
+		res = append(res, temp)
+
+		col++
+	}
+
+	return res
+}
+
+func makeTemp(n int) [][]byte {
+	m := make([][]byte, n+2)
+	for i := 0; i < n+2; i++ {
+		m[i] = make([]byte, n+2)
+	}
+	return m
+}
+
+func solve(m [][]byte, row, col int) (int, bool) {
+	if row == len(m)-1 {
+		return 0, true
+	}
+	for i := col; i < len(m)-1; i++ {
+		if isAvaliable(m, row, i) {
+			m[row][i] = 'Q'
+			_, ok := solve(m, row+1, 1)
+			if ok {
+				return i, true
+			}
+			m[row][i] = '.'
+		}
+	}
+
+	return 0, false
+}
+
+func isAvaliable(m [][]byte, row, col int) bool {
+
+	for i := row - 1; i <= row+1; i++ {
+		for j := col - 1; j <= col+1; j++ {
+			if m[i][j] == 'Q' {
+				return false
+			}
+		}
+	}
+
+	for i := 1; i < len(m)-1; i++ {
+		if m[row][i] == 'Q' || m[i][col] == 'Q' {
+			return false
+		}
+	}
+
+	return true
+}
