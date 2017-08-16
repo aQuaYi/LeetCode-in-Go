9e7a370f24966cb817db7a6933c4b949a71947dd
aQua <aQua@aQuaYi.com>
2017-08-16 11:52:35 +0800

    643 improving，解题时间从1182ms到285ms

--- Algorithms/0643.maximum-average-subarray-i/maximum-average-subarray-i.go
+++ Algorithms/0643.maximum-average-subarray-i/maximum-average-subarray-i.go
@@ -1,13 +1,16 @@
 package Problem0643
 
 func findMaxAverage(nums []int, k int) float64 {
-	max := -10001 * k
+	temp := 0
+	for j := 0; j < k; j++ {
+		temp += nums[j]
+	}
 
-	for i := 0; i+k-1 < len(nums); i++ {
-		temp := 0
-		for j := i; j < i+k; j++ {
-			temp += nums[j]
-		}
+	max := temp
+
+	for i, j := 0, k; j < len(nums); i, j = i+1, j+1 {
+		temp = temp - nums[i] + nums[j]
+
 		if max < temp {
 			max = temp
 		}
