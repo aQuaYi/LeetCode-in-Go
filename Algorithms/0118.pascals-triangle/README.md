af179547f04416ba0fda1de6f98a651f107bb912
aQua <aQua@aQuaYi.com>
2017-08-16 10:54:05 +0800

    118 accepted

--- Algorithms/0118.pascals-triangle/README.md
+++ Algorithms/0118.pascals-triangle/README.md
@@ -1,8 +1,18 @@
 # [118. Pascal's Triangle](https://leetcode.com/problems/pascals-triangle/)
 
 ## 题目
+Given numRows, generate the first numRows of Pascal's triangle.
 
-
+For example, given numRows = 5, Return
+```
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+```
 ## 解题思路
 
 
--- Algorithms/0118.pascals-triangle/pascals-triangle.go
+++ Algorithms/0118.pascals-triangle/pascals-triangle.go
@@ -1,2 +1,31 @@
 package Problem0118
 
+func generate(numRows int) [][]int {
+	res := [][]int{}
+	if numRows == 0 {
+		return res
+	}
+
+	res = append(res, []int{1})
+	if numRows == 1 {
+		return res
+	}
+
+	for i := 1; i < numRows; i++ {
+		res = append(res, genNext(res[i-1]))
+	}
+
+	return res
+}
+
+func genNext(p []int) []int {
+	temp := make([]int, 1)
+	temp = append(temp, p...)
+	temp = append(temp, 0)
+
+	for i := len(temp) - 1; i > 0; i-- {
+		temp[i] += temp[i-1]
+	}
+
+	return temp[1:]
+}
