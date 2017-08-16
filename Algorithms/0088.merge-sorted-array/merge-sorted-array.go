24caa069261be940a2930f6d1216155147ad7ee4
aQua <aQua@aQuaYi.com>
2017-08-16 10:35:20 +0800

    88 accepted

--- Algorithms/0088.merge-sorted-array/merge-sorted-array.go
+++ Algorithms/0088.merge-sorted-array/merge-sorted-array.go
@@ -1,2 +1,32 @@
 package Problem0088
 
+// 本题的要求是，把nums1的前m项和nums2的前n项合并，放入nums1中。
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	// 深度复制 nums1
+	temp := make([]int, m)
+	copy(temp, nums1)
+
+	j, k := 0, 0
+	for i := 0; i < len(nums1); i++ {
+		// nums2用完了
+		if k >= n {
+			nums1[i] = temp[j]
+			j++
+			continue
+		}
+		// temp 用完了
+		if j >= m {
+			nums1[i] = nums2[k]
+			k++
+			continue
+		}
+		// 比较后，放入
+		if temp[j] < nums2[k] {
+			nums1[i] = temp[j]
+			j++
+		} else {
+			nums1[i] = nums2[k]
+			k++
+		}
+	}
+}
--- Algorithms/0088.merge-sorted-array/merge-sorted-array_test.go
+++ Algorithms/0088.merge-sorted-array/merge-sorted-array_test.go
@@ -1,8 +1,8 @@
 package Problem0088
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -15,13 +15,16 @@ type question struct {
 // para 是参数
 // one 代表第一个参数
 type para struct {
-	one string
+	one []int
+	m   int
+	two []int
+	n   int
 }
 
 // ans 是答案
 // one 代表第一个答案
 type ans struct {
-	one string
+	one []int
 }
 
 func Test_Problem0088(t *testing.T) {
@@ -30,10 +33,30 @@ func Test_Problem0088(t *testing.T) {
 	qs := []question{
 
 		question{
-			para{""},
-			ans{""},
+			para{[]int{0}, 0, []int{1}, 1},
+			ans{[]int{1}},
 		},
-	
+
+		question{
+			para{[]int{1, 0}, 1, []int{2}, 1},
+			ans{[]int{1, 2}},
+		},
+
+		question{
+			para{[]int{}, 0, []int{1}, 1},
+			ans{[]int{}},
+		},
+
+		question{
+			para{[]int{1, 3, 5, 7}, 4, []int{2, 4}, 2},
+			ans{[]int{1, 2, 3, 4}},
+		},
+
+		question{
+			para{[]int{1, 3, 5, 7}, 4, []int{2, 2}, 2},
+			ans{[]int{1, 2, 2, 3}},
+		},
+
 		// 如需多个测试，可以复制上方元素。
 	}
 
@@ -41,6 +64,7 @@ func Test_Problem0088(t *testing.T) {
 		a, p := q.ans, q.para
 		fmt.Printf("~~%v~~\n", p)
 
-		ast.Equal(a.one, (p.one), "输入:%v", p)
+		merge(p.one, p.m, p.two, p.n)
+		ast.Equal(a.one, p.one, "输入:%v", p)
 	}
 }
