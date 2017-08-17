e06088a08c7e23849a72a28479196d5ad4b2c7a1
aQua <aQua@aQuaYi.com>
2017-08-17 17:41:19 +0800

    605 finish

--- Algorithms/0605.can-place-flowers/can-place-flowers.go
+++ Algorithms/0605.can-place-flowers/can-place-flowers.go
@@ -3,12 +3,14 @@ package Problem0605
 func canPlaceFlowers(bed []int, n int) bool {
 	l := len(bed)
 	for i := 0; i < l; i++ {
-		// if 的判断语句分别检查了，i，i-1 和 i+1的情况
-		if bed[i] == 0 &&
-			((i+1 < l && bed[i+1] == 0) || i+1 >= l) &&
-			((i-1 >= 0 && bed[i-1] == 0) || i-1 < 0) {
+		if bed[i] == 0 && // 检查 i 的值
+			((i+1 < l && bed[i+1] == 0) || i+1 >= l) && // 检查 i+1 的值
+			((i-1 >= 0 && bed[i-1] == 0) || i-1 < 0) { // 检查 i-1 的值
 			bed[i] = 1
 			n--
+			if n <= 0 {
+				return true
+			}
 		}
 	}
 
