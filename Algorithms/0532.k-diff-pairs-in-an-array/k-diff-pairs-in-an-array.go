8f63a177042ca9164c53ae2de8e7a4105b9f05d7
aQua <aQua@aQuaYi.com>
2017-08-19 22:52:51 +0800

    532 accepted

--- Algorithms/0532.k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.go
+++ Algorithms/0532.k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.go
@@ -1,2 +1,52 @@
 package Problem0532
 
+import (
+	"sort"
+)
+
+func findPairs(nums []int, k int) int {
+	switch {
+	case k < 0:
+		return 0
+	case k == 0:
+		return handleZero(nums)
+	default:
+		return handle(nums, k)
+	}
+}
+
+func handleZero(nums []int) int {
+	sort.Ints(nums)
+	ans := 0
+	for i := 0; i+1 < len(nums); i++ {
+		if nums[i] == nums[i+1] {
+			ans++
+		}
+		for i+1 < len(nums) && nums[i] == nums[i+1] {
+			i++
+		}
+	}
+	return ans
+}
+
+func handle(nums []int, k int) int {
+	sort.Ints(nums)
+	uniqueNums := make([]int, 0, len(nums))
+	uniqueMap := make(map[int]bool, len(nums))
+	for i := 0; i < len(nums); i++ {
+		uniqueNums = append(uniqueNums, nums[i])
+		uniqueMap[nums[i]] = true
+		for i+1 < len(nums) && nums[i] == nums[i+1] {
+			i++
+		}
+	}
+
+	ans := 0
+
+	for i := 0; i < len(uniqueNums); i++ {
+		if uniqueMap[uniqueNums[i]+k] {
+			ans++
+		}
+	}
+	return ans
+}
--- Algorithms/0532.k-diff-pairs-in-an-array/k-diff-pairs-in-an-array_test.go
+++ Algorithms/0532.k-diff-pairs-in-an-array/k-diff-pairs-in-an-array_test.go
@@ -1,8 +1,8 @@
 package Problem0532
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -15,13 +15,14 @@ type question struct {
 // para 是参数
 // one 代表第一个参数
 type para struct {
-	one string
+	one []int
+	k   int
 }
 
 // ans 是答案
 // one 代表第一个答案
 type ans struct {
-	one string
+	one int
 }
 
 func Test_Problem0532(t *testing.T) {
@@ -30,10 +31,30 @@ func Test_Problem0532(t *testing.T) {
 	qs := []question{
 
 		question{
-			para{""},
-			ans{""},
+			para{[]int{1, 2, 3, 4, 5}, -1},
+			ans{0},
 		},
-	
+
+		question{
+			para{[]int{3, 1, 4, 1, 5}, 2},
+			ans{2},
+		},
+
+		question{
+			para{[]int{1, 2, 3, 4, 5}, 1},
+			ans{4},
+		},
+
+		question{
+			para{[]int{3, 1, 4, 1, 5}, 0},
+			ans{1},
+		},
+
+		question{
+			para{[]int{3, 3, 5, 5, 5}, 2},
+			ans{1},
+		},
+
 		// 如需多个测试，可以复制上方元素。
 	}
 
@@ -41,6 +62,6 @@ func Test_Problem0532(t *testing.T) {
 		a, p := q.ans, q.para
 		fmt.Printf("~~%v~~\n", p)
 
-		ast.Equal(a.one, (p.one), "输入:%v", p)
+		ast.Equal(a.one, findPairs(p.one, p.k), "输入:%v", p)
 	}
 }
