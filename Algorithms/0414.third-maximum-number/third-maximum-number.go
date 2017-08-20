646a24481368ab184b669aa5ea5a0e38d38e3a3a
aQua <aQua@aQuaYi.com>
2017-08-21 00:04:14 +0800

    414 accepted

--- Algorithms/0414.third-maximum-number/third-maximum-number.go
+++ Algorithms/0414.third-maximum-number/third-maximum-number.go
@@ -1,2 +1,25 @@
 package Problem0414
 
+import (
+	"math"
+)
+
+func thirdMax(nums []int) int {
+	max1, max2, max3 := math.MinInt64, math.MinInt64, math.MinInt64
+	for _, n := range nums {
+		switch {
+		case max1 < n:
+			max3, max2, max1 = max2, max1, n
+		case max2 < n && n != max1:
+			max3, max2 = max2, n
+		case max3 < n && n != max2 && n != max1:
+			max3 = n
+		}
+	}
+
+	if max3 == math.MinInt64 || max3 == max2 {
+		return max1
+	}
+
+	return max3
+}
--- Algorithms/0414.third-maximum-number/third-maximum-number_test.go
+++ Algorithms/0414.third-maximum-number/third-maximum-number_test.go
@@ -1,8 +1,8 @@
 package Problem0414
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -15,13 +15,13 @@ type question struct {
 // para 是参数
 // one 代表第一个参数
 type para struct {
-	one string
+	one []int
 }
 
 // ans 是答案
 // one 代表第一个答案
 type ans struct {
-	one string
+	one int
 }
 
 func Test_Problem0414(t *testing.T) {
@@ -30,10 +30,40 @@ func Test_Problem0414(t *testing.T) {
 	qs := []question{
 
 		question{
-			para{""},
-			ans{""},
+			para{[]int{1, 2, 2, 5, 3, 5}},
+			ans{2},
 		},
-	
+
+		question{
+			para{[]int{1, 2, 3}},
+			ans{1},
+		},
+
+		question{
+			para{[]int{2, 2, 3, 1}},
+			ans{1},
+		},
+
+		question{
+			para{[]int{2, 3, 3}},
+			ans{3},
+		},
+
+		question{
+			para{[]int{3, 2, 1}},
+			ans{1},
+		},
+
+		question{
+			para{[]int{2, 2, 3}},
+			ans{3},
+		},
+
+		question{
+			para{[]int{1, 2}},
+			ans{2},
+		},
+
 		// 如需多个测试，可以复制上方元素。
 	}
 
@@ -41,6 +71,6 @@ func Test_Problem0414(t *testing.T) {
 		a, p := q.ans, q.para
 		fmt.Printf("~~%v~~\n", p)
 
-		ast.Equal(a.one, (p.one), "输入:%v", p)
+		ast.Equal(a.one, thirdMax(p.one), "输入:%v", p)
 	}
 }
