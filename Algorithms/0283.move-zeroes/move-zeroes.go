19ce4f72901c45be975f2f1c8ce94fb7ce615129
aQua <aQua@aQuaYi.com>
2017-08-21 23:30:37 +0800

    283 finish

--- Algorithms/0283.move-zeroes/move-zeroes.go
+++ Algorithms/0283.move-zeroes/move-zeroes.go
@@ -1,19 +1,21 @@
 package Problem0283
 
 func moveZeroes(nums []int) {
-	indexZero := 0
-	for indexZero < len(nums) && nums[indexZero] != 0 {
-		indexZero++
+	l := len(nums)
+	i, j := 0, 0
+
+	for j < l {
+		if nums[j] != 0 {
+			nums[i] = nums[j]
+			i++
+		}
+		j++
 	}
 
-	i := indexZero + 1
-	for i < len(nums) {
-		if nums[i] != 0 {
-			nums[i], nums[indexZero] = nums[indexZero], nums[i]
-			for indexZero < len(nums) && nums[indexZero] != 0 {
-				indexZero++
-			}
-		}
+	// 此时，i 以前的位置上，保存了nums中所有的非零数
+	// 所以，只要把 nums[i:] 都置零，即可
+	for i < l {
+		nums[i] = 0
 		i++
 	}
 }
