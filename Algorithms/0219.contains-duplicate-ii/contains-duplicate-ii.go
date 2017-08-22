62e64e9d6cf7af704c20fc1ef538f4ab96e3fdba
aQua <aQua@aQuaYi.com>
2017-08-22 09:51:56 +0800

    219 accepted 1126ms，最快的31ms

--- Algorithms/0219.contains-duplicate-ii/contains-duplicate-ii.go
+++ Algorithms/0219.contains-duplicate-ii/contains-duplicate-ii.go
@@ -1,5 +1,13 @@
 package Problem0219
 
 func containsNearbyDuplicate(nums []int, k int) bool {
+	for i, v := range nums {
+		for j := i + 1; j <= i+k && j < len(nums); j++ {
+			if v == nums[j] {
+				return true
+			}
+		}
+	}
 
+	return false
 }
--- Algorithms/0219.contains-duplicate-ii/contains-duplicate-ii_test.go
+++ Algorithms/0219.contains-duplicate-ii/contains-duplicate-ii_test.go
@@ -1,8 +1,8 @@
 package Problem0219
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -15,12 +15,12 @@ type question struct {
 // para 是参数
 type para struct {
 	nums []int
- k int
+	k    int
 }
 
 // ans 是答案
 type ans struct {
-	one  bool 
+	one bool
 }
 
 func Test_Problem0219(t *testing.T) {
@@ -30,13 +30,23 @@ func Test_Problem0219(t *testing.T) {
 
 		question{
 			para{
-					,
+				[]int{1, 2, 1, 3, 4, 5},
+				2,
 			},
 			ans{
-					,
+				true,
 			},
 		},
-	
+
+		question{
+			para{
+				[]int{1, 2, 3, 4, 5},
+				2,
+			},
+			ans{
+				false,
+			},
+		},
 		// 如需多个测试，可以复制上方元素。
 	}
 
@@ -44,6 +54,6 @@ func Test_Problem0219(t *testing.T) {
 		a, p := q.ans, q.para
 		fmt.Printf("~~%v~~\n", p)
 
-		ast.Equal(a.one, containsNearbyDuplicate(p.one), "输入:%v", p)
+		ast.Equal(a.one, containsNearbyDuplicate(p.nums, p.k), "输入:%v", p)
 	}
 }
