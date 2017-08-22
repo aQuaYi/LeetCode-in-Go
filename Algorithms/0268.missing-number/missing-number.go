d55b1a7930868a5b2ab36671be1bb26eff7882a6
aQua <aQua@aQuaYi.com>
2017-08-22 09:12:51 +0800

    268 improve

--- Algorithms/0268.missing-number/missing-number.go
+++ Algorithms/0268.missing-number/missing-number.go
@@ -7,7 +7,7 @@ func missingNumber(nums []int) int {
 		xor ^= i ^ n
 	}
 	// 所有的 i 再加上 len(nums)，就相当于 0,1,...,n。
-	// 利用 相同的数 异或 为0，及其交换律
+	// 利用 相同的数异或为0，及其交换律
 	// xor 最后的值，就是那个缺失的数
 	return xor ^ len(nums)
 }
