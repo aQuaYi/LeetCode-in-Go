74eaea05a3b88596cf11c7278ef6c5a7ad4161d9
aQua <aQua@aQuaYi.com>
2017-08-23 22:49:41 +0800

    75 accepted，耗时3ms，最快的耗时0ms

--- Algorithms/0075.sort-colors/sort-colors.go
+++ Algorithms/0075.sort-colors/sort-colors.go
@@ -1,5 +1,59 @@
 package Problem0075
 
-func sortColors(nums []int)  {
+func sortColors(nums []int) {
+	length := len(nums)
+	if length == 0 {
+		return
+	}
 
+	if only2 := setHead(nums); only2 {
+		return
+	}
+
+	// 三路快排
+	i, j, k := 0, 1, length-1
+	for j <= k {
+		switch {
+		case nums[j] < nums[i]:
+			nums[i], nums[j] = nums[j], nums[i]
+			i++
+			j++
+		case nums[i] < nums[j]:
+			nums[j], nums[k] = nums[k], nums[j]
+			k--
+		default:
+			j++
+		}
+	}
+	return
+}
+
+// 如果 nums 中有 1 ， nums[0] 应该为 1
+// 否则，如果 nums 中还有 0， nums[0] 应该为 0
+// 否则，说明 nums 中只有 2。
+func setHead(nums []int) bool {
+	i, l := 0, len(nums)
+	// 设置 nums[0] = 1
+	for i < l && nums[i] != 1 {
+		i++
+	}
+	if i < l {
+		nums[0], nums[i] = nums[i], nums[0]
+		return false
+	}
+
+	// nums 中没有 1
+	// 只好设置 nums[0] = 0
+	i = 0
+	for i < l && nums[i] != 0 {
+		i++
+	}
+
+	if i < l {
+		nums[0], nums[i] = nums[i], nums[0]
+		return false
+	}
+
+	// nums 中只有 2
+	return true
 }
--- Algorithms/0075.sort-colors/sort-colors_test.go
+++ Algorithms/0075.sort-colors/sort-colors_test.go
@@ -29,13 +29,46 @@ func Test_Problem0075(t *testing.T) {
 
 		question{
 			para{
+				[]int{0, 2, 0, 2, 0, 2},
+			},
+			ans{
+				[]int{0, 0, 0, 2, 2, 2},
+			},
+		},
+
+		question{
+			para{
+				[]int{1, 2, 1, 2, 1, 2},
+			},
+			ans{
+				[]int{1, 1, 1, 2, 2, 2},
+			},
+		},
+
+		question{
+			para{
 				[]int{0, 1, 2, 0, 1, 2, 0, 1, 2},
 			},
 			ans{
 				[]int{0, 0, 0, 1, 1, 1, 2, 2, 2},
 			},
 		},
-
+		question{
+			para{
+				[]int{},
+			},
+			ans{
+				[]int{},
+			},
+		},
+		question{
+			para{
+				[]int{2, 2, 2, 2, 2, 2},
+			},
+			ans{
+				[]int{2, 2, 2, 2, 2, 2},
+			},
+		},
 		// 如需多个测试，可以复制上方元素。
 	}
 
