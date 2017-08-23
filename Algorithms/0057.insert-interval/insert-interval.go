6a77752aad224a519ef9b0d225ede2a07157c44e
aQua <aQua@aQuaYi.com>
2017-08-23 11:49:01 +0800

    57 accepted

---
+++ Algorithms/0057.insert-interval/insert-interval.go
@@ -0,0 +1,73 @@
+package Problem0057
+
+// Interval is a couple of int
+type Interval struct {
+	Start int
+	End   int
+}
+
+func insert(a []Interval, newItv Interval) []Interval {
+	lenA := len(a)
+
+	if lenA == 0 {
+		return []Interval{newItv}
+	}
+
+	if newItv.End < a[0].Start {
+		return append([]Interval{newItv}, a...)
+	}
+
+	if a[lenA-1].End < newItv.Start {
+		return append(a, newItv)
+	}
+
+	res := make([]Interval, 0, len(a))
+	for i := range a {
+		if isOverlap(a[i], newItv) {
+			newItv = merge(a[i], newItv)
+			if i == lenA-1 {
+				res = append(res, newItv)
+			}
+			continue
+		}
+
+		if a[i].End < newItv.Start {
+			res = append(res, a[i])
+			continue
+		}
+
+		if newItv.End < a[i].Start {
+			res = append(res, newItv)
+			res = append(res, a[i:]...)
+			break
+		}
+
+	}
+
+	return res
+}
+
+func isOverlap(a, b Interval) bool {
+	return (a.Start <= b.Start && b.Start <= a.End) || (a.Start <= b.End && b.End <= a.End) || (b.Start <= a.Start && a.Start <= b.End)
+}
+
+func merge(a, b Interval) Interval {
+	return Interval{
+		Start: min(a.Start, b.Start),
+		End:   max(a.End, b.End),
+	}
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
---
+++ Algorithms/0057.insert-interval/insert-interval_test.go
@@ -0,0 +1,140 @@
+package Problem0057
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type question struct {
+	para
+	ans
+}
+
+// para 是参数
+type para struct {
+	intervals   []Interval
+	newInterval Interval
+}
+
+// ans 是答案
+type ans struct {
+	one []Interval
+}
+
+func Test_Problem0057(t *testing.T) {
+	ast := assert.New(t)
+	qs := []question{
+
+		question{
+			para{[]Interval{
+				Interval{1, 3},
+				Interval{6, 9},
+			},
+				Interval{7, 8},
+			},
+			ans{[]Interval{
+				Interval{1, 3},
+				Interval{6, 9},
+			},
+			},
+		},
+
+		question{
+			para{[]Interval{
+				Interval{1, 3},
+				Interval{6, 9},
+			},
+				Interval{4, 5},
+			},
+			ans{[]Interval{
+				Interval{1, 3},
+				Interval{4, 5},
+				Interval{6, 9},
+			},
+			},
+		},
+
+		question{
+			para{[]Interval{
+				Interval{1, 3},
+				Interval{6, 9},
+			},
+				Interval{2, 5},
+			},
+			ans{[]Interval{
+				Interval{1, 5},
+				Interval{6, 9},
+			},
+			},
+		},
+
+		question{
+			para{[]Interval{
+				Interval{1, 3},
+				Interval{6, 9},
+			},
+				Interval{-1, 0},
+			},
+			ans{[]Interval{
+				Interval{-1, 0},
+				Interval{1, 3},
+				Interval{6, 9},
+			},
+			},
+		},
+
+		question{
+			para{[]Interval{
+				Interval{1, 3},
+				Interval{6, 9},
+			},
+				Interval{11, 20},
+			},
+			ans{[]Interval{
+				Interval{1, 3},
+				Interval{6, 9},
+				Interval{11, 20},
+			},
+			},
+		},
+
+		question{
+			para{[]Interval{},
+				Interval{2, 5},
+			},
+			ans{[]Interval{
+				Interval{2, 5},
+			},
+			},
+		},
+
+		// 如需多个测试，可以复制上方元素。
+	}
+
+	for _, q := range qs {
+		a, p := q.ans, q.para
+		fmt.Printf("~~%v~~\n", p)
+
+		ast.Equal(a.one, insert(p.intervals, p.newInterval), "输入:%v", p)
+	}
+}
+
+func Test_min(t *testing.T) {
+	ast := assert.New(t)
+
+	actual := min(1, 0)
+	expected := 0
+	ast.Equal(expected, actual)
+
+}
+
+func Test_max(t *testing.T) {
+	ast := assert.New(t)
+
+	actual := max(1, 0)
+	expected := 1
+	ast.Equal(expected, actual)
+
+}
