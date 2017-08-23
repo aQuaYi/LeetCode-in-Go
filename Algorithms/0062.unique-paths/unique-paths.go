c24597ec70368cca685ffcbf73f63886ce152b47
aQua <aQua@aQuaYi.com>
2017-08-23 16:17:19 +0800

    62 finish

--- Algorithms/0062.unique-paths/unique-paths.go
+++ Algorithms/0062.unique-paths/unique-paths.go
@@ -1,24 +1,32 @@
 package Problem0062
 
 func uniquePaths(m int, n int) int {
-	res := 0
+	// dp[i][j] 代表了，到达 (i,j) 格子的不同路径数目
+	dp := [][]int{}
 
-	search(0, m, 0, n, &res)
-
-	return res
-}
-
-func search(i, m, j, n int, res *int) {
-	if i == m-1 && j == n-1 {
-		*res++
-		return
+	// 创建棋盘
+	for i := 0; i < m; i++ {
+		tmp := make([]int, n)
+		dp = append(dp, tmp)
 	}
 
-	if i == m || j == n {
-		return
+	for i := 0; i < m; i++ {
+		// 到达第 0 列的格子，只有一条路径
+		dp[i][0] = 1
 	}
 
-	search(i+1, m, j, n, res)
+	for j := 0; j < n; j++ {
+		// 到达第 0 行的格子，只有一条路径
+		dp[0][j] = 1
+	}
 
-	search(i, m, j+1, n, res)
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			// 到达 (i,j) 格子的路径数目，等于
+			// 到达 上方格子 和 左边格子 路径数之和
+			dp[i][j] = dp[i-1][j] + dp[i][j-1]
+		}
+	}
+
+	return dp[m-1][n-1]
 }
