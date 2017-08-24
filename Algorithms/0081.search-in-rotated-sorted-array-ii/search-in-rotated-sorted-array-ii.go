0cb47a3680611a67b69e210bdbd88007fd4ad54f
aQua <aQua@aQuaYi.com>
2017-08-24 14:16:27 +0800

    81 accepted

---
+++ Algorithms/0081.search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.go
@@ -0,0 +1,29 @@
+package Problem0081
+
+func search(nums []int, target int) bool {
+	length := len(nums)
+	if length == 0 {
+		return false
+	}
+	k := 1
+	for k < len(nums) && nums[k-1] <= nums[k] {
+		k++
+	}
+
+	i, j := 0, length-1
+	for i <= j {
+		m := (i + j) / 2
+		med := (m + k) % length
+
+		switch {
+		case nums[med] < target:
+			i = m + 1
+		case target < nums[med]:
+			j = m - 1
+		default:
+			return true
+		}
+	}
+
+	return false
+}
---
+++ Algorithms/0081.search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii_test.go
@@ -0,0 +1,79 @@
+package Problem0081
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type question struct {
+	para
+	ans
+}
+
+// para 是参数
+type para struct {
+	nums   []int
+	target int
+}
+
+// ans 是答案
+type ans struct {
+	one bool
+}
+
+func Test_Problem0081(t *testing.T) {
+	ast := assert.New(t)
+
+	qs := []question{
+
+		question{
+			para{
+				[]int{4, 4, 5, 5, 6, 6, 7, 7, 0, 0, 1, 1, 2, 2},
+				6,
+			},
+			ans{
+				true,
+			},
+		},
+
+		question{
+			para{
+				[]int{4, 5, 6, 7, 0, 1, 2},
+				6,
+			},
+			ans{
+				true,
+			},
+		},
+
+		question{
+			para{
+				[]int{4, 5, 6, 7, 0, 1, 2},
+				3,
+			},
+			ans{
+				false,
+			},
+		},
+
+		question{
+			para{
+				[]int{},
+				3,
+			},
+			ans{
+				false,
+			},
+		},
+		// 如需多个测试，可以复制上方元素。
+	}
+
+	for _, q := range qs {
+		a, p := q.ans, q.para
+		fmt.Printf("~~%v~~\n", p)
+
+		ast.Equal(a.one, search(p.nums, p.target), "输入:%v", p)
+	}
+}
