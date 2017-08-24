f880b5d506df641bc37899b2752d72d3cff05033
aQua <aQua@aQuaYi.com>
2017-08-24 11:19:56 +0800

    80 added

---
+++ Algorithms/0080.remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.go
@@ -0,0 +1,5 @@
+package Problem0080
+
+func removeDuplicates(nums []int) int {
+
+}
---
+++ Algorithms/0080.remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii_test.go
@@ -0,0 +1,52 @@
+package Problem0080
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type question struct {
+	para
+	ans
+}
+
+// para 是参数
+type para struct {
+	nums []int
+}
+
+// ans 是答案
+type ans struct {
+	one   int
+	first []int
+}
+
+func Test_Problem0080(t *testing.T) {
+	ast := assert.New(t)
+
+	qs := []question{
+
+		question{
+			para{
+				[]int{1, 1, 1, 2, 2, 3},
+			},
+			ans{
+				5,
+				[]int{1, 1, 2, 2, 3},
+			},
+		},
+
+		// 如需多个测试，可以复制上方元素。
+	}
+
+	for _, q := range qs {
+		a, p := q.ans, q.para
+		fmt.Printf("~~%v~~\n", p)
+
+		end := removeDuplicates(p.nums)
+		ast.Equal(a.one, end, "输入:%v", p)
+		ast.Equal(a.first, p.nums[:end], "输入:%v", p)
+	}
+}
