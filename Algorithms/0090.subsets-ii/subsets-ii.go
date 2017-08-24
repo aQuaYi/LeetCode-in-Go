0dcce24e7d37a42e324f6cc7af45432dc1a8ee77
aQua <aQua@aQuaYi.com>
2017-08-24 16:11:07 +0800

    90 成功减少了dfs的一个参数

--- Algorithms/0090.subsets-ii/subsets-ii.go
+++ Algorithms/0090.subsets-ii/subsets-ii.go
@@ -6,25 +6,25 @@ func subsetsWithDup(nums []int) [][]int {
 	res := [][]int{}
 	sort.Ints(nums)
 
-	var dfs func(int, []int, *[][]int)
+	var dfs func(int, []int)
 
-	dfs = func(idx int, temp []int, res *[][]int) {
+	dfs = func(idx int, temp []int) {
 		t := make([]int, len(temp))
 		copy(t, temp)
 		// 没有以上两行，答案就是错的
 		// 因为temp的底层数组在递归过程中，不停地修改
 		// 程序结束时，temp的底层数组的值，全部是 nums 的最大值。
-		*res = append(*res, t)
+		res = append(res, t)
 
 		for i := idx; i < len(nums); i++ {
 			if i == idx || nums[i] != nums[i-1] {
-				dfs(i+1, append(temp, nums[i]), res)
+				dfs(i+1, append(temp, nums[i]))
 			}
 		}
 	}
 
 	temp := make([]int, 0, len(nums))
-	dfs(0, temp, &res)
+	dfs(0, temp)
 
 	return res
 }
