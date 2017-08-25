1933a010082e86dd9f847064a57888cf506ac236
aQua <aQua@aQuaYi.com>
2017-08-25 12:25:49 +0800

    120 accepted, 32ms ，最快的6ms

---
+++ Algorithms/0120.triangle/triangle.go
@@ -0,0 +1,41 @@
+package Problem0120
+
+import (
+	"fmt"
+)
+
+func minimumTotal(triangle [][]int) int {
+	m := len(triangle)
+	if m == 0 {
+		return 0
+	}
+
+	for i := 1; i < m; i++ {
+		triangle[i][0] += triangle[i-1][0]
+		triangle[i][i] += triangle[i-1][i-1]
+	}
+
+	for i := 2; i < m; i++ {
+		for j := 1; j < i; j++ {
+			fmt.Println(triangle[i][j])
+			triangle[i][j] += min(triangle[i-1][j-1], triangle[i-1][j])
+		}
+	}
+
+	min := triangle[m-1][0]
+
+	for j := 1; j < m; j++ {
+		if min > triangle[m-1][j] {
+			min = triangle[m-1][j]
+		}
+	}
+
+	return min
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
---
+++ Algorithms/0120.triangle/triangle_test.go
@@ -0,0 +1,67 @@
+package Problem0120
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type question struct {
+	para
+	ans
+}
+
+// para 是参数
+type para struct {
+	triangle [][]int
+}
+
+// ans 是答案
+type ans struct {
+	one int
+}
+
+func Test_Problem0120(t *testing.T) {
+	ast := assert.New(t)
+
+	qs := []question{
+
+		question{
+			para{[][]int{
+
+				[]int{2},
+				[]int{3, 4},
+				[]int{6, 5, 7},
+				[]int{4, 1, 8, 3},
+			}},
+			ans{
+				11,
+			},
+		},
+
+		question{
+			para{[][]int{}},
+			ans{
+				0,
+			},
+		},
+
+		question{
+			para{[][]int{
+				[]int{2},
+			}},
+			ans{
+				2,
+			},
+		},
+		// 如需多个测试，可以复制上方元素。
+	}
+
+	for _, q := range qs {
+		a, p := q.ans, q.para
+		fmt.Printf("~~%v~~\n", p)
+
+		ast.Equal(a.one, minimumTotal(p.triangle), "输入:%v", p)
+	}
+}
