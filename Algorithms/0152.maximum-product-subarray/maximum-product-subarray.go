e00441afd1d06991870e5d15b67ae6db420a7304
aQua <aQua@aQuaYi.com>
2017-08-25 17:25:41 +0800

    152 fixing

--- Algorithms/0152.maximum-product-subarray/maximum-product-subarray.go
+++ Algorithms/0152.maximum-product-subarray/maximum-product-subarray.go
@@ -1,40 +1,61 @@
 package Problem0152
 
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
 func maxProduct(nums []int) int {
-	max := nums[0]
-	temps := make([]int, 0, len(nums))
-	for i := 0; i < len(nums); i++ {
+	size := len(nums)
+
+	maxp_sub_arr := make([]int, size)
+	ng_maxp_sub_arr := make([]int, size)
+	maxp_sub_arr[0] = nums[0]
+	ng_maxp_sub_arr[0] = 1
+	if maxp_sub_arr[0] < 0 {
+		ng_maxp_sub_arr[0] = maxp_sub_arr[0]
+	}
+
+	for i := 1; i < size; i += 1 {
 		if nums[i] == 0 {
-			if max < 0 {
-				max = 0
-			}
-			temps = make([]int, 0, len(nums))
 			continue
 		}
 
-		mult(temps, nums[i])
-		temps = append(temps, nums[i])
-		tempMax := getMax(temps)
-		if max < tempMax {
-			max = tempMax
+		if maxp_sub_arr[i-1] == 0 {
+			maxp_sub_arr[i] = nums[i]
+			if nums[i] < 0 {
+				ng_maxp_sub_arr[i] = maxp_sub_arr[i]
+			} else {
+				ng_maxp_sub_arr[i] = 1
+			}
+		}
+
+		if nums[i] > 0 {
+			maxp_sub_arr[i] = max(nums[i]*maxp_sub_arr[i-1], nums[i])
+			ng_maxp_sub_arr[i] = nums[i] * ng_maxp_sub_arr[i-1]
+		}
+
+		if nums[i] < 0 {
+			maxp_sub_arr[i] = nums[i] * ng_maxp_sub_arr[i-1]
+			ng_maxp_sub_arr[i] = min(nums[i]*maxp_sub_arr[i-1], nums[i])
 		}
 	}
 
-	return max
-}
-
-func mult(nums []int, n int) {
-	for i := 0; i < len(nums); i++ {
-		nums[i] *= n
-	}
-}
-
-func getMax(nums []int) int {
-	res := nums[0]
-	for i := 1; i < len(nums); i++ {
-		if res < nums[i] {
-			res = nums[i]
+	maxp := maxp_sub_arr[0]
+	for _, subp := range maxp_sub_arr {
+		if subp > maxp {
+			maxp = subp
 		}
 	}
-	return res
+
+	return maxp
 }
