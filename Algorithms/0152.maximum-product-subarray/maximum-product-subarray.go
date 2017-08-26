3206c5f98e26ff40a986b7e53a74bf6bd3f93e70
aQua <aQua@aQuaYi.com>
2017-08-26 18:17:08 +0800

    152 finish

--- Algorithms/0152.maximum-product-subarray/maximum-product-subarray.go
+++ Algorithms/0152.maximum-product-subarray/maximum-product-subarray.go
@@ -1,41 +1,27 @@
 package Problem0152
 
 func maxProduct(a []int) int {
-	Len := len(a)
+	cur, neg, max := 1, 1, a[0]
 
-	// maxs 用来存放最大值
-	maxs := make([]int, Len)
-	maxs[0] = a[0]
-	// mins 用来存放最小值
-	mins := make([]int, Len)
-	mins[0] = a[0]
+	for i := 0; i < len(a); i++ {
 
-	for i := 1; i < Len; i++ {
-		if a[i] > 0 {
-			maxs[i] = a[i] * maxs[i-1]
-			// 因为有可能 masx[i-1] <= 0
-			// 所以，需要这个步骤更新答案
-			if a[i] > a[i]*maxs[i-1] {
-				maxs[i] = a[i]
-			}
-			mins[i] = a[i] * mins[i-1]
-		} else if a[i] < 0 {
-			maxs[i] = a[i] * mins[i-1]
-			mins[i] = a[i] * maxs[i-1]
-			// 因为有可能 maxs[i-1] <= 0
-			// 所以，需要这个步骤更新答案
-			if a[i] < a[i]*maxs[i-1] {
-				mins[i] = a[i]
-			}
+		switch {
+		case a[i] > 0:
+			cur, neg = a[i]*cur, a[i]*neg
+		case a[i] < 0:
+			cur, neg = a[i]*neg, a[i]*cur
+		default:
+			cur, neg = 0, 1
+		}
+
+		if max < cur {
+			max = cur
+		}
+
+		if cur <= 0 {
+			cur = 1
 		}
 	}
 
-	res := maxs[0]
-	for _, m := range maxs {
-		if res < m {
-			res = m
-		}
-	}
-
-	return res
+	return max
 }
