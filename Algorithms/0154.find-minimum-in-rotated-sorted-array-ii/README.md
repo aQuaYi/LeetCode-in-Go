32cda3e4b4c1a9698d4067a0e08e34f5d058ae78
aQua <aQua@aQuaYi.com>
2017-08-26 18:21:57 +0800

    154 accepted

---
+++ Algorithms/0154.find-minimum-in-rotated-sorted-array-ii/README.md
@@ -0,0 +1,20 @@
+# [154. Find Minimum in Rotated Sorted Array II](https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/)
+
+## 题目
+Follow up for "Find Minimum in Rotated Sorted Array":
+What if duplicates are allowed?
+
+Would this affect the run-time complexity? How and why?
+
+
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+Find the minimum element.
+
+The array may contain duplicates.
+
+## 解题思路
+
+见程序注释
---
+++ Algorithms/0154.find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii_test.go
@@ -0,0 +1,66 @@
+package Problem0154
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type question struct {
+	para
+	ans
+}
+
+// para 是参数
+type para struct {
+	nums []int
+}
+
+// ans 是答案
+type ans struct {
+	one int
+}
+
+func Test_Problem0154(t *testing.T) {
+	ast := assert.New(t)
+
+	qs := []question{
+
+		question{
+			para{
+				[]int{4, 4, 5, 5, 6, 6, 7, 7, 0, 0, 1, 1, 2, 2},
+			},
+			ans{
+				0,
+			},
+		},
+
+		question{
+			para{
+				[]int{1,1},
+			},
+			ans{
+				1,
+			},
+		},
+
+		question{
+			para{
+				[]int{1, 2},
+			},
+			ans{
+				1,
+			},
+		},
+
+		// 如需多个测试，可以复制上方元素。
+	}
+
+	for _, q := range qs {
+		a, p := q.ans, q.para
+		fmt.Printf("~~%v~~\n", p)
+
+		ast.Equal(a.one, findMin(p.nums), "输入:%v", p)
+	}
+}
