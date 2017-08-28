f86ceb9421e3b55d336ed908676ca3f4fbf2c652
aQua <aQua@aQuaYi.com>
2017-08-28 16:07:26 +0800

    229 finish

--- Algorithms/0229.majority-element-ii/majority-element-ii.go
+++ Algorithms/0229.majority-element-ii/majority-element-ii.go
@@ -1,23 +1,48 @@
 package Problem0229
 
 func majorityElement(a []int) []int {
-	l := len(a)
-	if l < 2 {
+	if len(a) <= 1 {
 		return a
 	}
 
-	res := []int{}
-	rec := make(map[int]int, l)
-
-	for i := 0; i < l; i++ {
-		rec[a[i]]++
-	}
-
-	for k, n := range rec {
-		if 3*n > l {
-			res = append(res, k)
+	e1, e2, c1, c2 := 0, 1, 0, 0
+	for _, e := range a {
+		switch {
+		case e == e1:
+			c1++
+		case e == e2:
+			c2++
+		case c1 == 0:
+			e1 = e
+			c1 = 1
+		case c2 == 0:
+			e2 = e
+			c2 = 1
+		default:
+			c1--
+			c2--
 		}
 	}
 
+	res := []int{}
+
+	if maj(a, e2) {
+		res = append(res, e2)
+	}
+
+	if maj(a, e1) {
+		res = append(res, e1)
+	}
+
 	return res
+}
+
+func maj(a []int, n int) bool {
+	c := 0
+	for _, e := range a {
+		if e == n {
+			c++
+		}
+	}
+	return c > (len(a) / 3)
 }
