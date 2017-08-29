924d27973c7ef29b598ceac8025b7bfbe4f11b81
aQua <aQua@aQuaYi.com>
2017-08-29 16:06:55 +0800

    380 accepted

--- Algorithms/0380.insert-delete-getrandom-o1/insert-delete-getrandom-o1.go
+++ Algorithms/0380.insert-delete-getrandom-o1/insert-delete-getrandom-o1.go
@@ -1,26 +1,59 @@
 package Problem0380
 
+import "math/rand"
+
+// RandomizedSet 是一个随机获取的集合
 type RandomizedSet struct {
+	rec map[int]bool
 }
 
+// Constructor returns RandomizedSet
 /** Initialize your data structure here. */
 func Constructor() RandomizedSet {
-
+	return RandomizedSet{
+		rec: make(map[int]bool),
+	}
 }
 
+// Insert 插入数据
 /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
-func (this *RandomizedSet) Insert(val int) bool {
-
+func (r *RandomizedSet) Insert(val int) bool {
+	if _, ok := r.rec[val]; ok {
+		return false
+	}
+	r.rec[val] = true
+	return true
 }
 
+// Remove 删除数据
 /** Removes a value from the set. Returns true if the set contained the specified element. */
-func (this *RandomizedSet) Remove(val int) bool {
+func (r *RandomizedSet) Remove(val int) bool {
+	if _, ok := r.rec[val]; ok {
+		delete(r.rec, val)
+		return true
+	}
 
+	return false
 }
 
+// GetRandom 获取随机数据
 /** Get a random element from the set. */
-func (this *RandomizedSet) GetRandom() int {
+func (r *RandomizedSet) GetRandom() int {
+	if len(r.rec) == 0 {
+		return 0
+	}
 
+	rnd := rand.Int() % len(r.rec)
+	res := 0
+	for k := range r.rec {
+		if rnd == 0 {
+			res = k
+			break
+		}
+		rnd--
+	}
+
+	return res
 }
 
 /**
--- Algorithms/0380.insert-delete-getrandom-o1/insert-delete-getrandom-o1_test.go
+++ Algorithms/0380.insert-delete-getrandom-o1/insert-delete-getrandom-o1_test.go
@@ -9,4 +9,29 @@ import (
 func Test_Problem0380(t *testing.T) {
 	ast := assert.New(t)
 
+	rs := Constructor()
+
+	ast.Equal(0, rs.GetRandom(), "从空集合中，只能取出0")
+
+	ast.True(rs.Insert(1), "插入1")
+
+	ast.False(rs.Remove(2), "删除2")
+
+	ast.True(rs.Insert(2), "插入2")
+
+	ast.Contains([]int{1, 2}, rs.GetRandom(), "返回1或2")
+
+	ast.True(rs.Remove(1), "删除1")
+
+	ast.False(rs.Insert(2), "再一次插入2")
+
+	ast.Equal(2, rs.GetRandom(), "从只有2的集合中随机取出2")
+
+	length := 100
+	result := make([]int, length)
+	for i := 0; i < 100; i++ {
+		rs.Insert(i)
+		result[i] = i
+	}
+	ast.Contains(result, rs.GetRandom(), "随机获取0~100之间的数")
 }
