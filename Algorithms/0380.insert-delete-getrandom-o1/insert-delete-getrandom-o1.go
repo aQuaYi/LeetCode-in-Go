18bbabcce8aca067bc9142d128389cf8810b930d
aQua <aQua@aQuaYi.com>
2017-08-29 15:04:38 +0800

    380 adding

---
+++ Algorithms/0380.insert-delete-getrandom-o1/insert-delete-getrandom-o1.go
@@ -0,0 +1,32 @@
+package Problem0380
+
+type RandomizedSet struct {
+}
+
+/** Initialize your data structure here. */
+func Constructor() RandomizedSet {
+
+}
+
+/** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+func (this *RandomizedSet) Insert(val int) bool {
+
+}
+
+/** Removes a value from the set. Returns true if the set contained the specified element. */
+func (this *RandomizedSet) Remove(val int) bool {
+
+}
+
+/** Get a random element from the set. */
+func (this *RandomizedSet) GetRandom() int {
+
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Insert(val);
+ * param_2 := obj.Remove(val);
+ * param_3 := obj.GetRandom();
+ */
---
+++ Algorithms/0380.insert-delete-getrandom-o1/insert-delete-getrandom-o1_test.go
@@ -0,0 +1,12 @@
+package Problem0380
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Problem0380(t *testing.T) {
+	ast := assert.New(t)
+
+}
