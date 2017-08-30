9477c776b0182923ecfaaed2cded86f871baf452
aQua <aQua@aQuaYi.com>
2017-08-30 11:22:30 +0800

    560 finish

--- Algorithms/0560.subarray-sum-equals-k/subarray-sum-equals-k.go
+++ Algorithms/0560.subarray-sum-equals-k/subarray-sum-equals-k.go
@@ -1,17 +1,20 @@
 package Problem0560
 
 func subarraySum(a []int, k int) int {
-	cnt := 0
+	res, sum := 0, 0
+	rec := make(map[int]int, len(a))
+	rec[0] = 1
 
-	for i := 0; i < len(a); i++ {
-		tmp := 0
-		for j := i; j < len(a); j++ {
-			tmp += a[j]
-			if tmp == k {
-				cnt++
-			}
-		}
+	for i := range a {
+		// sum 等于 a[:i] 中所有元素之和
+		sum += a[i]
+		// rec[x] 是 a[:i] 中所有和为 x 的连续子序列的个数
+		// 假设 rec[sum-k] == 1
+		// 那么，有且仅有一个 j，满足 0<= j < i，且 a[j:i] 中所有元素的和为 k
+		res += rec[sum-k]
+		// 更新 rec
+		rec[sum]++
 	}
 
-	return cnt
+	return res
 }
