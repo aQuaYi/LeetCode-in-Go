a0fc111e0f477085898bd91d1616e715fb31c421
aQua <aQua@aQuaYi.com>
2017-08-30 11:43:03 +0800

    565 finish

--- Algorithms/0565.array-nesting/array-nesting.go
+++ Algorithms/0565.array-nesting/array-nesting.go
@@ -2,15 +2,19 @@ package Problem0565
 
 func arrayNesting(a []int) int {
 	max := 0
-	for i := 0; i < len(a); i++ {
+
+	for i := range a {
 		temp := 1
+
 		for a[i] != i {
 			a[i], a[a[i]] = a[a[i]], a[i]
 			temp++
 		}
+
 		if max < temp {
 			max = temp
 		}
 	}
+
 	return max
 }
