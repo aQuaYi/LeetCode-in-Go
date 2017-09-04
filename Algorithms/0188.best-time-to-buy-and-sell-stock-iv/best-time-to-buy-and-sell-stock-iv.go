74dc38c22e7ae068a05c9076489ff68b27d95696
aQua <aQua@aQuaYi.com>
2017-09-04 10:21:34 +0800

    pass ut

--- Algorithms/0188.best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.go
+++ Algorithms/0188.best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.go
@@ -1,72 +1,44 @@
 package Problem0188
 
-import (
-	"fmt"
-)
-
 func maxProfit(k int, prices []int) int {
 	size := len(prices)
 	if size <= 1 {
 		return 0
 	}
 
-	profits := []int{}
-	tmp := 0
+	if k > size {
+		return profits(prices)
+	}
+
+	local := make([]int, k+1)
+	global := make([]int, k+1)
+
 	for i := 1; i < size; i++ {
 		diff := prices[i] - prices[i-1]
-
-		if tmp*diff >= 0 {
-			tmp += diff
-			continue
+		for j := k; j >= 1; j-- {
+			local[j] = max(global[j-1]+max(diff, 0), local[j]+diff)
+			global[j] = max(local[j], global[j])
 		}
-
-		profits = append(profits, tmp)
-		tmp = diff
 	}
-	profits = append(profits, tmp)
 
+	return global[k]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func profits(prices []int) int {
 	res := 0
-	rec := map[string]int{}
-
-	max := func(begin, end int) int {
-		key := fmt.Sprintf("%d-%d", begin, end)
-		if t, ok := rec[key]; ok {
-			return t
-		}
-
-		max, tmp := 0, 0
-		for i := begin; i < end && i < len(profits); i++ {
-			if tmp < 0 {
-				tmp = 0
-			}
-
-			tmp += profits[i]
-
-			if max < tmp {
-				max = tmp
-			}
-		}
-
-		rec[key] = max
-		return max
-	}
-
-	var dfs func(int, int, int)
-	dfs = func(times, idx, tmp int) {
-		if times == 0 || idx >= len(profits) {
-			if res < tmp {
-				res = tmp
-			}
-			return
-		}
-
-		for i := idx + 2; i < len(profits)+2; i += 2 {
-
-			dfs(times-1, i, tmp+max(idx, i))
+	for i := 1; i < len(prices); i++ {
+		tmp := prices[i] - prices[i-1]
+		if tmp > 0 {
+			res += tmp
 		}
 	}
-
-	dfs(k, 0, 0)
 
 	return res
 }
