238743096289aa895cf94fcbb2d83974fd7c3a8e
aQua <aQua@aQuaYi.com>
2017-09-05 21:47:57 +0800

    71 accepted, 6ms. The Fastest is 3ms.

--- Algorithms/0071.simplify-path/simplify-path.go
+++ Algorithms/0071.simplify-path/simplify-path.go
@@ -1,6 +1,25 @@
 package Problem0071
 
-func simplifyPath(path string) string {
+import (
+	"strings"
+)
 
-	return "/home"
+func simplifyPath(path string) string {
+	paths := strings.Split(path, "/")
+	res := []string{}
+
+	for i, p := range paths {
+		switch p {
+		case ".", "":
+			continue
+		case "..":
+			if len(res) > 0 {
+				res = res[:len(res)-1]
+			}
+		default:
+			res = append(res, paths[i])
+		}
+	}
+
+	return "/" + strings.Join(res, "/")
 }
