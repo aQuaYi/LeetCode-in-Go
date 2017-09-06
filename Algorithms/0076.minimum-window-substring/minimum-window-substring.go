bc8966d758583a7dc2c7e8c5c34dc5e055174d54
aQua <aQua@aQuaYi.com>
2017-09-06 21:02:09 +0800

    76 runtime error: index out of range

--- Algorithms/0076.minimum-window-substring/minimum-window-substring.go
+++ Algorithms/0076.minimum-window-substring/minimum-window-substring.go
@@ -1,5 +1,9 @@
 package Problem0076
 
+import (
+	"sort"
+)
+
 func minWindow(s string, t string) string {
 	rec := make(map[byte][]int, len(s))
 
@@ -14,5 +18,29 @@ func minWindow(s string, t string) string {
 		}
 	}
 
-	return ""
+	min := len(s) + 1
+	first, last := 0, 0
+	list := make([]int, len(t))
+
+	var dfs func(int)
+	dfs = func(idx int) {
+		if idx == len(t) {
+			temp := make([]int, len(t))
+			copy(temp, list)
+			sort.Ints(temp)
+			dist := temp[len(t)-1] - temp[0]
+			if min > dist {
+				first, last = temp[0], temp[len(t)-1]
+			}
+		}
+
+		for i := 0; i < len(rec[t[idx]]); i++ {
+			list[idx] = rec[t[idx]][i]
+			dfs(idx + 1)
+		}
+	}
+
+	dfs(0)
+
+	return s[first : last+1]
 }
