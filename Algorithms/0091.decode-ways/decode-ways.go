4fac50c6ae2ab439578482ecd0402c611926c0f3
aQua <aQua@aQuaYi.com>
2017-09-07 23:43:50 +0800

    91 accepted. 采用动态规划的思路

--- Algorithms/0091.decode-ways/decode-ways.go
+++ Algorithms/0091.decode-ways/decode-ways.go
@@ -2,6 +2,60 @@ package Problem0091
 
 func numDecodings(s string) int {
 	n := len(s)
-	dp := make([]int, n+1)
+	if n == 0 {
+		return 0
+	}
 
+	if n == 1 {
+		return waysOfOne(s)
+	}
+
+	dp := make([]int, n+1)
+	dp[0], dp[1] = 1, waysOfOne(s[0:1])
+
+	for i := 2; i <= n; i++ {
+		w1, w2 := waysOfOne(s[i-1:i]), waysOfTwo(s[i-2:i])
+		switch {
+		case w1 == 0 && w2 == 0:
+			return 0
+		case w2 == 2:
+			dp[i] = dp[i-1] + dp[i-2]
+		case w1 == 0: // w2 == 1
+			dp[i] = dp[i-2]
+		case w2 == 0: // w1 == 1
+			dp[i] = dp[i-1]
+		}
+	}
+
+	return dp[n]
+}
+
+func waysOfOne(s string) int {
+	if s == "0" {
+		return 0
+	}
+	return 1
+}
+
+func waysOfTwo(s string) int {
+	switch {
+	case '3' <= s[0] && s[0] <= '9':
+		return 0
+	case s[0] == '2':
+		if s[1] == '7' || s[1] == '8' || s[1] == '9' {
+			return 0
+		}
+		if s[1] == '0' {
+			return 1
+		}
+		return 2
+	case s[0] == '1':
+		if s[1] == '0' {
+			return 1
+		}
+		return 2
+	default:
+		// s[0] == '0'
+		return 0
+	}
 }
--- Algorithms/0091.decode-ways/decode-ways_test.go
+++ Algorithms/0091.decode-ways/decode-ways_test.go
@@ -1,8 +1,8 @@
 package Problem0091
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -19,7 +19,7 @@ type para struct {
 
 // ans 是答案
 type ans struct {
-	one  int 
+	one int
 }
 
 func Test_Problem0091(t *testing.T) {
@@ -29,13 +29,179 @@ func Test_Problem0091(t *testing.T) {
 
 		question{
 			para{
-					,
+				"27",
 			},
 			ans{
-					,
+				1,
 			},
 		},
-	
+		question{
+			para{
+				"",
+			},
+			ans{
+				0,
+			},
+		},
+
+		question{
+			para{
+				"0",
+			},
+			ans{
+				0,
+			},
+		},
+
+		question{
+			para{
+				"1",
+			},
+			ans{
+				1,
+			},
+		},
+
+		question{
+			para{
+				"203",
+			},
+			ans{
+				1,
+			},
+		},
+
+		question{
+			para{
+				"119",
+			},
+			ans{
+				3,
+			},
+		},
+		question{
+			para{
+				"2011",
+			},
+			ans{
+				2,
+			},
+		},
+
+		question{
+			para{
+				"1192",
+			},
+			ans{
+				3,
+			},
+		},
+
+		question{
+			para{
+				"1102",
+			},
+			ans{
+				1,
+			},
+		},
+
+		question{
+			para{
+				"1100",
+			},
+			ans{
+				0,
+			},
+		},
+
+		question{
+			para{
+				"1190",
+			},
+			ans{
+				0,
+			},
+		},
+
+		question{
+			para{
+				"1128",
+			},
+			ans{
+				3,
+			},
+		},
+
+		question{
+			para{
+				"1",
+			},
+			ans{
+				1,
+			},
+		},
+
+		question{
+			para{
+				"12",
+			},
+			ans{
+				2,
+			},
+		},
+
+		question{
+			para{
+				"121",
+			},
+			ans{
+				3,
+			},
+		},
+		question{
+			para{
+				"1212",
+			},
+			ans{
+				5,
+			},
+		},
+
+		question{
+			para{
+				"12121",
+			},
+			ans{
+				8,
+			},
+		},
+
+		question{
+			para{
+				"121212",
+			},
+			ans{
+				13,
+			},
+		},
+
+		question{
+			para{
+				"1212121",
+			},
+			ans{
+				21,
+			},
+		},
+		question{
+			para{
+				"12121212",
+			},
+			ans{
+				34,
+			},
+		},
 		// 如需多个测试，可以复制上方元素。
 	}
 
@@ -43,6 +209,6 @@ func Test_Problem0091(t *testing.T) {
 		a, p := q.ans, q.para
 		fmt.Printf("~~%v~~\n", p)
 
-		ast.Equal(a.one, numDecodings(p.  ), "输入:%v", p)
+		ast.Equal(a.one, numDecodings(p.s), "输入:%v", p)
 	}
 }
