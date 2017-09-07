49c85a7ca5414b6bb2d410191a7a382891e268c1
aQua <aQua@aQuaYi.com>
2017-09-07 15:38:15 +0800

    89 added

---
+++ Algorithms/0089.gray-code/gray-code.go
@@ -0,0 +1,6 @@
+package Problem0089
+
+func grayCode(n int) []int {
+
+	return []int{0, 1, 3, 2}
+}
---
+++ Algorithms/0089.gray-code/gray-code_test.go
@@ -0,0 +1,48 @@
+package Problem0089
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type question struct {
+	para
+	ans
+}
+
+// para 是参数
+type para struct {
+	n int
+}
+
+// ans 是答案
+type ans struct {
+	one []int
+}
+
+func Test_Problem0089(t *testing.T) {
+	ast := assert.New(t)
+
+	qs := []question{
+
+		question{
+			para{
+				2,
+			},
+			ans{
+				[]int{0, 1, 3, 2},
+			},
+		},
+
+		// 如需多个测试，可以复制上方元素。
+	}
+
+	for _, q := range qs {
+		a, p := q.ans, q.para
+		fmt.Printf("~~%v~~\n", p)
+
+		ast.Equal(a.one, grayCode(p.n), "输入:%v", p)
+	}
+}
