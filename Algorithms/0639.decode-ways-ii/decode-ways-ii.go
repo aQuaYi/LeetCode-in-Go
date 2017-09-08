a23491b6c771c51bfd7846f0acd7a686927578b4
aQua <aQua@aQuaYi.com>
2017-09-08 10:35:52 +0800

    639 finish

--- Algorithms/0639.decode-ways-ii/decode-ways-ii.go
+++ Algorithms/0639.decode-ways-ii/decode-ways-ii.go
@@ -1,5 +1,8 @@
 package Problem0639
 
+// MOD 题目要求，返回的结果需要对 MOD 取余
+const MOD = 1000000007
+
 func numDecodings(s string) int {
 	n := len(s)
 	if n == 0 {
@@ -9,40 +12,20 @@ func numDecodings(s string) int {
 	dp := make([]int, n+1)
 	// dp[i] 表示 s[:i+1] 的组成方式数
 	dp[0], dp[1] = 1, one(s[0:1])
-	// 题目要求，返回的结果不能超过 mod
-	mod := 1000000007
-	dmap := getMap()
 
 	for i := 2; i <= n; i++ {
-		dp[i] = dp[i-1]*dmap[s[i-1:i]] + dp[i-2]*dmap[s[i-2:i]]
+		w1, w2 := one(s[i-1:i]), two(s[i-2:i])
+		dp[i] = dp[i-1]*w1 + dp[i-2]*w2
 		if dp[i] == 0 {
 			// 子字符串 s[:i+1] 的组成方式数为 0
 			// 则，s 的组成方式数肯定也为 0
 			// 这时可以提前结束
 			return 0
 		}
-		dp[i] %= mod
+		dp[i] %= MOD
 	}
 
 	return dp[n]
-}
-
-func getMap() map[string]int {
-	ch := "1234567890*"
-	res := make(map[string]int, len(ch)+len(ch)*len(ch))
-
-	for i := range ch {
-		s := ch[i : i+1]
-		res[s] = one(s)
-	}
-	for i := range ch {
-		for j := range ch {
-			s := ch[i:i+1] + ch[j:j+1]
-			res[s] = two(s)
-		}
-	}
-
-	return res
 }
 
 // 检查 s 是否为合格的单字符
