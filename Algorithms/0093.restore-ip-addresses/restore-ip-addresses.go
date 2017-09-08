0b190972965fd8bf3b1258276556e618a45dce17
aQua <aQua@aQuaYi.com>
2017-09-08 15:43:37 +0800

    93 adding

--- Algorithms/0093.restore-ip-addresses/restore-ip-addresses.go
+++ Algorithms/0093.restore-ip-addresses/restore-ip-addresses.go
@@ -1,6 +1,20 @@
 package Problem0093
 
 func restoreIpAddresses(s string) []string {
+	n := len(s)
+	if n < 4 {
+		return []string{}
+	}
+
+	res := []string{}
+
+	begin := n / 4
+	var dfs func(int)
+	dfs = func(idx int) {
+		if idx == 4 {
+
+		}
+	}
 
 	return []string{"255.255.11.135", "255.255.111.35"}
 }
