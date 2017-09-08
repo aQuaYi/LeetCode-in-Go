d9ff3efd52211e0abc6579a853f591030d0f22dd
aQua <aQua@aQuaYi.com>
2017-09-08 09:30:15 +0800

    639 wrong answer

---
+++ Algorithms/0639.decode-ways-ii/decode-ways-ii.go
@@ -0,0 +1,79 @@
+package Problem0639
+
+func numDecodings(s string) int {
+	n := len(s)
+	if n == 0 {
+		return 0
+	}
+
+	dp := make([]int, n+1)
+	// dp[i] 表示 s[:i+1] 的组成方式数
+	dp[0], dp[1] = 1, one(s[0:1])
+
+	for i := 2; i <= n; i++ {
+		w1, w2 := one(s[i-1:i]), two(s[i-2:i])
+		dp[i] = dp[i-1]*w1 + dp[i-2]*w2
+		if dp[i] == 0 {
+			// 子字符串 s[:i+1] 的组成方式数为 0
+			// 则，s 的组成方式数肯定也为 0
+			// 这时可以提前结束
+			return 0
+		}
+	}
+
+	return dp[n]
+}
+
+// 检查 s 是否为合格的单字符
+// len(s) == 1
+// 合格，  返回 1
+// 不合格， 返回 0
+// 注意：
+//     题目保证了 s 只含有数字和'*'
+func one(s string) int {
+	switch s[0] {
+	case '*':
+		// '*' 可以表示 1~9
+		return 9
+	case '0':
+		return 0
+	default:
+		return 1
+	}
+}
+
+// 检查 s 是否为合格的双字符
+// len(s) == 2
+// 合格，  返回 1
+// 不合格， 返回 0
+// 注意：
+//     题目保证了 s 只含有数字和'*'
+func two(s string) int {
+	switch s[0] {
+	case '*':
+		if s[1] == '*' {
+			return 15
+		}
+		if '1' <= s[1] && s[1] <= '6' {
+			return 2
+		}
+		return 1
+	case '1':
+		if s[1] == '*' {
+			return 9
+		}
+		return 1
+	case '2':
+		if s[1] == '*' {
+			return 6
+		}
+		if s[1] == '7' || s[1] == '8' || s[1] == '9' {
+			return 0
+		}
+		return 1
+	default:
+		// '3' <= s[0] && s[0] <= '9'，或
+		// s[0] == '0'
+		return 0
+	}
+}
---
+++ Algorithms/0639.decode-ways-ii/decode-ways-ii_test.go
@@ -0,0 +1,298 @@
+package Problem0639
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type question struct {
+	para
+	ans
+}
+
+// para 是参数
+type para struct {
+	s string
+}
+
+// ans 是答案
+type ans struct {
+	one int
+}
+
+func Test_Problem0639(t *testing.T) {
+	ast := assert.New(t)
+
+	qs := []question{
+
+		question{
+			para{
+				"*1*1*0",
+			},
+			ans{
+				404,
+			},
+		},
+
+		question{
+			para{
+				"*1*1*",
+			},
+			ans{
+				3438,
+			},
+		},
+
+		question{
+			para{
+				"27",
+			},
+			ans{
+				1,
+			},
+		},
+
+		question{
+			para{
+				"",
+			},
+			ans{
+				0,
+			},
+		},
+
+		question{
+			para{
+				"0",
+			},
+			ans{
+				0,
+			},
+		},
+
+		question{
+			para{
+				"1",
+			},
+			ans{
+				1,
+			},
+		},
+
+		question{
+			para{
+				"203",
+			},
+			ans{
+				1,
+			},
+		},
+
+		question{
+			para{
+				"119",
+			},
+			ans{
+				3,
+			},
+		},
+		question{
+			para{
+				"2011",
+			},
+			ans{
+				2,
+			},
+		},
+
+		question{
+			para{
+				"1192",
+			},
+			ans{
+				3,
+			},
+		},
+
+		question{
+			para{
+				"1102",
+			},
+			ans{
+				1,
+			},
+		},
+
+		question{
+			para{
+				"1100",
+			},
+			ans{
+				0,
+			},
+		},
+
+		question{
+			para{
+				"1190",
+			},
+			ans{
+				0,
+			},
+		},
+
+		question{
+			para{
+				"1128",
+			},
+			ans{
+				3,
+			},
+		},
+
+		question{
+			para{
+				"1",
+			},
+			ans{
+				1,
+			},
+		},
+
+		question{
+			para{
+				"12",
+			},
+			ans{
+				2,
+			},
+		},
+
+		question{
+			para{
+				"121",
+			},
+			ans{
+				3,
+			},
+		},
+		question{
+			para{
+				"1212",
+			},
+			ans{
+				5,
+			},
+		},
+
+		question{
+			para{
+				"12121",
+			},
+			ans{
+				8,
+			},
+		},
+
+		question{
+			para{
+				"121212",
+			},
+			ans{
+				13,
+			},
+		},
+
+		question{
+			para{
+				"1212*2",
+			},
+			ans{
+				73,
+			},
+		},
+
+		question{
+			para{
+				"1212**",
+			},
+			ans{
+				642,
+			},
+		},
+
+		question{
+			para{
+				"1212*7",
+			},
+			ans{
+				68,
+			},
+		},
+
+		question{
+			para{
+				"12121*",
+			},
+			ans{
+				117,
+			},
+		},
+
+		question{
+			para{
+				"1212**1",
+			},
+			ans{
+				768,
+			},
+		},
+
+		question{
+			para{
+				"1212121",
+			},
+			ans{
+				21,
+			},
+		},
+
+		question{
+			para{
+				"1212*21",
+			},
+			ans{
+				136,
+			},
+		},
+
+		question{
+			para{
+				"12121212",
+			},
+			ans{
+				34,
+			},
+		},
+
+		question{
+			para{
+				"1212*212",
+			},
+			ans{
+				209,
+			},
+		},
+
+		// 如需多个测试，可以复制上方元素。
+	}
+
+	for _, q := range qs {
+		a, p := q.ans, q.para
+		fmt.Printf("~~%v~~\n", p)
+
+		ast.Equal(a.one, numDecodings(p.s), "输入:%v", p)
+	}
+}
