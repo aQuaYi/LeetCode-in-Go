aa656137c8c1988a6f3893237e40875ec3e95896
aQua <aQua@aQuaYi.com>
2017-09-08 11:48:27 +0800

    93 added

---
+++ Algorithms/0093.restore-ip-addresses/restore-ip-addresses.go
@@ -0,0 +1,6 @@
+package Problem0093
+
+func restoreIpAddresses(s string) []string {
+
+	return []string{"255.255.11.135", "255.255.111.35"}
+}
---
+++ Algorithms/0093.restore-ip-addresses/restore-ip-addresses_test.go
@@ -0,0 +1,51 @@
+package Problem0093
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type question struct {
+	para
+	ans
+}
+
+// para 是参数
+type para struct {
+	s string
+}
+
+// ans 是答案
+type ans struct {
+	one []string
+}
+
+func Test_Problem0093(t *testing.T) {
+	ast := assert.New(t)
+
+	qs := []question{
+
+		question{
+			para{
+				"25525511135",
+			},
+			ans{
+				[]string{
+					"255.255.11.135",
+					"255.255.111.35",
+				},
+			},
+		},
+
+		// 如需多个测试，可以复制上方元素。
+	}
+
+	for _, q := range qs {
+		a, p := q.ans, q.para
+		fmt.Printf("~~%v~~\n", p)
+
+		ast.Equal(a.one, restoreIpAddresses(p.s), "输入:%v", p)
+	}
+}
