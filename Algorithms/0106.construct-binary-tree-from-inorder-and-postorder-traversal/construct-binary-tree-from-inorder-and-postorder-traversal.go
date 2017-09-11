61ddf7aa39f67fe4d98a76b0eb5ae048cbbec37c
aQua <aQua@aQuaYi.com>
2017-09-11 11:50:06 +0800

    106 finish

--- Algorithms/0106.construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.go
+++ Algorithms/0106.construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.go
@@ -1,5 +1,39 @@
 package Problem0106
 
-func buildTree(inorder []int, postorder []int) *TreeNode {
+import (
+	"github.com/aQuaYi/LeetCode-in-Golang/kit"
+)
 
+type TreeNode = kit.TreeNode
+
+func buildTree(in []int, post []int) *TreeNode {
+	if len(in) == 0 {
+		return nil
+	}
+
+	res := &TreeNode{
+		Val: post[len(post)-1],
+	}
+
+	if len(in) == 1 {
+		return res
+	}
+
+	idx := indexOf(res.Val, in)
+
+	res.Left = buildTree(in[:idx], post[:idx])
+	res.Right = buildTree(in[idx+1:], post[idx:len(post)-1])
+
+	return res
+}
+
+
+func indexOf(val int, nums []int) int {
+	for i, v := range nums {
+		if v == val {
+			return i
+		}
+	}
+
+	return 0
 }
--- Algorithms/0106.construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal_test.go
+++ Algorithms/0106.construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal_test.go
@@ -1,8 +1,10 @@
 package Problem0106
 
 import (
-	"testing"
 	"fmt"
+	"testing"
+
+	"github.com/aQuaYi/LeetCode-in-Golang/kit"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -14,13 +16,13 @@ type question struct {
 
 // para 是参数
 type para struct {
-	inorder []int
- postorder []int
+	inorder   []int
+	postorder []int
 }
 
 // ans 是答案
 type ans struct {
-	one  *TreeNode 
+	pre []int
 }
 
 func Test_Problem0106(t *testing.T) {
@@ -30,13 +32,14 @@ func Test_Problem0106(t *testing.T) {
 
 		question{
 			para{
-					,
+				[]int{4, 2, 5, 1, 6, 3, 7},
+				[]int{4, 5, 2, 6, 7, 3, 1},
 			},
 			ans{
-					,
+				[]int{1, 2, 4, 5, 3, 6, 7},
 			},
 		},
-	
+
 		// 如需多个测试，可以复制上方元素。
 	}
 
@@ -44,6 +47,10 @@ func Test_Problem0106(t *testing.T) {
 		a, p := q.ans, q.para
 		fmt.Printf("~~%v~~\n", p)
 
-		ast.Equal(a.one, buildTree(p.  ), "输入:%v", p)
+		ast.Equal(a.pre, kit.Tree2Preorder(buildTree(p.inorder, p.postorder)), "输入:%v", p)
 	}
+
+	ast.Nil(buildTree([]int{}, []int{}))
+
+	ast.Equal(0, indexOf(3, []int{0, 1, 2}))
 }
