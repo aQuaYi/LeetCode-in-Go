9de73d163d6aa259fa91ed97d28d0fabea7b8c37
aQua <aQua@aQuaYi.com>
2017-09-11 11:19:09 +0800

    94 finish

--- Algorithms/0094.binary-tree-inorder-traversal/binary-tree-inorder-traversal.go
+++ Algorithms/0094.binary-tree-inorder-traversal/binary-tree-inorder-traversal.go
@@ -8,5 +8,17 @@ type TreeNode struct {
 }
 
 func inorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
 
+	if root.Left == nil && root.Right == nil {
+		return []int{root.Val}
+	}
+
+	res := inorderTraversal(root.Left)
+	res = append(res, root.Val)
+	res = append(res, inorderTraversal(root.Right)...)
+
+	return res
 }
--- Algorithms/0094.binary-tree-inorder-traversal/binary-tree-inorder-traversal_test.go
+++ Algorithms/0094.binary-tree-inorder-traversal/binary-tree-inorder-traversal_test.go
@@ -1,8 +1,8 @@
 package Problem0094
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -14,12 +14,13 @@ type question struct {
 
 // para 是参数
 type para struct {
-	root *TreeNode
+	pre []int
+	in  []int
 }
 
 // ans 是答案
 type ans struct {
-	one  []int 
+	one []int
 }
 
 func Test_Problem0094(t *testing.T) {
@@ -29,13 +30,14 @@ func Test_Problem0094(t *testing.T) {
 
 		question{
 			para{
-					,
+				[]int{1, 2, 3},
+				[]int{1, 3, 2},
 			},
 			ans{
-					,
+				[]int{1, 3, 2},
 			},
 		},
-	
+
 		// 如需多个测试，可以复制上方元素。
 	}
 
@@ -43,6 +45,43 @@ func Test_Problem0094(t *testing.T) {
 		a, p := q.ans, q.para
 		fmt.Printf("~~%v~~\n", p)
 
-		ast.Equal(a.one, inorderTraversal(p.  ), "输入:%v", p)
+		ast.Equal(a.one, inorderTraversal(PreIn2Tree(p.pre, p.in)), "输入:%v", p)
 	}
+}
+
+// PreIn2Tree 把 preorder 和 inorder 切片转换成 二叉树
+func PreIn2Tree(pre, in []int) *TreeNode {
+	if len(pre) != len(in) {
+		panic("preIn2Tree 中两个切片的长度不相等")
+	}
+
+	if len(in) == 0 {
+		return nil
+	}
+
+	res := &TreeNode{
+		Val: pre[0],
+	}
+
+	if len(in) == 1 {
+		return res
+	}
+
+	idx := indexOf(res.Val, in)
+
+	res.Left = PreIn2Tree(pre[1:idx+1], in[:idx])
+	res.Right = PreIn2Tree(pre[idx+1:], in[idx+1:])
+
+	return res
+}
+
+func indexOf(val int, nums []int) int {
+	for i, v := range nums {
+		if v == val {
+			return i
+		}
+	}
+
+	msg := fmt.Sprintf("%d 不存在于 %v 中", val, nums)
+	panic(msg)
 }
