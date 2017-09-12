bddd8ccb2ec54bdcbe17d9f42f099908e7b92f4c
aQua <aQua@aQuaYi.com>
2017-09-12 23:00:43 +0800

    99 wrong answer

--- Algorithms/0099.recover-binary-search-tree/recover-binary-search-tree.go
+++ Algorithms/0099.recover-binary-search-tree/recover-binary-search-tree.go
@@ -1,5 +1,50 @@
 package Problem0099
 
-func recoverTree(root *TreeNode)  {
+import (
+	"github.com/aQuaYi/LeetCode-in-Golang/kit"
+)
 
+type TreeNode = kit.TreeNode
+
+// 已经假设 BST 中没有重复的值
+// 所以 root != nil 
+func recoverTree(root *TreeNode) {
+	// Golang int 类型的最小值与最大值
+	MIN, MAX := -1<<63, 1<<63-1
+
+	left := wrongNodeOfBST(MIN, root.Val, root.Left)
+	right := wrongNodeOfBST(root.Val, MAX, root.Right)
+
+	switch {
+	case left != nil && right != nil:
+		left.Val, right.Val = right.Val, left.Val
+	case left != nil:
+		left.Val, root.Val = root.Val, left.Val
+	case right != nil:
+		right.Val, root.Val = root.Val, right.Val
+	}
+}
+
+// 如果 root.Val 不在 (min, max) 范围内
+// root 就是出错的节点
+func wrongNodeOfBST(min, max int, root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	if root.Val < min || max < root.Val {
+		return root
+	}
+
+	node := wrongNodeOfBST(min, root.Val, root.Left)
+	if node != nil {
+		return node
+	}
+
+	node = wrongNodeOfBST(root.Val, max, root.Right)
+	if node != nil {
+		return node
+	}
+
+	return nil
 }
--- Algorithms/0099.recover-binary-search-tree/recover-binary-search-tree_test.go
+++ Algorithms/0099.recover-binary-search-tree/recover-binary-search-tree_test.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/aQuaYi/LeetCode-in-Golang/kit"
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -12,13 +14,32 @@ func Test_Problem0099(t *testing.T) {
 
 	// tcs is testcase slice
 	tcs := []struct {
-		pre,in []int 
-		ans []int 
+		pre, in []int
+		ans     []int
 	}{
 
 		{
-			
-			,
+			[]int{3, 2, 1},
+			[]int{3, 2, 1},
+			[]int{1, 2, 3},
+		},
+
+		{
+			[]int{5, 2, 1, 3, 6, 4, 7},
+			[]int{1, 2, 3, 5, 4, 6, 7},
+			[]int{1, 2, 3, 4, 5, 6, 7},
+		},
+
+		{
+			[]int{3, 2, 1, 4, 6, 5, 7},
+			[]int{1, 2, 4, 3, 5, 6, 7},
+			[]int{1, 2, 3, 4, 5, 6, 7},
+		},
+
+		{
+			[]int{4, 2, 1, 5, 6, 3, 7},
+			[]int{1, 2, 5, 4, 3, 6, 7},
+			[]int{1, 2, 3, 4, 5, 6, 7},
 		},
 
 		// 可以多个 testcase
@@ -27,6 +48,8 @@ func Test_Problem0099(t *testing.T) {
 	for _, tc := range tcs {
 		fmt.Printf("~~%v~~\n", tc)
 
-		ast.Equal(tc.ans, recoverTree(tc.root), "输入:%v", tc)
+		root := kit.PreIn2Tree(tc.pre, tc.in)
+		recoverTree(root)
+		ast.Equal(tc.ans, kit.Tree2Inorder(root), "输入:%v", tc)
 	}
 }
