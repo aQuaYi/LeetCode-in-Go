303adb6938c26cce9e5a311fa4f908dd92ad29af
aQua <aQua@aQuaYi.com>
2017-09-13 13:57:56 +0800

    103 finish

--- Algorithms/0103.binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.go
+++ Algorithms/0103.binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.go
@@ -8,23 +8,25 @@ type TreeNode = kit.TreeNode
 
 func zigzagLevelOrder(root *TreeNode) [][]int {
 	res := [][]int{}
-	var dfs func(*TreeNode, int)
 
+	var dfs func(*TreeNode, int)
 	dfs = func(root *TreeNode, level int) {
 		if root == nil {
 			return
 		}
-
 		// 出现了新的 level
 		if level >= len(res) {
 			res = append(res, []int{})
 		}
+		// 与 102 题相比，level 的奇偶不同，append 会不同。
 		if level%2 == 0 {
 			res[level] = append(res[level], root.Val)
 		} else {
-			res[level] = append([]int{root.Val}, res[level]...)
+			temp := make([]int ,  len(res[level])+1)
+			temp[0] = root.Val
+			copy(temp[1:], res[level])
+			res[level] = temp
 		}
-
 		dfs(root.Left, level+1)
 		dfs(root.Right, level+1)
 	}
