38381e9048b4f17cb9ffce44c4221f2649602ec2
aQua <aQua@aQuaYi.com>
2017-09-13 15:24:23 +0800

    108 accepted. 359ms > 249ms.

--- Algorithms/0108.convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.go
+++ Algorithms/0108.convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.go
@@ -12,12 +12,6 @@ func sortedArrayToBST(nums []int) *TreeNode {
 		return nil
 	}
 
-	if n == 1 {
-		return &TreeNode{
-			Val: nums[0],
-		}
-	}
-
 	return &TreeNode{
 		Val:   nums[n/2],
 		Left:  sortedArrayToBST(nums[:n/2]),
