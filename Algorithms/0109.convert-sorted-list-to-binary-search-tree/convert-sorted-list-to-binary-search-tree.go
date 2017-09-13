763804d93c1e30813ab98e7d58bbfbce9b154a51
aQua <aQua@aQuaYi.com>
2017-09-13 15:52:17 +0800

    109 accepted. 1345ms. 100%.

--- Algorithms/0109.convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.go
+++ Algorithms/0109.convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.go
@@ -8,5 +8,28 @@ type ListNode = kit.ListNode
 type TreeNode = kit.TreeNode
 
 func sortedListToBST(head *ListNode) *TreeNode {
+	return sortedArrayToBST(list2Slice(head))
+}
 
+func list2Slice(head *ListNode) []int {
+	res := []int{}
+	for head != nil {
+		res = append(res, head.Val)
+		head = head.Next
+	}
+
+	return res
+}
+
+func sortedArrayToBST(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+
+	mid := len(nums) / 2
+	return &TreeNode{
+		Val:   nums[mid],
+		Left:  sortedArrayToBST(nums[:mid]),
+		Right: sortedArrayToBST(nums[mid+1:]),
+	}
 }
--- Algorithms/0109.convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree_test.go
+++ Algorithms/0109.convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree_test.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/aQuaYi/LeetCode-in-Golang/kit"
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -32,6 +34,6 @@ func Test_Problem0109(t *testing.T) {
 	for _, tc := range tcs {
 		fmt.Printf("~~%v~~\n", tc)
 
-		ast.Equal(tc.ans, sortedListToBST(tc.head), "输入:%v", tc)
+		ast.Equal(tc.ans, kit.Tree2Inorder(sortedListToBST(kit.Slice2List(tc.head))), "输入:%v", tc)
 	}
 }
