a578e6d258bba4f6d1a19f08714785386a5535a7
aQua <aQua@aQuaYi.com>
2017-09-14 16:26:20 +0800

    98 修改细节

--- Algorithms/0098.validate-binary-search-tree/validate-binary-search-tree.go
+++ Algorithms/0098.validate-binary-search-tree/validate-binary-search-tree.go
@@ -10,16 +10,16 @@ func isValidBST(root *TreeNode) bool {
 	// Golang int 类型的最小值与最大值
 	MIN, MAX := -1<<63, 1<<63-1
 
-	return cur(MIN, MAX, root)
+	return recur(MIN, MAX, root)
 }
 
 // 以递归的方式，检查 root.Val 是否在 (min, max) 范围内。
-func cur(min, max int, root *TreeNode) bool {
+func recur(min, max int, root *TreeNode) bool {
 	if root == nil {
 		return true
 	}
 
 	return min < root.Val && root.Val < max &&
-		cur(min, root.Val, root.Left) &&
-		cur(root.Val, max, root.Right)
+		recur(min, root.Val, root.Left) &&
+		recur(root.Val, max, root.Right)
 }
