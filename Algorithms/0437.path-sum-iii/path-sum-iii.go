83f4e960ebdcb06b1d495aab9660ee257fd904d4
aQua <aQua@aQuaYi.com>
2017-09-14 14:39:20 +0800

    437 wrong answer

--- Algorithms/0437.path-sum-iii/path-sum-iii.go
+++ Algorithms/0437.path-sum-iii/path-sum-iii.go
@@ -8,18 +8,36 @@ type TreeNode = kit.TreeNode
 
 func pathSum(root *TreeNode, sum int) int {
 	res := 0
-	if root == nil {
-		return res
+	path := []int{}
+
+	var dfs func(*TreeNode, int, int)
+	dfs = func(node *TreeNode, level, sum int) {
+		if node == nil {
+			return
+		}
+
+		// 根据 level 来更新 path
+		if level >= len(path) {
+			path = append(path, node.Val)
+		} else {
+			path[level] = node.Val
+		}
+		sum -= node.Val
+
+		// 到达 leaf
+		// 并且，此条路径符合要求
+		if node.Left == nil && node.Right == nil && sum == 0 {
+			temp := make([]int, level+1)
+			// copy 不会复制 path[len(temp):]，如果 len(path) > len(temp) 的话
+			copy(temp, path)
+			res = append(res, temp)
+		}
+
+		dfs(node.Left, level+1, sum)
+		dfs(node.Right, level+1, sum)
 	}
 
-	newSum := sum - root.Val
-	if newSum == 0 {
-		res++
-		return 1 + pathSum(root.Left, sum) + pathSum(root.Right, sum)
-	}
+	dfs(root, 0, sum)
 
-	return pathSum(root.Left, sum) +
-		pathSum(root.Right, sum) +
-		pathSum(root.Left, newSum) +
-		pathSum(root.Right, newSum)
+	return res
 }
