2450461048b6f7f6b829639bb149297e93406101
aQua <aQua@aQuaYi.com>
2017-09-14 14:53:57 +0800

    437 accepted. 39ms > 26ms.

--- Algorithms/0437.path-sum-iii/path-sum-iii.go
+++ Algorithms/0437.path-sum-iii/path-sum-iii.go
@@ -7,37 +7,30 @@ import (
 type TreeNode = kit.TreeNode
 
 func pathSum(root *TreeNode, sum int) int {
-	res := 0
-	path := []int{}
+	if root == nil {
+		return 0
+	}
 
-	var dfs func(*TreeNode, int, int)
-	dfs = func(node *TreeNode, level, sum int) {
+	res := 0
+
+	var dfs func(*TreeNode, int)
+	dfs = func(node *TreeNode, sum int) {
 		if node == nil {
 			return
 		}
 
-		// 根据 level 来更新 path
-		if level >= len(path) {
-			path = append(path, node.Val)
-		} else {
-			path[level] = node.Val
-		}
 		sum -= node.Val
-
-		// 到达 leaf
-		// 并且，此条路径符合要求
-		if node.Left == nil && node.Right == nil && sum == 0 {
-			temp := make([]int, level+1)
-			// copy 不会复制 path[len(temp):]，如果 len(path) > len(temp) 的话
-			copy(temp, path)
-			res = append(res, temp)
+		if sum == 0 {
+			res++
 		}
 
-		dfs(node.Left, level+1, sum)
-		dfs(node.Right, level+1, sum)
+		dfs(node.Left, sum)
+		dfs(node.Right, sum)
 	}
 
-	dfs(root, 0, sum)
+	dfs(root, sum)
 
-	return res
+	return res +
+		pathSum(root.Left, sum) +
+		pathSum(root.Right, sum)
 }
