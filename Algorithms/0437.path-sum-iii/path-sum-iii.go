aad265a734b3cae3caaaa9b5682f8da0d1f4446c
aQua <aQua@aQuaYi.com>
2017-09-14 14:32:10 +0800

    437 wrong answer

--- Algorithms/0437.path-sum-iii/path-sum-iii.go
+++ Algorithms/0437.path-sum-iii/path-sum-iii.go
@@ -3,10 +3,23 @@ package Problem0437
 import (
 	"github.com/aQuaYi/LeetCode-in-Golang/kit"
 )
+
 type TreeNode = kit.TreeNode
 
 func pathSum(root *TreeNode, sum int) int {
-res :=0
+	res := 0
+	if root == nil {
+		return res
+	}
 
-	return res 
+	newSum := sum - root.Val
+	if newSum == 0 {
+		res++
+		return 1 + pathSum(root.Left, sum) + pathSum(root.Right, sum)
+	}
+
+	return pathSum(root.Left, sum) +
+		pathSum(root.Right, sum) +
+		pathSum(root.Left, newSum) +
+		pathSum(root.Right, newSum)
 }
