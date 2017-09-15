edf663d68ef9d67e9448111f37273dccf49ff869
aQua <aQua@aQuaYi.com>
2017-09-15 09:53:45 +0800

    124 为单元测试添加几组测试数据。

--- Algorithms/0124.binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.go
+++ Algorithms/0124.binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.go
@@ -6,6 +6,22 @@ import (
 type TreeNode = kit.TreeNode
 
 func maxPathSum(root *TreeNode) int {
+	res := -1<<31
+if root == nil {
+	return res 
+}
 
-	return 22
+
+	return  res 
+}
+
+func maxRoot2Leaf() {
+	
+}
+
+func max(a, b int ) int  {
+	if a> b {
+		return a 
+	}
+	return b 
 }
