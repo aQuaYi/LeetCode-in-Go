90fcde69cc49b94e552da4c44784c039dee2e105
aQua <aQua@aQuaYi.com>
2017-09-15 17:42:11 +0800

    126 注释了程序的上半部分

--- Algorithms/0126.word-ladder-ii/word-ladder-ii.go
+++ Algorithms/0126.word-ladder-ii/word-ladder-ii.go
@@ -1,18 +1,24 @@
 package Problem0126
 
 func findLadders(beginWord string, endWord string, words []string) [][]string {
-	res := [][]string{}
-	// 因为 beginWord 不能做 transformed word
-	// 很碍事，就先删掉，好让后面的逻辑简明一些
+	// 因为 beginWord 不能做 transformed word 很碍事，就先删掉，好让后面的逻辑简明一些
 	words = deleteBeginWord(words, beginWord)
 
+	// trans 用来记录 k -> v[i] 的转换关系。
 	trans := map[string][]string{}
+	// isMatchedEndWord 用于在生成 trans 的过程中，存在 -> endWord 的转换关系
+	// 用于提前结束
 	isMatchedEndWord := false
+	// cnt 用于记录生成trans的迭代次数
+	// 其实也是最短路径的长度
 	cnt := 1
 	var bfs func([]string, []string)
-
+	// 使用 bfs 方法，递归地生成 trans
 	bfs = func(words, nodes []string) {
 		cnt++
+		// words 中的 w
+		//     与 nodes 中的 n -> w，在 w 会被放入 newNodes
+		//     否则，w 会被放入 newWords
 		newWords := make([]string, 0, len(words))
 		newNodes := make([]string, 0, len(words))
 		for _, w := range words {
@@ -33,17 +39,22 @@ func findLadders(beginWord string, endWord string, words []string) [][]string {
 			}
 		}
 
-		if isMatchedEndWord || len(newWords) == 0 || len(newNodes) == 0 {
+		if isMatchedEndWord || // 匹配到 endWord 说明已经找到了所有的最短路径
+			len(newWords) == 0 || // 全部匹配完成
+			len(newNodes) == 0 { // newWords 中单词，是 beginWord 无法 trans 到的
 			return
 		}
 
+		// 继续完善 trans
 		bfs(newWords, newNodes)
 	}
 
 	nodes := []string{beginWord}
 	bfs(words, nodes)
 
+	res := [][]string{}
 	if !isMatchedEndWord {
+		// beginWord 无法 trans 到 endWord
 		return res
 	}
 
