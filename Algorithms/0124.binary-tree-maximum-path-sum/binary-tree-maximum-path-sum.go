cc55bfba801302d1929c4258d7ce2f1365d7127b
aQua <aQua@aQuaYi.com>
2017-09-15 10:22:25 +0800

    124 ut pass

--- Algorithms/0124.binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.go
+++ Algorithms/0124.binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.go
@@ -3,25 +3,49 @@ package Problem0124
 import (
 	"github.com/aQuaYi/LeetCode-in-Golang/kit"
 )
+
 type TreeNode = kit.TreeNode
 
 func maxPathSum(root *TreeNode) int {
-	res := -1<<31
-if root == nil {
-	return res 
-}
-
-
-	return  res 
-}
-
-func maxRoot2Leaf() {
-	
-}
-
-func max(a, b int ) int  {
-	if a> b {
-		return a 
+	if root == nil {
+		return -1<<31 
 	}
-	return b 
+
+	sum := root.Val +
+		max(0, maxSince(root.Left)) +
+		max(0, maxSince(root.Right))
+
+	return max(sum,
+		max(maxPathSum(root.Left), maxPathSum(root.Right)))
+}
+
+// 返回，从 root 出发，包含 root 在内的所有可能路径的最大的 sum 值
+func maxSince(root *TreeNode) int {
+	res := -1 << 31
+
+	var dfs func(*TreeNode, int)
+	dfs = func(root *TreeNode, sum int) {
+		if root == nil {
+			return
+		}
+
+		sum += root.Val
+		if res < sum {
+			res = sum
+		}
+
+		dfs(root.Left, sum)
+		dfs(root.Right, sum)
+	}
+
+	dfs(root, 0)
+
+	return res
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
