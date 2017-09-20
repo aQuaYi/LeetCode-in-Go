df56aeae9242e8d2fae6dcfcebef9debf2be15ec
aQua <aQua@aQuaYi.com>
2017-09-20 20:20:52 +0800

    137 wrong answer

--- Algorithms/0137.single-number-ii/single-number-ii.go
+++ Algorithms/0137.single-number-ii/single-number-ii.go
@@ -4,15 +4,18 @@ package Problem0137
 func singleNumber(nums []int) int {
 	n := len(nums)
 	res := 0
+	base := 1
 
 	var i uint
 	for ; i < 32; i++ {
 		temp := 0
 		for j := 0; j < n; j++ {
-			temp += nums[j] >> i & 1 //首先把输入数字的第i位加起来。
+			temp += nums[j] % 2 //首先把输入数字的第i位加起来。
+			nums[j] /= 2
 		}
-		temp %= 3        //然后求它们除以3的余数。
-		res |= temp << i //把二进制表示的结果转化为十进制表示的结果
+		temp %= 3          //然后求它们除以3的余数。
+		res += temp * base //把二进制表示的结果转化为十进制表示的结果
+		base *= 2
 	}
 
 	return res
