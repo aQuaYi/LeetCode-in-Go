36b5ba9265d16804ee7d25e32e559bbe5e6420aa
aQua <aQua@aQuaYi.com>
2017-09-20 20:45:46 +0800

    137 wrong answer

--- Algorithms/0137.single-number-ii/single-number-ii.go
+++ Algorithms/0137.single-number-ii/single-number-ii.go
@@ -1,21 +1,32 @@
 package Problem0137
 
+import "fmt"
+
 // Golang 中的 int 为 64 bit
 func singleNumber(nums []int) int {
 	n := len(nums)
 	res := 0
 	base := 1
+	sign := 1
 
-	for i := 0; i < 64; i++ {
+	i := 0
+	for ; i < 64; i++ {
 		temp := 0
 		for j := 0; j < n; j++ {
 			temp += nums[j] % 2 //首先把输入数字的第i位加起来。
 			nums[j] /= 2
 		}
-		temp %= 3          //然后求它们除以3的余数。
+		if i == 0 && temp < 0 {
+			sign = -1
+		}
+
+		temp %= 3 //然后求它们除以3的余数。
+		if temp*sign < 0 {
+			temp += 3 * sign
+		}
 		res += temp * base //把二进制表示的结果转化为十进制表示的结果
 		base *= 2
 	}
-
+	fmt.Println("sign", sign)
 	return res
 }
