d200907e2cce2b8797f7a757759e3bf658a06cee
aQua <aQua@aQuaYi.com>
2017-09-20 10:52:49 +0800

    131 finish, 46ms

--- Algorithms/0131.palindrome-partitioning/palindrome-partitioning.go
+++ Algorithms/0131.palindrome-partitioning/palindrome-partitioning.go
@@ -1,36 +1,42 @@
 package Problem0131
 
 func partition(s string) [][]string {
-	res := [][]string{}
-	if len(s) <= 1 {
-		res = append(res, []string{s})
-		return res
-	}
-
-	for i := 1; i < len(s); i++ {
-		if isPalindrome(s[:i]) {
-			other := partition(s[i:])
-			for _, o := range other {
-				temp := make([]string, len(o)+1)
-				temp[0] = s[:i]
-				copy(temp[1:], o)
-				res = append(res, temp)
-			}
-		}
-	}
-
-	if isPalindrome(s) {
-		res = append(res, []string{s})
-	}
-
-	return res
+	result := [][]string{}
+	current := make([]string, 0, len(s))
+	dfs(s, 0, current, &result)
+	return result
 }
 
-func isPalindrome(s string) bool {
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-		if s[i] != s[j] {
+func dfs(s string, i int, cur []string, result *[][]string) {
+	if i == len(s) {
+		tmp := make([]string, len(cur))
+		copy(tmp, cur)
+		*result = append(*result, tmp)
+		return
+	}
+
+	for j := i; j < len(s); j++ {
+		// i == 0 时，
+		// 按照 len(cur[0]) 的不同，来划分 res
+		// 并以此类推
+		if par(s[i : j+1]) {
+			dfs(s, j+1, append(cur, s[i:j+1]), result)
+		}
+	}
+}
+
+// s 为回文，则返回 true
+func par(s string) bool {
+	if len(s) <= 1 {
+		return true
+	}
+	a, b := 0, len(s)-1
+	for a < b {
+		if s[a] != s[b] {
 			return false
 		}
+		a++
+		b--
 	}
 	return true
 }
