1661182a0957d15432ef9353e45ff7748bc73fb6
aQua <aQua@aQuaYi.com>
2017-09-20 17:19:04 +0800

    135 finish

--- Algorithms/0135.candy/candy.go
+++ Algorithms/0135.candy/candy.go
@@ -15,12 +15,16 @@ func candy(ratings []int) int {
 
 	for i := 1; i < n; i++ {
 		if ratings[i-1] < ratings[i] {
+			// i 比左边的大
+			// 所以，他的数量要比左边的多一个
 			left[i] = left[i-1] + 1
 		} else {
 			left[i] = 1
 		}
 
 		if ratings[n-i-1] > ratings[n-i] {
+			// n-i-1 比右边的大
+			// 所以，他的数量要比右边的多一个
 			right[n-i-1] = right[n-i] + 1
 		} else {
 			right[n-i-1] = 1
@@ -29,6 +33,7 @@ func candy(ratings []int) int {
 
 	res := 0
 	for i := 0; i < n; i++ {
+		// i 的实际数量应该是max(left[i], right[i]
 		res += max(left[i], right[i])
 	}
 
