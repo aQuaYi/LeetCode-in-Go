039fed677fdda581926374df3150a475230e23ac
aQua <aQua@aQuaYi.com>
2017-09-20 16:48:41 +0800

    134 finish

--- Algorithms/0134.gas-station/gas-station.go
+++ Algorithms/0134.gas-station/gas-station.go
@@ -1,35 +1,25 @@
 package Problem0134
 
 func canCompleteCircuit(gas []int, cost []int) int {
-	n := len(gas)
-	if n == 0 {
+	remains, debts, start := 0, 0, 0
+
+	for i, g := range gas {
+		remains += g - cost[i]
+		if remains < 0 {
+			// i + 1 处重新开始
+			start = i + 1
+			// 记录沿路一共欠缺的油量
+			debts += remains
+			// remain 至零
+			remains = 0
+		}
+	}
+
+	if debts+remains < 0 {
+		// 最后的剩余的油量，如法全部偿还前期欠缺的油量
+		// 则，无法跑完一圈
 		return -1
 	}
 
-	gas = append(gas, gas...)
-	cost = append(cost, cost...)
-
-	for i := 0; i < n; i++ {
-		if success(gas[i:i+n], cost[i:i+n]) {
-			return i
-		}
-	}
-
-	return -1
-}
-
-func success(gas, cost []int) bool {
-	gSum := 0
-	cSum := 0
-
-	for i := 0; i < len(gas); i++ {
-		gSum += gas[i]
-		cSum += cost[i]
-
-		if gSum < cSum {
-			return false
-		}
-	}
-
-	return true
+	return start
 }
