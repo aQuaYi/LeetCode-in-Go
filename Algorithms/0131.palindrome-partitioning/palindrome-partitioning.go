19e675b5fd9a8fc7627f36b96a856fa3d8406b2d
aQua <aQua@aQuaYi.com>
2017-09-20 10:28:47 +0800

    131 accepted. 52ms > 43ms

--- Algorithms/0131.palindrome-partitioning/palindrome-partitioning.go
+++ Algorithms/0131.palindrome-partitioning/palindrome-partitioning.go
@@ -2,43 +2,27 @@ package Problem0131
 
 func partition(s string) [][]string {
 	res := [][]string{}
-
 	if len(s) <= 1 {
 		res = append(res, []string{s})
 		return res
 	}
 
-	basic := make([]string, len(s))
-	for i := 0; i < len(s); i++ {
-		basic[i] = s[i : i+1]
-	}
-	res = append(res, basic)
-
-	search := func(ss []string, k int) {
-		for i := 0; i+k < len(ss); i++ {
-			s := sum(ss, i, i+k)
-			if isPalindrome(s) {
-				temp := make([]string, len(ss)-k)
-				copy(temp[:i], ss)
-				temp[i] = s
-				copy(temp[i+1:], ss[i+k+1:])
+	for i := 1; i < len(s); i++ {
+		if isPalindrome(s[:i]) {
+			other := partition(s[i:])
+			for _, o := range other {
+				temp := make([]string, len(o)+1)
+				temp[0] = s[:i]
+				copy(temp[1:], o)
 				res = append(res, temp)
 			}
 		}
 	}
 
-	for i := 1; i < len(basic); i++ {
-		search(basic, i)
+	if isPalindrome(s) {
+		res = append(res, []string{s})
 	}
 
-	return res
-}
-
-func sum(ss []string, first, last int) string {
-	res := ""
-	for i := first; i <= last; i++ {
-		res += ss[i]
-	}
 	return res
 }
 
