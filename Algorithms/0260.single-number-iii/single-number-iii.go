d2ead2738c48d918417255b7664a658a6c269d82
aQua <aQua@aQuaYi.com>
2017-09-20 21:15:21 +0800

    260 accepted. 16ms > 13ms

--- Algorithms/0260.single-number-iii/single-number-iii.go
+++ Algorithms/0260.single-number-iii/single-number-iii.go
@@ -1,6 +1,29 @@
 package Problem0260
 
-func singleNumber(nums []int) []int {
+import (
+	"sort"
+)
 
-	return nil
+func singleNumber(nums []int) []int {
+	sort.Ints(nums)
+	res := make([]int, 2)
+	idx := 0
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i] == nums[i+1] {
+			i++
+			continue
+		} else {
+			res[idx] = nums[i]
+			idx++
+			if idx == 2 {
+				return res
+			}
+		}
+	}
+
+	if idx == 1 {
+		res[1] = nums[len(nums)-1]
+	}
+
+	return res
 }
