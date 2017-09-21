55eb7aa83939f3d2fd7c8fe21564232948c90a8e
aQua <aQua@aQuaYi.com>
2017-09-21 15:38:37 +0800

    143 accepted. 579ms > 23ms

--- Algorithms/0143.reorder-list/reorder-list.go
+++ Algorithms/0143.reorder-list/reorder-list.go
@@ -5,7 +5,22 @@ import (
 )
 
 type ListNode = kit.ListNode
-func reorderList(head *ListNode)  {
 
-	
+func reorderList(head *ListNode) {
+	if head == nil || head.Next == nil || head.Next.Next == nil {
+		return
+	}
+
+	next := head.Next
+	temp := next
+	for temp.Next.Next != nil {
+		temp = temp.Next
+	}
+	end := temp.Next
+	temp.Next = nil
+
+	head.Next = end
+	end.Next = next
+
+	reorderList(next)
 }
