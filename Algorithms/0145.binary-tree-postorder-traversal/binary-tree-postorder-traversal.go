94047ab93ab5131a3a376ca698856923453b42a9
aQua <aQua@aQuaYi.com>
2017-09-22 11:19:47 +0800

    145 accepted

--- Algorithms/0145.binary-tree-postorder-traversal/binary-tree-postorder-traversal.go
+++ Algorithms/0145.binary-tree-postorder-traversal/binary-tree-postorder-traversal.go
@@ -7,6 +7,34 @@ import (
 type TreeNode = kit.TreeNode
 
 func postorderTraversal(root *TreeNode) []int {
+	var stack []*TreeNode
+	res := []int{}
 
-	return nil
+	cur := root
+	for cur != nil {
+		if cur.Left == nil && cur.Right == nil { // 到达 leaf 节点
+			res = append(res, cur.Val)
+
+			if len(stack) == 0 {
+				break
+			}
+			cur = stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+		} else {
+			left := cur.Left
+			right := cur.Right
+			cur.Left, cur.Right = nil, nil
+			stack = append(stack, cur)
+
+			if left != nil {
+				cur = left
+				if right != nil {
+					stack = append(stack, right)
+				}
+			} else {
+				cur = right
+			}
+		}
+	}
+	return res
 }
