31b8eb85a6af7b213e95d8fadce06b1cd13b2b30
aQua <aQua@aQuaYi.com>
2017-09-22 11:32:08 +0800

    146 adding

---
+++ Algorithms/0146.lru-cache/lru-cache.go
@@ -0,0 +1,23 @@
+package Problem0146
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * obj := Constructor(capacity);
+ * param_1 := obj.Get(key);
+ * obj.Put(key,value);
+ */
+
+type LRUCache struct {
+}
+
+func Constructor(capacity int) LRUCache {
+
+}
+
+func (c *LRUCache) Get(key int) int {
+
+}
+
+func (c *LRUCache) Put(key int, value int) {
+
+}
