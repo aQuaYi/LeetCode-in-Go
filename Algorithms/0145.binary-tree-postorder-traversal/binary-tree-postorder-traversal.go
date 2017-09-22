ddd253e5295f3a37eba4148a8cd948c34ef7b79d
aQua <aQua@aQuaYi.com>
2017-09-22 11:26:56 +0800

    145 finish

--- Algorithms/0145.binary-tree-postorder-traversal/binary-tree-postorder-traversal.go
+++ Algorithms/0145.binary-tree-postorder-traversal/binary-tree-postorder-traversal.go
@@ -7,6 +7,7 @@ import (
 type TreeNode = kit.TreeNode
 
 func postorderTraversal(root *TreeNode) []int {
+	// 保存以后才处理的节点
 	var stack []*TreeNode
 	res := []int{}
 
@@ -14,27 +15,33 @@ func postorderTraversal(root *TreeNode) []int {
 	for cur != nil {
 		if cur.Left == nil && cur.Right == nil { // 到达 leaf 节点
 			res = append(res, cur.Val)
-
+			// 空 stack 说明，已经完成了遍历
 			if len(stack) == 0 {
 				break
 			}
+			// 从 stack 中取出下个节点
 			cur = stack[len(stack)-1]
 			stack = stack[:len(stack)-1]
 		} else {
-			left := cur.Left
-			right := cur.Right
+			// cur 不是 leaf，就先让 cur 入 stack
+			// 暂存 cur 的左右节点
+			left, right  := cur.Left, cur.Right
+			// 裁剪 cur，避免重复访问
 			cur.Left, cur.Right = nil, nil
 			stack = append(stack, cur)
 
+			// 移动 cur
 			if left != nil {
 				cur = left
 				if right != nil {
+					// 非空的 right 入 stack
 					stack = append(stack, right)
 				}
-			} else {
+			} else { // left == nil 
 				cur = right
 			}
 		}
 	}
+
 	return res
 }
