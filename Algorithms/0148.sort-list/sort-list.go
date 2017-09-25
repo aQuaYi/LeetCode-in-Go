a4ed07245710b003601070938969abc85fcfc109
aQua <aQua@aQuaYi.com>
2017-09-25 14:03:10 +0800

    148 copy，公用归并排序

--- Algorithms/0148.sort-list/sort-list.go
+++ Algorithms/0148.sort-list/sort-list.go
@@ -6,33 +6,58 @@ import (
 
 type ListNode = kit.ListNode
 
-func sortList(head *ListNode) *ListNode {
-	headPre := &ListNode{Next: head}
-
-	cur := head
-	for cur != nil && cur.Next != nil {
-		p := cur.Next
-		if cur.Val <= p.Val {
-			// p 不是需要插入到 cur 之前的元素
-			cur = p
-			continue
-		}
-
-		// p 是需要插入的元素
-		// 把 p 从 cur 后删除
-		cur.Next = p.Next
-		// 要把 p 插入到合适的 pre 和 next 之间
-		pre, next := headPre, headPre.Next
-		// 合适的位置的意思是
-		// pre.Val < p.Val <= next.Val
-		for next.Val < p.Val {
-			pre = next
-			next = next.Next
-		}
-		// 插入
-		pre.Next = p
-		p.Next = next
+func sortList(head *ListNode) *ListNode{
+    if head == nil || head.Next == nil {
+		return head
 	}
+	secHead := Split(head)
+	return MergeList(sortList(head), sortList(secHead))
+}
 
-	return headPre.Next
+// 从中间位置，切分 list
+func Split(head *ListNode) *ListNode {
+    if head == nil||head.Next == nil{
+        return nil
+	}
+	// fast 的变化速度是 slow 的两倍
+	// 当 fast 到达末尾的时候，slow 正好在 list 的中间
+	slow, fast := head, head
+	var tail *ListNode
+	for fast != nil && fast.Next != nil {
+		tail = slow
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	tail.Next = nil
+	return slow
+}
+
+func MergeList(left, right *ListNode) *ListNode {
+	if left == nil {
+		return right
+	} else if right == nil {
+		return left
+	}
+	var head, cur, p *ListNode
+	for left != nil && right != nil {
+		if left.Val < right.Val {
+			cur = left
+			left = left.Next
+		} else {
+			cur = right
+			right = right.Next
+		}
+		if head == nil {
+			head = cur
+		} else {
+			p.Next = cur
+		}
+		p = cur
+	}
+	if left == nil {
+		p.Next = right
+	} else {
+		p.Next = left
+	}
+	return head
 }
