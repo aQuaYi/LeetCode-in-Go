52abd1c51adc97390b42f5e7fde1c54b45231d32
aQua <aQua@aQuaYi.com>
2017-09-25 21:46:47 +0800

    155 accepted. 42ms > 38ms

--- Algorithms/0155.min-stack/min-stack.go
+++ Algorithms/0155.min-stack/min-stack.go
@@ -1,14 +1,9 @@
 package Problem0155
 
-// doublyLinkedNode 是双向链节点
-type doublyLinkedNode struct {
-	pre, next *doublyLinkedNode
-	val       int
-}
-
 // MinStack 是可以返回最小值的栈
 type MinStack struct {
-	min, max *doublyLinkedNode
+	min   int
+	stack []int
 }
 
 // Constructor 构造 MinStack
@@ -18,56 +13,40 @@ func Constructor() MinStack {
 
 // Push 存入数据
 func (s *MinStack) Push(x int) {
-	node := &doublyLinkedNode{val: x}
-
-	switch {
-	case s.min == nil:
-		s.min, s.max = node, node
-	case x <= s.min.val:
-		node.next = s.min
-		s.min.pre = node
-		s.min = node
-	case x >= s.max.val:
-		s.max.next = node
-		node.pre = s.max
-		s.max = node
-	default:
-		temp := s.max
-		for temp != nil && temp.val > x {
-			temp = temp.pre
-		}
-		node.next = temp.next
-		temp.next.pre = node
-		temp.next = node
-		node.pre = temp
+	if len(s.stack) == 0 {
+		s.min = x
 	}
 
+	s.stack = append(s.stack, x)
+	if s.min > x {
+		s.min = x
+	}
 }
 
-// Pop 抛弃最小值
+// Pop 抛弃最后一个入栈的值
 func (s *MinStack) Pop() {
-	if s.min == s.max {
-		s.min, s.max = nil, nil
+	if len(s.stack) == 0 {
 		return
 	}
-	s.min.next.pre = nil
-	s.min = s.min.next
+
+	s.stack = s.stack[:len(s.stack)-1]
+	s.min = 1<<63 - 1
+	for i := range s.stack {
+		if s.min > s.stack[i] {
+			s.min = s.stack[i]
+		}
+	}
 }
 
 // Top 返回最大值
 func (s *MinStack) Top() int {
-	if s.max != nil {
-		return s.max.val
+	if len(s.stack) > 0 {
+		return s.stack[len(s.stack)-1]
 	}
-
 	return 0
 }
 
 // GetMin 返回最小值
 func (s *MinStack) GetMin() int {
-	if s.min != nil {
-		return s.min.val
-	}
-
-	return 0
+	return s.min
 }
--- Algorithms/0155.min-stack/min-stack_test.go
+++ Algorithms/0155.min-stack/min-stack_test.go
@@ -16,12 +16,12 @@ func Test_Problem155_1(t *testing.T) {
 	s.Push(0)
 	// [-2, 0]
 	s.Push(-3)
-	// [-3, -2, 0]
-	ast.Equal(-3, s.GetMin(), "get min from [-3, -2, 0]")
-	// [-3, -2, 0]
+	// [-2, 0, -3]
+	ast.Equal(-3, s.GetMin(), "get min from [-2, 0, -3]")
+	// [-2, 0, -3]
 	s.Pop()
 	// [-2, 0]
-	ast.Equal(0, s.Top(), "get top from [-2,0]")
+	ast.Equal(0, s.Top(), "get top from [-2, 0]")
 	// [-2, 0]
 	ast.Equal(-2, s.GetMin(), "get min from [-2, 0]")
 	// [-2, 0]
@@ -36,13 +36,20 @@ func Test_Problem155_2(t *testing.T) {
 	s.Push(0)
 	// [-2, 0]
 	s.Push(-1)
-	// [-2, -1, 0]
+	// [-2, 0, -1]
 	ast.Equal(-2, s.GetMin(), "get min from [-2, -1, 0]")
-	// [-2, -1, 0]
-	ast.Equal(0, s.Top(), "get top from [-2, -1, 0]")
-	// [-2, -1, 0]
+	// [-2, 0, -1]
+	ast.Equal(-1, s.Top(), "get top from [-2, -1, 0]")
+	// [-2, 0, -1]
 	s.Pop()
-	// [-1, 0]
-	ast.Equal(-1, s.GetMin(), "get top from [-1,0]")
-	// [-1, 0]
+	// [-2, 0]
+	ast.Equal(-2, s.GetMin(), "get top from [0, -1]")
+	// [-2, 0]
+	s.Pop()
+	// [-2]
+	s.Pop()
+	// []
+	s.Pop()
+	// []
+	s.Top()
 }
