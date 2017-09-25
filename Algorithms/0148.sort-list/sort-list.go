685166b89d8f98f90511248e6a30ed5769dd681b
aQua <aQua@aQuaYi.com>
2017-09-25 13:52:15 +0800

    148 accepted. 336ms > 23ms

---
+++ Algorithms/0148.sort-list/sort-list.go
@@ -0,0 +1,38 @@
+package Problem0148
+
+import (
+	"github.com/aQuaYi/LeetCode-in-Golang/kit"
+)
+
+type ListNode = kit.ListNode
+
+func sortList(head *ListNode) *ListNode {
+	headPre := &ListNode{Next: head}
+
+	cur := head
+	for cur != nil && cur.Next != nil {
+		p := cur.Next
+		if cur.Val <= p.Val {
+			// p 不是需要插入到 cur 之前的元素
+			cur = p
+			continue
+		}
+
+		// p 是需要插入的元素
+		// 把 p 从 cur 后删除
+		cur.Next = p.Next
+		// 要把 p 插入到合适的 pre 和 next 之间
+		pre, next := headPre, headPre.Next
+		// 合适的位置的意思是
+		// pre.Val < p.Val <= next.Val
+		for next.Val < p.Val {
+			pre = next
+			next = next.Next
+		}
+		// 插入
+		pre.Next = p
+		p.Next = next
+	}
+
+	return headPre.Next
+}
---
+++ Algorithms/0148.sort-list/sort-list_test.go
@@ -0,0 +1,42 @@
+package Problem0148
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aQuaYi/LeetCode-in-Golang/kit"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	head []int
+	ans  []int
+}{
+
+	{
+		[]int{2, 4, 3, 1, 5},
+		[]int{1, 2, 3, 4, 5},
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_insertionSortList(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+
+		head := kit.Ints2List(tc.head)
+		ast.Equal(tc.ans, kit.List2Ints(sortList(head)), "输入:%v", tc)
+	}
+}
+
+func Benchmark_insertionSortList(b *testing.B) {
+	head := kit.Ints2List([]int{9, 6, 3, 1, 4, 8, 2, 5, 7, 19, 16, 13, 11, 14, 18, 12, 15, 17, 29, 26, 23, 21, 24, 28, 22, 25, 27, 39, 36, 33, 31, 34, 38, 32, 35, 37, 49, 46, 43, 41, 44, 48, 42, 45, 47})
+	for i := 0; i < b.N; i++ {
+		sortList(head)
+	}
+}
