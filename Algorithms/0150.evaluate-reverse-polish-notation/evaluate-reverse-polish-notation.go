6fb0b1d0b8812a4423a86d24ef028d8e59aab20c
aQua <aQua@aQuaYi.com>
2017-09-26 09:36:08 +0800

    150 accepted. 9ms == 9ms

--- Algorithms/0150.evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.go
+++ Algorithms/0150.evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.go
@@ -1,6 +1,38 @@
 package Problem0150
 
-func evalRPN(tokens []string) int {
+import (
+	"strconv"
+)
 
-	return 9
+func evalRPN(tokens []string) int {
+	// 用于存放数字的栈
+	nums := make([]int, 0, len(tokens))
+	for _, s := range tokens {
+		if s == "+" ||
+			s == "-" ||
+			s == "*" ||
+			s == "/" {
+			b, a := nums[len(nums)-1], nums[len(nums)-2]
+			nums = nums[:len(nums)-2]
+			nums = append(nums, compute(a, b, s))
+		} else {
+			temp, _ := strconv.Atoi(s)
+			nums = append(nums, temp)
+		}
+	}
+
+	return nums[0]
+}
+
+func compute(a, b int, opt string) int {
+	switch opt {
+	case "+":
+		return a + b
+	case "-":
+		return a - b
+	case "*":
+		return a * b
+	default:
+		return a / b
+	}
 }
