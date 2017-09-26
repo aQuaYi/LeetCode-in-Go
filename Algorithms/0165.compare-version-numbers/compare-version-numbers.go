b0ce52323b5d4ec9a52a63c8d9ff21c5db640a30
aQua <aQua@aQuaYi.com>
2017-09-26 15:20:51 +0800

    165 added

---
+++ Algorithms/0165.compare-version-numbers/compare-version-numbers.go
@@ -0,0 +1,6 @@
+package Problem0165
+
+func compareVersion(version1 string, version2 string) int {
+
+	return -1
+}
---
+++ Algorithms/0165.compare-version-numbers/compare-version-numbers_test.go
@@ -0,0 +1,53 @@
+package Problem0165
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	version1 string
+	version2 string
+	ans      int
+}{
+
+	{
+		"0.1",
+		"1.0",
+		-1,
+	},
+
+	{
+		"1.0",
+		"0.1",
+		1,
+	},
+
+	{
+		"1.1",
+		"1.1",
+		0,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_compareVersion(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, compareVersion(tc.version1, tc.version2), "输入:%v", tc)
+	}
+}
+
+func Benchmark_compareVersion(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			compareVersion(tc.version1, tc.version2)
+		}
+	}
+}
