c43c8db74bb50131ea4292235036bf42b64c9b1d
aQua <aQua@aQuaYi.com>
2017-09-27 09:46:26 +0800

    166 wrong answer

--- Algorithms/0166.fraction-to-recurring-decimal/fraction-to-recurring-decimal.go
+++ Algorithms/0166.fraction-to-recurring-decimal/fraction-to-recurring-decimal.go
@@ -1,6 +1,7 @@
 package Problem0166
 
 import (
+	"fmt"
 	"strconv"
 )
 
@@ -14,29 +15,40 @@ func fractionToDecimal(numerator int, denominator int) string {
 		minus = "-"
 	}
 
+	// 确保 n 和 d 是非负数
 	n, d := abs(numerator), abs(denominator)
 
-	di := 0
-	base := 0
+	// n / d 的小数部分
+	ds := ""
+
 	if n < d {
+		rec := make(map[int]int)
+		idx := 0
+		digit := 0
 		for {
-			n, base = next(n, d)
-			di = di*base + n/d
+			if i, ok := rec[n]; ok {
+				return fmt.Sprintf("0.%s(%s)", ds[:i], ds[i:])
+			}
+
+			idx += digit
+			rec[n] = idx
+
+			digit = 0
+			for n < d {
+				n *= 10
+				digit++
+			}
+
+			format := fmt.Sprintf("%%0%dd", digit)
+			ds += fmt.Sprintf(format, n/d)
 			n %= d
 
 			if n == 0 {
-				break
-			}
-
-			if ds, ok := checkRepeat(di); ok {
-				return ds
+				return "0." + ds
 			}
 		}
-
-		return "0." + strconv.Itoa(di)
 	}
 
-	ds := ""
 	ds = fractionToDecimal(n%d, d)
 	return minus + strconv.Itoa(n/d) + ds[1:]
 }
@@ -46,29 +58,4 @@ func abs(a int) int {
 		return a
 	}
 	return -a
-}
-
-func next(n, d int) (int, int) {
-	base := 1
-	for n < d {
-		n *= 10
-		base *= 10
-	}
-	return n, base
-}
-
-func checkRepeat(num int) (string, bool) {
-	s := strconv.Itoa(num)
-
-	n := len(s)
-
-	start := n % 2
-	for i := start; i <= n-2; i += 2 {
-		i1 := (n + i) / 2
-		if s[i:i1] == s[i1:] {
-			return "0." + s[:i] + "(" + s[i:i1] + ")", true
-		}
-	}
-
-	return "", false
 }
