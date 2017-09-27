86ec086e0dc0c383bda6bcb12c3658a03a640799
aQua <aQua@aQuaYi.com>
2017-09-27 16:01:01 +0800

    174 wrong answer

--- Algorithms/0174.dungeon-game/dungeon-game.go
+++ Algorithms/0174.dungeon-game/dungeon-game.go
@@ -11,40 +11,74 @@ func calculateMinimumHP(dungeon [][]int) int {
 		return 1
 	}
 
-	min := 1<<63 - 1
+	// init[i][j] 到达 (i,j) 后还活着，需要的最小初始健康值
+	init := make([][]int, m)
+	// health[i][j] 到达 (i,j) 后的健康值
+	health := make([][]int, m)
+	for i := 0; i < m; i++ {
+		init[i] = make([]int, n)
+		health[i] = make([]int, n)
+	}
 
-	var dfs func(int, int, int, int)
-	// health 是达到 (i,j) 点前的健康值
-	// init 是到达 (0,0) 点前的初始值
-	dfs = func(i, j, health, init int) {
-		if i < m && j < n {
-			//,到达 (i,j) 后，更新 health 值
-			health += dungeon[i][j]
-			if health < 1 {
-				// 提升 init 值
-				init = init - health + 1
-				// 确保到达 (i,j) 后，health == 1
-				health = 1
-			}
+	init[0][0] = 1
+	if dungeon[0][0] < 0 {
+		init[0][0] = 1 - dungeon[0][0]
+	}
+	health[0][0] = 1
 
-			if i == m-1 && j == n-1 && min > init {
-				// 到达右下角，且出现新低
-				min = init
-			}
-		}
-
-		if i+1 < m {
-			// 还能往下走，就往下走一步
-			dfs(i+1, j, health, init)
-		}
-
-		if j+1 < n {
-			// 还能往右走，就往右走一步
-			dfs(i, j+1, health, init)
+	for i := 1; i < m; i++ {
+		health[i][0] = health[i-1][0] + dungeon[i][0]
+		if health[i][0] < 1 {
+			init[i][0] = init[i-1][0] - health[i][0] + 1
+			health[i][0] = 1
+		} else {
+			init[i][0] = init[i-1][0]
 		}
 	}
 
-	dfs(0, 0, 1, 1)
+	for j := 1; j < n; j++ {
+		health[0][j] = health[0][j-1] + dungeon[0][j]
+		if health[0][j] < 0 {
+			init[0][j] = init[0][j-1] - health[0][j] + 1
+			health[0][j] = 1
+		} else {
+			init[0][j] = init[0][j-1]
+		}
+	}
+	iRight, hRight, iDown, hDown := 0, 0, 0, 0
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			iRight = init[i][j-1] - min(health[i][j-1]+dungeon[i][j]-1, 0)
+			hRight = max(1, health[i][j-1]+dungeon[i][j])
+			iDown = init[i-1][j] - min(health[i-1][j]+dungeon[i][j]-1, 0)
+			hDown = max(1, health[i-1][j]+dungeon[i][j])
+			switch {
+			case iRight == iDown:
+				init[i][j] = iRight
+				health[i][j] = max(hRight, hDown)
+			case iRight < iDown:
+				init[i][j] = iRight
+				health[i][j] = hRight
+			default:
+				init[i][j] = iDown
+				health[i][j] = hDown
+			}
+		}
+	}
 
-	return min
+	return init[m-1][n-1]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
