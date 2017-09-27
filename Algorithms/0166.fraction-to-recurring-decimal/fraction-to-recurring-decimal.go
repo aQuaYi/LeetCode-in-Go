4081965b94ffd971dd29f5af04706b73951cb201
aQua <aQua@aQuaYi.com>
2017-09-27 10:20:46 +0800

    166 添加了注释

--- Algorithms/0166.fraction-to-recurring-decimal/fraction-to-recurring-decimal.go
+++ Algorithms/0166.fraction-to-recurring-decimal/fraction-to-recurring-decimal.go
@@ -11,6 +11,7 @@ func fractionToDecimal(n int, d int) string {
 	}
 
 	if n*d < 0 {
+		// n, d 异号，则结果前面有负号
 		return "-" + fractionToDecimal(abs(n), abs(d))
 	}
 
@@ -19,31 +20,35 @@ func fractionToDecimal(n int, d int) string {
 
 	// n / d 的小数部分
 	ds := ""
-
-	if n < d {
-		rec := make(map[int]int, 1024)
-		idx := 0
-		for {
-			if i, ok := rec[n]; ok {
-				return fmt.Sprintf("0.%s(%s)", ds[:i], ds[i:])
-			}
-
-			rec[n] = idx
-
-			n *= 10
-			idx++
-
-			ds += string(n/d + '0')
-			n %= d
-
-			if n == 0 {
-				return "0." + ds
-			}
-		}
+	if n >= d {
+		ds = fractionToDecimal(n%d, d)
+		return strconv.Itoa(n/d) + ds[1:]
 	}
 
-	ds = fractionToDecimal(n%d, d)
-	return strconv.Itoa(n/d) + ds[1:]
+	// rec 记录所有出现过的 n
+	rec := make(map[int]int, 1024)
+	// idx 是 n/d 的结果的在 ds 的索引号
+	idx := 0
+	for {
+		if i, ok := rec[n]; ok {
+			// n 重复出现，则说明出现了循环的部分
+			// 循环部分的起点，就是 n 上次出现的 idx 值
+			return fmt.Sprintf("0.%s(%s)", ds[:i], ds[i:])
+		}
+
+		rec[n] = idx
+
+		n *= 10
+		idx++
+
+		ds += string(n/d + '0')
+		n %= d
+
+		if n == 0 {
+			// 没有循环部分
+			return "0." + ds
+		}
+	}
 }
 
 func abs(a int) int {
