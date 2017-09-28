ea456470ee245e92d7156a59a21a000888727c47
aQua <aQua@aQuaYi.com>
2017-09-28 14:29:06 +0800

    200 finish

--- Algorithms/0200.number-of-islands/number-of-islands.go
+++ Algorithms/0200.number-of-islands/number-of-islands.go
@@ -1,41 +1,45 @@
 package Problem0200
 
 func numIslands(grid [][]byte) int {
+	// 获取 grid 的大小
 	m := len(grid)
 	if m == 0 {
 		return 0
 	}
-
 	n := len(grid[0])
 
+	// bfs 搜索时，存放点坐标的队列
 	x := make([]int, 0, m*n)
 	y := make([]int, 0, m*n)
 
+	// 往队列中添加 (i,j) 点，并修改 (i,j) 点的值
+	// 避免重复搜索
 	var add = func(i, j int) {
 		x = append(x, i)
 		y = append(y, j)
 		grid[i][j] = '0'
 	}
 
+	// 从坐标队列中，取出坐标点
 	var pop = func() (int, int) {
 		i := x[0]
 		x = x[1:]
 		j := y[0]
 		y = y[1:]
-
 		return i, j
 	}
 
-	var bfs = func(col, row int) int {
-		if grid[col][row] == '0' {
+	var bfs = func(i, j int) int {
+		if grid[i][j] == '0' {
 			return 0
 		}
 
-		add(col, row)
+		add(i, j)
 
 		for len(x) > 0 {
-			i, j := pop()
+			i, j = pop()
 
+			// 搜索 (i,j) 点的 上下左右 四个方位
 			if 0 <= i-1 && grid[i-1][j] == '1' {
 				add(i-1, j)
 			}
