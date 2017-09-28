254a033509c5fb9f30e08c9dde69df689aca1d77
aQua <aQua@aQuaYi.com>
2017-09-28 13:46:02 +0800

    199 finish

--- Algorithms/0199.binary-tree-right-side-view/binary-tree-right-side-view.go
+++ Algorithms/0199.binary-tree-right-side-view/binary-tree-right-side-view.go
@@ -8,23 +8,26 @@ type TreeNode = kit.TreeNode
 
 func rightSideView(root *TreeNode) []int {
 	if root == nil {
-		return nil 
+		return nil
 	}
 
 	if root.Left == nil && root.Right == nil {
 		return []int{root.Val}
 	}
 
+	// 递归求解
 	ls := rightSideView(root.Left)
 	rs := rightSideView(root.Right)
 
-	if len(ls ) > len(rs) {
+	// 左边的分支比右边长的部分，还是可以从右边看到
+	if len(ls) > len(rs) {
 		rs = append(rs, ls[len(rs):]...)
 	}
 
-	res := make([]int , len(rs)+1)
+	// 添加 root 节点的值
+	res := make([]int, len(rs)+1)
 	res[0] = root.Val
 	copy(res[1:], rs)
 
-	return res 
+	return res
 }
