62937b14df5aafb253319761d5c43fbcb8a687e8
aQua <aQua@aQuaYi.com>
2017-09-28 11:14:38 +0800

    187 finish

--- Algorithms/0187.repeated-dna-sequences/repeated-dna-sequences.go
+++ Algorithms/0187.repeated-dna-sequences/repeated-dna-sequences.go
@@ -11,10 +11,12 @@ func findRepeatedDnaSequences(s string) []string {
 	}
 
 	str := ""
+	// rec 记录各个 subString 出现的次数
 	rec := make(map[string]int, len(s)-9)
 	for i := 0; i+10 <= len(s); i++ {
 		str = s[i : i+10]
 		if v := rec[str]; v == 1 {
+			// 把已经出现过一次的 subString 存放入 res
 			res = append(res, str)
 		}
 		rec[str]++
