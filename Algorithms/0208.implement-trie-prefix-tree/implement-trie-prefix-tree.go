f592db04c68781f64c7403b6904614c039854aa6
aQua <aQua@aQuaYi.com>
2017-09-29 14:26:11 +0800

    208 finish

--- Algorithms/0208.implement-trie-prefix-tree/implement-trie-prefix-tree.go
+++ Algorithms/0208.implement-trie-prefix-tree/implement-trie-prefix-tree.go
@@ -37,5 +37,6 @@ func (t *Trie) Search(word string) bool {
 
 // StartsWith returns true if there is any word in the trie that starts with the given prefix.
 func (t *Trie) StartsWith(prefix string) bool {
-	return t.dict[prefix ]||t.dictPrefix[prefix]
+	// dict 比较小，先检查
+	return t.dict[prefix] || t.dictPrefix[prefix]
 }
