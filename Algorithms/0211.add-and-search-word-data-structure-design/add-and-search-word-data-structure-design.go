5040feda52aa5665b22793a618191fff418175d2
aQua <aQua@aQuaYi.com>
2017-09-29 15:25:03 +0800

    211 accepted. 339ms > 219ms

--- Algorithms/0211.add-and-search-word-data-structure-design/add-and-search-word-data-structure-design.go
+++ Algorithms/0211.add-and-search-word-data-structure-design/add-and-search-word-data-structure-design.go
@@ -6,21 +6,38 @@ type WordDictionary struct {
 }
 
 // Constructor 构建 WordDictionary
-/** Initialize your data structure here. */
+// Initialize your data structure here.
 func Constructor() WordDictionary {
 	return WordDictionary{}
 }
 
 // AddWord 往 WordDictionary 中添加 word
-/** Adds a word into the data structure. */
-func (this *WordDictionary) AddWord(word string) {
-
+// Adds a word into the data structure.
+func (d *WordDictionary) AddWord(word string) {
+	d.dict = append(d.dict, word)
 }
 
 // Search 返回 true 如果 WordDictionary 中包含有 word
-/** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
-func (this *WordDictionary) Search(word string) bool {
+// Returns if the word is in the data structure.
+// A word could contain the dot character '.' to represent any one letter.
+func (d *WordDictionary) Search(word string) bool {
+	size := len(word)
+	i := 0
+	for _, w := range d.dict {
+		if len(w) == size {
+			i = 0
+			for ; i < size; i++ {
+				if word[i] != '.' && word[i] != w[i] {
+					break
+				}
+			}
+			if i == size {
+				return true
+			}
+		}
+	}
 
+	return false
 }
 
 /**
