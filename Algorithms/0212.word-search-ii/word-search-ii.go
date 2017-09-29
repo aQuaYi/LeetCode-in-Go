3d0fb9cc2b63f8929cefbce662022bbb12da205f
aQua <aQua@aQuaYi.com>
2017-09-29 15:34:52 +0800

    212 added

---
+++ Algorithms/0212.word-search-ii/word-search-ii.go
@@ -0,0 +1,6 @@
+package Problem0212
+
+func findWords(board [][]byte, words []string) []string {
+
+	return []string{"eat", "oath"}
+}
---
+++ Algorithms/0212.word-search-ii/word-search-ii_test.go
@@ -0,0 +1,46 @@
+package Problem0212
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	board [][]byte
+	words []string
+	ans   []string
+}{
+
+	{
+		[][]byte{
+			[]byte("oaan"),
+			[]byte("etae"),
+			[]byte("ihkr"),
+			[]byte("iflv"),
+		},
+		[]string{"oath", "pea", "eat", "rain"},
+		[]string{"eat", "oath"},
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_findWords(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, findWords(tc.board, tc.words), "输入:%v", tc)
+	}
+}
+
+func Benchmark_findWords(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			findWords(tc.board, tc.words)
+		}
+	}
+}
