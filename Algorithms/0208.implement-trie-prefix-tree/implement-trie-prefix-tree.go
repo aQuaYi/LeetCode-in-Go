a67f5ca7b9d083f6d156b4617e5915dfcfd5b239
aQua <aQua@aQuaYi.com>
2017-09-29 14:11:51 +0800

    208 wrong answer

--- Algorithms/0208.implement-trie-prefix-tree/implement-trie-prefix-tree.go
+++ Algorithms/0208.implement-trie-prefix-tree/implement-trie-prefix-tree.go
@@ -10,36 +10,29 @@ package Problem0208
 
 // Trie 是便于 word 插入与查找的数据结构
 type Trie struct {
-	words []string
+	dict map[string]bool
 }
 
 // Constructor initialize your data structure here.
 func Constructor() Trie {
-	return Trie{}
+	d := make(map[string]bool, 1024)
+	d[""] = true
+	return Trie{dict: d}
 }
 
 // Insert a word into the trie.
 func (t *Trie) Insert(word string) {
-	t.words = append(t.words, word)
+	for i := 1; i <= len(word); i++ {
+		t.dict[word[:i]] = true
+	}
 }
 
 // Search returns true if the word is in the trie.
 func (t *Trie) Search(word string) bool {
-	for _, w := range t.words {
-		if w == word {
-			return true
-		}
-	}
-	return false
+	return t.dict[word]
 }
 
 // StartsWith returns true if there is any word in the trie that starts with the given prefix.
 func (t *Trie) StartsWith(prefix string) bool {
-	end := len(prefix)
-	for _, w := range t.words {
-		if len(w) >= end && w[:end] == prefix {
-			return true
-		}
-	}
-	return false
+	return t.dict[prefix]
 }
