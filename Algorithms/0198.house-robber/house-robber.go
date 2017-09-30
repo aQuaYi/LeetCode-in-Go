07dbdd340071d7a616cbc71bb9d060893f0092db
aQua <aQua@aQuaYi.com>
2017-09-30 13:54:04 +0800

    198 wrong answer

--- Algorithms/0198.house-robber/house-robber.go
+++ Algorithms/0198.house-robber/house-robber.go
@@ -1,6 +1,20 @@
 package Problem0198
 
 func rob(nums []int) int {
+	return max(robbing(nums), robbing(nums[1:]))
+}
 
-	return 25
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func robbing(nums []int) int {
+	res := 0
+	for i := 0; i < len(nums); i += 2 {
+		res += nums[i]
+	}
+	return res
 }
