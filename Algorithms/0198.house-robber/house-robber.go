e6a93c50be9f09ada7014a969fb20be55d09f143
aQua <aQua@aQuaYi.com>
2017-09-30 14:54:19 +0800

    198 copy, accepted.

--- Algorithms/0198.house-robber/house-robber.go
+++ Algorithms/0198.house-robber/house-robber.go
@@ -6,11 +6,17 @@ func rob(nums []int) int {
 		return 0
 	case 1:
 		return nums[0]
-	case 2:
-		return max(nums[0], nums[1])
-	default:
-		return max(nums[0]+rob(nums[2:]), nums[1]+rob(nums[3:]))
 	}
+
+	dp := make([]int, len(nums)+1)
+	nums = append(nums, 0)
+	dp[0] = nums[0]
+	dp[1] = max(nums[0], nums[1])
+	for i := 2; i < len(nums); i++ {
+		dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+	}
+
+	return dp[len(nums)-1]
 }
 
 func max(a, b int) int {
