60fcb11856f412b71de6a6e9b4e6c3d7a51f23b3
aQua <aQua@aQuaYi.com>
2017-09-30 16:57:02 +0800

    337 copied.

--- Algorithms/0337.house-robber-iii/house-robber-iii.go
+++ Algorithms/0337.house-robber-iii/house-robber-iii.go
@@ -7,20 +7,23 @@ import (
 type TreeNode = kit.TreeNode
 
 func rob(root *TreeNode) int {
+	res := robSub(root)
+	return max(res[0], res[1])
+}
+
+func robSub(root *TreeNode) []int {
 	if root == nil {
-		return 0
+		return make([]int, 2)
 	}
 
-	robRoot := root.Val
-	if root.Left != nil {
-		robRoot += rob(root.Left.Left) + rob(root.Left.Right)
-	}
-	if root.Right != nil {
-		robRoot += rob(root.Right.Left) + rob(root.Right.Right)
-	}
+	left := robSub(root.Left)
+	right := robSub(root.Right)
 
-	noRoot := rob(root.Left)+rob(root.Right)
-	return max(robRoot, noRoot )
+	res := make([]int, 2)
+	res[0] = max(left[0], left[1]) + max(right[0], right[1])
+	res[1] = root.Val + left[0] + right[0]
+
+	return res
 }
 
 func max(a, b int) int {
