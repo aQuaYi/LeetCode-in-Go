4e3b0d5f423d9181aeb8baa738c3b39bb73ce218
aQua <aQua@aQuaYi.com>
2017-10-03 11:07:43 +0800

    215 Time Limit Exceeded

--- Algorithms/0215.kth-largest-element-in-an-array/kth-largest-element-in-an-array.go
+++ Algorithms/0215.kth-largest-element-in-an-array/kth-largest-element-in-an-array.go
@@ -1,6 +1,42 @@
 package Problem0215
 
-func findKthLargest(nums []int, k int) int {
+import "math/rand"
 
-	return 5
+func findKthLargest(a []int, k int) int {
+	quickSort(a)
+	return a[k-1]
+}
+
+func quickSort(a []int) {
+	if len(a) <= 1 {
+		return
+	}
+	i := partition(a)
+	quickSort(a[:i])
+	quickSort(a[i+1:])
+}
+
+func partition(a []int) int {
+	size := len(a)
+	i := rand.Intn(size)
+
+	a[0], a[i] = a[i], a[0]
+	t := a[0]
+
+	lo, hi := 1, size-1
+	for {
+		for lo < size-1 && a[lo] > t {
+			lo++
+		}
+		for 0 < hi && t > a[hi] {
+			hi--
+		}
+		if lo >= hi {
+			break
+		}
+		a[lo], a[hi] = a[hi], a[lo]
+	}
+
+	a[hi], a[0] = a[0], a[hi]
+	return hi
 }
