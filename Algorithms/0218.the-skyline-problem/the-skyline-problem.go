0d343260af169c18b290d9a4647165cd9c3414e0
aQua <aQua@aQuaYi.com>
2017-10-03 19:32:35 +0800

    218 added

--- Algorithms/0218.the-skyline-problem/the-skyline-problem.go
+++ Algorithms/0218.the-skyline-problem/the-skyline-problem.go
@@ -2,5 +2,13 @@ package Problem0218
 
 func getSkyline(buildings [][]int) [][]int {
 
-	return
+	return [][]int{
+		[]int{2, 10},
+		[]int{3, 15},
+		[]int{7, 12},
+		[]int{12, 0},
+		[]int{15, 10},
+		[]int{20, 8},
+		[]int{24, 0},
+	}
 }
--- Algorithms/0218.the-skyline-problem/the-skyline-problem_test.go
+++ Algorithms/0218.the-skyline-problem/the-skyline-problem_test.go
@@ -10,12 +10,26 @@ import (
 // tcs is testcase slice
 var tcs = []struct {
 	buildings [][]int
-	ans  [][]int 
+	ans       [][]int
 }{
 
 	{
-		,
-		,
+		[][]int{
+			[]int{2, 9, 10},
+			[]int{3, 7, 15},
+			[]int{5, 12, 12},
+			[]int{15, 20, 10},
+			[]int{19, 24, 8},
+		},
+		[][]int{
+			[]int{2, 10},
+			[]int{3, 15},
+			[]int{7, 12},
+			[]int{12, 0},
+			[]int{15, 10},
+			[]int{20, 8},
+			[]int{24, 0},
+		},
 	},
 
 	// 可以有多个 testcase
@@ -23,7 +37,7 @@ var tcs = []struct {
 
 func Test_getSkyline(t *testing.T) {
 	ast := assert.New(t)
-	
+
 	for _, tc := range tcs {
 		fmt.Printf("~~%v~~\n", tc)
 		ast.Equal(tc.ans, getSkyline(tc.buildings), "输入:%v", tc)
