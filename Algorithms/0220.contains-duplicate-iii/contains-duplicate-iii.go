b977b413affdac5d41a9472f7fa2e09e222a92d1
aQua <aQua@aQuaYi.com>
2017-10-04 13:27:24 +0800

    220 错误地理解了题意

--- Algorithms/0220.contains-duplicate-iii/contains-duplicate-iii.go
+++ Algorithms/0220.contains-duplicate-iii/contains-duplicate-iii.go
@@ -1,8 +1,15 @@
 package Problem0220
 
 func containsNearbyAlmostDuplicate(nums []int, k int, t int) bool {
+	for i := 0; i+k < len(nums); i++ {
+		for j := i + 1; j <= i+k; j++ {
+			if abs(nums[i]-nums[j]) > t {
+				return false
+			}
+		}
+	}
 
-	return
+	return true
 }
 
 func abs(a int) int {
