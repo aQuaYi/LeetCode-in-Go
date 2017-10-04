48e478492cecc5d9ebe915d2b4625488215f9598
aQua <aQua@aQuaYi.com>
2017-10-04 13:33:59 +0800

    220 accepted. 729ms > 19ms

--- Algorithms/0220.contains-duplicate-iii/contains-duplicate-iii.go
+++ Algorithms/0220.contains-duplicate-iii/contains-duplicate-iii.go
@@ -1,8 +1,8 @@
 package Problem0220
 
 func containsNearbyAlmostDuplicate(nums []int, k int, t int) bool {
-	for i := 0; i+k < len(nums); i++ {
-		for j := i + 1; j <= i+k; j++ {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums) && j <= i+k; j++ {
 			if abs(nums[i]-nums[j]) <= t {
 				return true
 			}
