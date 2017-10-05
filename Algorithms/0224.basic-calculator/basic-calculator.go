a04645a40a88cb5c5a1ed83ba74b2c80b20106b7
aQua <aQua@aQuaYi.com>
2017-10-05 16:29:41 +0800

    224 finish

--- Algorithms/0224.basic-calculator/basic-calculator.go
+++ Algorithms/0224.basic-calculator/basic-calculator.go
@@ -1,50 +1,48 @@
 package Problem0224
 
-// s 中不会含有 × ÷，所以，可以不用理会括号
 func calculate(s string) int {
+	res := 0
+	stack := make([]int, 0, len(s))
+	sign := 1
 	num := 0
-	temp := 0
-	opt := byte('+')
-	str := ""
 
 	for i := 0; i < len(s); i++ {
 		switch s[i] {
 		case '1', '2', '3', '4', '5', '6', '7', '8', '9', '0':
-			temp = temp*10 + int(s[i]-'0')
-		case '+', '-':
-			num = operate(num, temp, opt)
-			temp = 0
-			opt = s[i]
-		case '(':
-			str, i = cut(s, i)
-			num = operate(num, calculate(str), opt)
-			opt = byte('+')
-			temp = 0 // 此时 temp 应该是 0 ，再次赋值，以防万一
-		}
-	}
-
-	return operate(num, temp, opt)
-}
-
-func cut(s string, begin int) (string, int) {
-	num := 1
-	i := begin + 1
-	for ; i < len(s); i++ {
-		if s[i] == '(' {
-			num++
-		} else if s[i] == ')' {
-			num--
-			if num == 0 {
-				break
+			// 提取 s 中的数字
+			num = 0
+			for ; i < len(s) && s[i] >= '0' && s[i] <= '9'; i++ {
+				num = 10*num + int(s[i]-'0')
 			}
+			// 根据前面的记录，进行运算
+			res += sign * num
+			// 此时 s[i] 已经不是数字了
+			// for 语句中，会再＋1，所以这里先 -1
+			i--
+		case '+':
+			// 记录运算符
+			sign = 1
+		case '-':
+			// 记录运算符
+			sign = -1
+		case '(':
+			// 遇到 '(' 就把当前的 res 和 sign 入栈，保存好当前的运行环境
+			stack = append(stack, res, sign)
+			// 对 res 和 sign 赋予新的值
+			res = 0
+			sign = 1
+		case ')':
+			// 遇到 ')' 出栈
+			// sign 是与这个 ')' 匹配的 '(' 前的运算符号
+			sign = stack[len(stack)-1]
+			// temp 是 sign 前的运算结果
+			temp := stack[len(stack)-2]
+			stack = stack[:len(stack)-2]
+			// '(' 与 ')' 之间的运算结果
+			//          ↓
+			res = sign*res + temp
 		}
 	}
-	return s[begin+1 : i], i
-}
 
-func operate(a, b int, opt byte) int {
-	if opt == '+' {
-		return a + b
-	}
-	return a - b
+	return res
 }
