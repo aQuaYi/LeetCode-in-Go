96155c1414c2e30cb9ede8af52c74c8609be7698
aQua <aQua@aQuaYi.com>
2017-10-05 16:03:15 +0800

    224 ut fail

--- Algorithms/0224.basic-calculator/basic-calculator.go
+++ Algorithms/0224.basic-calculator/basic-calculator.go
@@ -2,39 +2,48 @@ package Problem0224
 
 // s 中不会含有 × ÷，所以，可以不用理会括号
 func calculate(s string) int {
-	nums := make([]int, 1, len(s))
-	nums[0] = 0
-	opt := '+'
+	num := 0
+	temp := 0
+	opt := byte('+')
+	str := ""
 
-	push := func(n int) {
-		nums = append(nums, n)
-	}
-
-	pop := func() int {
-		res := nums[len(nums)-1]
-		nums = nums[:len(nums)-1]
-		return res
-	}
-
-	for _, b := range s {
-		switch b {
+	for i := 0; i < len(s); i++ {
+		switch s[i] {
 		case '1', '2', '3', '4', '5', '6', '7', '8', '9', '0':
-			push(operate(pop(), int(b-'0'), opt))
+			temp = temp*10 + int(s[i]-'0')
 		case '+', '-':
-			opt = b
+			num = operate(num, temp, opt)
+			temp = 0
+			opt = s[i]
+		case '(':
+			str, i = cut(s, i)
+			num = operate(num, calculate(str), opt)
+			opt = byte('+')
+			temp = 0 // 此时 temp 应该是 0 ，再次赋值，以防万一
 		}
 	}
 
-	return nums[0]
+	return num
+}
+func cut(s string, begin int) (string, int) {
+	num := 1
+	i := begin + 1
+	for ; i < len(s); i++ {
+		if s[i] == '(' {
+			num++
+		} else if s[i] == ')' {
+			num--
+			if num == 0 {
+				break
+			}
+		}
+	}
+	return s[begin+1 : i], i
 }
 
-func operate(a, b int, opt rune) int {
-	switch opt {
-	case '+':
+func operate(a, b int, opt byte) int {
+	if opt == '+' {
 		return a + b
-	case '-':
-		return a - b
-	default:
-		return 0
 	}
+	return a - b
 }
