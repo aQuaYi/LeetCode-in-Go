dcb22f4ddbba59509e593b4624e77c363d3b0da8
aQua <aQua@aQuaYi.com>
2017-10-06 13:42:14 +0800

    230 accepted. 22ms == 22ms

--- Algorithms/0230.kth-smallest-element-in-a-bst/README.md
+++ Algorithms/0230.kth-smallest-element-in-a-bst/README.md
@@ -10,5 +10,10 @@ Follow up:
 What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently? How would you optimize the kthSmallest routine?
 
 ## 解题思路
+以下是[BST](https://zh.wikipedia.org/zh-cn/%E4%BA%8C%E5%85%83%E6%90%9C%E5%B0%8B%E6%A8%B9)的性质：
+1. 若任意节点的左子树不空，则左子树上所有节点的值均小于它的根节点的值；
+1. 若任意节点的右子树不空，则右子树上所有节点的值均大于它的根节点的值；
+1. 任意节点的左、右子树也分别为二叉查找树；
+1. 没有键值相等的节点。
 
 见程序注释
--- Algorithms/0230.kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.go
+++ Algorithms/0230.kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.go
@@ -3,9 +3,26 @@ package Problem0230
 import (
 	"github.com/aQuaYi/LeetCode-in-Golang/kit"
 )
+
 type TreeNode = kit.TreeNode
 
 func kthSmallest(root *TreeNode, k int) int {
+	leftSize := getSize(root.Left)
+	switch {
+	case k <= leftSize:
+		return kthSmallest(root.Left, k)
+	case leftSize+1 < k:
+		return kthSmallest(root.Right, k-leftSize-1)
+	default:
+		return root.Val
+	}
+}
 
-	return 2
+
+func getSize(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	return 1 + getSize(root.Left) + getSize(root.Right)
 }
