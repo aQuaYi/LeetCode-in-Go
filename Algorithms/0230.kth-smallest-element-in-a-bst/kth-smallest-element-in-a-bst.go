b94b7ba6eef92a395cefb2a88f6494c4d5345186
aQua <aQua@aQuaYi.com>
2017-10-06 13:43:48 +0800

    230 finish

--- Algorithms/0230.kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.go
+++ Algorithms/0230.kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.go
@@ -10,19 +10,21 @@ func kthSmallest(root *TreeNode, k int) int {
 	leftSize := getSize(root.Left)
 	switch {
 	case k <= leftSize:
+		// 答案存在于 root.Left 中
 		return kthSmallest(root.Left, k)
 	case leftSize+1 < k:
+		// 答案存在于 root.Right 中
 		return kthSmallest(root.Right, k-leftSize-1)
 	default:
+		// 答案是 root.Val
 		return root.Val
 	}
 }
 
-
+// 获取 root 树的节点数量
 func getSize(root *TreeNode) int {
 	if root == nil {
 		return 0
 	}
-
 	return 1 + getSize(root.Left) + getSize(root.Right)
 }
