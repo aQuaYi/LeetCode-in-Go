cc7452ccfc1d14b698ea89ec4b002a791bb3636b
aQua <aQua@aQuaYi.com>
2017-10-06 15:58:35 +0800

    203 finish

--- Algorithms/0203.remove-linked-list-elements/remove-linked-list-elements.go
+++ Algorithms/0203.remove-linked-list-elements/remove-linked-list-elements.go
@@ -7,15 +7,17 @@ import (
 type ListNode = kit.ListNode
 
 func removeElements(head *ListNode, val int) *ListNode {
-	headPre := &ListNode{Next: head}
+	headPre := ListNode{Next: head}
 
-	temp := headPre
+	temp := &headPre
 	for temp.Next != nil {
 		if temp.Next.Val == val {
+			// 删除符合条件的节点
 			temp.Next = temp.Next.Next
 		} else {
 			temp = temp.Next
 		}
 	}
+
 	return headPre.Next
 }
