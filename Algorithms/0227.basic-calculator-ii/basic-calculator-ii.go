347e5804d5550aa703d2c465ae3e872d3d1ebc3e
aQua <aQua@aQuaYi.com>
2017-10-06 09:41:45 +0800

    227 added

---
+++ Algorithms/0227.basic-calculator-ii/basic-calculator-ii.go
@@ -0,0 +1,6 @@
+package Problem0227
+
+func calculate(s string) int {
+
+	return 7
+}
---
+++ Algorithms/0227.basic-calculator-ii/basic-calculator-ii_test.go
@@ -0,0 +1,49 @@
+package Problem0227
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	s   string
+	ans int
+}{
+
+	{
+		" 3/2 ",
+		1,
+	},
+
+	{
+		" 3+5 / 2 ",
+		5,
+	},
+
+	{
+		"3+2*2",
+		7,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_calculate(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, calculate(tc.s), "输入:%v", tc)
+	}
+}
+
+func Benchmark_calculate(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			calculate(tc.s)
+		}
+	}
+}
