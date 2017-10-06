28aa78da047ada12a825e974166afc2fc2cf1d37
aQua <aQua@aQuaYi.com>
2017-10-06 15:09:45 +0800

    233 added

--- Algorithms/0233.number-of-digit-one/number-of-digit-one.go
+++ Algorithms/0233.number-of-digit-one/number-of-digit-one.go
@@ -1,6 +1,15 @@
 package Problem0233
 
 func countDigitOne(n int) int {
-
-	return 6
+	ones := 0
+	m := 1
+	for m <= n {
+		a, b := n/m, n%m
+		ones += (a + 8) / 10 * m
+		if a%10 == 1 {
+			ones += b + 1
+		}
+		m *= 10
+	}
+	return ones
 }
