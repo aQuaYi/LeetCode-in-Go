6a3b4edef576324105130674c52092fd36c1d7c6
aQua <aQua@aQuaYi.com>
2017-10-07 21:40:06 +0800

    264 finish

--- Algorithms/0264.ugly-number-ii/ugly-number-ii.go
+++ Algorithms/0264.ugly-number-ii/ugly-number-ii.go
@@ -6,36 +6,37 @@ func nthUglyNumber(n int) int {
 	}
 
 	pos := []int{0, 0, 0}
+	factors := []int{2, 3, 5}
 	candidates := []int{2, 3, 5}
-	pNums := []int{2, 3, 5}
-	result := make([]int, n)
 
-	result[0] = 1
+	res := make([]int, n)
+
+	res[0] = 1
 
 	for i := 1; i < n; i++ {
-		result[i] = minc(candidates)
+		res[i] = min(candidates)
 		for j := 0; j < 3; j++ {
-			if result[i] == candidates[j] {
+			if res[i] == candidates[j] {
 				pos[j]++
-				candidates[j] = result[pos[j]] * pNums[j]
+				candidates[j] = res[pos[j]] * factors[j]
 			}
 		}
 	}
 
-	return result[n-1]
+	return res[n-1]
 }
 
-func minc(candidates []int) int {
-
-	min := candidates[0]
-	for i := 1; i < 3; i++ {
-		if min > candidates[i] {
-			min = candidates[i]
-		}
-	}
-	return min
+func min(candidates []int) int {
+	return lesser(
+		candidates[0],
+		lesser(
+			candidates[1],
+			candidates[2],
+		),
+	)
 }
-func min3(a, b int) int {
+
+func lesser(a, b int) int {
 	if a < b {
 		return a
 	}
