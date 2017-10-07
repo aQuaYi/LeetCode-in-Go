966d1f46cbd065c42bb0bccd2e816bf83b3023e6
aQua <aQua@aQuaYi.com>
2017-10-07 14:27:57 +0800

    239 finish

--- Algorithms/0239.sliding-window-maximum/sliding-window-maximum.go
+++ Algorithms/0239.sliding-window-maximum/sliding-window-maximum.go
@@ -1,9 +1,13 @@
 package Problem0239
 
 func maxSlidingWindow(nums []int, k int) []int {
+	// 题目里面说好了 1 <= k 的
+	// testcase 里面还是出现了 k == 0
 	if k == 0 {
 		return nil
 	}
+
+	// res 的长度是可计算的
 	res := make([]int, len(nums)-k+1)
 	for i := 0; i+k <= len(nums); i++ {
 		res[i] = maxOf(nums[i : i+k])
@@ -12,6 +16,7 @@ func maxSlidingWindow(nums []int, k int) []int {
 	return res
 }
 
+// 获取局部的最大值
 func maxOf(nums []int) int {
 	max := nums[0]
 	for i := 1; i < len(nums); i++ {
