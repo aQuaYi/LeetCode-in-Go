6ce8305b1eebe687e8c56b6eb9b3b1fe87bc7028
aQua <aQua@aQuaYi.com>
2017-10-07 14:34:02 +0800

    240 added

---
+++ Algorithms/0240.search-a-2d-matrix-ii/search-a-2d-matrix-ii.go
@@ -0,0 +1,6 @@
+package Problem0240
+
+func searchMatrix(matrix [][]int, target int) bool {
+
+	return true
+}
---
+++ Algorithms/0240.search-a-2d-matrix-ii/search-a-2d-matrix-ii_test.go
@@ -0,0 +1,55 @@
+package Problem0240
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var m = [][]int{
+	[]int{1, 4, 7, 11, 15},
+	[]int{2, 5, 8, 12, 19},
+	[]int{3, 6, 9, 16, 22},
+	[]int{10, 13, 14, 17, 24},
+	[]int{18, 21, 23, 26, 30},
+}
+
+// tcs is testcase slice
+var tcs = []struct {
+	matrix [][]int
+	target int
+	ans    bool
+}{
+
+	{
+		m,
+		20,
+		false,
+	},
+
+	{
+		m,
+		5,
+		true,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_searchMatrix(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, searchMatrix(tc.matrix, tc.target), "输入:%v", tc)
+	}
+}
+
+func Benchmark_searchMatrix(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			searchMatrix(tc.matrix, tc.target)
+		}
+	}
+}
