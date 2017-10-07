d7d769ac7530c74db0ccc66618114d9f9057e268
aQua <aQua@aQuaYi.com>
2017-10-07 15:58:01 +0800

    240 finish

--- Algorithms/0240.search-a-2d-matrix-ii/search-a-2d-matrix-ii.go
+++ Algorithms/0240.search-a-2d-matrix-ii/search-a-2d-matrix-ii.go
@@ -10,32 +10,22 @@ func searchMatrix(matrix [][]int, target int) bool {
 		return false
 	}
 
-	return search(matrix, target, 0, m-1, 0, n-1)
-}
+	i, j := m-1, 0
+	for 0 <= i && j < n {
+		if matrix[i][j] == target {
+			return true
+		}
 
-func search(m [][]int, target, cl, cr, rl, rr int) bool {
-	if cl > cr || rl > rr {
-		return false
+		if matrix[i][j] < target {
+			j++
+			// 排除了 (i,j) 上方的所有比 matrix[i][j] 小的元素
+		} else {
+			i--
+			// 排除了 (i,j) 右方的所有比 matrix[i][j] 大的元素
+		}
 	}
-
-	if target < m[cl][rl] || m[cr][rr] < target {
-		return false
-	}
-
-	cm := (cl + cr) / 2
-	rm := (rl + rr) / 2
-	if m[cm][rm] == target {
-		return true
-	}
-
-	if search(m, target, cl, cm-1, rm+1, rr) ||
-		search(m, target, cm+1, cr, rl, rm-1) {
-		return true
-	}
-
-	if target < m[cm][rm] {
-		return search(m, target, cl, cm-1, rl, rm) || search(m, target, cm, cm, rl, rm-1)
-	}
-
-	return search(m, target, cm+1, cr, rm, rr) || search(m, target, cm, cm, rm+1, rr)
+	// 这种方法的效率，取决于 m 与 n 的相对大小关系
+	// 当 m == n 时，效率最高
+	// 当 m == 1 或 n ==1 时，效率最低
+	return false
 }
