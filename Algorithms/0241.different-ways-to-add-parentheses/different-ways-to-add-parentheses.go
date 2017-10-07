90b982756680d092db4d450e91c6dd7cccd89c5b
aQua <aQua@aQuaYi.com>
2017-10-07 19:17:19 +0800

    241 finish

--- Algorithms/0241.different-ways-to-add-parentheses/different-ways-to-add-parentheses.go
+++ Algorithms/0241.different-ways-to-add-parentheses/different-ways-to-add-parentheses.go
@@ -6,14 +6,14 @@ func diffWaysToCompute(input string) []int {
 	cache := make(map[string][]int)
 	var dfs func(string) []int
 	dfs = func(s string) []int {
-		res := make([]int, 0, len(s)/2)
+		res := make([]int, 0, len(s))
 		if t, ok := cache[s]; ok {
 			return t
 		}
 
 		for i := range s {
 			if s[i] == '+' || s[i] == '-' || s[i] == '*' {
-
+				// 此时，s[i] 作为最后一个运算的运算符
 				for _, left := range dfs(s[:i]) {
 					for _, right := range dfs(s[i+1:]) {
 						res = append(res, operate(left, right, s[i]))
@@ -22,6 +22,7 @@ func diffWaysToCompute(input string) []int {
 			}
 		}
 
+		// s 中不存在运算符
 		if len(res) == 0 {
 			temp, _ := strconv.Atoi(s)
 			res = append(res, temp)
