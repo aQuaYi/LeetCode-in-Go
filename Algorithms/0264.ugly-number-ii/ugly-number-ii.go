e7d7981a28daaf14b9dc0f35ad3e9e75a3118d6e
aQua <aQua@aQuaYi.com>
2017-10-07 21:10:07 +0800

    264 accepted. 186ms > 6ms

--- Algorithms/0264.ugly-number-ii/ugly-number-ii.go
+++ Algorithms/0264.ugly-number-ii/ugly-number-ii.go
@@ -5,22 +5,45 @@ func nthUglyNumber(n int) int {
 		return n
 	}
 
-	hash := make(map[int]bool, n)
-	for i := 2; i <= 6; i++ {
-		hash[i] = true
+	nums := make([]int, 6, n)
+	for i := 0; i < 6; i++ {
+		nums[i] = i + 1
 	}
 
-	count := 6
-	num := 6
-	for count < n {
-		num++
-		if (num%2 == 0 && hash[num/2]) ||
-			(num%3 == 0 && hash[num/3]) ||
-			(num%5 == 0 && hash[num/5]) {
-			hash[num] = true
-			count++
+	var search func(int, int, int)
+	search = func(left, right, max int) {
+		if left > right {
+			return
+		}
+		min := nums[len(nums)-1]
+		temp := 0
+
+		for left <= right {
+			temp = nums[left] * nums[right]
+
+			if max <= temp {
+				right--
+			} else if temp <= min {
+				left++
+			} else {
+				search(left, right-1, temp)
+				if len(nums) < n {
+					nums = append(nums, temp)
+				}
+				return
+			}
 		}
 	}
 
-	return num
+	k := 3
+	for {
+		search(2, k-1, 2*nums[k])
+		if len(nums) >= n {
+			break
+		}
+		nums = append(nums, 2*nums[k])
+		k++
+	}
+
+	return nums[n-1]
 }
