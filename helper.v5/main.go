41fb6acd9a87cd9bfde8cd798dc73f7c1132d784
aQua <aQua@aQuaYi.com>
2017-10-08 14:59:12 +0800

    helper 开始第 5 版的重构

---
+++ helper.v5/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/BurntSushi/toml"
+)
+
+// 程序辅助设置
+const (
+	VERSION = "5.0.0"
+	USAGE   = `使用方法：
+	0. helper -v 查看helper的版本
+	1. helper readme 会重新生成项目的README.md文件。
+	2. helper n 会生成第n题的答题文件夹。`
+)
+
+var cfg config
+var cfgFile = "leetcode.toml"
+var lcFile = "leetcode.json"
+var unavailableFile = "unavailable.json"
+
+func init() {
+	// 启动时，导入配置
+	if _, err := toml.DecodeFile(cfgFile, &cfg); err != nil {
+		log.Fatalf(err.Error())
+	}
+	log.Printf("Hi, %s. \n", cfg.Login)
+}
+
+func main() {
+	if len(os.Args) == 1 {
+		fmt.Println(USAGE)
+		return
+	}
+
+	switch os.Args[1] {
+	case "-v", "-version":
+		fmt.Printf("helper version %s\n", VERSION)
+	case "-h", "-help":
+		fmt.Println(USAGE)
+	case "readme":
+		rebuildReadme()
+	default:
+		buildProblemDir(os.Args[1])
+	}
+}
---
+++ helper.v5/readme.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func rebuildReadme() {
+	// 由于网络原因，有时候 signin 比较慢
+	signin()
+
+	lc := update()
+
+	makeREADME(lc)
+}
+
+func makeREADME(lc *leetcode) {
+	file := "README.md"
+	os.Remove(file)
+
+	// 更新 README.md 的内容
+	template := `%s
+
+## 答题进度：
+%s
+> 仅含能用 Go 语言解答的 Algorithms 类别的免费题
+## 参考解答
+%s
+以下题目，暂时不能使用 Golang 解答
+%s
+
+%s
+`
+
+	head := getHead(lc)
+
+	progressTable := lc.Algorithms.progressTable()
+
+	acceptedTable := lc.AllProblems.accepted().table()
+
+	unavailList := lc.UnavailProblem.list()
+
+	tail := read("README_TAIL.md")
+
+	content := fmt.Sprintf(template, head, acceptedPercent, count, accepted, tail)
+
+	// 保存 README.md 文件
+	err := ioutil.WriteFile(file, []byte(content), 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getHead(lc *leetcode) string {
+	headFormat := string(read("README_HEAD.md"))
+	return fmt.Sprintf(headFormat, lc.Username, lc.Username, lc.Ranking, lc.Username)
+}
