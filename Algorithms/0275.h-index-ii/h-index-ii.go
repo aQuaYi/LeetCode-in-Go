fdf2608ddade2bc419c918019511c34150d38da7
aQua <aQua@aQuaYi.com>
2017-10-09 11:10:14 +0800

    275 wrong answer

--- Algorithms/0275.h-index-ii/h-index-ii.go
+++ Algorithms/0275.h-index-ii/h-index-ii.go
@@ -4,6 +4,10 @@ package Problem0275
 func hIndex(nums []int) int {
 	size := len(nums)
 
+	if size == 1 {
+		return min(1, nums[0])
+	}
+
 	lo, hi := 0, size-1
 	var mid, midAO int // mid + midAO +1 == size
 	for lo <= hi {
@@ -17,4 +21,11 @@ func hIndex(nums []int) int {
 	}
 
 	return size - lo
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
