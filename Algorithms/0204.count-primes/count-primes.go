8b38ae0f2957b35b799560c383c7a2113f474849
aQua <aQua@aQuaYi.com>
2017-10-09 09:07:20 +0800

    204 Time Limit Exceeded

--- Algorithms/0204.count-primes/count-primes.go
+++ Algorithms/0204.count-primes/count-primes.go
@@ -1,6 +1,27 @@
 package Problem0204
 
 func countPrimes(n int) int {
+	if n <= 2 {
+		return 0
+	}
 
-	return 25
+	primes := make([]int, 1, n/2)
+	primes[0] = 2
+
+	isPrime := func(num int) bool {
+		for _, p := range primes {
+			if num%p == 0 {
+				return false
+			}
+		}
+		return true
+	}
+
+	for i := 3; i < n; i++ {
+		if isPrime(i) {
+			primes = append(primes, i)
+		}
+	}
+
+	return len(primes)
 }
