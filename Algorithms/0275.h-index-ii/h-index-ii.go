2fd080557c2ca4935547693f51d3c7a25047265d
aQua <aQua@aQuaYi.com>
2017-10-09 11:01:19 +0800

    275 wrong answer

--- Algorithms/0275.h-index-ii/h-index-ii.go
+++ Algorithms/0275.h-index-ii/h-index-ii.go
@@ -1,10 +1,20 @@
 package Problem0275
 
-func hIndex(citations []int) int {
-	size := len(citations)
-	h := 0
-	for h < size && citations[size-h-1] > h {
-		h++
+// citations 为升序排列
+func hIndex(nums []int) int {
+	size := len(nums)
+
+	lo, hi := 0, size-1
+	var mid, midAO int // mid + midAO +1 == size
+	for lo <= hi {
+		mid = (lo + hi) / 2
+		midAO = size - mid - 1
+		if nums[midAO] > mid {
+			lo = mid + 1
+		} else {
+			hi = mid - 1
+		}
 	}
-	return h
+
+	return size - lo
 }
