b5b14c057bbce642eb0215d13b761e2e7ead1d8d
aQua <aQua@aQuaYi.com>
2017-10-09 10:35:24 +0800

    274 finish

--- Algorithms/0274.h-index/h-index.go
+++ Algorithms/0274.h-index/h-index.go
@@ -3,11 +3,13 @@ package Problem0274
 import "sort"
 
 func hIndex(citations []int) int {
-
+	// 对 citations 进行降序排列
 	sort.Sort(sort.Reverse(sort.IntSlice(citations)))
 
 	size := len(citations)
 	h := 0
+	// h 的含义是
+	// 前 h 个的 citation 都 > h
 	for h < size && citations[h] > h {
 		h++
 	}
