1a134fcaeaa7df515f3f8be1531d55836e16d23f
aQua <aQua@aQuaYi.com>
2017-10-09 15:29:38 +0800

    279 finish

--- Algorithms/0279.perfect-squares/perfect-squares.go
+++ Algorithms/0279.perfect-squares/perfect-squares.go
@@ -1,23 +1,25 @@
 package Problem0279
 
+import (
+	"math"
+)
+
 func numSquares(n int) int {
-	perfects := make([]int, intSqrt(n))
-	for i := 0; (i+1)*(i+1) <= n; i++ {
-		perfects[i] = (i + 1) * (i + 1)
+	perfects := []int{}
+	for i := 1; i*i <= n; i++ {
+		perfects = append(perfects, i*i)
 	}
 
 	// dp[i] 表示 the least number of perfect square numbers which sum to i
-	var dp = make([]int, n+1)
-	maxInt := 1<<63 - 1
+	dp := make([]int, n+1)
 
-	dp[1] = 1
-	for i := 2; i < len(dp); i++ {
-		dp[i] = maxInt
+	for i := 1; i < len(dp); i++ {
+		dp[i] = math.MaxInt32
 	}
 
 	for _, p := range perfects {
 		for i := p; i < len(dp); i++ {
-			if dp[i]-1 > dp[i-p] {
+			if dp[i] > dp[i-p]+1 {
 				// 因为 i = ( i - p ) + p，p 是 平方数
 				// 所以 dp[i] = dp[i-p] + 1
 				dp[i] = dp[i-p] + 1
@@ -26,18 +28,4 @@ func numSquares(n int) int {
 	}
 
 	return dp[n]
-}
-
-// 返回 x 的平方根的整数部分
-// 这个函数比 int(math.Sqrt(float64(x))) 快的多
-// 详见 benchmark 的结果
-func intSqrt(x int) int {
-	res := x
-
-	// 牛顿法求平方根
-	for res*res > x {
-		res = (res + x/res) / 2
-	}
-
-	return res
 }
--- Algorithms/0279.perfect-squares/perfect-squares_test.go
+++ Algorithms/0279.perfect-squares/perfect-squares_test.go
@@ -82,4 +82,16 @@ func Benchmark_intSqrt(b *testing.B) {
 	}
 }
 
+// 返回 x 的平方根的整数部分
+// 这个函数比 int(math.Sqrt(float64(x))) 快的多
+// 详见 benchmark 的结果
+func intSqrt(x int) int {
+	res := x
 
+	// 牛顿法求平方根
+	for res*res > x {
+		res = (res + x/res) / 2
+	}
+
+	return res
+}
