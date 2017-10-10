5e3a0667e7a03c5b53d1315986fcc7f87864f0f2
aQua <aQua@aQuaYi.com>
2017-10-10 11:22:42 +0800

    282 accepted. 169ms < 182ms

--- Algorithms/0282.expression-add-operators/expression-add-operators.go
+++ Algorithms/0282.expression-add-operators/expression-add-operators.go
@@ -1,9 +1,5 @@
 package Problem0282
 
-import (
-	"fmt"
-)
-
 func addOperators(s string, target int) []string {
 	res := []string{}
 
@@ -45,37 +41,6 @@ func addOperators(s string, target int) []string {
 	return res
 }
 
-func compute(ss []string) int {
-	var n1, n2, n3 int
-	var opt1, opt2 string
-	// n1 opt1 n2 opt2 n3
-	// ↓   ↓   ↓   ↓   ↓
-	// 1   +   2   *   3
-	n1, opt1, n2 = 0, "+", integer(ss[0])
-
-	for i := 1; i < len(ss); i += 2 {
-		fmt.Println(ss)
-		opt2, n3 = ss[i], integer(ss[i+1])
-		if opt2 == "*" {
-			n2 = operate(n2, n3, opt2)
-		} else {
-			n1 = operate(n1, n2, opt1)
-			opt1 = opt2
-			n2 = n3
-		}
-	}
-
-	return operate(n1, n2, opt1)
-}
-
-func connect(ss []string) string {
-	res := ss[0]
-	for i := 1; i < len(ss); i++ {
-		res += ss[i]
-	}
-	return res
-}
-
 // 把 string 转换成 int
 func integer(s string) int {
 	res := int(s[0] - '0')
@@ -83,15 +48,4 @@ func integer(s string) int {
 		res = res*10 + int(s[i]-'0')
 	}
 	return res
-}
-
-func operate(a, b int, opt string) int {
-	switch opt[0] {
-	case '+':
-		return a + b
-	case '-':
-		return a - b
-	default:
-		return a * b
-	}
 }
