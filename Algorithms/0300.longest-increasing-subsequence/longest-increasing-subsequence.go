caa7af74018e9d34f2f808f10aed71dd05b6654d
aQua <aQua@aQuaYi.com>
2017-10-10 15:36:56 +0800

    300 自己写的二分查找还是没有标准库中的查找方法快

--- Algorithms/0300.longest-increasing-subsequence/longest-increasing-subsequence.go
+++ Algorithms/0300.longest-increasing-subsequence/longest-increasing-subsequence.go
@@ -1,24 +1,40 @@
 package Problem0300
 
 func lengthOfLIS(nums []int) int {
-	size := len(nums)
+	tails := make([]int, 0, len(nums))
 
-	max := 0
-	var dfs func(int, int, int)
-	dfs = func(idx, n, count int) {
-		if max < count {
-			max = count
-		}
-
-		for i := idx + 1; i < size; i++ {
-			// i < size-max+count 用于提前结束
-			if nums[i] > n && i < size-max+count {
-				dfs(i, nums[i], count+1)
-			}
+	for _, n := range nums {
+		at := search(tails, n)
+		if at == len(tails) {
+			tails = append(tails, n)
+		} else if tails[at] > n {
+			tails[at] = n
 		}
 	}
 
-	dfs(-1, -1<<63, 0)
+	return len(tails)
+}
 
-	return max
+func search(nums []int, n int) int {
+	if len(nums) == 0 || n < nums[0] {
+		return 0
+	}
+
+	if nums[len(nums)-1] < n {
+		return len(nums)
+	}
+
+	lo, hi := 0, len(nums)-1
+
+	for lo <= hi {
+		mid := (lo + hi) / 2
+		if nums[mid] == n {
+			return mid
+		} else if n < nums[mid] {
+			hi = mid - 1
+		} else {
+			lo = mid + 1
+		}
+	}
+	return lo
 }
