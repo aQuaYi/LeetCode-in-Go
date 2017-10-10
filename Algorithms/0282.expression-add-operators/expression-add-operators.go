cdcdbc1fb349bd30b27eaf5d139dd483d114c809
aQua <aQua@aQuaYi.com>
2017-10-10 11:51:12 +0800

    282 finish

--- Algorithms/0282.expression-add-operators/expression-add-operators.go
+++ Algorithms/0282.expression-add-operators/expression-add-operators.go
@@ -5,11 +5,14 @@ func addOperators(s string, target int) []string {
 
 	var dfs func(string, string, int, int)
 
-	dfs = func(s, resStr string, currRes, prevNum int) {
+	dfs = func(s, resStr string, result, prevAdd int) {
 		var currStr, nextS string
 		var currNum int
+
+		// s 切分完成
 		if len(s) == 0 {
-			if currRes == target {
+			// 检查是否符合 target
+			if result == target {
 				res = append(res, resStr)
 			}
 			return
@@ -17,8 +20,10 @@ func addOperators(s string, target int) []string {
 
 		for i := 1; i <= len(s); i++ {
 			currStr = s[:i]
+			// 排除类似 "01" 的数
 			if currStr[0] == '0' && len(currStr) > 1 {
 				// 不是 continue
+				// 因为出现了一次 "01" 以后，后面的 str 都只会是 "01X" 之类的。
 				return
 			}
 
@@ -27,12 +32,21 @@ func addOperators(s string, target int) []string {
 
 			if len(resStr) == 0 {
 				dfs(nextS, currStr, currNum, currNum)
-				continue
+			} else {
+				/*
+					当 运算符 opt 为 + 或 - 时，把计算式
+					result = result opt currNum
+					统一成
+					result += prevAdd, prevAdd = 0 opt currNum
+					这样做的原因是，当 opt 为 * 时，可以先利用 prevAdd 保证 乘法 运算的优先性
+					result -= prevAdd
+					prevAdd *= currNum
+					result += prevAdd
+				*/
+				dfs(nextS, resStr+"+"+currStr, result+currNum, currNum)
+				dfs(nextS, resStr+"-"+currStr, result-currNum, -currNum)
+				dfs(nextS, resStr+"*"+currStr, result-prevAdd+prevAdd*currNum, prevAdd*currNum)
 			}
-
-			dfs(nextS, resStr+"+"+currStr, currRes+currNum, currNum)
-			dfs(nextS, resStr+"-"+currStr, currRes-currNum, -currNum)
-			dfs(nextS, resStr+"*"+currStr, (currRes-prevNum)+prevNum*currNum, prevNum*currNum)
 		}
 	}
 
