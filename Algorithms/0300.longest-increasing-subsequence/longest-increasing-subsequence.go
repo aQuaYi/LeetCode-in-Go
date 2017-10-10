a417ce72037c18ff98a009d0da06379ba4a53ef4
aQua <aQua@aQuaYi.com>
2017-10-10 15:04:42 +0800

    300 accepted. 19ms > 3ms

--- Algorithms/0300.longest-increasing-subsequence/longest-increasing-subsequence.go
+++ Algorithms/0300.longest-increasing-subsequence/longest-increasing-subsequence.go
@@ -1,6 +1,24 @@
 package Problem0300
 
 func lengthOfLIS(nums []int) int {
+	size := len(nums)
 
-	return 4
+	max := 0
+	var dfs func(int, int, int)
+	dfs = func(idx, n, count int) {
+		if max < count {
+			max = count
+		}
+
+		for i := idx + 1; i < size; i++ {
+			// i < size-max+count 用于提前结束
+			if nums[i] > n && i < size-max+count {
+				dfs(i, nums[i], count+1)
+			}
+		}
+	}
+
+	dfs(-1, -1<<63, 0)
+
+	return max
 }
