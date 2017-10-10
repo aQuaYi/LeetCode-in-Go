73f8845dfd1f8b85e31e537e65152825143eb2e4
aQua <aQua@aQuaYi.com>
2017-10-10 14:09:46 +0800

    299 accepted. 16ms > 3ms

--- Algorithms/0299.bulls-and-cows/bulls-and-cows.go
+++ Algorithms/0299.bulls-and-cows/bulls-and-cows.go
@@ -1,6 +1,39 @@
 package Problem0299
 
-func getHint(secret string, guess string) string {
+import "fmt"
 
-	return "1A3B"
+func getHint(secret string, guess string) string {
+	var A, B int
+	s := []byte(secret)
+	g := []byte(guess)
+
+	// 寻找 bulls
+	for i := range s {
+		if g[i] == s[i] {
+			A++
+			g[i] = 'x'
+			s[i] = 'x'
+		}
+	}
+
+	// 寻找 cows
+	for i := range s {
+		if s[i] == 'x' {
+			continue
+		}
+
+		for j := range g {
+			if g[j] == 'x' {
+				continue
+			}
+
+			if s[i] == g[j] {
+				B++
+				g[j] = 'x'
+				break
+			}
+		}
+	}
+
+	return fmt.Sprintf("%dA%dB", A, B)
 }
