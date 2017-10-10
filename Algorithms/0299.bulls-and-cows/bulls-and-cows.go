28f3e00540a100c63b6a925b8d6427212279934d
aQua <aQua@aQuaYi.com>
2017-10-10 14:02:08 +0800

    299 added

---
+++ Algorithms/0299.bulls-and-cows/bulls-and-cows.go
@@ -0,0 +1,6 @@
+package Problem0299
+
+func getHint(secret string, guess string) string {
+
+	return "1A3B"
+}
---
+++ Algorithms/0299.bulls-and-cows/bulls-and-cows_test.go
@@ -0,0 +1,41 @@
+package Problem0299
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	secret string
+	guess  string
+	ans    string
+}{
+
+	{
+		"1807",
+		"7810",
+		"1A3B",
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_getHint(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, getHint(tc.secret, tc.guess), "输入:%v", tc)
+	}
+}
+
+func Benchmark_getHint(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			getHint(tc.secret, tc.guess)
+		}
+	}
+}
