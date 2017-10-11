ae9203cd943a95e90eef9f4a0a5d56342690b631
aQua <aQua@aQuaYi.com>
2017-10-11 16:48:44 +0800

    231 finish

--- Algorithms/0231.power-of-two/power-of-two.go
+++ Algorithms/0231.power-of-two/power-of-two.go
@@ -1,14 +1,16 @@
 package Problem0231
 
 func isPowerOfTwo(n int) bool {
-	switch {
-	case n < 1:
+	if n < 1 {
 		return false
-	case n == 1:
-		return true
-	case n%2 == 1:
-		return false
-	default:
-		return isPowerOfTwo(n / 2)
 	}
+
+	for n > 1 {
+		if n%2 == 1 {
+			return false
+		}
+		n /= 2
+	}
+
+	return true
 }
