5bb1d7c953b6f9e674feac38be3272edb8399f64
aQua <aQua@aQuaYi.com>
2017-10-11 13:20:12 +0800

    304 added

--- Algorithms/0304.range-sum-query-2d-immutable/range-sum-query-2d-immutable.go
+++ Algorithms/0304.range-sum-query-2d-immutable/range-sum-query-2d-immutable.go
@@ -1,15 +1,18 @@
 package Problem0304
 
+// NumMatrix 包含了需要求和的矩阵
 type NumMatrix struct {
 	int
 }
 
+// Constructor 创建 NumMatrix
 func Constructor(matrix [][]int) NumMatrix {
 
 	return NumMatrix{}
 }
 
-func (this *NumMatrix) SumRegion(row1 int, col1 int, row2 int, col2 int) int {
+// SumRegion 求和
+func (nm *NumMatrix) SumRegion(row1 int, col1 int, row2 int, col2 int) int {
 
 	return 8
 }
