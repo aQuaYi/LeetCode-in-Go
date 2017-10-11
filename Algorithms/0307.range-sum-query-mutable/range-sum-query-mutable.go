c6c3d7e7fb0e26a4291c407c125574a9953bc9b1
aQua <aQua@aQuaYi.com>
2017-10-11 14:55:52 +0800

    307 finish

--- Algorithms/0307.range-sum-query-mutable/range-sum-query-mutable.go
+++ Algorithms/0307.range-sum-query-mutable/range-sum-query-mutable.go
@@ -1,6 +1,35 @@
 package Problem0307
 
-func Constructor(nums []int) NumArray {
-
-	return
+// NumArray 是 nums 的和的切片
+type NumArray struct {
+	nums, dp []int
 }
+
+// Constructor 返回 NumArray
+func Constructor(nums []int) NumArray {
+	dp := make([]int, len(nums)+1)
+	for i := 0; i < len(nums); i++ {
+		dp[i+1] = dp[i] + nums[i]
+	}
+
+	return NumArray{nums: nums, dp: dp}
+}
+
+// Update 更新 nums
+func (na *NumArray) Update(i int, v int) {
+	for idx := i + 1; idx < len(na.dp); idx++ {
+		na.dp[idx] += v - na.nums[i]
+	}
+}
+
+// SumRange 返回 sum(nums[i:j+1])
+func (na *NumArray) SumRange(i int, j int) int {
+	return na.dp[j+1] - na.dp[i]
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * obj := Constructor(nums);
+ * obj.Update(i,val);
+ * param_2 := obj.SumRange(i,j);
+ */
--- Algorithms/0307.range-sum-query-mutable/range-sum-query-mutable_test.go
+++ Algorithms/0307.range-sum-query-mutable/range-sum-query-mutable_test.go
@@ -9,4 +9,13 @@ import (
 func Test_SumRange(t *testing.T) {
 	ast := assert.New(t)
 
+	nums := []int{1, 3, 5}
+
+	na := Constructor(nums)
+
+	ast.Equal(9, na.SumRange(0, 2), "update 前，SumRange(0, 2)")
+
+	na.Update(1, 2)
+
+	ast.Equal(8, na.SumRange(0, 2), "update 后，SumRange(0, 2)")
 }
