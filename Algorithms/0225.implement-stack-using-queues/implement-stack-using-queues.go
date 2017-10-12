2db768f9b12fdb8b10729349a30fa3ae6893ad46
aQua <aQua@aQuaYi.com>
2017-10-12 10:26:27 +0800

    225 finish

--- Algorithms/0225.implement-stack-using-queues/implement-stack-using-queues.go
+++ Algorithms/0225.implement-stack-using-queues/implement-stack-using-queues.go
@@ -1,31 +1,46 @@
 package Problem0225
 
+// MyStack 是用 Queue 实现的 栈
 type MyStack struct {
+	a, b *Queue
 }
 
-/** Initialize your data structure here. */
+// Constructor Initialize your data structure here.
 func Constructor() MyStack {
-
+	return MyStack{a: NewQueue(), b: NewQueue()}
 }
 
-/** Push element x onto stack. */
+// Push Push element x onto stack.
 func (ms *MyStack) Push(x int) {
-
+	if ms.a.Len() == 0 {
+		ms.a, ms.b = ms.b, ms.a
+	}
+	ms.a.Push(x)
 }
 
-/** Removes the element on top of the stack and returns that element. */
+// Pop Removes the element on top of the stack and returns that element.
 func (ms *MyStack) Pop() int {
+	if ms.a.Len() == 0 {
+		ms.a, ms.b = ms.b, ms.a
+	}
 
+	for ms.a.Len() > 1 {
+		ms.b.Push(ms.a.Pop())
+	}
+
+	return ms.a.Pop()
 }
 
-/** Get the top element. */
+// Top Get the top element.
 func (ms *MyStack) Top() int {
-
+	res := ms.Pop()
+	ms.Push(res)
+	return res
 }
 
-/** Returns whether the stack is empty. */
+// Empty Returns whether the stack is empty.
 func (ms *MyStack) Empty() bool {
-
+	return (ms.a.Len() + ms.b.Len()) == 0
 }
 
 /**
@@ -36,3 +51,35 @@ func (ms *MyStack) Empty() bool {
  * param_3 := obj.Top();
  * param_4 := obj.Empty();
  */
+
+// Queue 是用于存放 int 的队列
+type Queue struct {
+	nums []int
+}
+
+// NewQueue 返回 *kit.Queue
+func NewQueue() *Queue {
+	return &Queue{nums: []int{}}
+}
+
+// Push 把 n 放入队列
+func (q *Queue) Push(n int) {
+	q.nums = append(q.nums, n)
+}
+
+// Pop 从 q 中取出最先进入队列的值
+func (q *Queue) Pop() int {
+	res := q.nums[0]
+	q.nums = q.nums[1:]
+	return res
+}
+
+// Len 返回 q 的长度
+func (q *Queue) Len() int {
+	return len(q.nums)
+}
+
+// IsEmpty 反馈 q 是否为空
+func (q *Queue) IsEmpty() bool {
+	return q.Len() == 0
+}
--- Algorithms/0225.implement-stack-using-queues/implement-stack-using-queues_test.go
+++ Algorithms/0225.implement-stack-using-queues/implement-stack-using-queues_test.go
@@ -1,39 +1,47 @@
 package Problem0225
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-// tcs is testcase slice
-var tcs = []struct {
-	
-	ans  MyStack 
-}{
-
-	{
-		,
-		,
-	},
-
-	// 可以有多个 testcase
-}
-
-func Test_Constructor(t *testing.T) {
+func Test_MyStack(t *testing.T) {
 	ast := assert.New(t)
-	
-	for _, tc := range tcs {
-		fmt.Printf("~~%v~~\n", tc)
-		ast.Equal(tc.ans, Constructor(tc.), "输入:%v", tc)
+
+	s := Constructor()
+	ast.True(s.Empty(), "检查新建的 s 是否为空")
+
+	start, end := 0, 10
+
+	for i := start; i < end; i++ {
+		s.Push(i)
+		ast.Equal(i, s.Top(), "查看 s.Top()")
 	}
+
+	for i := end - 1; i >= start; i-- {
+		ast.Equal(i, s.Pop(), "从 s 中 pop 出数来。")
+	}
+
+	ast.True(s.Empty(), "检查 Pop 完毕后的 s 是否为空")
 }
 
-func Benchmark_Constructor(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		for _, tc := range tcs {
-			Constructor(tc.)
-		}
+func Test_Queue(t *testing.T) {
+	ast := assert.New(t)
+
+	q := NewQueue()
+	ast.True(q.IsEmpty(), "检查新建的 q 是否为空")
+
+	start, end := 0, 100
+
+	for i := start; i < end; i++ {
+		q.Push(i)
+		ast.Equal(i-start+1, q.Len(), "Push 后检查 q 的长度。")
 	}
+
+	for i := start; i < end; i++ {
+		ast.Equal(i, q.Pop(), "从 q 中 pop 出数来。")
+	}
+
+	ast.True(q.IsEmpty(), "检查 Pop 完毕后的 q 是否为空")
 }
