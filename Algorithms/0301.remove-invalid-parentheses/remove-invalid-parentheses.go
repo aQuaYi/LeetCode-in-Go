a07c2f736d63320383bf76b4b6b47f2c738efc28
aQua <aQua@aQuaYi.com>
2017-10-13 11:57:00 +0800

    301 重构中

--- Algorithms/0301.remove-invalid-parentheses/remove-invalid-parentheses.go
+++ Algorithms/0301.remove-invalid-parentheses/remove-invalid-parentheses.go
@@ -5,10 +5,10 @@ var pairRL = []byte{')', '('}
 
 func removeInvalidParentheses(s string) []string {
 	ans := []string{}
-	return remove(s, 0, 0, ans, pairLR)
+	return dfs(s, 0, 0, ans, pairLR)
 }
 
-func remove(s string, lastI, lastJ int, ans []string, pair []byte) []string {
+func dfs(s string, lastI, lastJ int, ans []string, pair []byte) []string {
 	stack := 0
 	for i := lastI; i < len(s); i++ {
 		if s[i] == pair[0] {
@@ -22,18 +22,18 @@ func remove(s string, lastI, lastJ int, ans []string, pair []byte) []string {
 		for j := lastJ; j <= i; j++ {
 			if s[j] == pair[1] && (j == lastJ || s[j-1] != pair[1]) {
 				s1 := s[:j] + s[j+1:]
-				ans = remove(s1, i, j, ans, pair)
+				ans = dfs(s1, i, j, ans, pair)
 			}
 		}
 		return ans
 	}
 	reversed := reverse(s)
+
 	if pair[0] == '(' {
-		ans = remove(reversed, 0, 0, ans, pairRL)
-	} else {
-		ans = append(ans, reverse(s))
+		return dfs(reversed, 0, 0, ans, pairRL)
 	}
-	return ans
+
+	return append(ans, reverse(s))
 }
 
 func reverse(s string) string {
