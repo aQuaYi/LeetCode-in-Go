32a39dbdd8deaa845fdc3e0941a1efbe2eaa13fa
aQua <aQua@aQuaYi.com>
2017-10-13 10:43:07 +0800

    301 adding

--- Algorithms/0301.remove-invalid-parentheses/remove-invalid-parentheses.go
+++ Algorithms/0301.remove-invalid-parentheses/remove-invalid-parentheses.go
@@ -1,6 +1,31 @@
 package Problem0301
 
 func removeInvalidParentheses(s string) []string {
+	res := []string{}
+	var dfs func()
+	dfs = func() {}
 
-	return nil
+	return res
+}
+
+func count(s string) (left, right int) {
+	for i := range s {
+		switch s[i] {
+		case '(':
+			left++
+		case ')':
+			if left > 0 {
+				left--
+			} else {
+				right++
+			}
+		}
+	}
+
+	return
+}
+
+func isOK(s string) bool {
+	l, r := count(s)
+	return l+r == 0
 }
