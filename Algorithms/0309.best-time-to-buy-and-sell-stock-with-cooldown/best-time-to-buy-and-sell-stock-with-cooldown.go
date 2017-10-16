92a85a99adf34e1c54681a4d652b1baf2303e6a4
aQua <aQua@aQuaYi.com>
2017-10-16 14:19:59 +0800

    309 wrong answer

--- Algorithms/0309.best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.go
+++ Algorithms/0309.best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.go
@@ -1,6 +1,23 @@
 package Problem0309
 
 func maxProfit(prices []int) int {
+	n := len(prices)
+	// buy[i] 表示，按照 prices[i-1] 的价格 买 后的利润
+	buy := make([]int, n+1)
+	// sell[i] 表示，按照 prices[i-1] 的价格 卖 后的利润
+	sell := make([]int, n+1)
+	buy[1] = -1 // 无法按照 prices[0] 的价格
+	for i := 2; i <= n; i++ {
+		buy[i] = max(sell[i-2]-prices[i-1], buy[i-1])
+		sell[i] = max(sell[i-1], buy[i-1]+prices[i-1])
+	}
 
-	return 3
+	return sell[n]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
