ac0dadaeff96e6ae001cee18eaa8d991b8981d63
aQua <aQua@aQuaYi.com>
2017-10-16 15:38:29 +0800

    234 finish

--- Algorithms/0234.palindrome-linked-list/palindrome-linked-list.go
+++ Algorithms/0234.palindrome-linked-list/palindrome-linked-list.go
@@ -7,12 +7,14 @@ import (
 type ListNode = kit.ListNode
 
 func isPalindrome(head *ListNode) bool {
+	// 获取 list 中的值
 	nums := make([]int, 0, 64)
 	for head != nil {
 		nums = append(nums, head.Val)
 		head = head.Next
 	}
 
+	// 按照规则对比值
 	l, r := 0, len(nums)-1
 	for l < r {
 		if nums[l] != nums[r] {
