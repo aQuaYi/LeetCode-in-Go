ced1cc0a1171dcb4c9acc781bec1616bfc586284
aQua <aQua@aQuaYi.com>
2017-10-16 17:20:01 +0800

    322 finish

--- Algorithms/0322.coin-change/coin-change.go
+++ Algorithms/0322.coin-change/coin-change.go
@@ -1,22 +1,17 @@
 package Problem0322
 
-import "sort"
-
 func coinChange(coins []int, amount int) int {
+	// dp[i] 更换金额为 i 时，最小的零钱数量
+	// res = dp[amount]
 	dp := make([]int, amount+1)
-	for i := range dp {
-		dp[i] = amount + 1
-	}
 	dp[0] = 0
 
-	sort.Ints(coins)
-
 	for i := 1; i <= amount; i++ {
+		dp[i] = amount + 1
 		for _, c := range coins {
-			if i-c < 0 {
-				break
+			if c <= i && dp[i] > dp[i-c]+1 {
+				dp[i] = dp[i-c] + 1
 			}
-			dp[i] = min(dp[i], dp[i-c]+1)
 		}
 	}
 
@@ -27,9 +22,35 @@ func coinChange(coins []int, amount int) int {
 	return dp[amount]
 }
 
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
+// func coinChange(coins []int, amount int) int {
+// 	max := amount + 1
+// 	dp := make([]int, amount+1)
+// 	for i := range dp {
+// 		dp[i] = max
+// 	}
+// 	dp[0] = 0
+
+// 	sort.Ints(coins)
+
+// 	for i := 1; i <= amount; i++ {
+// 		for _, c := range coins {
+// 			if i-c < 0 {
+// 				break
+// 			}
+// 			dp[i] = min(dp[i], dp[i-c]+1)
+// 		}
+// 	}
+
+// 	if dp[amount] > amount {
+// 		return -1
+// 	}
+
+// 	return dp[amount]
+// }
+
+// func min(a, b int) int {
+// 	if a < b {
+// 		return a
+// 	}
+// 	return b
+// }
