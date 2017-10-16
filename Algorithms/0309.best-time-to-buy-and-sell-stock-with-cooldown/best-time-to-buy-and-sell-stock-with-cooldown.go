fff9be75cfbcae1d03a019a939f5749565045fa8
aQua <aQua@aQuaYi.com>
2017-10-16 13:08:54 +0800

    309 added

---
+++ Algorithms/0309.best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.go
@@ -0,0 +1,6 @@
+package Problem0309
+
+func maxProfit(prices []int) int {
+
+	return 3
+}
---
+++ Algorithms/0309.best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown_test.go
@@ -0,0 +1,39 @@
+package Problem0309
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	prices []int
+	ans    int
+}{
+
+	{
+		[]int{1, 2, 3, 0, 2},
+		3,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_maxProfit(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, maxProfit(tc.prices), "输入:%v", tc)
+	}
+}
+
+func Benchmark_maxProfit(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			maxProfit(tc.prices)
+		}
+	}
+}
