d22d81f04db3080bb2456f53d3617ab042d251db
aQua <aQua@aQuaYi.com>
2017-10-16 23:05:31 +0800

    310 现在的程序不能同时删除所有的叶子节点，导致出错。

--- Algorithms/0310.minimum-height-trees/minimum-height-trees.go
+++ Algorithms/0310.minimum-height-trees/minimum-height-trees.go
@@ -12,22 +12,31 @@ func findMinHeightTrees(n int, edges [][]int) []int {
 
 	var ok bool
 	var nodes []int
+	var i, nd int
 
 	for len(eMap) > 2 {
-		for i := 0; i < n; i++ {
+
+		for i = 0; i < n; i++ {
 			if nodes, ok = eMap[i]; !ok {
 				continue
 			}
+
+			if len(nodes) == 1 {
+				continue
+			}
+
 			newNodes := make([]int, 0, len(nodes))
-			for _, nd := range nodes {
+			for _, nd = range nodes {
 				if len(eMap[nd]) == 1 {
 					delete(eMap, nd)
 				} else {
 					newNodes = append(newNodes, nd)
 				}
 			}
+
 			eMap[i] = newNodes
 		}
+
 	}
 
 	var res = make([]int, 0, len(eMap))
