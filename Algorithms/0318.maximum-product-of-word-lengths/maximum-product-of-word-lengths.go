fbad25895c8f7d344b02e6dd648d1ac86bc64a8a
aQua <aQua@aQuaYi.com>
2017-10-17 13:49:43 +0800

    318 accepted. 2842ms > 29ms

--- Algorithms/0318.maximum-product-of-word-lengths/maximum-product-of-word-lengths.go
+++ Algorithms/0318.maximum-product-of-word-lengths/maximum-product-of-word-lengths.go
@@ -1,6 +1,26 @@
 package Problem0318
 
 func maxProduct(words []string) int {
+	max := 0
+	for i := 0; i < len(words)-1; i++ {
+		for j := i + 1; j < len(words); j++ {
+			if !isShareLetters(words[i], words[j]) {
+				if max < len(words[i])*len(words[j]) {
+					max = len(words[i]) * len(words[j])
+				}
+			}
+		}
+	}
+	return max
+}
 
-	return 16
+func isShareLetters(a, b string) bool {
+	for i := range a {
+		for j := range b {
+			if a[i] == b[j] {
+				return true
+			}
+		}
+	}
+	return false
 }
