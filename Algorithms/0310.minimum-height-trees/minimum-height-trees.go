5213dd4000e7305274376f72fea45bac871850c1
aQua <aQua@aQuaYi.com>
2017-10-17 10:04:56 +0800

    310 finish

--- Algorithms/0310.minimum-height-trees/minimum-height-trees.go
+++ Algorithms/0310.minimum-height-trees/minimum-height-trees.go
@@ -5,34 +5,40 @@ func findMinHeightTrees(n int, edges [][]int) []int {
 		return []int{0}
 	}
 
-	eMap := make([][]int, n)
+	// 所有与 i 节点相连接的节点名称，保存在 linkNodes[i]
+	linkNodes := make([][]int, n)
 	for i := 0; i < n; i++ {
-		eMap[i] = make([]int, 0, 5)
+		linkNodes[i] = make([]int, 0, 5)
 	}
-	degrees := make([]int, n)
+
+	// count[i] 保存了与 i 节点链接的点的个数
+	count := make([]int, n)
 	for _, e := range edges {
-		eMap[e[0]] = append(eMap[e[0]], e[1])
-		eMap[e[1]] = append(eMap[e[1]], e[0])
-		degrees[e[0]]++
-		degrees[e[1]]++
+		linkNodes[e[0]] = append(linkNodes[e[0]], e[1])
+		linkNodes[e[1]] = append(linkNodes[e[1]], e[0])
+		count[e[0]]++
+		count[e[1]]++
 	}
 
+	// 收集所有的 叶子 节点
 	var leafs = make([]int, 0, n)
-	var nd, leaf int
-
 	for i := 0; i < n; i++ {
-		if degrees[i] == 1 {
+		if count[i] == 1 {
 			leafs = append(leafs, i)
 		}
 	}
 
+	var nd, leaf int
+
+	// 反复裁剪掉 叶子 节点。
+	// 当剩余节点数 <=2 时，就是答案
 	for n > 2 {
 		newLeafs := make([]int, 0, len(leafs))
 		for _, leaf = range leafs {
 			n--
-			for _, nd = range eMap[leaf] {
-				degrees[nd]--
-				if degrees[nd] == 1 {
+			for _, nd = range linkNodes[leaf] {
+				count[nd]--
+				if count[nd] == 1 {
 					newLeafs = append(newLeafs, nd)
 				}
 			}
