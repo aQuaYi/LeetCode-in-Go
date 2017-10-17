7def46bc3590725b2e3ec19a17e9d0a8f83868f5
aQua <aQua@aQuaYi.com>
2017-10-17 15:12:22 +0800

    319 无法完成这个方法，改用其他的方法。

--- Algorithms/0319.bulb-switcher/bulb-switcher.go
+++ Algorithms/0319.bulb-switcher/bulb-switcher.go
@@ -1,17 +1,48 @@
 package Problem0319
 
 func bulbSwitch(n int) int {
-	bulbs := make([]bool, n)
-
-	for i := 2; i <= n; i++ {
-		for k := i - 1; k < n; k += i {
-			bulbs[k] = !bulbs[k]
-		}
+	res := 1
+	if n <= 3 {
+		return res
 	}
 
-	var res int
-	for i := range bulbs {
-		if !bulbs[i] {
+	primes := make([]int, 2)
+	primes[0], primes[1] = 2, 3
+
+	var count func(int) int
+	count = func(x int) int {
+		isSquare := true
+		base2 := 1
+		for _, p := range primes {
+			c := 0
+			if p > x {
+				break
+			}
+			for x%p == 0 {
+				x /= p
+				base2 *= 2
+				c++
+			}
+			if isSquare && c%2 != 0 {
+				isSquare = false
+			}
+		}
+		if base2 == 1 {
+			primes = append(primes, x)
+			return 2
+		}
+		squareTail := 0
+		if isSquare {
+			squareTail = 1
+		}
+
+		res2 := base2/2 + 2 - squareTail
+		return res2
+	}
+
+	var i int
+	for i = 4; i <= n; i++ {
+		if count(i)%2 == 1 {
 			res++
 		}
 	}
