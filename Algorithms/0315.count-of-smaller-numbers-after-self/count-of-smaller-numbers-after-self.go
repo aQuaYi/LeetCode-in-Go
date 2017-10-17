36684c97a1190019980403865ca47ee180ed8076
aQua <aQua@aQuaYi.com>
2017-10-17 10:40:00 +0800

    315 wrong answer

--- Algorithms/0315.count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.go
+++ Algorithms/0315.count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.go
@@ -1,13 +1,17 @@
 package Problem0315
 
+import "sort"
+
 func countSmaller(nums []int) []int {
-	res := make([]int, len(nums))
-	for i := 0; i < len(nums); i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[j] < nums[i] {
-				res[i]++
-			}
-		}
+	size := len(nums)
+	res := make([]int, size)
+	sorted := make([]int, size)
+	copy(sorted, nums)
+	sort.Ints(sorted)
+
+	for i := 0; i < size; i++ {
+		j := sort.SearchInts(sorted, nums[i])
+		res[i] = size - i - j - 1
 	}
 	return res
 }
