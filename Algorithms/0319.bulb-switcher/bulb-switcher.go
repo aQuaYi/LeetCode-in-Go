1a7c909383673f5e3b551b976f1cd514ec7616e5
aQua <aQua@aQuaYi.com>
2017-10-17 14:19:12 +0800

    319 Time Limit Exceeded.

--- Algorithms/0319.bulb-switcher/bulb-switcher.go
+++ Algorithms/0319.bulb-switcher/bulb-switcher.go
@@ -1,6 +1,20 @@
 package Problem0319
 
 func bulbSwitch(n int) int {
+	bulbs := make([]bool, n)
 
-	return 1
+	for i := 2; i <= n; i++ {
+		for k := i - 1; k < n; k += i {
+			bulbs[k] = !bulbs[k]
+		}
+	}
+
+	var res int
+	for i := range bulbs {
+		if !bulbs[i] {
+			res++
+		}
+	}
+
+	return res
 }
