08e1c302fa00ecad45e674e2962e828f732ba4a8
aQua <aQua@aQuaYi.com>
2017-10-18 15:26:41 +0800

    324 准备用三路划分来做，没有 hold 住

--- Algorithms/0324.wiggle-sort-ii/wiggle-sort-ii.go
+++ Algorithms/0324.wiggle-sort-ii/wiggle-sort-ii.go
@@ -1,30 +1,81 @@
 package Problem0324
 
-import "sort"
+import "container/heap"
 
 func wiggleSort(nums []int) {
 	n := len(nums)
-	temp := make([]int, n)
-	copy(temp, nums)
-	sort.Sort(sort.Reverse(sort.IntSlice(temp)))
 
-	var i, j int
-	// 把 大数 按照降序，排列在奇数位
-	i = 0
-	j = 1
-	for j < n {
-		nums[j] = temp[i]
-		i++
-		j += 2
+	// 寻找 中位数 mid
+	left := new(maxHeap)
+	left.intHeap = nums[:n/2]
+	heap.Init(left)
+
+	right := new(minHeap)
+	right.intHeap = nums[n/2:]
+	heap.Init(right)
+
+	for left.intHeap[0] > right.intHeap[0] {
+		left.intHeap[0], right.intHeap[0] = right.intHeap[0], left.intHeap[0]
+		heap.Fix(left, 0)
+		heap.Fix(right, 0)
 	}
-	// 把 小数 按照降序，排列在偶数位
-	i, j = n-1, n-1
-	if j%2 != 0 {
-		j--
+
+	mid := left.intHeap[0]
+
+	// 3 路划分
+	i, j, k := 0, 0, n-1
+	for j <= k {
+		if nums[j] > mid {
+			nums[i], nums[j] = nums[j], nums[i]
+			i++
+			j++
+		} else if nums[j] < mid {
+			nums[j], nums[k] = nums[k], nums[j]
+			k--
+		} else {
+			j++
+		}
 	}
-	for 0 <= j {
-		nums[j] = temp[i]
-		j -= 2
-		i--
-	}
+}
+
+// maxHeap.intHeap[0] == max(maxHeap.intHeap)
+type maxHeap struct {
+	intHeap
+}
+
+func (h maxHeap) Less(i, j int) bool {
+	return h.intHeap[i] > h.intHeap[j]
+}
+
+// minHeap.intHeap[0] == min(minHeap.intHeap)
+type minHeap struct {
+	intHeap
+}
+
+// intHeap 实现了 heap 的接口
+type intHeap []int
+
+func (h intHeap) Len() int {
+	return len(h)
+}
+
+func (h intHeap) Less(i, j int) bool {
+	return h[i] < h[j]
+}
+
+func (h intHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+func (h *intHeap) Push(x interface{}) {
+	// Push 使用 *h，是因为
+	// Push 增加了 h 的长度
+	*h = append(*h, x.(int))
+}
+
+func (h *intHeap) Pop() interface{} {
+	// Pop 使用 *h ，是因为
+	// Pop 减短了 h 的长度
+	res := (*h)[len(*h)-1]
+	*h = (*h)[0 : len(*h)-1]
+	return res
 }
