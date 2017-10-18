422e4a8a48a4b4e577024171a0f20b2c952ba022
aQua <aQua@aQuaYi.com>
2017-10-18 14:09:37 +0800

    324 wrong answer

--- Algorithms/0324.wiggle-sort-ii/wiggle-sort-ii.go
+++ Algorithms/0324.wiggle-sort-ii/wiggle-sort-ii.go
@@ -1,6 +1,19 @@
 package Problem0324
 
+import "sort"
+
 func wiggleSort(nums []int) {
+	sort.Ints(nums)
+
+	offset := (len(nums) + 1) / 2
+	if offset%2 != 1 {
+		offset++
+	}
+	i := 1
+	for i < len(nums)/2 {
+		nums[i], nums[i+offset] = nums[i+offset], nums[i]
+		i += 2
+	}
 
 	return
 }
