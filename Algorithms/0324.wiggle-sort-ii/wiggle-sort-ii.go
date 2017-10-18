64c2f5f16e77c96eaa6bd0c86f24f1bc8bad6782
aQua <aQua@aQuaYi.com>
2017-10-18 14:36:59 +0800

    324 accepted

--- Algorithms/0324.wiggle-sort-ii/wiggle-sort-ii.go
+++ Algorithms/0324.wiggle-sort-ii/wiggle-sort-ii.go
@@ -3,17 +3,28 @@ package Problem0324
 import "sort"
 
 func wiggleSort(nums []int) {
-	sort.Ints(nums)
+	n := len(nums)
+	temp := make([]int, n)
+	copy(temp, nums)
+	sort.Sort(sort.Reverse(sort.IntSlice(temp)))
 
-	offset := (len(nums) + 1) / 2
-	if offset%2 != 1 {
-		offset++
-	}
-	i := 1
-	for i < len(nums)/2 {
-		nums[i], nums[i+offset] = nums[i+offset], nums[i]
-		i += 2
+	var i, j int
+
+	i = 0
+	j = 1
+	for j < n {
+		nums[j] = temp[i]
+		i++
+		j += 2
 	}
 
-	return
+	i, j = n-1, n-1
+	if j%2 != 0 {
+		j--
+	}
+	for 0 <= j {
+		nums[j] = temp[i]
+		j -= 2
+		i--
+	}
 }
