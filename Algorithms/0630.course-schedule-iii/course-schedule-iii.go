1c30ec633311b5ad4b1980b0f46fd79123bc7d17
aQua <aQua@aQuaYi.com>
2017-10-18 11:38:56 +0800

    630 accepted. 279ms < 288ms

--- Algorithms/0630.course-schedule-iii/course-schedule-iii.go
+++ Algorithms/0630.course-schedule-iii/course-schedule-iii.go
@@ -1,21 +1,56 @@
 package Problem0630
 
 import (
+	"container/heap"
 	"sort"
 )
 
 func scheduleCourse(courses [][]int) int {
+	taken := new(maxPQ)
+	heap.Init(taken)
+
 	css := cs(courses)
 	sort.Sort(css)
 
-	var count, now int
+	var date int
 	for _, c := range css {
-		if now+c[0] <= c[1] {
-			now += c[0]
-			count++
+		heap.Push(taken, c[0])
+		date += c[0]
+		for date > c[1] {
+			date -= heap.Pop(taken).(int)
 		}
 	}
-	return count
+
+	return taken.Len()
+}
+
+type maxPQ []int
+
+// Len is
+func (q maxPQ) Len() int {
+	return len(q)
+}
+
+func (q maxPQ) Less(i, j int) bool {
+	return q[i] > q[j]
+}
+
+func (q maxPQ) Swap(i, j int) {
+	q[i], q[j] = q[j], q[i]
+}
+
+func (q *maxPQ) Push(x interface{}) {
+	// Push 使用 *q，是因为
+	// Push 增加了 q 的长度
+	*q = append(*q, x.(int))
+}
+
+func (q *maxPQ) Pop() interface{} {
+	// Pop 使用 *q ，是因为
+	// Pop 减短了 q 的长度
+	res := (*q)[len(*q)-1]
+	*q = (*q)[:len(*q)-1]
+	return res
 }
 
 type cs [][]int
@@ -25,6 +60,9 @@ func (c cs) Len() int {
 }
 
 func (c cs) Less(i, j int) bool {
+	if c[i][1] == c[j][1] {
+		return c[i][0] < c[j][0]
+	}
 	return c[i][1] < c[j][1]
 }
 
