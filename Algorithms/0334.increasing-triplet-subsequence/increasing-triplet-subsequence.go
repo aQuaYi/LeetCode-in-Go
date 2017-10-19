d85ae5093cb04f577a1e4187cc84abc0c8fa17b8
aQua <aQua@aQuaYi.com>
2017-10-19 15:58:18 +0800

    334 wrong answer

--- Algorithms/0334.increasing-triplet-subsequence/increasing-triplet-subsequence.go
+++ Algorithms/0334.increasing-triplet-subsequence/increasing-triplet-subsequence.go
@@ -1,18 +1,26 @@
 package Problem0334
 
 func increasingTriplet(nums []int) bool {
-	i, j, k := -1<<31, -1<<31, -1<<31
-	for _, n := range nums {
-		if i < n {
-			k, j, i = j, i, n
-		} else if j < n {
-			i = n
-		} else if k < n {
-			k, j, i = -1<<31, -1<<31, n
+	var i, n int
+	rec := [][]int{[]int{nums[0]}}
+	for _, n = range nums {
+		for i = range rec {
+			if n > rec[i][0] {
+				if n < rec[i][len(rec[i])-1] {
+					rec[i][len(rec[i])-1] = n
+				} else if n > rec[i][len(rec[i])-1] {
+					if len(rec[i]) == 2 {
+						return true
+					}
+					rec[i] = append(rec[i], n)
+				}
+			}
 		}
 
-		if k > -1<<31 {
-			return true
+		if n < rec[len(rec)-1][0] {
+			temp := make([]int, 1, 2)
+			temp[0] = n
+			rec = append(rec, temp)
 		}
 	}
 
