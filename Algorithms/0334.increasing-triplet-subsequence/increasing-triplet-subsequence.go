aeec679e84e2a818fb76c79018f79612383e2126
aQua <aQua@aQuaYi.com>
2017-10-19 15:14:57 +0800

    334 wrong answer

--- Algorithms/0334.increasing-triplet-subsequence/increasing-triplet-subsequence.go
+++ Algorithms/0334.increasing-triplet-subsequence/increasing-triplet-subsequence.go
@@ -1,7 +1,11 @@
 package Problem0334
 
 func increasingTriplet(nums []int) bool {
-
-	return true
-
+	i, j, k := -1<<31, -1<<31, -1<<31
+	for _, n := range nums {
+		if i < n {
+			k, j, i = j, i, n
+		}
+	}
+	return k > -1<<31
 }
--- Algorithms/0334.increasing-triplet-subsequence/increasing-triplet-subsequence_test.go
+++ Algorithms/0334.increasing-triplet-subsequence/increasing-triplet-subsequence_test.go
@@ -10,20 +10,19 @@ import (
 // tcs is testcase slice
 var tcs = []struct {
 	nums []int
-	ans  bool 
+	ans  bool
 }{
 
-	{
-	[]int{1,4,3,2,5}	,
-	true	,
-	},
+	{[]int{1, 4, 3, 2, 5}, true},
+	{[]int{2, 5, 3, 4, 5}, true},
+	{[]int{5, 4, 3, 2, 1}, false},
 
 	// 可以有多个 testcase
 }
 
 func Test_increasingTriplet(t *testing.T) {
 	ast := assert.New(t)
-	
+
 	for _, tc := range tcs {
 		fmt.Printf("~~%v~~\n", tc)
 		ast.Equal(tc.ans, increasingTriplet(tc.nums), "输入:%v", tc)
