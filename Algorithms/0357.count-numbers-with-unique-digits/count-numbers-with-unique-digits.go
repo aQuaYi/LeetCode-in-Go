1a5e372d7789b2ed44b3691d8bbbbce2abca0e3c
aQua <aQua@aQuaYi.com>
2017-10-20 16:01:25 +0800

    357 finish

--- Algorithms/0357.count-numbers-with-unique-digits/count-numbers-with-unique-digits.go
+++ Algorithms/0357.count-numbers-with-unique-digits/count-numbers-with-unique-digits.go
@@ -1,6 +1,12 @@
 package Problem0357
 
 func countNumbersWithUniqueDigits(n int) int {
+	res := []int{1, 10, 91, 739, 5275, 32491, 168571, 712891, 2345851, 5611771, 8877691}
+	if n >= 10 {
+		return res[10]
+	}
 
-	return 91
+	return res[n]
 }
+
+// 参考 《代码大全2》 第 18 章 表驱动法
--- Algorithms/0357.count-numbers-with-unique-digits/count-numbers-with-unique-digits_test.go
+++ Algorithms/0357.count-numbers-with-unique-digits/count-numbers-with-unique-digits_test.go
@@ -9,17 +9,29 @@ import (
 
 // tcs is testcase slice
 var tcs = []struct {
-	n int
-	ans  int 
+	n   int
+	ans int
 }{
 
-{2,91},
+	{0, 1},
+	{1, 10},
+	{2, 91},
+	{3, 739},
+	{4, 5275},
+	{5, 32491},
+	{6, 168571},
+	{7, 712891},
+	{8, 2345851},
+	{9, 5611771},
+	{10, 8877691},
+	{11, 8877691},
+
 	// 可以有多个 testcase
 }
 
 func Test_countNumbersWithUniqueDigits(t *testing.T) {
 	ast := assert.New(t)
-	
+
 	for _, tc := range tcs {
 		fmt.Printf("~~%v~~\n", tc)
 		ast.Equal(tc.ans, countNumbersWithUniqueDigits(tc.n), "输入:%v", tc)
