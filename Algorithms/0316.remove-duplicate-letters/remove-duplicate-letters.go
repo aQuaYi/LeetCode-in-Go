ceed08a693f107b4ef9ff3c8760d4c561b96567a
aQua <aQua@aQuaYi.com>
2017-10-20 13:33:20 +0800

    316 accepted. 149ms > 3ms

--- Algorithms/0316.remove-duplicate-letters/remove-duplicate-letters.go
+++ Algorithms/0316.remove-duplicate-letters/remove-duplicate-letters.go
@@ -1,74 +1,32 @@
 package Problem0316
 
 func removeDuplicateLetters(s string) string {
-	count := make([]int, 26)
-	bytes := []byte(s)
-	for _, b := range bytes {
-		count[b-'a']++
+	cnt := make([]int, 26)
+	pos := 0
+	lens := len(s)
+	var i int
+	for i = 0; i < lens; i++ {
+		cnt[s[i]-'a']++
+	}
+	for i = 0; i < lens; i++ {
+		if s[i] < s[pos] {
+			pos = i
+		}
+		cnt[s[i]-'a']--
+		if cnt[s[i]-'a'] == 0 {
+			break
+		}
+	}
+	if lens == 0 {
+		return ""
 	}
 
-	var i, j, k int
-	i, j = 0, 1
-
-	for j < len(s) {
-		if s[i] > s[j] {
-			if count[s[i]-'a'] > 1 {
-				bytes[i] = 0
-				count[s[i]-'a']--
-			}
-			i++
-			for i < len(s) && bytes[i] == 0 {
-				i++
-			}
-
-			j++
-			for j < len(s) && bytes[j] == 0 {
-				j++
-			}
-		} else if s[i] < s[j] {
-			if count[s[i]-'a'] > 1 {
-				for k = j + 1; k < len(s); k++ {
-					if bytes[k] == s[i] {
-						bytes[k] = 0
-						count[s[i]-'a']--
-					}
-				}
-			}
-
-			if count[s[j]-'a'] > 1 {
-				for k = i - 1; 0 <= k; k-- {
-					if bytes[k] == s[j] {
-						bytes[k] = 0
-						count[s[j]-'a']--
-					}
-				}
-			}
-			i++
-			for i < len(s) && bytes[i] == 0 {
-				i++
-			}
-
-			j++
-			for j < len(s) && bytes[j] == 0 {
-				j++
-			}
-
-		} else {
-			bytes[j] = 0
-			count[s[j]-'a']--
-			j++
-			for j < len(s) && bytes[j] == 0 {
-				j++
-			}
+	newS := ""
+	for i = pos + 1; i < lens; i++ {
+		if s[i] != s[pos] {
+			newS += string(s[i])
 		}
 	}
 
-	res := ""
-	for _, b := range bytes {
-		if b != 0 {
-			res += string(b)
-		}
-	}
-
-	return res
+	return string(s[pos]) + removeDuplicateLetters(newS)
 }
