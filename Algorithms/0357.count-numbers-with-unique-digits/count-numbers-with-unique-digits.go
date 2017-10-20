024ff153aa9bbf8fa55b86b4191d7833ab0d7372
aQua <aQua@aQuaYi.com>
2017-10-20 16:03:17 +0800

    357 finish

--- Algorithms/0357.count-numbers-with-unique-digits/count-numbers-with-unique-digits.go
+++ Algorithms/0357.count-numbers-with-unique-digits/count-numbers-with-unique-digits.go
@@ -1,11 +1,11 @@
 package Problem0357
 
+var res = []int{1, 10, 91, 739, 5275, 32491, 168571, 712891, 2345851, 5611771, 8877691}
+
 func countNumbersWithUniqueDigits(n int) int {
-	res := []int{1, 10, 91, 739, 5275, 32491, 168571, 712891, 2345851, 5611771, 8877691}
 	if n >= 10 {
 		return res[10]
 	}
-
 	return res[n]
 }
 
