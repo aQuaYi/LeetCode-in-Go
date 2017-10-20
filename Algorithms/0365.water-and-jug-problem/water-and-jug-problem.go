1c7f0f7fd5b134d444641188c7f5f3f0dfada34c
aQua <aQua@aQuaYi.com>
2017-10-20 19:30:40 +0800

    365 finish

--- Algorithms/0365.water-and-jug-problem/water-and-jug-problem.go
+++ Algorithms/0365.water-and-jug-problem/water-and-jug-problem.go
@@ -1,23 +1,33 @@
 package Problem0365
 
 func canMeasureWater(x int, y int, z int) bool {
+	// 不需要水壶就能实现
 	if z == 0 {
 		return true
 	}
 
-	if x > y {
-		// 保证 x <= y
-		return canMeasureWater(y, x, z)
+	// 两个水壶都不够装
+	if x+y < z {
+		return false
 	}
 
+	// 此段以后，需要 x <= y
+	if x > y {
+		x, y = y, x
+	}
+
+	// 只有一个水壶的话，
+	// 只能提供另一个水壶的容量
 	if x == 0 {
 		return y == z
 	}
 
-	a, b := y, x
-	for a%b != 0 {
-		b, a = a%b, b
+	// y%x == 0 时，
+	// x 是初始 x 和 y 的最大公约数
+	// z%x == 0 的容量都可以提供
+	for y%x != 0 {
+		x, y = y%x, x
 	}
 
-	return z%b == 0 && x+y >= z
+	return z%x == 0
 }
