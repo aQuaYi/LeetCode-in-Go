de67588cf1e595f190218f554b47e2e8ee8b3ce9
aQua <aQua@aQuaYi.com>
2017-10-20 14:50:14 +0800

    347 accepted. 32ms > 29ms

--- Algorithms/0347.top-k-frequent-elements/top-k-frequent-elements.go
+++ Algorithms/0347.top-k-frequent-elements/top-k-frequent-elements.go
@@ -1,6 +1,28 @@
 package Problem0347
 
-func topKFrequent(nums []int, k int) []int {
+import "sort"
 
-	return []int{1, 2}
+func topKFrequent(nums []int, k int) []int {
+	res := make([]int, 0, k)
+	// 统计每个数字出现的次数
+	rec := make(map[int]int, len(nums))
+	for _, n := range nums {
+		rec[n]++
+	}
+	// 对出现次数进行排序
+	counts := make([]int, 0, len(rec))
+	for _, c := range rec {
+		counts = append(counts, c)
+	}
+	sort.Ints(counts)
+	// min 是 前 k 个高频数字的底线
+	min := counts[len(counts)-k]
+
+	for n, c := range rec {
+		if c >= min {
+			res = append(res, n)
+		}
+	}
+
+	return res
 }
