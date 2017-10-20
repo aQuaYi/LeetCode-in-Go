c6549cb24e94ee32df88761bdf7330ca666923c8
aQua <aQua@aQuaYi.com>
2017-10-20 14:31:29 +0800

    343 accepted. 0ms

--- Algorithms/0343.integer-break/integer-break.go
+++ Algorithms/0343.integer-break/integer-break.go
@@ -1,6 +1,34 @@
 package Problem0343
 
 func integerBreak(n int) int {
+	if n == 2 {
+		return 1
+	}
 
-	return
+	if n == 3 {
+		return 2
+	}
+
+	switch n % 3 {
+	case 0:
+		return pow3(n / 3)
+	case 1:
+		return 4 * pow3(n/3-1)
+	default:
+		return 2 * pow3(n/3)
+	}
+
+}
+
+func pow3(n int) int {
+	if n == 0 {
+		return 1
+	}
+
+	res := pow3(n >> 1)
+	if n&1 == 0 {
+		return res * res
+	}
+
+	return res * res * 3
 }
