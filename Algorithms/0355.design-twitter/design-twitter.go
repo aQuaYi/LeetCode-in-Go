b97d8a8a2cffc0b9308670f0e4e13631b77495c9
aQua <aQua@aQuaYi.com>
2017-10-20 14:56:42 +0800

    355 adding

--- Algorithms/0355.design-twitter/design-twitter.go
+++ Algorithms/0355.design-twitter/design-twitter.go
@@ -1,6 +1,38 @@
 package Problem0355
 
+type Twitter struct {
+}
+
+/** Initialize your data structure here. */
 func Constructor() Twitter {
 
-	return
 }
+
+/** Compose a new tweet. */
+func (this *Twitter) PostTweet(userId int, tweetId int) {
+
+}
+
+/** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
+func (this *Twitter) GetNewsFeed(userId int) []int {
+
+}
+
+/** Follower follows a followee. If the operation is invalid, it should be a no-op. */
+func (this *Twitter) Follow(followerId int, followeeId int) {
+
+}
+
+/** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
+func (this *Twitter) Unfollow(followerId int, followeeId int) {
+
+}
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.PostTweet(userId,tweetId);
+ * param_2 := obj.GetNewsFeed(userId);
+ * obj.Follow(followerId,followeeId);
+ * obj.Unfollow(followerId,followeeId);
+ */
