7ab1b7838f336536784c37fd2256a29b8ad134b0
aQua <aQua@aQuaYi.com>
2017-10-21 11:57:21 +0800

    327 refactoring

--- Algorithms/0327.count-of-range-sum/count-of-range-sum.go
+++ Algorithms/0327.count-of-range-sum/count-of-range-sum.go
@@ -1,21 +1,76 @@
 package Problem0327
 
-func countRangeSum(a []int, lower int, upper int) int {
-	size := len(a)
-	var count = 0
-	check := func(n int) {
-		if lower <= n && n <= upper {
-			count++
-		}
+var lo, hi int
+var tmp []int
+
+func countRangeSum(nums []int, lower int, upper int) int {
+	size := len(nums)
+	if size == 0 {
+		return 0
 	}
-	var sum int
-	for i := 0; i < size; i++ {
-		sum = 0
-		for j := i; j < size; j++ {
-			sum += a[j ]
-			check(sum)
+
+	sum := make([]int, size+1)
+	for i, n := range nums {
+		sum[i+1] = sum[i] + n
+	}
+
+	lo = lower
+	hi = upper
+	tmp = make([]int, len(sum))
+
+	return sort(sum)
+}
+
+func sort(nums []int) int {
+	size := len(nums)
+	if size == 1 {
+		return 0
+	}
+
+	mid := size / 2
+	left := nums[:mid]
+	right := nums[mid:]
+
+	count := sort(left) + sort(right)
+
+	i := 0
+	j := 0
+
+	sl := 0
+	sr := 0
+
+	for i < len(left) && j < len(right) {
+		if left[i] < right[j] {
+			tmp[i+j] = left[i]
+			for sl < len(right) && right[sl]-left[i] < lo {
+				sl++
+			}
+			for sr < len(right) && right[sr]-left[i] <= hi {
+				sr++
+			}
+			count += sr - sl
+			i++
+		} else {
+			tmp[i+j] = right[j]
+			j++
 		}
 	}
 
+	if i == len(left) {
+		copy(tmp[i+j:], right[j:])
+	} else {
+		copy(tmp[i+j:], left[i:])
+		for i < len(left) {
+			for sl < len(right) && right[sl]-left[i] < lo {
+				sl++
+			}
+			for sr < len(right) && right[sr]-left[i] <= hi {
+				sr++
+			}
+			count += sr - sl
+			i++
+		}
+	}
+	copy(nums, tmp)
 	return count
 }
