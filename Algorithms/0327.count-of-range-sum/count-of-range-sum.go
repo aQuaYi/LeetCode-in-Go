48358e2bec6f96c98618d0e318e0e17e6cb7b402
aQua <aQua@aQuaYi.com>
2017-10-21 10:43:37 +0800

    327 accepted. 596ms > 23ms

--- Algorithms/0327.count-of-range-sum/count-of-range-sum.go
+++ Algorithms/0327.count-of-range-sum/count-of-range-sum.go
@@ -1,6 +1,21 @@
 package Problem0327
 
-func countRangeSum(nums []int, lower int, upper int) int {
+func countRangeSum(a []int, lower int, upper int) int {
+	size := len(a)
+	var count = 0
+	check := func(n int) {
+		if lower <= n && n <= upper {
+			count++
+		}
+	}
+	var sum int
+	for i := 0; i < size; i++ {
+		sum = 0
+		for j := i; j < size; j++ {
+			sum += a[j ]
+			check(sum)
+		}
+	}
 
-	return 3
+	return count
 }
