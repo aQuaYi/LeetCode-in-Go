4ab80d4262eddeeb2698f4ee9db9617f5be42736
aQua <aQua@aQuaYi.com>
2017-10-21 12:48:38 +0800

    327 accepted. 23ms

--- Algorithms/0327.count-of-range-sum/count-of-range-sum.go
+++ Algorithms/0327.count-of-range-sum/count-of-range-sum.go
@@ -1,6 +1,6 @@
 package Problem0327
 
-var lo, hi int
+var lo, up int
 var tmp []int
 
 func countRangeSum(nums []int, lower int, upper int) int {
@@ -9,46 +9,65 @@ func countRangeSum(nums []int, lower int, upper int) int {
 		return 0
 	}
 
+	// sum[i] == sum(nums[:i])
+	// sum[j] - sum[i] == sum(nums[i:j])
 	sum := make([]int, size+1)
 	for i, n := range nums {
 		sum[i+1] = sum[i] + n
 	}
 
 	lo = lower
-	hi = upper
+	up = upper
 	tmp = make([]int, len(sum))
 
-	return sort(sum)
+	return mergeSort(sum)
 }
 
-func sort(nums []int) int {
+// 对前 i 项的和，进行归并排序
+// 归并 left 和 right 的同时，检查符合条件的子数组
+func mergeSort(nums []int) int {
 	size := len(nums)
 	if size == 1 {
 		return 0
 	}
 
 	mid := size / 2
+	// left 中任意一个值，所代表的 sum(nums[:i])
+	// right 中任意一个值，所代表的 sum(nums[:j])
+	// 可以得知， i < j， 这一点很重要，
+	// 正因为如此，对于任意一个 right[j]-left[i] 都是某个子数组的 sum，统计才有意义
 	left := nums[:mid]
 	right := nums[mid:]
 
-	count := sort(left) + sort(right)
+	// 分而治之
+	count := mergeSort(left) + mergeSort(right)
 
+	// 用于归并
 	i := 0
 	j := 0
 
-	sl := 0
-	sr := 0
+	// 用于 count
+	cl := 0
+	cr := 0
 
 	for i < len(left) && j < len(right) {
 		if left[i] < right[j] {
 			tmp[i+j] = left[i]
-			for sl < len(right) && right[sl]-left[i] < lo {
-				sl++
+
+			for cl < len(right) && right[cl]-left[i] < lo {
+				cl++
 			}
-			for sr < len(right) && right[sr]-left[i] <= hi {
-				sr++
+			for cr < len(right) && right[cr]-left[i] <= up {
+				cr++
 			}
-			count += sr - sl
+			// 对于 left[i] 所代表的 sum(nums[:x]) 来说
+			// 此时 right 中存在有 cr - cl 个 right[cy] 所代表的 sum(nums[:y])
+			// 能够保证 lower <= sum(nums[x:y]) <= upper
+			//
+			// 上段注释中，在 nums 中用到了新的索引号 x , y ，而不是 left 和 right中用到的 i 和 cy
+			// 是因为 left 和 right 经过排序后，位置已经打乱，两者的索引号对应关系也被打乱了
+			// 但是，我们知道 x < y 一定成立，就可以了
+			count += cr - cl
 			i++
 		} else {
 			tmp[i+j] = right[j]
@@ -60,17 +79,20 @@ func sort(nums []int) int {
 		copy(tmp[i+j:], right[j:])
 	} else {
 		copy(tmp[i+j:], left[i:])
+		// left 中的元素没有检查完，继续检查
 		for i < len(left) {
-			for sl < len(right) && right[sl]-left[i] < lo {
-				sl++
+			for cl < len(right) && right[cl]-left[i] < lo {
+				cl++
 			}
-			for sr < len(right) && right[sr]-left[i] <= hi {
-				sr++
+			for cr < len(right) && right[cr]-left[i] <= up {
+				cr++
 			}
-			count += sr - sl
+			count += cr - cl
 			i++
 		}
 	}
+
 	copy(nums, tmp)
+
 	return count
 }
