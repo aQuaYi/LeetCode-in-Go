c1503b52a32f8513db0cae25270a5ea6c218ad8c
aQua <aQua@aQuaYi.com>
2017-10-22 17:09:06 +0800

    290 accepted.

--- Algorithms/0290.word-pattern/word-pattern.go
+++ Algorithms/0290.word-pattern/word-pattern.go
@@ -10,15 +10,18 @@ func wordPattern(pattern string, str string) bool {
 	if len(ps) != len(ss) {
 		return false
 	}
-	return match(ps, ss) && match(ss, ps)
+	return isMatch(ps, ss) && isMatch(ss, ps)
 }
 
-func match(s1, s2 []string) bool {
+func isMatch(s1, s2 []string) bool {
 	size := len(s1)
+
 	m := make(map[string]string, size)
+
 	var i int
 	var w string
 	var ok bool
+
 	for i = 0; i < size; i++ {
 		if w, ok = m[s1[i]]; ok {
 			if w != s2[i] {
