5ff9eff3680772c1c565131f2e16bc470e31404f
aQua <aQua@aQuaYi.com>
2017-10-22 13:37:42 +0800

    368 copy

--- Algorithms/0368.largest-divisible-subset/largest-divisible-subset.go
+++ Algorithms/0368.largest-divisible-subset/largest-divisible-subset.go
@@ -5,23 +5,27 @@ import "sort"
 func largestDivisibleSubset(a []int) []int {
 	size := len(a)
 	sort.Ints(a)
+	res := make([]int, 0, size)
 
-	temps := make([][]int, 1, size-1)
-	temps[0] = []int{a[0]}
-
-	var i, j, k, t int
-	for i = 1; i < size; i++ {
-		for _, temp := range temps {
-			for j, t = range temp {
-				if a[i]%t != 0 {
-					break
-				}
-			}
-
-		}
+	if a[0] == 1 {
+		res = append(res, 1)
+		a = a[1:]
 	}
 
-	var res []int
+	temps := make([][]int, 0, size)
+	for len(a) > 0 {
+		t := a[0]
+		temp := make([]int, 1, len(a))
+		tempA := make([]int, 0, len(a))
+		temp[0] = t
+		for i := 1; i < len(a); i++ {
+			if a[i]%t == 0 {
+				temp = append(temp, a[i])
+			} else {
+				tempA = append(tempA, a[i])
+			}
+		}
+	}
 
 	return res
 }
