2e9275353e9bcaf3796d5d54b5e441b0a49b9bc2
aQua <aQua@aQuaYi.com>
2017-10-22 11:06:30 +0800

    327 Time Limit Exceeded & wrong answer

--- Algorithms/0329.longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.go
+++ Algorithms/0329.longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.go
@@ -1,6 +1,53 @@
 package Problem0329
 
-func longestIncreasingPath(matrix [][]int) int {
+func longestIncreasingPath(mat [][]int) int {
+	if len(mat) == 0 || len(mat[0]) == 0 {
+		return 0
+	}
 
-	return 4
+	m, n := len(mat), len(mat[0])
+
+	isChecked := make([][]bool, m)
+	for i := range isChecked {
+		isChecked[i] = make([]bool, n)
+	}
+
+	res := 0
+	var dfs func(int, int, int)
+	dfs = func(i, j, count int) {
+		if res < count {
+			res = count
+		}
+
+		isChecked[i][j] = true
+
+		if 0 <= i-1 && !isChecked[i-1][j] && mat[i-1][j] > mat[i][j] {
+			dfs(i-1, j, count+1)
+		}
+
+		if 0 <= j-1 && !isChecked[i][j-1] && mat[i][j-1] > mat[i][j] {
+			dfs(i, j-1, count+1)
+		}
+
+		if i+1 < m && !isChecked[i+1][j] && mat[i+1][j] > mat[i][j] {
+			dfs(i+1, j, count+1)
+		}
+
+		if j+1 < n && !isChecked[i][j+1] && mat[i][j+1] > mat[i][j] {
+			dfs(i, j+1, count+1)
+		}
+
+		isChecked[i][j] = false
+	}
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			dfs(i, j, 1)
+			if res >= m*n/2 {
+				return res
+			}
+		}
+	}
+
+	return res
 }
