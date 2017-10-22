2d1834e0c2720c8c1125b16927eced9f577aeac4
aQua <aQua@aQuaYi.com>
2017-10-22 12:50:52 +0800

    368 wrong answer

--- Algorithms/0368.largest-divisible-subset/largest-divisible-subset.go
+++ Algorithms/0368.largest-divisible-subset/largest-divisible-subset.go
@@ -1,7 +1,35 @@
 package Problem0368
 
-func largestDivisibleSubset(nums []int) []int {
-	res :=
+import "sort"
+
+func largestDivisibleSubset(a []int) []int {
+	var res []int
+
+	size := len(a)
+
+	sort.Ints(a)
+
+	var i, j, k int
+
+	for i = 0; i < size; i++ {
+		temp := make([]int, 0, size-i)
+		temp = append(temp, a[i])
+		for j = i + 1; j < size; j++ {
+			ok := true
+			for k = 0; k < len(temp); k++ {
+				if a[j]%temp[k] != 0 {
+					ok = false
+					break
+				}
+			}
+			if ok {
+				temp = append(temp, a[j])
+			}
+		}
+		if len(res) < len(temp) {
+			res = temp
+		}
+	}
 
 	return res
 }
