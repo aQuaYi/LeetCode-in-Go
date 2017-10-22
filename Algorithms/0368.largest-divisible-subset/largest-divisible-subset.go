7b811f36c4c96edd5def2bc312c2c8b853b631c1
aQua <aQua@aQuaYi.com>
2017-10-22 15:08:02 +0800

    368 finish

--- Algorithms/0368.largest-divisible-subset/largest-divisible-subset.go
+++ Algorithms/0368.largest-divisible-subset/largest-divisible-subset.go
@@ -12,7 +12,7 @@ func largestDivisibleSubset(a []int) []int {
 	// 注意：下面的 a 是升序的啦
 
 	// max 是符合题意的 largest subset 的长度
-	// idx 是这个 largest subset 中最小值的 index
+	// largest subset 中最小值是 a[idx]
 	max := 1
 	idx := 0
 
@@ -22,21 +22,24 @@ func largestDivisibleSubset(a []int) []int {
 		dp[i] = 1
 	}
 
-	// next[i]==j 说明
+	// next 用来记录符合题意的 largest subset 的元素的索引值
+	// largest subset 中 a[i] 的下一个元素是 a[next[i]]
+	// 这样的话，由 idx 就可以得出 result 了
 	next := make([]int, size)
-	for i := range next {
-		next[i] = i
-	}
 
+	// 从大往小，可以检查，因为
+	// 如果 a[j]%a[i]==0的话，a[next[j]]%a[i]==0 肯定成立，就无需再检查了
 	for i := size - 2; 0 <= i; i-- {
 		for j := size - 1; i < j; j-- {
 			if a[j]%a[i] != 0 {
 				continue
 			}
 			if dp[i] < dp[j]+1 {
+				// a[i] 指向 a[j] 可以让符合题意的 subset 变大
 				next[i] = j
 				dp[i] = dp[j] + 1
 			}
+			// 出现了更大的 subset
 			if max < dp[i] {
 				max = dp[i]
 				idx = i
@@ -44,12 +47,11 @@ func largestDivisibleSubset(a []int) []int {
 		}
 	}
 
-	res := make([]int, 0, max)
-	for next[idx] != idx {
-		res = append(res, a[idx])
+	res := make([]int, max)
+	for i := range res {
+		res[i] = a[idx]
 		idx = next[idx]
 	}
-	res = append(res, a[idx])
 
 	return res
 }
