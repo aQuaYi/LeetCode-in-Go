6adb83990e4a22f6e76380f1104356c2a44b4c93
aQua <aQua@aQuaYi.com>
2017-10-22 18:02:48 +0800

    349 finish

--- Algorithms/0349.intersection-of-two-arrays/intersection-of-two-arrays.go
+++ Algorithms/0349.intersection-of-two-arrays/intersection-of-two-arrays.go
@@ -2,15 +2,15 @@ package Problem0349
 
 func intersection(a1, a2 []int) []int {
 	res := []int{}
-	m1 := makeMap(a1)
-	m2 := makeMap(a2)
+	m1 := getInts(a1)
+	m2 := getInts(a2)
 
 	if len(m1) > len(m2) {
 		m1, m2 = m2, m1
 	}
-
+	// m1 是较短的字典，会快一些
 	for n := range m1 {
-		if _, ok := m2[n]; ok {
+		if m2[n] {
 			res = append(res, n)
 		}
 	}
@@ -18,7 +18,8 @@ func intersection(a1, a2 []int) []int {
 	return res
 }
 
-func makeMap(a []int) map[int]bool {
+// 清理重复的值，也便于查询
+func getInts(a []int) map[int]bool {
 	res := make(map[int]bool, len(a))
 
 	for i := range a {
