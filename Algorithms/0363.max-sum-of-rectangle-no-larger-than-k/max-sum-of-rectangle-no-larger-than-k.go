dc6f949a1f35c6dd16321da50354d96650ae1651
aQua <aQua@aQuaYi.com>
2017-10-23 19:32:59 +0800

    363 使用了 归并排序 后，程序提速了 40%

--- Algorithms/0363.max-sum-of-rectangle-no-larger-than-k/max-sum-of-rectangle-no-larger-than-k.go
+++ Algorithms/0363.max-sum-of-rectangle-no-larger-than-k/max-sum-of-rectangle-no-larger-than-k.go
@@ -1,7 +1,5 @@
 package Problem0363
 
-import "sort"
-
 func maxSumSubmatrix(mat [][]int, target int) int {
 	if len(mat) == 0 || len(mat[0]) == 0 {
 		return 0
@@ -10,13 +8,15 @@ func maxSumSubmatrix(mat [][]int, target int) int {
 	m, n := len(mat), len(mat[0])
 	// 本算法的复杂度是 O(M*M*N*logN)
 	// 让 N = max(m,n) 可以加快程序
-	M, N := min(m, n), max(m, n)
+	M := min(m, n)
+	N := max(m, n)
 	// 从此开始称， M 为行，N 为列
 
 	ans := -1 << 63
 
 	var findTarget func([]int, int, int) int
-	// 运用归并排序的思想，在 sums 中查找 <=target 的最大元素
+	// 运用归并排序，在给 sums 排序的同时
+	// 查找符合 <=target 的最大值
 	findTarget = func(sums []int, beg, end int) int {
 		if beg+1 >= end {
 			return -1 << 63
@@ -32,11 +32,11 @@ func maxSumSubmatrix(mat [][]int, target int) int {
 		var l, r int
 		r = mid
 		for l = beg; l < mid; l++ {
-			for r < len(sums) && sums[r]-sums[l] <= target {
+			for r < end && sums[r]-sums[l] <= target {
 				// sums[mid:end] 中任意的的 sums[r] 和
 				// sums[beg:mid] 中任意的的 sums[l] 相减
 				// 都是某个子矩阵的所有元素之和
-				// 此时，由于 sums[beg:mid] 和 sums[mid:beg] 都是递增的
+				// 此时，由于 sums[beg:mid] 和 sums[mid:end] 都是递增的
 				// 可以避免不必要的检查
 				temp := sums[r] - sums[l]
 				if temp == target {
@@ -47,12 +47,13 @@ func maxSumSubmatrix(mat [][]int, target int) int {
 			}
 		}
 
-		// 虽然没有使用归并排序，但是这样的程序更简洁
-		sort.Ints(sums[beg:end])
+		// 另外使用归并排序，这样的程序更简洁
+		copy(sums[beg:end], merge(sums[beg:mid], sums[mid:end]))
+
 		return res
 	}
 
-	var iFirst, iLast, j, minArea, maxArea int
+	var iFirst, iLast, j, minSum, maxSub int
 	var temp, sums []int
 
 	for iFirst = 0; iFirst < M; iFirst++ {
@@ -65,10 +66,10 @@ func maxSumSubmatrix(mat [][]int, target int) int {
 			// sums 中添加 0 为了 sums[j] - 0  , 表示
 			// mat[iFisrt:iLast+1][:j+1] 中所有元素之和
 			sums = []int{0}
-			// maxArea 是 mat[iFirst:iLast+1][:] 中所有子矩阵中，所有元素之和的 最大值
-			maxArea = -1 << 63
-			// minArea 是 mat[iFirst:iLast+1][:] 中所有子矩阵中，所有元素之和的 最小值
-			minArea = 1<<63 - 1
+			// maxSub 是 mat[iFirst:iLast+1][:] 中所有子矩阵中，所有元素之和的 最大值
+			maxSub = -1 << 63
+			// minSum 是 sums 中的 最小值
+			minSum = 1<<63 - 1
 			for j = 0; j < N; j++ {
 				// 分情况更新 temp[j]
 				if m < n {
@@ -79,24 +80,24 @@ func maxSumSubmatrix(mat [][]int, target int) int {
 
 				sums = append(sums, sums[len(sums)-1]+temp[j])
 
-				maxArea = max(maxArea, sums[len(sums)-1]-minArea)
+				maxSub = max(maxSub, sums[len(sums)-1]-minSum)
 
-				minArea = min(minArea, sums[len(sums)-1])
+				minSum = min(minSum, sums[len(sums)-1])
 			}
 
-			if maxArea <= ans {
+			if maxSub <= ans {
 				// mat[iFirst:iLast+1][:] 中不可能有比 ans 更接近 target 的数
 				// 可以开始下一轮了
 				continue
 			}
 
-			if maxArea == target {
+			if maxSub == target {
 				// mat[iFirst:iLast+1][:] 中有一个子矩阵的所有元素之和，正好就是 target
 				// 找到答案，可以结束程序了
 				return target
 			}
 
-			if maxArea > target {
+			if maxSub > target {
 				// mat[iFirst:iLast+1][:] 中可能有个子矩阵的所有元素之和，
 				// 比 ans 更接近 target 需要运行 findTarget 查找
 				tempAns := findTarget(sums, 0, N+1)
@@ -125,4 +126,31 @@ func min(a, b int) int {
 		return a
 	}
 	return b
+}
+
+func merge(a, b []int) []int {
+	lenA, lenB := len(a), len(b)
+	temp := make([]int, lenA+lenB)
+
+	var i, j, k int
+	for i < lenA && j < lenB {
+		if a[i] < b[j] {
+			temp[k] = a[i]
+			i++
+		} else {
+			temp[k] = b[j]
+			j++
+		}
+		k++
+	}
+
+	if i == lenA {
+		copy(temp[k:], b[j:])
+	}
+
+	if j == lenB {
+		copy(temp[k:], a[i:])
+	}
+
+	return temp
 }
