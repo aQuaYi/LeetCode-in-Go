09e1d304222d63396cebb263892e3a6de0d18918
aQua <aQua@aQuaYi.com>
2017-10-23 14:47:33 +0800

    363 copy

--- Algorithms/0363.max-sum-of-rectangle-no-larger-than-k/max-sum-of-rectangle-no-larger-than-k.go
+++ Algorithms/0363.max-sum-of-rectangle-no-larger-than-k/max-sum-of-rectangle-no-larger-than-k.go
@@ -1,7 +1,92 @@
 package Problem0363
 
-func maxSumSubmatrix(matrix [][]int, k int) int {
-	res := 2
+import (
+	"sort"
+)
 
-	return res
+func maxSumSubmatrix(mat [][]int, k int) int {
+	if len(mat) == 0 || len(mat[0]) == 0 {
+		return 0
+	}
+
+	m, n := len(mat), len(mat[0])
+	var M, N int
+	if m < n {
+		M, N = m, n
+	} else {
+		M, N = n, m
+	}
+
+	ans := -1 << 63
+
+	var findMaxArea func([]int, int, int) int
+	findMaxArea = func(sums []int, beg, end int) int {
+		if beg+1 >= end {
+			return -1 << 63
+		}
+
+		mid := (beg + end) >> 1
+
+		res := max(findMaxArea(sums, beg, mid), findMaxArea(sums, mid, end))
+
+		i := mid
+		for _, l := range sums[beg:mid] {
+			for i < len(sums) && sums[i]-l <= k {
+				res = max(res, sums[i]-l)
+				i++
+			}
+		}
+		sort.Ints(sums[beg:end])
+		return res
+	}
+
+	var i1, i2, j int
+	for i1 = 0; i1 < M; i1++ {
+		tmp := make([]int, N)
+		for i2 = i1; i2 < M; i2++ {
+			sums := []int{0}
+			low := 0
+			maxArea := -1 << 63
+			for j = 0; j < N; j++ {
+				if m < n {
+					tmp[j] += mat[i2][j]
+				} else {
+					tmp[j] += mat[j][i2]
+				}
+
+				sums = append(sums, sums[len(sums)-1]+tmp[j])
+
+				maxArea = max(maxArea, sums[len(sums)-1]-low)
+
+				low = min(low, sums[len(sums)-1])
+
+			}
+			if maxArea <= ans {
+				continue
+			}
+			if maxArea == k {
+				return k
+			}
+			if maxArea > k {
+				maxArea = findMaxArea(sums, 0, N+1)
+			}
+			ans = max(ans, maxArea)
+		}
+	}
+
+	return ans
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
