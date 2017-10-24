edb07725a7e8a4d6cf5a0dde16d91c5764779d02
aQua <aQua@aQuaYi.com>
2017-10-24 17:20:29 +0800

    413 accepted. 6ms > 0ms

--- Algorithms/0413.arithmetic-slices/arithmetic-slices.go
+++ Algorithms/0413.arithmetic-slices/arithmetic-slices.go
@@ -1,7 +1,19 @@
 package Problem0413
 
-func numberOfArithmeticSlices(A []int) int {
-	res :=
+func numberOfArithmeticSlices(a []int) int {
+	if len(a) < 3 {
+		return 0
+	}
 
-	return res
+	if a[1]-a[0] != a[2]-a[1] {
+		return numberOfArithmeticSlices(a[1:])
+	}
+
+	var i = 3
+	diff := a[1] - a[0]
+	for i < len(a) && a[i]-a[i-1] == diff {
+		i++
+	}
+
+	return (i*i-3*i+2)>>1 + numberOfArithmeticSlices(a[i:])
 }
