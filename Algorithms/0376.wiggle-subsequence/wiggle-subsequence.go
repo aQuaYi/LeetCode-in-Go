bcf1a8eb2c7e8bf17be744646a068a9871b18557
aQua <aQua@aQuaYi.com>
2017-10-24 14:29:55 +0800

    376 finish

--- Algorithms/0376.wiggle-subsequence/wiggle-subsequence.go
+++ Algorithms/0376.wiggle-subsequence/wiggle-subsequence.go
@@ -1,41 +1,39 @@
 package Problem0376
 
 func wiggleMaxLength(nums []int) int {
+	var res int
 	size := len(nums)
 
-	if size < 2 {
-		return size
-	}
-
-	checkFunc := func(n int) (func(int), func() int) {
+	checkFunc := func(n int) func(int) {
 		init := 1
 		if n < 0 {
 			init = -1
 		}
-		res := 2
+		res = 2
 
+		// 当 x 与 init 不同号的时候
+		// res++
 		return func(x int) {
-				var new int
-				switch {
-				case x < 0:
-					new = -1
-				case x > 0:
-					new = 1
-				default:
-					return
-				}
+			var new int
 
-				if init*new < 0 {
-					res++
-					init = new
-				}
-			}, func() int {
-				return res
+			switch {
+			case x < 0:
+				new = -1
+			case x > 0:
+				new = 1
+			default:
+				// 跳过 0
+				return
 			}
+
+			if init*new < 0 {
+				res++
+				init = new
+			}
+		}
 	}
 
 	var check func(int)
-	var res func() int
 	var i = 1
 	for i < size && nums[i]-nums[i-1] == 0 {
 		i++
@@ -45,12 +43,12 @@ func wiggleMaxLength(nums []int) int {
 		return 1
 	}
 
-	check, res = checkFunc(nums[i] - nums[i-1])
+	check = checkFunc(nums[i] - nums[i-1])
 
 	for i < size {
 		check(nums[i] - nums[i-1])
 		i++
 	}
 
-	return res()
+	return res
 }
