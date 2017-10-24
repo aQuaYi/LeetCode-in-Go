6aad43a8b562e2fe5207250baea4c43790a9a509
aQua <aQua@aQuaYi.com>
2017-10-24 17:05:00 +0800

    413 adde

---
+++ Algorithms/0413.arithmetic-slices/arithmetic-slices.go
@@ -0,0 +1,7 @@
+package Problem0413
+
+func numberOfArithmeticSlices(A []int) int {
+	res :=
+
+	return res
+}
---
+++ Algorithms/0413.arithmetic-slices/arithmetic-slices_test.go
@@ -0,0 +1,37 @@
+package Problem0413
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	A   []int
+	ans int
+}{
+
+	{[]int{1, 2, 3, 4}, 3},
+	{[]int{1, 1, 1, 2, 2, 2}, 2},
+
+	// 可以有多个 testcase
+}
+
+func Test_numberOfArithmeticSlices(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, numberOfArithmeticSlices(tc.A), "输入:%v", tc)
+	}
+}
+
+func Benchmark_numberOfArithmeticSlices(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			numberOfArithmeticSlices(tc.A)
+		}
+	}
+}
