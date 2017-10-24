f7324158b26394dd7d3e2d5f0d6e2d4212c8595d
aQua <aQua@aQuaYi.com>
2017-10-24 14:22:27 +0800

    376 越改越慢

--- Algorithms/0376.wiggle-subsequence/wiggle-subsequence.go
+++ Algorithms/0376.wiggle-subsequence/wiggle-subsequence.go
@@ -7,49 +7,50 @@ func wiggleMaxLength(nums []int) int {
 		return size
 	}
 
-	res := 1
-
-	checkFunc := func(n int) func(int) {
+	checkFunc := func(n int) (func(int), func() int) {
 		init := 1
 		if n < 0 {
 			init = -1
 		}
-		res++
+		res := 2
 
 		return func(x int) {
-			var new int
-			switch {
-			case x < 0:
-				new = -1
-			case x > 0:
-				new = 1
-			default:
-				return
-			}
+				var new int
+				switch {
+				case x < 0:
+					new = -1
+				case x > 0:
+					new = 1
+				default:
+					return
+				}
 
-			if init*new < 0 {
-				res++
-				init = new
+				if init*new < 0 {
+					res++
+					init = new
+				}
+			}, func() int {
+				return res
 			}
-		}
 	}
 
 	var check func(int)
+	var res func() int
 	var i = 1
 	for i < size && nums[i]-nums[i-1] == 0 {
 		i++
 	}
 
 	if i == size {
-		return res
+		return 1
 	}
 
-	check = checkFunc(nums[i] - nums[i-1])
+	check, res = checkFunc(nums[i] - nums[i-1])
 
 	for i < size {
 		check(nums[i] - nums[i-1])
 		i++
 	}
 
-	return res
+	return res()
 }
