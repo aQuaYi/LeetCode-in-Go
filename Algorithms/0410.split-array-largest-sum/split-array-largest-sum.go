738444ac7a4812baa5bca734444173727e1568d6
aQua <aQua@aQuaYi.com>
2017-10-25 08:50:49 +0800

    410 added

---
+++ Algorithms/0410.split-array-largest-sum/split-array-largest-sum.go
@@ -0,0 +1,7 @@
+package Problem0410
+
+func splitArray(nums []int, m int) int {
+	res :=
+
+	return res
+}
---
+++ Algorithms/0410.split-array-largest-sum/split-array-largest-sum_test.go
@@ -0,0 +1,41 @@
+package Problem0410
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	nums []int
+	m    int
+	ans  int
+}{
+
+	{
+		[]int{7, 2, 5, 10, 8},
+		2,
+		18,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_splitArray(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, splitArray(tc.nums, tc.m), "输入:%v", tc)
+	}
+}
+
+func Benchmark_splitArray(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			splitArray(tc.nums, tc.m)
+		}
+	}
+}
