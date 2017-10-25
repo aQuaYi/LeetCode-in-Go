9d03b787cc0121540c141179e611d650ad569346
aQua <aQua@aQuaYi.com>
2017-10-25 09:17:14 +0800

    helper 修改了细节

--- helper.v5/tasks.go
+++ helper.v5/tasks.go
@@ -14,7 +14,7 @@ func printTasks() {
 func makeTasksFile(problems problems) {
 	content := ""
 	for _, p := range problems {
-		content += fmt.Sprintf("%d - %s%% - %d - %s\n", p.Difficulty, p.PassRate, p.ID, p.Title)
+		content += fmt.Sprintf("%d - %s - %d - %s\n", p.Difficulty, p.PassRate, p.ID, p.Title)
 	}
 	// 保存 taskFile 文件
 	err := ioutil.WriteFile(tasksFile, []byte(content), 0755)
