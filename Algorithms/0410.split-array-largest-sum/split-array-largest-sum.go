bc1fd69647c5a6a0dba855a6cc02925079a7a95b
aQua <aQua@aQuaYi.com>
2017-10-25 13:05:09 +0800

    410 adding

--- Algorithms/0410.split-array-largest-sum/split-array-largest-sum.go
+++ Algorithms/0410.split-array-largest-sum/split-array-largest-sum.go
@@ -1,7 +1,23 @@
 package Problem0410
 
 func splitArray(nums []int, m int) int {
-	res :=
+	var Max, n, size, i, sum int
+	for _, n := range nums {
+		sum += n
+		if Max < n {
+			Max = n
+		}
+	}
 
+	if Max > sum/m {
+		return Max
+	}
+
+	sum /= m
+	size = len(nums)
+
+	// 从此开始，在 nums 找到一个子集，使得其和为 >=sum 的最小值
+
+	// dp[i][j][k] 表示 在 nums[i:j] 中
 	return res
 }
