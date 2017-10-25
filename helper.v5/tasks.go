429aeaddc01248530c438cc6a34574d97e04cb61
aQua <aQua@aQuaYi.com>
2017-10-25 09:19:53 +0800

    helper 添加遗漏的筛选条件

--- helper.v5/tasks.go
+++ helper.v5/tasks.go
@@ -14,7 +14,9 @@ func printTasks() {
 func makeTasksFile(problems problems) {
 	content := ""
 	for _, p := range problems {
-		content += fmt.Sprintf("%d - %s - %d - %s\n", p.Difficulty, p.PassRate, p.ID, p.Title)
+		if !p.IsAccepted && p.IsAvailable {
+			content += fmt.Sprintf("%d - %s - %d - %s\n", p.Difficulty, p.PassRate, p.ID, p.Title)
+		}
 	}
 	// 保存 taskFile 文件
 	err := ioutil.WriteFile(tasksFile, []byte(content), 0755)
