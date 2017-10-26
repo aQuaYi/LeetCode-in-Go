fd57f6b4fca7ed75a7ec1b22e251eda0af65a924
aQua <aQua@aQuaYi.com>
2017-10-26 14:22:27 +0800

    307 finish. 172ms > 162ms

--- Algorithms/0307.range-sum-query-mutable/range-sum-query-mutable.go
+++ Algorithms/0307.range-sum-query-mutable/range-sum-query-mutable.go
@@ -2,30 +2,26 @@ package Problem0307
 
 // NumArray 是 nums 的和的切片
 type NumArray struct {
-	nums, dp []int
+	nums []int
 }
 
 // Constructor 返回 NumArray
 func Constructor(nums []int) NumArray {
-	// dp[i+1] == SumRange(0,i)
-	dp := make([]int, len(nums)+1)
-	for i := 0; i < len(nums); i++ {
-		dp[i+1] = dp[i] + nums[i]
-	}
-
-	return NumArray{nums: nums, dp: dp}
+	return NumArray{nums: nums}
 }
 
 // Update 更新 nums
 func (na *NumArray) Update(i int, v int) {
-	for idx := i; idx < len(na.dp); idx++ {
-		na.dp[idx] += v - na.nums[i]
-	}
+	na.nums[i] = v
 }
 
 // SumRange 返回 sum(nums[i:j+1])
 func (na *NumArray) SumRange(i int, j int) int {
-	return na.dp[j+1] - na.dp[i]
+	res := 0
+	for k := i; k <= j; k++ {
+		res += na.nums[k]
+	}
+	return res
 }
 
 /**
