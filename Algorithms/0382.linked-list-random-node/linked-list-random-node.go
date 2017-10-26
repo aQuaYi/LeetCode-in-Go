bcf7b115cd293a42f5270974237638765c799405
aQua <aQua@aQuaYi.com>
2017-10-26 09:02:23 +0800

    382 added

--- Algorithms/0382.linked-list-random-node/linked-list-random-node.go
+++ Algorithms/0382.linked-list-random-node/linked-list-random-node.go
@@ -1,7 +1,34 @@
 package Problem0382
 
-func Constructor(head *ListNode) Solution {
-	res :=
+import (
+	"github.com/aQuaYi/LeetCode-in-Go/kit"
+)
 
-	return res
+type ListNode = kit.ListNode
+
+// Solution 是需要设计的数据结构
+// Definition for singly-linked list.
+// type ListNode struct {
+//     Val int
+//     Next *ListNode
+// }
+type Solution struct {
 }
+
+// Constructor 构建 Solution
+// head is The linked list's head.
+// Note that the head is guaranteed to be not null, so it contains at least one node. */
+func Constructor(head *ListNode) Solution {
+
+}
+
+// GetRandom returns a random node's value. */
+func (this *Solution) GetRandom() int {
+
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * obj := Constructor(head);
+ * param_1 := obj.GetRandom();
+ */
--- Algorithms/0382.linked-list-random-node/linked-list-random-node_test.go
+++ Algorithms/0382.linked-list-random-node/linked-list-random-node_test.go
@@ -1,37 +1,12 @@
 package Problem0382
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-// tcs is testcase slice
-var tcs = []struct {
-	head *ListNode
-	ans  Solution 
-}{
-
-	{ },
-	{ },
-
-	// 可以有多个 testcase
-}
-
 func Test_Constructor(t *testing.T) {
 	ast := assert.New(t)
-	
-	for _, tc := range tcs {
-		fmt.Printf("~~%v~~\n", tc)
-		ast.Equal(tc.ans, Constructor(tc.head), "输入:%v", tc)
-	}
-}
 
-func Benchmark_Constructor(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		for _, tc := range tcs {
-			Constructor(tc.head)
-		}
-	}
 }
