316847030f78bb07d2b2dae187a5fb68247b4bde
aQua <aQua@aQuaYi.com>
2017-10-26 14:59:47 +0800

    332 更改解题思路

--- Algorithms/0332.reconstruct-itinerary/reconstruct-itinerary.go
+++ Algorithms/0332.reconstruct-itinerary/reconstruct-itinerary.go
@@ -1,9 +1,25 @@
 package Problem0332
 
+import (
+	"sort"
+)
+
 func findItinerary(tickets [][]string) []string {
 	res := []string{}
 
-	
+	nexts := make(map[string][]string, len(tickets))
+
+	for _, t := range tickets {
+		nexts[t[0]] = append(nexts[t[0]], t[1])
+	}
+
+	for dep := range nexts {
+		sort.Strings(nexts[dep])
+	}
 
 	return res
+}
+
+func lastDep(temp string) string {
+	return temp[len(temp)-3 : len(temp)-1]
 }
