2c278959dde92a0751be78dc7cc959ffb7b39896
aQua <aQua@aQuaYi.com>
2017-10-26 17:21:32 +0800

    332 index out of range

--- Algorithms/0332.reconstruct-itinerary/reconstruct-itinerary.go
+++ Algorithms/0332.reconstruct-itinerary/reconstruct-itinerary.go
@@ -1,5 +1,43 @@
 package Problem0332
 
+import "sort"
+
 func findItinerary(tickets [][]string) []string {
 
+	nexts := make(map[string][]string, len(tickets))
+
+	for _, t := range tickets {
+		nexts[t[0]] = append(nexts[t[0]], t[1])
+	}
+
+	for k := range tickets {
+		sort.Strings(tickets[k])
+	}
+
+	size := len(tickets) + 1
+	res := make([]string, 1, size)
+	res[0] = "JFK"
+
+	var dfs func(int) bool
+	dfs = func(idx int) bool {
+		if idx == size {
+			return true
+		}
+
+		size := len(nexts[res[idx-1]])
+		old := nexts[res[idx-1]]
+		var i int
+		for i = 0; i < size; i++ {
+			res[idx] = old[i]
+			nexts[res[idx-1]] = append(old[:i:i], old[i+1:]...)
+			if dfs(idx + 1) {
+				return true
+			}
+		}
+
+		return false
+	}
+	dfs(1)
+
+	return res
 }
