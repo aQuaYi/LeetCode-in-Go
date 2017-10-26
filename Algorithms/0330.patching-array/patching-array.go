e497f2547681bd043fb722459bff07ccdb61e154
aQua <aQua@aQuaYi.com>
2017-10-26 11:49:30 +0800

    330 fixing

--- Algorithms/0330.patching-array/patching-array.go
+++ Algorithms/0330.patching-array/patching-array.go
@@ -1,14 +1,25 @@
 package Problem0330
 
 func minPatches(nums []int, n int) int {
-	remain, count := log2(n), 0
+	var i int
+
+	remain, count := log2(n)+1, 0
 	isGetted := make([]bool, remain)
+
+	pow2 := make([]int, 1, remain)
+	pow2[0] = 1
+	for i = 1; i < remain; i++ {
+		pow2[i] = pow2[i-1] * 2
+	}
 
 	// 利用 nums 已有的元素，填充isGetted
 	var dfs func(int, int)
+	var idx int 
 	dfs = func(sum, idx int) {
 		if idx == len(nums) {
-			if 0 < sum && sum <= n && !isGetted[sum] {
+			if 0 < sum && sum <= n {
+				idx = log2(sum)
+				if !isGetted[idx] && pow2[idx] == sum  {
 				isGetted[sum] = true
 				remain--
 			}
