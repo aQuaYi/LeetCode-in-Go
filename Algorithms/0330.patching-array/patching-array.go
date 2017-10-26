68dc154acd99f4e684e957a48e7d8959a35d13f2
aQua <aQua@aQuaYi.com>
2017-10-26 10:01:15 +0800

    330 adding

--- Algorithms/0330.patching-array/patching-array.go
+++ Algorithms/0330.patching-array/patching-array.go
@@ -1,7 +1,52 @@
 package Problem0330
 
-func minPatches(nums []int, n int) int {
-	res := 1
+import "sort"
+import "fmt"
 
-	return res
+func minPatches(nums []int, n int) int {
+	count := n
+	isGetted := make([]bool, n+1)
+
+	// 利用 nums 已有的元素，填充isGetted
+	var dfs func(int, int)
+	dfs = func(sum, idx int) {
+		if idx == len(nums) {
+			if 0 < sum && sum <= n && !isGetted[sum] {
+				isGetted[sum] = true
+				count--
+			}
+			return
+		}
+		dfs(sum, idx+1)
+		dfs(sum+nums[idx], idx+1)
+	}
+
+	check := func(x int) {
+		var i, j int
+		for i = 0; i < len(nums) && nums[i] <= x; i++ {
+			sum := 0
+			for j = i; j < len(nums) && nums[j] <= x; j++ {
+				sum += nums[j]
+				if sum == x {
+					return
+				}
+				if sum > x {
+					break
+				}
+			}
+		}
+		nums = append(nums, x)
+		count++
+		sort.Ints(nums)
+	}
+
+	sort.Ints(nums)
+
+	var i int
+	for i = 1; i <= n; i++ {
+		check(i)
+	}
+
+	fmt.Println(nums)
+	return count
 }
