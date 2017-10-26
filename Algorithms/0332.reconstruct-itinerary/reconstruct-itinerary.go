86f9f1f04ab5cc20d936026e3ff4f663c686e2fa
aQua <aQua@aQuaYi.com>
2017-10-26 16:26:06 +0800

    332 pasue

--- Algorithms/0332.reconstruct-itinerary/reconstruct-itinerary.go
+++ Algorithms/0332.reconstruct-itinerary/reconstruct-itinerary.go
@@ -1,36 +1,5 @@
 package Problem0332
 
-import "strings"
-
 func findItinerary(tickets [][]string) []string {
-	res := string('Z' + 1)
 
-	var dfs func(string, [][]string)
-	dfs = func(departs string, tickets [][]string) {
-		if departs > res {
-			return
-		}
-
-		if len(tickets) == 0 && res > departs {
-			res = departs
-		}
-
-		for i, t := range tickets {
-			if lastDep(departs) == t[0] {
-				dfs(departs+" "+t[1], append(tickets[:i:i], tickets[i+1:]...))
-			}
-		}
-	}
-
-	for i, t := range tickets {
-		if t[0] == "JFK" {
-			dfs("JFK "+t[1], append(tickets[:i:i], tickets[i+1:]...))
-		}
-	}
-
-	return strings.Split(res, " ")
-}
-
-func lastDep(temp string) string {
-	return temp[len(temp)-3:]
 }
