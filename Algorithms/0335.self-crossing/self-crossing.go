dc515ddf228afb40da6fe6b6d334327a31138611
aQua <aQua@aQuaYi.com>
2017-10-27 10:59:20 +0800

    335 重构中

--- Algorithms/0335.self-crossing/self-crossing.go
+++ Algorithms/0335.self-crossing/self-crossing.go
@@ -3,21 +3,21 @@ package Problem0335
 func isSelfCrossing(x []int) bool {
 	size := len(x)
 
-	cross4Line := func(i int) bool {
+	is4th := func(i int) bool {
 		return i >= 3 && x[i] >= x[i-2] && x[i-1] <= x[i-3]
 	}
 
-	cross5Line := func(i int) bool {
+	is5th := func(i int) bool {
 		return i >= 4 && x[i-1] == x[i-3] && x[i]+x[i-4] >= x[i-2]
 	}
 
-	cross6Line := func(i int) bool {
+	is6th := func(i int) bool {
 		return i >= 5 && x[i-2] >= x[i-4] && x[i]+x[i-4] >= x[i-2] && x[i-1] <= x[i-3] && x[i-1]+x[i-5] >= x[i-3]
 	}
 
 	var i int
 	for i = 3; i < size; i++ {
-		if cross4Line(i) || cross5Line(i) || cross6Line(i) {
+		if is4th(i) || is5th(i) || is6th(i) {
 			return true
 		}
 	}
