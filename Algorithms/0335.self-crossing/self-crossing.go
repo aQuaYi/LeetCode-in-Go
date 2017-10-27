8a50181b61de2f8a25173329855b2b2fafdad46f
aQua <aQua@aQuaYi.com>
2017-10-27 11:05:19 +0800

    335 refactoring

--- Algorithms/0335.self-crossing/self-crossing.go
+++ Algorithms/0335.self-crossing/self-crossing.go
@@ -4,19 +4,19 @@ func isSelfCrossing(x []int) bool {
 	size := len(x)
 
 	is4th := func(i int) bool {
-		return i >= 3 && x[i] >= x[i-2] && x[i-1] <= x[i-3]
+		return i+3 < size && x[i+2] <= x[i] && x[i+3] >= x[i+1]
 	}
 
 	is5th := func(i int) bool {
-		return i >= 4 && x[i-1] == x[i-3] && x[i]+x[i-4] >= x[i-2]
+		return i+4 < size && x[i+1] == x[i+3] && x[i]+x[i+4] >= x[i+2]
 	}
 
 	is6th := func(i int) bool {
-		return i >= 5 && x[i-2] >= x[i-4] && x[i]+x[i-4] >= x[i-2] && x[i-1] <= x[i-3] && x[i-1]+x[i-5] >= x[i-3]
+		return i+5 < size && x[i+2] > x[i] && x[i+3] > x[i+1] && x[i]+x[i+4] >= x[i+2] && x[i+1]+x[i+5] >= x[i+3]
 	}
 
 	var i int
-	for i = 3; i < size; i++ {
+	for i = 0; i < size; i++ {
 		if is4th(i) || is5th(i) || is6th(i) {
 			return true
 		}
