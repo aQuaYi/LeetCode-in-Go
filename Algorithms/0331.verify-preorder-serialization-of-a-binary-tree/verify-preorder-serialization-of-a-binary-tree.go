0a3abfae7540df4f9962dfe75baa675e847283d7
aQua <aQua@aQuaYi.com>
2017-10-27 16:11:17 +0800

    331 finish

--- Algorithms/0331.verify-preorder-serialization-of-a-binary-tree/verify-preorder-serialization-of-a-binary-tree.go
+++ Algorithms/0331.verify-preorder-serialization-of-a-binary-tree/verify-preorder-serialization-of-a-binary-tree.go
@@ -5,37 +5,17 @@ import (
 )
 
 func isValidSerialization(preorder string) bool {
-	ss := strings.Split(preorder, ",")
-
-	if (len(ss)-3)%2 != 0 {
-		return false
-	}
-
-	return check(ss)
-}
-
-func check(ss []string) bool {
-	n := len(ss)
-
-	if n == 1 && ss[0] == "#" {
-		return true
-	}
-
-	temp := make([]string, 0, n)
-
-	var i = 0
-	for i < n {
-		if i+2 < n && ss[i] != "#" && ss[i+1] == "#" && ss[i+2] == "#" {
-			// ss[i] 的两个子节点都是 叶子 节点
-			// 把 ss[i] 变成 叶子 节点，删除其子节点
-			temp = append(temp, "#")
-			i += 3
-			continue
+	parts := strings.Split(preorder, ",")
+	s := make([]string, 0, len(parts))
+	for _, p := range parts {
+		for p == "#" && len(s) > 0 && s[len(s)-1] == "#" {
+			s = s[:len(s)-1]
+			if len(s) == 0 {
+				return false
+			}
+			s = s[:len(s)-1]
 		}
-
-		temp = append(temp, ss[i])
-		i++
+		s = append(s, p)
 	}
-
-	return len(temp) < n && check(temp)
+	return len(s) == 1 && s[0] == "#"
 }
