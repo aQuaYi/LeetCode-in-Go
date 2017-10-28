f50c669d9e8cbaffa7b36bc8913b9bbc8507690f
aQua <aQua@aQuaYi.com>
2017-10-28 17:28:01 +0800

    395 accepted.

--- Algorithms/0395.longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.go
+++ Algorithms/0395.longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.go
@@ -1,13 +1,24 @@
 package Problem0395
 
+import (
+	"strings"
+)
+
 func longestSubstring(s string, k int) int {
+	if len(s) < k {
+		return 0
+	}
+
+	// count 中，记录了每个字母出现的次数
 	count := make(map[byte]int, len(s))
+	// maxCount 出现最多字母的出现次数
 	maxCount := 0
 	for i := range s {
 		count[s[i]]++
 		maxCount = max(maxCount, count[s[i]])
 	}
 	if maxCount < k {
+		// 没有字母达到 k 次
 		return 0
 	}
 
@@ -22,17 +33,18 @@ func longestSubstring(s string, k int) int {
 		return len(s)
 	}
 
-	res := 0
-	for _, b := range lessLetters {
-		for i, bs := range s {
-			if b == bs {
-				res = max(res, longestSubstring(s[:i], k))
-				res = max(res, longestSubstring(s[i+1:], k))
-			}
-		}
+	i := 0
+	for !strings.Contains(lessLetters, string(s[i])) {
+		i++
+	}
+	j := i + 1
+	for j < len(s) && strings.Contains(lessLetters, string(s[j])) {
+		j++
 	}
 
-	return res
+	// s[i:j] 中所有的字母的个数都 <k
+	// 所以，以 s[i:j] 分割 s，在两边寻找 longestSubstring
+	return max(longestSubstring(s[:i], k), longestSubstring(s[j:], k))
 }
 
 func max(a, b int) int {
