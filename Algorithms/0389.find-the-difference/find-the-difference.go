1ac1e9dc82990799cd4bc8995e1d2b29102d72a1
aQua <aQua@aQuaYi.com>
2017-10-28 17:56:01 +0800

    389 finish

---
+++ Algorithms/0389.find-the-difference/find-the-difference.go
@@ -0,0 +1,19 @@
+package Problem0389
+
+func findTheDifference(s string, t string) byte {
+	rec := make([]int, 26)
+	for i := range s {
+		rec[s[i]-'a']--
+		rec[t[i]-'a']++
+	}
+	rec[t[len(t)-1]-'a']++
+
+	var i int
+	for i = 0; i < 26; i++ {
+		if rec[i] == 1 {
+			break
+		}
+	}
+
+	return byte('a' + i)
+}
---
+++ Algorithms/0389.find-the-difference/find-the-difference_test.go
@@ -0,0 +1,41 @@
+package Problem0389
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	s   string
+	t   string
+	ans byte
+}{
+
+	{
+		"abcd",
+		"abcde",
+		'e',
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_findTheDifference(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, findTheDifference(tc.s, tc.t), "输入:%v", tc)
+	}
+}
+
+func Benchmark_findTheDifference(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			findTheDifference(tc.s, tc.t)
+		}
+	}
+}
