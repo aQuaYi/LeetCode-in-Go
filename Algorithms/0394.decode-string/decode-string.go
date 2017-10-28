b49715b68287a2ca3a60e9eb8673fd0f0f7ed9e3
aQua <aQua@aQuaYi.com>
2017-10-28 16:17:44 +0800

    394 思路发生错误

--- Algorithms/0394.decode-string/decode-string.go
+++ Algorithms/0394.decode-string/decode-string.go
@@ -1,7 +1,39 @@
 package Problem0394
 
 func decodeString(s string) string {
-	res :=
+	var i, beg, end int
+	for i = 0; i < len(s); i++ {
+		if s[i] == '[' {
+			beg = i
+			break
+		}
+	}
 
+	if i == len(s) {
+		return s
+	}
+
+	count := 1
+
+	for end = i + 1; end < len(s); end++ {
+		if s[end] == '[' {
+			count++
+		} else if s[end] == ']' {
+			count--
+		}
+
+		if count == 0 {
+			break
+		}
+	}
+
+	return ""
+}
+
+func times(n int, s string) string {
+	res := ""
+	for i := 0; i < n; i++ {
+		res += s
+	}
 	return res
 }
