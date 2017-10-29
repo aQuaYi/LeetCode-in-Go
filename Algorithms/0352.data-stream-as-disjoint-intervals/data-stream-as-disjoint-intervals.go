d51701bcfea523a11c723690eb30376ae2aa8040
aQua <aQua@aQuaYi.com>
2017-10-29 12:13:12 +0800

    352 index out of range

--- Algorithms/0352.data-stream-as-disjoint-intervals/data-stream-as-disjoint-intervals.go
+++ Algorithms/0352.data-stream-as-disjoint-intervals/data-stream-as-disjoint-intervals.go
@@ -1,5 +1,9 @@
 package Problem0352
 
+import (
+	"github.com/aQuaYi/LeetCode-in-Go/kit"
+)
+
 /**
  * Definition for an interval.
  * type Interval struct {
@@ -7,20 +11,64 @@ package Problem0352
  *	   End   int
  * }
  */
+type Interval = kit.Interval
+
 type SummaryRanges struct {
+	is []Interval
 }
 
 /** Initialize your data structure here. */
 func Constructor() SummaryRanges {
-
+	return SummaryRanges{}
 }
 
 func (sr *SummaryRanges) Addnum(val int) {
+	if sr.is == nil {
+		sr.is = []Interval{
+			Interval{
+				Start: val,
+				End:   val,
+			},
+		}
+		return
+	}
 
+lo,hi:=0,len(sr.is) -1
+for lo<= hi {
+	mid := lo+(hi-lo)>>1
+	if sr.is[mid].Start <= val && val <=   sr.is[mid].End {
+		return 
+	}else 	if val < sr.is[mid].Start {
+		hi= mid-1
+	}else{
+		lo= mid+1 
+	}
+}
+
+if sr.is[lo-1].End+1< val && val < sr.is[lo].Start-1 {
+	sr.is= append(sr.is, Interval{})
+	copy(sr.is[lo+1:],sr.is[lo:])
+	sr.is[lo] = Interval{Start:val,End:val }
+	return
+}
+
+if sr.is[lo-1].End== val-1 &&  val +1 == sr.is[lo].Start {
+	sr.is[lo-1].End = sr.is[lo].End
+	n := len(sr.is)
+	copy(sr.is[lo:],sr.is[lo+1:])
+	sr.is = sr.is[:n-1]
+	return
+}
+
+if sr.is[lo-1].End == val-1 {
+	sr.is[lo-1].End++
+}else {
+	sr.is[lo].Start-- 
+}
 }
 
 func (sr *SummaryRanges) Getintervals() []Interval {
-
+	return sr.is
 }
 
 /**
--- Algorithms/0352.data-stream-as-disjoint-intervals/data-stream-as-disjoint-intervals_test.go
+++ Algorithms/0352.data-stream-as-disjoint-intervals/data-stream-as-disjoint-intervals_test.go
@@ -1,36 +1,49 @@
 package Problem0352
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-// tcs is testcase slice
-var tcs = []struct {
-	
-	ans  SummaryRanges 
-}{
-
-
-	
-	// 可以有多个 testcase
-}
-
 func Test_Constructor(t *testing.T) {
 	ast := assert.New(t)
-	
-	for _, tc := range tcs {
-		fmt.Printf("~~%v~~\n", tc)
-		ast.Equal(tc.ans, Constructor(tc.), "输入:%v", tc)
-	}
-}
 
-func Benchmark_Constructor(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		for _, tc := range tcs {
-			Constructor(tc.)
-		}
-	}
+	sr := Constructor()
+
+	sr.Addnum(1)
+	ast.Equal([]Interval{Interval{Start: 1, End: 1}}, sr.Getintervals())
+
+	sr.Addnum(3)
+	ast.Equal([]Interval{
+		Interval{Start: 1, End: 1},
+		Interval{Start: 3, End: 3},
+	},
+		sr.Getintervals(),
+	)
+
+	sr.Addnum(7)
+	ast.Equal([]Interval{
+		Interval{Start: 1, End: 1},
+		Interval{Start: 3, End: 3},
+		Interval{Start: 7, End: 7},
+	},
+		sr.Getintervals(),
+	)
+
+	sr.Addnum(2)
+	ast.Equal([]Interval{
+		Interval{Start: 1, End: 3},
+		Interval{Start: 7, End: 7},
+	},
+		sr.Getintervals(),
+	)
+
+	sr.Addnum(6)
+	ast.Equal([]Interval{
+		Interval{Start: 1, End: 3},
+		Interval{Start: 6, End: 7},
+	},
+		sr.Getintervals(),
+	)
 }
