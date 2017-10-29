1602c43c7b8d701c58791452daf2e651e37f1650
aQua <aQua@aQuaYi.com>
2017-10-29 13:46:38 +0800

    393 finish

---
+++ Algorithms/0393.utf-8-validation/utf-8-validation.go
@@ -0,0 +1,30 @@
+package Problem0393
+
+func validUtf8(data []int) bool {
+	// cnt 是还需检查的 byte 的个数
+	cnt := 0
+	var d int
+	for _, d = range data {
+		if cnt == 0 {
+			switch {
+			case d>>3 == 30: //0b11110
+				cnt = 3
+			case d>>4 == 14: //0b1110
+				cnt = 2
+			case d>>5 == 6: //0b110
+				cnt = 1
+			case d>>7 > 0:
+				// data[0] 和 data[len(data)-1] 都会到此处来检查
+				return false
+			}
+		} else {
+			// 非首尾的 byte 必须以 0b10 开头
+			if d>>6 != 2 { //0b10
+				return false
+			}
+			cnt--
+		}
+	}
+
+	return 0 == cnt
+}
---
+++ Algorithms/0393.utf-8-validation/utf-8-validation_test.go
@@ -0,0 +1,54 @@
+package Problem0393
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	data []int
+	ans  bool
+}{
+
+	{
+		[]int{241, 130, 130, 130, 1},
+		true,
+	},
+
+	{
+		[]int{197, 130, 1},
+		true,
+	},
+
+	{
+		[]int{129},
+		false,
+	},
+
+	{
+		[]int{235, 140, 4},
+		false,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_validUtf8(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, validUtf8(tc.data), "输入:%v", tc)
+	}
+}
+
+func Benchmark_validUtf8(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			validUtf8(tc.data)
+		}
+	}
+}
