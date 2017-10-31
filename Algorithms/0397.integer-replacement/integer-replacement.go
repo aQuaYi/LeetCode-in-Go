f0214f2a18b2a8bd2da2a8de4d2fcee01f187ed5
aQua <aQua@aQuaYi.com>
2017-10-31 14:32:54 +0800

    397 finish

--- Algorithms/0397.integer-replacement/integer-replacement.go
+++ Algorithms/0397.integer-replacement/integer-replacement.go
@@ -1,15 +1,27 @@
 package Problem0397
 
 func integerReplacement(n int) int {
-	if n == 1 {
-		return 0
+	// rec[i] == integerReplacement(i)
+	rec := make(map[int]int)
+	rec[1] = 0
+
+	var ir func(int) int
+	ir = func(i int) int {
+		// 已经计算过的 i，可以直接从 rec 中读取出来
+		if n, ok := rec[i]; ok {
+			return n
+		}
+
+		if i%2 == 0 {
+			rec[i] = ir(i/2) + 1
+			return rec[i]
+		}
+
+		rec[i] = min(ir(i+1), ir(i-1)) + 1
+		return rec[i]
 	}
 
-	if n%2 == 0 {
-		return 1 + integerReplacement(n/2)
-	}
-
-	return 1 + min(integerReplacement(n+1), integerReplacement(n-1))
+	return ir(n)
 }
 
 func min(a, b int) int {
