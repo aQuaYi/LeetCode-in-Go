65f9a29f4533c1c2ab739ef8aedcaa47b1e5b4ef
aQua <aQua@aQuaYi.com>
2017-11-01 15:48:12 +0800

    390 accepted. 26ms

--- Algorithms/0390.elimination-game/elimination-game.go
+++ Algorithms/0390.elimination-game/elimination-game.go
@@ -1,7 +1,23 @@
 package Problem0390
 
 func lastRemaining(n int) int {
-	res :=
+	isLeftStart := true
+	// remain 是剩余数字的个数
+	remain := n
 
-	return res
+	// 剩余数字中，最小的数
+	min := 1
+	// min + step == 第二小的数字
+	step := 1
+	for remain > 1 {
+		if isLeftStart || remain%2 == 1 {
+			min += step
+		}
+
+		isLeftStart = !isLeftStart
+		step *= 2
+		remain /= 2
+	}
+
+	return min
 }
--- Algorithms/0390.elimination-game/elimination-game_test.go
+++ Algorithms/0390.elimination-game/elimination-game_test.go
@@ -9,18 +9,20 @@ import (
 
 // tcs is testcase slice
 var tcs = []struct {
-	n int
-	ans  int 
+	n   int
+	ans int
 }{
 
+	{9, 6},
 
-	
+	{123456789, 56614230},
+
 	// 可以有多个 testcase
 }
 
 func Test_lastRemaining(t *testing.T) {
 	ast := assert.New(t)
-	
+
 	for _, tc := range tcs {
 		fmt.Printf("~~%v~~\n", tc)
 		ast.Equal(tc.ans, lastRemaining(tc.n), "输入:%v", tc)
