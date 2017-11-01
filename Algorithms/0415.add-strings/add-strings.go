21cf7c3ef79a6959eabe21a2462a48f2d8470934
aQua <aQua@aQuaYi.com>
2017-11-01 09:32:21 +0800

    415 finish

--- Algorithms/0415.add-strings/add-strings.go
+++ Algorithms/0415.add-strings/add-strings.go
@@ -1,7 +1,41 @@
 package Problem0415
 
-func addStrings(num1 string, num2 string) string {
-	res :=
+func addStrings(s1, s2 string) string {
+	// 确保 n1 <= n2
+	if len(s1) > len(s2) {
+		s1, s2 = s2, s1
+	}
 
-	return res
+	n1, n2 := len(s1), len(s2)
+	a1, a2 := []byte(s1), []byte(s2)
+
+	carry := byte(0)
+
+	// buf 保存 []byte 格式的答案
+	buf := make([]byte, n2+1)
+	buf[0] = '1'
+
+	i := 1
+	for i <= n2 {
+		// a1 和 a2 相加
+		if i <= n1 {
+			buf[n2+1-i] = a1[n1-i] - '0'
+		}
+		buf[n2+1-i] += a2[n2-i] + carry
+
+		// 处理进位问题
+		if buf[n2+1-i] > '9' {
+			buf[n2+1-i] -= 10
+			carry = byte(1)
+		} else {
+			carry = byte(0)
+		}
+
+		i++
+	}
+
+	if carry == 1 {
+		return string(buf)
+	}
+	return string(buf[1:])
 }
