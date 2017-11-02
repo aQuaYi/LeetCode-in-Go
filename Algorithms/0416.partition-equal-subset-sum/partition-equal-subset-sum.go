84cf484b7fcdcc22535632928317ded2d110fdd2
aQua <aQua@aQuaYi.com>
2017-11-02 21:18:49 +0800

    416 修改 i 和 j 为 for 的局部变量

--- Algorithms/0416.partition-equal-subset-sum/partition-equal-subset-sum.go
+++ Algorithms/0416.partition-equal-subset-sum/partition-equal-subset-sum.go
@@ -14,26 +14,24 @@ func canPartition(nums []int) bool {
 	sum = sum >> 1
 	n := len(nums)
 
-	var i, j int
-
 	// dp[i][j] 表示 nums[:i] 中的元素，可以找出一些，他们的和为 j
 	dp := make([][]bool, n+1)
-	for i = range dp {
+	for i := range dp {
 		dp[i] = make([]bool, sum+1)
 	}
 
-	for i = 0; i < n+1; i++ {
+	for i := 0; i < n+1; i++ {
 		// 从任意多个元素中，挑选 0 个元素出来，其和是 0
 		dp[i][0] = true
 	}
 
-	for j = 1; j < sum+1; j++ {
+	for j := 1; j < sum+1; j++ {
 		// 从包含 0 个元素的 nums 中，挑不出来元素，使得其和为 j
 		dp[0][j] = false
 	}
 
-	for i = 1; i < n+1; i++ {
-		for j = 1; j < sum+1; j++ {
+	for i := 1; i < n+1; i++ {
+		for j := 1; j < sum+1; j++ {
 			dp[i][j] = dp[i-1][j]
 			if j >= nums[i-1] {
 				// nums[:i] 比 nums[:i-1] 多了 nums[i-1]，所以
