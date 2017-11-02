abc754001047f933e38632f37baf28a468ad352b
aQua <aQua@aQuaYi.com>
2017-11-02 21:27:49 +0800

    375 缩减了变量的作用范围

--- Algorithms/0375.guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.go
+++ Algorithms/0375.guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.go
@@ -8,14 +8,13 @@ func getMoneyAmount(n int) int {
 		dp[i] = make([]int, n+1)
 	}
 
-	var i, j, k int
-	for j = 2; j <= n; j++ {
-		for i = j - 1; 0 < i; i-- {
+	for j := 2; j <= n; j++ {
+		for i := j - 1; 0 < i; i-- {
 			// 为了确保可以猜出 i<=x<=j 中的 x
 			// 第一次，我们可以猜 x 为，i，i+1，...,j-1
 			// 所有这些可能性中的最小值就是 dp[i][j] 的值
 			dp[i][j] = i + dp[i+1][j]
-			for k = i + 1; k < j; k++ {
+			for k := i + 1; k < j; k++ {
 				// k+max(dp[i][k-1], dp[k+1][j])) 猜 x 为 k 所花费的最小费用
 				dp[i][j] = min(dp[i][j], k+max(dp[i][k-1], dp[k+1][j]))
 			}
