7d5b4c98604b82c42725060e308cd56b205cf3c8
aQua <aQua@aQuaYi.com>
2017-11-02 21:26:01 +0800

    378 缩减了变量的作用范围

--- Algorithms/0378.kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.go
+++ Algorithms/0378.kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.go
@@ -6,12 +6,11 @@ func kthSmallest(mat [][]int, k int) int {
 
 	lo, hi := mat[0][0], mat[n-1][n-1]
 
-	var mid, count, i, j int
 	for lo < hi {
-		mid = lo + (hi-lo)>>1
-		count = 0
-		j = n - 1
-		for i = 0; i < n; i++ {
+		mid := lo + (hi-lo)>>1
+		count := 0
+		j := n - 1
+		for i := 0; i < n; i++ {
 			for j >= 0 && mat[i][j] > mid {
 				j--
 			}
