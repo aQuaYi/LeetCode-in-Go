11df3f7575d4e6d53969d168138d35a7d3cbb716
aQua <aQua@aQuaYi.com>
2017-11-03 17:07:06 +0800

    398 accepted. 295ms > 232ms

---
+++ Algorithms/0398.random-pick-index/random-pick-index.go
@@ -0,0 +1,29 @@
+package Problem0398
+
+import "math/rand"
+
+// Solution 是答案所需的数据结构
+type Solution struct {
+	m map[int][]int
+}
+
+// Constructor 构建了 Solution
+func Constructor(nums []int) Solution {
+	m := make(map[int][]int, len(nums))
+	for i, v := range nums {
+		m[v] = append(m[v], i)
+	}
+
+	return Solution{m: m}
+}
+
+// Pick 随机出现 target 在 nums 中的索引号
+func (s *Solution) Pick(target int) int {
+	return s.m[target][rand.Intn(len(s.m[target]))]
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * obj := Constructor(nums);
+ * param_1 := obj.Pick(target);
+ */
---
+++ Algorithms/0398.random-pick-index/random-pick-index_test.go
@@ -0,0 +1,28 @@
+package Problem0398
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Constructor(t *testing.T) {
+	ast := assert.New(t)
+
+	nums := []int{1, 2, 2, 3, 3, 3}
+	// all index of 1
+	is1 := []int{0}
+	// all index of 2
+	is2 := []int{1, 2}
+	// all index of 3
+	is3 := []int{3, 4, 5}
+
+	s := Constructor(nums)
+
+	ast.Contains(is1, s.Pick(1), "无法正确第返回 1 的索引号")
+
+	ast.Contains(is2, s.Pick(2), "无法正确第返回 2 的索引号")
+
+	ast.Contains(is3, s.Pick(3), "无法正确第返回 3 的索引号")
+
+}
