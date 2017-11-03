63710b25e1c5eca5ebd9b19608f0467a6b45d0be
aQua <aQua@aQuaYi.com>
2017-11-03 16:48:47 +0800

    399 finish

--- Algorithms/0399.evaluate-division/evaluate-division.go
+++ Algorithms/0399.evaluate-division/evaluate-division.go
@@ -6,20 +6,22 @@ func calcEquation(equations [][]string, values []float64, queries [][]string) []
 	for i, e := range equations {
 		a, b := e[0], e[1]
 		v := values[i]
+		// 添加 a / b 的记录
 		if _, ok := m[a]; !ok {
 			m[a] = make(map[string]float64)
 		}
 		m[a][b] = 1.0 / v
+		// 添加 b / a 的记录
 		if _, ok := m[b]; !ok {
 			m[b] = make(map[string]float64)
 		}
 		m[b][a] = v
 	}
 
-	// 逐个查询 queries 的结果
-	res := make([]float64, 0, len(queries))
-	for _, q := range queries {
-		res = append(res, query(m, q[0], q[1]))
+	// 逐个搜索 queries 的结果
+	res := make([]float64, len(queries))
+	for i, q := range queries {
+		res[i] = bfs(m, q[0], q[1])
 	}
 
 	return res
@@ -30,7 +32,7 @@ type entry struct {
 	f float64
 }
 
-func query(m map[string]map[string]float64, a, b string) float64 {
+func bfs(m map[string]map[string]float64, a, b string) float64 {
 	_, ok := m[a]
 	if !ok {
 		return -1.0
@@ -44,22 +46,27 @@ func query(m map[string]map[string]float64, a, b string) float64 {
 		return 1.0
 	}
 
-	visited := make(map[string]bool)
+	isVisited := make(map[string]bool)
 	queue := []entry{{a, 1.0}}
 
 	for len(queue) > 0 {
-		top := queue[0]
+		e := queue[0]
 		queue = queue[1:]
-		if top.s == b {
-			return 1.0 / top.f
+		if e.s == b {
+			// 找到了 b
+			return 1.0 / e.f
 		}
-		if visited[top.s] {
+
+		if isVisited[e.s] {
 			continue
 		}
-		visited[top.s] = true
-		for k, v := range m[top.s] {
-			queue = append(queue, entry{k, v * top.f})
+
+		isVisited[e.s] = true
+
+		for k, v := range m[e.s] {
+			queue = append(queue, entry{k, v * e.f})
 		}
 	}
+	//没有找到 b
 	return -1.0
 }
