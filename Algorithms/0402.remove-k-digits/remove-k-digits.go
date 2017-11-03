e2e142be98c37c0ec205e7800ac833a7c4912a99
aQua <aQua@aQuaYi.com>
2017-11-03 13:20:52 +0800

    402 不用递归的解法

--- Algorithms/0402.remove-k-digits/remove-k-digits.go
+++ Algorithms/0402.remove-k-digits/remove-k-digits.go
@@ -1,46 +1,26 @@
 package Problem0402
 
-import (
-	"strings"
-)
-
-func minNum(s string) (at int) {
-	for i := 1; i < len(s); i++ {
-		if s[i] < s[at] {
-			at = i
-		}
-	}
-	return
-}
-
 func removeKdigits(num string, k int) string {
-	var prefix string
-	for k > 0 {
-		if len(num) <= k {
-			num = ""
-			break
-		}
-		pos := strings.IndexByte(num, '0')
-		if pos > 0 && pos <= k {
-			k -= pos
-			num = num[pos:]
-			num1 := strings.TrimLeft(num, "0")
-			if prefix != "" {
-				prefix += num[:len(num)-len(num1)]
-			}
-			num = num1
-			continue
-		}
-
-		pos = minNum(num[:k+1])
-		k -= pos
-		prefix += string(num[pos])
-		num = num[pos+1:]
-
-	}
-	num = prefix + num
-	if len(num) == 0 {
+	if len(num) <= k {
 		return "0"
 	}
-	return num
+	res := make([]byte, len(num)-k)
+	var i, j int
+	for 0 < k && i+1 < len(num) {
+		if num[i] <= num[i+1] {
+			res[j] = num[i]
+			i++
+			j++
+			continue
+		}
+		i++
+		k--
+	}
+	copy(res[j:], num[i:])
+
+	i = 0
+	for i < len(res)-1 && res[i] == '0' {
+		i++
+	}
+	return string(res[i:])
 }
