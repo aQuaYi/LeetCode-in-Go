780613f0fd5effad6d58d99b76337927062910b6
aQua <aQua@aQuaYi.com>
2017-11-04 12:13:28 +0800

    432 我把问题想复杂了

--- Algorithms/0432.all-oone-data-structure/all-oone-data-structure.go
+++ Algorithms/0432.all-oone-data-structure/all-oone-data-structure.go
@@ -1,35 +1,85 @@
 package Problem0432
 
+import "math/rand"
+
+type item struct {
+	key   string
+	value int
+	index int
+}
+
 // AllOne 是解题所需的结构
 type AllOne struct {
+	m                  map[string]*item
+	max, min           int
+	maxItems, minItems []*item
 }
 
-/** Initialize your data structure here. */
+// Constructor initialize your data structure here.
 func Constructor() AllOne {
-
-	return AllOne{}
+	return AllOne{m: make(map[string]*item),
+		max: -1 << 63,
+		min: 1<<63 - 1,
+	}
 }
 
-// Inc inserts a new key <Key> with value 1. Or increments an existing key by 1. */
-func (this *AllOne) Inc(key string) {
+// Inc inserts a new key <Key> with value 1. Or increments an existing key by 1.
+func (a *AllOne) Inc(key string) {
+	if _, ok := a.m[key]; ok {
+		a.m[key].value++
+	} else {
+		a.m[key] = &item{key: key, value: 1}
+	}
 
+	if a.m[key].value < a.min {
+		a.min = a.m[key].value
+		a.minItems = []*item{a.m[key]}
+		a.m[key].index = 0
+	} else if a.m[key].value == a.min {
+		a.minItems = append(a.minItems, a.m[key])
+		a.m[key].index = len(a.minItems) - 1
+	} else if a.max == a.m[key].value {
+		a.maxItems = append(a.maxItems, a.m[key])
+		a.m[key].index = len(a.maxItems) - 1
+	} else if a.max < a.m[key].value {
+		a.max = a.m[key].value
+		a.maxItems = []*item{a.m[key]}
+		a.m[key].index = 0
+	}
 }
 
-// Dec decrements an existing key by 1. If Key's value is 1, remove it from the data structure. */
-func (this *AllOne) Dec(key string) {
+// Dec decrements an existing key by 1. If Key's value is 1, remove it from the data structure.
+func (a *AllOne) Dec(key string) {
+	if _, ok := a.m[key]; !ok {
+		return
+	}
 
+	a.m[key].value--
+
+	if a.m[key].value < a.min {
+		a.min = a.m[key].value
+		a.minItems = []*item{a.m[key]}
+		a.m[key].index = 0
+	} else if a.min == a.m[key].value {
+		a.minItems = append(a.minItems, a.m[key])
+		a.m[key].index = len(a.minItems) - 1
+	}
 }
 
-// GetMaxKey returns one of the keys with maximal value. */
-func (this *AllOne) GetMaxKey() string {
-
-	return ""
+// GetMaxKey returns one of the keys with maximal value.
+func (a *AllOne) GetMaxKey() string {
+	if len(a.maxItems) == 0 {
+		return ""
+	}
+	return a.maxItems[rand.Intn(len(a.maxItems))].key
 }
 
-// GetMinKey returns one of the keys with Minimal value. */
-func (this *AllOne) GetMinKey() string {
-
-	return ""
+// GetMinKey returns one of the keys with Minimal value.
+func (a *AllOne) GetMinKey() string {
+	if len(a.minItems) == 0 {
+		return ""
+	}
+	return a.minItems[rand.Intn(len(a.minItems))].key
 }
 
 /**
--- Algorithms/0432.all-oone-data-structure/all-oone-data-structure_test.go
+++ Algorithms/0432.all-oone-data-structure/all-oone-data-structure_test.go
@@ -1,36 +1,44 @@
 package Problem0432
 
 import (
-	"fmt"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-// tcs is testcase slice
-var tcs = []struct {
-	
-	ans  AllOne 
-}{
-
-
-	
-	// 可以有多个 testcase
-}
-
-func Test_Constructor(t *testing.T) {
+func Test_AllOne(t *testing.T) {
 	ast := assert.New(t)
-	
-	for _, tc := range tcs {
-		fmt.Printf("~~%v~~\n", tc)
-		ast.Equal(tc.ans, Constructor(tc.), "输入:%v", tc)
-	}
-}
 
-func Benchmark_Constructor(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		for _, tc := range tcs {
-			Constructor(tc.)
+	a := Constructor()
+
+	ast.Equal("", a.GetMaxKey())
+	ast.Equal("", a.GetMinKey())
+
+	maxKeys := []string{"7", "8", "9"}
+	minKeys := []string{"1", "2", "3"}
+
+	for i := 0; i < 3; i++ {
+		for j := 1; j < 10; j++ {
+			a.Inc(strconv.Itoa(j))
 		}
 	}
+
+	for _, key := range maxKeys {
+		a.Inc(key)
+	}
+
+	for _, key := range minKeys {
+		a.Dec(key)
+	}
+
+	ast.Contains(maxKeys, a.GetMaxKey(), "无法获取正确的 max key")
+
+	ast.Contains(minKeys, a.GetMinKey(), "无法获取正确的 min key")
+
+	a.Inc("9")
+	ast.Equal("9", a.GetMaxKey(), "无法获取 唯一的 max key")
+
+	a.Dec("1")
+	ast.Equal("1", a.GetMinKey(), "无法获取 唯一的 min key")
 }
