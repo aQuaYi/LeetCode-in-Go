03589a519a9328ad772fe75f091d54a001b9c6d0
aQua <aQua@aQuaYi.com>
2017-11-04 18:20:25 +0800

    432 决定使用优先队列来解决问题

--- Algorithms/0432.all-oone-data-structure/all-oone-data-structure.go
+++ Algorithms/0432.all-oone-data-structure/all-oone-data-structure.go
@@ -1,10 +1,12 @@
 package Problem0432
 
+import "container/heap"
+
 // AllOne 是解题所需的结构
 type AllOne struct {
-	m              map[string]int
-	max, min       int
-	maxKey, minKey string
+	m   map[string]*item
+	max maxPQ
+	min minPQ
 }
 
 // Constructor initialize your data structure here.
@@ -65,3 +67,59 @@ func (a *AllOne) GetMinKey() string {
  * param_3 := obj.GetMaxKey();
  * param_4 := obj.GetMinKey();
  */
+
+// item 是 priorityQueue 中的元素
+type item struct {
+	key   string
+	value int
+	// index 是 item 在 heap 中的索引号
+	// item 加入 Priority Queue 后， Priority 会变化时，很有用
+	// 如果 item.priority 一直不变的话，可以删除 index
+	index int
+}
+
+// priorityQueue implements heap.Interface and holds items.
+type minPQ []*item
+
+func (pq minPQ) Len() int { return len(pq) }
+
+func (pq minPQ) Less(i, j int) bool {
+	return pq[i].value < pq[j].value
+}
+
+func (pq minPQ) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+// Push 往 pq 中放 item
+func (pq *minPQ) Push(x interface{}) {
+	n := len(*pq)
+	item := x.(*item)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+// Pop 从 pq 中取出最优先的 item
+func (pq *minPQ) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	item.index = -1 // for safety
+	*pq = old[0 : n-1]
+	return item
+}
+
+// update modifies the priority and value of an item in the queue.
+func (pq *minPQ) update(item *item, value string, priority int) {
+	item.key = value
+	item.value = priority
+	heap.Fix(pq, item.index)
+}
+
+type maxPQ minPQ
+
+func (pq maxPQ) Less(i, j int) bool {
+	return pq[i].value > pq[i].value
+}
