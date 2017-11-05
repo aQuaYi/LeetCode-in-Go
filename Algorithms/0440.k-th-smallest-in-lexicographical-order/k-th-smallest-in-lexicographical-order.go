d6ad91f66f79d43a347f9268a17e39e77aa6dd1c
aQua <aQua@aQuaYi.com>
2017-11-05 17:49:11 +0800

    440 wrong answer

--- Algorithms/0440.k-th-smallest-in-lexicographical-order/k-th-smallest-in-lexicographical-order.go
+++ Algorithms/0440.k-th-smallest-in-lexicographical-order/k-th-smallest-in-lexicographical-order.go
@@ -1,7 +1,36 @@
 package Problem0440
 
 func findKthNumber(n int, k int) int {
-	res := 1
-
+	var res int
+	for head := 1; head <= 9; head++ {
+		for digits := 1; digits <= 10; digits++ {
+			min, max, count := get(head, digits)
+			if n < min {
+				break
+			}
+			if n <= max {
+				if k <= n-min+1 {
+					return min + k - 1
+				}
+				k -= n - min + 1
+				break
+			}
+			if k <= count {
+				return min + k - 1
+			}
+			k -= count
+		}
+	}
 	return res
+}
+func get(head, digits int) (min, max, count int) {
+	base := 1
+	for i := 1; i < digits; i++ {
+		base *= 10
+	}
+
+	min = head * base
+	max = min + base - 1
+	count = base
+	return
 }
