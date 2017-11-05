d15cf99119d91a9b3afbea0f6e412469ce453971
aQua <aQua@aQuaYi.com>
2017-11-05 18:29:04 +0800

    440 感觉这个答案，复杂了一点

--- Algorithms/0440.k-th-smallest-in-lexicographical-order/k-th-smallest-in-lexicographical-order.go
+++ Algorithms/0440.k-th-smallest-in-lexicographical-order/k-th-smallest-in-lexicographical-order.go
@@ -1,36 +1,35 @@
 package Problem0440
 
 func findKthNumber(n int, k int) int {
-	var res int
-	for head := 1; head <= 9; head++ {
-		for digits := 1; digits <= 10; digits++ {
-			min, max, count := get(head, digits)
-			if n < min {
-				break
-			}
-			if n <= max {
-				if k <= n-min+1 {
-					return min + k - 1
-				}
-				k -= n - min + 1
-				break
-			}
-			if k <= count {
-				return min + k - 1
-			}
-			k -= count
+	curr := 1
+	k--
+	for k > 0 {
+		steps := calSteps(n, curr, curr+1)
+		if steps <= k {
+			curr++
+			k -= steps
+		} else {
+			curr *= 10
+			k--
 		}
 	}
-	return res
+	return curr
 }
-func get(head, digits int) (min, max, count int) {
-	base := 1
-	for i := 1; i < digits; i++ {
-		base *= 10
+
+func calSteps(n, n1, n2 int) int {
+	steps := 0
+	for n1 <= n {
+		steps += min(n+1, n2) - n1
+		n1 *= 10
+		n2 *= 10
 	}
 
-	min = head * base
-	max = min + base - 1
-	count = base
-	return
+	return steps
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
