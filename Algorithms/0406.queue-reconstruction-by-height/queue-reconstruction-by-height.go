ffc6a54e9e7cf6ae039577b2bea8ba70a6d12e11
aQua <aQua@aQuaYi.com>
2017-11-06 15:52:21 +0800

    406 accepted. 95ms > 59ms

--- Algorithms/0406.queue-reconstruction-by-height/queue-reconstruction-by-height.go
+++ Algorithms/0406.queue-reconstruction-by-height/queue-reconstruction-by-height.go
@@ -1,7 +1,38 @@
 package Problem0406
 
+import "sort"
+
 func reconstructQueue(people [][]int) [][]int {
-	res :=
+	res := make([][]int, 0, len(people))
+
+	sort.Sort(persons(people))
+	insert := func(idx int, person []int) {
+		res = append(res, person)
+		if len(res)-1 == idx {
+			return
+		}
+
+		copy(res[idx+1:], res[idx:])
+		res[idx] = person
+	}
+
+	for _, p := range people {
+		insert(p[1], p)
+	}
 
 	return res
 }
+
+// persons 实现了 sort.Interface 接口
+type persons [][]int
+
+func (p persons) Len() int { return len(p) }
+
+func (p persons) Less(i, j int) bool {
+	if p[i][0] == p[j][0] {
+		return p[i][1] < p[j][1]
+	}
+	return p[i][0] > p[j][0]
+}
+
+func (p persons) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
