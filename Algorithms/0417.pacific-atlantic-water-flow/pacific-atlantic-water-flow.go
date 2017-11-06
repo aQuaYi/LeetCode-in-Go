ecfd4ac9674739a01bc03f223f458f625bda9409
aQua <aQua@aQuaYi.com>
2017-11-06 11:38:30 +0800

    417 added

---
+++ Algorithms/0417.pacific-atlantic-water-flow/pacific-atlantic-water-flow.go
@@ -0,0 +1,15 @@
+package Problem0417
+
+func pacificAtlantic(matrix [][]int) [][]int {
+	res := [][]int{
+		{0, 4},
+		{1, 3},
+		{1, 4},
+		{2, 2},
+		{3, 0},
+		{3, 1},
+		{4, 0},
+	}
+
+	return res
+}
---
+++ Algorithms/0417.pacific-atlantic-water-flow/pacific-atlantic-water-flow_test.go
@@ -0,0 +1,53 @@
+package Problem0417
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	matrix [][]int
+	ans    [][]int
+}{
+
+	{
+		[][]int{
+			{1, 2, 2, 3, 5},
+			{3, 2, 3, 4, 4},
+			{2, 4, 5, 3, 1},
+			{6, 7, 1, 4, 5},
+			{5, 1, 1, 2, 4},
+		},
+		[][]int{
+			{0, 4},
+			{1, 3},
+			{1, 4},
+			{2, 2},
+			{3, 0},
+			{3, 1},
+			{4, 0},
+		},
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_pacificAtlantic(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, pacificAtlantic(tc.matrix), "输入:%v", tc)
+	}
+}
+
+func Benchmark_pacificAtlantic(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			pacificAtlantic(tc.matrix)
+		}
+	}
+}
