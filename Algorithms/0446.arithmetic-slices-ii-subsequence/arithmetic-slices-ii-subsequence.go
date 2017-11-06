5e756bcfb7fb36b1d1bd2e8d5c3fa21a88299297
aQua <aQua@aQuaYi.com>
2017-11-06 11:30:44 +0800

    446 finish

--- Algorithms/0446.arithmetic-slices-ii-subsequence/arithmetic-slices-ii-subsequence.go
+++ Algorithms/0446.arithmetic-slices-ii-subsequence/arithmetic-slices-ii-subsequence.go
@@ -1,31 +1,40 @@
 package Problem0446
 
 func numberOfArithmeticSlices(a []int) int {
-	size := len(a)
-	if size < 3 {
+	n := len(a)
+	if n < 3 {
 		return 0
 	}
 
+	min, max := -1<<31, 1<<31-1
+
+	// dp[j][d] == x 表示
+	// 如果存在 a[i] - a[j] == d, j < i < n, 那么, {a[:j+1]..., a[i]} 中
+	// 可以构成形如 [..., a[j], a[i]] 的差值为 d 的等差数组的个数
+	dp := make([]map[int]int, n)
+
 	res := 0
+	for i := 1; i < n; i++ {
+		dp[i] = make(map[int]int)
+		for j := 0; j < i; j++ {
+			d := a[i] - a[j]
 
-	var dfs func(int, int)
-	dfs = func(idx, diff int) {
-		if idx == size {
-			return
-		}
-		for i := idx + 1; i < size; i++ {
-			if a[i]-a[idx] == diff {
-				res++
-				dfs(i, diff)
+			// 存在一个特殊的 test case，用于加速
+			if d <= min || max <= d {
+				continue
 			}
-		}
-	}
 
-	for i := 0; i < size-2; i++ {
-		for j := i + 1; j < size-1; j++ {
-			dfs(j, a[j]-a[i])
+			// + 1 是因为 [a[j], a[i]] 无法构成等差数列
+			// 但是如果存在 a[k] == a[i] + d 的话，
+			// [a[j], a[i], a[k]] 就能构成 **1** 个等差数列了
+			// 这里再次强调一下，d[i][d] 的含义是
+			// 再加一个 a[i] + d 可以构成形如
+			// [..., a[i], a[i]+d] 的等差数列子数组的个数
+			dp[i][d] += dp[j][d] + 1
+			// a[j] 已经遇到了 a[j] + d == a[i]
+			// 所以，res 加上的是 dp[j][d]
+			res += dp[j][d]
 		}
-
 	}
 
 	return res
