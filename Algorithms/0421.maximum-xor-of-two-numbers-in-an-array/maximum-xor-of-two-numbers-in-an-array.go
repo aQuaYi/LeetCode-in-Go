c44599721bba9aa0a87e5dd17f3709c5122b3970
aQua <aQua@aQuaYi.com>
2017-11-07 20:48:26 +0800

    421 wrong answer

--- Algorithms/0421.maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.go
+++ Algorithms/0421.maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.go
@@ -1,7 +1,20 @@
 package Problem0421
 
 func findMaximumXOR(nums []int) int {
-	res := 28
+	maxXOR := -1 << 31
 
-	return res
+	for i := 0; i < len(nums)-1; i++ {
+		for j := i + 1; j < len(nums); j++ {
+			maxXOR = max(maxXOR, nums[i]^nums[j])
+		}
+	}
+
+	return maxXOR
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
