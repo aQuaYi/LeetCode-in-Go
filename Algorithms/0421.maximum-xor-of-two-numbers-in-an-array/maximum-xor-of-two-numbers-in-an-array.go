bab4a43420bb46377edcb02f4b8b874b9fc08159
aQua <aQua@aQuaYi.com>
2017-11-07 21:10:09 +0800

    421 finish

--- Algorithms/0421.maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.go
+++ Algorithms/0421.maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.go
@@ -1,20 +1,29 @@
 package Problem0421
 
 func findMaximumXOR(nums []int) int {
-	maxXOR := 0
+	var max, mask int
 
-	for i := 0; i < len(nums)-1; i++ {
-		for j := i + 1; j < len(nums); j++ {
-			maxXOR = max(maxXOR, nums[i]^nums[j])
+	// Basically we use the fact that Num1 ^ Num2 = Result ==> Num1 ^ Result = Num2
+	// We start with the highest bit since we try to find the max to build the possible
+	// result. All numbers with higher bits (have been processd or to be processed) are
+	// hashed, then we use Num1 ^ Result to look up hash, if Num2 exists, then it is a
+	// valid result, save it and go on to the next bit.
+	for i := 31; i >= 0; i-- {
+		mask |= 1 << uint(i)
+
+		nMap := make(map[int]bool)
+		for _, num := range nums {
+			nMap[num&mask] = true
+		}
+
+		tmp := max | (1 << uint32(i))
+		for key := range nMap {
+			if nMap[tmp^key] {
+				max = tmp
+				break
+			}
 		}
 	}
 
-	return maxXOR
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
+	return max
 }
