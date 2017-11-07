684e55818f481b4a23f2d2cbd2dbd30d5b01b7ff
aQua <aQua@aQuaYi.com>
2017-11-07 10:29:19 +0800

    424 added

---
+++ Algorithms/0424.longest-repeating-character-replacement/longest-repeating-character-replacement.go
@@ -0,0 +1,7 @@
+package Problem0424
+
+func characterReplacement(s string, k int) int {
+	res := 4
+
+	return res
+}
---
+++ Algorithms/0424.longest-repeating-character-replacement/longest-repeating-character-replacement_test.go
@@ -0,0 +1,47 @@
+package Problem0424
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	s   string
+	k   int
+	ans int
+}{
+
+	{
+		"ABAB",
+		2,
+		4,
+	},
+
+	{
+		"AABABBA",
+		1,
+		4,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_characterReplacement(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, characterReplacement(tc.s, tc.k), "输入:%v", tc)
+	}
+}
+
+func Benchmark_characterReplacement(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			characterReplacement(tc.s, tc.k)
+		}
+	}
+}
