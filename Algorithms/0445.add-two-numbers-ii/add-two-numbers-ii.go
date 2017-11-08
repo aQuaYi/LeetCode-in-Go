51b7a61bc016119dbf37773775bc2b15da5f9874
aQua <aQua@aQuaYi.com>
2017-11-08 16:50:47 +0800

    445 accepted. 28ms > 26ms

--- Algorithms/0445.add-two-numbers-ii/add-two-numbers-ii.go
+++ Algorithms/0445.add-two-numbers-ii/add-two-numbers-ii.go
@@ -7,7 +7,46 @@ import (
 type ListNode = kit.ListNode
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	var res *ListNode
 
-	return res
+	s1:=make([]int, 0, 128)
+	for l1 != nil {
+		s1 = append(s1, l1.Val)
+		l1 = l1.Next
+	}
+
+	s2:=make([]int, 0, 128)
+	for l2 != nil {
+		s2 = append(s2, l2.Val)
+		l2 = l2.Next
+	}
+
+	sum:=0
+
+	list := &ListNode{Val:0}
+
+	for len(s1) > 0 || len(s2)> 0  {
+
+		if len(s1) >0 {
+			sum += s1[len(s1)-1]
+			s1 = s1[:len(s1)-1]
+		}
+
+		if len(s2) >0 {
+			sum += s2[len(s2)-1]
+			s2 = s2[:len(s2)-1]
+		}
+
+		list.Val = sum % 10
+
+		head := & ListNode{Val:sum/ 10}
+		head.Next = list
+		list = head
+		sum/=10
+	}
+	
+	if list.Val == 0 {
+		return list.Next
+	}
+	return list
 }
+
