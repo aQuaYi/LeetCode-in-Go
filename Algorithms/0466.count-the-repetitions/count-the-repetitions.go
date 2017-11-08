460b3122a9831ec1bc7a5adabd068900e1d9092b
aQua <aQua@aQuaYi.com>
2017-11-08 10:54:14 +0800

    466 waiting

--- Algorithms/0466.count-the-repetitions/count-the-repetitions.go
+++ Algorithms/0466.count-the-repetitions/count-the-repetitions.go
@@ -1,44 +1,45 @@
 package Problem0466
 
-func getMaxRepetitions(s1 string, n1 int, s2 string, n2 int) int {
-	if !isPossible(s1, s2) {
-		return 0
-	}
-
-	i1, i2 := 0, 0
-	len1 := len(s1)
-	len2 := len(s2)
-	for i1 < n1*len1 {
-		if s1[i1%len1] == s2[i2%len2] {
-			if i2%len2 == len2-1 && i1%len1 == len1-1 {
-				return n1 * ((i2 + 1) / len2) / ((i1 + 1) / len1) / n2
-			}
-			i2++
-		}
-		i1++
-	}
-
-	return i2 / len2 / n2
+type pair struct {
+	fi int
+	se int
 }
 
-// s1 可以重组成 s2 时，返回 true
-func isPossible(s1, s2 string) bool {
-	r1 := [128]bool{}
-	r2 := [128]bool{}
+func getMaxRepetitions(s1 string, n1 int, s2 string, n2 int) int {
+	dp := make(map[pair]pair)
+	m1, m2, i, j := 0, 0, 0, 0
+	for m1 < n1 {
+		for s1[i] != s2[j] {
+			i++
+			if i == len(s1) {
+				m1++
+				i = 0
+			}
+			if m1 == n1 {
+				return m2 / n2
+			}
+		}
+		p := pair{i, j}
+		if l, ok := dp[p]; ok {
+			d := n1 - m1 - 1
+			if d < 0 {
+				d = 0
+			}
+			m2 += d / (m1 - l.fi) * (m2 - l.se)
+			m1 += d / (m1 - l.fi) * (m1 - l.fi)
+		}
+		dp[p] = pair{m1, m2}
 
-	for i := range s1 {
-		r1[s1[i]] = true
-	}
-
-	for i := range s2 {
-		r2[s2[i]] = true
-	}
-
-	for i := 0; i < 128; i++ {
-		if r1[i] == false && r2[i] == true {
-			return false
+		i++
+		j++
+		if i == len(s1) {
+			m1++
+			i = 0
+		}
+		if j == len(s2) {
+			m2++
+			j = 0
 		}
 	}
-
-	return true
+	return m2 / n2
 }
