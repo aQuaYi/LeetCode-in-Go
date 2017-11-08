6e08fd21534ff88ea504632ccb55459adf51c54b
aQua <aQua@aQuaYi.com>
2017-11-08 16:12:19 +0800

    447 finish

--- Algorithms/0447.number-of-boomerangs/number-of-boomerangs.go
+++ Algorithms/0447.number-of-boomerangs/number-of-boomerangs.go
@@ -4,26 +4,36 @@ func numberOfBoomerangs(points [][]int) int {
 	res := 0
 
 	size := len(points)
-	for i := 0; i < size-2; i++ {
-		for j := i + 1; j < size-1; j++ {
-			for k := j + 1; k < size; k++ {
-				pi := points[i]
-				pj := points[j]
-				pk := points[k]
-				if isBoomerang(pi, pj, pk) ||
-					isBoomerang(pj, pi, pk) ||
-					isBoomerang(pk, pj, pi) {
-					res += 2
-				}
+	if size < 3 {
+		return 0
+	}
+
+	for i := 0; i < size; i++ {
+		// dMap[9]6 表示， points 中与 points[i] 距离为 3 的点，共有 6 个。
+		dMap := make(map[int]int, size)
+
+		for j := 0; j < size; j++ {
+			if i == j {
+				continue
 			}
+
+			d := dSquare(points[i], points[j])
+
+			if _, ok := dMap[d]; ok {
+				dMap[d]++
+			} else {
+				dMap[d] = 1
+			}
+		}
+
+		for _, v := range dMap {
+			// 相同距离的 v 个点
+			// 总共有 v * (v - 1) 种排列
+			res += v * (v - 1)
 		}
 	}
 
 	return res
-}
-
-func isBoomerang(i, j, k []int) bool {
-	return dSquare(i, j) == dSquare(i, k)
 }
 
 func dSquare(p1, p2 []int) int {
