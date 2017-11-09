32f0e86a7a4201bab7fe7488141bcfef9f5fa877
aQua <aQua@aQuaYi.com>
2017-11-09 14:57:43 +0800

    452 added

---
+++ Algorithms/0452.minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.go
@@ -0,0 +1,7 @@
+package Problem0452
+
+func findMinArrowShots(points [][]int) int {
+	res := 2
+
+	return res
+}
---
+++ Algorithms/0452.minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons_test.go
@@ -0,0 +1,39 @@
+package Problem0452
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	points [][]int
+	ans    int
+}{
+
+	{
+		[][]int{{10, 16}, {2, 8}, {1, 6}, {7, 12}},
+		2,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_findMinArrowShots(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, findMinArrowShots(tc.points), "输入:%v", tc)
+	}
+}
+
+func Benchmark_findMinArrowShots(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			findMinArrowShots(tc.points)
+		}
+	}
+}
