c4231c043be326a7b42641b5a966a053745ee183
aQua <aQua@aQuaYi.com>
2017-11-10 16:08:03 +0800

    472 finish

--- Algorithms/0472.concatenated-words/concatenated-words.go
+++ Algorithms/0472.concatenated-words/concatenated-words.go
@@ -1,17 +1,16 @@
 package Problem0472
 
 func findAllConcatenatedWordsInADict(words []string) []string {
-	dict := make(map[int]map[string]bool)
+	hasLen := make(map[int]bool, len(words))
+	hasWord := make(map[string]bool, len(words))
 	for _, word := range words {
-		if _, ok := dict[len(word)]; !ok {
-			dict[len(word)] = make(map[string]bool)
-		}
-		dict[len(word)][word] = true
+		hasLen[len(word)] = true
+		hasWord[word] = true
 	}
 
 	res := make([]string, 0, len(words))
 	for _, word := range words {
-		if isConcatenated(word, dict, true) {
+		if isConcatenated(word, hasLen, hasWord, true) {
 			res = append(res, word)
 		}
 	}
@@ -19,17 +18,19 @@ func findAllConcatenatedWordsInADict(words []string) []string {
 	return res
 }
 
-func isConcatenated(word string, dict map[int]map[string]bool, isWordComplete bool) bool {
+func isConcatenated(word string, hasLen map[int]bool, hasWord map[string]bool, isWordComplete bool) bool {
 	for wLen := 1; wLen < len(word); wLen++ {
-		wordMap, ok := dict[wLen]
-		if ok &&
-			wordMap[word[:wLen]] &&
-			isConcatenated(word[wLen:], dict, false) {
+		if hasLen[wLen] &&
+			// word[:wLen] 非常耗时，确保 wrods 存在长度为 wLen 的单词
+			// 才检查 word[:wLen] 是否存在
+			hasWord[word[:wLen]] &&
+			isConcatenated(word[wLen:], hasLen, hasWord, false) {
 			return true
 		}
 	}
 
 	// 单词是完整的时候，会匹配到自己, 需要剔除这种情况
 	return !isWordComplete &&
-		dict[len(word)][word]
+		hasLen[len(word)] &&
+		hasWord[word]
 }
