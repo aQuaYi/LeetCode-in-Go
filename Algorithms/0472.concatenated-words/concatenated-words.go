aee38509a118ee80dd47703e3bd853b093cf4965
aQua <aQua@aQuaYi.com>
2017-11-13 08:46:00 +0800

    472 finish

--- Algorithms/0472.concatenated-words/concatenated-words.go
+++ Algorithms/0472.concatenated-words/concatenated-words.go
@@ -1,7 +1,9 @@
 package Problem0472
 
 func findAllConcatenatedWordsInADict(words []string) []string {
+	// hasLen[3] == true 表示，words 中存在长度为 3 的单词
 	hasLen := make(map[int]bool, len(words))
+	// hasWord["dog"] == true 表示， words 中存在单词 "dog"
 	hasWord := make(map[string]bool, len(words))
 	for _, word := range words {
 		hasLen[len(word)] = true
@@ -10,7 +12,7 @@ func findAllConcatenatedWordsInADict(words []string) []string {
 
 	res := make([]string, 0, len(words))
 	for _, word := range words {
-		if isConcatenated(word, hasLen, hasWord, true) {
+		if isConcatenated(word, hasLen, hasWord, false) {
 			res = append(res, word)
 		}
 	}
@@ -18,19 +20,21 @@ func findAllConcatenatedWordsInADict(words []string) []string {
 	return res
 }
 
-func isConcatenated(word string, hasLen map[int]bool, hasWord map[string]bool, isWordComplete bool) bool {
+func isConcatenated(word string, hasLen map[int]bool, hasWord map[string]bool, isCutted bool) bool {
 	for wLen := 1; wLen < len(word); wLen++ {
 		if hasLen[wLen] &&
-			// word[:wLen] 非常耗时，确保 wrods 存在长度为 wLen 的单词
+			// word[:wLen] 非常耗时，
+			// 只有当 wrods 存在长度为 wLen 的单词时
 			// 才检查 word[:wLen] 是否存在
 			hasWord[word[:wLen]] &&
-			isConcatenated(word[wLen:], hasLen, hasWord, false) {
+			isConcatenated(word[wLen:], hasLen, hasWord, true) {
 			return true
 		}
 	}
 
 	// 单词是完整的时候，会匹配到自己, 需要剔除这种情况
-	return !isWordComplete &&
+	// 只能对被切除过的部分，进行全单词匹配
+	return isCutted &&
 		hasLen[len(word)] &&
 		hasWord[word]
 }
