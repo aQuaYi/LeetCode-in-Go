f191ae3fd10bd5d91d0cbb8da3b07ad4fbad04c3
aQua <aQua@aQuaYi.com>
2017-11-13 17:16:46 +0800

    454 Time Limit Exceeded

--- Algorithms/0454.4sum-ii/4sum-ii.go
+++ Algorithms/0454.4sum-ii/4sum-ii.go
@@ -1,7 +1,19 @@
 package Problem0454
 
 func fourSumCount(A []int, B []int, C []int, D []int) int {
-	res := 2
+	res := 0
+	dHas := make(map[int]int, len(D))
+	for i := 0; i < len(D); i++ {
+		dHas[D[i]]++
+	}
+
+	for i := 0; i < len(A); i++ {
+		for j := 0; j < len(B); j++ {
+			for k := 0; k < len(C); k++ {
+				res += dHas[0-(A[i]+B[j]+C[k])]
+			}
+		}
+	}
 
 	return res
 }
