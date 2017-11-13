257c6ab93990f6b95e6e48b340296a1e09d0a426
aQua <aQua@aQuaYi.com>
2017-11-13 19:46:55 +0800

    436 added

---
+++ Algorithms/0436.find-right-interval/find-right-interval.go
@@ -0,0 +1,13 @@
+package Problem0436
+
+import (
+	"github.com/aQuaYi/LeetCode-in-Go/kit"
+)
+
+type Interval = kit.Interval
+
+func findRightInterval(intervals []Interval) []int {
+	res := make([]int, len(intervals))
+
+	return res
+}
---
+++ Algorithms/0436.find-right-interval/find-right-interval_test.go
@@ -0,0 +1,61 @@
+package Problem0436
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aQuaYi/LeetCode-in-Go/kit"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	intervals []Interval
+	ans       []int
+}{
+
+	{
+		kit.Intss2IntervalSlice([][]int{
+			{3, 4},
+			{2, 3},
+			{1, 2},
+		}),
+		[]int{-1, 0, 1},
+	},
+
+	{
+		kit.Intss2IntervalSlice([][]int{
+			{1, 2},
+		}),
+		[]int{-1},
+	},
+
+	{
+		kit.Intss2IntervalSlice([][]int{
+			{1, 4},
+			{2, 3},
+			{3, 4},
+		}),
+		[]int{-1, 2, -1},
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_findRightInterval(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, findRightInterval(tc.intervals), "输入:%v", tc)
+	}
+}
+
+func Benchmark_findRightInterval(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			findRightInterval(tc.intervals)
+		}
+	}
+}
