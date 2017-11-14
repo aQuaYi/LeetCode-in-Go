038adfe5f94c0f764d838a397d08c1e14b7700ae
aQua <aQua@aQuaYi.com>
2017-11-14 14:10:19 +0800

    483 wrong answer

--- Algorithms/0483.smallest-good-base/smallest-good-base.go
+++ Algorithms/0483.smallest-good-base/smallest-good-base.go
@@ -1,7 +1,22 @@
 package Problem0483
 
-func smallestGoodBase(n string) string {
-	res :=
+import (
+	"math"
+	"strconv"
+)
 
-	return res
+func smallestGoodBase(n string) string {
+	numInt, _ := strconv.Atoi(n)
+	num := float64(numInt)
+
+	mMax := math.Floor(math.Log2(num))
+
+	for m := mMax; 1 < m; m-- {
+		k := math.Floor(math.Pow(num, 1/m))
+		if int((math.Pow(k, m+1)-1)/(k-1)) == numInt {
+			return strconv.Itoa(int(k))
+		}
+	}
+
+	return strconv.Itoa(numInt - 1)
 }
