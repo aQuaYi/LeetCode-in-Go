c9952c5ec607482ca683a08d2532cdff8372cc8f
aQua <aQua@aQuaYi.com>
2017-11-14 09:43:27 +0800

    456 fixing

--- Algorithms/0456.132-pattern/132-pattern.go
+++ Algorithms/0456.132-pattern/132-pattern.go
@@ -1,22 +1,34 @@
 package Problem0456
 
-func find132pattern(nums []int) bool {
+func find132pattern(a []int) bool {
+	// 题目要求找到 i < j < k
+	// 使得 ai < ak < aj
 	ak := -1 << 31
-	ajs := make([]int, 0, len(nums))
+	// ajStack 存放了所有符合 ai < ak < aj 的 aj
+	ajStack := make([]int, 0, len(a))
 
-	for i := len(nums) - 1; 0 <= i; i-- {
+	for i := len(a) - 1; 0 <= i; i-- {
 
-		if nums[i] < ak {
+		if a[i] < ak {
+			// 找到了题目要求的 ai < aj < ak
 			return true
 		}
 
-		for len(ajs) > 0 &&
-			ajs[len(ajs)-1] < nums[i] {
-			ak = ajs[len(ajs)-1]
-			ajs = ajs[:len(ajs)-1]
+		for len(ajStack) > 0 &&
+			// ajStack 中的数字的 index 都比 i 大
+			// 从 ajStack 中挑选一个仅次于 a[i] 的数字作为新的 ak
+			// 然后，把 a[i] 当做下一轮 for 的 aj 放入 ajStack
+			ajStack[len(ajStack)-1] < a[i] {
+			ak = ajStack[len(ajStack)-1]
+			ajStack = ajStack[:len(ajStack)-1]
+
+			// 根据 stack 的进出栈规则
+			// ajStack 中的 ax 及其 ax 在 a 中的索引号 x，都是递减的
+			// 理解这一点很关键
+			// 始终维持了 ak < ax ∈ ajStack 且 ak 尽可能地大 这样的关系
 		}
 
-		ajs = append(ajs, nums[i])
+		ajStack = append(ajStack, a[i])
 	}
 
 	return false
