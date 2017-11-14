9d6333e4f377f287a7bbf000fc8440e8c9ccc0fa
aQua <aQua@aQuaYi.com>
2017-11-14 14:48:07 +0800

    483 wrong answer

--- Algorithms/0483.smallest-good-base/smallest-good-base.go
+++ Algorithms/0483.smallest-good-base/smallest-good-base.go
@@ -12,9 +12,12 @@ func smallestGoodBase(n string) string {
 	mMax := math.Floor(math.Log2(num))
 
 	for m := mMax; 1 < m; m-- {
-		k := math.Floor(math.Pow(num, 1/m))
-		if int((math.Pow(k, m+1)-1)/(k-1)) == numInt {
-			return strconv.Itoa(int(k))
+		k := int(math.Pow(num, 1.0/m))
+
+		tmp := int(math.Pow(float64(k), m+1)-1) / (k - 1)
+
+		if tmp == numInt {
+			return strconv.Itoa(k)
 		}
 	}
 
