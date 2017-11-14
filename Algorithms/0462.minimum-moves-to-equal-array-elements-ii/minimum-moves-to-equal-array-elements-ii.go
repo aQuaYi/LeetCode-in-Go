2438ab8c0b8a0009f398a6b2684cbffa68cfe356
aQua <aQua@aQuaYi.com>
2017-11-14 10:36:33 +0800

    462 wrong answer

--- Algorithms/0462.minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.go
+++ Algorithms/0462.minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.go
@@ -1,7 +1,26 @@
 package Problem0462
 
 func minMoves2(nums []int) int {
-	res := 2
+	sum := 0
+	size := len(nums)
+
+	for _, n := range nums {
+		sum += n
+	}
+
+	ave := int(float64(sum)/float64(size) + 0.5)
+
+	res := 0
+	for _, n := range nums {
+		res += diff(ave, n)
+	}
 
 	return res
+}
+
+func diff(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
 }
