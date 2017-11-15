b9d09d068c95e842dea4e8aa1f38586a8684175f
aQua <aQua@aQuaYi.com>
2017-11-15 09:35:46 +0800

    488 added

---
+++ Algorithms/0488.zuma-game/zuma-game.go
@@ -0,0 +1,7 @@
+package Problem0488
+
+func findMinStep(board string, hand string) int {
+	res :=
+
+	return res
+}
---
+++ Algorithms/0488.zuma-game/zuma-game_test.go
@@ -0,0 +1,59 @@
+package Problem0488
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	board string
+	hand  string
+	ans   int
+}{
+
+	{
+		"WRRBBW",
+		"RB",
+		-1,
+	},
+
+	{
+		"WWRRBBWW",
+		"WRBRW",
+		2,
+	},
+
+	{
+		"G",
+		"GGGGG",
+		2,
+	},
+
+	{
+		"RBYYBBRRB",
+		"YRBGB",
+		3,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_findMinStep(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, findMinStep(tc.board, tc.hand), "输入:%v", tc)
+	}
+}
+
+func Benchmark_findMinStep(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			findMinStep(tc.board, tc.hand)
+		}
+	}
+}
