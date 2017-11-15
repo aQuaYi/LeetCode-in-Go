cfa126d00161e5849ca60c6a3631e4174fb98602
aQua <aQua@aQuaYi.com>
2017-11-15 14:20:48 +0800

    468 wrong answer

--- Algorithms/0468.validate-ip-address/validate-ip-address.go
+++ Algorithms/0468.validate-ip-address/validate-ip-address.go
@@ -1,7 +1,81 @@
 package Problem0468
 
-func validIPAddress(IP string) string {
-	res :=
+import (
+	"strconv"
+	"strings"
+)
 
-	return res
+func validIPAddress(IP string) string {
+	switch {
+	case isIPv4(IP):
+		return "IPv4"
+	case isIPv6(IP):
+		return "IPv6"
+	default:
+		return "Neither"
+	}
+}
+
+func isIPv4(IP string) bool {
+	if !strings.Contains(IP, ".") {
+		return false
+	}
+
+	ss := strings.Split(IP, ".")
+
+	if len(ss) != 4 {
+		return false
+	}
+
+	for _, s := range ss {
+		if !isV4Num(s) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func isV4Num(s string) bool {
+	if len(s) == 0 {
+		return false
+	}
+
+	if len(s) > 1 && s[0] == '0' {
+		return false
+	}
+
+	n, err := strconv.Atoi(s)
+
+	return err == nil && 0 <= n && n < 256
+}
+
+func isIPv6(IP string) bool {
+	if !strings.Contains(IP, ":") {
+		return false
+	}
+
+	ss := strings.Split(IP, ":")
+
+	if len(ss) != 8 {
+		return false
+	}
+
+	for _, s := range ss {
+		if !isV6Num(s) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func isV6Num(s string) bool {
+	if len(s) == 0 {
+		return false
+	}
+
+	n, err := strconv.ParseInt(s, 16, 64)
+
+	return err == nil && 0 <= n && n < 1<<16
 }
--- Algorithms/0468.validate-ip-address/validate-ip-address_test.go
+++ Algorithms/0468.validate-ip-address/validate-ip-address_test.go
@@ -9,18 +9,36 @@ import (
 
 // tcs is testcase slice
 var tcs = []struct {
-	IP string
-	ans  string 
+	IP  string
+	ans string
 }{
 
+	{
+		"0.0.0.-0",
+		"Neither",
+	},
 
-	
+	{
+		"2001:0db8:85a3:0:0:8A2E:0370:7334",
+		"IPv6",
+	},
+
+	{
+		"172.16.254.1",
+		"IPv4",
+	},
+
+	{
+		"256.256.256.256",
+		"Neither",
+	},
+
 	// 可以有多个 testcase
 }
 
 func Test_validIPAddress(t *testing.T) {
 	ast := assert.New(t)
-	
+
 	for _, tc := range tcs {
 		fmt.Printf("~~%v~~\n", tc)
 		ast.Equal(tc.ans, validIPAddress(tc.IP), "输入:%v", tc)
