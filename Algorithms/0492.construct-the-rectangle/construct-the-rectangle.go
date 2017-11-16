298dae17b8c6168d47fb34dcc15ac410fec04979
aQua <aQua@aQuaYi.com>
2017-11-16 21:29:59 +0800

    492 wrong answer

--- Algorithms/0492.construct-the-rectangle/construct-the-rectangle.go
+++ Algorithms/0492.construct-the-rectangle/construct-the-rectangle.go
@@ -1,7 +1,17 @@
 package Problem0492
 
-func constructRectangle(area int) []int {
-	res :=
+import (
+	"math"
+)
 
-	return res
+func constructRectangle(area int) []int {
+	L := int(math.Sqrt(float64(area)))
+	W := area / L
+
+	for L*W != area {
+		L++
+		W = area / L
+	}
+
+	return []int{L, W}
 }
