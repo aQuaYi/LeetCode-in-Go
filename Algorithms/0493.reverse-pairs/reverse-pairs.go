f5b8931f70bd063bfe7a088594d40755c620fa80
aQua <aQua@aQuaYi.com>
2017-11-16 15:07:28 +0800

    493 Time Limit Exceeded

--- Algorithms/0493.reverse-pairs/reverse-pairs.go
+++ Algorithms/0493.reverse-pairs/reverse-pairs.go
@@ -1,7 +1,14 @@
 package Problem0493
 
 func reversePairs(nums []int) int {
-	res := 2
+	res := 0
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i] > 2*nums[j] {
+				res++
+			}
+		}
+	}
 
 	return res
 }
