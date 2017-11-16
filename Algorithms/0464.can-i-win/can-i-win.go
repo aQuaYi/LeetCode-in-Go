c6df809101df4c0840cf065dffe26b0870a6c8a1
aQua <aQua@aQuaYi.com>
2017-11-16 11:41:58 +0800

    464 wrong answer

--- Algorithms/0464.can-i-win/can-i-win.go
+++ Algorithms/0464.can-i-win/can-i-win.go
@@ -1,44 +1,31 @@
 package Problem0464
 
-func helper(maxInt int, remains int, bit int, dp map[int]bool, bitmap []int) bool {
-	if val, ok := dp[bit]; ok {
-		return val
-	}
-
-	if remains <= maxInt {
-		for i := maxInt; i >= remains; i-- {
-			if (bit & bitmap[i]) == 0 {
-				dp[bit] = true
-				return true
-			}
-		}
-	}
-
-	for i := maxInt; i > 0; i-- {
-		if (bit & bitmap[i]) > 0 {
-			continue
-		}
-		bit |= bitmap[i]
-		ret := helper(maxInt, remains-i, bit, dp, bitmap)
-		bit &= (^bitmap[i])
-		if ret == false {
-			dp[bit] = true
-			return true
-		}
-	}
-
-	dp[bit] = false
-
-	return false
-}
+// 影响成败的关键因素有两个
+// 1. 还能选择的数字
+// 2. 还需要累加的值
+// 这两个又其实只是一个因素 -- 已经使用过的数，因为
+// 1. 还能选择的数字 == 全部可选的数 - 已经使用过的数
+// 2. 还需要累加的值 == desiredTotal - sum(已经使用过的数)
+// 题目想要求解的是 "已经使用过的数" == nil 时，能不能赢
+//
+// 相同的 “已经使用过的数” 会导致相同的结果
+// 利用暴力算法 dfs 计算结果的时候，会多次遇到同样的 "已经使用过的数" 状态
+// 把各个 “已经使用过的数” 状态导致的结果，保存起来，就可以加快程序速度了
+//
+// 使用 int 这个可比较的变量 表征 “已经使用过的数” 的状态
+// 42 表示 数字 1,3,5 都被使用过了，因为 42 = 2^1 + 2^3 + 2^5
+// 42 的第 1,3,5 个 bit 位上的数字为 1
 
 func canIWin(maxChoosableInteger int, desiredTotal int) bool {
+	// dp[42] == true 表示
+	// 当 1，3,5 被选择后，我还有机会赢
 	dp := make(map[int]bool)
-	sum := 0
-	bitmap := make([]int, maxChoosableInteger+1)
+	// sum 是所有能选数之和
+	sum := (1 + maxChoosableInteger) * maxChoosableInteger / 2
+	// bit[i] 等于，仅在第 i 个 bit 位为 1 的整数值
+	bit := make([]int, maxChoosableInteger+1)
 	for i := maxChoosableInteger; i > 0; i-- {
-		sum += i
-		bitmap[i] = 1 << uint8(i)
+		bit[i] = 1 << uint8(i)
 	}
 
 	// 这种情况下，双方都不能赢
@@ -46,5 +33,44 @@ func canIWin(maxChoosableInteger int, desiredTotal int) bool {
 		return false
 	}
 
-	return helper(maxChoosableInteger, desiredTotal, 0, dp, bitmap)
+	return dfs(0, desiredTotal, maxChoosableInteger, dp, bit)
+}
+
+func dfs(used, remains, max int, dp map[int]bool, bit []int) bool {
+	// 已经计算过 used 这种状态了，直接返回结果
+	if res, ok := dp[used]; ok {
+		return res
+	}
+
+	// 轮到我选的时候，对手已经让总数达到 desiredTotal 了
+	if remains <= 0 {
+		dp[used] = false
+		return false
+	}
+
+	for i := max; i > 0; i-- {
+		if (used & bit[i]) > 0 {
+			// i 已经被使用过了
+			continue
+		}
+
+		old := used
+		// 我选择 i ，往 used 中添加 i
+		used |= bit[i]
+		res := dfs(used, remains-i, max, dp, bit)
+		// 在 used 中删除 i
+		used = old
+
+		if res == false { // 对手输了
+			// 所以，是我赢了
+			// 请注意，下面的 used 是删除了 i 的
+			// 此时的 dp[used] = true 表示
+			// 我在面临 used 这种状态下能赢，因为还有 i 可以选择
+			dp[used] = true
+			return true
+		}
+	}
+
+	dp[used] = false
+	return false
 }
