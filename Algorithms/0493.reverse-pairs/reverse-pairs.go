0782e815c01097d15a70243debfcdbca6a92525c
aQua <aQua@aQuaYi.com>
2017-11-16 16:32:56 +0800

    493 finish

--- Algorithms/0493.reverse-pairs/reverse-pairs.go
+++ Algorithms/0493.reverse-pairs/reverse-pairs.go
@@ -5,18 +5,24 @@ func reversePairs(nums []int) int {
 }
 
 func count(nums []int, beg, end int) int {
-	if beg+1 == end {
+	if beg+1 >= end {
+		// 当 nums[beg:end] 中只有 1 或 0 个 元素时
+		// 直接返回 0
 		return 0
 	}
 
+	// 把 nums[beg:end] 按照 mid 切分成两段
+	// 分别统计两段中符合题意的元素对
 	mid := beg + (end-beg)/2
-
 	res := count(nums, beg, mid) + count(nums, mid, end)
 
 	i, j := beg, mid
-
 	for i < mid && j < end {
 		if nums[i] > 2*nums[j] {
+			// nums[i] 是 nums[i:mid] 中最小的数
+			// nums[i] > 2*nums[j] 的话，
+			// nums[i:mid] 中的任意一个数和 nums[j] 都是满足题意的 pair
+			// 所以，一次就找到了 mid - i 对 pair
 			res += mid - i
 			j++
 		} else {
