2dc8340c1b506af733ede14c12c58f9ca9457a91
aQua <aQua@aQuaYi.com>
2017-11-16 17:08:42 +0800

    463 finish

--- Algorithms/0463.island-perimeter/island-perimeter.go
+++ Algorithms/0463.island-perimeter/island-perimeter.go
@@ -1,38 +1,25 @@
 package Problem0463
 
-func islandPerimeter(grid [][]int) int {
-	if len(grid) == 0 || len(grid[0]) == 0 {
-		return 0
-	}
+var dx = []int{-1, 1, 0, 0}
+var dy = []int{0, 0, -1, 1}
 
+func islandPerimeter(grid [][]int) int {
 	m, n := len(grid), len(grid[0])
+
 	res := 0
 	for i := 0; i < m; i++ {
 		for j := 0; j < n; j++ {
-			res += check(grid, m, n, i, j)
-		}
-	}
-
-	return res
-}
-
-var ds = [][]int{
-	{-1, 0},
-	{1, 0},
-	{0, 1},
-	{0, -1},
-}
-
-// check 返回 (i, j) 周围的周长
-func check(grid [][]int, m, n, i, j int) int {
-	if grid[i][j] == 0 {
-		return 0
-	}
-	res := 4
-	for _, d := range ds {
-		x, y := i+d[0], j+d[1]
-		if 0 <= x && x < m && 0 <= y && y < n && grid[x][y] == 1 {
-			res--
+			if grid[i][j] == 0 {
+				continue
+			}
+			res += 4
+			for k := 0; k < 4; k++ {
+				x := i + dx[k]
+				y := j + dy[k]
+				if 0 <= x && x < m && 0 <= y && y < n && grid[x][y] == 1 {
+					res--
+				}
+			}
 		}
 	}
 
