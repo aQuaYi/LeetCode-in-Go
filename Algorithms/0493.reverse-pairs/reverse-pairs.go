08ee2206eb26e0404aa625bdbb3b2b8550f50234
aQua <aQua@aQuaYi.com>
2017-11-16 15:49:13 +0800

    493 Memory Limit Exceeded

--- Algorithms/0493.reverse-pairs/reverse-pairs.go
+++ Algorithms/0493.reverse-pairs/reverse-pairs.go
@@ -1,13 +1,56 @@
 package Problem0493
 
 func reversePairs(nums []int) int {
-	res := 0
-	for i := 0; i < len(nums); i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[i] > 2*nums[j] {
-				res++
-			}
+	return count(nums, 0, len(nums))
+}
+
+func count(nums []int, beg, end int) int {
+	if beg+1 == end {
+		return 0
+	}
+
+	mid := beg + (end-beg)/2
+
+	res := count(nums, beg, mid) + count(nums, mid, end)
+
+	i, j := beg, mid
+
+	for i < mid && j < end {
+		if nums[i] > 2*nums[j] {
+			res += mid - i
+			j++
+		} else {
+			i++
 		}
+	}
+
+	copy(nums[beg:end], merge(nums[beg:mid], nums[mid:end]))
+
+	return res
+}
+
+// a, b 都是升序的切片
+// merge 把 a，b 合并起来，并保持升序
+func merge(a, b []int) []int {
+	lenA, lenB := len(a), len(b)
+	res := make([]int, lenA+lenB)
+
+	var i, j, k int
+	for i < lenA && j < lenB {
+		if a[i] < b[j] {
+			res[k] = a[i]
+			i++
+		} else {
+			res[k] = b[j]
+			j++
+		}
+		k++
+	}
+
+	if i == lenA {
+		copy(res[k:], b[j:])
+	} else {
+		copy(res[k:], a[i:])
 	}
 
 	return res
