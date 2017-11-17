f632fa8087620827f9f725beff80293836e22432
aQua <aQua@aQuaYi.com>
2017-11-17 13:39:17 +0800

    467 wrong answer

--- Algorithms/0467.unique-substrings-in-wraparound-string/unique-substrings-in-wraparound-string.go
+++ Algorithms/0467.unique-substrings-in-wraparound-string/unique-substrings-in-wraparound-string.go
@@ -1,7 +1,28 @@
 package Problem0467
 
 func findSubstringInWraproundString(p string) int {
-	res :=
+	m := make(map[string]bool, len(p))
 
-	return res
+	isContinuous := func(i, j int) bool {
+		a := p[i] - 'a'
+		b := p[j] - 'a'
+		return (a+byte(j-i))%26 == b
+	}
+
+	var first, last int
+
+	for last < len(p) {
+		if !isContinuous(first, last) {
+			first = last
+			continue
+		}
+
+		for i := last; first <= i; i-- {
+			m[p[i:last+1]] = true
+		}
+
+		last++
+	}
+
+	return len(m)
 }
