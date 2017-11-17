ac296e36657052cb60f0c85ed96ceda5a056df27
aQua <aQua@aQuaYi.com>
2017-11-17 15:46:01 +0800

    482 wrong answer

--- Algorithms/0482.license-key-formatting/license-key-formatting.go
+++ Algorithms/0482.license-key-formatting/license-key-formatting.go
@@ -1,7 +1,41 @@
 package Problem0482
 
-func licenseKeyFormatting(S string, K int) string {
-	res := 4
+import (
+	"strings"
+)
 
-	return res
+func licenseKeyFormatting(s string, k int) string {
+	countDashs := strings.Count(s, "-")
+	countAN := len(s) - countDashs
+
+	s = strings.Replace(s, "-", "", -1)
+	s = strings.ToUpper(s)
+
+	remain := countAN % k
+	if remain == 0 {
+		remain = k
+	}
+
+	res := make([]byte, (countAN+k-1)/k-1+countAN)
+
+	i, j := len(res)-1, 1
+	n := len(s)
+	count := 0
+	for {
+		res[i] = s[n-j]
+		j++
+		count++
+		i--
+
+		if 0 <= i && count%k == 0 {
+			res[i] = '-'
+			i--
+		}
+
+		if i < 0 {
+			break
+		}
+	}
+
+	return string(res)
 }
