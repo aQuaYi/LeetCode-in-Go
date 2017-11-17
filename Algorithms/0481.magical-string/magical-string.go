2c22c4130684a28a61801111e359399c5565b181
aQua <aQua@aQuaYi.com>
2017-11-17 17:13:52 +0800

    481 finish

--- Algorithms/0481.magical-string/magical-string.go
+++ Algorithms/0481.magical-string/magical-string.go
@@ -4,26 +4,30 @@ import (
 	"strings"
 )
 
+// 解题思路
+// 1. 构造出完美字符串 s
+// 2. 统计 s 中 1 的个数
 func magicalString(n int) int {
-	bytes := make([]byte, n+5)
+	// n+2 是为了防止添加字符时溢出
+	bytes := make([]byte, n+2)
+
 	copy(bytes, "122")
 
+	// i 指向下一个要添加字符的数量
+	// j 始终指向 bytes 中最后一个字符，以便确定下一个需要添加的字符
 	i, j := 2, 2
-	for {
+	for j < n {
 		// b 是下一个要填写的字符
 		b := (bytes[j] - '0') ^ 3 + '0'
 		// c 是下一个要填写字符的数量
 		c := bytes[i] - '0'
 		i++
 
+		// 在bytes中添加 c 个字符 b
 		for c > 0 {
 			j++
 			bytes[j] = b
 			c--
-		}
-
-		if j >= n {
-			break
 		}
 	}
 
