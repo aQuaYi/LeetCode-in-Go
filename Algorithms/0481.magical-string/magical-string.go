4c928491a7512b9821d8f6fd35d310b353723040
aQua <aQua@aQuaYi.com>
2017-11-17 16:57:02 +0800

    481 accepted. 3ms

--- Algorithms/0481.magical-string/magical-string.go
+++ Algorithms/0481.magical-string/magical-string.go
@@ -1,7 +1,31 @@
 package Problem0481
 
-func magicalString(n int) int {
-	res := 1
+import (
+	"strings"
+)
 
-	return res
+func magicalString(n int) int {
+	bytes := make([]byte, n+5)
+	copy(bytes, "122")
+
+	i, j := 2, 2
+	for {
+		// b 是下一个要填写的字符
+		b := (bytes[j] - '0') ^ 3 + '0'
+		// c 是下一个要填写字符的数量
+		c := bytes[i] - '0'
+		i++
+
+		for c > 0 {
+			j++
+			bytes[j] = b
+			c--
+		}
+
+		if j >= n {
+			break
+		}
+	}
+
+	return strings.Count(string(bytes[:n]), "1")
 }
