27c5c246e78456f2637844d0431a8f92831a1fb3
aQua <aQua@aQuaYi.com>
2017-11-18 13:45:44 +0800

    546 Time Limit Exceeded

--- Algorithms/0546.remove-boxes/remove-boxes.go
+++ Algorithms/0546.remove-boxes/remove-boxes.go
@@ -1,7 +1,43 @@
 package Problem0546
 
-func removeBoxes(boxes []int) int {
-	res := 23
+import "fmt"
 
-	return res
+func removeBoxes(boxes []int) int {
+	m := make(map[string]int, len(boxes))
+
+	var dfs func([]int) int
+	dfs = func(boxes []int) int {
+		key := fmt.Sprint(boxes)
+		if v, ok := m[key]; ok {
+			return v
+		}
+
+		if len(boxes) == 0 {
+			m[key] = 0
+			return 0
+		}
+
+		maxPoints := 0
+
+		beg, end := 0, 0
+		for beg < len(boxes) {
+			for end < len(boxes) && boxes[beg] == boxes[end] {
+				end++
+			}
+			maxPoints = max(maxPoints, dfs(append(boxes[:beg:beg], boxes[end:]...))+(end-beg)*(end-beg))
+			beg = end
+		}
+
+		m[key] = maxPoints
+		return maxPoints
+	}
+
+	return dfs(boxes)
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
