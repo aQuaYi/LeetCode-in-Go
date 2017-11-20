2067ecf3be6156b518ae5266484cffa02262e72a
aQua <aQua@aQuaYi.com>
2017-11-20 15:13:58 +0800

    475 finish

--- Algorithms/0475.heaters/heaters.go
+++ Algorithms/0475.heaters/heaters.go
@@ -15,29 +15,24 @@ func findRadius(houses []int, heaters []int) int {
 	sort.Ints(heaters)
 
 	iHeater := sort.SearchInts(heaters, houses[0])
-	iHouse := 0
 
-	for iHouse < len(houses) {
+	for iHouse := 0; iHouse < len(houses); iHouse++ {
 		for iHeater < len(heaters) && houses[iHouse] > heaters[iHeater] {
 			iHeater++
 		}
 
-		// if iHeater == len(heaters) {
-		// 	return max(res, houses[len(houses)-1]-heaters[iHeater-1])
-		// }
+		if iHeater == len(heaters) {
+			return max(res, houses[len(houses)-1]-heaters[iHeater-1])
+		}
 
 		left := 1<<31 - 1
 		if 0 <= iHeater-1 {
 			left = houses[iHouse] - heaters[iHeater-1]
 		}
-		right := heaters[iHeater] - houses[iHouse]
-		res = max(res, min(left, right))
 
-		for iHouse < len(houses) &&
-			((0 <= iHeater-1 && houses[iHouse] <= heaters[iHeater-1]+res) ||
-				(0 == iHeater)) {
-			iHouse++
-		}
+		right := heaters[iHeater] - houses[iHouse]
+
+		res = max(res, min(left, right))
 	}
 
 	return res
