6d2c7fcf311e9ebcf08f02c413f06ca5ef93dcc9
aQua <aQua@aQuaYi.com>
2017-11-21 13:23:45 +0800

    503 wrong answer

--- Algorithms/0503.next-greater-element-ii/next-greater-element-ii.go
+++ Algorithms/0503.next-greater-element-ii/next-greater-element-ii.go
@@ -1,5 +1,27 @@
 package Problem0503
 
 func nextGreaterElements(nums []int) []int {
+	size := len(nums)
+	res := make([]int, size)
 
+	for i, n := range nums {
+		res[i] = -1
+
+		j := i + 1
+		for {
+			if n > nums[j%size] {
+				j++
+				continue
+			}
+
+			if n == nums[j%size] {
+				res[i] = res[j%size]
+			} else {
+				res[i] = nums[j%size]
+			}
+			break
+		}
+	}
+
+	return res
 }
