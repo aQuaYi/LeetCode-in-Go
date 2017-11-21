a512f6ac34afaf8a4ce08c95b0561d48714cb901
aQua <aQua@aQuaYi.com>
2017-11-21 21:26:55 +0800

    506 accepted. 175ms > 152ms

--- Algorithms/0506.relative-ranks/relative-ranks_test.go
+++ Algorithms/0506.relative-ranks/relative-ranks_test.go
@@ -10,17 +10,25 @@ import (
 // tcs is testcase slice
 var tcs = []struct {
 	nums []int
-	ans  []string 
+	ans  []string
 }{
 
+	{
+		[]int{0, 4, 3, 2, 1},
+		[]string{"5", "Gold Medal", "Silver Medal", "Bronze Medal", "4"},
+	},
 
-	
+	{
+		[]int{5, 4, 3, 2, 1},
+		[]string{"Gold Medal", "Silver Medal", "Bronze Medal", "4", "5"},
+	},
+
 	// 可以有多个 testcase
 }
 
 func Test_findRelativeRanks(t *testing.T) {
 	ast := assert.New(t)
-	
+
 	for _, tc := range tcs {
 		fmt.Printf("~~%v~~\n", tc)
 		ast.Equal(tc.ans, findRelativeRanks(tc.nums), "输入:%v", tc)
