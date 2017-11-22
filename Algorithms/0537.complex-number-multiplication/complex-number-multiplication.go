db5e202471765ff9a8c408763df7e113006df3ca
aQua <aQua@aQuaYi.com>
2017-11-22 21:25:45 +0800

    537 finish

--- Algorithms/0537.complex-number-multiplication/complex-number-multiplication.go
+++ Algorithms/0537.complex-number-multiplication/complex-number-multiplication.go
@@ -1,21 +1,26 @@
 package Problem0537
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 )
 
 // c1 = a + b*i
 // c2 = c + d*i
-// res = (ac-bd) + (ad+bc)*i
+// res = (a*c-b*d) + (a*d+b*c)*i
 func complexNumberMultiply(c1 string, c2 string) string {
+	a, b := analyze(c1)
+	c, d := analyze(c2)
 
-	return res
+	return fmt.Sprintf("%d+%di", a*c-b*d, a*d+b*c)
 }
 
-func get(c string) (re, im int) {
+// c == re + im*i
+// 返回 re 和 im
+func analyze(c string) (re, im int) {
 	ss := strings.Split(c, "+")
 	re, _ = strconv.Atoi(ss[0])
-	im, _ = strconv.Atoi(ss[1][:len(ss[1]-2)])
+	im, _ = strconv.Atoi(ss[1][:len(ss[1])-1])
 	return
 }
