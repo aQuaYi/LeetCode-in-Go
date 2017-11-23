fe84a57b6a43664627fce7631756b6e1262f1882
aQua <aQua@aQuaYi.com>
2017-11-23 11:56:27 +0800

    564 adding

--- Algorithms/0564.find-the-closest-palindrome/find-the-closest-palindrome.go
+++ Algorithms/0564.find-the-closest-palindrome/find-the-closest-palindrome.go
@@ -1,7 +1,34 @@
 package Problem0564
 
 func nearestPalindromic(n string) string {
-	res := "121"
+	if !isPalindrome(n) {
+		return palindromefy(n)
+	}
 
-	return res
+	return n
+}
+
+func isPalindrome(s string) bool {
+	i, j := 0, len(s)-1
+	for i < j {
+		if s[i] != s[j] {
+			return false
+		}
+		i++
+		j--
+	}
+	return true
+}
+
+func palindromefy(n string) string {
+	bytes := []byte(n)
+	i, j := 0, len(bytes)-1
+	for i < j {
+		if bytes[i] != bytes[j] {
+			bytes[j] = bytes[i]
+		}
+		i++
+		j--
+	}
+	return string(bytes)
 }
