4ad5c469f174ceb1b6a9919afb2598327c3d5b64
aQua <aQua@aQuaYi.com>
2017-11-23 14:20:28 +0800

    564 ut fail

--- Algorithms/0564.find-the-closest-palindrome/find-the-closest-palindrome.go
+++ Algorithms/0564.find-the-closest-palindrome/find-the-closest-palindrome.go
@@ -35,22 +35,27 @@ func nearestPalindromic(n string) string {
 func getCanidates(n string) []int {
 	size := len(n)
 	prefix := n[:(size+1)/2]
-	p, _ := strconv.Atoi(prefix)
+	p1, _ := strconv.Atoi(prefix)
+	p0, p2 := p1-1, p1+1
 
 	res := make([]int, 3)
-	res[0] = p - 1
-	res[1] = p
-	res[2] = p + 1
+	res[0] = p0
+	res[1] = p1
+	res[2] = p2
 
 	if size%2 == 1 {
-		p /= 10
+		p0 /= 10
+		p1 /= 10
+		p2 /= 10
 	}
 
-	for p > 0 {
-		res[0] = res[0]*10 + p%10
-		res[1] = res[1]*10 + p%10
-		res[2] = res[2]*10 + p%10
-		p /= 10
+	for p0 > 0 {
+		res[0] = res[0]*10 + p0%10
+		res[1] = res[1]*10 + p1%10
+		res[2] = res[2]*10 + p2%10
+		p0 /= 10
+		p1 /= 10
+		p2 /= 10
 	}
 
 	return res
