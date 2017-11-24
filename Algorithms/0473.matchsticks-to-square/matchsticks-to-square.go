6e3a409dcdfb2a600e41f8196c5004b9b372cf41
aQua <aQua@aQuaYi.com>
2017-11-24 09:19:53 +0800

    473 accepted.

--- Algorithms/0473.matchsticks-to-square/matchsticks-to-square.go
+++ Algorithms/0473.matchsticks-to-square/matchsticks-to-square.go
@@ -1,73 +1,55 @@
 package Problem0473
 
-import (
-	"container/heap"
-	"sort"
-)
+import "sort"
 
 func makesquare(nums []int) bool {
-	if len(nums) == 12 &&
-		nums[0] == 10 &&
-		nums[1] == 6 &&
-		nums[2] == 5 &&
-		nums[3] == 5 &&
-		nums[4] == 5 &&
-		nums[5] == 3 &&
-		nums[6] == 3 &&
-		nums[7] == 3 &&
-		nums[8] == 2 &&
-		nums[9] == 2 &&
-		nums[10] == 2 &&
-		nums[11] == 2 {
-		return true
+	if len(nums) < 4 {
+		return false
 	}
 
-	if len(nums) < 4 {
+	ok, avg := isOK(nums)
+	if !ok {
 		return false
 	}
 
 	sort.Sort(sort.Reverse(sort.IntSlice(nums)))
 
-	edges := make(PQ, 4)
-	for _, n := range nums {
-		edges[0] += n
-		heap.Fix(&edges, 0)
-	}
+	edges := make([]int, 4)
 
-	return edges.isSquare()
+	return dfs(nums, edges, 0, avg)
 }
 
-// PQ implements heap.Interface and holds entries.
-type PQ []int
-
-func (pq PQ) isSquare() bool {
-	for i := 1; i < len(pq); i++ {
-		if pq[i] != pq[i-1] {
-			return false
+func dfs(nums, edges []int, index, target int) bool {
+	if index == len(nums) {
+		if edges[0] == target &&
+			edges[1] == target &&
+			edges[2] == target {
+			return true
 		}
+		return false
 	}
-	return true
+
+	for i := 0; i < 4; i++ {
+		if edges[i]+nums[index] > target {
+			continue
+		}
+		edges[i] += nums[index]
+		if dfs(nums, edges, index+1, target) {
+			return true
+		}
+		edges[i] -= nums[index]
+	}
+
+	return false
 }
 
-func (pq PQ) Len() int { return len(pq) }
-
-func (pq PQ) Less(i, j int) bool {
-	return pq[i] < pq[j]
-}
-
-func (pq PQ) Swap(i, j int) {
-	pq[i], pq[j] = pq[j], pq[i]
-}
-
-// Push 往 pq 中放 x
-func (pq *PQ) Push(x interface{}) {
-	temp := x.(int)
-	*pq = append(*pq, temp)
-}
-
-// Pop 从 pq 中取出最优先的 int
-func (pq *PQ) Pop() interface{} {
-	temp := (*pq)[len(*pq)-1]
-	*pq = (*pq)[0 : len(*pq)-1]
-	return temp
+func isOK(nums []int) (ok bool, avg int) {
+	sum := 0
+	for _, n := range nums {
+		sum += n
+	}
+	if sum%4 != 0 {
+		return false, 0
+	}
+	return true, sum / 4
 }
