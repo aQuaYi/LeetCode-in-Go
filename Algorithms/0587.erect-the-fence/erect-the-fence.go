ae912d66ce6528699425341950e0901b17be1ed8
aQua <aQua@aQuaYi.com>
2017-11-24 11:58:47 +0800

    587 fixing

--- Algorithms/0587.erect-the-fence/erect-the-fence.go
+++ Algorithms/0587.erect-the-fence/erect-the-fence.go
@@ -1,6 +1,7 @@
 package Problem0587
 
 import (
+	"math"
 	"github.com/aQuaYi/LeetCode-in-Go/kit"
 )
 
@@ -13,28 +14,63 @@ func outerTrees(points []Point) []Point {
 	}
 
 	res := []Point{}
-isUsed := make([]bool, size)
+	isUsed := make([]bool, size)
 
 	cx := points[0]
 	idx := 0
 	for i := 1; i < size; i++ {
-		cx,idx = cloest2X(cx, points[i],idx,i)
+		cx, idx = cloest2X(cx, points[i], idx, i)
 	}
 
-res = append(res, cx)
-isUsed[idx] = true
+	res = append(res, cx)
+	isUsed[idx] = true
 
-
+	var dfs func(Point, vector)
+dfs = func(o Point, b vector){
+	max :=-2.
+	idx :=-1
+	for i  , p  :=range points {
+	if isUsed[i] {
+		continue
+	}	
+	tmp := angle(makeVector(o, p), b )
+	if max < tmp {
+		max = tmp
+		idx = i
+	}
+	}
+	isUsed[idx]=true
+}
 
 	return res
 }
 
 // 返回更靠近 X 轴的点
-func cloest2X(a, b Point,ia,ib int) (Point,int) {
+func cloest2X(a, b Point, ia, ib int) (Point, int) {
 	A, B := a.Y*b.X, b.Y*a.X
 	if A < B ||
 		(A == B && a.X < b.X) {
-		return a,ia
+		return a, ia
 	}
-	return b,ib
+	return b, ib
+}
+
+type vector struct {
+	x, y float64
+}
+
+// 返回 a→b 的向量
+func makeVector(a, b Point) vector {
+	return vector{
+		x: float64(b.X - a.X),
+		y: float64(b.Y - a.Y),
+	}
+}
+
+// 返回两个向量的夹角
+func angle(a, b vector) float64 {
+	ab:=a.x*a.x + a.y*a.y
+	aNorm := math.Sqrt(a.x*a.x + a.y*a.y)
+	bNorm := math.Sqrt(b.x*b.x + b.y*b.y)
+	return ab/(aNorm*bNorm)
 }
