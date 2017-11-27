09b306584ead4b80a28d7d350e195b4dfb4b4a43
aQua <aQua@aQuaYi.com>
2017-11-27 14:10:16 +0800

    477 finish

--- Algorithms/0477.total-hamming-distance/total-hamming-distance.go
+++ Algorithms/0477.total-hamming-distance/total-hamming-distance.go
@@ -2,24 +2,18 @@ package Problem0477
 
 func totalHammingDistance(nums []int) int {
 	res := 0
-	for i := 0; i < len(nums); i++ {
-		for j := i + 1; j < len(nums); j++ {
-			res += hd(nums[i], nums[j])
+	n := len(nums)
+
+	for j := 0; j < 32; j++ {
+		k := 0
+		for i := 0; i < n; i++ {
+			k += (nums[i] >> uint(j)) & 1
 		}
+		// 在第 j 个 bit 位上，
+		// 共有 k 个数是 1 ， (n-k) 个位是 0
+		// 那么，在第 j 个 bit 位上，会产生 k*(n-k) 个 hamming distance
+		res += k * (n - k)
 	}
 
 	return res
-}
-
-// 返回 a 和 b 的 hamming distance
-func hd(a, b int) int {
-	sum := 0
-	t := a ^ b
-	for t > 0 {
-		if t&1 == 1 {
-			sum++
-		}
-		t >>= 1
-	}
-	return sum
 }
