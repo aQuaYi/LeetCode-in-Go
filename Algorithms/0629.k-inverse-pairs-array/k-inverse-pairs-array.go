cd60385dbdd65d815cfe68e3682eb71db069bb27
aQua <aQua@aQuaYi.com>
2017-11-27 10:38:55 +0800

    629 added

---
+++ Algorithms/0629.k-inverse-pairs-array/k-inverse-pairs-array.go
@@ -0,0 +1,7 @@
+package Problem0629
+
+func kInversePairs(n int, k int) int {
+	res := 1
+
+	return res
+}
---
+++ Algorithms/0629.k-inverse-pairs-array/k-inverse-pairs-array_test.go
@@ -0,0 +1,46 @@
+package Problem0629
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	n   int
+	k   int
+	ans int
+}{
+	{
+		3,
+		0,
+		1,
+	},
+
+	{
+		3,
+		1,
+		2,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_kInversePairs(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, kInversePairs(tc.n, tc.k), "输入:%v", tc)
+	}
+}
+
+func Benchmark_kInversePairs(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			kInversePairs(tc.n, tc.k)
+		}
+	}
+}
