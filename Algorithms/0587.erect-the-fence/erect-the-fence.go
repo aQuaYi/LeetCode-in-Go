bd0bdc60909be21c4c05ba5a60f36ad3b694cc79
aQua <aQua@aQuaYi.com>
2017-11-28 09:30:18 +0800

    587 finish

--- Algorithms/0587.erect-the-fence/erect-the-fence.go
+++ Algorithms/0587.erect-the-fence/erect-the-fence.go
@@ -7,53 +7,69 @@ import (
 type Point = kit.Point
 
 func outerTrees(points []Point) []Point {
-    n := len(points)
-    if n < 4 {
-        return points
-    }
-    
-    // Find the leftmost point as the first point in the result
-    first := 0
-    for i := 1; i < n; i++ {
-        if points[i].X < points[first].X {
-            first = i
-        }
-    }
-    
-    results := append([]Point{}, points[first])
-    
-    cur := first
-    for {
-        next := (cur + 1) % n
-        for i := 0; i < n; i++ {
-            if i == cur { continue }
-            cross := crossProductLength(points[cur], points[i], points[next])  
-            if cross < 0 || (cross == 0 && distance(points[cur], points[i]) > distance(points[cur], points[next])) {
-                next = i
-            }
-        }
-        
-        for i := 0; i < n; i++ {
-            if i == cur || i == first { continue }
-            if crossProductLength(points[cur], points[i], points[next]) == 0 {
-                results = append(results, points[i])
-            }
-        }
-        
-        cur = next
-        
-        if cur == first {
-            break
-        }
-    }
-    
-    return results
+	n := len(points)
+	if n < 4 {
+		return points
+	}
+
+	// 选择最右边的点，作为起始点
+	first := 0
+	for i := 1; i < n; i++ {
+		if points[i].X < points[first].X {
+			first = i
+		}
+	}
+
+	res := append([]Point{}, points[first])
+
+	cur := first
+	for {
+		// 寻找以 cur 为圆心，顺时针方向角度最大的点
+		next := (cur + 1) % n
+		for i := 0; i < n; i++ {
+			if i == cur {
+				continue
+			}
+			cross := crossProductLength(points[cur], points[next], points[i])
+			if cross < 0 ||
+				(cross == 0 && isFurther(points[cur], points[next], points[i])) {
+				next = i
+			}
+		}
+
+		// 把与 cur→next 共线的点都放入 res
+		for i := 0; i < n; i++ {
+			if i == cur || i == first {
+				// 这是已经放入过的点
+				continue
+			}
+			if crossProductLength(points[cur], points[next], points[i]) == 0 {
+				// next 也会被放入
+				res = append(res, points[i])
+			}
+		}
+
+		cur = next
+		// cur == first 说明围栏已经封闭了
+		if cur == first {
+			break
+		}
+	}
+
+	return res
 }
 
-func crossProductLength(a, b, c Point) int {
-    return (a.X - b.X) * (c.Y - b.Y) - (a.Y - b.Y) * (c.X - b.X)
+// 返回 oa×ob 的长度
+// 当返回值 <0 时，说明以 o 点圆心，b 点在 a 点的顺时针方向
+// 当返回值 =0 时，说明以 o 点，b 点和 a 点在同一条直线长
+func crossProductLength(o, a, b Point) int {
+	return (a.X-o.X)*(b.Y-o.Y) - (a.Y-o.Y)*(b.X-o.X)
+}
+
+func isFurther(o, a, b Point) bool {
+	return distance(o, a) < distance(o, b)
 }
 
 func distance(a, b Point) int {
-    return (a.X - b.X) * (a.X - b.X) + (a.Y - b.Y) * (a.Y - b.Y)
+	return (a.X-b.X)*(a.X-b.X) + (a.Y-b.Y)*(a.Y-b.Y)
 }
