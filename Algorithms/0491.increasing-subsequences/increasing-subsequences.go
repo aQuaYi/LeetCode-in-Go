154b381446957512b73a1c8f7cdb363fe6bdd6fe
aQua <aQua@aQuaYi.com>
2017-11-28 10:33:39 +0800

    491 wrong answer

--- Algorithms/0491.increasing-subsequences/increasing-subsequences.go
+++ Algorithms/0491.increasing-subsequences/increasing-subsequences.go
@@ -1,7 +1,30 @@
 package Problem0491
 
 func findSubsequences(nums []int) [][]int {
-	res :=
+	size := len(nums)
+	res := make([][]int, 0, (size-1)*(size-1))
+	if size < 2 {
+		return res
+	}
 
+	var dfs func([]int, int)
+	dfs = func(ints []int, idx int) {
+		if len(ints) > 1 {
+			tmp := make([]int, len(ints))
+			copy(tmp, ints)
+			res = append(res, tmp)
+		}
+
+		t := nums[0] - 1
+		for i := idx; i < size; i++ {
+			if t == nums[i] {
+				continue
+			}
+			dfs(append(ints, nums[i]), i+1)
+			t = nums[i]
+		}
+	}
+
+	dfs([]int{}, 0)
 	return res
 }
