51b2ff114c8def224455fdd07db3c85f44cfa80d
aQua <aQua@aQuaYi.com>
2017-11-28 17:16:49 +0800

    491 waiting

--- Algorithms/0491.increasing-subsequences/increasing-subsequences.go
+++ Algorithms/0491.increasing-subsequences/increasing-subsequences.go
@@ -1,61 +1,32 @@
 package Problem0491
 
-import (
-	"fmt"
-)
-
 func findSubsequences(nums []int) [][]int {
 	size := len(nums)
-	temp := make([][]int, 1, (size-1)*(size-1))
-	temp[0] = []int{}
-
-	for _, n := range nums {
-		sizeRes := len(temp)
-		for i := 0; i < sizeRes; i++ {
-			if len(temp[i]) == 0 ||
-				temp[i][len(temp[i])-1] <= n {
-				t := make([]int, len(temp[i])+1)
-				copy(t, temp[i])
-				t[len(temp[i])] = n
-				temp = append(temp, t)
-			}
-		}
-	}
-
-	isAdded := make(map[string]bool)
-	res := make([][]int, 0, len(temp))
-	for _, t := range temp {
-		key := fmt.Sprint(t)
-		if len(t) >= 2 && !isAdded[key] {
-			res = append(res, t)
-			isAdded[key] = true
-		}
-	}
-
+	res := make([][]int, 0, size*size)
+	list := make([]int, 0, size)
+	helper(nums, 0, &res, list)
 	return res
 }
 
-// class Solution {
-//     public List<List<Integer>> findSubsequences(int[] nums) {
-//         List<List<Integer>> result = new ArrayList<>();
-//         List<Integer> list = new ArrayList<>();
-//         findSubseq(nums, 0, result, list);
-//         return result;
-//     }
-//     private void findSubseq(int[] nums, int index, List<List<Integer>> result, List<Integer> list) {
-//         if (index == nums.length) {
-//             if (list.size() > 1) {
-//                 result.add(new ArrayList<>(list));
-//             }
-//             return ;
-//         }
-//         if (list.isEmpty() || list.get(list.size() - 1) <= nums[index]) {
-//             list.add(nums[index]);
-//             findSubseq(nums, index + 1, result, list);
-//             list.remove(list.size() - 1);
-//         }
-//         if (list.isEmpty() || list.get(list.size() - 1) != nums[index]){
-//             findSubseq(nums, index + 1, result, list);
-//         }
-//     }
-// }
+func helper(nums []int, index int, res *[][]int, list []int) {
+	if index == len(nums) {
+		if len(list) > 1 {
+			temp := make([]int, len(list))
+			copy(temp, list)
+			*res = append((*res), temp)
+		}
+		return
+	}
+
+	if len(list) == 0 ||
+		list[len(list)-1] <= nums[index] {
+		list = append(list, nums[index])
+		helper(nums, index+1, res, list)
+		list = list[:len(list)-1]
+	}
+
+	if len(list) == 0 ||
+		list[len(list)-1] != nums[index] {
+		helper(nums, index+1, res, list)
+	}
+}
