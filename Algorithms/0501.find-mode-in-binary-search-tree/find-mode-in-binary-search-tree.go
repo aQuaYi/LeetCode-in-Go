aa3eaf556da9588614fdb8fc8b413f5a69ddd51b
aQua <aQua@aQuaYi.com>
2017-11-28 16:08:16 +0800

    501 added

--- Algorithms/0501.find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.go
+++ Algorithms/0501.find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.go
@@ -1,7 +1,13 @@
 package Problem0501
 
+import (
+	"github.com/aQuaYi/LeetCode-in-Go/kit"
+)
+
+type TreeNode = kit.TreeNode
+
 func findMode(root *TreeNode) []int {
-	res :=
+	res := []int{}
 
 	return res
 }
--- Algorithms/0501.find-mode-in-binary-search-tree/find-mode-in-binary-search-tree_test.go
+++ Algorithms/0501.find-mode-in-binary-search-tree/find-mode-in-binary-search-tree_test.go
@@ -4,33 +4,41 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/aQuaYi/LeetCode-in-Go/kit"
+
 	"github.com/stretchr/testify/assert"
 )
 
 // tcs is testcase slice
 var tcs = []struct {
-	root *TreeNode
-	ans  []int 
+	pre, in []int
+	ans     []int
 }{
 
+	{
+		[]int{1, 2, 1, 2},
+		[]int{1, 1, 2, 2},
+		[]int{1, 2},
+	},
 
-	
 	// 可以有多个 testcase
 }
 
 func Test_findMode(t *testing.T) {
 	ast := assert.New(t)
-	
+
 	for _, tc := range tcs {
 		fmt.Printf("~~%v~~\n", tc)
-		ast.Equal(tc.ans, findMode(tc.root), "输入:%v", tc)
+		root := kit.PreIn2Tree(tc.pre, tc.in)
+		ast.Equal(tc.ans, findMode(root), "输入:%v", tc)
 	}
 }
 
 func Benchmark_findMode(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		for _, tc := range tcs {
-			findMode(tc.root)
+			root := kit.PreIn2Tree(tc.pre, tc.in)
+			findMode(root)
 		}
 	}
 }
