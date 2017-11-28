21e3f11230e551b77d8b63096863755ae6762e32
aQua <aQua@aQuaYi.com>
2017-11-28 16:28:53 +0800

    501 accepted. 46ms > 23ms

--- Algorithms/0501.find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.go
+++ Algorithms/0501.find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.go
@@ -5,9 +5,36 @@ import (
 )
 
 type TreeNode = kit.TreeNode
-
+// TODO: 这个方法，完全没有利用上 BST 的特性
+// 改进这个方法，效率提升一倍
 func findMode(root *TreeNode) []int {
+	r := map[int]int{}
+	search(root, r)
+
+	max := -1
+	for _, v := range r {
+		if max < v {
+			max = v
+		}
+	}
+
 	res := []int{}
+	for n, v := range r {
+		if v == max {
+			res = append(res, n)
+		}
+	}
 
 	return res
+}
+
+func search(root *TreeNode, rec map[int]int) {
+	if root == nil {
+		return
+	}
+
+	(rec)[root.Val]++
+
+	search(root.Left, rec)
+	search(root.Right, rec)
 }
