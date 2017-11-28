764c2dfdd203bdc3fd9b177103993d4300af34c8
aQua <aQua@aQuaYi.com>
2017-11-28 11:49:33 +0800

    491 fixing

--- Algorithms/0491.increasing-subsequences/increasing-subsequences.go
+++ Algorithms/0491.increasing-subsequences/increasing-subsequences.go
@@ -1,30 +1,36 @@
 package Problem0491
 
+import (
+	"fmt"
+)
+
 func findSubsequences(nums []int) [][]int {
 	size := len(nums)
-	res := make([][]int, 0, (size-1)*(size-1))
-	if size < 2 {
-		return res
-	}
+	temp := make([][]int, 1, (size-1)*(size-1))
+	temp[0] = []int{}
 
-	var dfs func([]int, int)
-	dfs = func(ints []int, idx int) {
-		if len(ints) > 1 {
-			tmp := make([]int, len(ints))
-			copy(tmp, ints)
-			res = append(res, tmp)
-		}
-
-		t := nums[0] - 1
-		for i := idx; i < size; i++ {
-			if t == nums[i] {
-				continue
+	for _, n := range nums {
+		sizeRes := len(temp)
+		for i := 0; i < sizeRes; i++ {
+			if len(temp[i]) == 0 ||
+				temp[i][len(temp[i])-1] <= n {
+				t := make([]int, len(temp[i])+1)
+				copy(t, temp[i])
+				t[len(temp[i])] = n
+				temp = append(temp, t)
 			}
-			dfs(append(ints, nums[i]), i+1)
-			t = nums[i]
 		}
 	}
 
-	dfs([]int{}, 0)
+	isAdded := make(map[string]bool)
+	res := make([][]int, 0, len(temp))
+	for _, t := range temp {
+		key := fmt.Sprint(t)
+		if len(t) >= 2 && !isAdded[key] {
+			res = append(res, t)
+			isAdded[key] = true
+		}
+	}
+
 	return res
 }
