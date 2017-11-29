42d353448439b31ec887fe0f63dc80c53b71c47d
aQua <aQua@aQuaYi.com>
2017-11-29 14:33:07 +0800

    486 finish

--- Algorithms/0486.predict-the-winner/predict-the-winner.go
+++ Algorithms/0486.predict-the-winner/predict-the-winner.go
@@ -9,13 +9,24 @@ func PredictTheWinner(nums []int) bool {
 	dp := make([][]int, n)
 	for i := range dp {
 		dp[i] = make([]int, n)
+		// 只有 nums[i] 时，play1 比 play2 多 nums[i] 分
+		// 因为 play1 选了 nums[i]，而 play2 无数可选了
 		dp[i][i] = nums[i]
 	}
 
-	for Len := 1; Len < n; Len++ {
-		for left := 0; left < n-Len; left++ {
-			right := left + Len
-			dp[left][right] = max(nums[left]-dp[left+1][right], nums[right]-dp[left][right-1])
+	for Len := 2; Len <= n; Len++ {
+		for i := 0; i <= n-Len; i++ {
+			// Len == j - i + 1
+			j := i + Len - 1
+
+			// dp[i][j] 表示 nums[i:j+1] 中 play1 比 play2 多的得分
+			// 那么 -dp[i][j] 表示什么呢?
+			// -dp[i][j] 表示 nums[i:j+1] 中 play2 比 play1 多的得分
+			// 所以，
+			// nums[i]-dp[i+1][j] 等于 play1 选择 左端 的数后比 play2 多的得分
+			// nums[j]-dp[i][j-1] 等于 play1 选择 右端 的数后比 play2 多的得分
+			// 从以上两者之间，选择一个大的，作为 dp[i][j]
+			dp[i][j] = max(nums[i]-dp[i+1][j], nums[j]-dp[i][j-1])
 		}
 	}
 
