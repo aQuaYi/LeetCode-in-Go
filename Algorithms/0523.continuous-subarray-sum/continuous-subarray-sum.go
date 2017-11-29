356a8d09dfa04d469467d9f61c178ae40b28167d
aQua <aQua@aQuaYi.com>
2017-11-29 16:37:39 +0800

    523 finish

--- Algorithms/0523.continuous-subarray-sum/continuous-subarray-sum.go
+++ Algorithms/0523.continuous-subarray-sum/continuous-subarray-sum.go
@@ -2,7 +2,15 @@ package Problem0523
 
 func checkSubarraySum(nums []int, k int) bool {
 	n := len(nums)
+
+	// index 中记录了，每个不同的余数，最早出现的位置
 	index := make(map[int]int, n)
+	// index[0] = -1 是为了以下情况准备的
+	// {
+	// 	[]int{0, 0},
+	// 	0,
+	// 	true,
+	// },
 	index[0] = -1
 
 	sum := 0
@@ -12,11 +20,17 @@ func checkSubarraySum(nums []int, k int) bool {
 		if k != 0 {
 			sum %= k
 		}
+
 		idx, ok := index[sum]
-		if ok && i-idx > 1 {
-			return true
+		if ok {
+			if i-idx > 1 {
+				return true
+			}
+		} else {
+			// 这个 if - else 结构确保了 index 中记录的值是最早出现的
+			index[sum] = i
 		}
-		index[sum] = i
+
 	}
 
 	return false
