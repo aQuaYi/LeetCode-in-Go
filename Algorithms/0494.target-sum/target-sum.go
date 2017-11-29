4cf59871b3a56b7202b501f35bec228460a057b6
aQua <aQua@aQuaYi.com>
2017-11-29 13:41:29 +0800

    494 finish

--- Algorithms/0494.target-sum/target-sum.go
+++ Algorithms/0494.target-sum/target-sum.go
@@ -1,27 +1,40 @@
 package Problem0494
 
 func findTargetSumWays(nums []int, S int) int {
+	sum := 0
 
-	mid := len(nums) / 2
-	left := make(map[int]int, len(nums))
-	right := make(map[int]int, len(nums))
-	dfs(nums[:mid], 0, 0, left)
-	dfs(nums[mid:], 0, 0, right)
-
-	res := 0
-	for l, c := range left {
-		res += c * right[S-l]
+	for i := 0; i < len(nums); i++ {
+		sum += nums[i]
 	}
 
-	return res
+	if sum < S {
+		return 0
+	}
+	// sum 和 S 必须同为奇数或者偶数
+	if (sum+S)%2 == 1 {
+		return 0
+	}
+
+	return calcSumWays(nums, (sum+S)/2)
 }
 
-func dfs(nums []int, index, sum int, rec map[int]int) {
-	if index == len(nums) {
-		rec[sum]++
-		return
+// nums 被分为两个部分 P 和 N
+// P 中的元素前面放的是 +
+// N 中的元素前面放的是 -
+// 可得
+//                   sum(P) - sum(N) = target
+// sum(P) + sum(N) + sum(P) - sum(N) = target + sum(P) + sum(N)
+//                        2 * sum(P) = target + sum(nums)
+// 于是，题目就被转换成了
+// nums 有多少个子集，其和为 (target + sum(nums))/2
+func calcSumWays(nums []int, target int) int {
+	// dp[i] 表示 nums 中和为 i 的子集个数
+	dp := make([]int, target+1)
+	dp[0] = 1
+	for i := 0; i < len(nums); i++ {
+		for j := target; j >= nums[i]; j-- {
+			dp[j] += dp[j-nums[i]]
+		}
 	}
-
-	dfs(nums, index+1, sum+nums[index], rec)
-	dfs(nums, index+1, sum-nums[index], rec)
+	return dp[target]
 }
