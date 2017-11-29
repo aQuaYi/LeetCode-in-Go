d6b848a90973cf755a698533df5dfe0f21fc1866
aQua <aQua@aQuaYi.com>
2017-11-29 16:00:42 +0800

    523 added

---
+++ Algorithms/0523.continuous-subarray-sum/continuous-subarray-sum.go
@@ -0,0 +1,7 @@
+package Problem0523
+
+func checkSubarraySum(nums []int, k int) bool {
+	res := true
+
+	return res
+}
---
+++ Algorithms/0523.continuous-subarray-sum/continuous-subarray-sum_test.go
@@ -0,0 +1,47 @@
+package Problem0523
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	nums []int
+	k    int
+	ans  bool
+}{
+
+	{
+		[]int{23, 2, 4, 6, 7},
+		6,
+		true,
+	},
+
+	{
+		[]int{23, 2, 6, 4, 7},
+		6,
+		true,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_checkSubarraySum(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, checkSubarraySum(tc.nums, tc.k), "输入:%v", tc)
+	}
+}
+
+func Benchmark_checkSubarraySum(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			checkSubarraySum(tc.nums, tc.k)
+		}
+	}
+}
