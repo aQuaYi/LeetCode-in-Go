2595ce347ab72155285a38c1a3d3fc811b9239e3
aQua <aQua@aQuaYi.com>
2017-11-29 14:11:30 +0800

    486 finish

--- Algorithms/0486.predict-the-winner/predict-the-winner.go
+++ Algorithms/0486.predict-the-winner/predict-the-winner.go
@@ -2,17 +2,29 @@ package Problem0486
 
 // PredictTheWinner 在 play1 能赢的时候，返回 true
 func PredictTheWinner(nums []int) bool {
-	if len(nums) == 1 {
-		return true
+	n := len(nums)
+
+	// dp[i][j] 表示 nums[i:j+1] 中 play1 比 play2 多的得分
+	// dp[0][n-1] >=0 表示 play1 获胜
+	dp := make([][]int, n)
+	for i := range dp {
+		dp[i] = make([]int, n)
+		dp[i][i] = nums[i]
 	}
-	return search(nums, 0, len(nums)-1, 0, 0)
+
+	for Len := 1; Len < n; Len++ {
+		for left := 0; left < n-Len; left++ {
+			right := left + Len
+			dp[left][right] = max(nums[left]-dp[left+1][right], nums[right]-dp[left][right-1])
+		}
+	}
+
+	return dp[0][n-1] >= 0
 }
 
-func search(nums []int, left, right, s1, s2 int) bool {
-	if left > right {
-		return s1 >= s2
+func max(a, b int) int {
+	if a > b {
+		return a
 	}
-
-	return !search(nums, left+1, right, s2, s1+nums[left]) ||
-		!search(nums, left, right-1, s2, s1+nums[right])
+	return b
 }
