f3bf96a13379869168a21dd6016f2b154085ab80
aQua <aQua@aQuaYi.com>
2017-11-29 11:52:47 +0800

    494 accepted. 1108ms > 3ms

--- Algorithms/0494.target-sum/target-sum.go
+++ Algorithms/0494.target-sum/target-sum.go
@@ -1,7 +1,19 @@
 package Problem0494
 
 func findTargetSumWays(nums []int, S int) int {
-	res := 5
-
+	res := 0
+	dfs(nums, 0, 0, S, &res)
 	return res
+}
+
+func dfs(nums []int, index, sum, target int, res *int) {
+	if index == len(nums) {
+		if sum == target {
+			*res++
+		}
+		return
+	}
+
+	dfs(nums, index+1, sum+nums[index], target, res)
+	dfs(nums, index+1, sum-nums[index], target, res)
 }
