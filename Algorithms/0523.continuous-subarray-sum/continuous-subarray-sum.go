d70c04ca7468642684b7c67aacbfc5139f7ab5f1
aQua <aQua@aQuaYi.com>
2017-11-29 16:28:59 +0800

    523 wrong answer

--- Algorithms/0523.continuous-subarray-sum/continuous-subarray-sum.go
+++ Algorithms/0523.continuous-subarray-sum/continuous-subarray-sum.go
@@ -1,7 +1,23 @@
 package Problem0523
 
 func checkSubarraySum(nums []int, k int) bool {
-	res := true
+	n := len(nums)
+	index := make(map[int]int, n)
+	index[0] = -1
 
-	return res
+	sum := 0
+
+	for i := 0; i < n; i++ {
+		sum += nums[i]
+		if k != 0 {
+			sum %= k
+		}
+		idx, ok := index[sum]
+		if ok && i-idx > 1 {
+			return true
+		}
+		index[sum] = i
+	}
+
+	return false
 }
