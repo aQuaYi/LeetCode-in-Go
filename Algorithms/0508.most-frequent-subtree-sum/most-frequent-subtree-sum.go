916f66f6cf3826f6427bf686f4bee374c2c2b7ee
aQua <aQua@aQuaYi.com>
2017-11-30 15:08:35 +0800

    508 finish

--- Algorithms/0508.most-frequent-subtree-sum/most-frequent-subtree-sum.go
+++ Algorithms/0508.most-frequent-subtree-sum/most-frequent-subtree-sum.go
@@ -7,7 +7,32 @@ import (
 type TreeNode = kit.TreeNode
 
 func findFrequentTreeSum(root *TreeNode) []int {
-	res :=[]int{}
+	rec := make(map[int]int)
+	sum(root, rec)
+
+	res := make([]int, 0, len(rec))
+	max := -1 
+	for s, c := range rec {
+		if max <= c {
+			if max < c {
+				max = c
+				res = res[0:0]
+			}
+			res = append(res , s)
+		}
+	}
 
 	return res
+}
+
+// 递归求子树的和，并统计各个和出现的次数
+func sum(root *TreeNode, r map[int]int) int {
+	if root == nil {
+		return 0
+	}
+
+	s := root.Val + sum(root.Left, r) + sum(root.Right, r)
+	r[s]++
+
+	return s
 }
