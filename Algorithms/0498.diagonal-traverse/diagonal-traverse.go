37d55131c6562c9e0c965e3f920b5ad1a3fce1b8
aQua <aQua@aQuaYi.com>
2017-11-30 14:33:58 +0800

    498 finish

--- Algorithms/0498.diagonal-traverse/diagonal-traverse.go
+++ Algorithms/0498.diagonal-traverse/diagonal-traverse.go
@@ -1,7 +1,50 @@
 package Problem0498
 
-func findDiagonalOrder(matrix [][]int) []int {
-	res := []int{}
+func findDiagonalOrder(mat [][]int) []int {
+	if len(mat) == 0 || len(mat[0]) == 0 {
+		return []int{}
+	}
+	m, n := len(mat), len(mat[0])
+
+	isUpping := true
+	next := func(i, j int) (int, int) {
+		if isUpping {
+			i--
+			j++
+			if 0 <= i && j < n {
+				return i, j
+			}
+
+			isUpping = false
+
+			if i < 0 && j < n {
+				return 0, j
+			}
+			return i + 2, j - 1
+		}
+
+		i++
+		j--
+		if i < m && 0 <= j {
+			return i, j
+		}
+
+		isUpping = true
+
+		if i < m && j < 0 {
+			return i, 0
+		}
+		return i - 1, j + 2
+	}
+
+	mn := m * n
+	res := make([]int, mn)
+
+	i, j := 0, 0
+	for k := 0; k < mn; k++ {
+		res[k] = mat[i][j]
+		i, j = next(i, j)
+	}
 
 	return res
 }
