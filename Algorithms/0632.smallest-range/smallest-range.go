d66a6c38c2cc014a0d997a46a855c0db0f33aea3
aQua <aQua@aQuaYi.com>
2017-11-30 13:48:39 +0800

    632 添加了注释，并微调了结构

--- Algorithms/0632.smallest-range/smallest-range.go
+++ Algorithms/0632.smallest-range/smallest-range.go
@@ -7,47 +7,50 @@ import (
 func smallestRange(intss [][]int) []int {
 	// 把 intss 中的数字连同其队伍信息，整合成 num 结构体放入 ns
 	ns := makeNums(intss)
-	// 让 ns 根据其数值降序排列
+	// 让 ns 根据其数值排列
 	sort.Sort(ns)
 
 	s := newStatus(ns, len(intss))
 	s.check()
+
 	return s.res
 }
 
 type status struct {
-	res       []int
-	ns        nums
-	i, j      int
-	isGetAll  bool
-	numCount  []int
-	teamCount int
-	min       int
+	res             []int
+	ns              nums
+	i, j            int
+	minDiff         int
+	absentTeamCount int
+	teamCount       []int
 }
 
-func newStatus(ns nums, teamCount int) *status {
+func newStatus(ns nums, teams int) *status {
 	return &status{
-		res:       make([]int, 2),
-		ns:        ns,
-		i:         0,
-		j:         -1,
-		isGetAll:  false,
-		numCount:  make([]int, teamCount),
-		teamCount: teamCount,
-		min:       1<<31 - 1,
+		res:             make([]int, 2),
+		ns:              ns,
+		i:               0,
+		j:               -1,
+		minDiff:         1<<31 - 1,
+		absentTeamCount: teams,
+		teamCount:       make([]int, teams),
 	}
 }
 
 func (s *status) check() {
 	for s.j < len(s.ns) {
-		for s.j < len(s.ns) && !s.isGetAll {
+		for s.j < len(s.ns) && !s.hasAllTeam() {
 			s.expend()
 		}
-		for s.isGetAll {
+		for s.hasAllTeam() {
 			s.updateRes()
 			s.shrink()
 		}
 	}
+}
+
+func (s status) hasAllTeam() bool {
+	return s.absentTeamCount == 0
 }
 
 // 让 s.j++ 并修改相关的状态值
@@ -56,35 +59,29 @@ func (s *status) expend() {
 	if s.j >= len(s.ns) {
 		return
 	}
-	if s.numCount[s.ns[s.j].team] == 0 {
-		s.teamCount--
+	if s.teamCount[s.ns[s.j].team] == 0 {
+		s.absentTeamCount--
 	}
-	s.numCount[s.ns[s.j].team]++
-	if s.teamCount == 0 {
-		s.isGetAll = true
-	}
+	s.teamCount[s.ns[s.j].team]++
 }
 
 // 让 s.i-- 并修改相关的状态值
 func (s *status) shrink() {
-	if s.numCount[s.ns[s.i].team] == 1 {
-		s.teamCount++
+	if s.teamCount[s.ns[s.i].team] == 1 {
+		s.absentTeamCount++
 	}
-	s.numCount[s.ns[s.i].team]--
-
-	if s.teamCount > 0 {
-		s.isGetAll = false
-	}
+	s.teamCount[s.ns[s.i].team]--
 	s.i++
 }
 
 // 更新 s.res
 func (s *status) updateRes() {
-	beg, end := s.ns[s.j].n, s.ns[s.i].n
-	if s.min >= end-beg {
+	beg, end := s.ns[s.i].n, s.ns[s.j].n
+	diff := end - beg
+	if s.minDiff > diff {
 		s.res[0] = beg
 		s.res[1] = end
-		s.min = end - beg
+		s.minDiff = diff
 	}
 }
 
@@ -105,12 +102,13 @@ func makeNums(intss [][]int) nums {
 
 // nums 实现了 sort.Interface 接口
 type nums []num
+
 type num struct {
 	n, team int
 }
 
 func (n nums) Len() int { return len(n) }
 
-func (n nums) Less(i, j int) bool { return n[i].n > n[j].n }
+func (n nums) Less(i, j int) bool { return n[i].n < n[j].n }
 
 func (n nums) Swap(i, j int) { n[i], n[j] = n[j], n[i] }
