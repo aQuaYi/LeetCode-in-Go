1379efb510deba2f13a1400a404c63b8bdc4637f
aQua <aQua@aQuaYi.com>
2017-12-01 16:14:59 +0800

    668 ut pass

--- Algorithms/0668.kth-smallest-number-in-multiplication-table/kth-smallest-number-in-multiplication-table.go
+++ Algorithms/0668.kth-smallest-number-in-multiplication-table/kth-smallest-number-in-multiplication-table.go
@@ -1,9 +1,13 @@
 package Problem0668
 
 func findKthNumber(m int, n int, k int) int {
+
 	mn := m * n
 	for i := 1; i <= mn; i++ {
-		for j := 1; j <= n; j++ {
+		for j := n; 1 <= j; j-- {
+			if i/j > m {
+				break
+			}
 			if i%j == 0 {
 				k--
 				if k == 0 {
