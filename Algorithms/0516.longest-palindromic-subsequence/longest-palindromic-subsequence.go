a86f6896963d9acb35d62a996799447f1b5d084d
aQua <aQua@aQuaYi.com>
2017-12-01 15:10:28 +0800

    516 accepted. 108ms > 13ms

--- Algorithms/0516.longest-palindromic-subsequence/longest-palindromic-subsequence.go
+++ Algorithms/0516.longest-palindromic-subsequence/longest-palindromic-subsequence.go
@@ -1,7 +1,30 @@
 package Problem0516
 
 func longestPalindromeSubseq(s string) int {
-	res :=
+	n := len(s)
+	dp := make([][]int, n)
+	for i := range dp {
+		dp[i] = make([]int, n)
+		dp[i][i] = 1
+	}
 
-	return res
+	for Len := 2; Len <= n; Len++ {
+		for i := 0; i+Len-1 < n; i++ {
+			j := i + Len - 1
+			if s[i] == s[j] {
+				dp[i][j] = dp[i+1][j-1] + 2
+			} else {
+				dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+			}
+		}
+	}
+
+	return dp[0][n-1]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
