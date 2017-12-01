a98595beb34f94632db63cb87b55c1145e0f4f7a
aQua <aQua@aQuaYi.com>
2017-12-01 10:45:29 +0800

    664 wrong answer

--- Algorithms/0664.strange-printer/strange-printer.go
+++ Algorithms/0664.strange-printer/strange-printer.go
@@ -1,28 +1,41 @@
 package Problem0664
 
 func strangePrinter(s string) int {
-	res := 0
-	for i := 0; i < len(s); i++ {
-		if i > 0 &&
-			(s[i] == s[i-1] || s[i] == s[0]) {
-			continue
-		}
-		res++
+	n := len(s)
+	if n < 1 {
+		return n
+	}
+	// dp[i][j] 表示打印 s[i:j+1] 所需的最小次数
+	dp := make([][]int, n)
+	for i := range dp {
+		dp[i] = make([]int, n)
+		dp[i][i] = 1
 	}
 
-	return res
+	for Len := 2; Len <= n; Len++ {
+		for i := 0; i+Len-1 < n; i++ {
+			j := i + Len - 1
+			//
+			left := 1
+			if s[i] == s[i+1] || s[i] == s[j] {
+				left = 0
+			}
+			//
+			right := 1
+			if s[j-1] == s[j] || s[j] == s[i] {
+				right = 0
+			}
+			//
+			dp[i][j] = min(left+dp[i+1][j], dp[i][j-1]+right)
+		}
+	}
+
+	return dp[0][n-1]
 }
 
-// func strangePrinter(s string) int {
-// 	n:= len(s)
-// 	// dp[i][j] 表示打印 s[i:j+1] 所需的最小次数
-// 	dp := make([][]int, n)
-// for i:=range dp {
-// 		dp[i]= make([]int, n)
-// 		dp[i][i] = 1
-// 	}
-
-// 	res :=
-
-// 	return res
-// }
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
--- Algorithms/0664.strange-printer/strange-printer_test.go
+++ Algorithms/0664.strange-printer/strange-printer_test.go
@@ -14,6 +14,11 @@ var tcs = []struct {
 }{
 
 	{
+		"ccdcadbddbaddcbccdcdabcbcddbccdcbad",
+		17,
+	},
+
+	{
 		"tbgtgb",
 		4,
 	},
