14c8d53976f4ae2b1de56dffc958fa6c6f1fdf12
aQua <aQua@aQuaYi.com>
2017-12-01 15:13:22 +0800

    516 finish

--- Algorithms/0516.longest-palindromic-subsequence/longest-palindromic-subsequence.go
+++ Algorithms/0516.longest-palindromic-subsequence/longest-palindromic-subsequence.go
@@ -2,6 +2,8 @@ package Problem0516
 
 func longestPalindromeSubseq(s string) int {
 	n := len(s)
+
+	// dp[i][j] 表示 s[i:j+1] 中最长的回文子字符串
 	dp := make([][]int, n)
 	for i := range dp {
 		dp[i] = make([]int, n)
@@ -12,8 +14,10 @@ func longestPalindromeSubseq(s string) int {
 		for i := 0; i+Len-1 < n; i++ {
 			j := i + Len - 1
 			if s[i] == s[j] {
+				// 首位相等，则在原先的长度长 +2
 				dp[i][j] = dp[i+1][j-1] + 2
 			} else {
+				// 首位不等，则在等于去除首尾后的较长者
 				dp[i][j] = max(dp[i+1][j], dp[i][j-1])
 			}
 		}
