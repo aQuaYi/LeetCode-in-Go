468ba690e9ac7c723caf9f8115c34d8244bbf2dd
aQua <aQua@aQuaYi.com>
2017-12-01 13:59:03 +0800

    515 wrong answer

--- Algorithms/0515.find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.go
+++ Algorithms/0515.find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.go
@@ -2,12 +2,41 @@ package Problem0515
 
 import (
 	"github.com/aQuaYi/LeetCode-in-Go/kit"
+	"math"
 )
 
 type TreeNode = kit.TreeNode
 
 func largestValues(root *TreeNode) []int {
-	res := []int{1, 3, 9}
+	res := []int{}
+
+	queue := []*TreeNode{root}
+	n := 1
+	max := math.MinInt64
+
+	for len(queue)>0 {
+		node := queue[0]
+		queue = queue[1:]
+		n--
+
+		if max < node.Val {
+			max = node.Val
+		}
+
+		if node.Left != nil {
+			queue = append(queue, node.Left)
+		}
+
+		if node.Right != nil {
+			queue = append(queue, node.Right)
+		}
+
+		if n == 0 {
+			n = len(queue)
+			res = append(res, max)
+			max = math.MinInt64
+		}
+	}
 
 	return res
 }
