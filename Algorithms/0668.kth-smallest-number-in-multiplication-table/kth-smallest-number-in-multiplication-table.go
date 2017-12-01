e3c1a0cb6b8a2fe3eda6ba42f8588bcd554d06bb
aQua <aQua@aQuaYi.com>
2017-12-01 15:58:40 +0800

    668 wrong answer

--- Algorithms/0668.kth-smallest-number-in-multiplication-table/kth-smallest-number-in-multiplication-table.go
+++ Algorithms/0668.kth-smallest-number-in-multiplication-table/kth-smallest-number-in-multiplication-table.go
@@ -1,7 +1,17 @@
 package Problem0668
 
 func findKthNumber(m int, n int, k int) int {
-	res :=
+	mn := m * n
+	for i := 1; i <= mn; i++ {
+		for j := 1; j <= n; j++ {
+			if i%j == 0 {
+				k--
+				if k == 0 {
+					return i
+				}
+			}
+		}
+	}
 
-	return res
+	return mn
 }
--- Algorithms/0668.kth-smallest-number-in-multiplication-table/kth-smallest-number-in-multiplication-table_test.go
+++ Algorithms/0668.kth-smallest-number-in-multiplication-table/kth-smallest-number-in-multiplication-table_test.go
@@ -9,32 +9,39 @@ import (
 
 // tcs is testcase slice
 var tcs = []struct {
-	m int
- n int
- k int
-	ans  int 
+	m   int
+	n   int
+	k   int
+	ans int
 }{
 
-{
- 3,
- 3,
- 5,
- 3	,
-},
+	{
+		3,
+		8,
+		19,
+		14,
+	},
 
-{
- 2,
- 3,
- 6,
- 6 ,
-},
+	{
+		3,
+		3,
+		5,
+		3,
+	},
+
+	{
+		2,
+		3,
+		6,
+		6,
+	},
 
 	// 可以有多个 testcase
 }
 
 func Test_findKthNumber(t *testing.T) {
 	ast := assert.New(t)
-	
+
 	for _, tc := range tcs {
 		fmt.Printf("~~%v~~\n", tc)
 		ast.Equal(tc.ans, findKthNumber(tc.m, tc.n, tc.k), "输入:%v", tc)
