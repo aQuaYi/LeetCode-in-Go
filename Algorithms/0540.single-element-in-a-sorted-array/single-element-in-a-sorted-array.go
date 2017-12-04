b06d968b2adc2c9af009de3cc118017026e81276
aQua <aQua@aQuaYi.com>
2017-12-04 16:07:36 +0800

    540 感觉判断逻辑好复杂

--- Algorithms/0540.single-element-in-a-sorted-array/single-element-in-a-sorted-array.go
+++ Algorithms/0540.single-element-in-a-sorted-array/single-element-in-a-sorted-array.go
@@ -1,6 +1,14 @@
 package Problem0540
 
 func singleNonDuplicate(nums []int) int {
+	n := len(nums)-1
+	lo,hi:= 0 , n-1
+	for lo< hi {
+		mid := lo + (hi-lo)/2
+		if (mid == 0&& nums[0]!= nums[1])||
+		(mid == n-1 && nums[n-2]!=nums[n-1])||
+		(nums[mid-1]!=)
+	}
 	res :=
 
 	return res
