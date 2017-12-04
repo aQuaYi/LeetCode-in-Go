8d7e3965e86e35ab475ea639e81765433f7b42cb
aQua <aQua@aQuaYi.com>
2017-12-04 16:44:54 +0800

    540 accepted

--- Algorithms/0540.single-element-in-a-sorted-array/single-element-in-a-sorted-array.go
+++ Algorithms/0540.single-element-in-a-sorted-array/single-element-in-a-sorted-array.go
@@ -1,16 +1,23 @@
 package Problem0540
 
 func singleNonDuplicate(nums []int) int {
-	n := len(nums)-1
-	lo,hi:= 0 , n-1
-	for lo< hi {
-		mid := lo + (hi-lo)/2
-if (mid %2==0 && (mid == n-1 || nums[mid]!= nums[mid+1]))||
-(mid%2==1&&(mid==0|| nums[mid-1]!= nums[mid])) {
-	return nums[mid]
-}
-	}
-	res :=
+	n := len(nums) - 1
+	lo, hi := 0, n-1
 
-	return res
+	//
+	for lo < hi {
+		mid := lo + (hi-lo)/2
+		if mid%2 != 0 {
+			mid--
+		}
+
+		if nums[mid] != nums[mid+1] {
+			hi = mid
+		} else {
+			lo = mid + 2
+		}
+
+	}
+
+	return nums[lo]
 }
