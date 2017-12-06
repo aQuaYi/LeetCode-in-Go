c2ffe17c8a8765bded0798f77a5c9a258e587478
aQua <aQua@aQuaYi.com>
2017-12-06 11:53:07 +0800

    543 added

---
+++ Algorithms/0543.diameter-of-binary-tree/diameter-of-binary-tree.go
@@ -0,0 +1,13 @@
+package Problem0543
+
+import (
+	"github.com/aQuaYi/LeetCode-in-Go/kit"
+)
+
+type TreeNode = kit.TreeNode
+
+func diameterOfBinaryTree(root *TreeNode) int {
+	res :=0
+
+	return res
+}
---
+++ Algorithms/0543.diameter-of-binary-tree/diameter-of-binary-tree_test.go
@@ -0,0 +1,44 @@
+package Problem0543
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aQuaYi/LeetCode-in-Go/kit"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	pre, in []int
+	ans     int
+}{
+
+	{
+		[]int{1, 2, 4, 5, 3},
+		[]int{4, 2, 5, 1, 3},
+		3,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_diameterOfBinaryTree(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		root := kit.PreIn2Tree(tc.pre, tc.in)
+		ast.Equal(tc.ans, diameterOfBinaryTree(root), "输入:%v", tc)
+	}
+}
+
+func Benchmark_diameterOfBinaryTree(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			root := kit.PreIn2Tree(tc.pre, tc.in)
+			diameterOfBinaryTree(root)
+		}
+	}
+}
