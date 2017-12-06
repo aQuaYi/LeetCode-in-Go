94d609f4a0aea0aa468a7a222b2e2a3503f86e41
aQua <aQua@aQuaYi.com>
2017-12-06 10:06:06 +0800

    529 added

--- Algorithms/0529.minesweeper/minesweeper.go
+++ Algorithms/0529.minesweeper/minesweeper.go
@@ -1,7 +1,10 @@
 package Problem0529
 
+var dx = []int{-1, -1, -1, 0, 0, 1, 1, 1}
+var dy = []int{-1, 0, 1, -1, 1, -1, 0, 1}
+
 func updateBoard(board [][]byte, click []int) [][]byte {
-	res :=
+	res := board
 
 	return res
 }
--- Algorithms/0529.minesweeper/minesweeper_test.go
+++ Algorithms/0529.minesweeper/minesweeper_test.go
@@ -10,18 +10,44 @@ import (
 // tcs is testcase slice
 var tcs = []struct {
 	board [][]byte
- click []int
-	ans  [][]byte 
+	click []int
+	ans   [][]byte
 }{
 
+	{
+		[][]byte{
+			{'E', 'E', 'E', 'E', 'E'},
+			{'E', 'E', 'M', 'E', 'E'},
+			{'E', 'E', 'E', 'E', 'E'},
+			{'E', 'E', 'E', 'E', 'E'}},
+		[]int{3, 0},
+		[][]byte{
+			{'B', '1', 'E', '1', 'B'},
+			{'B', '1', 'M', '1', 'B'},
+			{'B', '1', '1', '1', 'B'},
+			{'B', 'B', 'B', 'B', 'B'}},
+	},
 
-	
+	{
+		[][]byte{
+			{'B', '1', 'E', '1', 'B'},
+			{'B', '1', 'M', '1', 'B'},
+			{'B', '1', '1', '1', 'B'},
+			{'B', 'B', 'B', 'B', 'B'}},
+		[]int{1, 2},
+		[][]byte{
+			{'B', '1', 'E', '1', 'B'},
+			{'B', '1', 'X', '1', 'B'},
+			{'B', '1', '1', '1', 'B'},
+			{'B', 'B', 'B', 'B', 'B'}},
+	},
+
 	// 可以有多个 testcase
 }
 
 func Test_updateBoard(t *testing.T) {
 	ast := assert.New(t)
-	
+
 	for _, tc := range tcs {
 		fmt.Printf("~~%v~~\n", tc)
 		ast.Equal(tc.ans, updateBoard(tc.board, tc.click), "输入:%v", tc)
