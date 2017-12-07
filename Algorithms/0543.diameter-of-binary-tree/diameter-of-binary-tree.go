f4aeb34369c755c8172f302a3304f47d75a323af
aQua <aQua@aQuaYi.com>
2017-12-07 09:11:10 +0800

    543 accepted. 19ms > 9ms

--- Algorithms/0543.diameter-of-binary-tree/diameter-of-binary-tree.go
+++ Algorithms/0543.diameter-of-binary-tree/diameter-of-binary-tree.go
@@ -7,7 +7,26 @@ import (
 type TreeNode = kit.TreeNode
 
 func diameterOfBinaryTree(root *TreeNode) int {
-	res :=0
+	_, res := helper(root)
 
 	return res
+}
+
+func helper(root *TreeNode) (length, diameter int) {
+	if root == nil {
+		return 0,0 
+	}
+
+	leftLen, leftDia := helper(root.Left)
+	rightLen, rightDia := helper(root.Right)
+
+	return max(leftLen, rightLen) + 1, max(leftLen+rightLen, max(leftDia, rightDia))
+
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
