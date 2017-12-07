d20e507d49bcfbd8583df3f57175a4bd6dea036f
aQua <aQua@aQuaYi.com>
2017-12-07 17:15:38 +0800

    542 finish

--- Algorithms/0542.01-matrix/01-matrix.go
+++ Algorithms/0542.01-matrix/01-matrix.go
@@ -2,61 +2,43 @@ package Problem0542
 
 func updateMatrix(matrix [][]int) [][]int {
 	m, n := len(matrix), len(matrix[0])
+	MIN := m * n
 
-	for i := 0; i < m; i++ {
-		for j := 0; j < n; j++ {
-			if matrix[i][j] != 0 {
-				matrix[i][j] = m + n
-			}
-		}
-	}
-
+	// 根据 左方 和 上方 的格子，更新 (i,j)
 	for i := 0; i < m; i++ {
 		for j := 0; j < n; j++ {
 			if matrix[i][j] == 0 {
 				continue
 			}
-			checkLU(matrix, m, n, i, j)
+
+			matrix[i][j] = MIN
+
+			if 0 <= i-1 {
+				matrix[i][j] = min(matrix[i][j], matrix[i-1][j]+1)
+			}
+			if 0 <= j-1 {
+				matrix[i][j] = min(matrix[i][j], matrix[i][j-1]+1)
+			}
 		}
 	}
 
+	// 根据 右方 和 下方 的格子，更新 (i,j)
 	for i := m - 1; 0 <= i; i-- {
 		for j := n - 1; 0 <= j; j-- {
 			if matrix[i][j] == 0 {
 				continue
 			}
-			checkRD(matrix, m, n, i, j)
+
+			if i+1 < m {
+				matrix[i][j] = min(matrix[i][j], matrix[i+1][j]+1)
+			}
+			if j+1 < n {
+				matrix[i][j] = min(matrix[i][j], matrix[i][j+1]+1)
+			}
 		}
 	}
 
 	return matrix
-}
-
-var dx = []int{0, -1, 1, 0}
-var dy = []int{-1, 0, 0, 1}
-
-func checkLU(mat [][]int, m, n, i, j int) {
-	var x, y int
-	for k := 0; k < 2; k++ {
-		x = i + dx[k]
-		y = j + dy[k]
-		if 0 <= x && x < m &&
-			0 <= y && y < n {
-			mat[i][j] = min(mat[i][j], mat[x][y]+1)
-		}
-	}
-}
-
-func checkRD(mat [][]int, m, n, i, j int) {
-	var x, y int
-	for k := 2; k < 4; k++ {
-		x = i + dx[k]
-		y = j + dy[k]
-		if 0 <= x && x < m &&
-			0 <= y && y < n {
-			mat[i][j] = min(mat[i][j], mat[x][y]+1)
-		}
-	}
 }
 
 func min(a, b int) int {
