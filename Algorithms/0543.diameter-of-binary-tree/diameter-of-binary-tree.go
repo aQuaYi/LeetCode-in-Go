cdf7adfd2f6ba4982b609f322cea42ef50e51d05
aQua <aQua@aQuaYi.com>
2017-12-07 09:19:25 +0800

    543 finish

--- Algorithms/0543.diameter-of-binary-tree/diameter-of-binary-tree.go
+++ Algorithms/0543.diameter-of-binary-tree/diameter-of-binary-tree.go
@@ -8,20 +8,20 @@ type TreeNode = kit.TreeNode
 
 func diameterOfBinaryTree(root *TreeNode) int {
 	_, res := helper(root)
-
 	return res
 }
 
 func helper(root *TreeNode) (length, diameter int) {
 	if root == nil {
-		return 0,0 
+		return
 	}
 
 	leftLen, leftDia := helper(root.Left)
 	rightLen, rightDia := helper(root.Right)
 
-	return max(leftLen, rightLen) + 1, max(leftLen+rightLen, max(leftDia, rightDia))
-
+	length = max(leftLen, rightLen) + 1
+	diameter = max(leftLen+rightLen, max(leftDia, rightDia))
+	return
 }
 
 func max(a, b int) int {
