38799ed95f2dae5188b2ea809de75193be8b614d
aQua <aQua@aQuaYi.com>
2017-12-07 16:47:10 +0800

    542 accepted. 612ms > 442ms

--- Algorithms/0542.01-matrix/01-matrix.go
+++ Algorithms/0542.01-matrix/01-matrix.go
@@ -1,7 +1,55 @@
 package Problem0542
 
 func updateMatrix(matrix [][]int) [][]int {
-	res :=
+	m, n := len(matrix), len(matrix[0])
 
-	return res
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if matrix[i][j] != 0 {
+				matrix[i][j] = m + n
+			}
+		}
+	}
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if matrix[i][j] == 0 {
+				continue
+			}
+			check(matrix, m, n, i, j)
+		}
+	}
+
+	for i := m - 1; 0 <= i; i-- {
+		for j := n - 1; 0 <= j; j-- {
+			if matrix[i][j] == 0 {
+				continue
+			}
+			check(matrix, m, n, i, j)
+		}
+	}
+
+	return matrix
+}
+
+var dx = []int{-1, 1, 0, 0}
+var dy = []int{0, 0, -1, 1}
+
+func check(mat [][]int, m, n, i, j int) {
+	var x, y int
+	for k := 0; k < 4; k++ {
+		x = i + dx[k]
+		y = j + dy[k]
+		if 0 <= x && x < m &&
+			0 <= y && y < n {
+			mat[i][j] = min(mat[i][j], mat[x][y]+1)
+		}
+	}
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
