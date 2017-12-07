fc7c61e3a3a401a981666f148a881924dda1f842
aQua <aQua@aQuaYi.com>
2017-12-07 15:29:20 +0800

    539 finish

--- Algorithms/0539.minimum-time-difference/minimum-time-difference.go
+++ Algorithms/0539.minimum-time-difference/minimum-time-difference.go
@@ -6,9 +6,11 @@ import (
 )
 
 func findMinDifference(timePoints []string) int {
-	sort.Strings(timePoints)
 	n := len(timePoints)
+	// 把时间按升序排列
+	sort.Strings(timePoints)
 
+	// 把 minDiff 设置为 timePoints[n-1] 到 timePoints[0] 的分钟数
 	minDiff := 24*60 - diff(timePoints[0], timePoints[n-1])
 
 	for i := 1; i < n; i++ {
@@ -18,15 +20,14 @@ func findMinDifference(timePoints []string) int {
 	return minDiff
 }
 
-// 返回 从 a 到 b 的分钟数
+// 返回从 a 到 b 的分钟数
 func diff(a, b string) int {
-	ha, ma := analyze(a)
-	hb, mb := analyze(b)
-
+	ha, ma := getHourAndMintueOf(a)
+	hb, mb := getHourAndMintueOf(b)
 	return (hb-ha)*60 + (mb - ma)
 }
 
-func analyze(s string) (hour, minute int) {
+func getHourAndMintueOf(s string) (hour, minute int) {
 	hour, _ = strconv.Atoi(s[:2])
 	minute, _ = strconv.Atoi(s[3:])
 	return
