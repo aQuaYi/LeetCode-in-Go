791ee3f35e7920c6fe3b2a550cbaa5b31c69b642
aQua <aQua@aQuaYi.com>
2017-12-07 15:22:07 +0800

    539 accepted. 19ms < 22ms

--- Algorithms/0539.minimum-time-difference/minimum-time-difference.go
+++ Algorithms/0539.minimum-time-difference/minimum-time-difference.go
@@ -1,7 +1,40 @@
 package Problem0539
 
-func findMinDifference(timePoints []string) int {
-	res := 1
+import (
+	"sort"
+	"strconv"
+)
 
-	return res
+func findMinDifference(timePoints []string) int {
+	sort.Strings(timePoints)
+	n := len(timePoints)
+
+	minDiff := 24*60 - diff(timePoints[0], timePoints[n-1])
+
+	for i := 1; i < n; i++ {
+		minDiff = min(minDiff, diff(timePoints[i-1], timePoints[i]))
+	}
+
+	return minDiff
+}
+
+// 返回 从 a 到 b 的分钟数
+func diff(a, b string) int {
+	ha, ma := analyze(a)
+	hb, mb := analyze(b)
+
+	return (hb-ha)*60 + (mb - ma)
+}
+
+func analyze(s string) (hour, minute int) {
+	hour, _ = strconv.Atoi(s[:2])
+	minute, _ = strconv.Atoi(s[3:])
+	return
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
