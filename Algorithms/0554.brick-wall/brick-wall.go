05d6c114704264da9706f471a0be187dbb0ea39e
aQua <aQua@aQuaYi.com>
2017-12-08 15:11:03 +0800

    554 finish

--- Algorithms/0554.brick-wall/brick-wall.go
+++ Algorithms/0554.brick-wall/brick-wall.go
@@ -3,17 +3,21 @@ package Problem0554
 func leastBricks(wall [][]int) int {
 	m := len(wall)
 
-	rec := make(map[int]int, m)
-	max := 0
+	// 统计坐标相同的 edge 出现的次数
+	count := make(map[int]int, m)
 
 	for i := 0; i < m; i++ {
 		sum := wall[i][0]
 		for j := 1; j < len(wall[i]); j++ {
-			rec[sum]++
-			if max < rec[sum] {
-				max = rec[sum]
-			}
+			count[sum]++
 			sum += wall[i][j]
+		}
+	}
+
+	max := 0
+	for _, edges := range count {
+		if max < edges {
+			max = edges
 		}
 	}
 
