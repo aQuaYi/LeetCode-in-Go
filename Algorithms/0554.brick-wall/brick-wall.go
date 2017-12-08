5d5d67fa12d3c93626347f3b0d0932d0527b4bdc
aQua <aQua@aQuaYi.com>
2017-12-08 14:55:33 +0800

    554 added

---
+++ Algorithms/0554.brick-wall/brick-wall.go
@@ -0,0 +1,7 @@
+package Problem0554
+
+func leastBricks(wall [][]int) int {
+	res := 2
+
+	return res
+}
---
+++ Algorithms/0554.brick-wall/brick-wall_test.go
@@ -0,0 +1,44 @@
+package Problem0554
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	wall [][]int
+	ans  int
+}{
+
+	{
+		[][]int{{1, 2, 2, 1},
+			{3, 1, 2},
+			{1, 3, 2},
+			{2, 4},
+			{3, 1, 2},
+			{1, 3, 1, 1}},
+		2,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_leastBricks(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, leastBricks(tc.wall), "输入:%v", tc)
+	}
+}
+
+func Benchmark_leastBricks(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			leastBricks(tc.wall)
+		}
+	}
+}
