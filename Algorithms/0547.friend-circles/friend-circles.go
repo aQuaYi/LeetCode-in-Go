5733eace100b41247eaefe308d3b695a9303fce0
aQua <aQua@aQuaYi.com>
2017-12-08 16:33:11 +0800

    547 wrong answer

--- Algorithms/0547.friend-circles/friend-circles.go
+++ Algorithms/0547.friend-circles/friend-circles.go
@@ -1,49 +1,27 @@
 package Problem0547
 
-var dx = []int{-1, 1, 0, 0}
-var dy = []int{0, 0, -1, 1}
-
 func findCircleNum(M [][]int) int {
 	res := 0
 	N := len(M)
 
-	isChecked := make([]bool, N*N)
+	var group func(int)
+	group = func(i int) {
+		if M[i][i] == 0 {
+			return
+		}
 
-	check := func(i, j int) {
-		xs := make([]int, 1, N)
-		ys := make([]int, 1, N)
-		xs[0] = i
-		ys[0] = j
-
-		for len(xs) > 0 {
-			i = xs[0]
-			j = ys[0]
-			xs = xs[1:]
-			ys = ys[1:]
-
-			isChecked[i*N+j] = true
-
-			for k := 0; k < 4; k++ {
-				x := i + dx[k]
-				y := j + dy[k]
-
-				if 0 <= x && x < N &&
-					0 <= y && y <= x &&
-					M[x][y] == 1 &&
-					!isChecked[x*N+y] {
-					xs = append(xs, x)
-					ys = append(ys, y)
-				}
+		M[i][i] = 0
+		for j := i + 1; j < N; j++ {
+			if M[i][j] == 1 {
+				group(j)
 			}
 		}
 	}
 
 	for i := 0; i < N; i++ {
-		for j := 0; j <= i; j++ {
-			if M[i][j] == 1 && !isChecked[i*N+j] {
-				res++
-				check(i, j)
-			}
+		if M[i][i] == 1 {
+			res++
+			group(i)
 		}
 	}
 
