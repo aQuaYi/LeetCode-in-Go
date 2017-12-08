724dc073d44cff8604e54f582a35283dfcd78550
aQua <aQua@aQuaYi.com>
2017-12-08 15:04:18 +0800

    554 accepted. 59ms > 56ms

--- Algorithms/0554.brick-wall/brick-wall.go
+++ Algorithms/0554.brick-wall/brick-wall.go
@@ -1,7 +1,24 @@
 package Problem0554
 
 func leastBricks(wall [][]int) int {
-	res := 2
+	m := len(wall)
 
-	return res
+	rec := make(map[int]int, m)
+
+	for i := 0; i < m; i++ {
+		sum := wall[i][0]
+		for j := 1; j < len(wall[i]); j++ {
+			rec[sum]++
+			sum += wall[i][j]
+		}
+	}
+
+	max := 0
+	for _, bricks := range rec {
+		if max < bricks {
+			max = bricks
+		}
+	}
+
+	return m - max
 }
