ef9e91d89ddb4335adae49957540efb7e0a44547
aQua <aQua@aQuaYi.com>
2017-12-13 16:59:22 +0800

    598 added

---
+++ Algorithms/0598.range-addition-ii/range-addition-ii.go
@@ -0,0 +1,7 @@
+package Problem0598
+
+func maxCount(m int, n int, ops [][]int) int {
+	res := 4
+
+	return res
+}
---
+++ Algorithms/0598.range-addition-ii/range-addition-ii_test.go
@@ -0,0 +1,43 @@
+package Problem0598
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	m   int
+	n   int
+	ops [][]int
+	ans int
+}{
+
+	{
+		3,
+		3,
+		[][]int{{2, 2}, {3, 3}},
+		4,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_maxCount(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, maxCount(tc.m, tc.n, tc.ops), "输入:%v", tc)
+	}
+}
+
+func Benchmark_maxCount(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			maxCount(tc.m, tc.n, tc.ops)
+		}
+	}
+}
