2ef0bd84e8070ae0549c8b1e326bd68f397df019
aQua <aQua@aQuaYi.com>
2017-12-13 17:02:04 +0800

    598 accepted

--- Algorithms/0598.range-addition-ii/range-addition-ii.go
+++ Algorithms/0598.range-addition-ii/range-addition-ii.go
@@ -1,7 +1,16 @@
 package Problem0598
 
 func maxCount(m int, n int, ops [][]int) int {
-	res := 4
+	for _, o := range ops {
+		m = min(m, o[0])
+		n = min(n, o[1])
+	}
+	return m * n
+}
 
-	return res
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
