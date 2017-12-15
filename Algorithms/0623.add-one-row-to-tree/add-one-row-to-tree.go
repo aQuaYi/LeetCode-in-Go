0ffe0f260f34d343d3a785f9583f4e046fbb50e0
aQua <aQua@aQuaYi.com>
2017-12-15 20:17:12 +0800

    623 accepted. 125ms > 102ms

--- Algorithms/0623.add-one-row-to-tree/add-one-row-to-tree.go
+++ Algorithms/0623.add-one-row-to-tree/add-one-row-to-tree.go
@@ -7,6 +7,38 @@ import (
 type TreeNode = kit.TreeNode
 
 func addOneRow(root *TreeNode, v int, d int) *TreeNode {
-
+	if d == 1 {
+		newRoot := newNode(v)
+		newRoot.Left = root
+		return newRoot
+	}
+	helper(root, v, d)
 	return root
+}
+
+func helper(root *TreeNode, v, d int) {
+	if d == 2 {
+		tl := root.Left
+		root.Left = newNode(v)
+		root.Left.Left = tl
+		tr := root.Right
+		root.Right = newNode(v)
+		root.Right.Right = tr
+		return
+	}
+
+	if root.Left != nil {
+		helper(root.Left, v, d-1)
+	}
+
+	if root.Right != nil {
+		helper(root.Right, v, d-1)
+	}
+
+}
+
+func newNode(v int) *TreeNode {
+	return &TreeNode{
+		Val: v,
+	}
 }
