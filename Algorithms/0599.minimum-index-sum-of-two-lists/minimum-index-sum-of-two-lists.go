07ac2afc1a98a42c1301aecd14522a6f45323c0f
aQua <aQua@aQuaYi.com>
2017-12-15 21:02:35 +0800

    599 added

---
+++ Algorithms/0599.minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.go
@@ -0,0 +1,6 @@
+package Problem0599
+
+func findRestaurant(list1 []string, list2 []string) []string {
+
+	return list1
+}
---
+++ Algorithms/0599.minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists_test.go
@@ -0,0 +1,45 @@
+package Problem0599
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	list1, list2, ans []string
+}{
+
+	{
+		[]string{"Shogun", "Tapioca Express", "Burger King", "KFC"},
+		[]string{"Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"},
+		[]string{"Shogun"},
+	},
+
+	{
+		[]string{"Shogun", "Tapioca Express", "Burger King", "KFC"},
+		[]string{"KFC", "Shogun", "Burger King"},
+		[]string{"Shogun"},
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_fcName(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, findRestaurant(tc.list1, tc.list2), "输入:%v", tc)
+	}
+}
+
+func Benchmark_fcName(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			findRestaurant(tc.list1, tc.list2)
+		}
+	}
+}
