7eced7eec2e6c74103476d3ddf0a8f3b9eab070c
aQua <aQua@aQuaYi.com>
2017-12-18 19:52:56 +0800

    654 added

---
+++ Algorithms/0654.maximum-binary-tree/maximum-binary-tree.go
@@ -0,0 +1,12 @@
+package Problem0654
+
+import (
+	"github.com/aQuaYi/LeetCode-in-Go/kit"
+)
+
+type TreeNode = kit.TreeNode
+
+func constructMaximumBinaryTree(nums []int) *TreeNode {
+
+	return nil
+}
