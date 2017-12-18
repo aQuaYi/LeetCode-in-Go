aeb35cea8f5d05a7509d8e7bb0a7c04d7cf9b1aa
aQua <aQua@aQuaYi.com>
2017-12-18 11:18:02 +0800

    567 finish

--- Algorithms/0567.permutation-in-string/permutation-in-string.go
+++ Algorithms/0567.permutation-in-string/permutation-in-string.go
@@ -1,25 +1,34 @@
 package Problem0567
 
-func checkInclusion(s1, s2 string) bool {
-	n := len(s1)
-	b1 := []byte(s1)
-	b2 := []byte(s2)
-	for i := 0; i+n <= len(b2); i++ {
-		if isPermutation(b1, b2[i:i+n]) {
+func checkInclusion(s1 string, s2 string) bool {
+	n1 := len(s1)
+	n2 := len(s2)
+	if n1 > n2 {
+		return false
+	}
+
+	h1 := 0
+	h2 := 0
+	for i := 0; i < n1; i++ {
+		c1 := s1[i] - 'a'
+		c2 := s2[i] - 'a'
+		h1 += 1 << c1
+		h2 += 1 << c2
+	}
+
+	if h1 == h2 {
+		return true
+	}
+
+	for i := n1; i < n2; i++ {
+		cb := s2[i-n1] - 'a'
+		ce := s2[i] - 'a'
+		// 利用 cb 和 ce 更新 h2
+		h2 += (1 << ce) - (1 << cb)
+		if h1 == h2 {
 			return true
 		}
 	}
-	return false
-}
 
-func isPermutation(s1, s2 []byte) bool {
-	var xor byte
-	var sum1, sum2 int
-	n := len(s1)
-	for i := 0; i < n; i++ {
-		xor ^= s1[i] ^ s2[i]
-		sum1 += int(s1[i]) * int(s1[i])
-		sum2 += int(s2[i]) * int(s2[i])
-	}
-	return xor == 0 && sum1 == sum2
+	return false
 }
