8564e38b5b1255820d8c35d6d74fca56a7af5061
aQua <aQua@aQuaYi.com>
2017-12-18 16:58:30 +0800

    647 accepted. 103ms > 3ms

--- Algorithms/0647.palindromic-substrings/palindromic-substrings.go
+++ Algorithms/0647.palindromic-substrings/palindromic-substrings.go
@@ -1,7 +1,26 @@
 package Problem0647
 
 func countSubstrings(s string) int {
-	res := 0
-
+	res := len(s)
+	bs := []byte(s)
+	for i := 0; i < len(s); i++ {
+		for j := i + 2; j <= len(s); j++ {
+			if isPalindromic(bs[i:j]) {
+				res++
+			}
+		}
+	}
 	return res
+}
+
+func isPalindromic(bs []byte) bool {
+	i, j := 0, len(bs)-1
+	for i < j {
+		if bs[i] != bs[j] {
+			return false
+		}
+		i++
+		j--
+	}
+	return true
 }
