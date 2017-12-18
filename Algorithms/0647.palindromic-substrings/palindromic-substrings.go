44a3a70dd9246be316897e2f96f2411d3c1d72ac
aQua <aQua@aQuaYi.com>
2017-12-18 17:15:44 +0800

    647 added

--- Algorithms/0647.palindromic-substrings/palindromic-substrings.go
+++ Algorithms/0647.palindromic-substrings/palindromic-substrings.go
@@ -1,26 +1,23 @@
 package Problem0647
 
 func countSubstrings(s string) int {
-	res := len(s)
-	bs := []byte(s)
+	count := 0
+	// 换一个思路
 	for i := 0; i < len(s); i++ {
-		for j := i + 2; j <= len(s); j++ {
-			if isPalindromic(bs[i:j]) {
-				res++
-			}
-		}
+		// 统计所有以 i 为中心的回文
+		count += extendPalindrome(s, i, i)
+		// 统计所有以 i 和 i+1 为中心的回文
+		count += extendPalindrome(s, i, i+1)
 	}
-	return res
+	return count
 }
 
-func isPalindromic(bs []byte) bool {
-	i, j := 0, len(bs)-1
-	for i < j {
-		if bs[i] != bs[j] {
-			return false
-		}
-		i++
-		j--
+func extendPalindrome(s string, left, right int) int {
+	res := 0
+	for left >= 0 && right < len(s) && s[left] == s[right] {
+		res++
+		left--
+		right++
 	}
-	return true
+	return res
 }
