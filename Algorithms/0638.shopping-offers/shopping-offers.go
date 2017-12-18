8ebcdcfdc4737ab6f08296183c2e7f8b0743580b
aQua <aQua@aQuaYi.com>
2017-12-18 15:44:58 +0800

    638 added

---
+++ Algorithms/0638.shopping-offers/shopping-offers.go
@@ -0,0 +1,7 @@
+package Problem0638
+
+func shoppingOffers(price []int, special [][]int, needs []int) int {
+	res := 0
+
+	return res
+}
---
+++ Algorithms/0638.shopping-offers/shopping-offers_test.go
@@ -0,0 +1,36 @@
+package Problem0638
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	price   []int
+	special [][]int
+	needs   []int
+	ans     int
+}{
+
+// 可以有多个 testcase
+}
+
+func Test_fn(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, shoppingOffers(tc.price, tc.special, tc.needs), "输入:%v", tc)
+	}
+}
+
+func Benchmark_fn(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			shoppingOffers(tc.price, tc.special, tc.needs)
+		}
+	}
+}
