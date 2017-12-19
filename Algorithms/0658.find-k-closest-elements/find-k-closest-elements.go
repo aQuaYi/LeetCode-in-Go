48748f0395d37b2e652b08413276105fb6cc8c49
aQua <aQua@aQuaYi.com>
2017-12-19 15:54:01 +0800

    658 越改越慢

--- Algorithms/0658.find-k-closest-elements/find-k-closest-elements.go
+++ Algorithms/0658.find-k-closest-elements/find-k-closest-elements.go
@@ -6,12 +6,6 @@ import (
 
 func findClosestElements(a []int, k int, x int) []int {
 	i := sort.SearchInts(a, x)
-	if i == 0 && x < a[0] {
-		return a[:k]
-	}
-	if i >= len(a)-1 {
-		return a[len(a)-k:]
-	}
 
 	l, r := i-1, i
 	for r-l-1 < k {
