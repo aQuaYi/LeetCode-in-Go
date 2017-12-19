6d67f181dffc9c2cbbb363ee315bc2be51406765
aQua <aQua@aQuaYi.com>
2017-12-19 14:50:04 +0800

    655 accepted. 6ms < 12ms

--- Algorithms/0655.print-binary-tree/print-binary-tree.go
+++ Algorithms/0655.print-binary-tree/print-binary-tree.go
@@ -2,11 +2,79 @@ package Problem0655
 
 import (
 	"github.com/aQuaYi/LeetCode-in-Go/kit"
+	"strconv"
 )
 
 type TreeNode = kit.TreeNode
 
 func printTree(root *TreeNode) [][]string {
+	level := getLevel(root)
+	// loc 是 root　节点的位置
+	loc := 1 << uint(level-1)
 
-	return nil
+	size := 1 << uint(level)-1
+	res := make([][]string, level)
+	for i := range res {
+		res[i] = make([]string, size)
+	}
+
+	getRes(root, 0,  loc-1, res)
+
+	return res
 }
+
+func getRes(root *TreeNode, i, j int, res [][]string) {
+	if root == nil {
+		return
+	}
+
+	res[i][j] = strconv.Itoa(root.Val)
+
+	level := len(res)
+	if level-i-2 < 0 {
+		return
+	}
+
+	diff := 1 << uint(level-i-2)
+
+	getRes(root.Left, i+1, j-diff, res)
+	getRes(root.Right, i+1, j+diff, res)
+}
+
+// 返回树的层数
+func getLevel(root *TreeNode) (res int) {
+	res = 1
+	queue := make([]*TreeNode, 1, 1024)
+	queue[0] = root
+
+	for {
+		hasChild := false
+		for i := 0; i < len(queue); i++ {
+			if queue[i].Left != nil || queue[i].Right != nil {
+				hasChild = true
+				break
+			}
+		}
+
+		if !hasChild {
+			break
+		}
+
+		res++
+
+		fQueue := queue[:len(queue)]
+		queue = queue[len(queue):]
+
+		for i := 0; i < len(fQueue); i++ {
+			if fQueue[i].Left != nil {
+				queue = append(queue, fQueue[i].Left)
+			}
+			if fQueue[i].Right != nil {
+				queue = append(queue, fQueue[i].Right)
+			}
+		}
+	}
+
+	return res
+}
+
