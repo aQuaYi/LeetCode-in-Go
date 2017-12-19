fc0c3a209ab919e40a9bdb562ac31ea32460c35d
aQua <aQua@aQuaYi.com>
2017-12-19 15:48:21 +0800

    658 accepted. 635ms > 412ms

--- Algorithms/0658.find-k-closest-elements/find-k-closest-elements.go
+++ Algorithms/0658.find-k-closest-elements/find-k-closest-elements.go
@@ -1,5 +1,36 @@
 package Problem0658
 
-func findClosestElements(arr []int, k int, x int) []int {
-	return nil
+import (
+	"sort"
+)
+
+func findClosestElements(a []int, k int, x int) []int {
+	i := sort.SearchInts(a, x)
+	if i == 0 && x < a[0] {
+		return a[:k]
+	}
+	if i >= len(a)-1 {
+		return a[len(a)-k:]
+	}
+
+	l, r := i-1, i
+	for r-l-1 < k {
+		if l == -1 {
+			r++
+			continue
+		}
+
+		if r == len(a) {
+			l--
+			continue
+		}
+
+		if a[l]+a[r] >= 2*x {
+			l--
+		} else {
+			r++
+		}
+	}
+
+	return a[l+1 : r]
 }
