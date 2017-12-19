a5215160785da18a4b0b48aae5ff84513ec2d894
aQua <aQua@aQuaYi.com>
2017-12-19 16:12:08 +0800

    658 使用二分查找法

--- Algorithms/0658.find-k-closest-elements/find-k-closest-elements.go
+++ Algorithms/0658.find-k-closest-elements/find-k-closest-elements.go
@@ -1,30 +1,15 @@
 package Problem0658
 
-import (
-	"sort"
-)
-
 func findClosestElements(a []int, k int, x int) []int {
-	i := sort.SearchInts(a, x)
-
-	l, r := i-1, i
-	for r-l-1 < k {
-		if l == -1 {
-			r++
-			continue
-		}
-
-		if r == len(a) {
-			l--
-			continue
-		}
-
-		if a[l]+a[r] >= 2*x {
-			l--
+	lo, hi := 0, len(a)-k
+	for lo < hi {
+		mid := lo + (hi-lo)/2
+		if x-a[mid] > a[mid+k]-x {
+			lo = mid + 1
 		} else {
-			r++
+			hi = mid
 		}
 	}
 
-	return a[l+1 : r]
+	return a[lo : lo+k]
 }
