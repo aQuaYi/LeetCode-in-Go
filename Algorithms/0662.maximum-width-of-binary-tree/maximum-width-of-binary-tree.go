7cead15a7d4abb74a9b4ab793711624a2ecee0c4
aQua <aQua@aQuaYi.com>
2017-12-20 11:36:44 +0800

    662 finish

--- Algorithms/0662.maximum-width-of-binary-tree/maximum-width-of-binary-tree.go
+++ Algorithms/0662.maximum-width-of-binary-tree/maximum-width-of-binary-tree.go
@@ -10,6 +10,10 @@ func widthOfBinaryTree(root *TreeNode) int {
 	if root == nil  {
 		return 0
 	}
+
+	// locQueue[i] = j 表示
+	// 当 树 是满的时候，
+	// nodeQueue[i] 节点在其所在层的索引号为 j
 	locQueue := make([]int, 1, 1024)
 	nodeQueue := make([]*TreeNode, 1, 1024)
 	locQueue[0] = 0
@@ -17,14 +21,16 @@ func widthOfBinaryTree(root *TreeNode) int {
 
 	res := 0
 
+	// 使用 BFS 来遍历树
 	for len(locQueue) > 0 {
 		res = max(res, getWidth(locQueue))
 
 		n := len(locQueue)
+
 		fLocQueue := locQueue[:n]
-		locQueue = locQueue[n:]
 		fNodeQueue := nodeQueue[:n]
-		nodeQueue = nodeQueue[:n]
+		locQueue = locQueue[n:]
+		nodeQueue = nodeQueue[n:]
 
 		for i := 0; i < n; i++ {
 			if fNodeQueue[i].Left != nil {
@@ -36,10 +42,12 @@ func widthOfBinaryTree(root *TreeNode) int {
 				locQueue = append(locQueue, fLocQueue[i]*2+1)
 			}
 		}
+
 	}
 
 	return res
 }
+// 本题也可以使用 DFS 来做
 
 func getWidth(locQueue []int) int {
 	n := len(locQueue)
