8881cbeebc9ed84685bb2ed73dd81eace49786ec
aQua <aQua@aQuaYi.com>
2017-12-20 10:09:40 +0800

    659 accepted. 299ms > 189ms

--- Algorithms/0659.split-array-into-consecutive-subsequences/split-array-into-consecutive-subsequences.go
+++ Algorithms/0659.split-array-into-consecutive-subsequences/split-array-into-consecutive-subsequences.go
@@ -1,7 +1,29 @@
 package Problem0659
 
 func isPossible(nums []int) bool {
-	res := true
+	count := make(map[int]int, 10000)
+	next := make(map[int]int, 10000)
+	for i := range nums {
+		count[nums[i]]++
+	}
 
-	return res
+	for _, n := range nums {
+		if count[n] == 0 {
+			continue
+		}
+
+		if next[n] > 0 {
+			next[n]--
+			next[n+1]++
+		} else if count[n+1] > 0 && count[n+2] > 0 {
+			count[n+1]--
+			count[n+2]--
+			next[n+3]++
+		} else {
+			return false
+		}
+		count[n]--
+	}
+
+	return true
 }
