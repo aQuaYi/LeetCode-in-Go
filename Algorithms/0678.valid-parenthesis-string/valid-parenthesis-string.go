630bdcb4b33711f2758dd0a5cb620c9b335a6a46
aQua <aQua@aQuaYi.com>
2017-12-21 16:46:35 +0800

    678 accepted. 362ms > 0ms

--- Algorithms/0678.valid-parenthesis-string/valid-parenthesis-string.go
+++ Algorithms/0678.valid-parenthesis-string/valid-parenthesis-string.go
@@ -1,7 +1,27 @@
 package Problem0678
 
 func checkValidString(s string) bool {
-	res := true
+	return check(0, 0, s)
+}
 
-	return res
+func check(lp, i int, s string) bool {
+	for i < len(s) {
+		if s[i] == '(' {
+			lp++
+		} else if s[i] == ')' {
+			lp--
+		} else {
+			return check(lp+1, i+1, s) ||
+				check(lp, i+1, s) ||
+				check(lp-1, i+1, s)
+		}
+
+		i++
+
+		if lp < 0 {
+			return false
+		}
+	}
+
+	return lp == 0
 }
--- Algorithms/0678.valid-parenthesis-string/valid-parenthesis-string_test.go
+++ Algorithms/0678.valid-parenthesis-string/valid-parenthesis-string_test.go
@@ -14,6 +14,11 @@ var tcs = []struct {
 }{
 
 	{
+		")(",
+		false,
+	},
+
+	{
 		"()",
 		true,
 	},
