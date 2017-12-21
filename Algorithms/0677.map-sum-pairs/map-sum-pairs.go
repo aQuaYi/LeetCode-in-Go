1cba4342a2bcaf08f85d749675c48a8ad472aaa2
aQua <aQua@aQuaYi.com>
2017-12-21 10:19:47 +0800

    677 finish

--- Algorithms/0677.map-sum-pairs/map-sum-pairs.go
+++ Algorithms/0677.map-sum-pairs/map-sum-pairs.go
@@ -1,31 +1,42 @@
 package Problem0677
 
-import (
-	"strings"
-)
+import "strings"
+import "sort"
 
 // MapSum 可以返回　map 中具有相同前缀的　value 之和
 type MapSum struct {
-	m map[string]int
+	m    map[string]int
+	keys []string
 }
 
 // Constructor 构建了 MapSum
 func Constructor() MapSum {
-	return MapSum{m: make(map[string]int, 128)}
+	return MapSum{m: make(map[string]int, 128),
+		keys: make([]string, 0, 128)}
 }
 
 // Insert 插入 (key, val)
 func (ms *MapSum) Insert(key string, val int) {
 	ms.m[key] = val
+
+	i := sort.SearchStrings(ms.keys, key)
+	// 遇到重复的 key ，不操作
+	if i < len(ms.keys) && ms.keys[i] == key {
+		return
+	}
+
+	ms.keys = append(ms.keys, "")
+	copy(ms.keys[i+1:], ms.keys[i:])
+	ms.keys[i] = key
 }
 
 // Sum 返回所有具有 prefix 的 key 的 val 之和
 func (ms *MapSum) Sum(prefix string) int {
 	res := 0
-	for k, v := range ms.m {
-		if strings.HasPrefix(k, prefix) {
-			res += v
-		}
+	i := sort.SearchStrings(ms.keys, prefix)
+	for i < len(ms.keys) && strings.HasPrefix(ms.keys[i], prefix) {
+		res += ms.m[ms.keys[i]]
+		i++
 	}
 	return res
 }
--- Algorithms/0677.map-sum-pairs/map-sum-pairs_test.go
+++ Algorithms/0677.map-sum-pairs/map-sum-pairs_test.go
@@ -19,3 +19,17 @@ func Test_MapSum(t *testing.T) {
 
 	ast.Equal(5, ms.Sum("ap"))
 }
+
+func Test_MapSum_duplicateKey(t *testing.T) {
+	ast := assert.New(t)
+
+	ms := Constructor()
+
+	ms.Insert("apple", 3)
+
+	ast.Equal(3, ms.Sum("ap"))
+
+	ms.Insert("apple", 2)
+
+	ast.Equal(2, ms.Sum("ap"))
+}
