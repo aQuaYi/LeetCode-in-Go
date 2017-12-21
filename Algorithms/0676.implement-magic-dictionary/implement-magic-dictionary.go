d105224bd8516708b602d9027181e7de48ecd99f
aQua <aQua@aQuaYi.com>
2017-12-21 13:44:44 +0800

    676 finish

--- Algorithms/0676.implement-magic-dictionary/implement-magic-dictionary.go
+++ Algorithms/0676.implement-magic-dictionary/implement-magic-dictionary.go
@@ -2,35 +2,20 @@ package Problem0676
 
 // MagicDictionary 是一个神奇的字典
 type MagicDictionary struct {
-	lens    map[int]bool
-	keys    map[string]bool
-	changes map[string]bool
+	keys []string
 }
-
-var replaceByte = byte('?')
 
 // Constructor 构建了神奇字典
 func Constructor() MagicDictionary {
 	return MagicDictionary{
-		lens:    make(map[int]bool, 1024),
-		keys:    make(map[string]bool, 1024),
-		changes: make(map[string]bool, 1024),
+		keys: make([]string, 0, 1024),
 	}
 }
 
 // BuildDict 往字典里面添加内容
 func (md *MagicDictionary) BuildDict(dict []string) {
 	for _, w := range dict {
-		n := len(w)
-		md.lens[n] = true
-
-		bs := []byte(w)
-		for i := 0; i < n; i++ {
-			t := bs[i]
-			bs[i] = replaceByte
-			md.changes[string(bs)] = true
-			bs[i] = t
-		}
+		md.keys = append(md.keys, w)
 	}
 }
 
@@ -38,20 +23,23 @@ func (md *MagicDictionary) BuildDict(dict []string) {
 func (md *MagicDictionary) Search(word string) bool {
 	n := len(word)
 
-	if !md.lens[n] {
-		return false
-	}
-
-	bs := []byte(word)
-	count := 0
-	for i := 0; i < n; i++ {
-		t := bs[i]
-		bs[i] = replaceByte
-		if md.changes[string(bs)] {
-			count++
+	for _, w := range md.keys {
+		if len(w) != n || w == word {
+			continue
 		}
-		bs[i] = t
+		if isChanges(w, word) {
+			return true
+		}
 	}
+	return false
+}
 
-	return count == 1 || count == n
+func isChanges(w, word string) bool {
+	n := len(w)
+	for i := range w {
+		if w[i] == word[i] {
+			n--
+		}
+	}
+	return n == 1
 }
