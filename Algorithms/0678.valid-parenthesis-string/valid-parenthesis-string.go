e357b23d53cb795c2f5385ad1605f3bfd118eaa6
aQua <aQua@aQuaYi.com>
2017-12-21 17:24:00 +0800

    678 fixing

--- Algorithms/0678.valid-parenthesis-string/valid-parenthesis-string.go
+++ Algorithms/0678.valid-parenthesis-string/valid-parenthesis-string.go
@@ -1,23 +1,33 @@
 package Problem0678
 
 func checkValidString(s string) bool {
-	min, max := 0, 0
+	// low  记录了 s 中无配对的 '(' 的下限
+	// high 记录了 s 中无配对的 '(' 的上限
+	low, high := 0, 0
 	for i := 0; i < len(s); i++ {
-
 		if s[i] == '(' {
-			min++
-			max++
+			// 遇到新的无配对的 '(' ，
+			// low 和 high 同时增加
+			low++
+			high++
 		} else if s[i] == ')' {
-			min--
-			max--
-			if max < 0 {
+			// 新出现了 ')'
+			// 无配对 '(' 的上限会下降
+			high--
+			if high < 0 {
 				return false
 			}
+			//
+			if low > 0 {
+				low--
+			}
 		} else {
-			min--
-			max++
+			if low > 0 {
+				low--
+			}
+			high++
 		}
 	}
 
-	return min <= 0 && 0 <= max
+	return low == 0
 }
