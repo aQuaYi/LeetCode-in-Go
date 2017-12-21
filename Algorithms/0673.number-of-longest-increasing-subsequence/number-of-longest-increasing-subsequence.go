085fb5f8529c17df588b6fc32640073acbfc646e
aQua <aQua@aQuaYi.com>
2017-12-21 16:13:38 +0800

    673 finish

--- Algorithms/0673.number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.go
+++ Algorithms/0673.number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.go
@@ -1,28 +1,39 @@
 package Problem0673
 
 func findNumberOfLIS(a []int) int {
+	n := len(a)
 	res := 0
 	maxLen := 0
 
-	dfs(a, -1<<63, 0, 0, &maxLen, &res)
+	// length[i] == m 表示，以 a[i] 结尾的最长子序列的长度为 m
+	length := make([]int, n)
+	// count[i] == n 表示，以 a[i] 结尾的最长子序列有 n 个
+	count := make([]int, n)
 
-	return res
-}
+	for j := 0; j < n; j++ {
+		length[j] = 1
+		count[j] = 1
+		for i := 0; i < j; i++ {
+			if a[i] < a[j] {
+				if length[j] == length[i]+1 {
+					// 出现相同的长度
+					count[j] += count[i]
+				} else if length[j] < length[i]+1 {
+					// 出现了长度的子序列
+					length[j] = length[i] + 1
+					count[j] = count[i]
+				}
+			}
+		}
 
-func dfs(a []int, Min, index, length int, maxLen, res *int) {
-	if *maxLen < length {
-		*maxLen = length
-		*res = 1
-	} else if *maxLen == length && length > 0 {
-		*res++
-	}
-
-	Max := 1<<63 - 1
-
-	for i := index; i < len(a); i++ {
-		if Min < a[i] && a[i] <= Max {
-			Max = a[i]
-			dfs(a, a[i], i+1, length+1, maxLen, res)
+		// 对于每一个 j 都更新一次 res
+		if maxLen == length[j] {
+			res += count[j]
+		} else if maxLen < length[j] {
+			res = count[j]
+			maxLen = length[j]
 		}
 	}
+
+	return res
 }
