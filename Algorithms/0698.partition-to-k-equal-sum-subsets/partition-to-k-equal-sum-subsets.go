7c664adafa61f96a81999a32c2e4ff0ad717b47a
aQua <aQua@aQuaYi.com>
2017-12-22 16:41:53 +0800

    698 ut fail

--- Algorithms/0698.partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.go
+++ Algorithms/0698.partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.go
@@ -1,5 +1,9 @@
 package Problem0698
 
+import (
+	"sort"
+)
+
 func canPartitionKSubsets(nums []int, k int) bool {
 	size := len(nums)
 	sum := 0
@@ -15,9 +19,30 @@ func canPartitionKSubsets(nums []int, k int) bool {
 		return false
 	}
 
-	avg := sum / size
+	sort.Sort(sort.Reverse(sort.IntSlice(nums)))
+	isUsed := make([]bool, size)
+	return dfs(nums, isUsed, 0, 0, 0, 0, sum/size)
+}
 
-	res := true
+func dfs(nums []int, isUsed []bool, startIndex, k, curSum, curNum, target int) bool {
+	if k == 1 {
+		return true
+	}
 
-	return res
+	if curSum == target && curNum > 0 {
+		return dfs(nums, isUsed, 0, k-1, 0, 0, target)
+	}
+
+	for i := startIndex; i < len(nums); i++ {
+		if !isUsed[i] {
+			isUsed[i] = true
+			if dfs(nums, isUsed, i+1, k, curSum+nums[i], curNum+1, target) {
+				return true
+			}
+			isUsed[i] = false
+		}
+
+	}
+
+	return false
 }
