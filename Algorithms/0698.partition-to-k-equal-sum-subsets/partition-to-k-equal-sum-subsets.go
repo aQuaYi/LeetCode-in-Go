6de718c8b524b6a7d276b81bc500374e18d570c8
aQua <aQua@aQuaYi.com>
2017-12-22 17:07:21 +0800

    698 finish

--- Algorithms/0698.partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.go
+++ Algorithms/0698.partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.go
@@ -20,27 +20,33 @@ func canPartitionKSubsets(nums []int, k int) bool {
 		return false
 	}
 
+	// nums 降序排列可以加快 dfs 的速度
 	sort.Sort(sort.Reverse(sort.IntSlice(nums)))
 
 	// isUsed[i] == true 表示 nums[i] 已经被归类到某个小组，无法再使用了
 	isUsed := make([]bool, size)
 
-	return dfs(nums, isUsed, 0, k, 0, 0, sum/k)
+	return dfs(nums, isUsed, k, 0, 0, sum/k)
 }
 
-func dfs(nums []int, isUsed []bool, startIndex, k, curSum, curNum, target int) bool {
+func dfs(nums []int, isUsed []bool, k, start, sum, target int) bool {
 	if k == 1 {
+		// 已经找到了 k-1 组解
+		// 剩下的自然就是第 k 组解
 		return true
 	}
 
-	if curSum == target && curNum > 0 {
-		return dfs(nums, isUsed, 0, k-1, 0, 0, target)
+	if sum == target {
+		// 找到第 k 组的一种解
+		// 开始搜索 k-1 组的解
+		return dfs(nums, isUsed, k-1, 0, 0, target)
 	}
 
-	for i := startIndex; i < len(nums); i++ {
-		if !isUsed[i] && curSum+nums[i] <= target {
+	for i := start; i < len(nums); i++ {
+		if !isUsed[i] && sum+nums[i] <= target {
 			isUsed[i] = true
-			if dfs(nums, isUsed, i+1, k, curSum+nums[i], curNum+1, target) {
+			// 试着 sum+nums[i]
+			if dfs(nums, isUsed, k, i+1, sum+nums[i], target) {
 				return true
 			}
 			isUsed[i] = false
