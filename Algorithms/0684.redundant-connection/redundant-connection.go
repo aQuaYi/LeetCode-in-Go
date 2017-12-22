95f7e0a0974896dcc5a5760bc3edd86b2fbff162
aQua <aQua@aQuaYi.com>
2017-12-22 11:05:46 +0800

    684 fixing

--- Algorithms/0684.redundant-connection/redundant-connection.go
+++ Algorithms/0684.redundant-connection/redundant-connection.go
@@ -1,19 +1,29 @@
 package Problem0684
 
 func findRedundantConnection(edges [][]int) []int {
-	N := len(edges)
-	count := make([]int, N+1)
-	var idx int
-	max := 0
-	for i, e := range edges {
-		a, b := e[0], e[1]
-		count[a]++
-		count[b]++
-		if max < count[a]+count[b] && count[a] > 1 && count[b] > 1 {
-			max = count[a] + count[b]
-			idx = i
-		}
+	n := len(edges)
+	parent := make([]int, n+1)
+	for i := 0; i < n; i++ {
+		parent[i] = i
 	}
 
-	return edges[idx]
+	var i int
+	var e []int
+	for i, e = range edges {
+		f, t := e[0], e[1]
+		pf := find(parent, f)
+		pt := find(parent, t)
+		if pf == pt {
+			break
+		}
+		parent[pf] = pt
+	}
+	return edges[i]
+}
+
+func find(parent []int, f int) int {
+	for f != parent[f] {
+		f = parent[f]
+	}
+	return f
 }
