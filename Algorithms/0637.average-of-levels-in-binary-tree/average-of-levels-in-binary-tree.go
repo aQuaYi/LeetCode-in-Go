10088b221660319afc4276e67f78bbf51050a3da
aQua <aQua@aQuaYi.com>
2017-12-22 11:34:35 +0800

    637 added

---
+++ Algorithms/0637.average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.go
@@ -0,0 +1,10 @@
+package Problem0637
+
+import (
+	"github.com/aQuaYi/LeetCode-in-Go/kit"
+)
+
+type TreeNode = kit.TreeNode
+func averageOfLevels(root *TreeNode) []float64 {
+	return nil 
+}
---
+++ Algorithms/0637.average-of-levels-in-binary-tree/average-of-levels-in-binary-tree_test.go
@@ -0,0 +1,44 @@
+package Problem0637
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aQuaYi/LeetCode-in-Go/kit"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	pre, in []int
+	ans     []float64
+}{
+
+	{
+		[]int{3, 9, 20, 15, 7},
+		[]int{9, 3, 15, 20, 7},
+		[]float64{3, 14.5, 11},
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_fn(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		root := kit.PreIn2Tree(tc.pre, tc.in)
+		ast.Equal(tc.ans, averageOfLevels(root), "输入:%v", tc)
+	}
+}
+
+func Benchmark_fn(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			root := kit.PreIn2Tree(tc.pre, tc.in)
+			averageOfLevels(root)
+		}
+	}
+}
