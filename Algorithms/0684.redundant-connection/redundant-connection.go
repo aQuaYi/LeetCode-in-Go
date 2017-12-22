a4de74ad59415e1152cc02a44c7f4761fff6b2cc
aQua <aQua@aQuaYi.com>
2017-12-22 09:41:37 +0800

    684 wrong anwser

--- Algorithms/0684.redundant-connection/redundant-connection.go
+++ Algorithms/0684.redundant-connection/redundant-connection.go
@@ -1,6 +1,30 @@
 package Problem0684
 
 func findRedundantConnection(edges [][]int) []int {
+	N := len(edges)
+	// connected
+	conn := make([][]int, N+1)
+	triple := 0
+	for _, e := range edges {
+		conn[e[0]] = append(conn[e[0]], e[1])
+		conn[e[1]] = append(conn[e[1]], e[0])
+		if len(conn[e[0]]) == 3 {
+			triple = e[0]
+		} else if len(conn[e[1]]) == 3 {
+			triple = e[1]
+		}
+	}
 
-	return nil
+	if triple == 0 {
+		return edges[N-1]
+	}
+
+	var node int
+	for i := 2; 0 <= i; i-- {
+		node = conn[triple][i]
+		if len(conn[node]) == 2 {
+			break
+		}
+	}
+	return []int{triple, node}
 }
