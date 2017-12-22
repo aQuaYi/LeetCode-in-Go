1d0d61f7d116ca55c4778888a5aa835f75b0a6b1
aQua <aQua@aQuaYi.com>
2017-12-22 11:48:30 +0800

    637 finish

--- Algorithms/0637.average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.go
+++ Algorithms/0637.average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.go
@@ -5,6 +5,30 @@ import (
 )
 
 type TreeNode = kit.TreeNode
+
 func averageOfLevels(root *TreeNode) []float64 {
-	return nil 
+	res := make([]float64, 0, 128)
+
+	nodes := make([]*TreeNode, 1, 1024)
+	nodes[0] = root
+
+	for len(nodes) > 0 {
+		n := len(nodes)
+
+		sum := 0
+		for i := 0; i < n; i++ {
+			sum += nodes[i].Val
+			if nodes[i].Left != nil {
+				nodes = append(nodes, nodes[i].Left)
+			}
+			if nodes[i].Right != nil {
+				nodes = append(nodes, nodes[i].Right)
+			}
+		}
+
+		res = append(res,float64( sum)/float64(n))
+		nodes = nodes[n:]
+	}
+
+	return res
 }
