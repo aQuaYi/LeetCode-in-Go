07d80f94f784e64ec799173ef8128cf6d91ea55f
aQua <aQua@aQuaYi.com>
2017-12-22 15:46:56 +0800

    698 fixing

--- Algorithms/0698.partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.go
+++ Algorithms/0698.partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.go
@@ -1,6 +1,22 @@
 package Problem0698
 
 func canPartitionKSubsets(nums []int, k int) bool {
+	size := len(nums)
+	sum := 0
+	max := nums[0]
+	for _, n := range nums {
+		sum += n
+		if max < n {
+			max = n
+		}
+	}
+
+	if sum%4 != 0 || sum/size < max {
+		return false
+	}
+
+	avg := sum / size
+
 	res := true
 
 	return res
