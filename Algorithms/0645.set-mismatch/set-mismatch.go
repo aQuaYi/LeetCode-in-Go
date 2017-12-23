c0857c25c07c18ca40c30f52ec6366863fde9f27
aQua <aQua@aQuaYi.com>
2017-12-23 13:24:02 +0800

    645 finish

--- Algorithms/0645.set-mismatch/set-mismatch.go
+++ Algorithms/0645.set-mismatch/set-mismatch.go
@@ -1,23 +1,39 @@
 package Problem0645
 
-import "sort"
-
 func findErrorNums(nums []int) []int {
-	sort.Ints(nums)
-
-	res := make([]int, 2)
-	res[1] = len(nums)
-
-	pre := 0
+	// n == len(nums)
+	// 而 nums 的数字属于 [1,n]，那么
+	// 对于每一个 nums[abs(nums[i])-1] 中的数取反
+	// 操作过程中，
+	// 取反前，nums[abs(nums[i])-1] 已经是负数的话，说明 abs(nums[i]) 是重复的数
+	// 操作结束后，
+	// nums[i] > 0，说明 nums[i] 没有被取反过， i+1 是丢失的数
+	dup := 0
 	for i := 0; i < len(nums); i++ {
-		if nums[i] == pre {
-			res[0] = pre
-		} else if nums[i] == pre+2 {
-			res[1] = pre + 1
-		}
+		n := abs(nums[i])
 
-		pre = nums[i]
+		if nums[n-1] < 0 {
+			dup = n
+			// NOTICE: nums[n-1]<0 的时候，不能再取反为正数了
+		} else {
+			nums[n-1] = -nums[n-1]
+		}
 	}
 
-	return res
+	mis := 0
+	for i, v := range nums {
+		if v > 0 {
+			mis = i + 1
+			break
+		}
+	}
+
+	return []int{dup, mis}
+}
+
+func abs(a int) int {
+	if a > 0 {
+		return a
+	}
+	return -a
 }
