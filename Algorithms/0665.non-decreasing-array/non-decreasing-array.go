19d56ca474ec4021ffa551c924ac1c74cfbafeda
aQua <aQua@aQuaYi.com>
2017-12-23 15:00:05 +0800

    665　ｆｉｎｉｓｈ

--- Algorithms/0665.non-decreasing-array/non-decreasing-array.go
+++ Algorithms/0665.non-decreasing-array/non-decreasing-array.go
@@ -7,10 +7,22 @@ import (
 func checkPossibility(nums []int) bool {
 	for i := 1; i < len(nums); i++ {
 		if nums[i-1] > nums[i] {
-			nums[i] = nums[i-1]
-			return sort.IsSorted(sort.IntSlice(nums))
+			pre := deepCopy(nums)
+			// 把 nums[i-1] 修改成　nums[i]
+			pre[i-1] = pre[i]
+			next := deepCopy(nums)
+			// 把 nums[i] 修改成　nums[i-1]
+			next[i] = next[i-1]
+			// 修改完成后，查看修改完成后，能否满足题意
+			return sort.IsSorted(sort.IntSlice(pre)) || sort.IsSorted(sort.IntSlice(next))
 		}
 	}
 
 	return true
+}
+
+func deepCopy(nums []int) []int {
+	res := make([]int, len(nums))
+	copy(res, nums)
+	return res
 }
