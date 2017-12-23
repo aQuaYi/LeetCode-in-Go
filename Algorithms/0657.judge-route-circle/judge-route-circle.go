3e0e573ab9eb65cb01f7a7c07da5dcb03e15279e
aQua <aQua@aQuaYi.com>
2017-12-23 14:15:16 +0800

    657 accepted. 13ms > 6ms

--- Algorithms/0657.judge-route-circle/judge-route-circle.go
+++ Algorithms/0657.judge-route-circle/judge-route-circle.go
@@ -1,24 +1,18 @@
 package Problem0657
 
-var dx = map[byte]int{
-	'R': 1,
-	'L': -1,
-	'U': 0,
-	'D': 0,
-}
-
-var dy = map[byte]int{
-	'R': 0,
-	'L': 0,
-	'U': -1,
-	'D': 1,
-}
-
 func judgeCircle(moves string) bool {
 	x, y := 0, 0
 	for i := range moves {
-		x += dx[moves[i]]
-		y += dy[moves[i]]
+		switch moves[i] {
+		case 'R':
+			x++
+		case 'L':
+			x--
+		case 'U':
+			y--
+		case 'D':
+			y++
+		}
 	}
 
 	return x == 0 && y == 0
