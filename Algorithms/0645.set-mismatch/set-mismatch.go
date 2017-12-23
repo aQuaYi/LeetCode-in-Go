961874b273832968a1cfe77e46ccdc930565b79d
aQua <aQua@aQuaYi.com>
2017-12-23 13:03:49 +0800

    645 accepted. 89ms > 46ms

--- Algorithms/0645.set-mismatch/set-mismatch.go
+++ Algorithms/0645.set-mismatch/set-mismatch.go
@@ -1,6 +1,23 @@
 package Problem0645
 
-func findErrorNums(nums []int) []int {
+import "sort"
 
-	return nil
+func findErrorNums(nums []int) []int {
+	sort.Ints(nums)
+
+	res := make([]int, 2)
+	res[1] = len(nums)
+
+	pre := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == pre {
+			res[0] = pre
+		} else if nums[i] == pre+2 {
+			res[1] = pre + 1
+		}
+
+		pre = nums[i]
+	}
+
+	return res
 }
