1b3eb78db4394f25cf54a649238dff97b084c75f
aQua <aQua@aQuaYi.com>
2017-12-25 10:58:21 +0800

    669 finish

--- Algorithms/0669.trim-a-binary-search-tree/trim-a-binary-search-tree.go
+++ Algorithms/0669.trim-a-binary-search-tree/trim-a-binary-search-tree.go
@@ -5,5 +5,19 @@ import (
 )
 type TreeNode = kit.TreeNode
 func trimBST(root *TreeNode, L int, R int) *TreeNode {
-	return nil
+	if root == nil {
+		return nil 
+	}
+
+	if root.Val < L {
+return trimBST(root.Right, L, R)
+	}
+
+	if R< root.Val  {
+return trimBST(root.Left, L, R)
+	}
+
+	root.Left = trimBST(root.Left, L, R)
+	root.Right = trimBST(root.Right, L, R)
+	return root
 }
--- Algorithms/0669.trim-a-binary-search-tree/trim-a-binary-search-tree_test.go
+++ Algorithms/0669.trim-a-binary-search-tree/trim-a-binary-search-tree_test.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/aQuaYi/LeetCode-in-Go/kit"
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -27,7 +29,7 @@ var tcs = []struct {
 		[]int{0, 1, 2, 3, 4},
 		1,
 		3,
-		[]int{3, 2, 1},
+		[]int{1, 2, 3},
 	},
 
 	// 可以有多个 testcase
@@ -38,14 +40,17 @@ func Test_fn(t *testing.T) {
 
 	for _, tc := range tcs {
 		fmt.Printf("~~%v~~\n", tc)
-		ast.Equal(tc.ans, trimBST(tc.para), "输入:%v", tc)
+		root := kit.PreIn2Tree(tc.pre, tc.in)
+		ansPost := kit.Tree2Postorder(trimBST(root, tc.L, tc.R))
+		ast.Equal(tc.ansPost, ansPost, "输入:%v", tc)
 	}
 }
 
 func Benchmark_fn(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		for _, tc := range tcs {
-			trimBST(tc.para)
+			root := kit.PreIn2Tree(tc.pre, tc.in)
+			trimBST(root, tc.L, tc.R)
 		}
 	}
 }
