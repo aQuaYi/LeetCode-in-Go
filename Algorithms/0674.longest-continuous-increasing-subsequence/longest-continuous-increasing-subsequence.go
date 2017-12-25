a9b44a19b7941169051d48a648941fa622e64caa
aQua <aQua@aQuaYi.com>
2017-12-25 16:15:45 +0800

    674 wrong answer

--- Algorithms/0674.longest-continuous-increasing-subsequence/longest-continuous-increasing-subsequence.go
+++ Algorithms/0674.longest-continuous-increasing-subsequence/longest-continuous-increasing-subsequence.go
@@ -1,7 +1,25 @@
 package Problem0674
 
-func findLengthOfLCIS(nums []int) int {
-	res := 0
+func findLengthOfLCIS(a []int) int {
+
+	res := 1
+
+	var i, j int
+	i = 0
+	j = 1
+
+	for j < len(a) {
+		for j < len(a) && a[j-1] < a[j] {
+			j++
+		}
+
+		if res < j-i {
+			res = j - i
+		}
+
+		i = j
+		j++
+	}
 
 	return res
 }
