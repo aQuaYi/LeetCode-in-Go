0bb126a6e4cb39219f758115aef5d035360ad54f
aQua <aQua@aQuaYi.com>
2017-12-25 16:51:53 +0800

    682 accepted. 3ms

---
+++ Algorithms/0682.baseball-game/baseball-game.go
@@ -0,0 +1,32 @@
+package Problem0682
+
+import (
+	"strconv"
+)
+
+func calPoints(ops []string) int {
+	pointStack := make([]int, 0, len(ops))
+
+	for i := range ops {
+		switch ops[i] {
+		case "+":
+			r1 := pointStack[len(pointStack)-1]
+			r2 := pointStack[len(pointStack)-2]
+			pointStack = append(pointStack, r1+r2)
+		case "D":
+			r1 := pointStack[len(pointStack)-1]
+			pointStack = append(pointStack, 2*r1)
+		case "C":
+			pointStack = pointStack[:len(pointStack)-1]
+		default:
+			point, _ := strconv.Atoi(ops[i])
+			pointStack = append(pointStack, point)
+		}
+	}
+
+	res := 0
+	for _, p := range pointStack {
+		res += p
+	}
+	return res
+}
---
+++ Algorithms/0682.baseball-game/baseball-game_test.go
@@ -0,0 +1,44 @@
+package Problem0682
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	ops []string
+	ans int
+}{
+
+	{
+		[]string{"5", "2", "C", "D", "+"},
+		30,
+	},
+
+	{
+		[]string{"5", "-2", "4", "C", "D", "9", "+", "+"},
+		27,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_fn(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, calPoints(tc.ops), "输入:%v", tc)
+	}
+}
+
+func Benchmark_fn(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			calPoints(tc.ops)
+		}
+	}
+}
