f76bc4cf8ed1b7eebe6aa0407aba032cad0146f4
aQua <aQua@aQuaYi.com>
2017-12-26 13:55:03 +0800

    583 fix 一处理解错误

--- Algorithms/0583.delete-operation-for-two-strings/delete-operation-for-two-strings.go
+++ Algorithms/0583.delete-operation-for-two-strings/delete-operation-for-two-strings.go
@@ -3,7 +3,7 @@ package Problem0583
 func minDistance(word1, word2 string) int {
 	n1, n2 := len(word1), len(word2)
 
-	// dp[i][j] == k 表示 word1[:i+1] 和 word2[:j+1] 的最大公共子序列的长度为 k
+	// dp[i][j] == k 表示 word1[:i] 和 word2[:j] 的最大公共子序列的长度为 k
 	dp := make([][]int, n1+1)
 	for i := range dp {
 		dp[i] = make([]int, n2+1)
