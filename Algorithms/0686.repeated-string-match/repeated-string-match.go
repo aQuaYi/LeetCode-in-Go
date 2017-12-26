78a71284f5928cee5bb7525d42a2f0dba41f88af
aQua <aQua@aQuaYi.com>
2017-12-26 22:21:50 +0800

    686 accepted. 39ms > 3ms

--- Algorithms/0686.repeated-string-match/repeated-string-match.go
+++ Algorithms/0686.repeated-string-match/repeated-string-match.go
@@ -5,39 +5,22 @@ import (
 )
 
 func repeatedStringMatch(a, b string) int {
-	lenA := len(a)
-	lenB := len(b)
-	i := strings.Index(b, a)
-	j := i
-	if i != -1 {
-		for j+lenA <= lenB && b[j:j+lenA] == a {
-			j += lenA
+
+	c := a
+	res := 1
+	for len(c) < len(b) {
+		c += a
+		res++
+	}
+
+	for len(a)*2+len(b) > len(c) {
+		if strings.Contains(c, b) {
+			return res
 		}
-	}
+		c += a
+		res++
 
-	res := (j - i) / lenA
-	t := b
-	if i < j {
-		t = b[:i] + b[j:]
-	}
-
-	lenT := len(t)
-	if t == "" {
-		return res
-	}
-
-	if (lenT <= lenA && a[:lenT] == t) ||
-		(0 <= lenA-lenT && a[lenA-lenT:] == t) {
-		return res + 1
-	}
-
-	for i := 1; i < lenT-1; i++ {
-		if 0 <= lenA-i && t[:i] == a[lenA-i:] &&
-			lenT-i <= lenA && t[i:] == a[:lenT-i] {
-			return res + 2
-		}
 	}
 
 	return -1
-
 }
