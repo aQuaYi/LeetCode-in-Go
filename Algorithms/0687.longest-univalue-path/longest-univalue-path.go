6d5c114c05a1bf8104ddeddff5bc0fa36fa19032
aQua <aQua@aQuaYi.com>
2017-12-27 14:44:16 +0800

    687 finish

--- Algorithms/0687.longest-univalue-path/longest-univalue-path.go
+++ Algorithms/0687.longest-univalue-path/longest-univalue-path.go
@@ -6,40 +6,44 @@ import (
 
 type TreeNode = kit.TreeNode
 
+func longestUnivaluePath(root *TreeNode) int {
+	maxLen := 0
+	helper(root, &maxLen)
+	return maxLen
+}
+
+// 返回从 root 出发拥有相同 Val 值的线路上的 edge 数量
+func helper(n *TreeNode, maxLen *int) int {
+	if n == nil {
+		return 0
+	}
+
+	l := helper(n.Left, maxLen)
+	r := helper(n.Right, maxLen)
+	res := 0
+
+	// 左侧单边的最长距离
+	if n.Left != nil && n.Val == n.Left.Val {
+		*maxLen = max(*maxLen, l+1)
+		res = max(res, l+1)
+	}
+	// 右侧单边的最长距离
+	if n.Right != nil && n.Val == n.Right.Val {
+		*maxLen = max(*maxLen, r+1)
+		res = max(res, r+1)
+	}
+	// 通过根节点的最长边
+	if n.Left != nil && n.Val == n.Left.Val &&
+		n.Right != nil && n.Val == n.Right.Val {
+		*maxLen = max(*maxLen, l+r+2)
+	}
+
+	return res
+}
+
 func max(a, b int) int {
 	if a > b {
 		return a
 	}
 	return b
-}
-
-//
-func helper(n *TreeNode, maxLen *int) int {
-	if n == nil {
-		return 1
-	}
-
-	l := helper(n.Left, maxLen)
-	r := helper(n.Right, maxLen)
-	lt := 1
-
-	if n.Left != nil && n.Val == n.Left.Val {
-		*maxLen = max(*maxLen, l+1)
-		lt = max(lt, l+1)
-	}
-	if n.Right != nil && n.Val == n.Right.Val {
-		*maxLen = max(*maxLen, r+1)
-		lt = max(lt, r+1)
-	}
-	if n.Left != nil && n.Right != nil && n.Val == n.Left.Val && n.Val == n.Right.Val {
-		*maxLen = max(*maxLen, l+r+1)
-	}
-
-	return lt
-}
-
-func longestUnivaluePath(root *TreeNode) int {
-	maxLen := 1
-	helper(root, &maxLen)
-	return maxLen - 1
 }
