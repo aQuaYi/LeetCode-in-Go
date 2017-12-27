103fe6c999e4188e08b20932765cc1f766d35384
aQua <aQua@aQuaYi.com>
2017-12-27 20:23:51 +0800

    728 finish

---
+++ Algorithms/0728.self-dividing-numbers/self-dividing-numbers.go
@@ -0,0 +1,23 @@
+package Problem0728
+
+func selfDividingNumbers(left, right int) []int {
+	res := make([]int, 0, right-left+1)
+	for i := left; i <= right; i++ {
+		if isSelfDividing(i) {
+			res = append(res, i)
+		}
+	}
+	return res
+}
+
+func isSelfDividing(n int) bool {
+	t := n
+	for t > 0 {
+		d := t % 10
+		t /= 10
+		if d == 0 || n%d != 0 {
+			return false
+		}
+	}
+	return true
+}
---
+++ Algorithms/0728.self-dividing-numbers/self-dividing-numbers_test.go
@@ -0,0 +1,40 @@
+package Problem0728
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	left, right int
+	ans         []int
+}{
+
+	{
+		1,
+		22,
+		[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22},
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_fn(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, selfDividingNumbers(tc.left, tc.right), "输入:%v", tc)
+	}
+}
+
+func Benchmark_fn(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			selfDividingNumbers(tc.left, tc.right)
+		}
+	}
+}
