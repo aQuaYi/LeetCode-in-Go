0ecdcd3be75e4e617c0795f05195c9fe138ee8f9
aQua <aQua@aQuaYi.com>
2017-12-27 09:08:22 +0800

    719 accepted. 612ms > 16ms

--- Algorithms/0719.find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.go
+++ Algorithms/0719.find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.go
@@ -1,7 +1,48 @@
 package Problem0719
 
-func smallestDistancePair(nums []int, k int) int {
-	res := 0
+import (
+	"sort"
+)
 
+func smallestDistancePair(a []int, k int) int {
+	n := len(a)
+	sort.Ints(a)
+
+	low := a[1] - a[0]
+	for i := 2; i < n; i++ {
+		low = min(low, a[i]-a[i-1])
+	}
+
+	high := a[n-1] - a[0]
+
+	for low < high {
+		mid := low + (high-low)/2
+		if countPairs(a, mid) < k {
+			low = mid + 1
+		} else {
+			high = mid
+		}
+	}
+
+	return low
+}
+
+func countPairs(a []int, mid int) int {
+	n := len(a)
+	res := 0
+	for i := 0; i < n; i++ {
+		j := i
+		for j < n && a[j]-a[i] <= mid {
+			j++
+		}
+		res += j - i - 1
+	}
 	return res
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
