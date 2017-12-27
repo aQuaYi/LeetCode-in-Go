80141798a05cea31c11283094d4fd217c21fb1d0
aQua <aQua@aQuaYi.com>
2017-12-27 09:42:31 +0800

    719 finish

--- Algorithms/0719.find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.go
+++ Algorithms/0719.find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.go
@@ -12,7 +12,9 @@ func smallestDistancePair(a []int, k int) int {
 	}
 
 	high := a[n-1] - a[0]
-
+	// 由题意可知，res 必定存在于 [low, high] 之中
+	// 又由 count(a, mid) 可以 a 中距离 <= mid 的个数
+	// 故可以使用 二分查找法
 	for low < high {
 		mid := low + (high-low)/2
 		if count(a, mid) < k {
@@ -20,11 +22,16 @@ func smallestDistancePair(a []int, k int) int {
 		} else {
 			high = mid
 		}
+		// NOTICE:
+		// count(a,mid) == k , 并不能立即返回 mid 作为答案
+		// 例如 count([]int{1,1,4}, 2) == 1,
+		// 但是 smallestDistancePair([]int{1,1,4}, 1) == 0
 	}
 
 	return low
 }
 
+// 统计 a 中距离 <= mid 的元素对的数量
 func count(a []int, mid int) int {
 	n := len(a)
 	res := 0
