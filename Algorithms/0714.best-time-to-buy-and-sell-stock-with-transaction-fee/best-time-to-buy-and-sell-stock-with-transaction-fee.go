7a2aeef3502bfa22a8e1413c2cd8a081907f0596
aQua <aQua@aQuaYi.com>
2017-12-27 10:37:56 +0800

    714 finish

--- Algorithms/0714.best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.go
+++ Algorithms/0714.best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.go
@@ -1,7 +1,21 @@
 package Problem0714
 
 func maxProfit(prices []int, fee int) int {
-	res := 0
+	empty := 0
+	hold := -1 << 63
 
-	return res
+	for _, p := range prices {
+		temp := empty
+		empty = max(empty, hold+p)
+		hold = max(hold, temp-p-fee)
+	}
+
+	return empty
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
