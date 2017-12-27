5ea3881257c8de7d389bf4ad3d08676cf5c22486
aQua <aQua@aQuaYi.com>
2017-12-27 16:28:33 +0800

    695 wrong answer

---
+++ Algorithms/0695.max-area-of-island/max-area-of-island.go
@@ -0,0 +1,68 @@
+package Problem0695
+
+var dx = []int{-1, 1, 0, 0}
+var dy = []int{0, 0, -1, 1}
+
+func maxAreaOfIsland(grid [][]int) int {
+	if len(grid) == 0 || len(grid[0]) == 0 {
+		return 0
+	}
+
+	m, n := len(grid), len(grid[0])
+	isChecked := make([]bool, m*n)
+
+	maxArea := 0
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if grid[i][j] == 1 && !isChecked[i*n+j] {
+				search(grid, isChecked, i, j, &maxArea)
+			}
+		}
+	}
+
+	return maxArea
+}
+
+func search(grid [][]int, isChecked []bool, x, y int, maxArea *int) {
+	m, n := len(grid), len(grid[0])
+	area := 0
+	xq := make([]int, 1, 50*50)
+	yq := make([]int, 1, 50*50)
+	xq[0] = x
+	yq[0] = y
+
+	for len(xq) > 0 {
+		x = xq[0]
+		y = yq[0]
+		xq = xq[1:]
+		yq = yq[1:]
+
+		// 标记 [x,y] 为检查过
+		isChecked[x*n+y] = true
+		// 更新 area
+		area++
+
+		// 添加 [x,y] 周围还没有检查的点
+		for k := 0; k < 4; k++ {
+			i := x + dx[k]
+			j := y + dy[k]
+			if 0 <= i && i < m && 0 <= j && j < n &&
+				grid[i][j] == 1 &&
+				!isChecked[i*n+j] {
+				xq = append(xq, i)
+				yq = append(yq, j)
+			}
+		}
+
+	}
+
+	*maxArea = max(*maxArea, area)
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
---
+++ Algorithms/0695.max-area-of-island/max-area-of-island_test.go
@@ -0,0 +1,72 @@
+package Problem0695
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	grid [][]int
+	ans  int
+}{
+
+	{
+		[][]int{
+			{1, 1, 0, 0, 0},
+			{1, 1, 0, 0, 0},
+			{0, 0, 0, 1, 1},
+			{0, 0, 0, 1, 1},
+		},
+		4,
+	},
+
+	{
+		[][]int{
+			{1},
+		},
+		1,
+	},
+
+	{
+		[][]int{
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		},
+		0,
+	},
+
+	{
+		[][]int{
+			{0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+			{0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+			{0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0},
+		},
+		6,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_fn(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, maxAreaOfIsland(tc.grid), "输入:%v", tc)
+	}
+}
+
+func Benchmark_fn(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			maxAreaOfIsland(tc.grid)
+		}
+	}
+}
