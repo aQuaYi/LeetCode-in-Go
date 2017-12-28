41ae9c14a3f23b2aafaa9ad559ee5d536d3a9b91
aQua <aQua@aQuaYi.com>
2017-12-28 21:55:49 +0800

    722 fixing

--- Algorithms/0722.remove-comments/remove-comments.go
+++ Algorithms/0722.remove-comments/remove-comments.go
@@ -5,6 +5,7 @@ import "strings"
 // NOTICE: 使用正则表达式替换，可以很简单地解决这个题目
 
 func removeComments(source []string) []string {
+	// 末尾添加 "$$" 是为了当最后一行是 "// ..." 时，能找到终止符号
 	s := strings.Join(source, "$$") + "$$"
 	s = helper(s)
 	source = split(s)
@@ -12,21 +13,25 @@ func removeComments(source []string) []string {
 }
 
 func helper(s string) string {
-	i := strings.Index(s, "//")
-	j := strings.Index(s, "/*")
+	i := strings.Index(s, "/*")
+	j := strings.Index(s, "//")
 
-	if (i == -1 && 0 <= j) ||
-		(0 <= j && j < i) {
-		k := j + 2 + strings.Index(s[j+2:], "*/")
-		return s[:j] + helper(s[k+2:])
+	// "//" 起作用了
+	if (i == -1 && 0 <= j) || // 只有 "//"
+		(0 <= j && j < i) { // "//" 在前，先起作用了
+		k := j + strings.Index(s[j:], "$$")
+		return s[:j] + helper(s[k:])
 	}
 
-	if (j == -1 && 0 <= i) ||
-		(0 <= i && i < j) {
-		k := i + strings.Index(s[i:], "$$")
-		return s[:i] + helper(s[k:])
+	// "/*" 起作用了
+	if (j == -1 && 0 <= i) || // 没有 "//"，只有 "/*"
+		(0 <= i && i < j) { // "/*" 在前， 先起作用了
+		// i+2 是为了避免匹配到 "/*/" 中的 "*/"
+		k := i + 2 + strings.Index(s[i+2:], "*/")
+		return s[:i] + helper(s[k+2:]) // 相当于删除了 s[j:k+2]
 	}
 
+	// 已经没有注释了
 	return s
 }
 
@@ -38,61 +43,5 @@ func split(s string) []string {
 			res = append(res, s)
 		}
 	}
-	return res
-}
-
-func dealLine(source []string) []string {
-	res := make([]string, 0, len(source))
-	for _, s := range source {
-		i := strings.Index(s, "//")
-		j := strings.Index(s, "/*")
-		k := strings.LastIndex(s, "*/")
-
-		if i == 0 && k < 0 { // 后面要求没有 "*/"
-			continue
-		}
-
-		if i == -1 || // 无 "//"
-			(0 <= j && j < i) || // "/*" 在 "//" 前面起作用了
-			i < k { // "//" 后面还有 "*/" 所以要留着
-			res = append(res, s)
-			continue
-		}
-
-		res = append(res, s[:i])
-	}
-	return res
-}
-
-func dealBlock(source []string) []string {
-	res := make([]string, 0, len(source))
-	isInBlock := false
-	temp := ""
-	for k := 0; k < len(source); k++ {
-		s := source[k]
-		if isInBlock {
-			i := strings.LastIndex(s, "*/")
-			if i >= 0 {
-				temp += s[i+2:]
-				if temp != "" {
-					res = append(res, temp)
-				}
-				temp = ""
-				isInBlock = false
-			}
-			continue
-		}
-
-		i := strings.Index(s, "/*")
-
-		if i == -1 {
-			res = append(res, s)
-		} else {
-			isInBlock = true
-			temp = s[:i]
-			k--
-		}
-	}
-
 	return res
 }
