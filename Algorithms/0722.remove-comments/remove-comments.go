4f7869e52812dd817ba0321e74433d56bbde2229
aQua <aQua@aQuaYi.com>
2017-12-28 22:05:36 +0800

    722 finish

--- Algorithms/0722.remove-comments/remove-comments.go
+++ Algorithms/0722.remove-comments/remove-comments.go
@@ -2,7 +2,7 @@ package Problem0722
 
 import "strings"
 
-// NOTICE: 使用正则表达式替换，可以很简单地解决这个题目
+// TODO: 使用正则表达式替换，可以很简单地解决这个题目
 
 func removeComments(source []string) []string {
 	// 末尾添加 "$$" 是为了当最后一行是 "// ..." 时，能找到终止符号
@@ -16,19 +16,19 @@ func helper(s string) string {
 	i := strings.Index(s, "/*")
 	j := strings.Index(s, "//")
 
-	// "//" 起作用了
-	if (i == -1 && 0 <= j) || // 只有 "//"
-		(0 <= j && j < i) { // "//" 在前，先起作用了
-		k := j + strings.Index(s[j:], "$$")
-		return s[:j] + helper(s[k:])
-	}
-
 	// "/*" 起作用了
-	if (j == -1 && 0 <= i) || // 没有 "//"，只有 "/*"
-		(0 <= i && i < j) { // "/*" 在前， 先起作用了
+	if 0 <= i &&
+		(j == -1 || i < j) {
 		// i+2 是为了避免匹配到 "/*/" 中的 "*/"
 		k := i + 2 + strings.Index(s[i+2:], "*/")
-		return s[:i] + helper(s[k+2:]) // 相当于删除了 s[j:k+2]
+		return s[:i] + helper(s[k+2:]) // 删除了 s[i:k+2]
+	}
+
+	// "//" 起作用了
+	if 0 <= j &&
+		(i == -1 || j < i) {
+		k := j + strings.Index(s[j:], "$$")
+		return s[:j] + helper(s[k:]) // 删除了 s[j:k]
 	}
 
 	// 已经没有注释了
