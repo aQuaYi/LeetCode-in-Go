d5492477d4b76d3f53c82b524e2965c4cd02b37c
aQua <aQua@aQuaYi.com>
2017-12-31 17:41:07 +0800

    731 wrong answer

--- Algorithms/0731.my-calendar-ii/my-calendar-ii.go
+++ Algorithms/0731.my-calendar-ii/my-calendar-ii.go
@@ -1,82 +1,84 @@
 package Problem0731
 
+import (
+	"sort"
+)
+
 // MyCalendarTwo 第二个日历类
 type MyCalendarTwo struct {
-	head, tail *event
-}
-
-type event struct {
-	start, end int
-	count      int
-	pre, next  *event
+	events    [][]int
+	overlaped [][]int
 }
 
 // Constructor 返回 MyCalendarTwo
 func Constructor() MyCalendarTwo {
-	head := &event{start: -1, end: -1}
-	tail := &event{start: 1e9 + 1, end: 1e9 + 1}
-	head.next = tail
-	tail.pre = head
-	return MyCalendarTwo{head: head, tail: tail}
+	return MyCalendarTwo{}
 }
 
 // Book 可以预约事件，预约成功返回 true
 func (m *MyCalendarTwo) Book(start, end int) bool {
-	e := &event{start: start, end: end, count: 1}
+	e := []int{start, end, 1}
 
-	cur := m.head
-	for cur.end <= e.start {
-		cur = cur.next
+	n := len(m.events)
+	if n == 0 {
+		m.events = append(m.events, e)
+		return true
 	}
 
-	for e != nil {
-		if e.end <= cur.start {
-			e.pre, e.next = cur.pre, cur
-			e.pre.next, e.next.pre = e, e
-			e = nil
-			continue
-		}
+	i := sort.Search(n, func(i int) bool {
+		return e[0] < m.events[i][1]
+	})
 
+	j := sort.Search(n, func(j int) bool {
+		return e[1] <= m.events[j][0]
+	})
+
+	if i == j {
+		m.events = append(m.events, nil)
+		copy(m.events[i+1:], m.events[i:])
+		m.events[i] = e
+		return true
 	}
 
-	return false
-}
-
-// 在 a 的 pre 方向插入 aPre
-func addPre(a, aPre *event) {
-	aPre.next, aPre.pre = a, a.pre
-	aPre.pre.next, aPre.next.pre = aPre, aPre
-}
-
-func split(e, c *event) (ecp, ec, ecn *event) {
-
-}
-
-func isAddable(headPre, tailNext, e *event) bool {
-	cur := headPre.next
-	for cur != tailNext {
-		if cur.count >= 2 {
+	for k := i; k < j; k++ {
+		if m.events[k][2] == 2 {
 			return false
 		}
 	}
 
+	segements := overlap(m.events[i:j], e)
+	temp := make([][]int, len(m.events)-j)
+	copy(temp, m.events[j:])
+	m.events = m.events[:i]
+	m.events = append(m.events, segements...)
+	m.events = append(m.events, temp...)
+
 	return true
 }
 
-// if old.end<new.end 返回  &event{start:old.end,end:new.end}
-// 否则，返回 nil
-func overlap(old, new *event) *event {
-	if new.start < old.start {
-		t := &event{start: new.start, end: old.start, count: new.count}
-		old.pre.next = t
-		t.next = old
-		t.pre = old.pre
-		old.pre = t
-	} else {
+func overlap(events [][]int, e []int) [][]int {
+	res := make([][]int, 0, len(events)*2+1)
 
+	for i := 0; i < len(events); i++ {
+		if e[0] < events[i][0] {
+			res = append(res, []int{e[0], events[i][0], 1})
+		}
+		//
+		if e[1] < events[i][1] {
+			e1 := []int{events[i][0], e[1], 2}
+			e2 := []int{e[1], events[i][1], 1}
+			res = append(res, e1, e2)
+		} else if e[1] == events[i][1] {
+			events[i][2] = 2
+			res = append(res, events[i])
+		} else {
+			events[i][2] = 2
+			res = append(res, events[i])
+			e[0] = events[i][1]
+		}
 	}
 
-	return nil
+	return res
 }
 
 /**
