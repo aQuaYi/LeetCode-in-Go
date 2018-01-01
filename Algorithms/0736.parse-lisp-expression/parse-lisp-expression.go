e983546c3fb539f7dff2fd8efe7f83aeadabb0e1
aQua <aQua@aQuaYi.com>
2018-01-01 17:10:42 +0800

    736 wrong answer

--- Algorithms/0736.parse-lisp-expression/parse-lisp-expression.go
+++ Algorithms/0736.parse-lisp-expression/parse-lisp-expression.go
@@ -1,6 +1,7 @@
 package Problem0736
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 )
@@ -32,29 +33,33 @@ func helper(exp string, m map[string]int) int {
 		for i = 1; i < len(es)-2; i += 2 {
 			m[es[i]] = helper(es[i+1], copy(m))
 		}
+		// TODO: 删除此处内容
+		fmt.Println("\n=======\n", es[i])
 		return helper(es[i], copy(m))
 	}
 
 }
 
 func split(exp string) []string {
+	exp = strings.Replace(exp, ")", " )", -1)
+	// TODO: 删除此处内容
+	fmt.Println(exp)
 	ss := strings.Split(exp, " ")
-	endSign := ""
-	isSawBrackets := false
+	leftCount := 0
 	res := make([]string, 0, len(ss))
 	for _, s := range ss {
-		if !isSawBrackets {
+		if leftCount == 0 {
 			res = append(res, s)
 		} else {
 			res[len(res)-1] += " " + s
 		}
+		// TODO: 删除此处内容
+		fmt.Println(res[len(res)-1])
 
-		if strings.Contains(s, "(") {
-			isSawBrackets = true
-			endSign += ")"
-		} else if strings.Contains(s, endSign) {
-			isSawBrackets = false
-			endSign = ""
+		if s[0] == '(' {
+			leftCount++
+		} else if s == ")" {
+			leftCount--
 		}
 	}
 
