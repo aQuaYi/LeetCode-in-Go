36b035c5481731435a8a2a7da5351a5bf0824964
aQua <aQua@aQuaYi.com>
2018-01-02 09:14:26 +0800

    741 added

---
+++ Algorithms/0741.cherry-pickup/cherry-pickup.go
@@ -0,0 +1,7 @@
+package Problem0741
+
+func cherryPickup(grid [][]int) int {
+	res := 0
+
+	return res
+}
---
+++ Algorithms/0741.cherry-pickup/cherry-pickup_test.go
@@ -0,0 +1,43 @@
+package Problem0741
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	grid [][]int
+	ans  int
+}{
+
+	{
+		[][]int{
+			{0, 1, -1},
+			{1, 0, -1},
+			{1, 1, 1},
+		},
+		5,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_fn(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, cherryPickup(tc.grid), "输入:%v", tc)
+	}
+}
+
+func Benchmark_fn(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			cherryPickup(tc.grid)
+		}
+	}
+}
