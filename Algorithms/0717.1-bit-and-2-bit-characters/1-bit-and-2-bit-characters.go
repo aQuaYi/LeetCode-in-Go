d169dfaecd69fcd96b747949f1ae6a090dd7571a
aQua <aQua@aQuaYi.com>
2018-01-02 11:47:45 +0800

    717 finish

--- Algorithms/0717.1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.go
+++ Algorithms/0717.1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.go
@@ -2,31 +2,14 @@ package Problem0717
 
 func isOneBitCharacter(bits []int) bool {
 	n := len(bits)
-	if n == 1 ||
-		(n >= 2 && bits[n-2] == 0) ||
-		(n == 3 && bits[n-3] == 1) ||
-		(n >= 4 && bits[n-4] == 0 && bits[n-3] == 1 && bits[n-2] == 1) {
-		return true
-	}
-	return false
-}
-
-func deleteTwoBits(bits []int) []int {
-	res := make([]int, 0, len(bits))
-	isJump := false
-	for i := range bits {
-		if isJump {
-			isJump = false
-			continue
-		}
-
+	i := 0
+	for i < n-1 {
 		if bits[i] == 1 {
-			isJump = true
-			continue
+			// 遇见 1 说明遇见了 twoBits
+			i += 2
+		} else {
+			i++
 		}
-
-		res = append(res, bits[i])
 	}
-
-	return res
+	return i == n-1
 }
