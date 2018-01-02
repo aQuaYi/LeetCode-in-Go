5efa2408c2824c63e827ab061697b579c79c4f55
aQua <aQua@aQuaYi.com>
2018-01-02 16:56:57 +0800

    739 Limit Time Exceeded

--- Algorithms/0739.daily-temperatures/daily-temperatures.go
+++ Algorithms/0739.daily-temperatures/daily-temperatures.go
@@ -3,16 +3,24 @@ package Problem0739
 func dailyTemperatures(temperatures []int) []int {
 	n := len(temperatures)
 	res := make([]int, n)
+
+	stack := make([]record, 0, n)
+
+	last := -1
 	for i := 0; i < n; i++ {
-		j := i + 1
-		for j < n && temperatures[i] >= temperatures[j] {
-			j++
+		for last >= 0 && stack[last].t < temperatures[i] {
+			res[stack[last].i] = i - stack[last].i
+			stack = stack[:last]
+			last--
 		}
 
-		if j < n {
-			res[i] = j - i
-		}
+		stack = append(stack, record{t: temperatures[i], i: i})
+		last++
 	}
 
 	return res
+}
+
+type record struct {
+	t, i int
 }
