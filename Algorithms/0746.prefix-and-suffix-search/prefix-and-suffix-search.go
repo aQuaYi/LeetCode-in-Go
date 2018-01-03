d97a26d8c198fcf5015632e5524af8df3add05dd
aQua <aQua@aQuaYi.com>
2018-01-03 09:57:47 +0800

    746 wrong answer

--- Algorithms/0746.prefix-and-suffix-search/prefix-and-suffix-search.go
+++ Algorithms/0746.prefix-and-suffix-search/prefix-and-suffix-search.go
@@ -1,30 +1,30 @@
 package Problem0746
 
-import (
-	"strings"
-)
-
 // WordFilter 是字符过滤器
 type WordFilter struct {
-	ords []string
+	ords map[string]int
 }
 
 // Constructor 返回 WordFilter
 func Constructor(words []string) WordFilter {
-	return WordFilter{ords: words}
+	o := make(map[string]int, len(words)*5)
+	for k := 0; k < len(words); k++ {
+		for i := 0; i <= 10 && i < len(words[k]); i++ {
+			for j := len(words[k]); 0 <= j && len(words[k])-10 <= j; j-- {
+				pps := words[k][:i] + "#" + words[k][j:]
+				o[pps] = k
+			}
+		}
+	}
+	return WordFilter{ords: o}
 }
 
 // F 用于查找同时拥有前缀和后缀的单词的索引号，同时存在多个符合条件的单词的话，返回其中的最大值
 func (w *WordFilter) F(prefix, suffix string) int {
-	n := len(w.ords)
-
-	for i := n - 1; 0 <= i; i-- {
-		if strings.HasPrefix(w.ords[i], prefix) &&
-			strings.HasSuffix(w.ords[i], suffix) {
-			return i
-		}
+	pps := prefix + "#" + suffix
+	if index, ok := w.ords[pps]; ok {
+		return index
 	}
-
 	return -1
 }
 
--- Algorithms/0746.prefix-and-suffix-search/prefix-and-suffix-search_test.go
+++ Algorithms/0746.prefix-and-suffix-search/prefix-and-suffix-search_test.go
@@ -19,5 +19,19 @@ func Test_wordFilter(t *testing.T) {
 	ast.Equal(2, w.F("a", "e"))
 
 	ast.Equal(2, w.F("", ""))
+}
+func Test_wordFilter_2(t *testing.T) {
+	ast := assert.New(t)
 
+	words := []string{"pop"}
+	w := Constructor(words)
+
+	pss := [][]string{
+		{"", ""}, {"", "p"}, {"", "op"}, {"", "pop"}, {"p", ""}, {"p", "p"}, {"p", "op"}, {"p", "pop"}, {"po", ""}, {"po", "p"}, {"po", "op"}, {"po", "pop"}, {"pop", ""}, {"pop", "p"}, {"pop", "op"}, {"pop", "pop"}, {"", ""}, {"", "p"}, {"", "gp"}, {"", "pgp"}, {"p", ""}, {"p", "p"}, {"p", "gp"}, {"p", "pgp"}, {"pg", ""}, {"pg", "p"}, {"pg", "gp"}, {"pg", "pgp"}, {"pgp", ""}, {"pgp", "p"}, {"pgp", "gp"}, {"pgp", "pgp"}}
+
+	ans := []int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1, 0, 0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}
+
+	for i := 0; i < len(pss); i++ {
+		ast.Equal(ans[i], w.F(pss[i][0], pss[i][1]), "输入 %v, %d", pss[i], ans[i])
+	}
 }
