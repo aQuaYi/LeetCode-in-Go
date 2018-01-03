f4234fb4277d3859f861816ffbf08a8cb618c073
aQua <aQua@aQuaYi.com>
2018-01-03 16:22:45 +0800

    749 accepted. 13ms

--- Algorithms/0749.shortest-completing-word/shortest-completing-word.go
+++ Algorithms/0749.shortest-completing-word/shortest-completing-word.go
@@ -1,6 +1,45 @@
 package Problem0749
 
-func shortestCompletingWord(licensePlate string, words []string) string {
+import (
+	"strings"
+)
 
-	return ""
+func shortestCompletingWord(licensePlate string, words []string) string {
+	keys := getKeys(licensePlate)
+	minLen := 1<<63 - 1
+	res := ""
+	for _, w := range words {
+		if len(w) >= minLen {
+			continue
+		}
+
+		isCompleting := true
+		for k, c := range keys {
+			if strings.Count(w, k) < c {
+				isCompleting = false
+				break
+			}
+		}
+
+		if isCompleting {
+			res = w
+			minLen = len(w)
+		}
+	}
+
+	return res
+}
+
+func getKeys(licensePlate string) map[string]int {
+	licensePlate = strings.ToLower(licensePlate)
+	bs := []byte(licensePlate)
+
+	res := make(map[string]int, len(licensePlate))
+	for _, b := range bs {
+		if 'a' <= b && b <= 'z' {
+			res[string(b)]++
+		}
+	}
+
+	return res
 }
