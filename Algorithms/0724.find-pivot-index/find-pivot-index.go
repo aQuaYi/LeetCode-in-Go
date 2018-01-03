a255ae51d4d212364faf1f50b32919541b3f892c
aQua <aQua@aQuaYi.com>
2018-01-03 10:55:44 +0800

    724 finish

---
+++ Algorithms/0724.find-pivot-index/find-pivot-index.go
@@ -0,0 +1,18 @@
+package Problem0724
+
+func pivotIndex(nums []int) int {
+	sum := 0
+	for i := range nums {
+		sum += nums[i]
+	}
+
+	left := 0
+	for i := range nums {
+		if left*2+nums[i] == sum {
+			return i
+		}
+		left += nums[i]
+	}
+
+	return -1
+}
---
+++ Algorithms/0724.find-pivot-index/find-pivot-index_test.go
@@ -0,0 +1,44 @@
+package Problem0724
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	nums []int
+	ans  int
+}{
+
+	{
+		[]int{1, 7, 3, 6, 5, 6},
+		3,
+	},
+
+	{
+		[]int{1, 2, 3},
+		-1,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_fn(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, pivotIndex(tc.nums), "输入:%v", tc)
+	}
+}
+
+func Benchmark_fn(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			pivotIndex(tc.nums)
+		}
+	}
+}
