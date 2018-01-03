513cc610c7e55f0e03306cadfd50f3e7b5bc4c7b
aQua <aQua@aQuaYi.com>
2018-01-03 10:41:36 +0800

    738 finish

--- Algorithms/0738.monotone-increasing-digits/monotone-increasing-digits.go
+++ Algorithms/0738.monotone-increasing-digits/monotone-increasing-digits.go
@@ -1,7 +1,24 @@
 package Problem0738
 
-func monotoneIncreasingDigits(n int) int {
-	res := 0
+import (
+	"strconv"
+)
 
+func monotoneIncreasingDigits(n int) int {
+	s := []byte(strconv.Itoa(n))
+	for i := len(s) - 2; 0 <= i; i-- {
+		if s[i] <= s[i+1] {
+			continue
+		}
+
+		// 当 s[i] > s[i+1] 时
+		s[i]--
+		// s[i+1:] 中的数字全部变成　９
+		for j := i + 1; j < len(s); j++ {
+			s[j] = '9'
+		}
+	}
+
+	res, _ := strconv.Atoi(string(s))
 	return res
 }
