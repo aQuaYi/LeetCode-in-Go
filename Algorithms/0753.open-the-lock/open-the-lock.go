d269fd0e4ad62a7f0d67c0b4d6f89ac513868c84
aQua <aQua@aQuaYi.com>
2018-01-04 09:47:46 +0800

    753 finish

--- Algorithms/0753.open-the-lock/open-the-lock.go
+++ Algorithms/0753.open-the-lock/open-the-lock.go
@@ -1,21 +1,69 @@
 package Problem0753
 
 func openLock(deadends []string, target string) int {
-	dp := [10][10][10][10]int{}
-
-for _,d:=range deadends {
-dp[d[0]-'0'][d[1]-'0'][d[2]-'0'][d[3]-'0']	= -1
+	isDeadends := dealDeadends(deadends)
+	return bfs(convert(target), isDeadends)
 }
 
-	res := 0
+func bfs(target [4]int, isDeadends map[[4]int]bool) int {
+	queue := [][4]int{convert("0000")}
+	if isDeadends[queue[0]] {
+		return -1
+	}
+	if target == queue[0] {
+		return 0
+	}
+	isChecked := make(map[[4]int]bool, 1e4)
+	isChecked[queue[0]] = true
+	leng := 1
+	count := 1
 
+	for len(queue) > 0 {
+		if count == 0 {
+			leng++
+			count = len(queue)
+		}
+
+		for i := 0; i < 4; i++ {
+			a := queue[0]
+			a[i] = (a[i] + 9) % 10
+			if !isDeadends[a] && !isChecked[a] {
+				if a == target {
+					return leng
+				}
+				queue = append(queue, a)
+				isChecked[a] = true
+			}
+			b := queue[0]
+			b[i] = (b[i] + 1) % 10
+			if !isDeadends[b] && !isChecked[b] {
+				if b == target {
+					return leng
+				}
+				queue = append(queue, b)
+				isChecked[b] = true
+			}
+		}
+
+		queue = queue[1:]
+		count--
+	}
+
+	return -1
+}
+
+func dealDeadends(deadends []string) map[[4]int]bool {
+	res := make(map[[4]int]bool, len(deadends))
+	for _, d := range deadends {
+		res[convert(d)] = true
+	}
 	return res
 }
 
-
-func getIndex(s string) []int {
-res := make([]int, 4)	
-for i:=0; i<4; i++{
-res[i] = s[i]-'0')	
-}
+func convert(s string) [4]int {
+	res := [4]int{}
+	for i := range res {
+		res[i] = int(s[i] - '0')
+	}
+	return res
 }
