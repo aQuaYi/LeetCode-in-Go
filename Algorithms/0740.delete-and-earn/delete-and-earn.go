146ed16e157f9f78bec5977e15409e8609a52ad8
aQua <aQua@aQuaYi.com>
2018-01-04 11:34:47 +0800

    740 accepted. 9ms

--- Algorithms/0740.delete-and-earn/delete-and-earn.go
+++ Algorithms/0740.delete-and-earn/delete-and-earn.go
@@ -1,54 +1,37 @@
 package Problem0740
 
-import (
-	"sort"
-)
-
 func deleteAndEarn(nums []int) int {
 	if len(nums) == 0 {
 		return 0
 	}
 
-	sort.Ints(nums)
-
-	cs := make(cards, 0, len(nums))
-	c := card{num: nums[0], point: nums[0]}
-
-	for i := 1; i < len(nums); i++ {
-		if c.num == nums[i] {
-			c.point += nums[i]
-		} else {
-			cs = append(cs, c)
-			c = card{num: nums[i], point: nums[i]}
-		}
-	}
-	cs = append(cs, c)
-
-	res := 0
-	sort.Sort(cs)
-
-	isDeleted := make(map[int]bool, len(cs))
-	for _, c := range cs {
-		if isDeleted[c.num] {
-			continue
-		}
-		res += c.point
-		isDeleted[c.num+1] = true
-		isDeleted[c.num-1] = true
+	if len(nums) == 1 {
+		return nums[0]
 	}
 
-	return res
+	size := int(1e4 + 1)
+
+	dp := make([]int, size)
+
+	for _, n := range nums {
+		dp[n] += n
+	}
+
+	maxPoint := dp[1]
+
+	for i := 2; i < size; i++ {
+		dp[i] = max(dp[i-1], dp[i]+dp[i-2])
+		if maxPoint < dp[i] {
+			maxPoint = dp[i]
+		}
+	}
+
+	return maxPoint
 }
 
-type card struct {
-	num, point int
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
-
-// cards 实现了 sort.Interface 接口
-type cards []card
-
-func (c cards) Len() int { return len(c) }
-
-func (c cards) Less(i, j int) bool { return c[i].point > c[j].point }
-
-func (c cards) Swap(i, j int) { c[i], c[j] = c[j], c[i] }
