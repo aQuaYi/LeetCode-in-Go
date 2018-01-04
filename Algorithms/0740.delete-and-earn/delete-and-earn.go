ad624ab39288b56468218e3e0e27c62c59525cc0
aQua <aQua@aQuaYi.com>
2018-01-04 11:50:11 +0800

    740 finish

--- Algorithms/0740.delete-and-earn/delete-and-earn.go
+++ Algorithms/0740.delete-and-earn/delete-and-earn.go
@@ -4,29 +4,26 @@ func deleteAndEarn(nums []int) int {
 	if len(nums) == 0 {
 		return 0
 	}
-
 	if len(nums) == 1 {
 		return nums[0]
 	}
 
 	size := int(1e4 + 1)
-
+	// dp[n] 表示，删除 nums 中所有 <=n 的数后，能获取的最大分数
 	dp := make([]int, size)
-
 	for _, n := range nums {
 		dp[n] += n
 	}
 
-	maxPoint := dp[1]
-
 	for i := 2; i < size; i++ {
+		// 删除了 i-1，所以 i 是被消除掉的，不能累加 point
+		//              ↓
 		dp[i] = max(dp[i-1], dp[i]+dp[i-2])
-		if maxPoint < dp[i] {
-			maxPoint = dp[i]
-		}
+		//                        ↑
+		// i-1 是被消除掉的，所以，此时是 dp[i]+dp[i-2]
 	}
 
-	return maxPoint
+	return dp[size-1]
 }
 
 func max(a, b int) int {
