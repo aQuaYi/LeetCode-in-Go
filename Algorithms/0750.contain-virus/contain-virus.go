eb3c31bd99fb4c701293bca511fdbd097b74328e
aQua <aQua@aQuaYi.com>
2018-01-04 16:11:23 +0800

    750 finish

--- Algorithms/0750.contain-virus/contain-virus.go
+++ Algorithms/0750.contain-virus/contain-virus.go
@@ -1,7 +1,7 @@
 package Problem0750
 
 // 贪心算法
-// 每次把感染最多的区域围起来
+// 每次把会导致最多感染的区域围起来
 
 var dx = []int{-1, 1, 0, 0}
 var dy = []int{0, 0, -1, 1}
@@ -13,10 +13,12 @@ func containVirus(grid [][]int) int {
 		queue := make([][2]int, 1, m*n)
 		queue[0] = [2]int{i, j}
 		isChecked[i*n+j] = true
+
 		for len(queue) > 0 {
 			i, j = queue[0][0], queue[0][1]
 			queue = queue[1:]
 			grid[i][j] = regionID
+
 			for k := 0; k < 4; k++ {
 				x := i + dx[k]
 				y := j + dy[k]
@@ -31,6 +33,7 @@ func containVirus(grid [][]int) int {
 		}
 	}
 
+	// 统计区域数量，并对其进行编号
 	checkRegion := func() int {
 		regionID := 1
 		isChecked := make([]bool, m*n)
@@ -45,25 +48,33 @@ func containVirus(grid [][]int) int {
 		return regionID
 	}
 
-	// 如果不修墙的话，下次会导致最多感染的区域 ID
+	// 返回，如果不修墙的话，下次会导致最多感染的区域 ID
 	// 如果没有新感染的话，返回 -1
-	mostRegionID := func(size int) int {
+	maxRegion := func(size int) int {
+		// 统计各个区域会导致感染的最大数量
+		// 统计的时候，
+		// 	0 如果可以被同一个区域的多个细胞感染的话，只能算作这个区域的一次感染
+		//	0 如果可以被多个区域感染的话，每个区域分别多一个
 		count := make([]int, size)
 		for i := 0; i < m; i++ {
 			for j := 0; j < n; j++ {
 				if grid[i][j] == 0 {
+					isCounted := make(map[int]bool, 4)
 					for k := 0; k < 4; k++ {
 						x := i + dx[k]
 						y := j + dy[k]
 						if 0 <= x && x < m &&
 							0 <= y && y < n &&
-							grid[x][y] > 0 {
+							grid[x][y] > 0 &&
+							!isCounted[grid[x][y]] {
 							count[grid[x][y]]++
+							isCounted[grid[x][y]] = true
 						}
 					}
 				}
 			}
 		}
+		// 寻找会导致最多感染的区域
 		max := 0
 		id := -1
 		for i := 1; i < size; i++ {
@@ -76,6 +87,8 @@ func containVirus(grid [][]int) int {
 		return id
 	}
 
+	// 给指定区域安装围墙
+	// 同时把区域内的所有值，标记为 -1，就能不在统计此区域
 	installWalls := func(regionID int) int {
 		res := 0
 		for i := 0; i < m; i++ {
@@ -97,6 +110,7 @@ func containVirus(grid [][]int) int {
 		return res
 	}
 
+	// 夜间的病毒传播
 	nightInfect := func() {
 		isNewInfected := make([]bool, m*n)
 		for i := 0; i < m; i++ {
@@ -120,7 +134,7 @@ func containVirus(grid [][]int) int {
 	res := 0
 	for {
 		size := checkRegion()
-		id := mostRegionID(size)
+		id := maxRegion(size)
 		if id == -1 {
 			break
 		}
