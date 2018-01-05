8152a785e388570ff49c53149b6b602b5a3384bc
aQua <aQua@aQuaYi.com>
2018-01-05 09:41:37 +0800

    735 finish

--- Algorithms/0735.asteroid-collision/asteroid-collision.go
+++ Algorithms/0735.asteroid-collision/asteroid-collision.go
@@ -1,6 +1,31 @@
 package Problem0735
 
-func asteroidCollision(para []int) []int {
+func asteroidCollision(asteroids []int) []int {
+	res := make([]int, 0, len(asteroids))
+	stack := make([]int, 0, len(asteroids))
 
-	return nil
+	for _, a := range asteroids {
+		if a > 0 {
+			stack = append(stack, a)
+			continue
+		}
+
+		for len(stack) > 0 && stack[len(stack)-1] <= -a {
+			last := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			if last == -a {
+				a = 0
+				break
+			}
+		}
+
+		if len(stack) == 0 && a != 0 {
+			res = append(res, a)
+			continue
+		}
+	}
+
+	res = append(res, stack...)
+
+	return res
 }
