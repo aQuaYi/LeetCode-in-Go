64df76dab84f9f3c101aed62ea042eb9062e5756
aQua <aQua@aQuaYi.com>
2018-01-05 13:38:17 +0800

    747 finish

--- Algorithms/0747.min-cost-climbing-stairs/min-cost-climbing-stairs.go
+++ Algorithms/0747.min-cost-climbing-stairs/min-cost-climbing-stairs.go
@@ -1,7 +1,20 @@
 package Problem0747
 
 func minCostClimbingStairs(cost []int) int {
-	res := 0
+	n := len(cost)
+	// dp[i] 表示，爬到 i-th 台阶需要的花费
+	dp := make([]int, n+1)
 
-	return res
+	for i := 2; i <= n; i++ {
+		dp[i] = min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2])
+	}
+
+	return dp[n]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
