011af7f120bb715975fb7bb1f5fdf12511fca0e7
aQua <aQua@aQuaYi.com>
2018-01-05 11:52:07 +0800

    744 删除了 size 变量，100% coverage

--- Algorithms/0744.network-delay-time/network-delay-time.go
+++ Algorithms/0744.network-delay-time/network-delay-time.go
@@ -1,11 +1,10 @@
 package Problem0744
 
 func networkDelayTime(times [][]int, N int, K int) int {
-	size := N + 1
 	maxInt := int(1e4)
 
 	// minTime[i] == m 表示， i 节点接收到信号所需的最小时间为 m
-	minTime := make([]int, size)
+	minTime := make([]int, N+1)
 	for i := 1; i <= N; i++ {
 		minTime[i] = maxInt
 	}
@@ -13,10 +12,10 @@ func networkDelayTime(times [][]int, N int, K int) int {
 	minTime[K] = 0
 
 	// cost[i][j] = m 表示，从 i 节点到 j 节点所需的时间
-	cost := make([][]int, size)
+	cost := make([][]int, N+1)
 	for i := range cost {
-		cost[i] = make([]int, size)
-		for j := 0; j < size; j++ {
+		cost[i] = make([]int, N+1)
+		for j := 0; j <= N; j++ {
 			cost[i][j] = maxInt
 		}
 	}
@@ -24,13 +23,13 @@ func networkDelayTime(times [][]int, N int, K int) int {
 		cost[t[0]][t[1]] = t[2]
 	}
 
-	queue := make([]int, 1, size)
+	queue := make([]int, 1, N+1)
 	queue[0] = K
 	for len(queue) > 0 {
 		u := queue[0]
 		queue = queue[1:]
 
-		for v := 1; v < size; v++ {
+		for v := 1; v <= N; v++ {
 			if minTime[v] > minTime[u]+cost[u][v] {
 				minTime[v] = minTime[u] + cost[u][v]
 				queue = append(queue, v)
@@ -39,7 +38,7 @@ func networkDelayTime(times [][]int, N int, K int) int {
 	}
 
 	res := 0
-	for i := 1; i < size; i++ {
+	for i := 1; i <= N; i++ {
 		res = max(res, minTime[i])
 	}
 
