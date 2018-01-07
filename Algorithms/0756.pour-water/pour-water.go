8face3dc69f0183db8d6772e6b6be94000f7584f
aQua <aQua@aQuaYi.com>
2018-01-07 18:05:29 +0800

    756 finish

--- Algorithms/0756.pour-water/pour-water.go
+++ Algorithms/0756.pour-water/pour-water.go
@@ -10,33 +10,41 @@ func pourWater(heights []int, V int, K int) []int {
 	return heights
 }
 
+// 当 water 可以放在 cent 的 left 边时，返回 true
+// 否则，返回 false
 func isDroppedLeft(heights []int, center int) bool {
-	idx := center
+	index := center
 	i := center
 	for 0 <= i-1 && heights[i-1] <= heights[i] {
+		// 出现落差，说明
+		// i-1 是一个可行的落脚点
 		if heights[i-1] < heights[i] {
-			idx = i - 1
+			index = i - 1
 		}
 		i--
 	}
-	if idx < center {
-		heights[idx]++
+	if index < center {
+		heights[index]++
 		return true
 	}
 	return false
 }
 
+// 当 water 可以放在 cent 的 right 边时，返回 true
+// 否则，返回 false
 func isDroppedRight(heights []int, center int) bool {
-	idx := center
+	index := center
 	i := center
 	for i+1 < len(heights) && heights[i] >= heights[i+1] {
+		// 出现落差，说明
+		// i+1 是一个可行的落脚点
 		if heights[i] > heights[i+1] {
-			idx = i + 1
+			index = i + 1
 		}
 		i++
 	}
-	if center < idx {
-		heights[idx]++
+	if center < index {
+		heights[index]++
 		return true
 	}
 	return false
