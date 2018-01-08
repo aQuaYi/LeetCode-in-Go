fcb2aec7f105a27d4b9aa6dc2cbbad66a9a9d9ae
aQua <aQua@aQuaYi.com>
2018-01-08 14:18:34 +0800

    759 wrong answer

--- Algorithms/0759.set-intersection-size-at-least-two/set-intersection-size-at-least-two.go
+++ Algorithms/0759.set-intersection-size-at-least-two/set-intersection-size-at-least-two.go
@@ -13,35 +13,24 @@ func intersectionSizeTwo(intervals [][]int) int {
 	})
 
 	n := len(intervals)
-	lastA := intervals[n-1][0]
+	hasNum := make(map[int]bool, n*2)
 
-	hasNum := make(map[int]bool, len(intervals)*2)
-	hasNum[lastA] = true
-	hasNum[lastA+1] = true
+	interSect := intervals[0]
+	for i := 1; i < n; i++ {
+		if interSect[1] <= intervals[i][0] {
+			hasNum[interSect[1]] = true
+			hasNum[interSect[1]-1] = true
+			interSect = intervals[i]
+			continue
+		}
 
-	for i := 0; i < n-1; i++ {
-		update(hasNum, intervals[i])
-	}
-
-	return len(hasNum)
-}
-
-func update(hasNum map[int]bool, interval []int) {
-	countDown := 2
-	a, b := interval[0], interval[1]
-	for i := a; i <= b && countDown > 0; i++ {
-		if hasNum[i] {
-			countDown--
-		} else {
-			break
+		interSect[0] = intervals[i][0]
+		if intervals[i][1] < interSect[1] {
+			interSect[1] = intervals[i][1]
 		}
 	}
+	hasNum[interSect[0]] = true
+	hasNum[interSect[0]+1] = true
 
-	for countDown > 0 {
-		hasNum[b] = true
-		b--
-		countDown--
-	}
-
-	return
+	return len(hasNum)
 }
