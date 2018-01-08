d080c8f9a377452bd8e3f36ea83df47ca23d39e9
aQua <aQua@aQuaYi.com>
2018-01-08 11:25:10 +0800

    757 修改为闭包

--- Algorithms/0757.pyramid-transition-matrix/pyramid-transition-matrix.go
+++ Algorithms/0757.pyramid-transition-matrix/pyramid-transition-matrix.go
@@ -7,33 +7,33 @@ import (
 func pyramidTransition(bottom string, allowed []string) bool {
 	blocks := "#" + strings.Join(allowed, "#")
 
+	var helper func(string, string, int, int) bool
+	helper = func(bottom, blocks string, curr, length int) bool {
+		if curr+2 > length {
+			bottom = bottom[length:]
+			length = len(bottom)
+			if length == 1 {
+				return true
+			}
+			return helper(bottom, blocks, 0, length)
+		}
+
+		b := "#" + bottom[curr:curr+2]
+		beg := 0
+		for beg < len(blocks) {
+			index := strings.Index(blocks[beg:], b) + beg
+			if index < beg {
+				break
+			}
+			beg = index + 4
+			color := blocks[index+3 : index+4]
+			if helper(bottom+color, blocks, curr+1, length) {
+				return true
+			}
+		}
+
+		return false
+	}
+
 	return helper(bottom, blocks, 0, len(bottom))
-}
-
-func helper(bottom, blocks string, curr, length int) bool {
-	if curr+2 > length {
-		bottom = bottom[length:]
-		length = len(bottom)
-		if length == 1 {
-			return true
-		}
-		return helper(bottom, blocks, 0, length)
-	}
-
-	b := "#" + bottom[curr:curr+2]
-
-	count := strings.Count(blocks, b)
-
-	for i := 0; i < count; i++ {
-		index := strings.Index(blocks, b)
-		blk := blocks[index : index+4]
-		color := blk[3:]
-		blocks = strings.Replace(blocks, blk, "", 1)
-		blocks += blk
-		if helper(bottom+color, blocks, curr+1, length) {
-			return true
-		}
-	}
-
-	return false
 }
