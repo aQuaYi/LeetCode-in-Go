b3ab9c885fa6507c7b12dedf1b74cb2288a02480
aQua <aQua@aQuaYi.com>
2018-01-08 22:54:30 +0800

    591 ut fail

--- Algorithms/0591.tag-validator/tag-validator.go
+++ Algorithms/0591.tag-validator/tag-validator.go
@@ -1,7 +1,64 @@
 package Problem0591
 
-func isValid(code string) bool {
-	res := true
+import (
+	"strings"
+)
 
-	return res
+func isValid(code string) bool {
+	stack := make([]string, 0, 16)
+	for i := 0; i < len(code); {
+		if i > 0 && len(stack) == 0 {
+			return false
+		}
+
+		if strings.HasPrefix(code[i:], "<![CDATA[") {
+			j := i + 9
+			i = strings.Index(code[j:], "]]>") + j
+			if i < j {
+				return false
+			}
+			i += 3
+		} else if strings.HasPrefix(code[i:], "</") {
+			j := i + 2
+			i = strings.Index(code[j:], ">") + j
+			if i < j || // 没有 ">"
+				i == j || // TAG_NAME 为 ""
+				i-j > 9 { // TAG_NAME 长度超过 9
+				return false
+			}
+			for k := j; k < i; k++ {
+				if code[k] < 'A' || 'Z' < code[k] {
+					return false
+				}
+			}
+			tagName := code[j:i]
+			i++
+			n := len(stack)
+			if n == 0 || stack[n-1] != tagName {
+				return false
+			}
+			stack = stack[:n-1]
+		} else if code[i] == '<' {
+			j := i + 1
+			i := j + strings.Index(code[j:], ">")
+			if i < j || // 没有 ">"
+				i == j || // TAG_NAME 为 ""
+				i-j > 9 { // TAG_NAME 长度超过 9
+				return false
+			}
+			for k := j; k < i; k++ {
+				if code[k] < 'A' || 'Z' < code[k] {
+					return false
+				}
+			}
+			tagName := code[j:i]
+			i++
+			stack = append(stack, tagName)
+		} else {
+			i++
+		}
+
+	}
+
+	return len(stack) == 0
 }
