649e4669851e1276296a69236289707cdfcf83ff
aQua <aQua@aQuaYi.com>
2018-01-12 21:28:12 +0800

    762 finish

--- Algorithms/0762.find-anagram-mappings/find-anagram-mappings.go
+++ Algorithms/0762.find-anagram-mappings/find-anagram-mappings.go
@@ -2,29 +2,15 @@ package Problem0762
 
 func anagramMappings(A, B []int) []int {
 	n := len(A)
-	indexs := make(map[int]int, n)
 	values := make(map[int]int, n)
 	res := make([]int, n)
 
+	for i := range B {
+		values[B[i]] = i
+	}
+
 	for i := range res {
-		if A[i] == B[i] {
-			res[i] = i
-			continue
-		}
-
-		if val, ok := values[A[i]]; ok {
-			res[i] = val
-			delete(values, A[i])
-		} else {
-			indexs[A[i]] = i
-		}
-
-		if idx, ok := indexs[B[i]]; ok {
-			res[idx] = i
-			delete(indexs, B[i])
-		} else {
-			values[B[i]] = i
-		}
+		res[i] = values[A[i]]
 	}
 
 	return res
