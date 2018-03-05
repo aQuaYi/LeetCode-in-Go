8b2d00e03ab4de2aa85b4b584e748580896ff1f7
aQua <aQua@aQuaYi.com>
2018-03-05 23:21:16 +0800

    767 finish

--- Algorithms/0767.reorganize-string/reorganize-string.go
+++ Algorithms/0767.reorganize-string/reorganize-string.go
@@ -11,36 +11,45 @@ func reorganizeString(s string) string {
 	count := make([][2]int, 26)
 	maxCount := 0
 	for _, b := range bs {
-		count[b-'a'][0]++
-		count[b-'a'][1] = int(b - 'a')
+		count[b-'a'][0]++              // 0 位放字母的个数
+		count[b-'a'][1] = int(b - 'a') // 1 位字母本身
 		maxCount = max(maxCount, count[b-'a'][0])
 	}
 
-	if (n%2 == 0 && maxCount > n/2) ||
-		(n%2 == 1 && maxCount > (n/2+1)) {
+	// 利用个数最多的元素，进行快速判断。
+	if 2*maxCount > n+1 {
 		return ""
 	}
 
 	sort.Slice(count, func(i, j int) bool {
+		if count[i][0] == count[j][0] {
+			// 字母个数相同的时候，ascii 码小的在前面
+			return count[i][1] < count[j][1]
+		}
+		// 个数多的字母排在前面
 		return count[i][0] > count[j][0]
 	})
 
-	res := make([]byte, n)
-
 	idx := 0
 
-	for i := 0; count[i][0] > 0; i++ {
+	for i := 0; i < 26 && count[i][0] > 0; i++ {
 		b := byte('a' + count[i][1])
 
 		for count[i][0] > 0 {
-			res[idx] = b
+			bs[idx] = b
 			count[i][0]--
-			idx = (idx + 2) % n
+			// 填写的时候，留下间隔
+			// 避免相同的字母相邻
+			idx += 2
+			if idx >= n {
+				// 到头了以后，从 1 位重新填写
+				idx = 1
+			}
 		}
 
 	}
 
-	return string(res)
+	return string(bs)
 }
 
 func max(a, b int) int {
