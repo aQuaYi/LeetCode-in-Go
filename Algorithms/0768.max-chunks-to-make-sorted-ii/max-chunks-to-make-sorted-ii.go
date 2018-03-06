b4cd5fef7849eaf524c8530330fb74e169f19c7c
aQua <aQua@aQuaYi.com>
2018-03-06 22:45:45 +0800

    768 修改一下细节

--- Algorithms/0768.max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.go
+++ Algorithms/0768.max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.go
@@ -8,7 +8,13 @@ func maxChunksToSorted(arr []int) int {
 	return helper(convert(arr))
 }
 
-// 把 768 题的参数，转换成 769 题的参数
+// 把
+// 	arr will have length in range [1, 2000].
+// 	arr[i] will be an integer in range [0, 10**8]
+// 转换成
+// 	arr will have length in range [1, 10].
+// 	arr[i] will be a permutation of [0, 1, ..., arr.length - 1]
+// 就可以利用 769 题的答案来解答了
 func convert(arr []int) []int {
 	a := make([][]int, len(arr))
 	for i := range a {
@@ -33,23 +39,14 @@ func convert(arr []int) []int {
 
 //  直接复制了 769 的答案
 func helper(arr []int) int {
-	// 充分利用好 arr[i] will be a permutation of [0, 1, ..., arr.length - 1]
-	// 假设某个 chunk 为 arr[j:k]，则对于
-	// j <=     i  < k ，必有
-	// j <= arr[i] < k
-	// 因为排序后，arr[x]=x
-	//
-	// lastIdx 是 下一个要切下来的 chunk 中最后一个元素的索引号
 	lastIdx, res := 0, 0
 	for i := 0; i < len(arr); i++ {
 		if lastIdx < arr[i] {
 			lastIdx = arr[i]
-			// lastIdx == arr[i]，才能满足前面提到的要求
 			continue
 		}
 
 		if i == lastIdx {
-			// 此时可以切下一刀
 			res++
 			lastIdx++
 		}
