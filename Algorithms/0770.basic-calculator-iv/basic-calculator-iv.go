b05f8ed60fdcff8f59322223f2d59fd750ae19c0
aQua <aQua@aQuaYi.com>
2018-03-09 17:03:54 +0800

    770 没有找到一个合理的思路

--- Algorithms/0770.basic-calculator-iv/basic-calculator-iv.go
+++ Algorithms/0770.basic-calculator-iv/basic-calculator-iv.go
@@ -4,3 +4,20 @@ func basicCalculatorIV(expression string, evalvars []string, evalints []int) []s
 
 	return nil
 }
+
+type nums []num
+
+type num struct {
+	vars []string // 单个变量
+	c    int      // 系数
+}
+
+func (n *num) update(m map[string]int) {
+	for i, s := range n.vars {
+		if v, ok := m[s]; ok {
+			n.c *= v
+			n.vars[i] = ""
+		}
+	}
+
+}
