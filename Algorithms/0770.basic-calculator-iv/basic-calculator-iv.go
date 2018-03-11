2ae8b1337ebec381eddac1a57c812b7e9ba3fab5
aQua <aQua@aQuaYi.com>
2018-03-11 21:22:37 +0800

    思路不够清晰

--- Algorithms/0770.basic-calculator-iv/basic-calculator-iv.go
+++ Algorithms/0770.basic-calculator-iv/basic-calculator-iv.go
@@ -1,8 +1,43 @@
 package Problem0770
 
-func basicCalculatorIV(expression string, evalvars []string, evalints []int) []string {
+import (
+	"strings"
+)
 
-	return nil
+func basicCalculatorIV(expression string, evalvars []string, evalints []int) []string {
+	m := make(map[string]int, len(evalvars))
+	for i := range evalvars {
+		m[evalvars[i]] = evalints[i]
+	}
+
+	expression = strings.Replace(expression, " ", "", -1)
+
+	numbers := parse(expression, m)
+
+	return getResult(numbers)
+}
+
+func parse(expression string, m map[string]int) nums {
+	res := make(nums, 0, 128)
+	for len(expression) > 0 {
+		if expression[0] == '(' {
+			i := indexOfCounterParentheses(expression)
+			res = append(res, parse(expression[1:i], m))
+			expression = expression[i+1:]
+		}
+	}
+}
+
+func indexOfCounterParentheses(expression string) int {
+	for i := 1; i < len(expression); i++ {
+		if expression[i] == ')' {
+			return i
+		}
+	}
+}
+
+func getResult(numbers nums) []string {
+
 }
 
 type nums []num
