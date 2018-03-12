4d8fd3520dc28fe6a18c022647526ac15eb51d90
aQua <aQua@aQuaYi.com>
2018-03-12 11:49:14 +0800

    771 finish

---
+++ Algorithms/0771.jewels-and-stones/jewels-and-stones.go
@@ -0,0 +1,17 @@
+package Problem0771
+
+func numJewelsInStones(J string, S string) int {
+	isJewel := make(map[byte]bool, len(J))
+	for i := range J {
+		isJewel[J[i]] = true
+	}
+
+	res := 0
+	for i := range S {
+		if isJewel[S[i]] {
+			res++
+		}
+	}
+
+	return res
+}
---
+++ Algorithms/0771.jewels-and-stones/jewels-and-stones_test.go
@@ -0,0 +1,47 @@
+package Problem0771
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	J   string
+	S   string
+	ans int
+}{
+
+	{
+		"aA",
+		"aAAbbbb",
+		3,
+	},
+
+	{
+		"z",
+		"ZZ",
+		0,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_numJewelsInStones(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, numJewelsInStones(tc.J, tc.S), "输入:%v", tc)
+	}
+}
+
+func Benchmark_numJewelsInStones(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			numJewelsInStones(tc.J, tc.S)
+		}
+	}
+}
