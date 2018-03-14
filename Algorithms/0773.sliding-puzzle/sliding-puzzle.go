c216ab8ce5a33f342b3514ac2d1221f91351b70a
aQua <aQua@aQuaYi.com>
2018-03-14 18:53:15 +0800

    773 added

---
+++ Algorithms/0773.sliding-puzzle/sliding-puzzle.go
@@ -0,0 +1,6 @@
+package Problem0773
+
+func slidingPuzzle(board [][]int) int {
+
+	return 0
+}
---
+++ Algorithms/0773.sliding-puzzle/sliding-puzzle_test.go
@@ -0,0 +1,54 @@
+package Problem0773
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	board [][]int
+	ans   int
+}{
+
+	{
+		[][]int{{1, 2, 3}, {4, 0, 5}},
+		1,
+	},
+
+	{
+		[][]int{{1, 2, 3}, {5, 4, 0}},
+		-1,
+	},
+
+	{
+		[][]int{{4, 1, 2}, {5, 0, 3}},
+		5,
+	},
+
+	{
+		[][]int{{3, 2, 4}, {1, 5, 0}},
+		14,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_slidingPuzzle(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, slidingPuzzle(tc.board), "输入:%v", tc)
+	}
+}
+
+func Benchmark_slidingPuzzle(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			slidingPuzzle(tc.board)
+		}
+	}
+}
