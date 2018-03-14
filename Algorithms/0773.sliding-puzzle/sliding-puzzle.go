a4327d821f34185dc0a5b764bf838d3df9336cf8
aQua <aQua@aQuaYi.com>
2018-03-14 20:31:00 +0800

    773 pass

--- Algorithms/0773.sliding-puzzle/sliding-puzzle.go
+++ Algorithms/0773.sliding-puzzle/sliding-puzzle.go
@@ -5,28 +5,23 @@ import (
 )
 
 func slidingPuzzle(board [][]int) int {
-	hasSeen := make(map[string]bool, 720)
-	q := make([]string, 1, 720)
-	q[0] = convert(board)
-	hasSeen[q[0]] = true
-	target := "123450"
-
-	res := 0
-
-	if q[0] == target {
-		return res
+	original, target := "123450", convert(board)
+	if original == target {
+		return 0
 	}
 
+	q := make([]string, 1, 720)
+	q[0] = original
+
+	hasSeen := make(map[string]bool, 720)
+	hasSeen[original] = true
+
+	res := 0 // q 中最后一个候选项是 origin 经过 res 次变化得来的
 	d := []int{-1, 1, 3, -3}
 	size := len(q)
 	for len(q) > 0 {
+		//
 		s := q[0]
-		q = q[1:]
-		size--
-		if size == 0 {
-			size = len(q)
-			res++
-		}
 		i := strings.IndexByte(s, '0')
 		for k := range d {
 			j := i + d[k]
@@ -36,12 +31,9 @@ func slidingPuzzle(board [][]int) int {
 				i == 3 && j == 2 {
 				continue
 			}
-			b := []byte(s)
-			b[i], b[j] = b[j], b[i]
-
-			c := string(b)
+			c := swap(s, i, j)
 			if c == target {
-				return res
+				return res + 1
 			}
 
 			if !hasSeen[c] {
@@ -50,6 +42,12 @@ func slidingPuzzle(board [][]int) int {
 			}
 		}
 
+		q = q[1:]
+		size--
+		if size == 0 {
+			size = len(q)
+			res++
+		}
 	}
 
 	return -1
@@ -61,4 +59,10 @@ func convert(board [][]int) string {
 		res[i] = byte(board[i/3][i%3]) + '0'
 	}
 	return string(res)
+}
+
+func swap(s string, i, j int) string {
+	b := []byte(s)
+	b[i], b[j] = b[j], b[i]
+	return string(b)
 }
