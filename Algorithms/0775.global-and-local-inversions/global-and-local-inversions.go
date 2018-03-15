7e054d7e4d4016eb3bc9357b01885199d103c709
aQua <aQua@aQuaYi.com>
2018-03-15 17:12:10 +0800

    775 finish

--- Algorithms/0775.global-and-local-inversions/global-and-local-inversions.go
+++ Algorithms/0775.global-and-local-inversions/global-and-local-inversions.go
@@ -1,6 +1,19 @@
 package Problem0775
 
-func isIdealPermutation(A []int) bool {
+func isIdealPermutation(a []int) bool {
+	for i := 0; i < len(a)-1; i++ {
+		if a[i] == i {
+			continue
+		}
+
+		// 只有 **仅有** 相邻的两个数交换的时候
+		// 才有可能满足题意，局部逆序等于全局逆序
+		if a[i] == i+1 && a[i+1] == i {
+			i++
+		} else {
+			return false
+		}
+	}
 
 	return true
 }
