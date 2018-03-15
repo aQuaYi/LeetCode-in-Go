5041cad7a0355cdce4b42226f80979ce8705e537
aQua <aQua@aQuaYi.com>
2018-03-15 20:56:59 +0800

    777 finish

--- Algorithms/0777.swap-adjacent-in-lr-string/swap-adjacent-in-lr-string.go
+++ Algorithms/0777.swap-adjacent-in-lr-string/swap-adjacent-in-lr-string.go
@@ -1,12 +1,17 @@
 package problem0777
 
+import (
+	"strings"
+)
+
 func canTransform(start string, end string) bool {
-	return isOK(indexSlice(start, 'R'), indexSlice(end, 'R'), notBigger) &&
-		isOK(indexSlice(start, 'L'), indexSlice(end, 'L'), notSmaller)
+	return strings.Replace(start, "X", "", -1) == strings.Replace(end, "X", "", -1) &&
+		isOK(idxs(start, 'R'), idxs(end, 'R'), lessAndEqual) &&
+		isOK(idxs(start, 'L'), idxs(end, 'L'), moreAndEqual)
 }
 
-// indexSlice 返回 s 中所有 b 字符的索引号
-func indexSlice(s string, b byte) []int {
+// idxs 返回 s 中所有 b 字符的索引号
+func idxs(s string, b byte) []int {
 	res := make([]int, 0, len(s))
 	for i := range s {
 		if s[i] == b {
@@ -16,25 +21,29 @@ func indexSlice(s string, b byte) []int {
 	return res
 }
 
-func isOK(a, b []int, compare func(x, y int) bool) bool {
+// 对于任意的 i ，都能使的 isAvailable(a[i], b[i]) 返回 true
+// 则 isOK 返回 true
+func isOK(a, b []int, isAvailable func(x, y int) bool) bool {
 	for i := range a {
-		if compare(a[i], b[i]) {
+		if !isAvailable(a[i], b[i]) {
 			return false
 		}
 	}
 	return true
 }
 
-func notBigger(x, y int) bool {
+func lessAndEqual(x, y int) bool {
 	if x <= y {
 		return true
 	}
 	return false
 }
 
-func notSmaller(x, y int) bool {
+func moreAndEqual(x, y int) bool {
 	if x >= y {
 		return true
 	}
 	return false
 }
+
+// TODO: 使用 goroutine 来做，看看会不会快一些
