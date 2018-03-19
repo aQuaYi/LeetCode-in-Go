078880d43ff47bf7fbf7716942671e0393044192
aQua <aQua@aQuaYi.com>
2018-03-19 11:52:55 +0800

    779 finish

--- Algorithms/0779.k-th-symbol-in-grammar/k-th-symbol-in-grammar.go
+++ Algorithms/0779.k-th-symbol-in-grammar/k-th-symbol-in-grammar.go
@@ -1,23 +1,27 @@
 package problem0779
 
 func kthGrammar(N int, K int) int {
-	return helper(N, K)
+	if N == 1 {
+		return 0
+	}
+
+	if K%2 == 1 {
+		//      0
+		//     / \
+		//    0   1
+		//   /\   /\
+		//  0  1 1  0
+		// 把变化按照 binary tree 的样子写下来，会发现
+		// 当 K 为奇数时，其数值与其父节点 (N-1, (K+1)/2) 一样
+		return kthGrammar(N-1, (K+1)/2)
+	}
+
+	// 当 K 为偶数时，其数值与其父节点 (N-1, K/2) 相反
+	return opposite(kthGrammar(N-1, K/2))
 }
 
-func helper(n, k int) int {
-	row := []int8{0}
-	n--
-	for n > 0 {
-		temp := make([]int8, 0, len(row)*2)
-		for i := range row {
-			if row[i] == 0 {
-				temp = append(temp, 0, 1)
-			} else {
-				temp = append(temp, 1, 0)
-			}
-		}
-		row = temp
-		n--
-	}
-	return int(row[k-1])
+// 当 a 是 0 时，返回 1
+// 当 a 是 1 时，返回 0
+func opposite(a int) int {
+	return (a + 1) % 2
 }
