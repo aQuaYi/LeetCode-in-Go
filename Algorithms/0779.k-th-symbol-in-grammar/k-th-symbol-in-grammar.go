da30e9a1a3b3f89a70a6e67b7546fb24ca509bd6
aQua <aQua@aQuaYi.com>
2018-03-19 11:22:24 +0800

    779 Limit Time Exceeded

--- Algorithms/0779.k-th-symbol-in-grammar/k-th-symbol-in-grammar.go
+++ Algorithms/0779.k-th-symbol-in-grammar/k-th-symbol-in-grammar.go
@@ -1,6 +1,23 @@
 package problem0779
 
 func kthGrammar(N int, K int) int {
+	return helper(N, K)
+}
 
-	return 0
+func helper(n, k int) int {
+	row := []int8{0}
+	n--
+	for n > 0 {
+		temp := make([]int8, 0, len(row)*2)
+		for i := range row {
+			if row[i] == 0 {
+				temp = append(temp, 0, 1)
+			} else {
+				temp = append(temp, 1, 0)
+			}
+		}
+		row = temp
+		n--
+	}
+	return int(row[k-1])
 }
