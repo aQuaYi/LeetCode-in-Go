4294cb42034573349773795df66d5eb0540fc500
aQua <aQua@aQuaYi.com>
2018-03-20 16:59:30 +0800

    781 finish

--- Algorithms/0781.rabbits-in-forest/rabbits-in-forest.go
+++ Algorithms/0781.rabbits-in-forest/rabbits-in-forest.go
@@ -1,17 +1,35 @@
 package problem0781
 
 func numRabbits(answers []int) int {
-	isSaw := make(map[int]bool, len(answers))
-	res := 0
+	count := [1000]int{}
 	for _, v := range answers {
-		if isSaw[v] {
-			continue
-		}
-		res += v
-		isSaw[v] = true
+		count[v]++
 	}
 
-	res += len(isSaw)
+	res := 0
+
+	// 假设所有的 answer 都是 3
+	// answer  count -> res
+	//   3       1       4
+	//   3       2       4
+	//   3       3       4
+	//   3       4       4
+	//   3       5       8
+	//   3       6       8
+	//   3       7       8
+	//   3       8       8
+	//   3       9      12
+
+	for ans, c := range count {
+		if c == 0 {
+			continue
+		}
+		ans++
+		res += c / ans * ans
+		if c%ans > 0 {
+			res += ans
+		}
+	}
 
 	return res
 }
