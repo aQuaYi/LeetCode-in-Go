bb745f767a8401d60e7dffd219c798f3bfd6301d
aQua <aQua@aQuaYi.com>
2018-03-21 16:15:03 +0800

    780 wrong answer

--- Algorithms/0780.reaching-points/reaching-points.go
+++ Algorithms/0780.reaching-points/reaching-points.go
@@ -11,7 +11,13 @@ func reachingPoints(sx int, sy int, tx int, ty int) bool {
 	}
 
 	if tx > ty {
+		if ty == 1 {
+			return reachingPoints(sx, sy, tx-ty, ty)
+		}
 		return reachingPoints(sx, sy, tx%ty, ty)
+	}
+	if tx == 1 {
+		return reachingPoints(sx, sy, tx, ty-tx)
 	}
 	return reachingPoints(sx, sy, tx, ty%tx)
 }
