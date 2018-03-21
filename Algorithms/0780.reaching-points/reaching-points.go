fca756194ee03b621f806e7c12da880b7010ee2b
aQua <aQua@aQuaYi.com>
2018-03-21 16:58:15 +0800

    780 finish

--- Algorithms/0780.reaching-points/reaching-points.go
+++ Algorithms/0780.reaching-points/reaching-points.go
@@ -1,23 +1,43 @@
 package problem0780
 
+// 考虑到 x y 的范围是 [1, 10^9]
+// (x, y) 在变形的过程中，只会越变越大
+// 那么，反过来看，想要得到 (tx,ty)
+// 	当 tx > ty 时， 必有 (tx-ty,ty) -> (tx,ty)
+// 	当 tx < ty 时， 必有 (tx,ty-tx) -> (tx,ty)
+// 所以，通过递归地减小 (tx,ty) 的值，可以很快的得到答案
 func reachingPoints(sx int, sy int, tx int, ty int) bool {
-	if sx == tx && sy == ty {
-		return true
-	}
-
-	if sx > tx || sy > ty ||
-		tx == ty {
-		return false
-	}
-
+	// 让 tx 比 ty 大
 	if tx < ty {
 		sx, sy = sy, sx
 		tx, ty = ty, tx
 	}
 
-	if sy == ty && sx%sy == tx%ty {
-		return true
+	var helper func(int, int) bool
+	helper = func(x, y int) bool {
+		// 找到答案
+		if sx == x && sy == y {
+			return true
+		}
+
+		// 确定无法找到答案
+		if sx > x || sy > y ||
+			x == y {
+			// x == y 也返回 false，是因为 x-y == 0，超过了取值范围
+			return false
+		}
+
+		if sy == y {
+			// 当 sy == y 时
+			// 只有 x-y 这一种运算了。
+			// 就可以依据 sx%sy == x%y 判断结果
+			return sx%sy == x%y
+		}
+
+		// 根据对称性
+		// 直接把较小的数，放在 y 的位置
+		return helper(y, x%y)
 	}
 
-	return reachingPoints(sy, sx, ty, tx%ty)
+	return helper(tx, ty)
 }
