1e3779371da59c6aa212b521110fb4257bf8c49b
aQua <aQua@aQuaYi.com>
2018-03-23 17:03:42 +0800

    782 added

--- Algorithms/0782.transform-to-chessboard/transform-to-chessboard.go
+++ Algorithms/0782.transform-to-chessboard/transform-to-chessboard.go
@@ -4,3 +4,18 @@ func movesToChessboard(board [][]int) int {
 
 	return 0
 }
+
+// 想要 board 可行，需要满足以下条件
+// 	board 中的行，只有两种，且分别是 r 和 r 的反集
+// 	r 中 1 的个数
+// 		当 N 为偶数时， 1 的个数为 N/2
+// 		当 N 为奇数时， 1 的个数为 N/2 或 N/2+1
+// 	board 中全部 0 和 1 的个数
+// 		当 N 为偶数时， 个数等
+// 		当 N 为奇数时， 数
+// 
+//
+func isValid(board [][]int) bool {
+
+	return
+}
