2a5cab9b0645b6d8a61ca61b644c2186a712cff3
aQua <aQua@aQuaYi.com>
2018-04-06 21:44:27 +0800

    785 wrong answer

--- Algorithms/0785.is-graph-bipartite/is-graph-bipartite.go
+++ Algorithms/0785.is-graph-bipartite/is-graph-bipartite.go
@@ -1,6 +1,64 @@
 package problem0785
 
 func isBipartite(graph [][]int) bool {
+	// set[i] == 'a' 表示 node i 在 a 集合中
+	// set[i] == 'b' 表示 node i 在 b 集合中
+	// set[i] == 0 表示 node i 还没有被标记集合
+	set := [100]byte{}
 
+	// 把 node 0 放入 a 集合
+	set[0] = 'a'
+	// nodes 收集了同在一个 set 中的所有点
+	nodes := []int{0}
+	// 与 nodes 所在集合相反的集合名称
+	theOtherSet := byte('b')
+	otherNodes := make([]int, 0, 100)
+
+	for {
+		if len(nodes) == 0 {
+			// 当前集合中的 node 检查完了
+			// 就去检查另一边的集合中的 node
+			nodes = otherNodes
+			if len(nodes) == 0 {
+				// 另一边集合中，没有 node
+				// 说明，全部检查完了
+				break
+			}
+			// 为 other nodes 提供存在容器
+			otherNodes = make([]int, 0, 100)
+			// 变化集合名称
+			theOtherSet = change(theOtherSet)
+		}
+
+		// 逐个检查与 nodes[0] 相连的点
+		for _, nd := range graph[nodes[0]] {
+			// 如果 nd 已经被分好类了
+			if set[nd] == 'a' || set[nd] == 'b' {
+				// 检查 nd 的分类会不会出现矛盾
+				if set[nd] != theOtherSet {
+					// 出现矛盾说明，不能满足题意
+					return false
+				}
+			} else { // 还没有分好类
+				// 就为 nd 标记分类
+				set[nd] = theOtherSet
+				// 并放入 otherNodes 当中
+				otherNodes = append(otherNodes, nd)
+			}
+		}
+
+		// 从 nodes 中排除 nodes[0]
+		nodes = nodes[1:]
+	}
+
+	// 全部检查完成，没有出错
+	// 说明满足题意要求
 	return true
+}
+
+func change(s byte) byte {
+	if s == 'a' {
+		return 'b'
+	}
+	return 'a'
 }
