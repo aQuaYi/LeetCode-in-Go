734d9f16137d8f715d4e60264c81c82144fed125
aQua <aQua@aQuaYi.com>
2018-04-07 16:04:32 +0800

    788 accepted

--- Algorithms/0788.rotated-digits/rotated-digits.go
+++ Algorithms/0788.rotated-digits/rotated-digits.go
@@ -1,6 +1,26 @@
 package problem0788
 
-func rotatedDigits(N int) int {
+func rotatedDigits(n int) int {
+	count := 0
+	// 暴力方法，逐个检查
+	for i := 2; i <= n; i++ {
+		if isValid(i) {
+			count++
+		}
+	}
+	return count
+}
 
-	return 0
+func isValid(n int) bool {
+	var hasFoundValid bool
+	for n > 0 {
+		switch n % 10 {
+		case 2, 5, 6, 9:
+			hasFoundValid = true
+		case 3, 4, 7:
+			return false
+		}
+		n /= 10
+	}
+	return hasFoundValid
 }
