9bd62be4a5a3ac55552e5ae667611eda3f7642c3
aQua <aQua@aQuaYi.com>
2018-04-07 11:53:51 +0800

    786 Limit Time Exceed

--- Algorithms/0786.k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.go
+++ Algorithms/0786.k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.go
@@ -1,6 +1,68 @@
 package problem0786
 
-func kthSmallestPrimeFraction(A []int, K int) []int {
+import (
+	"sort"
+)
 
-	return nil
+func kthSmallestPrimeFraction(A []int, K int) []int {
+	n := len(A)
+	fracs := make([][]int, 0, n)
+
+	for i := n - 1; i > 0; i-- {
+		for j := 0; j < n-1; j++ {
+			fracs = append(fracs, []int{A[j], A[i]})
+		}
+	}
+
+	sort.Slice(fracs, func(i int, j int) bool {
+		return isLess(fracs[i], fracs[j])
+	})
+
+	return fracs[K-1]
 }
+
+// 如果 a < b ，则返回 true
+func isLess(a, b []int) bool {
+	return a[0]*b[1] < a[1]*b[0]
+}
+
+// func findKthNumber(m int, n int, k int) int {
+// 	// 后面的 count 函数，m 较小比较有利
+// 	if m > n {
+// 		m, n = n, m
+// 	}
+
+// 	lo, hi := 1, m*n
+// 	for lo < hi {
+// 		mid := lo + (hi-lo)/2
+// 		c := count(mid, m, n)
+// 		if c >= k {
+// 			// 说明 mid 大了
+// 			// 需要降低 hi
+// 			hi = mid
+// 		} else {
+// 			// 与 hi 同理，提高 lo
+// 			// +1 是为了避免死循环
+// 			lo = mid + 1
+// 		}
+// 	}
+
+// 	return hi
+// }
+
+// // v >= a * b , 1<=a<=m, 1<=b<=n
+// // count 返回 a 和 b 的所有可能组合的个数
+// func count(v, m, n int) int {
+// 	c := 0
+// 	for i := 1; i <= m; i++ {
+// 		c += min(v/i, n)
+// 	}
+// 	return c
+// }
+
+// func min(a, b int) int {
+// 	if a < b {
+// 		return a
+// 	}
+// 	return b
+// }
