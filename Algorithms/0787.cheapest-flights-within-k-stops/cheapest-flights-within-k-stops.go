94a922e612bb19ff244eb30fc1cc7fdd86d73710
aQua <aQua@aQuaYi.com>
2018-04-09 16:23:51 +0800

    787 wrong answer

--- Algorithms/0787.cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.go
+++ Algorithms/0787.cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.go
@@ -1,6 +1,43 @@
 package problem0787
 
-func findCheapestPrice(n int, flights [][]int, src int, dst int, K int) int {
+const maxPrice = 1<<63 - 1
 
-	return 0
+func findCheapestPrice(n int, flights [][]int, src int, dst int, k int) int {
+	nextGroup := make([][][]int, n)
+	for i := range flights {
+		src := flights[i][0]
+		nextGroup[src] = append(nextGroup[src], flights[i][1:])
+	}
+
+	isChecked := make([]bool, n)
+	res := maxPrice
+	dfs(src, dst, k, 0, &res, isChecked, nextGroup)
+
+	if res == maxPrice {
+		return -1
+	}
+	return res
+}
+
+func dfs(src, dst, k, spent int, res *int, isChecked []bool, nextGroup [][][]int) {
+	if src == dst {
+		*res = min(*res, spent)
+		return
+	}
+
+	if k < 0 || isChecked[src] {
+		return
+	}
+
+	isChecked[src] = true
+	for _, next := range nextGroup[src] {
+		dfs(next[0], dst, k-1, spent+next[1], res, isChecked, nextGroup)
+	}
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
