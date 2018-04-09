f1334a040a784dc6b158c4a647a137611d777979
aQua <aQua@aQuaYi.com>
2018-04-09 17:12:13 +0800

    787 wrong answer

--- Algorithms/0787.cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.go
+++ Algorithms/0787.cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.go
@@ -1,17 +1,20 @@
 package problem0787
 
+import "fmt"
+
 const maxPrice = 1<<63 - 1
 
 func findCheapestPrice(n int, flights [][]int, src int, dst int, k int) int {
 	nextGroup := make([][][]int, n)
 	for i := range flights {
-		src := flights[i][0]
-		nextGroup[src] = append(nextGroup[src], flights[i][1:])
+		nextGroup[flights[i][0]] = append(nextGroup[flights[i][0]], flights[i][1:])
 	}
 
 	isChecked := make([]bool, n)
 	res := maxPrice
-	dfs(src, dst, k, 0, &res, isChecked, nextGroup)
+	path := make([]int, k+5)
+	path[0] = src
+	dfs(src, dst, k, 0, &res, isChecked, nextGroup, path)
 
 	if res == maxPrice {
 		return -1
@@ -19,19 +22,25 @@ func findCheapestPrice(n int, flights [][]int, src int, dst int, k int) int {
 	return res
 }
 
-func dfs(src, dst, k, spent int, res *int, isChecked []bool, nextGroup [][][]int) {
+func dfs(src, dst, k, spent int, res *int, isChecked []bool, nextGroup [][][]int, path []int) {
 	if src == dst {
 		*res = min(*res, spent)
+
+		// TODO: 删除此处输出}
+		fmt.Println(path)
+
 		return
 	}
 
-	if k < 0 || isChecked[src] {
+	if k < 0 {
 		return
 	}
 
 	isChecked[src] = true
+
 	for _, next := range nextGroup[src] {
-		dfs(next[0], dst, k-1, spent+next[1], res, isChecked, nextGroup)
+		path[len(path)-k-4] = next[0]
+		dfs(next[0], dst, k-1, spent+next[1], res, isChecked, nextGroup, path)
 	}
 }
 
