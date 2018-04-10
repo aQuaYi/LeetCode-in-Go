5227f11cdc97251dd2ee5c65a8ccacddb7841239
aQua <aQua@aQuaYi.com>
2018-04-10 13:44:22 +0800

    789 first try

--- Algorithms/0789.escape-the-ghosts/escape-the-ghosts.go
+++ Algorithms/0789.escape-the-ghosts/escape-the-ghosts.go
@@ -1,6 +1,11 @@
 package problem0789
 
 func escapeGhosts(ghosts [][]int, target []int) bool {
-
-	return false
+	steps := target[0] + target[1]
+	for _, g := range ghosts {
+		if steps > g[0]+g[1] {
+			return false
+		}
+	}
+	return true
 }
