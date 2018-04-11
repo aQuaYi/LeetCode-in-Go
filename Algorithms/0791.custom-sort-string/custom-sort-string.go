15912737e266bcafde11042244372e0c198943f9
aQua <aQua@aQuaYi.com>
2018-04-11 17:00:59 +0800

    791 accepted

--- Algorithms/0791.custom-sort-string/custom-sort-string.go
+++ Algorithms/0791.custom-sort-string/custom-sort-string.go
@@ -1,6 +1,18 @@
 package problem0791
 
-func customSortString(S string, T string) string {
+import (
+	"strings"
+)
 
-	return ""
+func customSortString(S string, T string) string {
+	res := ""
+	for i := 0; i < len(S); i++ {
+		count := strings.Count(T, S[i:i+1])
+		res += strings.Repeat(S[i:i+1], count)
+		T = strings.Replace(T, S[i:i+1], "", -1)
+	}
+
+	return res + T
 }
+
+// TODO: strings.builder
