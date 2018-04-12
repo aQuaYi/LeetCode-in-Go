888a6cdb924eddaa18ae4801f81d72cd6cb726fc
aQua <aQua@aQuaYi.com>
2018-04-12 15:04:39 +0800

    790 更精简

--- Algorithms/0790.domino-and-tromino-tiling/domino-and-tromino-tiling.go
+++ Algorithms/0790.domino-and-tromino-tiling/domino-and-tromino-tiling.go
@@ -3,10 +3,7 @@ package problem0790
 const mod = 1e9 + 7
 
 func numTilings(N int) int {
-	dp := [1001]int{}
-	dp[1] = 1
-	dp[2] = 2
-	dp[3] = 5
+	dp := [1001]int{1: 1, 2: 2, 3: 5}
 	if N <= 3 {
 		return dp[N]
 	}
