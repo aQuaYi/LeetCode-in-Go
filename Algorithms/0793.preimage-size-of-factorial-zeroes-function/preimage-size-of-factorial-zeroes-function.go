01ee5c69adefbbb212207321edaf5e17c3226ecd
aQua <aQua@aQuaYi.com>
2018-04-13 15:27:24 +0800

    793 finish

--- Algorithms/0793.preimage-size-of-factorial-zeroes-function/preimage-size-of-factorial-zeroes-function.go
+++ Algorithms/0793.preimage-size-of-factorial-zeroes-function/preimage-size-of-factorial-zeroes-function.go
@@ -1,14 +1,29 @@
 package problem0793
 
-func preimageSizeFZF(K int) int {
-	for f := 305175781; f >= 1; f = (f - 1) / 5 {
-		if K == 5*f {
-			return 0
-		}
-		K %= f
-	}
+// https://leetcode.com/problems/preimage-size-of-factorial-zeroes-function/discuss/117821/Four-binary-search-solutions-based-on-different-ideas
 
-	return 5
+func preimageSizeFZF(k int) int {
+	l, r := 0, 5*(k+1)
+	for l <= r {
+		m := l + (r-l)/2
+		km := zeros(m)
+		if km < k {
+			l = m + 1
+		} else if km > k {
+			r = m - 1
+		} else {
+			return 5
+		}
+	}
+	return 0
 }
 
-// https://leetcode.com/problems/preimage-size-of-factorial-zeroes-function/discuss/117821/Four-binary-search-solutions-based-on-different-ideas
+// 返回 x! 尾部的 0 的个数
+func zeros(x int) int {
+	res := 0
+	for x > 0 {
+		res += x / 5
+		x /= 5
+	}
+	return res
+}
