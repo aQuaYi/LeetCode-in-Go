bd8d0a822c3d68b6372d5b98732b82ba4fbf132d
aQua <aQua@aQuaYi.com>
2018-04-13 23:08:15 +0800

    795 better

--- Algorithms/0795.number-of-subarrays-with-bounded-maximum/number-of-subarrays-with-bounded-maximum.go
+++ Algorithms/0795.number-of-subarrays-with-bounded-maximum/number-of-subarrays-with-bounded-maximum.go
@@ -1,28 +1,20 @@
 package problem0795
 
 func numSubarrayBoundedMax(a []int, l int, r int) int {
-	outOfRange := func(x int) bool {
-		return x < l || r < x
-	}
-	res := 0
-	lena := len(a)
-	for i := 0; i < lena; i++ {
-		if outOfRange(a[i]) {
-			continue
-		}
-		left := i - 1
-		for left >= 0 && a[left] < a[i] {
-			left--
-		}
-		left = i - left - 1
+	res, heads, tails := 0, 0, 0
 
-		right := i
-		for right < lena && a[right] <= a[i] {
-			right++
+	for i := 0; i < len(a); i++ {
+		if l <= a[i] && a[i] <= r {
+			heads += tails + 1
+			tails = 0
+			res += heads
+		} else if a[i] < l {
+			tails++
+			res += heads
+		} else {
+			heads = 0
+			tails = 0
 		}
-		right = right - i - 1
-
-		res += 1 + left + right + left*right
 	}
 
 	return res
