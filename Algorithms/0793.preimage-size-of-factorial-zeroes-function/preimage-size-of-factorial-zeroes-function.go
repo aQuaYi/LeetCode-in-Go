2fdfe2c7929f2f8b1fc2d2924ef4a280c5777740
aQua <aQua@aQuaYi.com>
2018-04-13 13:57:48 +0800

    793 finish

--- Algorithms/0793.preimage-size-of-factorial-zeroes-function/preimage-size-of-factorial-zeroes-function.go
+++ Algorithms/0793.preimage-size-of-factorial-zeroes-function/preimage-size-of-factorial-zeroes-function.go
@@ -1,6 +1,14 @@
 package problem0793
 
 func preimageSizeFZF(K int) int {
-    
-	return 0
+	for f := 305175781; f >= 1; f = (f - 1) / 5 {
+		if K == 5*f {
+			return 0
+		}
+		K %= f
+	}
+
+	return 5
 }
+
+// https://leetcode.com/problems/preimage-size-of-factorial-zeroes-function/discuss/117821/Four-binary-search-solutions-based-on-different-ideas
