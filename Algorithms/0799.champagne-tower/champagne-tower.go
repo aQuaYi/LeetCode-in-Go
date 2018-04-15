59b3ed2ba8df86d5d071c4b325ca73bd19dd8ed0
aQua <aQua@aQuaYi.com>
2018-04-15 20:34:22 +0800

    799 finish

--- Algorithms/0799.champagne-tower/champagne-tower.go
+++ Algorithms/0799.champagne-tower/champagne-tower.go
@@ -6,23 +6,32 @@ func champagneTower(poured int, queryRow int, queryGlass int) float64 {
 		queryGlass = queryRow - queryGlass
 	}
 
-	oldRow := [99]float64{}
-	oldRow[0] = float64(poured)
+	row := [100]float64{0: float64(poured)}
+	i := 0 // 代表 row 所在的行数
 	for {
-		newRow := [99]float64{}
-		for j := 0; j <= queryGlass; j++ {
-			if oldRow[j] > 1 {
-				newRow[j] += (oldRow[j] - 1) / 2
-				newRow[j+1] += (oldRow[j] - 1) / 2
-				oldRow[j] = 1
+		nextRow := [100]float64{}
+		for j := max(i+queryGlass-queryRow, 0); j <= queryGlass; j++ {
+			if row[j] > 1 {
+				// 对于所有超过的杯子，多余的香槟，分给它下面的杯子
+				nextRow[j] += (row[j] - 1) / 2
+				nextRow[j+1] += (row[j] - 1) / 2
+				row[j] = 1
 			}
 		}
 
-		queryRow--
-		if queryRow < 0 {
-			return oldRow[queryGlass]
+		if i == queryRow {
+			// 已经完成了对最后一行的检查
+			return row[queryGlass]
 		}
-		oldRow = newRow
-	}
 
+		row = nextRow
+		i++
+	}
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
