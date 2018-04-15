ca2236fe434258729ef283514d964380b226f898
aQua <aQua@aQuaYi.com>
2018-04-15 16:53:41 +0800

    797 finish

--- Algorithms/0797.all-paths-from-source-to-target/all-paths-from-source-to-target.go
+++ Algorithms/0797.all-paths-from-source-to-target/all-paths-from-source-to-target.go
@@ -1,6 +1,23 @@
 package problem0797
 
 func allPathsSourceTarget(graph [][]int) [][]int {
-    
-	return nil
+	path := make([]int, len(graph))
+	res := make([][]int, 0, len(graph))
+
+	dfs(0, len(graph)-1, 1, path, graph, &res)
+
+	return res
+}
+
+func dfs(id, dst, pathLen int, path []int, graph [][]int, res *[][]int) {
+	if id == dst {
+		temp := make([]int, pathLen)
+		copy(temp, path)
+		*res = append(*res, temp)
+	}
+
+	for _, node := range graph[id] {
+		path[pathLen] = node
+		dfs(node, dst, pathLen+1, path, graph, res)
+	}
 }
