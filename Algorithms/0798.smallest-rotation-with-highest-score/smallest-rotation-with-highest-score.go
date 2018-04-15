54a54bc286cedaa740a6200395dbcdd5b1086dd7
aQua <aQua@aQuaYi.com>
2018-04-15 17:14:44 +0800

    798 Time Limit Exceeded

--- Algorithms/0798.smallest-rotation-with-highest-score/smallest-rotation-with-highest-score.go
+++ Algorithms/0798.smallest-rotation-with-highest-score/smallest-rotation-with-highest-score.go
@@ -1,6 +1,26 @@
 package problem0798
 
-func bestRotation(A []int) int {
-    
-	return 0
+func bestRotation(a []int) int {
+	maxScore := -1
+	res := -1
+	for k := 0; k < len(a); k++ {
+		newScore := score(k, a)
+		if maxScore < newScore {
+			maxScore = newScore
+			res = k
+		}
+	}
+	return res
+}
+
+func score(k int, a []int) int {
+	s := 0
+	size := len(a)
+	for i, v := range a {
+		newIdx := (i - k + size) % size
+		if v <= newIdx {
+			s++
+		}
+	}
+	return s
 }
