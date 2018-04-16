633caff9584730f51157db1360aa5d41e59c82d3
aQua <aQua@aQuaYi.com>
2018-04-16 10:14:00 +0800

    801 wrong answer

--- Algorithms/0801.minimum-swaps-to-make-sequences-increasing/minimum-swaps-to-make-sequences-increasing.go
+++ Algorithms/0801.minimum-swaps-to-make-sequences-increasing/minimum-swaps-to-make-sequences-increasing.go
@@ -3,17 +3,15 @@ package problem0801
 func minSwap(a []int, b []int) int {
 	res := 0
 	size := len(a)
-
-	for i := 0; i < size-1; i++ {
-		if a[i] < a[i+1] {
-			continue
-		}
-		a[i], b[i] = b[i], a[i]
-		res++
+	if size%2 == 0 {
+		a = append(a, 3000)
+		b = append(b, 3000)
+		size++
 	}
 
-	for i := 0; i < size-1; i++ {
-		if b[i] < b[i+1] {
+	for i := 1; i < size; i += 2 {
+		if a[i-1] < a[i] && a[i] < a[i+1] &&
+			b[i-1] < b[i] && b[i] < b[i+1] {
 			continue
 		}
 		a[i], b[i] = b[i], a[i]
