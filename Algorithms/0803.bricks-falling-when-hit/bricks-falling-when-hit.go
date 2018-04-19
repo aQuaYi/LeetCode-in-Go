287e3838bf55e41cd172c070ac4f812e0ea92a6c
aQua <aQua@aQuaYi.com>
2018-04-19 21:51:04 +0800

    803 panic

--- Algorithms/0803.bricks-falling-when-hit/bricks-falling-when-hit.go
+++ Algorithms/0803.bricks-falling-when-hit/bricks-falling-when-hit.go
@@ -1,15 +1,48 @@
 package problem0803
 
 func hitBricks(grid [][]int, hits [][]int) []int {
+	m, n := len(grid), len(grid[0])
 	res := make([]int, len(hits))
 	for idx, hit := range hits {
 		i, j := hit[0], hit[1]
 		grid[i][j] = 0
 		for k := 0; k < 4; k++ {
-			res[idx] += check(i+dx[k], j+dy[k], grid)
+			x := i + dx[k]
+			y := j + dy[k]
+			isVisited := make([]bool, m*n)
+			if isHanging(x, y, isVisited, grid) {
+				continue
+			}
+			res[idx] += check(x, y, grid)
 		}
 	}
 	return res
+}
+
+func isHanging(i, j int, isVisited []bool, grid [][]int) bool {
+	m, n := len(grid), len(grid[0])
+
+	if i < 0 || m <= i ||
+		j < 0 || n <= j ||
+		grid[i][j] == 0 {
+		return false
+	}
+
+	isVisited[i*n+j] = true
+
+	for k := 0; k < 4; k++ {
+		x := i + dx[k]
+		y := j + dy[k]
+		if x == 0 && grid[x][y] == 1 {
+			return true
+		}
+		isVisited[x*n+y] = true
+		if isHanging(x, y, isVisited, grid) {
+			return true
+		}
+	}
+
+	return false
 }
 
 func check(i, j int, grid [][]int) int {
@@ -56,5 +89,5 @@ func check(i, j int, grid [][]int) int {
 	return len(locations)
 }
 
-var dx = []int{-1, 1, 0, 0}
-var dy = []int{0, 0, -1, 1}
+var dx = []int{-1, 0, 0, 1}
+var dy = []int{0, -1, 1, 0}
