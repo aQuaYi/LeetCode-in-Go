560b09b04f450c0092167ccb6600b80559ce799c
aQua <aQua@aQuaYi.com>
2018-04-20 00:30:16 +0800

    803 finish

--- Algorithms/0803.bricks-falling-when-hit/bricks-falling-when-hit.go
+++ Algorithms/0803.bricks-falling-when-hit/bricks-falling-when-hit.go
@@ -5,62 +5,75 @@ func hitBricks(grid [][]int, hits [][]int) []int {
 	res := make([]int, len(hits))
 
 	for i := range hits {
+		// 先把所有的点都消去
+		// 使用 -- 而不是 =0
+		// 是为了让原先是 0 的点变成 -1
+		// 这很重要
 		grid[hits[i][0]][hits[i][1]]--
 	}
 
+	// 全部点消去后，仍然挂着的点，全部变成 2
 	for j := 0; j < len(grid[0]); j++ {
 		if grid[0][j] == 1 {
-			update(2, 0, j, grid)
+			check(0, j, grid)
 		}
 	}
 
+	// 倒着往上面添加点
 	for idx := len(hits) - 1; 0 <= idx; idx-- {
 		i, j := hits[idx][0], hits[idx][1]
-		if grid[i][j] == -1 {
+		// 先把 (i,j) 还原
+		grid[i][j]++
+		if grid[i][j] == 0 {
+			// 原先就是 0 的点，不会引起别的点下落
 			continue
 		}
-		grid[i][j] = 1
+		// 然后，检查 (i,j) 是否和 2 相连
+		// 或者
+		// i == 0，表示 (i,j) 本身，就在顶部
 		if (0 <= i-1 && grid[i-1][j] == 2) ||
 			(0 <= j-1 && grid[i][j-1] == 2) ||
 			(i+1 < m && grid[i+1][j] == 2) ||
 			(j+1 < n && grid[i][j+1] == 2) ||
 			i == 0 {
-			res[idx] = drop(2, i, j, grid) - 1
+			// 所有和 (i,j) 相连的值为 1 的点，都变成 2
+			// 这些点在 (i,j) 被擦除后，都会掉落
+			// 但是这些点中也包含了 (i,j) ，所以结果要 -1
+			res[idx] = add(i, j, grid) - 1
 		}
 	}
+
 	return res
 }
 
-func update(hanging, i, j int, grid [][]int) {
-	grid[i][j] = hanging
+func check(i, j int, grid [][]int) {
+	grid[i][j] = 2
 	m, n := len(grid), len(grid[0])
 	for k := 0; k < 4; k++ {
 		x := i + dx[k]
 		y := j + dy[k]
-		if 0 <= x && x < m && 0 <= y && y < n && grid[x][y] == hanging-1 {
-			update(hanging, x, y, grid)
+		if 0 <= x && x < m && 0 <= y && y < n && grid[x][y] == 1 {
+			check(x, y, grid)
 		}
 	}
 }
 
-func drop(hanging, i, j int, grid [][]int) int {
+func add(i, j int, grid [][]int) int {
 	m, n := len(grid), len(grid[0])
 
 	if i < 0 || m <= i ||
 		j < 0 || n <= j ||
-		grid[i][j] == hanging ||
-		grid[i][j] == -1 ||
-		grid[i][j] == 0 {
+		grid[i][j] != 1 {
 		return 0
 	}
 
-	grid[i][j] = hanging
+	grid[i][j] = 2
 	res := 1
 
 	for k := 0; k < 4; k++ {
 		x := i + dx[k]
 		y := j + dy[k]
-		res += drop(hanging, x, y, grid)
+		res += add(x, y, grid)
 	}
 
 	return res
