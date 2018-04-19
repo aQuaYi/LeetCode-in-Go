5a53238d6dbca0d30bfc074f4ea40986d2913150
aQua <aQua@aQuaYi.com>
2018-04-19 22:11:04 +0800

    803 Limit Time Exceeded

--- Algorithms/0803.bricks-falling-when-hit/bricks-falling-when-hit.go
+++ Algorithms/0803.bricks-falling-when-hit/bricks-falling-when-hit.go
@@ -3,9 +3,11 @@ package problem0803
 func hitBricks(grid [][]int, hits [][]int) []int {
 	m, n := len(grid), len(grid[0])
 	res := make([]int, len(hits))
+
 	for idx, hit := range hits {
 		i, j := hit[0], hit[1]
 		grid[i][j] = 0
+
 		for k := 0; k < 4; k++ {
 			x := i + dx[k]
 			y := j + dy[k]
@@ -13,9 +15,11 @@ func hitBricks(grid [][]int, hits [][]int) []int {
 			if isHanging(x, y, isVisited, grid) {
 				continue
 			}
-			res[idx] += check(x, y, grid)
+
+			res[idx] += drop(x, y, grid)
 		}
 	}
+
 	return res
 }
 
@@ -38,9 +42,6 @@ func isHanging(i, j int, isVisited []bool, grid [][]int) bool {
 	for k := 0; k < 4; k++ {
 		x := i + dx[k]
 		y := j + dy[k]
-		if x == 0 && grid[x][y] == 1 {
-			return true
-		}
 		if isHanging(x, y, isVisited, grid) {
 			return true
 		}
@@ -49,48 +50,25 @@ func isHanging(i, j int, isVisited []bool, grid [][]int) bool {
 	return false
 }
 
-func check(i, j int, grid [][]int) int {
+func drop(i, j int, grid [][]int) int {
 	m, n := len(grid), len(grid[0])
 
-	if i <= 0 || m <= i ||
+	if i < 0 || m <= i ||
 		j < 0 || n <= j ||
 		grid[i][j] == 0 {
 		return 0
 	}
 
-	isVisited := make([]bool, m*n)
-	isVisited[i*n+j] = true
-	locations := make([][]int, 1, m*n)
-	locations[0] = []int{i, j}
-	cur := 0
+	grid[i][j] = 0
+	res := 1
 
-	for cur < len(locations) {
-		i = locations[cur][0]
-		j = locations[cur][1]
-
-		for k := 0; k < 4; k++ {
-			x := i + dx[k]
-			y := j + dy[k]
-
-			if x == 0 && grid[x][y] == 1 {
-				return 0
-			}
-
-			if 0 < x && x < m && 0 <= y && y < n && !isVisited[x*n+y] && grid[x][y] == 1 {
-				isVisited[x*n+y] = true
-				locations = append(locations, []int{x, y})
-			}
-
-		}
-
-		cur++
+	for k := 0; k < 4; k++ {
+		x := i + dx[k]
+		y := j + dy[k]
+		res += drop(x, y, grid)
 	}
 
-	for _, loc := range locations {
-		grid[loc[0]][loc[1]] = 0
-	}
-
-	return len(locations)
+	return res
 }
 
 var dx = []int{-1, 0, 0, 1}
