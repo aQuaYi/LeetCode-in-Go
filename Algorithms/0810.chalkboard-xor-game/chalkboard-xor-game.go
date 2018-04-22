1991b7f2771610300c49f554bce7cf7d818a4995
aQua <aQua@aQuaYi.com>
2018-04-22 19:36:34 +0800

    810 finish

--- Algorithms/0810.chalkboard-xor-game/chalkboard-xor-game.go
+++ Algorithms/0810.chalkboard-xor-game/chalkboard-xor-game.go
@@ -1,42 +1,20 @@
 package problem0810
 
-import "sort"
-
 func xorGame(nums []int) bool {
-	sort.Ints(nums)
-	return game(nums)
+	n := len(nums)
+	xor := 0
+	for i := 0; i < n; i++ {
+		xor ^= nums[i]
+	}
+	return xor == 0 || n%2 == 0
 }
 
-func game(nums []int) bool {
-	if xor(nums) == 0 {
-		return true
-	}
-	idx := 0
-	num := -1
-	for ; idx < len(nums); idx++ {
-		if nums[idx] == num {
-			continue
-		}
-		temp := make([]int, len(nums)-1)
-		copy(temp[:idx], nums[:idx])
-		copy(temp[idx:], nums[idx+1:])
-		if !game(temp) {
-			return true
-		}
-		num = nums[idx]
-	}
-	return false
-}
+// Let’s discuss it if we add this condition.
+// If xor == 0, Alice win directly.
+// If xor != 0 and length of numbers is even, Alice will win.
 
-func xor(nums []int) int {
-	if len(nums) == 0 {
-		return 0
-	}
-	res := nums[0]
-
-	for i := 1; i < len(nums); i++ {
-		res ^= nums[i]
-	}
-
-	return res
-}
+// Beacause:
+// All numbers won’t be the same. Otherwise xor will be equal to 0
+// If all numbers are not the same, It means there are at least 2 different numbers.
+// Alice can always erase a number different from current xor.
+// So Alice won’t never lose this turn at this situation.
--- Algorithms/0810.chalkboard-xor-game/chalkboard-xor-game_test.go
+++ Algorithms/0810.chalkboard-xor-game/chalkboard-xor-game_test.go
@@ -9,23 +9,33 @@ import (
 
 // tcs is testcase slice
 var tcs = []struct {
+	name string
 	nums []int
 	ans  bool
 }{
 
 	{
+		"1",
 		[]int{7, 8, 6, 10, 14, 15, 16, 6, 4, 0, 4, 9, 3, 3, 8, 10, 5, 5, 3, 10},
 		true,
 	},
 
 	{
+		"2",
 		[]int{0, 4, 4, 0, 4, 4, 2, 2, 2, 3, 0, 3, 1, 3, 2, 4, 2, 0, 1, 3},
 		true,
 	},
 
 	{
+		"3",
 		[]int{1, 1, 2},
 		false,
+	},
+
+	{
+		"4",
+		[]int{1, 3, 2},
+		true,
 	},
 
 	// 可以有多个 testcase
@@ -36,7 +46,7 @@ func Test_xorGame(t *testing.T) {
 
 	for _, tc := range tcs {
 		fmt.Printf("~~%v~~\n", tc)
-		ast.Equal(tc.ans, xorGame(tc.nums), "输入:%v", tc)
+		ast.Equal(tc.ans, xorGame(tc.nums), "%s, 输入:%v", tc.name, tc)
 	}
 }
 
