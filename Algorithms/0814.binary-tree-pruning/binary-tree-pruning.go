9ddcae79b5e06f15f6b5f86dec7530dd5ed3db98
aQua <aQua@aQuaYi.com>
2018-04-23 22:38:48 +0800

    814 finish

--- Algorithms/0814.binary-tree-pruning/binary-tree-pruning.go
+++ Algorithms/0814.binary-tree-pruning/binary-tree-pruning.go
@@ -22,17 +22,13 @@ func pruneTree(root *TreeNode) *TreeNode {
 		return nil
 	}
 
-	pl := pruneTree(root.Left)
-	pr := pruneTree(root.Right)
+	root.Left = pruneTree(root.Left)
+	root.Right = pruneTree(root.Right)
 
 	if root.Val == 0 &&
-		pl == nil &&
-		pr == nil {
+		root.Left == nil &&
+		root.Right == nil {
 		return nil
 	}
-
-	root.Left = pl
-	root.Right = pr
-
 	return root
 }
