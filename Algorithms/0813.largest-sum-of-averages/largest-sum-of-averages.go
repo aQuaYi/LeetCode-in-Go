72dc9d326d87ffeba2c4b8d471504ca24d5d1d44
aQua <aQua@aQuaYi.com>
2018-04-23 13:06:34 +0800

    813 Limit Time Exceeded

--- Algorithms/0813.largest-sum-of-averages/largest-sum-of-averages.go
+++ Algorithms/0813.largest-sum-of-averages/largest-sum-of-averages.go
@@ -1,6 +1,30 @@
 package problem0813
 
 func largestSumOfAverages(A []int, K int) float64 {
-    
-	return 0
+	if K == 1 {
+		return avg(A)
+	}
+	sum := 0.0
+	maxAvg := -1.
+	for i := 0; i < len(A)-K+1; i++ {
+		sum += float64(A[i])
+		maxAvg = max(maxAvg, sum/float64(i+1)+largestSumOfAverages(A[i+1:], K-1))
+	}
+
+	return maxAvg
+}
+
+func max(a, b float64) float64 {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func avg(A []int) float64 {
+	sum := 0
+	for i := range A {
+		sum += A[i]
+	}
+	return float64(sum) / float64(len(A))
 }
