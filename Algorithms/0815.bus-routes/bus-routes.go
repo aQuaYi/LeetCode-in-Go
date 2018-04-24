a8df12f96e33f098828f4e35ca3d842942017ce1
aQua <aQua@aQuaYi.com>
2018-04-24 17:26:20 +0800

    815 wrong answer

--- Algorithms/0815.bus-routes/bus-routes.go
+++ Algorithms/0815.bus-routes/bus-routes.go
@@ -1,6 +1,51 @@
 package problem0815
 
 func numBusesToDestination(routes [][]int, S int, T int) int {
-    
-	return 0
+	// buses[7]=={0,1} 表示
+	// 7 号站点，有 0,1 两辆 bus 停靠
+	busesSlice := make(map[int][]int, len(routes))
+	for i := 0; i < len(routes); i++ {
+		for j := 0; j < len(routes[i]); j++ {
+			busesSlice[routes[i][j]] = append(busesSlice[routes[i][j]], i)
+		}
+	}
+	isVisited := make(map[int]bool, len(routes))
+	isVisited[S] = true
+	ans := 1<<63 - 1
+	dfs(S, T, 1, &ans, busesSlice, isVisited, routes)
+
+	if ans == 1<<63-1 {
+		return -1
+	}
+	return ans
+}
+
+func dfs(s, t, k int, ans *int, busesSlice map[int][]int, isVisited map[int]bool, routes [][]int) {
+	buses := busesSlice[s]
+
+	for _, bus := range buses {
+		stops := routes[bus]
+		for _, stop := range stops {
+			if isVisited[stop] {
+				continue
+			}
+
+			if stop == t {
+				*ans = min(*ans, k)
+				return
+			}
+
+			if !isVisited[stop] {
+				isVisited[stop] = true
+				dfs(stop, t, k+1, ans, busesSlice, isVisited, routes)
+			}
+		}
+	}
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
