86fb09f620291cf2f32d694f878fb63880fe9269
aQua <aQua@aQuaYi.com>
2018-04-25 22:29:51 +0800

    22 finish

--- Algorithms/0022.generate-parentheses/generate-parentheses.go
+++ Algorithms/0022.generate-parentheses/generate-parentheses.go
@@ -1,28 +1,31 @@
 package problem0022
 
 func generateParenthesis(n int) []string {
-	res := []string{}
-	bytes := make([]byte, 0, n*2)
-	gen(n, n, 0, bytes, &res)
+	res := make([]string, 0, n*n)
+	bytes := make([]byte, n*2)
+	dfs(n, n, 0, bytes, &res)
 	return res
 }
 
-func gen(left, right, idx int, bytes []byte, res *[]string) {
-	// 成功找到一个解
+func dfs(left, right, idx int, bytes []byte, res *[]string) {
+	// 所有符号都添加完毕
 	if left == 0 && right == 0 {
 		*res = append(*res, string(bytes))
 		return
 	}
 
-	// 因为左括号不用担心匹配问题，只要还有左括号，就可以随便加。
+	// "(" 不用担心匹配问题，
+	// 只要 left > 0 就可以直接添加
 	if left > 0 {
 		bytes[idx] = '('
-		gen(left-1, right, idx+1, bytes, res)
+		dfs(left-1, right, idx+1, bytes, res)
 	}
 
-	// 右括号只有在左括号剩余较少的前提下，才能加
+	// 想要添加 ")" 时
+	// 需要 left < right，
+	// 即在 bytes[:idx] 至少有一个 "(" 可以与 这个 ")" 匹配
 	if right > 0 && left < right {
 		bytes[idx] = ')'
-		gen(left, right-1, idx+1, bytes, res)
+		dfs(left, right-1, idx+1, bytes, res)
 	}
 }
