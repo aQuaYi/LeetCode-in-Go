9ff9c90b92b53403085e1e9f9ce21d470db569fc
aQua <aQua@aQuaYi.com>
2018-05-16 10:45:44 +0800

    820 wrong answer

--- Algorithms/0820.short-encoding-of-words/short-encoding-of-words.go
+++ Algorithms/0820.short-encoding-of-words/short-encoding-of-words.go
@@ -1,6 +1,39 @@
 package problem0820
 
+import (
+	"sort"
+)
+
 func minimumLengthEncoding(words []string) int {
-    
-	return 0
+	sort.Slice(words, func(i, j int) bool {
+		curi := len(words[i])
+		curj := len(words[j])
+		for curi > 0 && curj > 0 {
+			curi--
+			curj--
+			if words[i][curi] > words[j][curj] {
+				return true
+			}
+		}
+
+		return len(words[i]) < len(words[j])
+	})
+
+	words = append(words, "")
+	res, i := 0, 1
+	for ; i < len(words); i++ {
+		if !endWith(words[i], words[i-1]) {
+			res += len(words[i-1]) + 1
+		}
+	}
+
+	return res
+}
+
+// if s end with post, return true
+func endWith(s, post string) bool {
+	if len(s) < len(post) {
+		return false
+	}
+	return s[len(s)-len(post):] == post
 }
