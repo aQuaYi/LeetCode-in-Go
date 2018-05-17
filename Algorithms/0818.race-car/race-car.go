b20012dc27b5a26d5d5ac36f405edcbccc96d0fb
aQua <aQua@aQuaYi.com>
2018-05-17 14:24:12 +0800

    818 wrong answer

--- Algorithms/0818.race-car/race-car.go
+++ Algorithms/0818.race-car/race-car.go
@@ -1,6 +1,46 @@
 package problem0818
 
+import (
+	"math"
+)
+
 func racecar(target int) int {
-    
-	return 0
+	switch target {
+	case 1:
+		return 1
+	case -1:
+		return 2
+	default:
+		return helper(target, 0)
+	}
+}
+
+func helper(target, pre int) int {
+	switch target {
+	case 0:
+		return pre
+	case 1:
+		return pre + 1
+	}
+
+	s := step(target)
+
+	n := nextTarget(target, s)
+
+	if n < 0 {
+		pre += s + 1
+		return helper(-n, pre)
+	}
+	pre += s + 2
+	return helper(n, pre)
+
+}
+
+func step(target int) int {
+	return int(math.Log2(float64(target))*2+1) / 2
+}
+
+func nextTarget(target, step int) int {
+	return target -
+		(int(math.Pow(2, float64(step))) - 1)
 }
