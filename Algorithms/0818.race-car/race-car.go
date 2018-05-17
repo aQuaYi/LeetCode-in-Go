af631dd42957d919cde02f699fcc1c08978604b6
aQua <aQua@aQuaYi.com>
2018-05-17 15:27:18 +0800

    818 finish

--- Algorithms/0818.race-car/race-car.go
+++ Algorithms/0818.race-car/race-car.go
@@ -4,43 +4,51 @@ import (
 	"math"
 )
 
-func racecar(target int) int {
-	switch target {
-	case 1:
-		return 1
-	case -1:
-		return 2
-	default:
-		return helper(target, 0)
-	}
-}
+var dp [10001]int
 
-func helper(target, pre int) int {
-	switch target {
-	case 0:
-		return pre
-	case 1:
-		return pre + 1
+func racecar(t int) int {
+	if dp[t] > 0 {
+		return dp[t]
 	}
 
-	s := step(target)
+	// 2^(n-1) <= target < 2^n
+	n := uint(math.Log2(float64(t))) + 1
 
-	n := nextTarget(target, s)
+	if t == 1<<n-1 {
+		// n 个 A，就可以正好走到 t
+		dp[t] = int(n)
+	} else {
+		// 一开始就 n 个 A，距离 (1<<n-1) > t 的情况
+		dp[t] = racecar(1<<n-1-t) + int(n) + 1
+		// 		^					^		 ^
+		// 		|					|		 |
+		// 		|					|		转向 R
+		// 		|					n 个 A, 到达 1<<n-1 需要的步骤数目
+		// 		走完剩下距离的最优解
 
-	if n < 0 {
-		pre += s + 1
-		return helper(-n, pre)
+		for m := uint(0); m < n-1; m++ {
+			// 一开始就连续 (n-1) 个 A，到达 1<<(n-1)-1
+			// 转向 R
+			// 连续 m 个 A，到达 1<<(n-1) - 1 - (1<<m - 1) = 1<<(n-1) - 1<<m
+			// 再一次转向 R，如果不转向的话，就是背朝着 target 了
+			dp[t] = min(dp[t], racecar(t-1<<(n-1)+1<<m)+int(n-1)+1+int(m)+1)
+			//					^						 ^	 	 ^  ^     ^
+			// 					|						 |       |  |	  |
+			// 					|						 |     	 |  |	转向 R
+			// 					|	 					 | 	  	 |  m 个 A
+			// 					| 						 |       转向 R
+			// 					|						n-1 个 A
+			// 					走完剩下的距离的最优解
+
+		}
 	}
-	pre += s + 2
-	return helper(n, pre)
 
+	return dp[t]
 }
 
-func step(target int) int {
-	return int(math.Log2(float64(target))*2+1) / 2
-}
-
-func nextTarget(target, step int) int {
-	return target -
-		(int(math.Pow(2, float64(step))) - 1)
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
