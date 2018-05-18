14c0342a32f93256ac9660600af69f691660708a
aQua <aQua@aQuaYi.com>
2018-05-18 08:58:44 +0800

    822 finish

--- Algorithms/0822.card-flipping-game/card-flipping-game.go
+++ Algorithms/0822.card-flipping-game/card-flipping-game.go
@@ -1,27 +1,38 @@
 package problem0822
 
 func flipgame(fronts []int, backs []int) int {
-	count := make(map[int]int, len(fronts)+len(backs))
+	size := len(fronts)
 
-	for i := range fronts {
-		count[fronts[i]]++
-	}
-
-	for i := range backs {
-		count[backs[i]]++
-	}
-
-	upLimit := 2001
-	min := upLimit
-	for n, c := range count {
-		if c == 1 && min > n {
-			min = n
+	isBoth := make(map[int]bool, size)
+	for i := 0; i < size; i++ {
+		if fronts[i] == backs[i] {
+			isBoth[fronts[i]] = true
 		}
 	}
 
-	if min == upLimit {
+	upLimit := 2001
+
+	res := upLimit
+
+	for i := 0; i < size; i++ {
+		if !isBoth[fronts[i]] {
+			res = min(res, fronts[i])
+		}
+		if !isBoth[backs[i]] {
+			res = min(res, backs[i])
+		}
+	}
+
+	if res == upLimit {
 		return 0
 	}
-	return min
+	return res
 
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
