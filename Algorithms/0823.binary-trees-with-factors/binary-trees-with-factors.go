c1c48d52f4c2d176a8799df2d9a8d1b5efcbf4dc
aQua <aQua@aQuaYi.com>
2018-05-20 00:05:20 +0800

    823 wrong answer

--- Algorithms/0823.binary-trees-with-factors/binary-trees-with-factors.go
+++ Algorithms/0823.binary-trees-with-factors/binary-trees-with-factors.go
@@ -1,6 +1,53 @@
 package problem0823
 
+import (
+	"sort"
+)
+
+const (
+	modulo = 1E9 + 7
+)
+
 func numFactoredBinaryTrees(A []int) int {
-    
-	return 0
+	sort.Ints(A)
+
+	factorIndex := make(map[int]int, len(A))
+	for i := range A {
+		factorIndex[A[i]] = i
+	}
+
+	ress := make([]int, len(A))
+	for i := range ress {
+		ress[i] = 1
+	}
+
+	for i := 1; i < len(A); i++ {
+		for j := 0; j < i; j++ {
+			quotient, remainder := A[i]/A[j], A[i]%A[j]
+
+			if remainder != 0 ||
+				(factorIndex[quotient] == 0 && quotient != A[0]) {
+				continue
+			}
+
+			ress[i] += ress[j]
+			ress[i] = mod(ress[i])
+
+		}
+	}
+
+	return sum(ress)
+}
+
+func mod(n int) int {
+	return n % modulo
+}
+
+func sum(a []int) int {
+	res := 0
+	for i := range a {
+		res += a[i]
+		res = mod(res)
+	}
+	return res
 }
