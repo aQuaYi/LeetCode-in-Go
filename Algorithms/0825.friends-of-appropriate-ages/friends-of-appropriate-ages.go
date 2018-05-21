1be1dd74de3abcb1c6259423482b372a702cc39e
aQua <aQua@aQuaYi.com>
2018-05-21 17:00:34 +0800

    825 finish

--- Algorithms/0825.friends-of-appropriate-ages/friends-of-appropriate-ages.go
+++ Algorithms/0825.friends-of-appropriate-ages/friends-of-appropriate-ages.go
@@ -1,24 +1,39 @@
 package problem0825
 
 func numFriendRequests(ages []int) int {
-	size := len(ages)
-	res := 0
-	for i := 0; i < size; i++ {
-		for j := 0; j < size; j++ {
-			if i == j || isNotRequest(ages[i], ages[j]) {
-				continue
-			}
-			res++
-		}
+	count := [121]int{}
+	for i := range ages {
+		count[ages[i]]++
 	}
+
+	sums := [121]int{}
+	sum := 0
+	res := 0
+	for a := range count {
+		sum += count[a]
+		sums[a] = sum
+
+		if a <= 14 || count[a] == 0 {
+			// 此时 a/2+7 >= a
+			continue
+		}
+
+		b := a/2 + 7
+
+		res += count[a] * (sums[a] - sums[b] - 1)
+		// sums[a] - sums[b]，年龄段 (b,a] 中的人数
+		// sums[a] - sums[b] - 1，减一是因为不能和自己交朋友
+		//
+		// 题目中的 3 条交友规则，归纳总结一下，就是
+		// 年龄为 a 的人
+		// 只能和年龄段 (a/2+7, a] 中除了自己以外的人交朋友，
+	}
+
 	return res
 }
 
-func isNotRequest(a, b int) bool {
-	if b <= a/2+7 ||
-		b > a ||
-		(b > 100 && a < 100) {
-		return true
-	}
-	return false
-}
+/**
+ * 当 age[B] <= 0.5 * age[A] + 7 和 age[B] > age[A] 成立时
+ * age[B] > 100 && age[A] < 100 一定成立，所以这个限制是障眼法
+ *
+**/
