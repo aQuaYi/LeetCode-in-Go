94e7f31ef7d8d5f53ab5a4a70e49c4f9afa43323
aQua <aQua@aQuaYi.com>
2018-05-23 09:18:50 +0800

    826 accepted 244ms

--- Algorithms/0826.most-profit-assigning-work/most-profit-assigning-work.go
+++ Algorithms/0826.most-profit-assigning-work/most-profit-assigning-work.go
@@ -1,18 +1,37 @@
 package problem0826
 
+import (
+	"sort"
+)
+
 func maxProfitAssignment(difficulty []int, profit []int, worker []int) int {
+	works := make([]work, len(difficulty))
+	for i := range difficulty {
+		works[i] = work{
+			d: difficulty[i],
+			p: profit[i],
+		}
+	}
+
+	sort.Slice(works, func(i int, j int) bool {
+		if works[i].p == works[j].p {
+			return works[i].d < works[j].d
+		}
+		return works[i].p > works[j].p
+	})
+
 	res := 0
 	for _, w := range worker {
-		p := 0
-		for i := range difficulty {
-			if difficulty[i] > w {
-				continue
-			}
-			if p < profit[i] {
-				p = profit[i]
+		for i := range works {
+			if works[i].d <= w {
+				res += works[i].p
+				break
 			}
 		}
-		res += p
 	}
 	return res
+}
+
+type work struct {
+	d, p int
 }
