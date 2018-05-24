561cc6426c3881303a06965563e42961b2982fb6
aQua <aQua@aQuaYi.com>
2018-05-24 09:22:15 +0800

    830 修改格式

--- Algorithms/0830.positions-of-large-groups/positions-of-large-groups.go
+++ Algorithms/0830.positions-of-large-groups/positions-of-large-groups.go
@@ -3,6 +3,7 @@ package problem0830
 func largeGroupPositions(s string) [][]int {
 	res := make([][]int, 0, len(s)/3)
 	l, r := 0, 1
+
 	for ; r < len(s); r++ {
 		if s[l] != s[r] {
 			l = r
@@ -14,7 +15,6 @@ func largeGroupPositions(s string) [][]int {
 		} else if r-l+1 > 3 { // 最后一个 large group 继续变大
 			res[len(res)-1][1] = r
 		}
-
 	}
 
 	return res
