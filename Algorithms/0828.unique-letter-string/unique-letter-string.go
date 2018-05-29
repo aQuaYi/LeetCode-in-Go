3258214221cdfe1ab85ff1488237698664621746
aQua <aQua@aQuaYi.com>
2018-05-29 09:20:44 +0800

    828 wrong answer

--- Algorithms/0828.unique-letter-string/unique-letter-string.go
+++ Algorithms/0828.unique-letter-string/unique-letter-string.go
@@ -1,6 +1,33 @@
 package problem0828
 
-func uniqueLetterString(S string) int {
-    
-	return 0
+func uniqueLetterString(s string) int {
+	size := len(s)
+	res := 0
+	for i := 0; i < size; i++ {
+		for j := i + 1; j <= size; j++ {
+			res += uniqueNum(s[i:j])
+		}
+	}
+	return res
+}
+
+func uniqueNum(s string) int {
+	isSaw := map[rune]bool{}
+	res := 0
+	for _, b := range s {
+		if isSaw[b] {
+			res--
+		} else {
+			isSaw[b] = true
+			res++
+		}
+	}
+	return res
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
