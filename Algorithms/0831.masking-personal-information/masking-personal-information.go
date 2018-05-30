bab8052099c740f6fcd30399084e332679badb69
aQua <aQua@aQuaYi.com>
2018-05-30 09:23:36 +0800

    831 加粗了函数的粒度

--- Algorithms/0831.masking-personal-information/masking-personal-information.go
+++ Algorithms/0831.masking-personal-information/masking-personal-information.go
@@ -6,7 +6,7 @@ func maskPII(s string) string {
 	if strings.ContainsRune(s, '@') {
 		return maskEmailAddress(strings.ToLower(s))
 	}
-	return maskPhoneNumber(parse(s))
+	return maskPhoneNumber(s)
 }
 
 func maskEmailAddress(s string) string {
@@ -15,21 +15,20 @@ func maskEmailAddress(s string) string {
 	return strings.Join(ss, "@")
 }
 
-func maskPhoneNumber(size int, s string) string {
-	if size == 10 {
-		return "***-***-" + s
-	}
-	return "+" + strings.Repeat("*", size-10) + "-***-***-" + s
-}
-
-// 返回 s 中数字的个数，和，s 中最后 4 个数字
-func parse(s string) (int, string) {
+func maskPhoneNumber(s string) string {
 	tmp := make([]byte, 0, 13)
 	for i := range s {
 		if '0' <= s[i] && s[i] <= '9' {
 			tmp = append(tmp, s[i])
 		}
 	}
+
 	size := len(tmp)
-	return size, string(tmp[size-4:])
+	s = string(tmp[size-4:])
+
+	if size == 10 {
+		return "***-***-" + s
+	}
+
+	return "+" + strings.Repeat("*", size-10) + "-***-***-" + s
 }
