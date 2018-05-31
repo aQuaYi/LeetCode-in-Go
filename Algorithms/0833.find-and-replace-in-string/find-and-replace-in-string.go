6b91071a6dd6c9aafb866520759b503f797bb4a0
aQua <aQua@aQuaYi.com>
2018-05-31 10:59:54 +0800

    833 finish

--- Algorithms/0833.find-and-replace-in-string/find-and-replace-in-string.go
+++ Algorithms/0833.find-and-replace-in-string/find-and-replace-in-string.go
@@ -10,46 +10,49 @@ type entry struct {
 	source, target string
 }
 
-func findReplaceString(S string, indexes []int, sources []string, targets []string) string {
-	entries := make([]*entry, len(indexes))
-	for i := range entries {
-		entries[i] = &entry{
+func findReplaceString(Str string, indexes []int, sources []string, targets []string) string {
+	es := make([]*entry, len(indexes))
+	for i := range es {
+		es[i] = &entry{
 			idx:    indexes[i],
 			source: sources[i],
 			target: targets[i],
 		}
 	}
 
-	sort.Slice(entries, func(i int, j int) bool {
-		return entries[i].idx < entries[j].idx
+	// 按照 idx 升排序
+	sort.Slice(es, func(i int, j int) bool {
+		return es[i].idx < es[j].idx
 	})
 
-	size := len(S)
-
-	ss := make([]string, 0, len(indexes)*2+2)
+	size := len(Str)
+	ss := make([]string, 0, len(indexes)*2+1)
 
 	end := 0
-	for i := range entries {
+	// 按照 es 分割 Str
+	for _, e := range es {
 		begin := end
-		ss = append(ss, S[begin:entries[i].idx])
-		end = min(size, entries[i].idx+len(entries[i].source))
-		ss = append(ss, S[entries[i].idx:end])
+		ss = append(ss, Str[begin:e.idx])
+		end = min(size, e.idx+len(e.source))
+		ss = append(ss, Str[e.idx:end])
 	}
-
+	// 尾部有剩余，也要加入 ss
 	if end < size {
-		ss = append(ss, S[end:])
+		ss = append(ss, Str[end:])
 	}
 
 	var buf bytes.Buffer
 
 	for i, s := range ss {
+		// ss 索引号为偶数的元素，不需要替换
 		if i%2 == 0 {
 			buf.WriteString(s)
 			continue
 		}
 
-		if s == entries[i/2].source {
-			buf.WriteString(entries[i/2].target)
+		// ss 索引号为奇数的元素，需要检查是否要替换
+		if s == es[i/2].source {
+			buf.WriteString(es[i/2].target)
 		} else {
 			buf.WriteString(s)
 		}
