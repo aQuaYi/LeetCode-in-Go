2ee707a986002fae214345fc823add5f2654cbf3
aQua <aQua@aQuaYi.com>
2018-05-31 09:01:32 +0800

    helper 修改题目 README 的生成方法

---
+++ Algorithms/0833.find-and-replace-in-string/find-and-replace-in-string.go
@@ -0,0 +1,6 @@
+package problem0833
+
+func findReplaceString(S string, indexes []int, sources []string, targets []string) string {
+    
+	return ""
+}
---
+++ Algorithms/0833.find-and-replace-in-string/find-and-replace-in-string_test.go
@@ -0,0 +1,39 @@
+package problem0833
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	S string
+ indexes []int
+ sources []string
+ targets []string
+	ans string
+}{
+
+
+	
+	// 可以有多个 testcase
+}
+
+func Test_findReplaceString(t *testing.T) {
+	ast := assert.New(t)
+	
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, findReplaceString(tc.S, tc.indexes, tc.sources, tc.targets), "输入:%v", tc)
+	}
+}
+
+func Benchmark_findReplaceString(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			findReplaceString(tc.S, tc.indexes, tc.sources, tc.targets)
+		}
+	}
+}
