d509f3c4e64babf8401eb3513a1d1c61a561a13b
aQua <aQua@aQuaYi.com>
2018-05-31 09:06:53 +0800

    833 added

--- Algorithms/0833.find-and-replace-in-string/find-and-replace-in-string_test.go
+++ Algorithms/0833.find-and-replace-in-string/find-and-replace-in-string_test.go
@@ -9,21 +9,35 @@ import (
 
 // tcs is testcase slice
 var tcs = []struct {
-	S string
- indexes []int
- sources []string
- targets []string
-	ans string
+	S       string
+	indexes []int
+	sources []string
+	targets []string
+	ans     string
 }{
 
+	{
+		"abcd",
+		[]int{0, 2},
+		[]string{"a", "cd"},
+		[]string{"eee", "ffff"},
+		"eeebffff",
+	},
 
-	
+	{
+		"abcd",
+		[]int{0, 2},
+		[]string{"ab", "ec"},
+		[]string{"eee", "ffff"},
+		"eeecd",
+	},
+
 	// 可以有多个 testcase
 }
 
 func Test_findReplaceString(t *testing.T) {
 	ast := assert.New(t)
-	
+
 	for _, tc := range tcs {
 		fmt.Printf("~~%v~~\n", tc)
 		ast.Equal(tc.ans, findReplaceString(tc.S, tc.indexes, tc.sources, tc.targets), "输入:%v", tc)
