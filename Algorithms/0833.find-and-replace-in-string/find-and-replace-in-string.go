92043e8d151cca05c6243ef520daf76e6e119c5e
aQua <aQua@aQuaYi.com>
2018-05-31 09:33:17 +0800

    833 import 出错

--- Algorithms/0833.find-and-replace-in-string/find-and-replace-in-string.go
+++ Algorithms/0833.find-and-replace-in-string/find-and-replace-in-string.go
@@ -1,6 +1,13 @@
 package problem0833
 
 func findReplaceString(S string, indexes []int, sources []string, targets []string) string {
-    
-	return ""
+	size := len(S)
+	bytes := []byte(S)
+
+	buf := bytes.NewBuffer([]byte{})
+	for i := 0; i < size; i++ {
+
+	}
+
+	return buf.String()
 }
