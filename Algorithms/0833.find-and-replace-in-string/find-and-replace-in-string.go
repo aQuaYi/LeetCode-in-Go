0907b9aed6a18131e7b3bfeeac0bc66bad170c12
aQua <aQua@aQuaYi.com>
2018-05-31 10:47:05 +0800

    833 accepted

--- Algorithms/0833.find-and-replace-in-string/find-and-replace-in-string.go
+++ Algorithms/0833.find-and-replace-in-string/find-and-replace-in-string.go
@@ -2,19 +2,38 @@ package problem0833
 
 import (
 	"bytes"
+	"sort"
 )
 
+type entry struct {
+	idx            int
+	source, target string
+}
+
 func findReplaceString(S string, indexes []int, sources []string, targets []string) string {
+	entries := make([]*entry, len(indexes))
+	for i := range entries {
+		entries[i] = &entry{
+			idx:    indexes[i],
+			source: sources[i],
+			target: targets[i],
+		}
+	}
+
+	sort.Slice(entries, func(i int, j int) bool {
+		return entries[i].idx < entries[j].idx
+	})
+
 	size := len(S)
 
 	ss := make([]string, 0, len(indexes)*2+2)
 
 	end := 0
-	for i, idx := range indexes {
+	for i := range entries {
 		begin := end
-		ss = append(ss, S[begin:idx])
-		end = min(size, idx+len(sources[i]))
-		ss = append(ss, S[idx:end])
+		ss = append(ss, S[begin:entries[i].idx])
+		end = min(size, entries[i].idx+len(entries[i].source))
+		ss = append(ss, S[entries[i].idx:end])
 	}
 
 	if end < size {
@@ -29,8 +48,8 @@ func findReplaceString(S string, indexes []int, sources []string, targets []stri
 			continue
 		}
 
-		if s == sources[i/2] {
-			buf.WriteString(targets[i/2])
+		if s == entries[i/2].source {
+			buf.WriteString(entries[i/2].target)
 		} else {
 			buf.WriteString(s)
 		}
