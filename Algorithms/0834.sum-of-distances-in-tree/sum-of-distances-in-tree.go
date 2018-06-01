5d9685be446ec6451b8b338a29f139aed3915c85
aQua <aQua@aQuaYi.com>
2018-06-01 15:02:27 +0800

    834  没能实现目标

--- Algorithms/0834.sum-of-distances-in-tree/sum-of-distances-in-tree.go
+++ Algorithms/0834.sum-of-distances-in-tree/sum-of-distances-in-tree.go
@@ -4,20 +4,20 @@ func sumOfDistancesInTree(N int, edges [][]int) []int {
 	// TODO: 修改程序，只用处理矩阵一半的数据
 
 	dist := make([][]int, N)
-	for i := range dist {
+	for i := 0; i < N; i++ {
 		dist[i] = make([]int, N)
-		for j := range dist[i] {
+		for j := i + 1; j < N; j++ {
 			dist[i][j] = -1
 		}
 	}
 
 	nextsOf := make([][]int, N)
 	for _, e := range edges {
+		// 假设 i < j 永远成立
 		i, j := e[0], e[1]
 		nextsOf[i] = append(nextsOf[i], j)
 		nextsOf[j] = append(nextsOf[j], i)
 		dist[i][j] = 1
-		dist[j][i] = 1
 	}
 
 	res := make([]int, N)
@@ -25,6 +25,10 @@ func sumOfDistancesInTree(N int, edges [][]int) []int {
 		res[i] = sumOf(i, N, nextsOf, dist)
 	}
 	return res
+}
+
+func fill(dist, nextsOf [][]int) {
+
 }
 
 func sumOf(src, N int, nextsOf, dist [][]int) int {
@@ -43,6 +47,10 @@ func sumOf(src, N int, nextsOf, dist [][]int) int {
 }
 
 func getDist(src, dst, N int, isChecked []bool, nextsOf, dist [][]int) int {
+	if src > dst {
+		src, dst = dst, src
+	}
+
 	if dist[src][dst] != -1 {
 		return dist[src][dst]
 	}
