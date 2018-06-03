531d9d3e67add5b9d57e8eaa063c897ffc54f6d2
aQua <aQua@aQuaYi.com>
2018-06-03 11:29:54 +0800

    840 wrong answer

--- Algorithms/0840.magic-squares-in-grid/magic-squares-in-grid.go
+++ Algorithms/0840.magic-squares-in-grid/magic-squares-in-grid.go
@@ -1,6 +1,48 @@
 package problem0840
 
-func numMagicSquaresInside(grid [][]int) int {
-    
-	return 0
+func numMagicSquaresInside(g [][]int) int {
+	m, n := len(g), len(g[0])
+
+	res := 0
+
+	for i := 0; i+2 < m && i+2 < n; i++ {
+		for j := 0; j+2 < m && j+2 < n; j++ {
+			if !isAvailbleNum(i, j, g) {
+				continue
+			}
+
+			if // 检查行
+			g[i][j]+g[i][j+1]+g[i][j+2] == 15 &&
+				g[i+1][j]+g[i+1][j+1]+g[i+1][j+2] == 15 &&
+				g[i+2][j]+g[i+2][j+1]+g[i+2][j+2] == 15 &&
+				// 检查列
+				g[j][i]+g[j][i+1]+g[j][i+2] == 15 &&
+				g[j+1][i]+g[j+1][i+1]+g[j+1][i+2] == 15 &&
+				g[j+2][i]+g[j+2][i+1]+g[j+2][i+2] == 15 &&
+				// 检查对角
+				g[i][j]+g[i+1][j+1]+g[i+2][j+2] == 15 &&
+				g[i][j+2]+g[i+1][j+1]+g[i+2][j] == 15 {
+				res++
+			}
+		}
+	}
+
+	return res
+}
+
+func isAvailbleNum(x, y int, g [][]int) bool {
+	tmp := [16]int{}
+
+	for i := x; i <= x+2; i++ {
+		for j := y; j <= y+2; j++ {
+			tmp[g[i][j]]++
+		}
+	}
+
+	for i := 1; i <= 9; i++ {
+		if tmp[i] != 1 {
+			return false
+		}
+	}
+	return true
 }
