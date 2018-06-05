91a4536a23b2a9f370604361cb263660542548ad
aQua <aQua@aQuaYi.com>
2018-06-05 09:40:53 +0800

    838 accepted

--- Algorithms/0838.push-dominoes/push-dominoes.go
+++ Algorithms/0838.push-dominoes/push-dominoes.go
@@ -1,6 +1,38 @@
 package problem0838
 
 func pushDominoes(dominoes string) string {
-    
-	return ""
+	return oneSecond([]byte(dominoes))
+}
+
+func oneSecond(d []byte) string {
+	old := string(d)
+	size := len(d)
+
+	for i := 0; i+1 < size; i++ {
+		if d[i] != '.' {
+			continue
+		}
+		if d[i+1] == 'L' &&
+			(i == 0 || old[i-1] != 'R') {
+			d[i] = 'L'
+		}
+	}
+
+	for i := size - 1; 0 < i; i-- {
+		if d[i] != '.' {
+			continue
+		}
+		if d[i-1] == 'R' &&
+			(i+1 == size || old[i+1] != 'L') {
+			d[i] = 'R'
+		}
+	}
+
+	newState := string(d)
+
+	if newState == old {
+		return newState
+	}
+
+	return oneSecond(d)
 }
