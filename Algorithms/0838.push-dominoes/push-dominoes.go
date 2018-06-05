e1108fd29ef03fa36fb0c42f861a0f6027a6f907
aQua <aQua@aQuaYi.com>
2018-06-05 10:06:58 +0800

    838 finish

--- Algorithms/0838.push-dominoes/push-dominoes.go
+++ Algorithms/0838.push-dominoes/push-dominoes.go
@@ -1,19 +1,9 @@
 package problem0838
 
-import (
-	"bytes"
-)
-
-/**
- * 'R......R' => 'RRRRRRRR'
- * 'R......L' => 'RRRRLLLL' or 'RRRR.LLLL'
- * 'L......R' => 'L......R'
- * 'L......L' => 'LLLLLLLL'
- */
 func pushDominoes(dominoes string) string {
-	var res bytes.Buffer
 	d := "L" + dominoes + "R"
 	size := len(d)
+	res := make([]byte, 0, size)
 
 	for i, j := 0, 1; j < size; j++ {
 		if d[j] == '.' {
@@ -21,34 +11,38 @@ func pushDominoes(dominoes string) string {
 		}
 
 		if 0 < i {
-			res.WriteByte(d[i])
+			res = append(res, d[i])
 		}
 
 		mid := j - i - 1
 
 		switch {
-		case d[i] == d[j]:
-			for k := 0; k < mid; k++ {
-				res.WriteByte(d[i])
-			}
 		case d[i] == 'R' && d[j] == 'L':
+			// 'R......L' => 'RRRRLLLL' or 'RRRR.LLLL'
 			for k := 0; k < mid/2; k++ {
-				res.WriteByte('R')
+				res = append(res, 'R')
 			}
 			if mid%2 == 1 {
-				res.WriteByte('.')
+				res = append(res, '.')
 			}
 			for k := 0; k < mid/2; k++ {
-				res.WriteByte('L')
+				res = append(res, 'L')
+			}
+		case d[i] == 'L' && d[j] == 'R':
+			// 'L......R' => 'L......R'
+			for k := 0; k < mid; k++ {
+				res = append(res, '.')
 			}
 		default:
+			//  'R......R' => 'RRRRRRRR'
+			//  'L......L' => 'LLLLLLLL'
 			for k := 0; k < mid; k++ {
-				res.WriteByte('.')
+				res = append(res, d[i])
 			}
 		}
 
 		i = j
 	}
 
-	return res.String()
+	return string(res)
 }
