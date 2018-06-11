9795e9db0dc035b0746241786283ba9a0b3f9772
aQua <aQua@aQuaYi.com>
2018-06-11 16:04:42 +0800

    837 finish

--- Algorithms/0837.new-21-game/new-21-game.go
+++ Algorithms/0837.new-21-game/new-21-game.go
@@ -1,24 +1,45 @@
 package problem0837
 
-import (
-	"math"
-)
-
 func new21Game(N int, K int, W int) float64 {
-	var res float64
-	search(0, 0, N, K, W, &res)
-	return res
-}
+	if K == 0 ||
+		N >= K+W {
+		return 1.0
+	}
 
-func search(start, exp, n, k, w int, res *float64) {
-	if start >= k {
-		if start <= n {
-			*res += math.Pow(float64(w), float64(exp))
+	// dp[i]: 获取分数 i 的概率
+	// 假设 p 为抽取 [1,W] 中任意一个数的概率，即 p = 1/W
+	//
+	// dp[i] = dp[i-1]*p + dp[i-2]*p + ... + dp[i-W]*p
+	// 		 = (dp[i-1] + dp[i-2] + ... + dp[i-W]) * p
+	// 		 = (dp[i-1] + dp[i-2] + ... + dp[i-W]) / W
+	// 令 last= dp[i-1] + dp[i-2] + ... + dp[i-W]
+	// dp[i] = last/ W
+	//
+	// 由于 i 的取值范围为 [1,N]
+	// 当 W	<= i < K 时
+	// 		last= dp[i-1] + dp[i-2] + ... + dp[i-W]
+	// 当 i < W 时
+	// 		last应为 dp[i-1] + dp[i-2] + ... + dp[0]
+	// 当 K <= i 时
+	// 		last应为 dp[k-1] + dp[k-2] + ... + dp[i-W]
+	dp := make([]float64, N+1)
+	dp[0] = 1.0
+	last := 1.0 // 初始概率为 1
+	res := 0.0
+	for i := 1; i <= N; i++ {
+		dp[i] = last / float64(W)
+
+		if W <= i {
+			last -= dp[i-W]
 		}
-		return
+
+		if i < K {
+			last += dp[i]
+		} else {
+			res += dp[i]
+		}
+
 	}
 
-	for i := 1; i <= w; i++ {
-		search(start+i, exp-1, n, k, w, res)
-	}
+	return res
 }
