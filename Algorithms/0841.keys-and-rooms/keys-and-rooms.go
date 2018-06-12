7b2303fab9e620a9c26ddcd7e079ac13fabe73b5
aQua <aQua@aQuaYi.com>
2018-06-12 21:56:30 +0800

    841 finish

--- Algorithms/0841.keys-and-rooms/keys-and-rooms.go
+++ Algorithms/0841.keys-and-rooms/keys-and-rooms.go
@@ -1,6 +1,33 @@
 package problem0841
 
 func canVisitAllRooms(rooms [][]int) bool {
-    
-	return false
+	N := len(rooms)
+
+	next := make([]int, 1, N)
+	next[0] = 0
+
+	isEntered := make([]bool, N)
+	isEntered[0] = true
+	count := 1
+
+	for len(next) > 0 {
+		r := next[0]
+		next = next[1:]
+
+		for _, x := range rooms[r] {
+			if isEntered[x] {
+				continue
+			}
+			next = append(next, x)
+			isEntered[x] = true
+			count++
+		}
+
+		if count == N {
+			return true
+		}
+
+	}
+
+	return count == N
 }
