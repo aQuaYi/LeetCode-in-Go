bb4550d9f972e2855b9adac1ad12fbd01dbcb5ca
aQua <aQua@aQuaYi.com>
2018-06-27 21:13:41 +0800

    完善了 master api 和 843

--- Algorithms/0843.guess-the-word/guess-the-word.go
+++ Algorithms/0843.guess-the-word/guess-the-word.go
@@ -1,12 +1,13 @@
 package problem0843
 
-/**
- * // This is the Master's API interface.
- * // You should not implement it, or speculate about its implementation
- * type Master struct {}
- *
- * func (this *Master) Guess(word string) int {}
- */
+import "github.com/aQuaYi/LeetCode-in-Go/kit"
+
+// Master is the Master's API interface.
+// You should not implement it, or speculate about its implementation
+// type Master struct {}
+// func (this *Master) Guess(word string) int {}
+type Master = kit.Master
+
 func findSecretWord(wordlist []string, master *Master) {
 
 }
