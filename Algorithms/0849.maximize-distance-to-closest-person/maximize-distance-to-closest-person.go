6b069a42098b3ce07d5ba6c8d2ac9dd1da7c2950
aQua <aQua@aQuaYi.com>
2018-06-28 17:19:52 +0800

    849 finish

--- Algorithms/0849.maximize-distance-to-closest-person/maximize-distance-to-closest-person.go
+++ Algorithms/0849.maximize-distance-to-closest-person/maximize-distance-to-closest-person.go
@@ -2,39 +2,28 @@ package problem0849
 
 func maxDistToClosest(seats []int) int {
 	size := len(seats)
-
-	left := make([]int, size)
-	right := make([]int, size)
-
-	leftPerson := -size
-	rightPerson := size * 2
-
+	maxDis := 0
+	// e 代表了连续空位的个数
+	// 当连续空位两边都有人的时候，maxDis = (e+e%2)/2
+	// 如果有一边没人的话，      maxDis = e
+	e := 0
 	for i := 0; i < size; i++ {
+		if e == i {
+			// 说明 seats[0:i] 全是 0
+			maxDis = e
+		} else {
+			maxDis = max(maxDis, (e+e%2)/2)
+		}
 		if seats[i] == 1 {
-			leftPerson = i
+			e = 0
+		} else {
+			e++
 		}
-		left[i] = i - leftPerson
-
-		j := size - i - 1
-		if seats[j] == 1 {
-			rightPerson = j
-		}
-		right[j] = rightPerson - j
 	}
 
-	res := 0
-	for i := 0; i < size; i++ {
-		res = max(res, min(left[i], right[i]))
-	}
-
-	return res
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
+	// 当 seats[size-1]==0 的时候
+	// e 最后的值，有可能 > maxDis
+	return max(maxDis, e)
 }
 
 func max(a, b int) int {
