4e5ea2e47dbb2abdb74edefc1dc1ff4b374b6670
aQua <aQua@aQuaYi.com>
2018-06-29 16:38:58 +0800

    846 wrong answer

--- Algorithms/0846.hand-of-straights/hand-of-straights.go
+++ Algorithms/0846.hand-of-straights/hand-of-straights.go
@@ -1,6 +1,47 @@
 package problem0846
 
+import (
+	"sort"
+)
+
 func isNStraightHand(hand []int, W int) bool {
-    
-	return false
+	size := len(hand)
+	group := size / W
+	if group*W != size {
+		// 没有办法保证每组的个数是 W
+		return false
+	}
+
+	groups := make([][]int, group)
+	for i := range groups {
+		groups[i] = make([]int, 1, W)
+		groups[i][0] = -1
+	}
+
+	sort.Ints(hand)
+
+	for _, c := range hand {
+		g := 0
+		for ; g < group; g++ {
+			if len(groups[g]) == W {
+				continue
+			}
+			if groups[g][0] == -1 {
+				groups[g][0] = c
+				break
+			}
+			last := len(groups[g]) - 1
+			if groups[g][last]+1 == c {
+				groups[g] = append(groups[g], c)
+				break
+			}
+		}
+
+		if g == group {
+			return false
+		}
+
+	}
+
+	return true
 }
