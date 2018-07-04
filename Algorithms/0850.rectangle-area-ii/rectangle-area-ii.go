ca7c3ce4a5787469aa66cc99a1b7471a926bb0bf
aQua <aQua@aQuaYi.com>
2018-07-04 15:55:33 +0800

    850 finish

--- Algorithms/0850.rectangle-area-ii/rectangle-area-ii.go
+++ Algorithms/0850.rectangle-area-ii/rectangle-area-ii.go
@@ -5,6 +5,14 @@ import (
 )
 
 // 思路来自 https://leetcode.com/problems/rectangle-area-ii/discuss/137914/C++Python-Discretization-and-O(NlogN)
+//
+// 可以想象成，
+// 根据 rectangles 中所有的 x 值，在平面上划 竖线
+// 根据 rectangles 中所有的 y 值，在平面上划 横线
+// 原先的长方形被划线分隔成更小的长方形
+// 这些小长方形，要么单独存在，要么与别的小长方形**完全重合**
+//
+// 然后，从下往上，依次统计每一行，所有小长方形的面积，重合的小长方形只统计一次
 
 const mod = 1e9 + 7
 
@@ -12,14 +20,22 @@ func rectangleArea(rectangles [][]int) int {
 	// 提取 rectangles 中所有的 x 坐标值，并按照升序排列
 	xs := getXs(rectangles)
 
+	// idxs 记录了 x 在 xs 中的索引值，即
+	// idxs[key]==val <==> xs[val]==key
 	idxs := make(map[int]int, 2*len(xs))
 	for idx, x := range xs {
 		idxs[x] = idx
 	}
 
-	count := make([]int, len(xs))
-
+	// labels[i]=[y,x1,x2,sig]
+	// 其中 [y,[x1,x2]] 表示小长方形的一条横边
+	// sig==1  为底边
+	// sig==-1 为顶边
 	labels := getLabels(rectangles)
+
+	// 当 curY=j ，count[i]=5 时，意味着
+	// 以 y=j,[xs[i],xs[i+1]] 为底边的小长方形，有 5 个
+	count := make([]int, len(xs))
 
 	curY, curXSum, area := 0, 0, 0
 
@@ -27,12 +43,16 @@ func rectangleArea(rectangles [][]int) int {
 		y, x1, x2, sig := l[0], l[1], l[2], l[3]
 		area += (y - curY) * curXSum
 		curY = y
+		// 更新 curXSum
+		curXSum = 0
 		for i := idxs[x1]; i < idxs[x2]; i++ {
+			// 更新每个小长方形的重合度
 			count[i] += sig
 		}
-		curXSum = 0
 		for i := 0; i+1 < len(count); i++ {
 			if count[i] > 0 {
+				// curY 上
+				// 所有小长方形**底边**长度相加
 				curXSum += xs[i+1] - xs[i]
 			}
 		}
@@ -43,17 +63,17 @@ func rectangleArea(rectangles [][]int) int {
 
 func getXs(rects [][]int) []int {
 	size := len(rects)
-	res := make([]int, 1, size*2)
+	xs := make([]int, 0, size*2)
 	xMap := make(map[int]bool, size*2)
 	for _, r := range rects {
 		xMap[r[0]] = true
 		xMap[r[2]] = true
 	}
 	for k := range xMap {
-		res = append(res, k)
+		xs = append(xs, k)
 	}
-	sort.Ints(res)
-	return res
+	sort.Ints(xs)
+	return xs
 }
 
 func getLabels(rects [][]int) [][]int {
@@ -68,18 +88,6 @@ func getLabels(rects [][]int) [][]int {
 
 	// 对 labels 进行排序
 	sort.Slice(labels, func(i int, j int) bool {
-		eq0 := labels[i][0] == labels[j][0]
-		eq1 := labels[i][1] == labels[j][1]
-		eq2 := labels[i][2] == labels[j][2]
-		if eq0 && eq1 && eq2 {
-			return labels[i][3] < labels[j][3]
-		}
-		if eq0 && eq1 {
-			return labels[i][2] < labels[j][2]
-		}
-		if eq0 {
-			return labels[i][1] < labels[j][1]
-		}
 		return labels[i][0] < labels[j][0]
 	})
 
