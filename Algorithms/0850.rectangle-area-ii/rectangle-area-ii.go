475c6cc414489d9a9dab2a148d6110a4bd5ef229
aQua <aQua@aQuaYi.com>
2018-07-04 15:13:56 +0800

    850 accepted

--- Algorithms/0850.rectangle-area-ii/rectangle-area-ii.go
+++ Algorithms/0850.rectangle-area-ii/rectangle-area-ii.go
@@ -4,61 +4,38 @@ import (
 	"sort"
 )
 
+// 思路来自 https://leetcode.com/problems/rectangle-area-ii/discuss/137914/C++Python-Discretization-and-O(NlogN)
+
 const mod = 1e9 + 7
 
 func rectangleArea(rectangles [][]int) int {
+	// 提取 rectangles 中所有的 x 坐标值，并按照升序排列
 	xs := getXs(rectangles)
-	sort.Ints(xs)
 
-	idxX := make(map[int]int, 2*len(xs))
-	for k, x := range xs {
-		idxX[x] = k
+	idxs := make(map[int]int, 2*len(xs))
+	for idx, x := range xs {
+		idxs[x] = idx
 	}
 
 	count := make([]int, len(xs))
 
-	L := make([][]int, 0, 2*len(rectangles))
-
-	for _, r := range rectangles {
-		x1, y1, x2, y2 := r[0], r[1], r[2], r[3]
-		L = append(L,
-			[]int{y1, x1, x2, 1},
-			[]int{y2, x1, x2, -1},
-		)
-	}
-
-	sort.Slice(L, func(i int, j int) bool {
-		eq0 := L[i][0] == L[j][0]
-		eq1 := L[i][1] == L[j][1]
-		eq2 := L[i][2] == L[j][2]
-		if eq0 && eq1 && eq2 {
-			return L[i][3] < L[j][3]
-		}
-		if eq0 && eq1 {
-			return L[i][2] < L[j][2]
-		}
-		if eq0 {
-			return L[i][1] < L[j][1]
-		}
-		return L[i][0] < L[j][0]
-	})
+	labels := getLabels(rectangles)
 
 	curY, curXSum, area := 0, 0, 0
 
-	for _, l := range L {
+	for _, l := range labels {
 		y, x1, x2, sig := l[0], l[1], l[2], l[3]
 		area += (y - curY) * curXSum
 		curY = y
-		for i := idxX[x1]; i < idxX[x2]; i++ {
+		for i := idxs[x1]; i < idxs[x2]; i++ {
 			count[i] += sig
 		}
 		curXSum = 0
 		for i := 0; i+1 < len(count); i++ {
-			if count[i] != 0 {
+			if count[i] > 0 {
 				curXSum += xs[i+1] - xs[i]
 			}
 		}
-
 	}
 
 	return area % mod
@@ -68,15 +45,43 @@ func getXs(rects [][]int) []int {
 	size := len(rects)
 	res := make([]int, 1, size*2)
 	xMap := make(map[int]bool, size*2)
-
 	for _, r := range rects {
 		xMap[r[0]] = true
 		xMap[r[2]] = true
 	}
-
 	for k := range xMap {
 		res = append(res, k)
 	}
-
+	sort.Ints(res)
 	return res
+}
+
+func getLabels(rects [][]int) [][]int {
+	labels := make([][]int, 0, 2*len(rects))
+	for _, r := range rects {
+		x1, y1, x2, y2 := r[0], r[1], r[2], r[3]
+		labels = append(labels,
+			[]int{y1, x1, x2, 1},
+			[]int{y2, x1, x2, -1},
+		)
+	}
+
+	// 对 labels 进行排序
+	sort.Slice(labels, func(i int, j int) bool {
+		eq0 := labels[i][0] == labels[j][0]
+		eq1 := labels[i][1] == labels[j][1]
+		eq2 := labels[i][2] == labels[j][2]
+		if eq0 && eq1 && eq2 {
+			return labels[i][3] < labels[j][3]
+		}
+		if eq0 && eq1 {
+			return labels[i][2] < labels[j][2]
+		}
+		if eq0 {
+			return labels[i][1] < labels[j][1]
+		}
+		return labels[i][0] < labels[j][0]
+	})
+
+	return labels
 }
