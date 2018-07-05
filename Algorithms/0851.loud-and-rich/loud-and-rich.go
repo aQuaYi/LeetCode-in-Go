da5986920e8bc1ca82c56eabb48e34da19147adf
aQua <aQua@aQuaYi.com>
2018-07-05 22:32:39 +0800

    851 rewrite

--- Algorithms/0851.loud-and-rich/loud-and-rich.go
+++ Algorithms/0851.loud-and-rich/loud-and-rich.go
@@ -3,16 +3,16 @@ package problem0851
 func loudAndRich(richer [][]int, quiet []int) []int {
 	size := len(quiet)
 
+	// rs[y] 中保存了所有比 y 有钱的人
 	rs := make([][]int, size)
-	for i := range rs {
-		rs[i] = make([]int, 0, size)
-	}
 	for _, r := range richer {
 		x, y := r[0], r[1]
 		rs[y] = append(rs[y], x)
 	}
 
 	res := make([]int, size)
+	// res 全部设置为 -1
+	// 作为是否设置过的标记
 	for i := range res {
 		res[i] = -1
 	}
