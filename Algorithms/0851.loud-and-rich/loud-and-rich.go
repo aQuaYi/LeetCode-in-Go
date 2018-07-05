3ae9a5e2d80dd788bd81abea069c9003d70f65b1
aQua <aQua@aQuaYi.com>
2018-07-05 17:26:44 +0800

    851 finish

--- Algorithms/0851.loud-and-rich/loud-and-rich.go
+++ Algorithms/0851.loud-and-rich/loud-and-rich.go
@@ -1,6 +1,39 @@
 package problem0851
 
 func loudAndRich(richer [][]int, quiet []int) []int {
-    
-	return nil
+	size := len(quiet)
+
+	rs := make([][]int, size)
+	for i := range rs {
+		rs[i] = make([]int, 0, size)
+	}
+	for _, r := range richer {
+		x, y := r[0], r[1]
+		rs[y] = append(rs[y], x)
+	}
+
+	res := make([]int, size)
+	for i := range res {
+		res[i] = -1
+	}
+
+	var dfs func(int) int
+	dfs = func(i int) int {
+		if res[i] >= 0 {
+			return res[i]
+		}
+		res[i] = i
+		for _, j := range rs[i] {
+			if quiet[res[i]] > quiet[dfs(j)] {
+				res[i] = res[j]
+			}
+		}
+		return res[i]
+	}
+
+	for i := 0; i < size; i++ {
+		dfs(i)
+	}
+
+	return res
 }
