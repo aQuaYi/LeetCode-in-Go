b6443200b53540b980edcee4fa941fb69bb82cd9
aQua <aQua@aQuaYi.com>
2018-07-06 17:07:17 +0800

    852 finish

--- Algorithms/0852.peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.go
+++ Algorithms/0852.peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.go
@@ -1,12 +1,16 @@
 package problem0852
 
 func peakIndexInMountainArray(a []int) int {
-	size := len(a)
-	i := 1
-	for ; i+1 < size; i++ {
-		if a[i] > a[i+1] {
-			break
+	l, r := 0, len(a)-1
+	for {
+		m := (l + r) / 2
+		switch {
+		case a[m] < a[m+1]:
+			l = m // 想想看，为什么不是 l = m+1
+		case a[m-1] > a[m]:
+			r = m // 想想看，为什么不是 r = m-1
+		default:
+			return m
 		}
 	}
-	return i
 }
