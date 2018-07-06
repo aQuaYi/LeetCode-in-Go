1d982b638496f5420830d4b7540984df651a787a
aQua <aQua@aQuaYi.com>
2018-07-06 16:23:36 +0800

    852 added

---
+++ Algorithms/0852.peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.go
@@ -0,0 +1,6 @@
+package problem0852
+
+func peakIndexInMountainArray(A []int) int {
+    
+	return 0
+}
---
+++ Algorithms/0852.peak-index-in-a-mountain-array/peak-index-in-a-mountain-array_test.go
@@ -0,0 +1,44 @@
+package problem0852
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	A   []int
+	ans int
+}{
+
+	{
+		[]int{0, 1, 0},
+		1,
+	},
+
+	{
+		[]int{0, 2, 1, 0},
+		1,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_peakIndexInMountainArray(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, peakIndexInMountainArray(tc.A), "输入:%v", tc)
+	}
+}
+
+func Benchmark_peakIndexInMountainArray(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			peakIndexInMountainArray(tc.A)
+		}
+	}
+}
