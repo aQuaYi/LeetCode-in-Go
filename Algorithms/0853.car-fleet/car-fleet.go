bee65d63165d452a3dccc04bee70f9fe6a3466e4
aQua <aQua@aQuaYi.com>
2018-07-06 13:31:18 +0800

    853 accepted

--- Algorithms/0853.car-fleet/car-fleet.go
+++ Algorithms/0853.car-fleet/car-fleet.go
@@ -1,6 +1,39 @@
 package problem0853
 
+import (
+	"sort"
+)
+
 func carFleet(target int, position []int, speed []int) int {
-    
-	return 0
+	size := len(position)
+	rs := make([]record, size)
+	for i := range rs {
+		pos, spd := position[i], speed[i]
+		rs[i] = record{
+			initPos:     pos,
+			arrivalTime: float64(target-pos) / float64(spd),
+		}
+	}
+
+	sort.Slice(rs, func(i int, j int) bool {
+		return rs[i].initPos > rs[j].initPos
+	})
+
+	fleetTime := -1.0
+	res := 0
+
+	for _, r := range rs {
+		at := r.arrivalTime
+		if fleetTime < at {
+			fleetTime = at
+			res++
+		}
+	}
+
+	return res
+}
+
+type record struct {
+	initPos     int
+	arrivalTime float64
 }
