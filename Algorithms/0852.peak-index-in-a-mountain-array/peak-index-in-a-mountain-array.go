87afcbc3ac653b6c801010dc188f72cff879f96d
aQua <aQua@aQuaYi.com>
2018-07-06 16:28:15 +0800

    852 accepted

--- Algorithms/0852.peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.go
+++ Algorithms/0852.peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.go
@@ -1,6 +1,12 @@
 package problem0852
 
-func peakIndexInMountainArray(A []int) int {
-    
-	return 0
+func peakIndexInMountainArray(a []int) int {
+	size := len(a)
+	i := 1
+	for ; i+1 < size; i++ {
+		if a[i] > a[i+1] {
+			break
+		}
+	}
+	return i
 }
