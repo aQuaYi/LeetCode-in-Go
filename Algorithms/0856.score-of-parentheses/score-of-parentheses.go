9ec54948c3ab452dd0b77e3e0d53bc32a96a644c
aQua <aQua@aQuaYi.com>
2018-07-09 20:03:08 +0800

    856 accepted

--- Algorithms/0856.score-of-parentheses/score-of-parentheses.go
+++ Algorithms/0856.score-of-parentheses/score-of-parentheses.go
@@ -1,6 +1,38 @@
 package problem0856
 
-func scoreOfParentheses(S string) int {
-    
-	return 0
+import "strings"
+
+func scoreOfParentheses(s string) int {
+	s = strings.Replace(s, "()", "1", -1)
+	return helper(s)
+}
+
+func helper(s string) int {
+	if s == "" {
+		return 0
+	}
+	if s[0] == '1' {
+		return 1 + helper(s[1:])
+	}
+	i := match(s)
+	return 2*helper(s[1:i]) + helper(s[i+1:])
+}
+
+// 寻找与 s[0] == '('
+func match(s string) int {
+	count := 1
+	size := len(s)
+	i := 1
+	for ; i < size; i++ {
+		switch s[i] {
+		case '(':
+			count++
+		case ')':
+			count--
+		}
+		if count == 0 {
+			break
+		}
+	}
+	return i
 }
