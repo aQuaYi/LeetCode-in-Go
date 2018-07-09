f51f5457bb1125adfb123775ab73ddcbd2ac7669
aQua <aQua@aQuaYi.com>
2018-07-09 20:11:57 +0800

    856 finish

--- Algorithms/0856.score-of-parentheses/score-of-parentheses.go
+++ Algorithms/0856.score-of-parentheses/score-of-parentheses.go
@@ -1,38 +1,18 @@
 package problem0856
 
-import "strings"
-
 func scoreOfParentheses(s string) int {
-	s = strings.Replace(s, "()", "1", -1)
-	return helper(s)
-}
-
-func helper(s string) int {
-	if s == "" {
-		return 0
-	}
-	if s[0] == '1' {
-		return 1 + helper(s[1:])
-	}
-	i := match(s)
-	return 2*helper(s[1:i]) + helper(s[i+1:])
-}
-
-// 寻找与 s[0] == '('
-func match(s string) int {
-	count := 1
+	res := 0
+	layers := uint(0)
 	size := len(s)
-	i := 1
-	for ; i < size; i++ {
-		switch s[i] {
-		case '(':
-			count++
-		case ')':
-			count--
+	for i := 0; i < size; i++ {
+		if s[i] == '(' {
+			layers++
+		} else {
+			layers--
 		}
-		if count == 0 {
-			break
+		if s[i] == '(' && s[i+1] == ')' {
+			res += 1 << (layers - 1)
 		}
 	}
-	return i
+	return res
 }
