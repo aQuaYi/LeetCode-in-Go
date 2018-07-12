8c767858d26078a5aa85f79c1cc261ae31aa957a
aQua <aQua@aQuaYi.com>
2018-07-12 17:20:02 +0800

    859 added

--- Algorithms/0859.buddy-strings/buddy-strings.go
+++ Algorithms/0859.buddy-strings/buddy-strings.go
@@ -1,6 +1,13 @@
 package problem0859
 
-func buddyStrings(A string, B string) bool {
-    
+func buddyStrings(A, B string) bool {
+	if A == B || len(A) != len(B) {
+		return false
+	}
+
+	size := len(A)
+
+	ca, cb := byte(0), byte(0)
+
 	return false
 }
