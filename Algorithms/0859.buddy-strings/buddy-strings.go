1cf5af0893a369f22526ba5e0c54e364b1d7411c
aQua <aQua@aQuaYi.com>
2018-07-12 17:26:33 +0800

    859 addeing

--- Algorithms/0859.buddy-strings/buddy-strings.go
+++ Algorithms/0859.buddy-strings/buddy-strings.go
@@ -1,13 +1,29 @@
 package problem0859
 
 func buddyStrings(A, B string) bool {
-	if A == B || len(A) != len(B) {
+	if len(A) != len(B) {
 		return false
+	}
+
+	if A == B {
+		return hasDouble(A)
 	}
 
 	size := len(A)
 
 	ca, cb := byte(0), byte(0)
 
+	return false
+}
+
+func hasDouble(s string) bool {
+	seen := [26]bool{}
+	for i := range s {
+		b := s[i] - 'a'
+		if seen[b] {
+			return true
+		}
+		seen[b] = true
+	}
 	return false
 }
