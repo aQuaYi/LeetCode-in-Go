b334070bfd45a93f8540d1bcafe3a3feae172628
aQua <aQua@aQuaYi.com>
2018-07-13 16:50:54 +0800

    433 added

---
+++ Algorithms/0433.minimum-genetic-mutation/minimum-genetic-mutation_test.go
@@ -0,0 +1,57 @@
+package problem0433
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	start string
+	end   string
+	bank  []string
+	ans   int
+}{
+
+	{
+		"AACCGGTT",
+		"AACCGGTA",
+		[]string{"AACCGGTA"},
+		1,
+	},
+
+	{
+		"AACCGGTT",
+		"AAACGGTA",
+		[]string{"AACCGGTA", "AACCGCTA", "AAACGGTA"},
+		2,
+	},
+
+	{
+		"AAAAACCC",
+		"AACCCCCC",
+		[]string{"AAAACCCC", "AAACCCCC", "AACCCCCC"},
+		3,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_countSegments(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, minMutation(tc.start, tc.end, tc.bank), "输入:%v", tc)
+	}
+}
+
+func Benchmark_countSegments(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			minMutation(tc.start, tc.end, tc.bank)
+		}
+	}
+}
