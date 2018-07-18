a1c96ddd8d8406276c24062e86b8946c56b23b61
aQua <aQua@aQuaYi.com>
2018-07-18 11:04:26 +0800

    457  优化了结构

--- Algorithms/0457.circular-array-loop/circular-array-loop.go
+++ Algorithms/0457.circular-array-loop/circular-array-loop.go
@@ -1,5 +1,9 @@
 package problem0457
 
+import (
+	"unsafe"
+)
+
 func circularArrayLoop(nums []int) bool {
 	size := len(nums)
 
@@ -8,18 +12,18 @@ func circularArrayLoop(nums []int) bool {
 		nums[i] %= size
 	}
 
+	// 获取 int 型变量的位数 - 1
+	bits := uint(unsafe.Sizeof(size) - 1)
+
 	for i, n := range nums {
 		// 用于标记 nums[i]
 		// 每个外层 for 循环的 mark 必须不一样，才能检查此次路径是否闭合
 		// mark 还需要与 n 同符号
-		mark := i + size
-		if n < 0 {
-			mark = -i - size
-		}
+		mark := (i + size) * (n>>bits | 1)
 
 		// 每次内层 for 循环只需要检查未检查过的节点即可。
 		// 他们的特点是 -size < n && n != 0 && n < size
-		for -size < n && n != 0 && n < size {
+		for -size < n && n < size && n != 0 {
 			// nums[i] 通过了 for 的检查条件
 			// 现在标记 i 节点
 			nums[i] = mark
