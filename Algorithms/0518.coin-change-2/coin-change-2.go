e2dc0d0b0b5154d9e73c4d534f39960fb89c371c
aQua <aQua@aQuaYi.com>
2018-07-21 16:33:08 +0800

    518 accepted

--- Algorithms/0518.coin-change-2/coin-change-2.go
+++ Algorithms/0518.coin-change-2/coin-change-2.go
@@ -1,23 +1,24 @@
 package problem0518
 
 func change(amount int, coins []int) int {
-	if amount == 0 {
-		return 1
+	size := len(coins)
+	// dp[i][j] 表示，使用 coins[:i] 中的零钱，组成金额为 j 的总种类数
+	dp := make([][]int, size+1)
+	for i := range dp {
+		dp[i] = make([]int, amount+1)
+	}
+	dp[0][0] = 1
+
+	for i := 1; i <= size; i++ {
+		dp[i][0] = 1
+		for j := 1; j <= amount; j++ {
+			dp[i][j] = dp[i-1][j] // 不使用 coins[i-1] 时候的种类数
+			if j >= coins[i-1] {
+				// 此外，如果
+				dp[i][j] += dp[i][j-coins[i-1]]
+			}
+		}
 	}
 
-	if len(coins) == 0 {
-		return 0
-	}
-
-	res := 0
-	coin := coins[0]
-	coins = coins[1:]
-	t := 0
-
-	for t <= amount {
-		res += change(amount-t, coins)
-		t += coin
-	}
-
-	return res
+	return dp[size][amount]
 }
