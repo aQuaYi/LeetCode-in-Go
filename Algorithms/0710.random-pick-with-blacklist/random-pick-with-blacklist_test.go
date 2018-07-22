b8d07d1eac10b987c0313d859c076b85ffc9ec6d
aQua <aQua@aQuaYi.com>
2018-07-22 12:48:04 +0800

    710 Time Limit Exceeded

--- Algorithms/0710.random-pick-with-blacklist/random-pick-with-blacklist_test.go
+++ Algorithms/0710.random-pick-with-blacklist/random-pick-with-blacklist_test.go
@@ -9,4 +9,14 @@ import (
 func Test_Solution(t *testing.T) {
 	ast := assert.New(t)
 
+	N := 100000
+	blacklist := make([]int, 0, N)
+	for i := 1; i < N; i++ {
+		blacklist = append(blacklist, i)
+	}
+	s := Constructor(N, blacklist)
+
+	for i := 0; i < 100; i++ {
+		ast.NotContains(blacklist, s.Pick())
+	}
 }
