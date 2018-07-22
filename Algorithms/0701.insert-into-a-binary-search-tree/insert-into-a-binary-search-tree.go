e1c1ba7c407bad3ce221bd71f21af4405857a5ca
aQua <aQua@aQuaYi.com>
2018-07-22 11:29:53 +0800

    701 accepted

--- Algorithms/0701.insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.go
+++ Algorithms/0701.insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.go
@@ -17,6 +17,17 @@ import (
 type TreeNode = kit.TreeNode
 
 func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return &TreeNode{
+			Val: val,
+		}
+	}
 
-	return nil
+	if root.Val <= val {
+		root.Right = insertIntoBST(root.Right, val)
+	} else {
+		root.Left = insertIntoBST(root.Left, val)
+	}
+
+	return root
 }
