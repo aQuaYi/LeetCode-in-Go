4008c8f2b2623755df475981d8ca044e2ae7ef7f
aQua <aQua@aQuaYi.com>
2018-07-22 14:43:38 +0800

    710 finish

--- Algorithms/0710.random-pick-with-blacklist/random-pick-with-blacklist.go
+++ Algorithms/0710.random-pick-with-blacklist/random-pick-with-blacklist.go
@@ -14,25 +14,29 @@ type Solution struct {
 // Constructor 构建了 Solution
 func Constructor(N int, blacklist []int) Solution {
 	rand.Seed(time.Now().UnixNano())
-
+	// M 是可以自由选择的数的个数
 	M := N - len(blacklist)
 
 	blackMap := make(map[int]int, len(blacklist))
-	//
+	// 由于 blacklist 是乱序的，只好先把 blacklist 中的元素全部添加为 blackMap 的 key
 	for _, b := range blacklist {
-		blackMap[b] = -1
+		blackMap[b] = b
 	}
+
 	for _, b := range blacklist {
 		if b >= M {
 			continue
 		}
 
-		for blackMap[N-1] == -1 {
+		// 对于所有的 b < M
+		// 与 [M,N) 中某个不在 BlackList 中的数，关联上
+
+		N--
+		for blackMap[N] == N {
 			N--
 		}
 
-		blackMap[b] = N - 1
-		N--
+		blackMap[b] = N
 	}
 
 	return Solution{
@@ -44,8 +48,12 @@ func Constructor(N int, blacklist []int) Solution {
 
 // Pick 选取了不在 BlackList 中的值
 func (s *Solution) Pick() int {
+	// 在 [0,M) 中任意挑选一个数 r
 	r := rand.Intn(s.M)
 	if t, ok := s.blackMap[r]; ok {
+		// r 是 s.blackMap 的 key
+		// 说明 r 是 BlackList 中的值
+		// 需要返回 与 r 相关联的值
 		return t
 	}
 	return r
