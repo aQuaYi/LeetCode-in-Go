58d91ec09151b8cfa4a5d1b54e8aee1fca679631
aQua <aQua@aQuaYi.com>
2018-07-22 10:56:46 +0800

    701 added

---
+++ Algorithms/0701.insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.go
@@ -0,0 +1,22 @@
+package problem0701
+
+import (
+	"github.com/aQuaYi/LeetCode-in-Go/kit"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+// TreeNode 是预定义的结构体
+type TreeNode = kit.TreeNode
+
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+
+	return nil
+}
---
+++ Algorithms/0701.insert-into-a-binary-search-tree/insert-into-a-binary-search-tree_test.go
@@ -0,0 +1,46 @@
+package problem0701
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aQuaYi/LeetCode-in-Go/kit"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	root []int
+	val  int
+	ans  []int
+}{
+
+	{
+		[]int{4, 2, 7, 1, 3},
+		5,
+		[]int{4, 2, 1, 3, 7, 5}, // preorder
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_insertIntoBST(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		root := kit.Ints2TreeNode(tc.root)
+		ans := kit.Tree2Preorder(insertIntoBST(root, tc.val))
+		ast.Equal(tc.ans, ans, "输入:%v", tc)
+	}
+}
+
+func Benchmark_insertIntoBST(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			root := kit.Ints2TreeNode(tc.root)
+			insertIntoBST(root, tc.val)
+		}
+	}
+}
