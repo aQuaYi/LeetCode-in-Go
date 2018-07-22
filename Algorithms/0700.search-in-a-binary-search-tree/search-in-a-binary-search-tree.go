62abbf91c93bcb806967c52010abeabf506dc675
aQua <aQua@aQuaYi.com>
2018-07-22 10:33:00 +0800

    700 added

---
+++ Algorithms/0700.search-in-a-binary-search-tree/search-in-a-binary-search-tree.go
@@ -0,0 +1,20 @@
+package problem0700
+
+import "github.com/aQuaYi/LeetCode-in-Go/kit"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+// TreeNode 是预定义的类型
+type TreeNode = kit.TreeNode
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+
+	return nil
+}
