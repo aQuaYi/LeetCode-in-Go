7413e80b8142efc71c45e848238a7e7122660989
aQua <aQua@aQuaYi.com>
2018-07-26 09:10:56 +0800

    857 修改注释

--- Algorithms/0857.minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.go
+++ Algorithms/0857.minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.go
@@ -34,40 +34,42 @@ func mincostToHireWorkers(quality []int, wage []int, K int) float64 {
 		if len(pq) == K {
 			cost = min(cost, qsum*w[0])
 		}
-
-		/**
-		 * 为了满足题目的两个条件，可知，总的工资支出
-		 * cost = (sum of k q) * (max of k w/q)
-		 *
-		 * 在 for 循环中， (max of k w/q) == w[0]
-		 * 而 w[0] 是单调递增的，这一点很重要
-		 *
-		 * (sum of k q) == qsum
-		 * 在 for 循环中， qsum 是单调递减的
-		 *
-		 * 由于 w[0] 和 qsum 的单调性
-		 * 所以， 最小的 cost 注定在某个 qsum * w[0] 中
-		 *
-		 * 思考一下，如果 workers[i][1] (i>K) 是最大的 quality
-		 * 那么，qsum += workers[i][1] 后，立马就会 qsum -= workers[i][1]
-		 * 也就是说，qsum 中没有算上 workers[i][1]
-		 * 但是，还是会执行
-		 * cost = min(cost, qsum*workers[i][0])
-		 * 也就是说，(sum of k q) 与 (max of k w/q) 中的 k 不一致了，不会有问题吗？
-		 *
-		 *
-		 *
-		 * 答，不会有问题
-		 * cost = min(cost, qsum*workers[i][0])
-		 * 实际上是
-		 * cost = min(<=qsum*workers[i-1][0], qsum*workers[i][0])
-		 * 因为，根据 w[0] 的单调性
-		 * workers[i-1][0] <= workers[i][0]
-		 * 所以，
-		 * qsum*workers[i][0] 虽然是一个错误的组合，但是也注定了这个值不会是最小值
-		 *
-		 */
 	}
+
+	/**
+	 * 为了满足题目的两个条件，可知，总的工资支出
+	 * cost = (sum of k q) * (max of k w/q)
+	 *
+	 * 在 for 循环中， (max of k w/q) = w[0]
+	 * 而 w[0] 是单调递增的，这一点很重要
+	 *
+	 * (sum of k q) = qsum
+	 * 在 for 循环中， qsum 是单调递减的
+	 *
+	 * 由于 w[0] 和 qsum 的单调性
+	 * 所以， 最小的 cost 注定在某个 qsum * w[0] 中
+	 *
+	 *
+	 *
+	 * 思考一下， 假设 workers[i][1] (i>K) 是最大的 quality
+	 * 那么，qsum += workers[i][1] 后，立马就会 qsum -= workers[i][1]
+	 * 也就是说，组成 qsum 的 k 个 worker 不包括 i
+	 * 但是，还是会执行
+	 * cost = min(cost, qsum*workers[i][0])
+	 * 也就是说，(sum of k q) 与 (max of k w/q) 中的 k 不一致了，不会有问题吗？
+	 *
+	 *
+	 *
+	 * 答，不会有问题
+	 * 在执行 min(cost, qsum*workers[i][0]) 的时候
+	 * 可以肯定 cost <= qsum*workers[i-1][0]
+	 * 因为，根据 w[0] 的单调性
+	 * workers[i-1][0] <= workers[i][0]
+	 * 也就是说 cost <= qsum*workers[i][0]
+	 * 所以，
+	 * qsum*workers[i][0] 虽然是一个错误的组合，但这个值不会带来错误的最小值
+	 *
+	 */
 
 	return cost
 }
