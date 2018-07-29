7a7b6f9348492dd2ababef1eb648ae2ee4c66737
aQua <aQua@aQuaYi.com>
2018-07-29 22:00:25 +0800

    865 添加 TreeNode 数据类型

--- Algorithms/0865.smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.go
+++ Algorithms/0865.smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.go
@@ -1,5 +1,20 @@
 package problem0865
 
+import (
+	"github.com/aQuaYi/LeetCode-in-Go/kit"
+)
+
+// TreeNode 是预定义的数据结构
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+type TreeNode = kit.TreeNode
+
 func subtreeWithAllDeepest(root *TreeNode) *TreeNode {
 
 	return nil
