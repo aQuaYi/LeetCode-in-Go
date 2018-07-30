60de437d5e86f0f05c514497e9a19c5efc41003a
aQua <aQua@aQuaYi.com>
2018-07-30 20:54:56 +0800

    867 finish

--- Algorithms/0867.transpose-matrix/transpose-matrix.go
+++ Algorithms/0867.transpose-matrix/transpose-matrix.go
@@ -1,6 +1,18 @@
 package problem0867
 
 func transpose(A [][]int) [][]int {
+	m, n := len(A), len(A[0])
 
-	return nil
+	res := make([][]int, n)
+	for i := 0; i < n; i++ {
+		res[i] = make([]int, m)
+	}
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			res[j][i] = A[i][j]
+		}
+	}
+
+	return res
 }
