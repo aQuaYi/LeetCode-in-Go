cc8218b591ee9139137e6abc4da5f7b525c77de4
aQua <aQua@aQuaYi.com>
2018-07-31 23:59:18 +0800

    866 finish

--- Algorithms/0866.prime-palindrome/prime-palindrome.go
+++ Algorithms/0866.prime-palindrome/prime-palindrome.go
@@ -5,86 +5,69 @@ import (
 	"strconv"
 )
 
-func primePalindrome(n int) int {
-	n = nextPalindrome(n)
+func primePalindrome(N int) int {
 
-	for !isPrime(n) {
-		n = palindromeAfterPalindrome(n)
+	switch {
+	case N <= 2:
+		return 2
+	case N == 3:
+		return 3
+	case N <= 5:
+		return 5
+	case N <= 7:
+		return 7
+	case N <= 11:
+		return 11
+	case N <= 101:
+		return 101
 	}
 
-	return n
-}
+	size := len(strconv.Itoa(N))
 
-func isPalindrome(x int) bool {
-	s := []byte(strconv.Itoa(x))
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-		if s[i] != s[j] {
-			return false
+	left := 1
+	for i := 0; i < size/2; i++ {
+		left *= 10
+	}
+	if size%2 == 1 {
+		left = N / left
+	}
+
+	// 当 size 为偶数时，
+	// 可知
+	// 要么，下个回文数的长度，也为偶数
+	// 要么，下个回文数是 10^size + 1
+	// 又
+	// 当回文数的长度为偶数时，肯定不是质数，因为 11 是因子
+	// 所以，直接从 10^size + 1 开始找起
+
+	for {
+		// 以 left 的末尾为镜子，制造出回文数
+		// 假设 left 看起来像 abc，则要找一个 n 看起来像 abcba
+		n := left
+		tmp := left / 10
+		for tmp > 0 {
+			n = n*10 + tmp%10
+			tmp /= 10
 		}
-	}
-	return true
-}
 
-func nextPalindrome(n int) int {
-	for !isPalindrome(n) {
-		n++
-	}
-	return n
-}
+		// 需要验证 n >= N，是因为
+		// 刚开始的 n 可能会比 N 小，
+		// 例如，N = 98390 时，第一个 n 是 98389
+		if n >= N && isPrime(n) {
+			return n
+		}
 
-func palindromeAfterPalindrome(n int) int {
-	if n < 9 {
-		return n + 1
-	}
-
-	ok, base, length := isAll9(n)
-	if ok {
-		return base + 1
-	}
-
-	left := n
-	rightRevers := 0
-	for i := length; i > ((length / 2) + (length % 2)); i-- {
-		rightRevers = rightRevers*10 + left%10
-		left /= 10
-	}
-
-	if length%2 == 1 {
-		rightRevers = rightRevers*10 + left%10
-	}
-
-	if left <= rightRevers {
 		left++
 	}
-
-	rightRevers = left
-	res := left
-	if length%2 == 1 {
-		rightRevers /= 10
-	}
-	for rightRevers > 0 {
-		res = res*10 + rightRevers%10
-		rightRevers /= 10
-	}
-
-	return res
 }
 
-func isAll9(n int) (bool, int, int) {
-	base := 1
-	len := 0
-	for n%10 == 9 {
-		n /= 10
-		base *= 10
-		len++
-	}
-	t := n
-	for t > 0 {
-		t /= 10
-		len++
-	}
-	return n == 0, base, len
-}
+/**
+ * 此题要点：
+ * 1. 利用数学知识简化需要处理的情况
+ *    形如 abccba 等长度为偶数的回文数，都不是素数，因为有因子 11
+ * 2. 把 res <= 101 这样的与后面处理方式不兼容的少数情况，单独处理。
+ * 3. 没有把寻找 n 作为单独的函数，简化了判断条件。
+ */
 
 // https://blog.csdn.net/willduan1/article/details/50975381
 func isPrime(n int) bool {
@@ -101,11 +84,4 @@ func isPrime(n int) bool {
 		}
 	}
 	return true
-}
-
-func max(a, b byte) byte {
-	if a > b {
-		return a
-	}
-	return b
 }
