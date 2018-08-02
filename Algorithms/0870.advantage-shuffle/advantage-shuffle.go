53949906c950de774cb2127dcc6cdee68930d2a8
aQua <aQua@aQuaYi.com>
2018-08-02 20:27:21 +0800

    870 wrong answer

--- Algorithms/0870.advantage-shuffle/advantage-shuffle.go
+++ Algorithms/0870.advantage-shuffle/advantage-shuffle.go
@@ -1,6 +1,41 @@
 package problem0870
 
-func advantageCount(A []int, B []int) []int {
+import (
+	"sort"
+)
 
-	return nil
+func advantageCount(A []int, B []int) []int {
+	size := len(A)
+	isTaken := make([]bool, size)
+
+	search := func(n int) int {
+		l, r := 0, size-1
+
+		for l < r {
+			m := (l + r) / 2
+			if A[m] <= n {
+				l = m + 1
+			} else {
+				r = m
+			}
+		}
+
+		for isTaken[r] {
+			r = (r + 1) % size
+		}
+
+		isTaken[r] = true
+
+		return r
+	}
+
+	sort.Ints(A)
+
+	res := make([]int, size)
+
+	for i, n := range B {
+		res[i] = A[search(n)]
+	}
+
+	return res
 }
