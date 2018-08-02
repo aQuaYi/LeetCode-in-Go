5b9b7d2e593c73e5014d22bb6010075f1f7fd993
aQua <aQua@aQuaYi.com>
2018-08-02 19:54:42 +0800

    870 added

---
+++ Algorithms/0870.advantage-shuffle/README.md
@@ -0,0 +1,31 @@
+# [870. Advantage Shuffle](https://leetcode.com/problems/advantage-shuffle/)
+
+## 题目
+
+Given two arrays A and B of equal size, the advantage of A with respect to B is the number of indices ifor which A[i] > B[i].
+
+Return any permutation of A that maximizes its advantage with respect to B.
+
+Example 1:
+
+```text
+Input: A = [2,7,11,15], B = [1,10,4,11]
+Output: [2,11,7,15]
+```
+
+Example 2:
+
+```text
+Input: A = [12,24,8,32], B = [13,25,32,11]
+Output: [24,32,8,12]
+```
+
+Note:
+
+1. 1 <= A.length = B.length <= 10000
+1. 0 <= A[i] <= 10^9
+1. 0 <= B[i] <= 10^9
+
+## 解题思路
+
+见程序注释
---
+++ Algorithms/0870.advantage-shuffle/advantage-shuffle_test.go
@@ -0,0 +1,47 @@
+package problem0870
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	A   []int
+	B   []int
+	ans []int
+}{
+
+	{
+		[]int{2, 7, 11, 15},
+		[]int{1, 10, 4, 11},
+		[]int{2, 11, 7, 15},
+	},
+
+	{
+		[]int{12, 24, 8, 32},
+		[]int{13, 25, 32, 11},
+		[]int{24, 32, 8, 12},
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_advantageCount(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, advantageCount(tc.A, tc.B), "输入:%v", tc)
+	}
+}
+
+func Benchmark_advantageCount(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			advantageCount(tc.A, tc.B)
+		}
+	}
+}
