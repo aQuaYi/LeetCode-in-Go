15e3903fa1bed6e3599932fd615dddb978eb8960
aQua <aQua@aQuaYi.com>
2018-08-04 21:05:04 +0800

    478 accepted. 180ms

--- Algorithms/0478.generate-random-point-in-a-circle/generate-random-point-in-a-circle.go
+++ Algorithms/0478.generate-random-point-in-a-circle/generate-random-point-in-a-circle.go
@@ -1,6 +1,7 @@
 package problem0478
 
 import (
+	"fmt"
 	"math"
 	"math/rand"
 )
@@ -9,28 +10,24 @@ import (
 // obj := Constructor(radius, x_center, y_center);
 // param_1 := obj.RandPoint();
 type Solution struct {
-	r, a, b, ratio float64
+	r, a, b float64
 }
 
 // Constructor 构建 Solution
 func Constructor(radius, xCenter, yCenter float64) Solution {
-	ratio := 1.
-	if radius < 1 {
-		radius = 1.
-		ratio = 1. / radius
-	}
 	return Solution{
-		r:     radius,
-		a:     xCenter,
-		b:     yCenter,
-		ratio: ratio,
+		r: radius,
+		a: xCenter,
+		b: yCenter,
 	}
 }
 
 // RandPoint 返回随机点
 func (s *Solution) RandPoint() []float64 {
-	rjd := rand.Float64()
-	x := s.a + s.r*math.Sin(rjd)
-	y := s.b + s.r*math.Cos(rjd)
-	return []float64{x / s.ratio, y / s.ratio}
+	len := math.Sqrt(rand.Float64()) * s.r
+	deg := rand.NormFloat64() * 2 * math.Pi
+	x := s.a + len*math.Cos(deg)
+	y := s.b + len*math.Sin(deg)
+	fmt.Printf("len = %f, x = %f, y = %f\n", len, x, y)
+	return []float64{x, y}
 }
