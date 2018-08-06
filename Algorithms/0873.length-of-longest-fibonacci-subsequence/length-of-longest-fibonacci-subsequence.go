5da69f30bf3ff9eae5ea0ac078a9806b678d2c7f
aQua <aQua@aQuaYi.com>
2018-08-06 22:17:04 +0800

    873

--- Algorithms/0873.length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.go
+++ Algorithms/0873.length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.go
@@ -1,32 +1,60 @@
 package problem0873
 
-func lenLongestFibSubseq(A []int) int {
-	size := len(A)
+func lenLongestFibSubseq(a []int) (r int) {
+	size := len(a)
 
-	dp := make([][]int, size)
-	for i := range dp {
-		dp[i] = make([]int, size)
-	}
+	for k := 2; k < size; k++ {
+		for i, j := 0, k-1; i < j; {
+			s := a[i] + a[j]
 
-	res := 0
-
-	index := make(map[int]int, size)
-	for k := 0; k < size; k++ {
-		index[A[k]] = k
-		for j := 0; j < k; j++ {
-			dp[j][k] = 2
-			i, ok := index[A[k]-A[j]]
-			if ok && A[k]-A[j] < A[j] {
-				dp[j][k] = dp[i][j] + 1
+			if s < a[k] {
+				i++
+				continue
+			} else if s > a[k] {
+				j--
+				continue
 			}
-			res = max(res, dp[j][k])
+
+			count := 3
+			x0, x1 := a[j], a[k]
+			l := k + 1
+
+			for {
+
+				f, p, x2 := l, size, x0+x1
+
+				for p-f >= 4 {
+					m := (p + f) / 2
+
+					if x2 < a[m] {
+						p = m
+					} else if x2 > a[m] {
+						f = m + 1
+					} else {
+						f = m
+						break
+					}
+				}
+
+				for ; f < p; f++ {
+					if a[f] == x2 {
+						count, x0, x1, l = count+1, x1, x2, f+1
+						break
+					}
+				}
+				if f == p {
+					break
+				}
+
+			}
+
+			r = max(r, count)
+			i++
+			j--
 		}
 	}
 
-	if res <= 2 {
-		return 0
-	}
-	return res
+	return
 }
 
 func max(a, b int) int {
