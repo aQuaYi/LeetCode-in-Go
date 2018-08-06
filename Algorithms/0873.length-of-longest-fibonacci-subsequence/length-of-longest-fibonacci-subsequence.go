a0874f85cbe6c0821ba766429021bf50c724f186
aQua <aQua@aQuaYi.com>
2018-08-06 21:28:31 +0800

    873 accepted. 72ms < 20ms

--- Algorithms/0873.length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.go
+++ Algorithms/0873.length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.go
@@ -1,6 +1,35 @@
 package problem0873
 
 func lenLongestFibSubseq(A []int) int {
+	size := len(A)
 
-	return 0
+	idxs := make(map[int]int, size)
+	mp := make(map[int]bool, size)
+	for i, a := range A {
+		idxs[a] = i
+		mp[a] = true
+	}
+
+	res := 0
+
+	for i := 0; i < size; i++ {
+		for j := i + 1; j < size; j++ {
+			tmp := 2
+			i0, i1 := i, j
+			for mp[A[i0]+A[i1]] {
+				tmp++
+				res = max(res, tmp)
+				i0, i1 = i1, idxs[A[i0]+A[i1]]
+			}
+		}
+	}
+
+	return res
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
