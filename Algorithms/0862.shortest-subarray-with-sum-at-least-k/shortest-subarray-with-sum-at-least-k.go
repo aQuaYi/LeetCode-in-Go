e01f3166833bb8066287e1f706205fd57334071a
aQua <aQua@aQuaYi.com>
2018-08-07 19:41:23 +0800

    862 wrong answer 但我觉得是答案错了

--- Algorithms/0862.shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.go
+++ Algorithms/0862.shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.go
@@ -1,6 +1,27 @@
 package problem0862
 
-func shortestSubarray(A []int, K int) int {
+const intMax = 1<<63 - 1
 
-	return 0
+func shortestSubarray(a []int, K int) int {
+	size := len(a)
+
+	sums := make([]int, size+1)
+	s := 0
+	for i, n := range a {
+		if n == K {
+			return 1
+		}
+		s += n
+		sums[i+1] = s
+	}
+
+	for i := 0; i < size; i++ {
+		for j := i + 1; j <= size; j++ {
+			if sums[j]-sums[i] == K {
+				return j - i
+			}
+		}
+	}
+
+	return -1
 }
