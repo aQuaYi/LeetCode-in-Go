d3b48f381d4d19b96d49df60e0a6e408e325a790
aQua <aQua@aQuaYi.com>
2018-08-11 12:16:05 +0800

    871 fail

--- Algorithms/0871.minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.go
+++ Algorithms/0871.minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.go
@@ -1,6 +1,34 @@
 package problem0871
 
 func minRefuelStops(target int, startFuel int, stations [][]int) int {
+	size := len(stations)
+	dp := make([]int, size+1)
+
+	f := startFuel
+	for i, s := range stations {
+		if f < s[0] {
+			return -1
+		}
+		f = f - s[0] + s[1]
+		dp[i] = i + 1
+	}
+	dp[size-1] = size
+
+	fs := make([]int, size+1)
+	miles := 0
+	for i := range fs {
+		miles += stations[i][0]
+		fs[i] = startFuel - miles
+	}
+	if fs[size] >= 0 {
+		return 0
+	}
+
+	stops := 0
+
+	for i := 0; i < size; i++ {
+
+	}
 
 	return 0
 }
