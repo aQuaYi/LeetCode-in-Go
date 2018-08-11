9078fb0afeb76666222f409b4b0a2f3e2abe24bf
aQua <aQua@aQuaYi.com>
2018-08-11 13:28:51 +0800

    871 wrong answer

--- Algorithms/0871.minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.go
+++ Algorithms/0871.minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.go
@@ -1,34 +1,69 @@
 package problem0871
 
+import "container/heap"
+
 func minRefuelStops(target int, startFuel int, stations [][]int) int {
 	size := len(stations)
-	dp := make([]int, size+1)
 
-	f := startFuel
-	for i, s := range stations {
-		if f < s[0] {
-			return -1
+	pq := make(PQ, 0, size*500)
+	pq = append(pq, station{
+		index: -1,
+		miles: startFuel,
+		stops: 0,
+	})
+	heap.Init(&pq)
+
+	for len(pq) > 0 {
+		s := heap.Pop(&pq).(station)
+		if s.miles >= target {
+			return s.stops
 		}
-		f = f - s[0] + s[1]
-		dp[i] = i + 1
-	}
-	dp[size-1] = size
 
-	fs := make([]int, size+1)
-	miles := 0
-	for i := range fs {
-		miles += stations[i][0]
-		fs[i] = startFuel - miles
-	}
-	if fs[size] >= 0 {
-		return 0
+		for i := s.index + 1; i < size && stations[i][0] <= s.miles; i++ {
+			heap.Push(&pq, station{
+				index: i,
+				miles: s.miles + stations[i][1],
+				stops: s.stops + 1,
+			})
+		}
 	}
 
-	stops := 0
+	return -1
+}
 
-	for i := 0; i < size; i++ {
+// station 是 priorityQueue 中的元素
+type station struct {
+	stops, index, miles int
+}
 
+// PQ implements heap.Interface and holds entries.
+type PQ []station
+
+func (pq PQ) Len() int { return len(pq) }
+
+func (pq PQ) Less(i, j int) bool {
+	if pq[i].stops == pq[j].stops {
+		// if pq[i].miles == pq[j].miles {
+		// return pq[i].index > pq[j].index
+		// }
+		return pq[i].miles > pq[j].miles
 	}
+	return pq[i].stops < pq[j].stops
+}
 
-	return 0
+func (pq PQ) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+
+// Push 往 pq 中放 entry
+func (pq *PQ) Push(x interface{}) {
+	temp := x.(station)
+	*pq = append(*pq, temp)
+}
+
+// Pop 从 pq 中取出最优先的 entry
+func (pq *PQ) Pop() interface{} {
+	temp := (*pq)[len(*pq)-1]
+	*pq = (*pq)[0 : len(*pq)-1]
+	return temp
 }
