78eb49a20973fdbbcc0cfbdcf57d5d8ee03f6170
aQua <aQua@aQuaYi.com>
2018-08-12 15:07:45 +0800

    707 wrong answer

--- Algorithms/0707.design-linked-list/design-linked-list_test.go
+++ Algorithms/0707.design-linked-list/design-linked-list_test.go
@@ -6,6 +6,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_myFunc(t *testing.T) {
+func Test_myLinkedList(t *testing.T) {
 	ast := assert.New(t)
+
+	l := Constructor()
+
+	l.AddAtHead(1)
+	l.AddAtHead(3)
+
+	l.AddAtIndex(5, 5)
+	ast.Equal(-1, l.Get(5))
+
+	l.AddAtIndex(1, 2)
+
+	ast.Equal(2, l.Get(1))
+
+	l.DeleteAtIndex(1)
+
+	ast.Equal(3, l.Get(1))
 }
