84e5ddb8c98b94a9c988ff03e94fece353912df7
aQua <aQua@aQuaYi.com>
2018-08-12 14:14:25 +0800

    706 added

---
+++ Algorithms/0706.design-hashmap/design-hashmap.go
@@ -0,0 +1,31 @@
+package problem0706
+
+// MyHashMap object will be instantiated and called as such:
+// obj := Constructor();
+// obj.Put(key,value);
+// param_2 := obj.Get(key);
+// obj.Remove(key);
+type MyHashMap struct {
+}
+
+// Constructor initialize your data structure here. */
+func Constructor() MyHashMap {
+
+	return MyHashMap{}
+}
+
+// Put value will always be non-negative. */
+func (m *MyHashMap) Put(key int, value int) {
+
+}
+
+// Get returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+func (m *MyHashMap) Get(key int) int {
+
+	return 0
+}
+
+// Remove the mapping of the specified value key if this map contains a mapping for the key */
+func (m *MyHashMap) Remove(key int) {
+
+}
