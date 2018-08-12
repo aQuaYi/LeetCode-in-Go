1139c4e8cc48ede7bf26c9dccc13d9057bdac2d1
aQua <aQua@aQuaYi.com>
2018-08-12 14:08:08 +0800

    705 finish

--- Algorithms/0705.design-hashset/design-hashset.go
+++ Algorithms/0705.design-hashset/design-hashset.go
@@ -6,26 +6,25 @@ package problem0705
 // obj.Remove(key);
 // param_3 := obj.Contains(key);
 type MyHashSet struct {
+	table []bool
 }
 
 // Constructor initialize your data structure here. */
 func Constructor() MyHashSet {
-
-	return MyHashSet{}
+	return MyHashSet{table: make([]bool, 1000001)}
 }
 
 // Add 添加 key
 func (s *MyHashSet) Add(key int) {
-
+	s.table[key] = true
 }
 
 // Remove 移除 key
 func (s *MyHashSet) Remove(key int) {
-
+	s.table[key] = false
 }
 
 // Contains returns true if this set contains the specified element */
 func (s *MyHashSet) Contains(key int) bool {
-
-	return true
+	return s.table[key]
 }
