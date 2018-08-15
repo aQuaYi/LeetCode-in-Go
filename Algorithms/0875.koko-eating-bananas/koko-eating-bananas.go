fdaeba9e0a2718579dddab64645000d80fe4c3dc
aQua <aQua@aQuaYi.com>
2018-08-15 16:43:29 +0800

    875 accepted. 72ms > 30ms. 二分查找法

--- Algorithms/0875.koko-eating-bananas/koko-eating-bananas.go
+++ Algorithms/0875.koko-eating-bananas/koko-eating-bananas.go
@@ -1,6 +1,35 @@
 package problem0875
 
-func minEatingSpeed(piles []int, H int) int {
+import (
+	"sort"
+)
 
-	return 0
+func minEatingSpeed(piles []int, H int) int {
+	sort.Ints(piles)
+
+	n := len(piles)
+
+	l, r := 1, piles[n-1]
+	for l < r {
+		m := (l + r) / 2
+		if canEatAll(m, H, n, piles) {
+			r = m
+		} else {
+			l = m + 1
+		}
+	}
+
+	return r
+}
+
+func canEatAll(k, h, n int, piles []int) bool {
+	t := sort.Search(n, func(i int) bool {
+		return piles[i] > k
+	})
+
+	for b, i := t, n-1; b <= i && t <= h; i-- {
+		t += (piles[i] + k - 1) / k
+	}
+
+	return t <= h
 }
