ab6acef2eaa014e7934df12e434f91339b0f6a58
aQua <aQua@aQuaYi.com>
2018-08-16 20:13:43 +0800

    883 finish

--- Algorithms/0883.projection-area-of-3d-shapes/projection-area-of-3d-shapes.go
+++ Algorithms/0883.projection-area-of-3d-shapes/projection-area-of-3d-shapes.go
@@ -1,23 +1,31 @@
 package problem0883
 
-func projectionArea(grids [][]int) int {
-	xy := make(map[int]bool, 2500)
-	yz := make(map[int]bool, 2500)
-	xz := make(map[int]bool, 2500)
+func projectionArea(grid [][]int) int {
+	xs := [51]int{}
+	ys := [51]int{}
+	res := 0
 
-	for i, grid := range grids {
-		for j, cubes := range grid {
-			for k := 0; k < cubes; k++ {
-				xy[encode(i, j)] = true
-				yz[encode(j, k)] = true
-				xz[encode(i, k)] = true
+	for i, line := range grid {
+		for j, k := range line {
+			if k == 0 {
+				continue
 			}
+			res++
+			xs[i] = max(xs[i], k)
+			ys[j] = max(ys[j], k)
 		}
 	}
 
-	return len(xy) + len(yz) + len(xz)
+	for i := range xs {
+		res += xs[i] + ys[i]
+	}
+
+	return res
 }
 
-func encode(a, b int) int {
-	return a<<8 | b
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
