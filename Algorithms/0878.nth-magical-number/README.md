7f3c44565fef8fe0b11e94fdd99fde958781c81a
aQua <aQua@aQuaYi.com>
2018-08-18 16:32:20 +0800

    878 added

---
+++ Algorithms/0878.nth-magical-number/README.md
@@ -0,0 +1,45 @@
+# [878. Nth Magical Number](https://leetcode.com/problems/nth-magical-number/)
+
+## 题目
+
+A positive integeris magicalif it is divisible by either Aor B.
+
+Return the N-th magical number. Since the answer may be very large, return it modulo 10^9 + 7.
+
+Example 1:
+
+```text
+Input: N = 1, A = 2, B = 3
+Output: 2
+```
+
+Example 2:
+
+```text
+Input: N = 4, A = 2, B = 3
+Output: 6
+```
+
+Example 3:
+
+```text
+Input: N = 5, A = 2, B = 4
+Output: 10
+```
+
+Example 4:
+
+```text
+Input: N = 3, A = 6, B = 4
+Output: 8
+```
+
+Note:
+
+1. 1 <= N<= 10^9
+1. 2 <= A<= 40000
+1. 2 <= B<= 40000
+
+## 解题思路
+
+见程序注释
---
+++ Algorithms/0878.nth-magical-number/nth-magical-number_test.go
@@ -0,0 +1,64 @@
+package problem0878
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	N   int
+	A   int
+	B   int
+	ans int
+}{
+
+	{
+		1,
+		2,
+		3,
+		2,
+	},
+
+	{
+		4,
+		2,
+		3,
+		6,
+	},
+
+	{
+		5,
+		2,
+		4,
+		10,
+	},
+
+	{
+		3,
+		6,
+		4,
+		8,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_nthMagicalNumber(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, nthMagicalNumber(tc.N, tc.A, tc.B), "输入:%v", tc)
+	}
+}
+
+func Benchmark_nthMagicalNumber(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			nthMagicalNumber(tc.N, tc.A, tc.B)
+		}
+	}
+}
