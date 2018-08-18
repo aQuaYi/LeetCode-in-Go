43f0e3d00becf43e6c0a94b461578673d6514b40
aQua <aQua@aQuaYi.com>
2018-08-18 16:45:03 +0800

    878 Limit Time Exceeded

--- Algorithms/0878.nth-magical-number/nth-magical-number.go
+++ Algorithms/0878.nth-magical-number/nth-magical-number.go
@@ -1,6 +1,18 @@
 package problem0878
 
 func nthMagicalNumber(N int, A int, B int) int {
+	if A > B {
+		A, B = B, A
+	}
 
-	return 0
+	num := A - 1
+	count := 0
+	for count < N {
+		num++
+		if num%A == 0 || num%B == 0 {
+			count++
+		}
+	}
+
+	return num
 }
