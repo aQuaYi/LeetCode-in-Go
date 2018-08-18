0cd1ba293e0e480723d84495a140015c891b4971
aQua <aQua@aQuaYi.com>
2018-08-18 18:52:18 +0800

    878 accepted.

--- Algorithms/0879.profitable-schemes/profitable-schemes.go
+++ Algorithms/0879.profitable-schemes/profitable-schemes.go
@@ -1,6 +1,39 @@
 package problem0879
 
-func profitableSchemes(G, P int, group, profit []int) int {
+const mod = 1e9 + 7
 
-	return 0
+func profitableSchemes(G, P int, group, profit []int) int {
+	// dp[p][g] = x 表示， g 个人想要获得收益 p ，一共有 x 种选择
+	dp := [101][101]int{}
+	dp[0][0] = 1
+
+	size := len(group)
+	for k := 0; k < size; k++ {
+		g := group[k]
+		p := profit[k]
+		for i := P; i >= 0; i-- {
+			ip := min(i+p, P)
+			for j := G - g; j >= 0; j-- {
+				dp[ip][j+g] += dp[i][j]
+				dp[ip][j+g] %= mod
+			}
+		}
+	}
+
+	res := 0
+	for i, x := range dp[P] {
+		if i > G {
+			break
+		}
+		res = (res + x) % mod
+	}
+
+	return res
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
