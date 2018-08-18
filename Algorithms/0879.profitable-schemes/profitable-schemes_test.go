a1238878bd2c2560deb41099d736ac25caf003a7
aQua <aQua@aQuaYi.com>
2018-08-18 17:51:02 +0800

    879 added

---
+++ Algorithms/0879.profitable-schemes/profitable-schemes_test.go
@@ -0,0 +1,53 @@
+package problem0879
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	G      int
+	P      int
+	group  []int
+	profit []int
+	ans    int
+}{
+
+	{
+		5,
+		3,
+		[]int{2, 2},
+		[]int{2, 3},
+		2,
+	},
+
+	{
+		10,
+		5,
+		[]int{2, 3, 5},
+		[]int{6, 7, 8},
+		7,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_profitableSchemes(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, profitableSchemes(tc.G, tc.P, tc.group, tc.profit), "输入:%v", tc)
+	}
+}
+
+func Benchmark_profitableSchemes(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			profitableSchemes(tc.G, tc.P, tc.group, tc.profit)
+		}
+	}
+}
