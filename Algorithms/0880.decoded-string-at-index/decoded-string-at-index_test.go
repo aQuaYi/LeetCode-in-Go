64fc48657037247f63830efea1f2670c7662bc86
aQua <aQua@aQuaYi.com>
2018-08-19 15:57:41 +0800

    880 finish

--- Algorithms/0880.decoded-string-at-index/decoded-string-at-index_test.go
+++ Algorithms/0880.decoded-string-at-index/decoded-string-at-index_test.go
@@ -15,9 +15,57 @@ var tcs = []struct {
 }{
 
 	{
+		"a2345678999999999999999",
+		12314145213412,
+		"a",
+	},
+
+	{
+		"a2b3c4d5e6f7g8h9",
+		3,
+		"b",
+	},
+
+	{
+		"leet2code3for2you2",
+		160,
+		"y",
+	},
+
+	{
+		"leet2code3for2you2",
+		89,
+		"t",
+	},
+
+	{
+		"leet2code3for2you2",
+		13,
+		"l",
+	},
+
+	{
+		"leet2code3for2you2",
+		90,
+		"c",
+	},
+
+	{
 		"leet2code3",
 		10,
 		"o",
+	},
+
+	{
+		"leet2code3for2you",
+		16,
+		"t",
+	},
+
+	{
+		"leet2code3for2you2",
+		85,
+		"t",
 	},
 
 	{
@@ -37,10 +85,22 @@ var tcs = []struct {
 
 func Test_decodeAtIndex(t *testing.T) {
 	ast := assert.New(t)
-
 	for _, tc := range tcs {
 		fmt.Printf("~~%v~~\n", tc)
 		ast.Equal(tc.ans, decodeAtIndex(tc.S, tc.K), "输入:%v", tc)
+	}
+}
+
+func Test_decodeAtIndex_2(t *testing.T) {
+	ast := assert.New(t)
+
+	s := "leat2code3for2you2"
+	ansStr := "leatleatcodeleatleatcodeleatleatcodeforleatleatcodeleatleatcodeleatleatcodeforyouleatleatcodeleatleatcodeleatleatcodeforleatleatcodeleatleatcodeleatleatcodeforyou"
+	for i := range ansStr {
+		expectd := ansStr[i : i+1]
+		actual := decodeAtIndex(s, i+1)
+		msg := fmt.Sprintf("%s 的第 %d 个字符应该是 %s", s, i+1, expectd)
+		ast.Equal(expectd, actual, msg)
 	}
 }
 
