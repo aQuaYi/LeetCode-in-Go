d34c3a76f77bf1ea745727e4a929a42cb6c268ef
aQua <aQua@aQuaYi.com>
2018-08-19 15:57:41 +0800

    880 finish

--- Algorithms/0880.decoded-string-at-index/decoded-string-at-index.go
+++ Algorithms/0880.decoded-string-at-index/decoded-string-at-index.go
@@ -1,6 +1,34 @@
 package problem0880
 
 func decodeAtIndex(S string, K int) string {
+	N, i := 0, 0
+	b := byte(0)
 
-	return ""
+	for i = 0; N < K; i++ {
+		b = S[i]
+		if isDigit(b) {
+			N *= int(b - '0')
+		} else {
+			N++
+		}
+	}
+
+	for {
+		i--
+		b = S[i]
+		if isDigit(b) {
+			N /= int(b - '0')
+			K %= N
+		} else {
+			if K == 0 || K == N {
+				return string(b)
+			}
+			N--
+		}
+	}
+
+}
+
+func isDigit(b byte) bool {
+	return b <= '9'
 }
