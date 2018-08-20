2236007de8c52242b3f4cf8282ab133109bf0898
aQua <aQua@aQuaYi.com>
2018-08-20 23:57:06 +0800

    882 finish

--- Algorithms/0882.reachable-nodes-in-subdivided-graph/reachable-nodes-in-subdivided-graph.go
+++ Algorithms/0882.reachable-nodes-in-subdivided-graph/reachable-nodes-in-subdivided-graph.go
@@ -12,37 +12,53 @@ func reachableNodes(edges [][]int, M int, N int) int {
 		nextTo[j] = append(nextTo[j], i)
 	}
 
+	// pq[x] = []int{r, i} 表示，
+	// 到达 i 节点时，已经走了 M-r 步，即，还可以走 r 步
 	pq := make(PQ, 1, 1000)
 	pq[0] = []int{M, 0}
 
+	// pq 很有可能存在多个 {r1, i}, {r2, i}, {r3, i}
+	// 利用 pq 的特性，优先处理 r 值最大的情况。
+	// 并在处理时，
+	// 在 seen 中标记已经处理过 i
+	// 在 maxStepsRemaining 中记录 r 的最大值
 	seen := [3001]bool{}
-	steps := [3001]int{}
+	// maxStepsRemaining[3] = 10 表示，
+	// 通过最短的路径到达 3 节点时，还可以走 10 步
+	maxStepsRemaining := [3001]int{}
 
 	res := 0
 	for len(pq) > 0 {
-		stepLeft := pq[0][0]
+		r := pq[0][0]
 		i := pq[0][1]
 		heap.Pop(&pq)
-		if !seen[i] {
-			seen[i] = true
-			steps[i] = stepLeft
-			res++
-			for _, j := range nextTo[i] {
-				if seen[j] {
-					continue
-				}
-				n := nodes[encode(i, j)]
-				jLeft := stepLeft - n - 1
-				if jLeft >= 0 {
-					heap.Push(&pq, []int{jLeft, j})
-				}
+		if seen[i] {
+			continue
+		}
+		seen[i] = true
+		maxStepsRemaining[i] = r
+		res++ // 收获 edge 端点 i
+
+		for _, j := range nextTo[i] {
+			if seen[j] {
+				continue
+			}
+			n := nodes[encode(i, j)]
+			jRemaining := r - n - 1
+			if jRemaining >= 0 {
+				// 如果可以在 M 步内到达 j
+				heap.Push(&pq, []int{jRemaining, j})
 			}
 		}
 	}
 
 	for _, e := range edges {
 		i, j, n := e[0], e[1], e[2]
-		res += min(steps[i]+steps[j], n)
+		// maxStepsRemaining[i] = ri 表示达到 i 点后，最多还可以走 ri 步
+		// maxStepsRemaining[j] = rj 表示达到 j 点后，最多还可以走 rj 步
+		// 如果 ri + rj >= n, 则 edge(i,j) 中间的 n 个点都可以被走到
+		// 否则 edge(i,j) 中只有 ri+rj 个点被走到
+		res += min(maxStepsRemaining[i]+maxStepsRemaining[j], n)
 	}
 
 	return res
@@ -61,9 +77,6 @@ type PQ [][]int
 func (pq PQ) Len() int { return len(pq) }
 
 func (pq PQ) Less(i, j int) bool {
-	// if pq[i][0] < pq[j][0]{
-	// return pq[i][1] < pq[j][1]
-	// }
 	return pq[i][0] > pq[j][0]
 }
 
