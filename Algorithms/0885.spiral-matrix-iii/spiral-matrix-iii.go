506ed025bfd6280b01954e833c48bef1c677d5e9
aQua <aQua@aQuaYi.com>
2018-08-21 20:14:06 +0800

    885 finish

--- Algorithms/0885.spiral-matrix-iii/spiral-matrix-iii.go
+++ Algorithms/0885.spiral-matrix-iii/spiral-matrix-iii.go
@@ -1,33 +1,31 @@
 package problem0885
 
-var xs = []int{0, 1, 0, -1}
-var ys = []int{1, 0, -1, 0}
-
-func spiralMatrixIII(R int, C int, r0 int, c0 int) [][]int {
+func spiralMatrixIII(R int, C int, r int, c int) [][]int {
 	countDown := R * C
 
 	res := make([][]int, 1, countDown)
-	res[0] = []int{r0, c0}
+	res[0] = []int{r, c}
 	countDown--
 
-	x, y := r0, c0
-
 	steps := 2
-	direct := 0
+	x, y, tmp := 0, 1, 0
 
 	for countDown > 0 {
-		dx, dy := xs[direct], ys[direct]
-		s := steps / 2
-		for s > 0 {
-			s--
-			x += dx
-			y += dy
-			if 0 <= x && x < R && 0 <= y && y < C {
-				res = append(res, []int{x, y})
+		for s := steps / 2; s > 0; s-- {
+			r += x
+			c += y
+			if 0 <= r && r < R && 0 <= c && c < C {
+				res = append(res, []int{r, c})
 				countDown--
 			}
 		}
-		direct = (direct + 1) % 4
+
+		// 方向变化这一段，很巧妙，可以参考
+		// https://leetcode.com/problems/spiral-matrix-iii/discuss/158970/C++JavaPython-112233-Steps
+
+		tmp = x
+		x = y
+		y = -tmp
 		steps++
 	}
 
