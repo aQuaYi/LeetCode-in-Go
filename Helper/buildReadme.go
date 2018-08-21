f5d5a06d74ad7df54491192331f11d59e6637bc2
aQua <aQua@aQuaYi.com>
2018-08-21 09:23:05 +0800

    helper 修改程序细节 1、不再添加 re 任务 2. 中间题号添加新题，也会添加 do 任务

--- Helper/buildReadme.go
+++ Helper/buildReadme.go
@@ -14,16 +14,6 @@ func buildReadme() {
 	lc := newLeetCode()
 	makeReadmeFile(lc)
 
-	// 放在 makefile 中实现这个功能了。
-	// // 利用 chrome 打开github.com/aQuaYi/LeetCode-in-Go
-	// link := "https://github.com/aQuaYi/LeetCode-in-Go#leetcode-%E7%9A%84-go-%E8%A7%A3%E7%AD%94"
-	// cmd := exec.Command("google-chrome", link)
-	// _, err := cmd.Output()
-	// if err != nil {
-	// 	panic(err.Error())
-	// }
-	// log.Println("正在打开项目主页")
-
 	log.Println("完成，重建 README 文档")
 }
 
