0aea46cbe05dcc7ec0b684183d6ec6c58a9d5294
aQua <aQua@aQuaYi.com>
2018-08-21 08:41:57 +0800

    882 完成美化

--- Algorithms/0882.reachable-nodes-in-subdivided-graph/reachable-nodes-in-subdivided-graph.go
+++ Algorithms/0882.reachable-nodes-in-subdivided-graph/reachable-nodes-in-subdivided-graph.go
@@ -12,31 +12,31 @@ func reachableNodes(edges [][]int, M int, N int) int {
 		nextTo[j] = append(nextTo[j], i)
 	}
 
-	// pq[x] = []int{r, i} 表示，
-	// 到达 i 节点时，还可以走 r 步
-	pq := make(PQ, 1, 1000)
+	// pq[x] = []int{m, i} 表示，
+	// 到达 i 节点时，还可以走 m 步
+	pq := make(PQ, 1, N*2)
 	pq[0] = []int{M, 0}
 
-	// pq 很有可能存在多个 {r1, i}, {r2, i}, {r3, i}
-	// 利用 pq 的特性，优先处理 r 值最大的情况。
+	// pq 很有可能存在多个 {m1, i}, {m2, i}, {m3, i}
+	// 利用 pq 的特性，优先处理 m 值最大的情况。
 	// 并在处理时，
 	// 在 seen 中标记已经处理过 i
-	// 在 maxMovesRemaining 中记录 r 的最大值
-	seen := [3001]bool{}
-	// maxMovesRemaining[3] = 10 表示，
+	// 在 maxRemainMoves 中记录 m 的最大值
+	seen := make([]bool, N)
+	// maxRemainMoves[3] = 10 表示，
 	// 通过最短的路径到达 3 节点时，还可以走 10 步
-	maxMovesRemaining := [3001]int{}
+	maxRemainMoves := make([]int, N)
 
 	res := 0
 	for len(pq) > 0 {
-		r := pq[0][0]
+		m := pq[0][0]
 		i := pq[0][1]
 		heap.Pop(&pq)
 		if seen[i] {
 			continue
 		}
 		seen[i] = true
-		maxMovesRemaining[i] = r
+		maxRemainMoves[i] = m
 		res++ // 收获 edge 端点 i
 
 		for _, j := range nextTo[i] {
@@ -44,21 +44,21 @@ func reachableNodes(edges [][]int, M int, N int) int {
 				continue
 			}
 			n := nodes[encode(i, j)]
-			jRemaining := r - n - 1
-			if jRemaining >= 0 {
+			jRemainMoves := m - n - 1
+			if jRemainMoves >= 0 {
 				// 如果可以在 M 步内到达 j
-				heap.Push(&pq, []int{jRemaining, j})
+				heap.Push(&pq, []int{jRemainMoves, j})
 			}
 		}
 	}
 
 	for _, e := range edges {
 		i, j, n := e[0], e[1], e[2]
-		// maxMovesRemaining[i] = ri 表示达到 i 点后，最多还可以走 ri 步
-		// maxMovesRemaining[j] = rj 表示达到 j 点后，最多还可以走 rj 步
-		// 如果 ri + rj >= n, 则 edge(i,j) 中间的 n 个点都可以被走到
-		// 否则 edge(i,j) 中只有 ri+rj 个点被走到
-		res += min(maxMovesRemaining[i]+maxMovesRemaining[j], n)
+		// maxRemainMoves[i] = mi 表示达到 i 点后，最多还可以走 mi 步
+		// maxRemainMoves[j] = mj 表示达到 j 点后，最多还可以走 mj 步
+		// 如果 mi + mj >= n, 则 edge(i,j) 中间的 n 个点都可以被走到
+		// 否则 edge(i,j) 中只有 mi+mj 个点被走到
+		res += min(maxRemainMoves[i]+maxRemainMoves[j], n)
 	}
 
 	return res
