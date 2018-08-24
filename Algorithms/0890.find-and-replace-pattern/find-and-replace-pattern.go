b0544a348fb92c8cac913bb1f783e60de89d75ce
aQua <aQua@aQuaYi.com>
2018-08-24 08:49:38 +0800

    890  修改了函数名称为动词

--- Algorithms/0890.find-and-replace-pattern/find-and-replace-pattern.go
+++ Algorithms/0890.find-and-replace-pattern/find-and-replace-pattern.go
@@ -1,31 +1,29 @@
 package problem0890
 
 func findAndReplacePattern(words []string, pattern string) []string {
-	np := normal(pattern)
+	np := normalize(pattern)
 	res := make([]string, 0, len(words))
 	for _, w := range words {
-		if normal(w) == np {
+		if normalize(w) == np {
 			res = append(res, w)
 		}
 	}
 	return res
 }
 
-// 把 s 标准化
-func normal(s string) string {
-	m := make(map[rune]byte, len(s))
-	for _, c := range s {
-		/**按照字符在 s 中第一次出现的顺序，依次映射到 a,b,c,... */
+// 把 str 标准化
+func normalize(str string) string {
+	m := make(map[rune]byte, len(str))
+	/**按照字符在 s 中第一次出现的顺序，依次映射到 a,b,c,... */
+	for _, c := range str {
 		if _, ok := m[c]; !ok {
 			m[c] = byte(len(m)) + 'a'
 		}
 	}
-
-	res := make([]byte, len(s))
-	for i, c := range s {
-		/**按照字符的映射关系，映射 s 到 res */
+	/**按照字符的映射关系，映射 s 到 res */
+	res := make([]byte, len(str))
+	for i, c := range str {
 		res[i] = m[c]
 	}
-
 	return string(res)
 }
