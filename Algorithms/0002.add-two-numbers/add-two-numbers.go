8a6a0b8018b263b60f7b46e65403bb2b71e8b14c
aQua <aQua@aQuaYi.com>
2018-08-26 11:47:49 +0800

    2 让程序更简洁

--- Algorithms/0002.add-two-numbers/add-two-numbers.go
+++ Algorithms/0002.add-two-numbers/add-two-numbers.go
@@ -1,69 +1,39 @@
 package problem0002
 
+import (
+	"github.com/aQuaYi/LeetCode-in-Go/kit"
+)
+
 // ListNode defines for singly-linked list.
 //  type ListNode struct {
 //      Val int
 //      Next *ListNode
 //  }
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
+type ListNode = kit.ListNode
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	result := &ListNode{}
-	temp := result
-	v, n := 0, 0
+	resPre := &ListNode{}
+	cur := resPre
+	carry := 0
 
-	for {
-		// 在当前位上进行加法运算
-		v, n = add(l1, l2, n)
-		temp.Val = v
+	for l1 != nil || l2 != nil || carry > 0 {
+		sum := carry
 
-		// 进入下一位
-		l1 = next(l1)
-		l2 = next(l2)
-		// 如果两个数的下一位都为nil，则结束按位相加的运算
-		if l1 == nil && l2 == nil {
-			break
+		if l1 != nil {
+			sum += l1.Val
+			l1 = l1.Next
 		}
 
-		// 为下一位运算准备节点
-		temp.Next = &ListNode{}
-		temp = temp.Next
+		if l2 != nil {
+			sum += l2.Val
+			l2 = l2.Next
+		}
+
+		carry = sum / 10
+
+		cur.Next = &ListNode{Val: sum % 10}
+		cur = cur.Next
 	}
 
-	// n == 1 说明，最后一次加运算进位了，需要再添加一个节点。
-	if n == 1 {
-		temp.Next = &ListNode{Val: n}
-	}
-
-	return result
-}
-
-// next 进入l的下一位。
-func next(l *ListNode) *ListNode {
-	if l != nil {
-		return l.Next
-	}
-	return nil
-}
-
-func add(n1, n2 *ListNode, i int) (v, n int) {
-	if n1 != nil {
-		v += n1.Val
-	}
-
-	if n2 != nil {
-		v += n2.Val
-	}
-
-	v += i
-
-	if v > 9 {
-		v -= 10
-		n = 1
-	}
-
-	return
+	return resPre.Next
 }
