b101081b29752723f83314b32fbe9a0aa0753c98
aQua <aQua@aQuaYi.com>
2018-08-27 17:17:24 +0800

    893 wrong answer

--- Algorithms/0893.groups-of-special-equivalent-strings/groups-of-special-equivalent-strings_test.go
+++ Algorithms/0893.groups-of-special-equivalent-strings/groups-of-special-equivalent-strings_test.go
@@ -14,6 +14,11 @@ var tcs = []struct {
 }{
 
 	{
+		[]string{"demp", "cfhp", "dzvf", "ggxe", "hkte", "clug", "nhgk", "hvwj", "zozr", "datm", "hisr", "gfry", "jknr", "laju", "emsf", "duwe", "ilta", "gjrd", "woaq", "zhdm", "ujmz", "jalu", "tkhe", "gexg", "hrsi", "tail", "ilta", "xegg", "srhi", "clug", "cgul", "gexg", "tehk", "ulcg", "xgge", "cgul", "hrsi", "aowq", "jalu", "laju", "vzdf", "gexg", "hpcf", "zhdm", "hfcp", "zhdm", "ulcg", "jalu", "ggxe", "gexg", "nkgh", "hrsi", "vfdz", "nkgh", "cgul", "hpcf", "hetk", "zrzo", "xegg", "nkgh", "srhi", "smef", "ulcg", "hrsi", "ggxe", "ggxe", "efsm", "ggxe", "jalu", "srhi", "dmzh", "laju", "zmdh", "sfem", "tehk", "srhi", "wqao", "gknh", "jalu", "iatl", "gexg", "ugcl", "nkgh", "hrsi", "srhi", "hkte", "gdrj", "zozr", "hisr", "sihr", "smef", "zmdh", "clug", "iatl", "cgul", "woaq", "jrnk", "sihr", "xegg", "luja"},
+		21,
+	},
+
+	{
 		[]string{"a", "b", "c", "a", "c", "c"},
 		3,
 	},
