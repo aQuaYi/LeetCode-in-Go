1cae5e64c70b7a4759cb8e3ad4dd7a2fdfc70014
aQua <aQua@aQuaYi.com>
2018-08-27 17:17:24 +0800

    893 wrong answer

--- Algorithms/0893.groups-of-special-equivalent-strings/groups-of-special-equivalent-strings.go
+++ Algorithms/0893.groups-of-special-equivalent-strings/groups-of-special-equivalent-strings.go
@@ -1,6 +1,21 @@
 package problem0893
 
 func numSpecialEquivGroups(A []string) int {
+	gMap := make(map[int]int, len(A))
+	for _, a := range A {
+		gMap[encode(a)]++
+	}
+	return len(gMap)
+}
 
-	return 0
+func encode(str string) int {
+	evens, odds := 0, 0
+	for i, r := range str {
+		if i%2 == 0 {
+			evens += int(r)
+		} else {
+			odds += int(r)
+		}
+	}
+	return evens<<16 | odds
 }
