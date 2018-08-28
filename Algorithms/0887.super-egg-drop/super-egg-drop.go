6fdd1115c76d4a73bfb609d128d758bbefb92c0a
aQua <aQua@aQuaYi.com>
2018-08-28 17:21:08 +0800

    887 accetped.

--- Algorithms/0887.super-egg-drop/super-egg-drop.go
+++ Algorithms/0887.super-egg-drop/super-egg-drop.go
@@ -1,6 +1,13 @@
 package problem0887
 
-func superEggDrop(K int, N int) int {
-
-	return 0
+func superEggDrop(K, N int) int {
+	dp := [10001][101]int{}
+	m := 0
+	for dp[m][K] < N {
+		m++
+		for k := 1; k <= K; k++ {
+			dp[m][k] = dp[m-1][k-1] + dp[m-1][k] + 1
+		}
+	}
+	return m
 }
