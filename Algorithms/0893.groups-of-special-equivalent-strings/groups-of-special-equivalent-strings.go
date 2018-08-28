a3d07b075035fe88050f92139eddeda0b2cf1a95
aQua <aQua@aQuaYi.com>
2018-08-28 11:52:33 +0800

    893 修改了变量名

--- Algorithms/0893.groups-of-special-equivalent-strings/groups-of-special-equivalent-strings.go
+++ Algorithms/0893.groups-of-special-equivalent-strings/groups-of-special-equivalent-strings.go
@@ -1,24 +1,23 @@
 package problem0893
 
 func numSpecialEquivGroups(A []string) int {
-	aSize := len(A[0])
-	oddBit := 1 << 31
-	gMap := make(map[[26]int]bool, len(A))
+	strSize := len(A[0])
+	groups := make(map[[26]int]bool, len(A))
 	for _, a := range A {
 		count := [26]int{}
 		i := 0
-		for i = 0; i+1 < aSize; i += 2 {
+		for i = 0; i+1 < strSize; i += 2 {
 			// 使用循环展开，避免了判断 i 的奇偶性的 mod 运算
 			// 还减短了关键路径的长度。
 			count[a[i]-'a']++
-			// 由于题目条件限制了 aSize <= 20
-			// 所以可以利用同一个数据同时统计奇偶位上字母
-			count[a[i+1]-'a'] += oddBit
+			// 由于题目条件限制了 strSize <= 20
+			// 所以可以在同一个数据同时统计奇偶位上字母
+			count[a[i+1]-'a'] += 100
 		}
-		if i < aSize {
+		if i < strSize {
 			count[a[i]-'a']++
 		}
-		gMap[count] = true
+		groups[count] = true
 	}
-	return len(gMap)
+	return len(groups)
 }
