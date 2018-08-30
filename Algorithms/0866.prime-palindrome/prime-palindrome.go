0c969ab7dfc1284bf695bcf3bb3253b836d9079f
aQua <aQua@aQuaYi.com>
2018-08-30 19:32:36 +0800

    866 重构了代码

--- Algorithms/0866.prime-palindrome/prime-palindrome.go
+++ Algorithms/0866.prime-palindrome/prime-palindrome.go
@@ -18,56 +18,51 @@ func primePalindrome(N int) int {
 		return 7
 	case N <= 11:
 		return 11
-	case N <= 101:
-		return 101
 	}
 
-	size := len(strconv.Itoa(N))
-
-	left := 1
-	for i := 0; i < size/2; i++ {
-		left *= 10
-	}
-	if size%2 == 1 {
-		left = N / left
-	}
-
-	// 当 size 为偶数时，
-	// 可知
-	// 要么，下个回文数的长度，也为偶数
-	// 要么，下个回文数是 10^size + 1
-	// 又
-	// 当回文数的长度为偶数时，肯定不是质数，因为 11 是因子
-	// 所以，直接从 10^size + 1 开始找起
+	/** 由于 11 是偶数长度的 palindrome 的因子
+	 *  所以答案一定是奇数长度的
+	 *  假设答案的形式如 lmr (Left + Middle + Right) */
+	lm := genLM(N)
 
 	for {
-		// 以 left 的末尾为镜子，制造出回文数
-		// 假设 left 看起来像 abc，则要找一个 n 看起来像 abcba
-		n := left
-		tmp := left / 10
-		for tmp > 0 {
-			n = n*10 + tmp%10
-			tmp /= 10
+		p := genPalindrome(lm)
+		if p >= N && isPrime(p) {
+			/** 需要验证 p >= N，是因为
+			 * 刚开始的 p 可能会比 N 小，
+			 * 例如，N = 98390 时，第一个 p 是 98389 */
+			return p
 		}
-
-		// 需要验证 n >= N，是因为
-		// 刚开始的 n 可能会比 N 小，
-		// 例如，N = 98390 时，第一个 n 是 98389
-		if n >= N && isPrime(n) {
-			return n
-		}
-
-		left++
+		lm++
 	}
 }
 
-/**
- * 此题要点：
- * 1. 利用数学知识简化需要处理的情况
- *    形如 abccba 等长度为偶数的回文数，都不是素数，因为有因子 11
- * 2. 把 res <= 101 这样的与后面处理方式不兼容的少数情况，单独处理。
- * 3. 没有把寻找 n 作为单独的函数，简化了判断条件。
- */
+// 根据 N 生成下一个 palindrome 的 left 和 middle 部分
+func genLM(N int) int {
+	size := len(strconv.Itoa(N))
+	base := int(math.Pow10(size / 2))
+
+	/** N 的长度是偶数， 把 base 当做 下一个答案的 lm
+	 * 下一个答案会是 10^size+1 */
+	lm := base
+	if size&1 == 1 {
+		/** N 的长度是奇数， lm 是 N 的左边和中间部分 */
+		lm = N / base
+	}
+
+	return lm
+}
+
+// 利用 left 和 middle 生成一个奇数位长度的 palindrome
+func genPalindrome(lm int) int {
+	res := lm
+	rr := lm / 10 // reverse right
+	for rr > 0 {
+		res = res*10 + rr%10
+		rr /= 10
+	}
+	return res
+}
 
 // https://blog.csdn.net/willduan1/article/details/50975381
 func isPrime(n int) bool {
@@ -85,3 +80,11 @@ func isPrime(n int) bool {
 	}
 	return true
 }
+
+/**
+ * 此题要点：
+ * 1. 利用数学知识简化需要处理的情况
+ *    形如 abccba 等长度为偶数的回文数，都不是素数，因为有因子 11
+ * 2. 把 res <= 101 这样的与后面处理方式不兼容的少数情况，单独处理。
+ * 3. 没有把寻找 n 作为单独的函数，简化了判断条件。
+ */
