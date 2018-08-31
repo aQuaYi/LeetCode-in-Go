3a005ad9e8df62a5d69f197fa690a684511dadbf
aQua <aQua@aQuaYi.com>
2018-08-31 19:53:51 +0800

    864 重构了代码

--- Algorithms/0864.shortest-path-to-get-all-keys/shortest-path-to-get-all-keys.go
+++ Algorithms/0864.shortest-path-to-get-all-keys/shortest-path-to-get-all-keys.go
@@ -10,28 +10,28 @@ func shortestPathAllKeys(grid []string) int {
 	// 使用 6 bit 的整数，就可以记录全部 2^6 = 64 种拥有钥匙的状态
 	// 所以，30×30×64 的数组，就可以记录所有的状态。
 	// 像这样知道范围的矩阵，尽量使用数组，可以快很多
+
 	hasSeen := [30][30][64]bool{}
-	// 同理，由于 x, y, keys 的范围都很小
-	// 可以用同一个 int 数字的不同的 bit 位段，来分别记录他们的值
-	// 具体的记录方式，参考下方的 encode 和 decode 函数
 	queue := make([]int, 1, m*n*4)
 
-	// 获取起点位置，和所有 key 的信息
 	allKeys := 0
+
+	// 获取起点位置，和所有 key 的信息
 	for i := 0; i < m; i++ {
 		for j := 0; j < n; j++ {
 			b := grid[i][j]
-			if b == '@' {
+			if 'a' <= b {
+				allKeys |= 1 << uint(b-'a')
+			} else if b == '@' {
 				hasSeen[i][j][0] = true
 				queue[0] = encode(i, j, 0)
-			} else if b >= 'a' {
-				allKeys |= 1 << uint(b-'a')
 			}
 		}
 	}
 
 	steps := 0
 
+	/**bfs */
 	for len(queue) > 0 {
 		size := len(queue)
 
@@ -58,8 +58,8 @@ func shortestPathAllKeys(grid []string) int {
 				}
 
 				nKeys := keys
-				if b >= 'a' {
-					// 记录刚刚遇到的钥匙
+				if 'a' <= b {
+					// 记录这个钥匙
 					nKeys |= 1 << uint(b-'a')
 				}
 
@@ -67,8 +67,8 @@ func shortestPathAllKeys(grid []string) int {
 					continue
 				}
 
-				queue = append(queue, encode(nx, ny, nKeys))
 				hasSeen[nx][ny][nKeys] = true
+				queue = append(queue, encode(nx, ny, nKeys))
 			}
 		}
 
@@ -82,7 +82,12 @@ func shortestPathAllKeys(grid []string) int {
 const (
 	xBits = 16
 	yBits = 8
+	mask  = 0xFF
 )
+
+/** 同理，由于 x, y, keys 的范围都很小
+ * 可以用同一个 int 数字的不同的 bit 位段，来分别记录他们的值
+ * 具体的记录方式，参考下方的 encode 和 decode 函数 */
 
 func encode(x, y, keys int) int {
 	return x<<xBits | y<<yBits | keys
@@ -90,7 +95,7 @@ func encode(x, y, keys int) int {
 
 func decode(state int) (x, y, keys int) {
 	x = state >> xBits
-	y = state >> yBits & 0xFF
-	keys = state & 0xFF
+	y = state >> yBits & mask
+	keys = state & mask
 	return
 }
