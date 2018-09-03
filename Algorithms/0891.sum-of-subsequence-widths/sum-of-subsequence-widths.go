078f45c0925ac8e7c16501212be4dc788ad7b678
aQua <aQua@aQuaYi.com>
2018-09-03 17:24:52 +0800

    891 finish

--- Algorithms/0891.sum-of-subsequence-widths/sum-of-subsequence-widths.go
+++ Algorithms/0891.sum-of-subsequence-widths/sum-of-subsequence-widths.go
@@ -8,14 +8,26 @@ const mod = 1e9 + 7
 
 func sumSubseqWidths(a []int) int {
 	sort.Ints(a)
-	res := 0
 
 	n := len(a)
-	c := 1
+	res := 0
+	times := 1
 	for i := 0; i < n; i++ {
-		res = (res + a[i]*c - a[n-i-1]*c) % mod
-		c = (c << 1) % mod
+		res += (a[i] - a[n-i-1]) * times
+		res %= mod
+		times = (times << 1) % mod
 	}
 
-	return res % mod
+	return res
 }
+
+/**
+ * https://leetcode.com/problems/sum-of-subsequence-widths/discuss/161267/C++Java1-line-Python-Sort-and-One-Pass
+ * For A[i]:
+ * There are i smaller numbers,
+ * so there are 2 ^ i sequences in which A[i] is maximum.
+ * we should do res += A[i] * (2 ^ i)
+ * There are n - i - 1 bigger numbers,
+ * so there are 2 ^ (n - i - 1) sequences in which A[i] is minimum.
+ * we should do res -= A[i] * 2 ^ (n - i - 1)
+ */
