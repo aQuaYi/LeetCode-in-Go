3f3ed13ed1c513791c117d3d53cda6f85f4728a7
aQua <aQua@aQuaYi.com>
2018-09-03 16:19:32 +0800

    891 wrong answer

--- Algorithms/0891.sum-of-subsequence-widths/sum-of-subsequence-widths.go
+++ Algorithms/0891.sum-of-subsequence-widths/sum-of-subsequence-widths.go
@@ -1,6 +1,23 @@
 package problem0891
 
-func sumSubseqWidths(A []int) int {
+import (
+	"sort"
+)
 
-	return 0
+const mod = 1e9 + 7
+
+func sumSubseqWidths(a []int) int {
+	sort.Ints(a)
+	res := 0
+
+	size := len(a)
+	for i := 0; i < size; i++ {
+		base := 1
+		for j := i + 1; j < size; j++ {
+			res += (a[j] - a[i]) * base
+			base *= 2
+		}
+		res %= mod
+	}
+	return res
 }
