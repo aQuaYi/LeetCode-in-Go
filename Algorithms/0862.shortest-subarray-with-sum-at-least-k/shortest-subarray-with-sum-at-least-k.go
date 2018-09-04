a9ebfd6bcc6ff8612a801b76919422042f74aa21
aQua <aQua@aQuaYi.com>
2018-09-04 23:54:19 +0800

    862 重构了代码 快了一点点，也好懂了一点点

--- Algorithms/0862.shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.go
+++ Algorithms/0862.shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.go
@@ -4,39 +4,44 @@ func shortestSubarray(a []int, K int) int {
 	size := len(a)
 
 	sums := make([]int, size+1)
+	s := 0
 	for i, n := range a {
 		if n == K {
 			return 1
 		}
-		sums[i+1] = sums[i] + n
+		s += n
+		sums[i+1] = s
 	}
 
-	res := size + 1
+	initialValue := size * 2
+	res := initialValue
 
-	deque := make([]int, size+2)
-	l, r := 0, -1
+	deque := make([]int, size+1)
+	deque[0] = 0
+	first, last := 0, 0
+	// deque[first:last+1] 中保存了以后会用到的 sums 中元素的索引号
 
-	for i := 0; i < size+1; i++ {
-		for r-l >= 0 && sums[i]-sums[deque[l]] >= K {
+	for i := 1; i <= size; i++ {
+		for first <= last && sums[i]-sums[deque[first]] >= K {
 			// 由于 i 递增
-			// 也许以后会有 j>i 使得 sums[j] - sums[deque[l]] >= K
-			// 但是由于 j>i, j-deque[l] > i-deque[l] 不会是更短的答案。
-			// 所以，可以把 deque[l] 删除
-			res = min(res, i-deque[l])
-			l++
+			// 即使以后会有 j>i 使得 sums[j] - sums[deque[first]] >= K
+			// 但是由于 j>i, 导致 j-deque[first] > i-deque[first] 不会是更短的答案。
+			// 所以，可以把 deque[first] 删除
+			res = min(res, i-deque[first])
+			first++
 		}
-		for r-l >= 0 && sums[i] <= sums[deque[r]] {
-			// 如果存在 j>i>deque[r] 使得 sums[j] - sums[deque[r]] >= K
-			// 由于 sums[deque[r]] >= sums[i] 则
-			// sums[j] - sums[i] >= K 一定成立，并且 j-i < j-deque[r]
-			// 所以，没有必要把 deque[r] 放入队列中
-			r--
+		for first <= last && sums[i] <= sums[deque[last]] {
+			// 如果存在 j>i>deque[last] 使得 sums[j] - sums[deque[last]] >= K
+			// 由于 sums[deque[last]] >= sums[i] 则
+			// sums[j] - sums[i] >= K 一定成立，并且 j-i < j-deque[last]
+			// 所以，没有必要把 deque[last] 放入队列中
+			last--
 		}
-		r++
-		deque[r] = i
+		last++
+		deque[last] = i
 	}
 
-	if res == size+1 {
+	if res == initialValue {
 		return -1
 	}
 	return res
