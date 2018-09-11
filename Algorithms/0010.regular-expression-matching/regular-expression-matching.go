d96baab1362a762be617b8e554ffb1d4483834de
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-09-11 23:13:02 +0800

    10 重构代码

--- Algorithms/0010.regular-expression-matching/regular-expression-matching.go
+++ Algorithms/0010.regular-expression-matching/regular-expression-matching.go
@@ -24,7 +24,10 @@ func isMatch(s, p string) bool {
 
 	for i := 0; i < sSize; i++ {
 		for j := 0; j < pSize; j++ {
-			if p[j] == '*' {
+			if p[j] == '.' || p[j] == s[i] {
+				/* p[j] 与 s[i] 可以匹配上，所以，只要前面匹配，这里就能匹配上 */
+				dp[i+1][j+1] = dp[i][j]
+			} else if p[j] == '*' {
 				/* 此时，p[j] 的匹配情况与 p[j-1] 的内容相关。 */
 				if p[j-1] != s[i] && p[j-1] != '.' {
 					/**
@@ -41,9 +44,6 @@ func isMatch(s, p string) bool {
 						dp[i+1][j] || /* "x*" 解释为 "x" */
 						dp[i][j+1] /* "x*" 解释为 "xx" */
 				}
-			} else if p[j] == '.' || p[j] == s[i] {
-				/* p[j] 与 s[i] 可以匹配上，所以，只要前面匹配，这里就能匹配上 */
-				dp[i+1][j+1] = dp[i][j]
 			}
 		}
 	}
