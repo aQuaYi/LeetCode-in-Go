36a3e0f9ad39c2a321bd7f0f2293b86e25600106
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-09-16 23:22:16 +0800

    894 finish. new record: 476ms.

--- Algorithms/0894.all-possible-full-binary-trees/all-possible-full-binary-trees.go
+++ Algorithms/0894.all-possible-full-binary-trees/all-possible-full-binary-trees.go
@@ -7,35 +7,30 @@ import (
 // TreeNode 是题目预定义的树结构
 type TreeNode = kit.TreeNode
 
-var forest = make([][]*TreeNode, 20)
+var forest = [10][]*TreeNode{[]*TreeNode{&TreeNode{Val: 0}}}
 
 func allPossibleFBT(N int) []*TreeNode {
 	if N%2 == 0 {
 		return nil
+	} else if forest[N/2] != nil {
+		return forest[N/2]
 	}
 
-	if forest[N] != nil {
-		return forest[N]
-	}
-
-	forest[1] = []*TreeNode{&TreeNode{Val: 0}}
-
-	for n := 3; n <= N; n += 2 {
-		trees := make([]*TreeNode, 0, n*2)
-		for les := 1; les <= n-2; les += 2 {
-			ris := n - 1 - les
-			for _, left := range allPossibleFBT(les) {
-				for _, right := range allPossibleFBT(ris) {
-					trees = append(trees, &TreeNode{
-						Val:   0,
-						Left:  left,
-						Right: right,
-					})
-				}
+	trees := make([]*TreeNode, 0, N*2)
+	for les := 1; les <= N-2; les += 2 {
+		ris := N - 1 - les
+		for _, left := range allPossibleFBT(les) {
+			for _, right := range allPossibleFBT(ris) {
+				trees = append(trees, &TreeNode{
+					Val:   0,
+					Left:  left,
+					Right: right,
+				})
 			}
 		}
-		forest[n] = trees
 	}
 
-	return forest[N]
+	forest[N/2] = trees
+
+	return forest[N/2]
 }
