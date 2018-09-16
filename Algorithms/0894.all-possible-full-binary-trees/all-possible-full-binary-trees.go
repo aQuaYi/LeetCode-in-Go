860fcf760443df56253df73cdfa1b36f094599db
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-09-16 23:00:41 +0800

    894 accepted.

--- Algorithms/0894.all-possible-full-binary-trees/all-possible-full-binary-trees.go
+++ Algorithms/0894.all-possible-full-binary-trees/all-possible-full-binary-trees.go
@@ -12,5 +12,26 @@ func allPossibleFBT(N int) []*TreeNode {
 		return nil
 	}
 
-	return nil
+	cache := [20][]*TreeNode{}
+
+	cache[1] = []*TreeNode{&TreeNode{Val: 0}}
+
+	for n := 3; n <= N; n += 2 {
+		tmp := make([]*TreeNode, 0, n*2)
+		for le := 1; le <= n-2; le += 2 {
+			ri := n - 1 - le
+			for _, left := range cache[le] {
+				for _, right := range cache[ri] {
+					tmp = append(tmp, &TreeNode{
+						Val:   0,
+						Left:  left,
+						Right: right,
+					})
+				}
+			}
+		}
+		cache[n] = tmp
+	}
+
+	return cache[N]
 }
