19a85f1d1a7d0f647a3ccc7c53727ae380722e72
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-09-18 22:25:57 +0800

    33 重构了变量名称

--- Algorithms/0033.search-in-rotated-sorted-array/search-in-rotated-sorted-array.go
+++ Algorithms/0033.search-in-rotated-sorted-array/search-in-rotated-sorted-array.go
@@ -3,31 +3,31 @@ package problem0033
 func search(nums []int, target int) int {
 	size := len(nums)
 
-	lo, hi := 0, size-1
-	for lo < hi {
-		mid := (lo + hi) / 2
-		if nums[mid] > nums[hi] {
-			lo = mid + 1
+	left, right := 0, size-1
+	for left < right {
+		mid := (left + right) / 2
+		if nums[mid] > nums[right] {
+			left = mid + 1
 		} else {
-			hi = mid
+			right = mid
 		}
 	}
 
 	/* lo = hi，是最小值的索引值 */
 
-	rotated := lo /* 数组旋转了的距离 */
+	rotated := left /* 数组旋转了的距离 */
 
-	lo, hi = 0, size-1
+	left, right = 0, size-1
 
-	for lo <= hi {
-		mid := (lo + hi) / 2
+	for left <= right {
+		mid := (left + right) / 2
 		/* nums 是 rotated，所以需要使用 rotatedMid 来获取 mid 的值 */
 		rotatedMid := (rotated + mid) % size
 		switch {
 		case nums[rotatedMid] > target:
-			hi = mid - 1
+			right = mid - 1
 		case nums[rotatedMid] < target:
-			lo = mid + 1
+			left = mid + 1
 		default:
 			return rotatedMid
 		}
