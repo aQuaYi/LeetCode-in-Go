795dfdab2da7cd4389533c428fef156df1442738
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-09-18 17:11:06 +0800

    896 accepted

--- Algorithms/0896.monotonic-array/monotonic-array.go
+++ Algorithms/0896.monotonic-array/monotonic-array.go
@@ -1,6 +1,25 @@
 package problem0896
 
 func isMonotonic(A []int) bool {
+	return isMonotoneIncreasing(A) || isMonotoneDecreasing(A)
+}
 
-	return false
+func isMonotoneIncreasing(A []int) bool {
+	size := len(A)
+	for i := 1; i < size; i++ {
+		if A[i-1] > A[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func isMonotoneDecreasing(A []int) bool {
+	size := len(A)
+	for i := 1; i < size; i++ {
+		if A[i-1] < A[i] {
+			return false
+		}
+	}
+	return true
 }
