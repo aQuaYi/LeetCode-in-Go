03d69e178e2736bb812f1ade04d6db6793c015a8
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-10-07 10:48:23 +0800

    897 added

---
+++ Algorithms/0897.increasing-order-search-tree/increasing-order-search-tree.go
@@ -0,0 +1,16 @@
+package problem0897
+
+import "github.com/aQuaYi/LeetCode-in-Go/kit"
+
+// TreeNode Definition for a binary tree node.
+// type TreeNode struct {
+//     Val int
+//     Left *TreeNode
+//     Right *TreeNode
+// }
+type TreeNode = kit.TreeNode
+
+func increasingBST(root *TreeNode) *TreeNode {
+
+	return nil
+}
