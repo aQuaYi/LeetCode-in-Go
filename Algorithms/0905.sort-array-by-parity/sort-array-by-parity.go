0680306e84057d53bd5d6b283fbc46a58729a02b
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-10-07 10:33:16 +0800

    905 finish

--- Algorithms/0905.sort-array-by-parity/sort-array-by-parity.go
+++ Algorithms/0905.sort-array-by-parity/sort-array-by-parity.go
@@ -1,17 +1,21 @@
 package problem0905
 
 func sortArrayByParity(a []int) []int {
-	i, j, size := 0, len(a)-1, len(a)
-	for i < j {
-		for i < size && a[i]%2 == 0 {
+	i, j := 0, len(a)-1
+	for {
+		for i < j && a[i]%2 == 0 {
 			i++
 		}
 		for i < j && a[j]%2 == 1 {
 			j--
 		}
+
 		if i < j {
 			a[i], a[j] = a[j], a[i]
+		} else {
+			break
 		}
+
 	}
 	return a
 }
