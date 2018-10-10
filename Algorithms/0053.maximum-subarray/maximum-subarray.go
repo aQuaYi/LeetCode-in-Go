42a4fac4bfd9f62096f19fd83016972c106883d8
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-10-10 22:34:45 +0800

    53 修改了变量名

--- Algorithms/0053.maximum-subarray/maximum-subarray.go
+++ Algorithms/0053.maximum-subarray/maximum-subarray.go
@@ -2,18 +2,19 @@ package problem0053
 
 func maxSubArray(nums []int) int {
 	size := len(nums)
-	tmp := nums[0]
-	res := tmp
+
+	sum := nums[0]
+	res := sum
 
 	for i := 1; i < size; i++ {
 		// 题目要求是连续的子数组，假设 maxSum = sum(nums[i:k])
 		// 可知必有 sum(nums[i:j]) >= 0 ,其中 i<j<k
-		if tmp < 0 {
-			tmp = nums[i]
+		if sum < 0 {
+			sum = nums[i]
 		} else {
-			tmp += nums[i]
+			sum += nums[i]
 		}
-		res = max(res, tmp)
+		res = max(res, sum)
 	}
 
 	return res
