bc89bf00d4710ef198ee01be920cf7f824eed740
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-10-15 17:07:13 +0800

    898 Limit Time Exceed

--- Algorithms/0898.bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.go
+++ Algorithms/0898.bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.go
@@ -1,5 +1,20 @@
 package problem0898
 
 func subarrayBitwiseORs(A []int) int {
-	return 0
+	size := len(A)
+	tmp := make(map[int]bool, size*2)
+	for i := 0; i < size; i++ {
+		for j := i + 1; j <= size; j++ {
+			tmp[bor(A[i:j])] = true
+		}
+	}
+	return len(tmp)
+}
+
+func bor(a []int) int {
+	res := 0
+	for i := range a {
+		res |= a[i]
+	}
+	return res
 }
