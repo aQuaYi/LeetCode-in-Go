3d4f295e05989e1e7294189c737893a7c0c85e55
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-10-16 17:27:33 +0800

    899 Time Limit Exceeded

--- Algorithms/0899.orderly-queue/orderly-queue.go
+++ Algorithms/0899.orderly-queue/orderly-queue.go
@@ -1,6 +1,31 @@
 package problem0899
 
-func orderlyQueue(S string, K int) string {
+func orderlyQueue(s string, k int) string {
+	seen := make(map[string]bool, len(s))
+	min := s
+	helper(s, k, &min, seen)
+	return min
+}
 
-	return ""
+func helper(s string, k int, min *string, seen map[string]bool) {
+	if seen[s] {
+		return
+	}
+
+	seen[s] = true
+	if *min > s {
+		*min = s
+	}
+
+	for i := 0; i < k; i++ {
+		helper(move(s, i), k, min, seen)
+	}
+}
+
+func move(s string, i int) string {
+	bytes := []byte(s)
+	b := bytes[i]
+	copy(bytes[i:], bytes[i+1:])
+	bytes[len(bytes)-1] = b
+	return string(bytes)
 }
