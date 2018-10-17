a7f78a1e9fa9907d3c841a986fe1cabd92d39baf
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-10-17 17:04:00 +0800

    900 added

---
+++ Algorithms/0900.rle-iterator/rle-iterator.go
@@ -0,0 +1,19 @@
+package problem0900
+
+// RLEIterator object will be instantiated and called as such:
+// obj := Constructor(A);
+// param_1 := obj.Next(n);
+type RLEIterator struct {
+}
+
+// Constructor is
+func Constructor(a []int) RLEIterator {
+
+	return RLEIterator{}
+}
+
+// Next is
+func (r *RLEIterator) Next(n int) int {
+
+	return 0
+}
