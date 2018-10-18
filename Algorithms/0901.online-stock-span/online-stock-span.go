a69ff83058600ee49ba1e3f2e711730cc2f2925e
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-10-18 08:44:28 +0800

    901 finish

--- Algorithms/0901.online-stock-span/online-stock-span.go
+++ Algorithms/0901.online-stock-span/online-stock-span.go
@@ -12,8 +12,8 @@ type StockSpanner struct {
 func Constructor() StockSpanner {
 	ps := make([]int, 1, 10000)
 	ds := make([]int, 1, 10000)
-	ps[0] = 100001 // more than max-price
-	ds[0] = -1     // the day before first-day
+	ps[0] = 1<<63 - 1 // more than max-price
+	ds[0] = -1        // the day before first-day
 	return StockSpanner{
 		prices: ps,
 		days:   ds,
@@ -26,10 +26,15 @@ func (s *StockSpanner) Next(price int) int {
 	// s.prices[i] 中保存了昨天的 price
 	// s.days[i] 中保存了昨天的 日期
 	today := s.days[i] + 1
-	for i >= 0 && s.prices[i] <= price {
-		// 深入 s.prices 栈，直到找到比 price 大的价格
+
+	for i >= 0 {
+		if s.prices[i] > price {
+			// 深入 s.prices 栈，直到找到比 price 大的价格
+			break
+		}
 		i--
 	}
+
 	res := today - s.days[i]
 	i++
 	s.prices = append(s.prices[:i], price)
