0042c6ee02193b4736c8f1f380c01e15a69b1660
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-10-24 08:44:19 +0800

    75 进一步简化

--- Algorithms/0075.sort-colors/sort-colors.go
+++ Algorithms/0075.sort-colors/sort-colors.go
@@ -2,14 +2,12 @@ package problem0075
 
 // 借鉴三路快排中的划分思路
 func sortColors(a []int) {
-	size := len(a)
-	if size == 0 {
-		return
-	}
-
-	i, j, k := 0, 0, size-1
+	i, j, k := 0, 0, len(a)-1
 
 	// for 循环中， nums[i:j] 中始终全是 1
+	// 循环结束后，
+	// nums[:i] 中全是 0
+	// nums[j:] 中全是 2
 	for j <= k {
 		switch a[j] {
 		case 0:
