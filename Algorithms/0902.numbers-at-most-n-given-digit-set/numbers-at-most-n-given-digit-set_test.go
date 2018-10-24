617453a08f42a08ef0ddb425af8366d142edee12
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-10-24 22:22:51 +0800

    902 wrong answer

--- Algorithms/0902.numbers-at-most-n-given-digit-set/numbers-at-most-n-given-digit-set_test.go
+++ Algorithms/0902.numbers-at-most-n-given-digit-set/numbers-at-most-n-given-digit-set_test.go
@@ -15,6 +15,18 @@ var tcs = []struct {
 }{
 
 	{
+		[]string{"7"},
+		8,
+		1,
+	},
+
+	{
+		[]string{"1", "3", "5", "7"},
+		1357,
+		112,
+	},
+
+	{
 		[]string{"1", "3", "5", "7"},
 		200,
 		36,
@@ -49,5 +61,62 @@ func Benchmark_atMostNGivenDigitSet(b *testing.B) {
 		for _, tc := range tcs {
 			atMostNGivenDigitSet(tc.D, tc.N)
 		}
+	}
+}
+
+func Test_empty(t *testing.T) {
+	type args struct {
+		size   int
+		length int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+
+		{
+			"4 个数字，组成最多 2 位数的组合数",
+			args{
+				4,
+				3,
+			},
+			20,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := empty(tt.args.size, tt.args.length); got != tt.want {
+				t.Errorf("empty() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_less(t *testing.T) {
+	type args struct {
+		size   int
+		length int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"4 个不同的数，组合 5 位数的组合数",
+			args{
+				4,
+				6, // 5+1
+			},
+			1024,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := less(tt.args.size, tt.args.length); got != tt.want {
+				t.Errorf("less() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
