fe7bfe3e52829d893dfbdb4f71a74b4c7d131808
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-10-24 17:22:58 +0800

    902 solving

--- Algorithms/0902.numbers-at-most-n-given-digit-set/numbers-at-most-n-given-digit-set.go
+++ Algorithms/0902.numbers-at-most-n-given-digit-set/numbers-at-most-n-given-digit-set.go
@@ -1,6 +1,28 @@
 package problem0902
 
-func atMostNGivenDigitSet(D []string, N int) int {
+import (
+	"math"
+	"strconv"
+)
 
+func atMostNGivenDigitSet(D []string, N int) int {
+	ns := strconv.Itoa(N)
+	return less(D, ns)
+}
+
+// 更高位小于 N 的最高位
+func less(D []string, N string) int {
+	a := float64(len(D))
+	s := float64(len(N))
+	res := (math.Pow(a, s) - a) / (a - 1)
+	return int(res)
+}
+
+// 更高位等于 N 的最高位
+func equal(D []string, N string) int {
+	i := 0
+	for i < len(D) && D[i] < N[1] {
+
+	}
 	return 0
 }
