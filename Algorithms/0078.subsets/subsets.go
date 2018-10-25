52e36f813e692734ab716c013901b0380607bad3
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-10-25 09:11:31 +0800

    78 添加了 benchmark 测试

--- Algorithms/0078.subsets/subsets.go
+++ Algorithms/0078.subsets/subsets.go
@@ -1,9 +1,5 @@
 package problem0078
 
-import (
-	"sort"
-)
-
 func subsets(nums []int) [][]int {
 	res := [][]int{}
 	rec(nums, []int{}, &res)
@@ -13,12 +9,15 @@ func subsets(nums []int) [][]int {
 func rec(nums, temp []int, res *[][]int) {
 	size := len(nums)
 	if size == 0 {
-		sort.Ints(temp)
+		// sort.Ints(temp)
 		*res = append(*res, temp)
 		return
 	}
 
-	rec(nums[:size-1], temp, res)
+	// 对于每个元素来说，要么存在于某个子集中，要么不存在
 
+	// 把 nums 中的最后一个元素，不放入 temp
+	rec(nums[:size-1], temp, res)
+	// 把 nums 中的最后一个元素，*放入* temp
 	rec(nums[:size-1], append([]int{nums[size-1]}, temp...), res)
 }
