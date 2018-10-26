e140eba0647b1acf23740a49056270e3479d0368
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-10-26 17:19:42 +0800

    914 accepted. 40ms

--- Algorithms/0914.x-of-a-kind-in-a-deck-of-cards/x-of-a-kind-in-a-deck-of-cards.go
+++ Algorithms/0914.x-of-a-kind-in-a-deck-of-cards/x-of-a-kind-in-a-deck-of-cards.go
@@ -1,6 +1,32 @@
 package problem0914
 
 func hasGroupsSizeX(deck []int) bool {
+	size := len(deck)
+	if size < 2 {
+		return false
+	}
 
-	return true
+	count := make(map[int]int, size)
+	for _, v := range deck {
+		count[v]++
+	}
+
+	g := count[deck[0]]
+
+	for _, c := range count {
+		g = gcd(g, c)
+	}
+
+	return g > 1
+}
+
+// 最大公约数
+func gcd(a, b int) int {
+	if a < b {
+		a, b = b, a
+	}
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
 }
