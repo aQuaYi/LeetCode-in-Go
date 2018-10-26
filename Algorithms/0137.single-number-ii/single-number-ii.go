6536e649e58265ba7875656eb9c15d4dac2e42e8
mlkr <gatsby.yun@gmail.com>
2018-10-26 11:58:57 +0800

    137 一个更好的解法

--- Algorithms/0137.single-number-ii/single-number-ii.go
+++ Algorithms/0137.single-number-ii/single-number-ii.go
@@ -1,19 +1,41 @@
 package problem0137
 
-// 假设 nums 中的数字为 32 bit
-func singleNumber(nums []int) int {
-	n := len(nums)
-	res := 0
+// 	 ref: https://cloud.tencent.com/developer/article/1131945
 
-	var i uint
-	for ; i < 64; i++ {
-		temp := 0
-		for j := 0; j < n; j++ {
-			temp += nums[j] >> i & 1 //首先把输入数字的第i位加起来。
-		}
-		temp %= 3        //然后求它们除以3的余数。
-		res |= temp << i //把二进制表示的结果转化为十进制表示的结果
+// 如果是出现两次的话，用一个bit就可以
+// 比如 b，初始为0
+// 当5第1次出现时， b=5
+// 当5第2次出现是， b清空为0，表示b可以去处理其他数字了
+// 所以，最后 如果 b !=0的话，b记录的就是只出现了一次的那个数字
+
+// ->公式就是 b = b xor i  或者 b = b^i
+
+// 那么，如果是三次的话，香浓定理，需要用2bits进行记录
+
+// 当5第一次出现的时候，b = 5, a=0,  b记录这个数字
+// 当5第二次出现的时候，b = 0, a=5， a记录了这个数字
+// 当5第三次出现的时候，b = 0, a=0， 都清空了，可以去处理其他数字了
+// 所以，如果有某个数字出现了1次，就存在b中，出现了两次，就存在a中，所以返回 a|b
+
+// 公式方面 ，上面两次的时候，b清空的公式是 b = b xor i
+//         而第三次时，b要等于零，而这时a是True，所以再 & 一个a的非就可以，b = b xor & ~a
+// -> b = b xor i & ~a
+//    a = a xor i & ~b
+
+// """
+// 	```
+// a = b = 0
+// for num in nums:
+//     b = b ^ num & ~a
+//     a = a ^ num & ~b
+// return a|b
+// 		```
+func singleNumber2(nums []int) int {
+	ones, twos := 0, 0
+	for i := 0; i < len(nums); i++ {
+		ones = (ones ^ nums[i]) & ^twos
+		twos = (twos ^ nums[i]) & ^ones
 	}
 
-	return res
+	return ones
 }
