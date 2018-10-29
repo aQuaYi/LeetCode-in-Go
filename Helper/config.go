1b9f057ba5a292e2b2a86ce7764d890426403906
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-10-29 11:24:43 +0800

    更新 README.md

--- Helper/config.go
+++ Helper/config.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/BurntSushi/toml"
@@ -22,12 +23,26 @@ type config struct {
 	EmailPassword string
 }
 
+func (c config) String() string {
+	format := "Username: %s, Password: %s, SMTP: %s, Port: %d, From: %s, To: %s, EmailPassword: %s "
+	return fmt.Sprintf(format,
+		c.Username,
+		c.Password,
+		c.SMTP,
+		c.Port,
+		c.From,
+		c.To,
+		c.EmailPassword)
+}
+
 func getConfig() *config {
 	cfg := new(config)
 
 	if _, err := toml.DecodeFile(configTOML, &cfg); err != nil {
 		log.Fatalf(err.Error())
 	}
+
+	log.Printf("get config: %s", cfg)
 
 	return cfg
 }
