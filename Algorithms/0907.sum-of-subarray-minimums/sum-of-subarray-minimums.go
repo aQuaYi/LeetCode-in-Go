ed50726758aae2819be334a02fdd8bea596ff179
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-11-08 21:20:59 +0800

    907 finish

--- Algorithms/0907.sum-of-subarray-minimums/sum-of-subarray-minimums.go
+++ Algorithms/0907.sum-of-subarray-minimums/sum-of-subarray-minimums.go
@@ -4,8 +4,8 @@ const modulo = 1e9 + 7
 
 // 求解 A 的所有子数组的最小值之和
 func sumSubarrayMins(A []int) int {
-	minOfA := 1 // 题目规定了 A[i]>=1
-	A = append(A, minOfA-1)
+	minOfA := 1             // 题目规定了 A[i]>=1
+	A = append(A, minOfA-1) // 加入低过 A 下限的值，是为了简化代码
 
 	size := len(A)
 	res := 0
@@ -13,15 +13,22 @@ func sumSubarrayMins(A []int) int {
 	s := new(stack)
 	s.push(0)
 
-	for i := 1; i < size; i++ {
-		for s.len() > 0 && A[s.top()] >= A[i] {
+	for k := 1; k < size; k++ {
+		for s.len() > 0 && A[s.top()] > A[k] {
 			j := s.pop()
-			res += (j - s.top()) * (i - j) * A[j]
-			// (j-s.top()) * (i-j) 是所有以 A[j] 为最小值的子数组的个数
-			// j-s.top() 表示这些子数组可以有多少个左端点
-			// i-j 表示这些子数组可以做多少个右端点
+			i := s.top()
+			res += (j - i) * (k - j) * A[j]
+			// 由题意可知，
+			// 为了找到全部以 A[j] 为最小值的子数组，
+			// 需要找到这些子数组左右端点的个数
+			// anyoneOf(A[i+1:j]) > A[j]，但是 A[i] < A[j]
+			// 	   所以，A[i+1:j+1] 中的数，都可以做左端点，共有 j-i 个
+			// anyoneOf(A[j+1:k]) > A[j]，但是 A[j] > A[k]
+			//     所以，A[j:k] 中的数，都可以做右端点，共有 k-j 个
+			// 两边的端点可以任意组合，所以是乘法关系
+			// (j-i) * (k-j) 是所有以 A[j] 为最小值的子数组的个数
 		}
-		s.push(i)
+		s.push(k)
 	}
 
 	return res % modulo
@@ -34,7 +41,6 @@ type stack []int
 func (s *stack) top() int {
 	size := len(*s)
 	if size == 0 {
-		// 返回 -1 是为了使得 "res += ..." 行的逻辑统一
 		return -1
 	}
 	return (*s)[size-1]
