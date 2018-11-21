b38108bcb1965fc02b559eacf0df54440cf4f511
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-11-21 21:27:04 +0800

    913 added

---
+++ Algorithms/0913.cat-and-mouse/cat-and-mouse.go
@@ -0,0 +1,6 @@
+package problem0913
+
+func catMouseGame(graph [][]int) int {
+
+	return 0
+}
---
+++ Algorithms/0913.cat-and-mouse/cat-and-mouse_test.go
@@ -0,0 +1,40 @@
+package problem0913
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	graph [][]int
+	ans   int
+}{
+
+	{
+
+		[][]int{{2, 5}, {3}, {0, 4, 5}, {1, 4, 5}, {2, 3}, {0, 2, 3}},
+		0,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_catMouseGame(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		ast.Equal(tc.ans, catMouseGame(tc.graph), "输入:%v", tc)
+	}
+}
+
+func Benchmark_catMouseGame(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			catMouseGame(tc.graph)
+		}
+	}
+}
