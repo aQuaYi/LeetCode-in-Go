fa8634fce13bcde90844b56c2eb71b451f42c1cb
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-12-03 17:20:56 +0800

    918 adding

--- Algorithms/0918.maximum-sum-circular-subarray/maximum-sum-circular-subarray.go
+++ Algorithms/0918.maximum-sum-circular-subarray/maximum-sum-circular-subarray.go
@@ -1,6 +1,23 @@
 package problem0918
 
 func maxSubarraySumCircular(A []int) int {
+	size := len(A)
+	res := -30001
+	tmp := 0
 
+	for i := 0; i < size; i++ {
+		for j := i + 1; j < i+size; j++ {
+if tmp < 0 {
+	tmp=
+}
+		}
+	}
 	return 0
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
