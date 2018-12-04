85ada1e3beae13033e6ee32cf3fcb940e5d2871a
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-12-04 17:19:29 +0800

    Helper 准备换一种方式获取题目内容。

--- Helper/problemReadme.go
+++ Helper/problemReadme.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"strings"
-
-	"github.com/chromedp/chromedp"
 )
 
 func creatREADME(p problem) {
@@ -54,47 +51,52 @@ func replaceCharacters(s string) string {
 func getDescription(url string) string {
 	log.Printf("准备访问 %s", url)
 
-	var err error
+	fmt.Println(string(getRaw(url)))
 
-	// create context
-	ctxt, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	// create chrome instance
-	// c, err := chromedp.New(ctxt, chromedp.WithLog(log.Printf))
-	c, err := chromedp.New(ctxt)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// run task list
-	var res string
-	err = c.Run(ctxt, text(url, &res))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// shutdown chrome
-	err = c.Shutdown(ctxt)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// wait for chrome to finish
-	err = c.Wait()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println("Desc:", res)
-
-	return res
+	return ""
 }
 
-func text(url string, res *string) chromedp.Tasks {
-	sel := `#question-detail-main-tabs > div.tab-pane__280T.css-c363ty-TabContent.e5i1odf5 > div > div.content__eAC7`
-	return chromedp.Tasks{
-		chromedp.Navigate(url),
-		chromedp.Text(sel, res, chromedp.NodeVisible, chromedp.BySearch),
-	}
-}
+// 	var err error
+
+// 	// create context
+// 	ctxt, cancel := context.WithCancel(context.Background())
+// 	defer cancel()
+
+// 	// create chrome instance
+// 	// c, err := chromedp.New(ctxt, chromedp.WithLog(log.Printf))
+// 	c, err := chromedp.New(ctxt)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	// run task list
+// 	var res string
+// 	err = c.Run(ctxt, text(url, &res))
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	// shutdown chrome
+// 	err = c.Shutdown(ctxt)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	// wait for chrome to finish
+// 	err = c.Wait()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	log.Println("Desc:", res)
+
+// 	return res
+// }
+
+// func text(url string, res *string) chromedp.Tasks {
+// 	sel := `#question-detail-main-tabs > div.tab-pane__280T.css-c363ty-TabContent.e5i1odf5 > div > div.content__eAC7`
+// 	return chromedp.Tasks{
+// 		chromedp.Navigate(url),
+// 		chromedp.Text(sel, res, chromedp.NodeVisible, chromedp.BySearch),
+// 	}
+// }
