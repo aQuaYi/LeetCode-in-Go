8b070f8ed59e3ca87d0a42a81e0809f5201a9b6c
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-12-07 21:21:26 +0800

    920 more clean. finish

--- Algorithms/0920.number-of-music-playlists/number-of-music-playlists.go
+++ Algorithms/0920.number-of-music-playlists/number-of-music-playlists.go
@@ -6,17 +6,26 @@ const mod = 1e9 + 7
 
 func numMusicPlaylists(N int, L int, K int) int {
 	dp := [101][101]int{}
-	// dp[n][l] 就是题目要求的那个意思
-	for n := K + 1; n <= N; n++ { // n <= K 没有意义，所以从 K+1 开始
-		for l := n; l <= L; l++ { // 按照题设定 N<=L， 所以， n<=l
-			if n == l || n == K+1 {
-				// 此时，做一个 n 个元素的全排列即可，不会再有其他情况
-				dp[n][l] = factorial(n)
-			} else {
-				dp[n][l] = (dp[n-1][l-1]*n + dp[n][l-1]*(n-K)) % mod
-				// dp[n-1][l-1]*n : 如果 n-1 首歌已经符合要求地放入了前 l-1 个位置，那么第 n 首歌不会与 [0,l) 行中的歌重复，可以直接放入第 l 行。另外，第 n 首歌，可以是 n 首歌中的任意一首，所以，需要 *n。
-				// dp[n][l-1]*(n-K) : 如果 n 首歌已经符合要求地放入了前 l-1 个位置，那么，放入第 l 行的歌不能与 [l-k,l) 行中的歌重复，只有 （n-k）首歌可以选，所以是 *(n-k)。
-			}
+	// dp[n][l] means n songs in l lines and K other songs has been played when some song wants to play again
+
+	// 0 <= K < N <= L <= 100
+	for n := K + 1; n <= N; n++ {
+		// when n songs in n lines,
+		//     just factorial n conditions
+		dp[n][n] = factorial(n)
+		// when n songs in n+ lines,
+		for l := n + 1; l <= L; l++ {
+			count := dp[n-1][l-1]*n + dp[n][l-1]*(n-K)
+			// dp[n-1][l-1] *n
+			//     dp[n-1][l-1] means n-1 songs has been in [1,l-1] lines,
+			//     want to add a new song which is not in n-1 songs to l line
+			//     the new song could be anyone of n songs, So *n
+			// dp[n][l-1] *(n-K)
+			//     dp[n][l-1] means n songs has been in [1,l-1] lines,
+			//     add one of n songs to l line
+			//     the song to add must be different with [l-k, l-1] line songs
+			//     only choose from (n-k) songs, So *(n-k)
+			dp[n][l] = count % mod
 		}
 	}
 	return dp[N][L]
@@ -27,5 +36,4 @@ func factorial(n int) int {
 		return 1
 	}
 	return (n * factorial(n-1)) % mod
-	// * / % 具有相同的优先级，按照从左往右计算
 }
