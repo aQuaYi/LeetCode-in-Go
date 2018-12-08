fe37e751ce52e66063b456e811392d70d37a258a
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-12-08 19:39:38 +0800

    922 accepted. 512ms > 408 ms

--- Algorithms/0922.sort-array-by-parity-ii/sort-array-by-parity-ii.go
+++ Algorithms/0922.sort-array-by-parity-ii/sort-array-by-parity-ii.go
@@ -1,6 +1,18 @@
 package problem0922
 
 func sortArrayByParityII(A []int) []int {
+	size := len(A)
+	res := make([]int, size)
+	odd, even := 0, 0
+	for _, a := range A {
+		if a%2 == 0 {
+			res[even*2] = a
+			even++
+		} else {
+			res[odd*2+1] = a
+			odd++
+		}
+	}
 
-	return []int{4, 5, 2, 7}
+	return res
 }
