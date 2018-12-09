9e48c6e7fd4035d21fdfda2a6b5807bf026e5ee5
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-12-09 18:25:08 +0800

    923 added

---
+++ Algorithms/0923.3sum-with-multiplicity/3sum-with-multiplicity.go
@@ -0,0 +1,6 @@
+package problem0923
+
+func threeSumMulti(A []int, target int) int {
+
+	return 0
+}
---
+++ Algorithms/0923.3sum-with-multiplicity/3sum-with-multiplicity_test.go
@@ -0,0 +1,45 @@
+package problem0923
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	A      []int
+	target int
+	ans    int
+}{
+
+	{
+		[]int{1, 1, 2, 2, 3, 3, 4, 4, 5, 5},
+		8,
+		20,
+	},
+
+	{
+		[]int{1, 1, 2, 2, 2, 2},
+		5,
+		12,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_threeSumMulti(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		ast.Equal(tc.ans, threeSumMulti(tc.A, tc.target), "输入:tc.A, tc.target", tc)
+	}
+}
+
+func Benchmark_threeSumMulti(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			threeSumMulti(tc.A, tc.target)
+		}
+	}
+}
