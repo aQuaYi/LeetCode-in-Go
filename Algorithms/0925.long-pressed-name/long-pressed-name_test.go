09b33fc6346be06fc0af910927553508e69b845c
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-12-11 17:08:43 +0800

    925 added

---
+++ Algorithms/0925.long-pressed-name/long-pressed-name_test.go
@@ -0,0 +1,57 @@
+package problem0925
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	name  string
+	typed string
+	ans   bool
+}{
+
+	{
+		"alex",
+		"aaleex",
+		true,
+	},
+
+	{
+		"saeed",
+		"ssaaedd",
+		false,
+	},
+
+	{
+		"leelee",
+		"lleeelee",
+		true,
+	},
+
+	{
+		"laiden",
+		"laiden",
+		true,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_isLongPressedName(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		ast.Equal(tc.ans, isLongPressedName(tc.name, tc.typed), "输入:%v", tc)
+	}
+}
+
+func Benchmark_isLongPressedName(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			isLongPressedName(tc.name, tc.typed)
+		}
+	}
+}
