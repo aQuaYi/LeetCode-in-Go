5bb6da0d67e58460fa33b6e26cc682dcd4b853f0
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-12-11 17:27:39 +0800

    925 accepted. 0ms

--- Algorithms/0925.long-pressed-name/long-pressed-name.go
+++ Algorithms/0925.long-pressed-name/long-pressed-name.go
@@ -1,6 +1,36 @@
 package problem0925
 
 func isLongPressedName(name string, typed string) bool {
+	if name == typed {
+		return true
+	}
 
-	return false
+	nameSize := len(name)
+	typedSize := len(typed)
+
+	nameBytes := []byte(name)
+	typedBytes := []byte(typed)
+
+	i, j := 0, 0
+	for i < nameSize {
+		count := 1
+		for i+1 < nameSize && nameBytes[i] == nameBytes[i+1] {
+			count++
+			i++
+		}
+
+		for j < typedSize && nameBytes[i] == typedBytes[j] {
+			count--
+			j++
+		}
+
+		i++
+
+		if count > 0 {
+			return false
+		}
+
+	}
+
+	return true
 }
