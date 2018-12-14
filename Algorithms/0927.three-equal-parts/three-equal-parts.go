e1cca934b4a7cd89d510671a5505b8972ff44ea0
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-12-14 16:32:15 +0800

    927 accepted. 112ms > 60ms

--- Algorithms/0927.three-equal-parts/three-equal-parts.go
+++ Algorithms/0927.three-equal-parts/three-equal-parts.go
@@ -1,6 +1,36 @@
 package problem0927
 
 func threeEqualParts(A []int) []int {
+	size := len(A)
+	indexs := make([]int, 0, size)
+	for i, n := range A {
+		if n == 1 {
+			indexs = append(indexs, i)
+		}
+	}
 
-	return nil
+	count := len(indexs)
+	if count == 0 {
+		return []int{0, size - 1}
+	}
+
+	if count%3 != 0 {
+		return []int{-1, -1}
+	}
+
+	i1 := count / 3
+	i2 := i1 * 2
+	s0, s1, s2 := indexs[0], indexs[i1], indexs[i2]
+
+	for s2 < size && A[s0] == A[s1] && A[s1] == A[s2] {
+		s0++
+		s1++
+		s2++
+	}
+
+	if s2 == size {
+		return []int{s0 - 1, s1}
+	}
+
+	return []int{-1, -1}
 }
