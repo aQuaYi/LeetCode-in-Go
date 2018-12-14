b75759746e54dc26bf80d6c7955436edbe75676c
Sandy <openset.wang@gmail.com>
2018-12-14 11:41:43 +0800

    Update valid-parentheses.go

--- Algorithms/0020.valid-parentheses/valid-parentheses.go
+++ Algorithms/0020.valid-parentheses/valid-parentheses.go
@@ -1,45 +1,26 @@
 package problem0020
 
 func isValid(str string) bool {
-	s := new(stack)
+    m := map[rune]rune{
+        '(': ')',
+        '[': ']',
+        '{': '}',
+    }
+    stack := make([]rune, len(s))
+    top := 0
+    for _, c := range s {
+        switch c {
+        case '(', '[', '{':
+            stack[top] = m[c]
+            top++
+        case ')', ']', '}':
+            if top > 0 && stack[top-1] == c {
+                top--
+            } else {
+                return false
+            }
+        }
+    }
 
-	for _, b := range str {
-		switch b {
-		case '(', '[', '{':
-			s.push(b)
-		case ')', ']', '}':
-			if r, ok := s.pop(); !ok || r != matching[b] {
-				// !ok 说明“（[{”的数量，小于")]}"的数量
-				return false
-			}
-		}
-	}
-
-	// len(*s) > 0 说明"([{"的数量，大于")]}"的数量
-	if len(*s) > 0 {
-		return false
-	}
-
-	return true
-}
-
-var matching = map[rune]rune{
-	')': '(',
-	']': '[',
-	'}': '{',
-}
-
-type stack []rune
-
-func (s *stack) push(b rune) {
-	*s = append(*s, b)
-}
-
-func (s *stack) pop() (rune, bool) {
-	if len(*s) > 0 {
-		res := (*s)[len(*s)-1]
-		*s = (*s)[:len(*s)-1]
-		return res, true
-	}
-	return 0, false
+    return top == 0
 }
