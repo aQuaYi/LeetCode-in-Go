a30d9edfef4d63094d5819a6bc71ca76d1795958
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-12-14 17:06:34 +0800

    927 添加了注释

--- Algorithms/0927.three-equal-parts/three-equal-parts.go
+++ Algorithms/0927.three-equal-parts/three-equal-parts.go
@@ -17,23 +17,34 @@ func threeEqualParts(A []int) []int {
 	count := len(indexs)
 
 	if count == 0 {
-		return []int{0, size - 1}
+		return []int{0, 2}
 	}
 
 	if count%3 != 0 {
+		// 此时，无法保证每个部分都有相同数目的 1
+		// 所以，不可能出现可行解
 		return noPossible
 	}
 
-	i1, i2 := count/3, count/3*2
-	s0, s1, s2 := indexs[0], indexs[i1], indexs[i2]
+	c1, c2 := count/3, count/3*2
+	s0, s1, s2 := indexs[0], indexs[c1], indexs[c2]
 
-	for s2 < size && A[s0] == A[s1] && A[s1] == A[s2] {
+	// 此时 A[s0:s1]， A[s1:s2] 和 A[s2:] 中分别有三分之一的 1
+	// 且
+	// A[s0]==A[s1]==A[s2]==1 三个部分的第一个 1 已经对齐
+	// 可以开始逐个比较
+	for s2 < size &&
+		A[s0] == A[s1] &&
+		A[s1] == A[s2] {
 		s0++
 		s1++
 		s2++
 	}
 
 	if s2 == size {
+		// 此时， 所有的 1 都被比较过了
+		// 且三段中的 1 分布相同。
+		// 没有被比较的元素都是 0
 		return []int{s0 - 1, s1}
 	}
 
