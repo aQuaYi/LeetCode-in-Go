32715cf5c84c6dabb308dd4d8c4597991b963760
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-12-15 21:41:50 +0800

    20 重构了变量名

--- Algorithms/0020.valid-parentheses/valid-parentheses.go
+++ Algorithms/0020.valid-parentheses/valid-parentheses.go
@@ -1,26 +1,26 @@
 package problem0020
 
 func isValid(s string) bool {
-    m := map[rune]rune{
-        '(': ')',
-        '[': ']',
-        '{': '}',
-    }
-    stack := make([]rune, len(s))
-    top := 0
-    for _, c := range s {
-        switch c {
-        case '(', '[', '{':
-            stack[top] = m[c]
-            top++
-        case ')', ']', '}':
-            if top > 0 && stack[top-1] == c {
-                top--
-            } else {
-                return false
-            }
-        }
-    }
+	opposite := map[rune]rune{
+		'(': ')',
+		'[': ']',
+		'{': '}',
+	}
+	stack := make([]rune, len(s))
+	top := 0
+	for _, c := range s {
+		switch c {
+		case '(', '[', '{':
+			stack[top] = opposite[c]
+			top++
+		case ')', ']', '}':
+			if top > 0 && stack[top-1] == c {
+				top--
+			} else {
+				return false
+			}
+		}
+	}
 
-    return top == 0
+	return top == 0
 }
