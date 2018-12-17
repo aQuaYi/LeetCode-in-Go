64661430081e0bbdba9aafc970a89e6089233510
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-12-17 13:55:01 +0800

    926 修改注释

--- Algorithms/0926.flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.go
+++ Algorithms/0926.flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.go
@@ -13,9 +13,10 @@ func minFlipsMonoIncr(S string) int {
 
 	res := size
 	for i := 0; i <= size; i++ {
-		// for S[i:] is all 1
+		// for S[:i] is all 0
 		// need flip 1 to 0 in S[:i],
 		//      there are count[i] 1s in S[i:]
+		// for S[i:] is all 1
 		// need flip 0 to 1 in S[i:],
 		//      length of S[i:] is size-i,
 		//      count of 1s in S[i:] is count[size]-count[i],
