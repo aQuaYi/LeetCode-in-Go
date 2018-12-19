7e5d5f0f3db02a9c9ab9db2a82a9c6e5714afadd
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-12-19 17:20:14 +0800

    937 accepted. 12ms > 8ms

--- Algorithms/0937.reorder-log-files/reorder-log-files.go
+++ Algorithms/0937.reorder-log-files/reorder-log-files.go
@@ -1,6 +1,36 @@
 package problem0937
 
-func reorderLogFiles(logs []string) []string {
+import (
+	"sort"
+	"strings"
+)
 
-	return nil
+func reorderLogFiles(logs []string) []string {
+	size := len(logs)
+
+	letters := make([][]string, 0, size)
+	digits := make([]string, 0, size)
+
+	for _, log := range logs {
+		ls := strings.SplitN(log, " ", 2)
+		b := ls[1][0]
+		if '0' <= b && b <= '9' {
+			digits = append(digits, log)
+		} else {
+			letters = append(letters, ls)
+		}
+	}
+
+	sort.Slice(letters, func(i int, j int) bool {
+		return letters[i][1] < letters[j][1]
+	})
+
+	res := make([]string, 0, size)
+	for _, ls := range letters {
+		res = append(res, strings.Join(ls, " "))
+	}
+
+	res = append(res, digits...)
+
+	return res
 }
