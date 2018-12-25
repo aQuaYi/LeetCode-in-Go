17354d434b746cd4b5fee837b7cc7e580417190c
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-12-25 17:24:37 +0800

    930 wrong answer

--- Algorithms/0930.binary-subarrays-with-sum/binary-subarrays-with-sum.go
+++ Algorithms/0930.binary-subarrays-with-sum/binary-subarrays-with-sum.go
@@ -1,6 +1,33 @@
 package problem0930
 
 func numSubarraysWithSum(A []int, S int) int {
+	size := len(A)
+	i, j, sum := 0, 0, 0
 
-	return 0
+	for j < size && sum < S {
+		sum += A[j]
+		j++
+	}
+
+	res := 0
+	for i < size && j <= size {
+		left := 1
+		for i < size && A[i] == 0 {
+			i++
+			left++
+		}
+
+		right := 1
+		for j < size && A[j] == 0 {
+			j++
+			right++
+		}
+
+		res += left * right
+
+		i++
+		j++
+	}
+
+	return res
 }
