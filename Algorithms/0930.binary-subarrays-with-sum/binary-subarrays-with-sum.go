5239db155bf85c7af8ffa160312237496de76e6e
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-12-25 17:11:02 +0800

    930 added

---
+++ Algorithms/0930.binary-subarrays-with-sum/binary-subarrays-with-sum.go
@@ -0,0 +1,6 @@
+package problem0930
+
+func numSubarraysWithSum(A []int, S int) int {
+
+	return 0
+}
---
+++ Algorithms/0930.binary-subarrays-with-sum/binary-subarrays-with-sum_test.go
@@ -0,0 +1,39 @@
+package problem0930
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	A   []int
+	S   int
+	ans int
+}{
+
+	{
+		[]int{1, 0, 1, 0, 1},
+		2,
+		4,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_numSubarraysWithSum(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		ast.Equal(tc.ans, numSubarraysWithSum(tc.A, tc.S), "输入:%v", tc)
+	}
+}
+
+func Benchmark_numSubarraysWithSum(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			numSubarraysWithSum(tc.A, tc.S)
+		}
+	}
+}
