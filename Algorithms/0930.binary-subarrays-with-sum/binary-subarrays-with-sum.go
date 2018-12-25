094223e3ace28750b1fc07d726639dd02fbe7521
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-12-25 21:41:07 +0800

    930 新解法

--- Algorithms/0930.binary-subarrays-with-sum/binary-subarrays-with-sum.go
+++ Algorithms/0930.binary-subarrays-with-sum/binary-subarrays-with-sum.go
@@ -1,60 +1,17 @@
 package problem0930
 
+// ref: https://leetcode.com/problems/binary-subarrays-with-sum/discuss/186683/C%2B%2BJavaPython-Straight-Forward
+
 func numSubarraysWithSum(A []int, S int) int {
-	if S == 0 {
-		return zero(A)
-	}
-	return nonZero(A, S)
-}
-
-func zero(A []int) int {
-	res, count := 0, 0
+	preSum, res := 0, 0
+	count := make([]int, len(A)+1)
+	count[0] = 1
 	for _, n := range A {
-		if n == 0 {
-			count++
-			res += count
-		} else {
-			count = 0
+		preSum += n
+		if preSum >= S {
+			res += count[preSum-S]
 		}
+		count[preSum]++
 	}
-	return res
-}
-
-func nonZero(A []int, S int) int {
-	size := len(A)
-
-	end, sum := 0, 0
-	for end < size && sum < S {
-		sum += A[end]
-		end++
-	}
-
-	if sum != S {
-		// A 中所有的 1 加起来都比 S 小
-		return 0
-	}
-
-	begin, res := 0, 0
-	for begin < size && end <= size {
-		left := 1
-		for begin < size && A[begin] == 0 {
-			begin++
-			left++
-		}
-
-		right := 1
-		for end < size && A[end] == 0 {
-			end++
-			right++
-		}
-
-		res += left * right
-
-		// 此时， A[begin] == A[end] == 1
-		begin++ // 相当于 sum--
-		end++   // 相当于 sum++
-		// 所以， sum 保持不变
-	}
-
 	return res
 }
