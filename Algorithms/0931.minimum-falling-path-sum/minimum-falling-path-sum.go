ac585fb8054c10f77c34a0b0b3ef8bfd98588a56
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-01-28 16:28:52 +0800

    931 Time Limit Exceeded

--- Algorithms/0931.minimum-falling-path-sum/minimum-falling-path-sum.go
+++ Algorithms/0931.minimum-falling-path-sum/minimum-falling-path-sum.go
@@ -1,6 +1,41 @@
 package problem0931
 
 func minFallingPathSum(A [][]int) int {
+	size := len(A)
+	res := 1 << 20
 
-	return 0
+	var falling func(int, int, int)
+	falling = func(i, j, sum int) {
+		if i == size {
+			res = min(res, sum)
+			return
+		}
+
+		sum += A[i][j]
+
+		x, y := i+1, max(j-1, 0)
+
+		for ; y < size && y <= j+1; y++ {
+			falling(x, y, sum)
+		}
+	}
+
+	for j := 0; j < size; j++ {
+		falling(0, j, 0)
+	}
+
+	return res
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
