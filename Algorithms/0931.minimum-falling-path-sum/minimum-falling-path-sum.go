8e321cb274ace689bc7afa84f8a80322a543a27c
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-01-28 16:51:22 +0800

    931 finish

--- Algorithms/0931.minimum-falling-path-sum/minimum-falling-path-sum.go
+++ Algorithms/0931.minimum-falling-path-sum/minimum-falling-path-sum.go
@@ -5,26 +5,25 @@ const maxSum = 10000
 func minFallingPathSum(A [][]int) int {
 	size := len(A)
 
-	recs := make([]int, size)
-	copy(recs, A[0])
+	dp := make([]int, size)
+	copy(dp, A[0])
 
 	for i := 1; i < size; i++ {
 		tmps := [100]int{}
 		for j := 0; j < size; j++ {
-			Aij := A[i][j]
-			tmp := maxSum
+			a, tmp := A[i][j], maxSum
 			l, r := max(j-1, 0), min(j+1, size-1)
 			for k := l; k <= r; k++ {
-				tmp = min(tmp, Aij+recs[k])
+				tmp = min(tmp, a+dp[k])
 			}
 			tmps[j] = tmp
 		}
-		recs = tmps[:size]
+		dp = tmps[:size]
 	}
 
 	res := maxSum
 	for i := 0; i < size; i++ {
-		res = min(res, recs[i])
+		res = min(res, dp[i])
 	}
 
 	return res
@@ -36,6 +35,7 @@ func max(a, b int) int {
 	}
 	return b
 }
+
 func min(a, b int) int {
 	if a < b {
 		return a
