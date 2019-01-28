169d304834cac446288f57cde0d9ce3cfd25455f
sulinehk <sulinehk@gmail.com>
2019-01-28 21:05:01 +0800

    0191: fast solutions

--- Algorithms/0191.number-of-1-bits/number-of-1-bits.go
+++ Algorithms/0191.number-of-1-bits/number-of-1-bits.go
@@ -2,11 +2,9 @@ package problem0191
 
 func hammingWeight(num uint32) int {
 	res := 0
-	for num > 0 {
-		if num&1 == 1 {
-			res++
-		}
-		num = num >> 1
+	for num != 0 {
+		res++
+		num &= num - 1
 	}
 	return res
 }
