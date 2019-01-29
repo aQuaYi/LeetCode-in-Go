87b29a34af7c341b66cfef02e090e945b0b1a902
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-01-29 10:51:23 +0800

    191 添加注释

--- Algorithms/0191.number-of-1-bits/number-of-1-bits.go
+++ Algorithms/0191.number-of-1-bits/number-of-1-bits.go
@@ -1,10 +1,13 @@
 package problem0191
 
 func hammingWeight(num uint32) int {
-	res := 0
+	count := 0
 	for num != 0 {
-		res++
+		count++
 		num &= num - 1
+		// assume num = 0b1000
+		// num-1 = 0b0111
+		// num & (num-1) = 0
 	}
-	return res
+	return count
 }
