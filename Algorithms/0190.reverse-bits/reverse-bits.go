58c5aa8cfe38c72c4c5b9f7072ca0efccdb57c95
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-01-29 11:09:32 +0800

    190 finish

--- Algorithms/0190.reverse-bits/reverse-bits.go
+++ Algorithms/0190.reverse-bits/reverse-bits.go
@@ -1,6 +1,10 @@
 package problem0190
 
 func reverseBits(num uint32) uint32 {
-
-	return 0
+	res := uint32(0)
+	for bits := 0; bits < 32; bits++ {
+		res = res<<1 + num&1
+		num >>= 1
+	}
+	return res
 }
