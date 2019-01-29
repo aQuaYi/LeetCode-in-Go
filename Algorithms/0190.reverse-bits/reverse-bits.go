993f94200957129f969dd90f3cac8285547f33e7
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-01-29 11:17:31 +0800

    190 finish

--- Algorithms/0190.reverse-bits/reverse-bits.go
+++ Algorithms/0190.reverse-bits/reverse-bits.go
@@ -1,10 +1,13 @@
 package problem0190
 
-func reverseBits(num uint32) uint32 {
-	res := uint32(0)
-	for bits := 0; bits < 32; bits++ {
-		res = res<<1 + num&1
-		num >>= 1
-	}
-	return res
+func reverseBits(n uint32) uint32 {
+	n = (n >> 16) | (n << 16)
+	n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8)
+	n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4)
+	n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2)
+	n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1)
+	return n
 }
+
+// for 8 bit binary number abcdefgh, the process is as follow:
+// abcdefgh -> efghabcd -> ghefcdab -> hgfedcba
