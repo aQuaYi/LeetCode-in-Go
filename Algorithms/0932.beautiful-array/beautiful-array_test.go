6d91fe52137ef558c10307038db4d24f42ec7b59
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-01-30 13:58:58 +0800

    932 finish

--- Algorithms/0932.beautiful-array/beautiful-array_test.go
+++ Algorithms/0932.beautiful-array/beautiful-array_test.go
@@ -1,6 +1,7 @@
 package problem0932
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,28 +9,63 @@ import (
 
 // tcs is testcase slice
 var tcs = []struct {
-	N   int
-	ans []int
+	N int
 }{
 
 	{
 		4,
-		[]int{2, 1, 4, 3},
 	},
 
 	{
 		5,
-		[]int{3, 1, 2, 5, 4},
+	},
+
+	{
+		10,
+	},
+
+	{
+		1000,
 	},
 
 	// 可以有多个 testcase
+}
+
+func isBeautiful(A []int) bool {
+	size := len(A)
+	for i := 0; i < size; i++ {
+		for j := i + 2; j < size; j++ {
+			AiPlusAj := A[i] + A[j]
+			for k := i + 1; k < j; k++ {
+				Ak := A[k]
+				if Ak*2 == AiPlusAj || Ak > size {
+					fmt.Println(A[i], A[k], A[j])
+					return false
+				}
+			}
+		}
+	}
+	return true
+}
+
+func Test_checkArray(t *testing.T) {
+	ast := assert.New(t)
+
+	results := [][]int{
+		{2, 1, 4, 5, 3},
+		{3, 5, 1, 2, 4},
+	}
+
+	for _, r := range results {
+		ast.True(isBeautiful(r))
+	}
 }
 
 func Test_beautifulArray(t *testing.T) {
 	ast := assert.New(t)
 
 	for _, tc := range tcs {
-		ast.Equal(tc.ans, beautifulArray(tc.N), "输入:%v", tc)
+		ast.True(isBeautiful(beautifulArray(tc.N)), "输入:%v", tc)
 	}
 }
 
