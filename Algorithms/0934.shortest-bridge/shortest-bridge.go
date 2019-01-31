d4dcc86ed70ff8a7e03fc7913561bf52913b1e79
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-01-31 14:19:42 +0800

    934 added

---
+++ Algorithms/0934.shortest-bridge/shortest-bridge.go
@@ -0,0 +1,6 @@
+package problem0934
+
+func shortestBridge(A [][]int) int {
+
+	return 0
+}
---
+++ Algorithms/0934.shortest-bridge/shortest-bridge_test.go
@@ -0,0 +1,60 @@
+package problem0934
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	A   [][]int
+	ans int
+}{
+
+	{
+		[][]int{
+			{0, 1},
+			{1, 0},
+		},
+		1,
+	},
+
+	{
+		[][]int{
+			{0, 1, 0},
+			{0, 0, 0},
+			{0, 0, 1},
+		},
+		2,
+	},
+
+	{
+		[][]int{
+			{1, 1, 1, 1, 1},
+			{1, 0, 0, 0, 1},
+			{1, 0, 1, 0, 1},
+			{1, 0, 0, 0, 1},
+			{1, 1, 1, 1, 1},
+		},
+		1,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_shortestBridge(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		ast.Equal(tc.ans, shortestBridge(tc.A), "输入:%v", tc)
+	}
+}
+
+func Benchmark_shortestBridge(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			shortestBridge(tc.A)
+		}
+	}
+}
