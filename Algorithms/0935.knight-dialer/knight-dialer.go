b4cfb9ab71872842e44efc84e9e1f0a58fc617a3
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-02-02 23:09:32 +0800

    935 accepted. 32ms

--- Algorithms/0935.knight-dialer/knight-dialer.go
+++ Algorithms/0935.knight-dialer/knight-dialer.go
@@ -1,6 +1,58 @@
 package problem0935
 
-func knightDialer(N int) int {
+const mod = 1e9 + 7
 
-	return 0
+var hop = [][]int{
+	{4, 6},    // from 0
+	{6, 8},    // from 1
+	{7, 9},    // from 2
+	{4, 8},    // from 3
+	{3, 9, 0}, // from 4
+	{},        // from 5
+	{0, 1, 7}, // from 6
+	{2, 6},    // from 7
+	{1, 3},    // from 8
+	{2, 4},    // from 9
+}
+
+func knightDialer(N int) int {
+	if N == 1 {
+		// now can chose 5
+		return 10
+	}
+
+	rec := [5000][10]int{}
+
+	res := 0
+	for i := 0; i < 10; i++ {
+		res += recur(N-1, i, &rec)
+	}
+
+	return res % mod
+}
+
+func recur(N, digit int, rec *[5000][10]int) int {
+	r := (*rec)[N][digit]
+	if r > 0 {
+		return r
+	}
+
+	h := hop[digit]
+
+	if N == 1 {
+		l := len(h)
+		(*rec)[N][digit] = l
+		return l
+	}
+
+	count := 0
+	for _, d := range h {
+		count += recur(N-1, d, rec)
+	}
+
+	count %= mod
+
+	(*rec)[N][digit] = count
+
+	return count
 }
