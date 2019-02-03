d36a24f7df0afed4090fe136d153f841311bae29
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-02-03 17:07:45 +0800

    936 accepted.

--- Algorithms/0936.stamping-the-sequence/stamping-the-sequence.go
+++ Algorithms/0936.stamping-the-sequence/stamping-the-sequence.go
@@ -1,18 +1,54 @@
 package problem0936
 
-import (
-	"container/list"
-	"strings"
-)
+import "strings"
 
 func movesToStamp(stamp string, target string) []int {
-	r := strings.Repeat("*", len(stamp))
-	l := list.New()
-	for strings.Contains(target, stamp) {
-		index := strings.Index(target, stamp)
-		l.PushBack(index)
-		target = strings.Replace(target, stamp, r, 1)
+
+	n, m, t, s := len(target), len(stamp), []byte(target), []byte(stamp)
+	res := make([]int, 0, n)
+
+	check := func(i int) bool {
+		changed := false
+		for j := 0; j < m; j++ {
+			if t[i+j] == '?' {
+				continue
+			}
+			if t[i+j] != s[j] {
+				return false
+			}
+			changed = true
+		}
+
+		if changed {
+			for k := i; k < i+m; k++ {
+				t[k] = '?'
+			}
+			res = append(res, i)
+		}
+		return changed
 	}
 
+	changed := true
+	for changed {
+		changed = false
+		for i := 0; i < n-m+1; i++ {
+			changed = changed || check(i)
+		}
+	}
+
+	reverse(res)
+
+	if string(t) == strings.Repeat("?", n) {
+		return res
+	}
 	return nil
+}
+
+func reverse(a []int) {
+	l, r := 0, len(a)-1
+	for l < r {
+		a[l], a[r] = a[r], a[l]
+		l++
+		r--
+	}
 }
