803e2aef9a70aff6d202e9f525c77ddefd1c7cc7
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-02-03 14:03:56 +0800

    984 wrong answer

--- Algorithms/0984.string-without-aaa-or-bbb/string-without-aaa-or-bbb.go
+++ Algorithms/0984.string-without-aaa-or-bbb/string-without-aaa-or-bbb.go
@@ -1,6 +1,27 @@
 package problem0984
 
-func strWithout3a3b(A int, B int) string {
+import "strings"
 
-	return ""
+func strWithout3a3b(A int, B int) string {
+	var sb strings.Builder
+	sb.Grow(A + B)
+
+	a, b := 'a', 'b'
+	if A < B {
+		A, B = B, A
+		a, b = b, a
+	}
+
+	for A > 0 || B > 0 {
+		for i := 2; i > 0 && A > 0; i-- {
+			sb.WriteRune(a)
+			A--
+		}
+		for i := 2; i > 0 && B > 0; i-- {
+			sb.WriteRune(b)
+			B--
+		}
+	}
+
+	return sb.String()
 }
