2d25f04e1016727a873c4357c752177ffbbc1870
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-02-05 13:13:08 +0800

    938 accepted. 112ms

---
+++ Algorithms/0938.range-sum-of-bst/README.md
@@ -0,0 +1,24 @@
+# [938. Range Sum of BST](https://leetcode.com/problems/range-sum-of-bst/)
+
+Given the `root` node of a binary search tree, return the sum of values of all nodes with value between `L` and `R` (inclusive).
+
+The binary search tree is guaranteed to have unique values.
+
+Example 1:
+
+```text
+Input: root = [10,5,15,3,7,null,18], L = 7, R = 15
+Output: 32
+```
+
+Example 2:
+
+```text
+Input: root = [10,5,15,3,7,13,18,1,null,6], L = 6, R = 10
+Output: 23
+```
+
+Note:
+
+1. The number of nodes in the tree is at most `10000`.
+1. The final answer is guaranteed to be less than `2^31`.
---
+++ Algorithms/0938.range-sum-of-bst/range-sum-of-bst_test.go
@@ -0,0 +1,53 @@
+package problem0938
+
+import (
+	"testing"
+
+	"github.com/aQuaYi/LeetCode-in-Go/kit"
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	root []int
+	L    int
+	R    int
+	ans  int
+}{
+
+	{
+		[]int{10, 5, 15, 3, 7, kit.NULL, 18},
+		7,
+		15,
+		32,
+	},
+
+	{
+		[]int{10, 5, 15, 3, 7, 13, 18, 1, kit.NULL, 6},
+		6,
+		10,
+		23,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_rangeSumBST(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		root := kit.Ints2TreeNode(tc.root)
+		ast.Equal(tc.ans, rangeSumBST(root, tc.L, tc.R), "输入:%v", tc)
+	}
+}
+
+func Benchmark_rangeSumBST(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			b.StopTimer()
+			root := kit.Ints2TreeNode(tc.root)
+			b.StartTimer()
+			rangeSumBST(root, tc.L, tc.R)
+		}
+	}
+}
