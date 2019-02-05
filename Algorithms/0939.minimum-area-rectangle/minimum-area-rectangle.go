5e93b27c3e70a6b36ba0a4add1071c22ef376f0f
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-02-05 14:18:53 +0800

    939 accepted. beats 16.67%

--- Algorithms/0939.minimum-area-rectangle/minimum-area-rectangle.go
+++ Algorithms/0939.minimum-area-rectangle/minimum-area-rectangle.go
@@ -6,30 +6,24 @@ const initialArea = 2000000000 // > 40000*40000
 
 func minAreaRect(points [][]int) int {
 	isExisted := make(map[string]bool, 500)
-	xs := make(map[int][]int, 500)
-	ys := make(map[int][]int, 500)
 	for _, p := range points {
 		px, py := p[0], p[1]
 		isExisted[convert(px, py)] = true
-		xs[px] = append(xs[px], py)
-		ys[py] = append(ys[py], px)
 	}
 
 	area := initialArea
 
-	for _, p := range points {
-		xld, yld := p[0], p[1]
-		for _, yrt := range xs[xld] {
-			for _, xrt := range ys[yld] {
-				if xld >= xrt || yld >= yrt {
-					continue
-				}
-				if isExisted[convert(xrt, yrt)] {
-					area = min(area, (xrt-xld)*(yrt-yld))
-				}
+	for _, pld := range points {
+		xld, yld := pld[0], pld[1]
+		for _, prt := range points {
+			xrt, yrt := prt[0], prt[1]
+			if xld >= xrt || yld >= yrt {
+				continue
+			}
+			if isExisted[convert(xrt, yld)] && isExisted[convert(xld, yrt)] {
+				area = min(area, (xrt-xld)*(yrt-yld))
 			}
 		}
-
 	}
 
 	if area == initialArea {
