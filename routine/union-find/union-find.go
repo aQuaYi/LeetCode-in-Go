a1f498cdebb98122afc3a66b2b91cb219ed897f5
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-02-07 13:57:17 +0800

    952 Time Limit Exceeded

--- routine/union-find/union-find.go
+++ routine/union-find/union-find.go
@@ -1,39 +1,13 @@
-package problem0952
-
-func largestComponentSize(A []int) int {
-	size := len(A)
-	u := newUnion(size)
-
-	for i := 0; i < size-1; i++ {
-		for j := i + 1; j < size; j++ {
-			a, b := A[i], A[j]
-			if gcd(a, b) > 1 {
-				u.union(i, j)
-			}
-		}
-	}
-
-	return u.max
-}
-
-// Greatest Common Divisor(GCD)
-func gcd(a, b int) int {
-	if a < b {
-		return gcd(b, a)
-	}
-	for b != 0 {
-		a, b = b, a%b
-	}
-	return a
-}
+package unionfind
 
 // Robert Sedgewick 算法（第4版） 1.5.2.7
 // union-find (加权 quick-union)，还作了路径压缩优化
 
+// union is ...
 type union struct {
-	id  []int // 父链接数组(由触点索引)
-	sz  []int // (由触点索引的) 各个根节点所对应的分量的大小
-	max int   // 连通分量的数量
+	id    []int // 父链接数组(由触点索引)
+	sz    []int // (由触点索引的) 各个根节点所对应的分量的大小
+	count int   // 连通分量的数量
 }
 
 func newUnion(N int) *union {
@@ -46,9 +20,9 @@ func newUnion(N int) *union {
 		sz[i] = 1
 	}
 	return &union{
-		id:  id,
-		sz:  sz,
-		max: 1,
+		id:    id,
+		sz:    sz,
+		count: N,
 	}
 }
 
@@ -75,13 +49,6 @@ func (u *union) union(p, q int) {
 	// 将小树的根节点连接到大树的根节点
 	u.id[i] = j
 	u.sz[j] += u.sz[i]
-	u.max = max(u.max, u.sz[j])
+	u.count--
 	return
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
 }
