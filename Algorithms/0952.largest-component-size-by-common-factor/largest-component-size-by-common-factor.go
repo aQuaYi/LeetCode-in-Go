0f883bacf83e9ec7b6bfdb48cf19a7dae1efc280
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-02-07 17:29:18 +0800

    952 accepted. 384ms

--- Algorithms/0952.largest-component-size-by-common-factor/largest-component-size-by-common-factor.go
+++ Algorithms/0952.largest-component-size-by-common-factor/largest-component-size-by-common-factor.go
@@ -1,46 +1,64 @@
 package problem0952
 
+var primes = []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313}
+
 func largestComponentSize(A []int) int {
 	size := len(A)
 	u := newUnion(size)
 
-	// 获取最大值
-	max := -1
-	for _, v := range A {
-		if max < v {
-			max = v
-		}
-	}
+	// // TODO: 删除此处内容
+	// sort.Ints(A)
 
-	halfMax := max / 2
+	// rec = map[factor]index
+	rec := make(map[int]int, size*2)
 
-	primes := []int{2, 3}
-	for i := 5; i <= halfMax; i += 2 {
-		isPrime := true
-		for _, p := range primes {
-			if i%p == 0 {
-				isPrime = false
-				break
-			}
-		}
-		if isPrime {
-			primes = append(primes, i)
-		}
-	}
-
-	for _, p := range primes {
-		i := 0
-		for i < size && A[i]%p != 0 {
-			i++
-		}
-
-		for j := i + 1; j < size; j++ {
-			if A[j]%p != 0 {
+	for i := 0; i < size; i++ {
+		a := A[i]
+		// for _, p := range primes {
+		// if p*p > a {
+		// break
+		// }
+		for p := 2; p*p <= a; p++ {
+			if a%p != 0 {
 				continue
 			}
+			if j, ok := rec[p]; ok {
+				u.union(i, j)
+			} else {
+				rec[p] = i
+			}
+			d := a / p
+			if j, ok := rec[d]; ok {
+				u.union(i, j)
+			} else {
+				rec[d] = i
+			}
+		}
+		if j, ok := rec[a]; ok {
 			u.union(i, j)
+		} else {
+			rec[a] = i
 		}
 	}
+
+	// fmt.Println("===")
+	// for k, v := range rec {
+	// 	if k == 29 {
+	// 		fmt.Println(k, v)
+	// }
+	// }
+
+	// for i := 0; i < size; i++ {
+	// 	star := ""
+	// 	if u.parents[i] != 1 {
+	// 		star = "*"
+	// 	}
+	// 	ej := ""
+	// 	if A[i]%29 == 0 {
+	// 		ej = "$"
+	// 	}
+	// 	fmt.Println(i, u.parents[i], A[i], star, ej)
+	// }
 
 	return u.max
 }
@@ -50,7 +68,7 @@ func largestComponentSize(A []int) int {
 
 type union struct {
 	parents []int // 父链接数组(由触点索引)
-	counts  []int // (由触点索引的) 各个根节点所对应的分量的大小
+	sizes   []int // (由触点索引的) 各个根节点所对应的分量的大小
 	max     int
 }
 
@@ -65,7 +83,7 @@ func newUnion(N int) *union {
 	}
 	return &union{
 		parents: parents,
-		counts:  counts,
+		sizes:   counts,
 		max:     1,
 	}
 }
@@ -88,14 +106,14 @@ func (u *union) union(p, q int) {
 		return
 	}
 
-	if u.counts[i] > u.counts[j] {
+	if u.sizes[i] > u.sizes[j] {
 		i, j = j, i
 	}
 
 	// 将小树的根节点连接到大树的根节点
 	u.parents[i] = j
-	u.counts[j] += u.counts[i]
-	u.max = max(u.max, u.counts[j])
+	u.sizes[j] += u.sizes[i]
+	u.max = max(u.max, u.sizes[j])
 	return
 }
 
