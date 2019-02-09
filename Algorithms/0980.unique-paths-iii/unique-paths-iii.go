174b99dcede4d21d8f14edcfb46073bb82a8bfd9
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-02-09 12:58:34 +0800

    980 程序不会终止

--- Algorithms/0980.unique-paths-iii/unique-paths-iii.go
+++ Algorithms/0980.unique-paths-iii/unique-paths-iii.go
@@ -1,6 +1,56 @@
 package problem0980
 
-func uniquePathsIII(grid [][]int) int {
+var dx = []int{0, 0, 1, -1}
+var dy = []int{1, -1, 0, 0}
 
-	return 0
+func uniquePathsIII(grid [][]int) int {
+	m, n := len(grid), len(grid[0])
+
+	queue := make([][2]int, 0, m*n*4)
+	paths := make([]int, 0, m*n*4)
+	target := 0
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			switch grid[i][j] {
+			case 0:
+				target |= 1 << uint(i*n+j)
+			case 1:
+				queue = append(queue, [2]int{i, j})
+				paths = append(paths, 1<<uint(i*n+j))
+			}
+		}
+	}
+
+	count := 0
+
+	// BFS
+	for len(queue) > 0 {
+		size := len(queue)
+		for s := 0; s < size; s++ {
+			x, y := queue[s][0], queue[s][1]
+			path := paths[s]
+			for k := 0; k < 4; k++ {
+				i, j := x+dx[k], y+dy[k]
+				if i < 0 || m <= i ||
+					j < 0 || n <= j ||
+					path&(1<<uint(i*n+j)) == 1 { // has Walked over
+					continue
+				}
+				switch grid[i][j] {
+				case 0:
+					queue = append(queue, [2]int{i, j})
+					paths = append(paths, path|(1<<uint(i*n+j)))
+				case 2:
+					if path == target {
+						count++
+					}
+				}
+			}
+		}
+		queue = queue[size:]
+		paths = paths[size:]
+	}
+
+	return count
 }
