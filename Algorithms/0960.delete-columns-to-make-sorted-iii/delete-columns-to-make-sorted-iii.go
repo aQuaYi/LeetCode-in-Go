c1bbc8b16f65bf5185b851792841e1d857517a62
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-02-10 19:08:59 +0800

    960 accepted.

--- Algorithms/0960.delete-columns-to-make-sorted-iii/delete-columns-to-make-sorted-iii.go
+++ Algorithms/0960.delete-columns-to-make-sorted-iii/delete-columns-to-make-sorted-iii.go
@@ -1,92 +1,39 @@
 package problem0960
 
-import "sort"
+// ref: https://leetcode.com/problems/delete-columns-to-make-sorted-iii/discuss/205679/C%2B%2BJavaPython-Maximum-Increasing-Subsequence
 
 func minDeletionSize(A []string) int {
-	size, wordSize := len(A), len(A[0])
-	keeps := getKeeps(A[0])
+	m, n := len(A), len(A[0])
+	res := n - 1
 
-	sort.Slice(keeps, func(i int, j int) bool {
-		return len(keeps[i]) > len(keeps[j])
-	})
+	dp := make([]int, n)
+	// dp[i] means the longest subsequence ends with i-th element.
+	// For all j < i, if A[][j] < A[][i], then dp[j] = max(dp[j], dp[i] + 1)
+	for i := 0; i < n; i++ {
+		dp[i] = 1
+	}
 
-	for _, k := range keeps {
-		isAllLexicographic := true
-		for i := 1; i < size; i++ {
-			if !isLexicographic(A[i], k) {
-				isAllLexicographic = false
-				break
+	for j := 0; j < n; j++ {
+		for i := 0; i < j; i++ {
+			k := 0
+			for ; k < m; k++ {
+				if A[k][i] > A[k][j] {
+					break
+				}
+			}
+			if k == m && dp[i]+1 > dp[j] {
+				dp[j] = dp[i] + 1
 			}
 		}
-		if isAllLexicographic {
-			return wordSize - len(k)
-		}
-	}
-	return wordSize - 1
-}
-
-func getKeeps(s string) [][]int {
-	size := len(s)
-	res := make([][]int, 1, size)
-	res[0] = make([]int, 1, size)
-
-	for i := 1; i < size; i++ {
-		isAdded := false
-		for j := range res {
-			if s[res[j][len(res[j])-1]] <= s[i] {
-				res[j] = append(res[j], i)
-				isAdded = true
-			}
-		}
-		if !isAdded {
-			t := make([]int, 1, size)
-			t[0] = i
-			res = append(res, t)
-		}
+		res = min(res, n-dp[j])
 	}
 
-	t := make([][]int, 0, size)
-	for i := 0; i < len(res); i++ {
-		for j := i + 1; j < len(res); j++ {
-			set := intersection(res[i], res[j])
-			if len(set) > 0 {
-				t = append(t, set)
-			}
-		}
-	}
-
-	return append(res, t...)
-
-}
-
-func isLexicographic(s string, keep []int) bool {
-	size := len(keep)
-	a := s[keep[0]]
-	for i := 1; i < size; i++ {
-		b := s[keep[i]]
-		if a > b {
-			return false
-		}
-		a = b
-	}
-	return true
-}
-
-func intersection(a, b []int) []int {
-	res := make([]int, 0, 100)
-	i, j := 0, 0
-	for i < len(a) && j < len(b) {
-		m, n := a[i], b[j]
-		switch {
-		case m < n:
-			i++
-		case m > n:
-			j++
-		default:
-			res = append(res, m)
-			i++
-			j++
-		}
-	}
 	return res
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
