cb855465873c5b8139eb87b4f8d971b7680253b2
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-02-22 21:48:36 +0800

    941 wrong answer

--- Algorithms/0941.valid-mountain-array/valid-mountain-array.go
+++ Algorithms/0941.valid-mountain-array/valid-mountain-array.go
@@ -1,6 +1,19 @@
 package problem0941
 
 func validMountainArray(A []int) bool {
+	size := len(A)
+	if size < 3 {
+		return false
+	}
 
-	return false
+	i := 1
+	for i < size && A[i-1] < A[i] {
+		i++
+	}
+
+	for i < size && A[i-1] > A[i] {
+		i++
+	}
+
+	return i == size
 }
