08f365ea56b60c288ab5d0db22d2889999ec37d9
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-02-28 20:57:52 +0800

    944 finish

--- Algorithms/0944.delete-columns-to-make-sorted/delete-columns-to-make-sorted.go
+++ Algorithms/0944.delete-columns-to-make-sorted/delete-columns-to-make-sorted.go
@@ -1,6 +1,15 @@
 package problem0944
 
 func minDeletionSize(A []string) int {
-
-	return 0
+	m, n := len(A), len(A[0])
+	res := 0
+	for j := 0; j < n; j++ {
+		for i := 1; i < m; i++ {
+			if A[i-1][j] > A[i][j] {
+				res++
+				break
+			}
+		}
+	}
+	return res
 }
