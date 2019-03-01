62159e2729700e322fe4d598e01f97bb39be7f87
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-03-01 20:48:14 +0800

    945 added

---
+++ Algorithms/0945.minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.go
@@ -0,0 +1,6 @@
+package problem0945
+
+func minIncrementForUnique(A []int) int {
+
+	return 0
+}
---
+++ Algorithms/0945.minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique_test.go
@@ -0,0 +1,42 @@
+package problem0945
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	A   []int
+	ans int
+}{
+
+	{
+		[]int{1, 2, 2},
+		1,
+	},
+
+	{
+		[]int{3, 2, 1, 2, 1, 7},
+		6,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_minIncrementForUnique(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		ast.Equal(tc.ans, minIncrementForUnique(tc.A), "输入:%v", tc)
+	}
+}
+
+func Benchmark_minIncrementForUnique(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			minIncrementForUnique(tc.A)
+		}
+	}
+}
