7ca3dfcb7e5d524c3358a68fbfdfd9ad77db9f55
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-03-01 20:57:05 +0800

    945 accepted. 484ms faster than 13.75%

--- Algorithms/0945.minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.go
+++ Algorithms/0945.minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.go
@@ -1,11 +1,15 @@
 package problem0945
 
 func minIncrementForUnique(A []int) int {
+	size := len(A)
+	if size == 0 {
+		return 0
+	}
+
 	rec := [80000]bool{}
 
 	rec[A[0]] = true
 
-	size := len(A)
 	res := 0
 
 	for i := 1; i < size; i++ {
