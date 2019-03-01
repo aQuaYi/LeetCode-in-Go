e425cd444c4ccd5cd5647a98e4b911cf1dcd0e1c
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-03-01 20:51:18 +0800

    945 accepted. 1092ms faster than 5.17%.

--- Algorithms/0945.minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.go
+++ Algorithms/0945.minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.go
@@ -1,6 +1,19 @@
 package problem0945
 
+import "sort"
+
 func minIncrementForUnique(A []int) int {
 
-	return 0
+	sort.Ints(A)
+
+	size := len(A)
+	res := 0
+	for i := 1; i < size; i++ {
+		for A[i-1] >= A[i] {
+			A[i]++
+			res++
+		}
+	}
+
+	return res
 }
