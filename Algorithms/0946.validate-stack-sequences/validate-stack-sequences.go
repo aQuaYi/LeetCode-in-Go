99223871522c4aae21e61687036a83fd3602cb94
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-03-04 20:58:36 +0800

    946 finish

--- Algorithms/0946.validate-stack-sequences/validate-stack-sequences.go
+++ Algorithms/0946.validate-stack-sequences/validate-stack-sequences.go
@@ -2,20 +2,22 @@ package problem0946
 
 func validateStackSequences(pushed, popped []int) bool {
 	size := len(pushed)
-	top, in, out := -1, 0, 0
 
-	for in < size {
-		if pushed[in] == popped[out] {
+	stack := make([]int, size)
+	top := -1
+
+	for in, out := 0, 0; in < size; in++ {
+		if pushed[in] != popped[out] {
+			top++
+			stack[top] = pushed[in]
+		} else {
 			out++
-			for top >= 0 && pushed[top] == popped[out] {
+			for top >= 0 && stack[top] == popped[out] {
 				top--
 				out++
 			}
-		} else {
-			top++
 		}
-		in++
 	}
 
-	return top == -1 && out == size
+	return top == -1
 }
