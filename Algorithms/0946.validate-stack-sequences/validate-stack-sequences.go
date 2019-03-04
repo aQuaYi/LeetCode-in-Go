9b9cab7a74c75abb34c33435c935aa02c0dbdc35
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-03-04 20:29:45 +0800

    946 accepted.

--- Algorithms/0946.validate-stack-sequences/validate-stack-sequences.go
+++ Algorithms/0946.validate-stack-sequences/validate-stack-sequences.go
@@ -1,6 +1,22 @@
 package problem0946
 
-func validateStackSequences(pushed []int, popped []int) bool {
+func validateStackSequences(pushed, popped []int) bool {
+	size := len(pushed)
+	in, out, s := 0, 0, 0
+	stack := make([]int, size)
 
-	return false
+	for in = 0; in < size && out < size; in++ {
+		if pushed[in] == popped[out] {
+			out++
+			for s > 0 && stack[s-1] == popped[out] {
+				s--
+				out++
+			}
+			continue
+		}
+		stack[s] = pushed[in]
+		s++
+	}
+
+	return s == 0 && out == size
 }
