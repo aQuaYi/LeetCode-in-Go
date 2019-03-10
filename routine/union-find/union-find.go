1a9810a70dd6327fa6b5f3bfdbb42af1110e6057
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-03-10 19:33:33 +0800

    routine 简化了 union found 算法

--- routine/union-find/union-find.go
+++ routine/union-find/union-find.go
@@ -1,54 +1,30 @@
 package unionfind
 
-// Robert Sedgewick 算法（第4版） 1.5.2.7
-// union-find (加权 quick-union)，还作了路径压缩优化
+// leetcode 中需要 union 的集合都太小了，最普通的集合算法，就够用了
 
-// union is ...
 type union struct {
-	id    []int // 父链接数组(由触点索引)
-	sz    []int // (由触点索引的) 各个根节点所对应的分量的大小
-	count int   // 连通分量的数量
+	parent []int
 }
 
-func newUnion(N int) *union {
-	id := make([]int, N)
-	for i := range id {
-		id[i] = i
-	}
-	sz := make([]int, N)
-	for i := range sz {
-		sz[i] = 1
+func newUnion(size int) *union {
+	parent := make([]int, size)
+	for i := range parent {
+		parent[i] = i
 	}
 	return &union{
-		id:    id,
-		sz:    sz,
-		count: N,
+		parent: parent,
 	}
 }
 
-func (u *union) isConnected(p, q int) bool {
-	return u.find(p) == u.find(q)
+func (u *union) find(i int) int {
+	if u.parent[i] == i {
+		return i
+	}
+	u.parent[i] = u.find(u.parent[i])
+	return u.parent[i]
 }
 
-func (u *union) find(p int) int {
-	// 跟随连接找到根节点
-	for p != u.id[p] {
-		p = u.id[p]
-	}
-	return p
-}
-
-func (u *union) union(p, q int) {
-	i, j := u.find(p), u.find(q)
-	if i == j {
-		return
-	}
-	if u.sz[i] > u.sz[j] {
-		i, j = j, i
-	}
-	// 将小树的根节点连接到大树的根节点
-	u.id[i] = j
-	u.sz[j] += u.sz[i]
-	u.count--
-	return
+func (u *union) union(x, y int) {
+	xp, yp := u.find(x), u.find(y)
+	u.parent[yp] = xp
 }
