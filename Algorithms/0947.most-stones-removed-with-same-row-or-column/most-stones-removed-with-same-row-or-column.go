8d1245ecdf01f694324afced03688a1c622c9424
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-03-10 19:23:05 +0800

    947 添加注释中

--- Algorithms/0947.most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.go
+++ Algorithms/0947.most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.go
@@ -1,33 +1,36 @@
 package problem0947
 
+// 如果把相同行或者列上的点，看作是联通的
+// 那么本题的本质就是，每个联通区域只保留一个点，则可以删除多少个点
+
 func removeStones(stones [][]int) int {
 	u := newUnion(20000)
-
 	for _, s := range stones {
+		// 把一个点的 x 和 y 坐标进行连通，可以把复杂度从 O^2 降低到 O
+		// 当为了避免坐标轴的重叠，需要把每个 y 坐标的值 +10000
 		u.union(s[0], s[1]+10000)
 	}
 
-	roots := make(map[int]int, 1000)
-
+	keeps := make(map[int]int, 1000)
 	for _, s := range stones {
 		root := u.find(s[0])
-		roots[root]++
+		keeps[root]++
 	}
 
-	return len(stones) - len(roots)
+	return len(stones) - len(keeps)
 }
 
 type union struct {
-	parent []int // 父链接数组(由触点索引)
+	parent []int
 }
 
 func newUnion(size int) *union {
-	p := make([]int, size)
-	for i := range p {
-		p[i] = i
+	parent := make([]int, size)
+	for i := range parent {
+		parent[i] = i
 	}
 	return &union{
-		parent: p,
+		parent: parent,
 	}
 }
 
