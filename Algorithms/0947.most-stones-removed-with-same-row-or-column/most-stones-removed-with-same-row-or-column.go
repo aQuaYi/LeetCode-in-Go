ef5d9fc7fa8128411bcd49e62ae049789a4b1c1a
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-03-10 16:54:27 +0800

    947 wrong answer

--- Algorithms/0947.most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.go
+++ Algorithms/0947.most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.go
@@ -2,13 +2,12 @@ package problem0947
 
 func removeStones(stones [][]int) int {
 	res := 0
-	hasRow := [1000]bool{}
+	hasRow := [10000]bool{}
 	hasCol := [10000]bool{}
 	for _, s := range stones {
 		i, j := s[0], s[1]
 		if hasRow[i] || hasCol[j] {
 			res++
-			continue
 		}
 		hasRow[i] = true
 		hasCol[j] = true
