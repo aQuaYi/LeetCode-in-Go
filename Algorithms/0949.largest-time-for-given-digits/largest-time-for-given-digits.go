75ce03988387d23f3bee1509a226b915dba9b375
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-03-12 21:26:40 +0800

    949 accepted.

--- Algorithms/0949.largest-time-for-given-digits/largest-time-for-given-digits.go
+++ Algorithms/0949.largest-time-for-given-digits/largest-time-for-given-digits.go
@@ -1,11 +1,47 @@
 package problem0949
 
-func largestTimeFromDigits(A []int) string {
+import "fmt"
 
-	return ""
+var permutations = [][]int{
+	{0, 1, 2, 3},
+	{0, 1, 3, 2},
+	{0, 2, 1, 3},
+	{0, 2, 3, 1},
+	{0, 3, 1, 2},
+	{0, 3, 2, 1},
+	{1, 0, 2, 3},
+	{1, 0, 3, 2},
+	{1, 2, 0, 3},
+	{1, 2, 3, 0},
+	{1, 3, 0, 2},
+	{1, 3, 2, 0},
+	{2, 0, 1, 3},
+	{2, 0, 3, 1},
+	{2, 1, 0, 3},
+	{2, 1, 3, 0},
+	{2, 3, 1, 0},
+	{2, 3, 0, 1},
+	{3, 0, 2, 1},
+	{3, 0, 1, 2},
+	{3, 1, 0, 2},
+	{3, 1, 2, 0},
+	{3, 2, 0, 1},
+	{3, 2, 1, 0},
 }
 
-func dfs(A, temp []int, isUsed []bool, res []int) {
-
-	return
+func largestTimeFromDigits(A []int) string {
+	h, m := -1, -1
+	res := ""
+	for _, p := range permutations {
+		a, b, c, d := A[p[0]], A[p[1]], A[p[2]], A[p[3]]
+		ab, cd := a*10+b, c*10+d
+		if ab > 23 ||
+			cd > 59 ||
+			ab*60+cd <= h*60+m {
+			continue
+		}
+		h, m = ab, cd
+		res = fmt.Sprintf("%02d:%02d", ab, cd)
+	}
+	return res
 }
