bc67118469aed4b4042de77bdff7adb2c7aa84c6
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-03-13 21:39:49 +0800

    950 finish

--- Algorithms/0950.reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.go
+++ Algorithms/0950.reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.go
@@ -11,17 +11,14 @@ func deckRevealedIncreasing(deck []int) []int {
 
 	sort.Ints(deck)
 
-	t := make([]int, 1, size*2)
-
-	t[0] = deck[size-1]
+	t := make([]int, 0, size*2)
 
 	b := 0
-	for i := size - 2; i > 0; i-- {
+	for i := size - 1; i > 0; i-- {
 		t = append(t, deck[i])
 		t = append(t, t[b])
 		b++
 	}
-
 	t = append(t, deck[0])
 
 	for i, j := b, len(t)-1; i < j; i, j = i+1, j-1 {
