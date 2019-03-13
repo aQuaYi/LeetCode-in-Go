9813bcdec7d031e97c3b78dad5a79fc281de5fd2
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-03-13 21:45:40 +0800

    950 add test case

--- Algorithms/0950.reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.go
+++ Algorithms/0950.reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.go
@@ -14,12 +14,13 @@ func deckRevealedIncreasing(deck []int) []int {
 	t := make([]int, 0, size*2)
 
 	b := 0
-	for i := size - 1; i > 0; i-- {
+	for i := size - 1; i >= 0; i-- {
 		t = append(t, deck[i])
-		t = append(t, t[b])
-		b++
+		if i > 0 {
+			t = append(t, t[b])
+			b++
+		}
 	}
-	t = append(t, deck[0])
 
 	for i, j := b, len(t)-1; i < j; i, j = i+1, j-1 {
 		t[i], t[j] = t[j], t[i]
