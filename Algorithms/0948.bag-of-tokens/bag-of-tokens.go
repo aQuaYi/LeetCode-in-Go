a255a51c90c4ae198aadfc2ac96d36d5372c289f
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-03-15 22:59:00 +0800

    948 more clean

--- Algorithms/0948.bag-of-tokens/bag-of-tokens.go
+++ Algorithms/0948.bag-of-tokens/bag-of-tokens.go
@@ -6,6 +6,7 @@ import "sort"
 
 func bagOfTokensScore(tokens []int, P int) int {
 	sort.Ints(tokens)
+
 	maxScore, score := 0, 0
 	i, j := 0, len(tokens)-1
 
@@ -13,7 +14,12 @@ func bagOfTokensScore(tokens []int, P int) int {
 		if P >= tokens[i] {
 			P -= tokens[i]
 			score++
-			maxScore = max(maxScore, score) // record the largest score
+			maxScore = score
+			// 因为 tokens 是升序排列
+			// 所以，卖了 tokens[j] 后，肯定应该买得起 tokens[i]
+			// 除非 i==j，会导致 score--，比真正的结果小 1
+			// 于是，每次 score++ 后，score 肯定是最大值。
+			// 使用 maxScore 记录一下，避免错误的结果。
 			i++
 		} else if score > 0 {
 			P += tokens[j]
@@ -25,11 +31,4 @@ func bagOfTokensScore(tokens []int, P int) int {
 	}
 
 	return maxScore
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
 }
