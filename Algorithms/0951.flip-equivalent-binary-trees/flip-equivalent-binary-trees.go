3992be7e34d6f28fa883b3561de7f1a0b5a3eb83
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-03-15 20:40:06 +0800

    951 finish

--- Algorithms/0951.flip-equivalent-binary-trees/flip-equivalent-binary-trees.go
+++ Algorithms/0951.flip-equivalent-binary-trees/flip-equivalent-binary-trees.go
@@ -5,19 +5,19 @@ import "github.com/aQuaYi/LeetCode-in-Go/kit"
 // TreeNode is pre-defined in kit library
 type TreeNode = kit.TreeNode
 
-func flipEquiv(root1, root2 *TreeNode) bool {
-	if root1 == nil && root2 == nil {
+func flipEquiv(r1, r2 *TreeNode) bool {
+	if r1 == nil && r2 == nil {
 		return true
 	}
 
-	if (root1 != nil && root2 == nil) ||
-		(root1 == nil && root2 != nil) ||
-		root1.Val != root2.Val {
+	if (r1 != nil && r2 == nil) ||
+		(r1 == nil && r2 != nil) ||
+		r1.Val != r2.Val {
 		return false
 	}
 
-	if (flipEquiv(root1.Left, root2.Left) && flipEquiv(root1.Right, root2.Right)) ||
-		(flipEquiv(root1.Left, root2.Right) && flipEquiv(root1.Right, root2.Left)) {
+	if (flipEquiv(r1.Left, r2.Left) && flipEquiv(r1.Right, r2.Right)) ||
+		(flipEquiv(r1.Left, r2.Right) && flipEquiv(r1.Right, r2.Left)) {
 		return true
 	}
 
