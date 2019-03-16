5fc27f4634c25bcf2f665a9c4f8b836802f409b3
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-03-16 19:19:23 +0800

    954 wrong answer

--- Algorithms/0954.array-of-doubled-pairs/array-of-doubled-pairs.go
+++ Algorithms/0954.array-of-doubled-pairs/array-of-doubled-pairs.go
@@ -1,6 +1,48 @@
 package problem0954
 
-func canReorderDoubled(A []int) bool {
+import "sort"
 
-	return false
+func canReorderDoubled(A []int) bool {
+	size := len(A)
+	A1, A2 := make([]int, 0, size), make([]int, 0, size)
+	for _, v := range A {
+		if v >= 0 {
+			A1 = append(A1, v)
+		} else {
+			A2 = append(A2, -v)
+		}
+	}
+
+	return isPossible(A1) && isPossible(A2)
+}
+
+func isPossible(A []int) bool {
+	size := len(A)
+
+	if size%2 == 1 {
+		return false
+	}
+
+	sort.Ints(A)
+
+	i, j := 0, 1
+	count := size / 2
+	for j < size {
+		for A[i] < 0 {
+			i++
+		}
+		double := A[i] * 2
+		i++
+		for j < size && A[j] < double {
+			j++
+		}
+		if j == size || A[j] != double {
+			return false
+		}
+		A[j] = -1
+		j++
+		count--
+	}
+
+	return count == 0
 }
