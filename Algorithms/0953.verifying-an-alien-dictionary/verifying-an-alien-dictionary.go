8f39a2aada94df70fbcf46784c2a343beed3a1b5
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-03-16 16:42:07 +0800

    853 finish

--- Algorithms/0953.verifying-an-alien-dictionary/verifying-an-alien-dictionary.go
+++ Algorithms/0953.verifying-an-alien-dictionary/verifying-an-alien-dictionary.go
@@ -1,15 +1,15 @@
 package problem0953
 
 func isAlienSorted(words []string, order string) bool {
-	oi := [123]int{}
+	indexs := make([]int, 123)
 	for i, c := range order {
-		oi[c] = i
+		indexs[c] = i
 	}
 
 	less := func(i, j int) bool {
 		si, sj := len(words[i]), len(words[j])
 		for k := 0; k < si && k < sj; k++ {
-			ii, ij := oi[words[i][k]], oi[words[j][k]]
+			ii, ij := indexs[words[i][k]], indexs[words[j][k]]
 			switch {
 			case ii < ij:
 				return true
@@ -25,6 +25,5 @@ func isAlienSorted(words []string, order string) bool {
 			return false
 		}
 	}
-
 	return true
 }
