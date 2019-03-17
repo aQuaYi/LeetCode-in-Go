c8c3713fef798f2d20a08b70932a298fdd734bbb
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-03-17 11:37:03 +0800

    955 added

---
+++ Algorithms/0955.delete-columns-to-make-sorted-ii/delete-columns-to-make-sorted-ii.go
@@ -0,0 +1,6 @@
+package problem0955
+
+func minDeletionSize(A []string) int {
+
+	return 0
+}
---
+++ Algorithms/0955.delete-columns-to-make-sorted-ii/delete-columns-to-make-sorted-ii_test.go
@@ -0,0 +1,47 @@
+package problem0955
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	A   []string
+	ans int
+}{
+
+	{
+		[]string{"ca", "bb", "ac"},
+		1,
+	},
+
+	{
+		[]string{"xc", "yb", "za"},
+		0,
+	},
+
+	{
+		[]string{"zyx", "wvu", "tsr"},
+		3,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_minDeletionSize(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		ast.Equal(tc.ans, minDeletionSize(tc.A), "输入:%v", tc)
+	}
+}
+
+func Benchmark_minDeletionSize(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			minDeletionSize(tc.A)
+		}
+	}
+}
