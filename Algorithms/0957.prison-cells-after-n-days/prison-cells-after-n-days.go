47fbbe14fec1b5811febb6e3fd5cf17c71770a96
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-03-18 20:51:12 +0800

    957 wrong answer

---
+++ Algorithms/0957.prison-cells-after-n-days/prison-cells-after-n-days.go
@@ -0,0 +1,31 @@
+package problem0957
+
+func prisonAfterNDays(cells []int, N int) []int {
+	n := cells2int(cells)
+
+	for i := 0; i < N; i++ {
+		n = ^((n << 1) ^ (n >> 1))
+	}
+
+	return int2cells(n)
+}
+
+func cells2int(cells []int) int {
+	res := 0
+	t := uint(7)
+	for _, c := range cells {
+		res += c << t
+		t--
+	}
+	return res
+}
+
+func int2cells(n int) []int {
+	cells := make([]int, 8)
+	t := uint(7)
+	for i := range cells {
+		cells[i] = (n >> t) & 1
+		t--
+	}
+	return cells
+}
---
+++ Algorithms/0957.prison-cells-after-n-days/prison-cells-after-n-days_test.go
@@ -0,0 +1,45 @@
+package problem0957
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	cells []int
+	N     int
+	ans   []int
+}{
+
+	{
+		[]int{0, 1, 0, 1, 1, 0, 0, 1},
+		7,
+		[]int{0, 0, 1, 1, 0, 0, 0, 0},
+	},
+
+	{
+		[]int{1, 0, 0, 1, 0, 0, 1, 0},
+		1000000000,
+		[]int{0, 0, 1, 1, 1, 1, 1, 0},
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_prisonAfterNDays(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		ast.Equal(tc.ans, prisonAfterNDays(tc.cells, tc.N), "输入:%v", tc)
+	}
+}
+
+func Benchmark_prisonAfterNDays(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			prisonAfterNDays(tc.cells, tc.N)
+		}
+	}
+}
