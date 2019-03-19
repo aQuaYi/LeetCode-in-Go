c90457469d668726590ac4e7b73bc26e7e4ff53e
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-03-19 21:20:06 +0800

    961 accepted. 28ms, 6.6MB

--- Algorithms/0961.n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.go
+++ Algorithms/0961.n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.go
@@ -1,6 +1,14 @@
 package problem0961
 
 func repeatedNTimes(A []int) int {
-
-	return 0
+	N := len(A) / 2
+	hasSeen := make(map[int]bool, N+1)
+	var v int
+	for _, v = range A {
+		if hasSeen[v] {
+			break
+		}
+		hasSeen[v] = true
+	}
+	return v
 }
