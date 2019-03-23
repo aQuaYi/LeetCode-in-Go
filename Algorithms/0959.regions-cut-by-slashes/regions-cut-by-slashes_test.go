4ef98e5d34196ea0cf37f66f3c33c68eaa5fe12c
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-03-23 16:28:03 +0800

    959 wrong answer

---
+++ Algorithms/0959.regions-cut-by-slashes/regions-cut-by-slashes_test.go
@@ -0,0 +1,79 @@
+package problem0959
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	grid []string
+	ans  int
+}{
+
+	{
+		[]string{
+			" /",
+			"/ ",
+		},
+		2,
+	},
+
+	{
+		[]string{
+			"/",
+		},
+		2,
+	},
+
+	{
+		[]string{
+			" /",
+			"  ",
+		},
+		1,
+	},
+
+	// {
+	// 	[]string{
+	// 		"\\/",
+	// 		"/\\",
+	// 	},
+	// 	4,
+	// },
+
+	// {
+	// 	[]string{
+	// 		"/\\",
+	// 		"\\/",
+	// 	},
+	// 	5,
+	// },
+
+	// {
+	// 	[]string{
+	// 		"//",
+	// 		"/ ",
+	// 	},
+	// 	3,
+	// },
+
+	// // 可以有多个 testcase
+}
+
+func Test_regionsBySlashes(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		ast.Equal(tc.ans, regionsBySlashes(tc.grid), "输入:%v", tc)
+	}
+}
+
+func Benchmark_regionsBySlashes(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			regionsBySlashes(tc.grid)
+		}
+	}
+}
