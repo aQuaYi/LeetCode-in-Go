7363ea02b912631c9aff57477c8a48db92d1c60b
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-03-25 20:06:37 +0800

    963 added

---
+++ Algorithms/0963.minimum-area-rectangle-ii/minimum-area-rectangle-ii_test.go
@@ -0,0 +1,52 @@
+package problem0963
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	points [][]int
+	ans    float64
+}{
+
+	{
+		[][]int{{1, 2}, {2, 1}, {1, 0}, {0, 1}},
+		2.00000,
+	},
+
+	{
+		[][]int{{0, 1}, {2, 1}, {1, 1}, {1, 0}, {2, 0}},
+		1.00000,
+	},
+
+	{
+		[][]int{{0, 3}, {1, 2}, {3, 1}, {1, 3}, {2, 1}},
+		0,
+	},
+
+	{
+		[][]int{{3, 1}, {1, 1}, {0, 1}, {2, 1}, {3, 3}, {3, 2}, {0, 2}, {2, 3}},
+		2.00000,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_minAreaFreeRect(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		ast.Equal(tc.ans, minAreaFreeRect(tc.points), "输入:%v", tc)
+	}
+}
+
+func Benchmark_minAreaFreeRect(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			minAreaFreeRect(tc.points)
+		}
+	}
+}
