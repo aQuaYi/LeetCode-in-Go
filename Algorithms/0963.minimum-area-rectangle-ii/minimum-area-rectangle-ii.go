af79e8c9b0309d8e1c94c9a0ccc79fe3123e4e8e
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-03-25 20:48:58 +0800

    963 思路错误

--- Algorithms/0963.minimum-area-rectangle-ii/minimum-area-rectangle-ii.go
+++ Algorithms/0963.minimum-area-rectangle-ii/minimum-area-rectangle-ii.go
@@ -1,6 +1,55 @@
 package problem0963
 
-func minAreaFreeRect(points [][]int) float64 {
+import "math"
 
-	return 0
+func minAreaFreeRect(points [][]int) float64 {
+	mids := make(map[[2]float64][][2]int, 0)
+	size := len(points)
+	for i := 0; i < size; i++ {
+		for j := i + 1; j < size; j++ {
+			xi, yi := points[i][0], points[i][1]
+			xj, yj := points[j][0], points[j][1]
+			x, y := float64(xi+xj)/2, float64(yi+yj)/2
+			m := [2]float64{x, y}
+			mids[m] = append(mids[m], [2]int{i, j})
+		}
+	}
+
+	res := math.MaxFloat64
+	for _, ms := range mids {
+		sz := len(ms)
+		if sz == 1 {
+			continue
+		}
+		for i := 0; i < sz; i++ {
+			p, q := points[ms[i][0]], points[ms[i][1]]
+			for j := i + 1; j < sz; j++ {
+				o := points[ms[j][0]]
+				res = min(res, area(p, q, o))
+			}
+		}
+	}
+
+	return res
+}
+
+func area(p, q, o []int) float64 {
+	xpo, ypo := p[0]-o[0], p[1]-o[1]
+	xqo, yqo := q[0]-o[0], q[1]-o[1]
+	if xpo*xqo+ypo*yqo != 0 { // not rectangle
+		return 0
+	}
+	return length(xpo, ypo) * length(xqo, yqo)
+}
+
+func length(x, y int) float64 {
+	xf, yf := float64(x), float64(y)
+	return math.Sqrt(xf*xf + yf*yf)
+}
+
+func min(a, b float64) float64 {
+	if a < b {
+		return a
+	}
+	return b
 }
