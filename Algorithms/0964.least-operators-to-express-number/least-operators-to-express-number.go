27d7ce4ecefcc630dbf30b5efc46f98aa789afc7
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-03-26 23:56:16 +0800

    964 wrong answer

--- Algorithms/0964.least-operators-to-express-number/least-operators-to-express-number.go
+++ Algorithms/0964.least-operators-to-express-number/least-operators-to-express-number.go
@@ -1,23 +1,30 @@
 package problem0964
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 func leastOpsExpressTarget(x int, target int) int {
+	fmt.Println("-=-=-=-=-=-=-=-=-")
 	res := math.MaxInt64
 	helper(x, target, 0, &res)
 	return res
 }
 
 func helper(x, target, count int, res *int) {
+	fmt.Println(target, count)
 	if count >= *res {
 		return
 	}
 
 	if target == x {
 		*res = min(*res, count)
+		fmt.Println("-", *res)
 		return
 	} else if target == 1 {
 		*res = min(*res, count+1)
+		fmt.Println("-", *res)
 		return
 	}
 
@@ -27,13 +34,26 @@ func helper(x, target, count int, res *int) {
 	intRoot := int(math.Floor(root))
 	if base == target {
 		*res = min(*res, count+intRoot-1)
+		fmt.Println("-", *res)
 		return
 	}
 
 	intRoot = max(intRoot, 1)
-
-	helper(x, target-base, count+intRoot, res)
-	helper(x, base*x-target, count+intRoot+1, res)
+	if target-base <= base*x-target {
+		if base == 1 {
+			helper(x, target-base, count+intRoot+1, res)
+		} else {
+			helper(x, target-base, count+intRoot, res)
+		}
+		helper(x, base*x-target, count+intRoot+1, res)
+	} else {
+		if base == 1 {
+			helper(x, base*x-target, count+intRoot, res)
+		} else {
+			helper(x, base*x-target, count+intRoot+1, res)
+		}
+		helper(x, target-base, count+intRoot, res)
+	}
 }
 
 func max(a, b int) int {
