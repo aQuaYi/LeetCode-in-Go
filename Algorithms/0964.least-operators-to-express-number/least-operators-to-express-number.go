2404d4d788d63811e8fc67f034f5d95942fca076
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-03-26 22:01:29 +0800

    964 wrong answer 递归无法收敛

--- Algorithms/0964.least-operators-to-express-number/least-operators-to-express-number.go
+++ Algorithms/0964.least-operators-to-express-number/least-operators-to-express-number.go
@@ -1,6 +1,39 @@
 package problem0964
 
-func leastOpsExpressTarget(x int, target int) int {
+import "math"
 
-	return 0
+func leastOpsExpressTarget(x int, target int) int {
+	if target == x {
+		return 0
+	} else if target == 1 {
+		return 1
+	}
+
+	root := math.Log10(float64(target)) / math.Log10(float64(x))
+
+	base := int(math.Pow(float64(x), math.Floor(root)))
+	intRoot := int(math.Floor(root))
+	if base == target {
+		return intRoot - 1
+	}
+
+	return min(
+		intRoot+leastOpsExpressTarget(x, abs(base-target)),
+		intRoot+1+leastOpsExpressTarget(x, abs(base*x-target)),
+	)
+
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
 }
