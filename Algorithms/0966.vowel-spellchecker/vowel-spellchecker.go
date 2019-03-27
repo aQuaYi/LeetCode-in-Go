7b87913a327e60948ce44b6abc26b2a3b5dea937
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-03-27 21:41:33 +0800

    966 done

--- Algorithms/0966.vowel-spellchecker/vowel-spellchecker.go
+++ Algorithms/0966.vowel-spellchecker/vowel-spellchecker.go
@@ -6,7 +6,7 @@ func spellchecker(wordlist []string, queries []string) []string {
 	isExactlyMatch := make(map[string]bool, len(wordlist))
 	cap := make(map[string]int, len(wordlist))
 	vow := make(map[string]int, len(wordlist))
-	for i := len(wordlist) - 1; i >= 0; i-- {
+	for i := len(wordlist) - 1; i >= 0; i-- { // 必须从大到小
 		w := wordlist[i]
 		isExactlyMatch[w] = true // case sensitive
 		w = strings.ToLower(w)
@@ -29,10 +29,15 @@ func spellchecker(wordlist []string, queries []string) []string {
 }
 
 func replacingVowel(s string) string {
-	s = strings.ToLower(s) // case insensitive
-	vowels := []string{"a", "e", "i", "o", "u"}
-	for _, v := range vowels {
-		s = strings.Replace(s, v, "*", -1)
+	var sb strings.Builder
+	for _, b := range s {
+		if 'A' <= b && b <= 'Z' {
+			b += 'a' - 'A' // case insensitive
+		}
+		if b == 'a' || b == 'e' || b == 'i' || b == 'o' || b == 'u' {
+			b = '*'
+		}
+		sb.WriteRune(b)
 	}
-	return s
+	return sb.String()
 }
