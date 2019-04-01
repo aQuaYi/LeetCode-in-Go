4e0da15a7dada653da0be28847a3a2cec3653900
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-04-01 20:00:55 +0800

    967 wrong answer

--- Algorithms/0967.numbers-with-same-consecutive-differences/numbers-with-same-consecutive-differences.go
+++ Algorithms/0967.numbers-with-same-consecutive-differences/numbers-with-same-consecutive-differences.go
@@ -1,6 +1,30 @@
 package problem0967
 
 func numsSameConsecDiff(N int, K int) []int {
+	queue := prepare(K)
+	for n := 1; n < N; n++ {
+		tmp := make([]int, 0, len(queue)*2)
+		for _, v := range queue {
+			r := v % 10
+			if r-K >= 0 {
+				tmp = append(tmp, v*10+r-K)
+			}
+			if r+K <= 9 {
+				tmp = append(tmp, v*10+r+K)
+			}
+		}
+		queue = tmp
+	}
+	return queue
+}
 
-	return nil
+func prepare(k int) []int {
+	res := make([]int, 0, 32)
+	for i := 1; i < 10; i++ {
+		if i+k > 9 && i-k < 0 {
+			continue
+		}
+		res = append(res, i)
+	}
+	return res
 }
