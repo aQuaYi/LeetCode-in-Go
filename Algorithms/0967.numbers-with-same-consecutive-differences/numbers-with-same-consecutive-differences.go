e5dcb9ad4efffaf0101d6a606a55bd0c6279e895
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-04-01 20:21:17 +0800

    967 wrong answer

--- Algorithms/0967.numbers-with-same-consecutive-differences/numbers-with-same-consecutive-differences.go
+++ Algorithms/0967.numbers-with-same-consecutive-differences/numbers-with-same-consecutive-differences.go
@@ -2,21 +2,33 @@ package problem0967
 
 func numsSameConsecDiff(N int, K int) []int {
 	res := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-	for n := 1; n < N; n++ {
-		tmp := make([]int, 0, len(res)*2)
-		for _, v := range res {
-			if v == 0 {
-				continue
-			}
-			r := v % 10
-			if r-K >= 0 {
-				tmp = append(tmp, v*10+r-K)
-			}
-			if K > 0 && r+K <= 9 {
-				tmp = append(tmp, v*10+r+K)
-			}
-		}
-		res = tmp
+	if N == 1 {
+		return res
 	}
+
+	res = make([]int, 0, 20)
+	for i := 1; i < 10; i++ {
+		if t, ok := makeNumber(i, N, -K); ok {
+			res = append(res, t)
+		}
+		if K == 0 {
+			continue
+		}
+		if t, ok := makeNumber(i, N, K); ok {
+			res = append(res, t)
+		}
+	}
+
 	return res
+}
+
+func makeNumber(x, N, K int) (int, bool) {
+	if x+K < 0 || 9 < x+K {
+		return 0, false
+	}
+	for i := 1; i < N; i++ {
+		x = x*10 + x%10 + K
+		K = -K
+	}
+	return x, true
 }
