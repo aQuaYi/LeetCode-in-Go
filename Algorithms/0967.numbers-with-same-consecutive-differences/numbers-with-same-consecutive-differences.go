13093f8c027d8452ed9e7ecf4ce8406b8c37ffcc
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-04-02 08:46:53 +0800

    967 修改了小瑕疵。

--- Algorithms/0967.numbers-with-same-consecutive-differences/numbers-with-same-consecutive-differences.go
+++ Algorithms/0967.numbers-with-same-consecutive-differences/numbers-with-same-consecutive-differences.go
@@ -4,7 +4,7 @@ func numsSameConsecDiff(N int, K int) []int {
 	nums := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 	if N == 1 {
 		// when N is 1, whatever K is, the answer is nums
-		return nums[:]
+		return nums
 	}
 
 	res := nums[1:] // when N>1, delete 0
