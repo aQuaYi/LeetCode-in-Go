81dc4bc46dc6f4b7503d26cf40e7911ce1a74de2
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-04-02 22:14:16 +0800

    969 finish

--- Algorithms/0969.pancake-sorting/pancake-sorting.go
+++ Algorithms/0969.pancake-sorting/pancake-sorting.go
@@ -4,21 +4,25 @@ func pancakeSort(A []int) []int {
 	size := len(A)
 	res := make([]int, 0, size*2)
 	for Ai := size; 0 < Ai; Ai-- {
-		i := Ai - 1
-		if A[i] == Ai {
+		// every round makes A[i-1] is i
+		if A[Ai-1] == Ai {
 			continue
 		}
+		i := Ai - 2
 		for A[i] != Ai {
 			i--
 		}
-		swap(A, i+1)
-		swap(A, Ai)
-		res = append(res, i+1, Ai)
+		if i != 0 {
+			reverse(A, i+1) // move Ai to A[0]
+			res = append(res, i+1)
+		}
+		reverse(A, Ai) // move Ai to A[Ai-1]
+		res = append(res, Ai)
 	}
 	return res
 }
 
-func swap(A []int, k int) {
+func reverse(A []int, k int) {
 	for i, j := 0, k-1; i < j; i, j = i+1, j-1 {
 		A[i], A[j] = A[j], A[i]
 	}
