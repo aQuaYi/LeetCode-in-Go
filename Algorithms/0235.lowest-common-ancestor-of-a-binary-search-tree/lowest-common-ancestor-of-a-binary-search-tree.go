90513d720fe870196e9cf1b483e17f5b2d0779c7
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-04-09 21:49:11 +0800

    235 finish

--- Algorithms/0235.lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.go
+++ Algorithms/0235.lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.go
@@ -2,16 +2,19 @@ package problem0235
 
 import "github.com/aQuaYi/LeetCode-in-Go/kit"
 
+// TreeNode is pre-defined..
 type TreeNode = kit.TreeNode
 
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
-	if root.Val > p.Val && root.Val > q.Val {
-		return lowestCommonAncestor(root.Left, p, q)
-	}
+	return helper(root, p.Val, q.Val)
+}
 
-	if root.Val < p.Val && root.Val < q.Val {
-		return lowestCommonAncestor(root.Right, p, q)
+func helper(root *TreeNode, p, q int) *TreeNode {
+	r := root.Val
+	if p < r && q < r {
+		return helper(root.Left, p, q)
+	} else if r < p && r < q {
+		return helper(root.Right, p, q)
 	}
-
 	return root
 }
