68f66dcddc048a90afd932f5aa3957c38c40afe8
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-04-11 20:11:52 +0800

    509 finish

--- Algorithms/0509.fibonacci-number/fibonacci-number.go
+++ Algorithms/0509.fibonacci-number/fibonacci-number.go
@@ -1,15 +1,15 @@
 package problem0509
 
-var f = make([]int, 31)
+var fibs = make([]int, 31)
 
 func fib(N int) int {
 	if N == 0 || N == 1 {
 		return N
 	}
-	res := f[N]
+	res := fibs[N]
 	if res == 0 {
 		res = fib(N-1) + fib(N-2)
+		fibs[N] = res
 	}
-	f[N] = res
 	return res
 }
