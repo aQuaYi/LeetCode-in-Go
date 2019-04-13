79244a9e86c8c664d099b337f38bdf5341ae484b
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-04-13 16:41:04 +0800

    238 improve

--- Algorithms/0238.product-of-array-except-self/product-of-array-except-self.go
+++ Algorithms/0238.product-of-array-except-self/product-of-array-except-self.go
@@ -5,13 +5,13 @@ func productExceptSelf(nums []int) []int {
 
 	res := make([]int, size)
 
-	left := 1 // product of all left
+	left := 1 // product all left of nums[i]
 	for i := 0; i < size; i++ {
 		res[i] = left
 		left *= nums[i]
 	}
 
-	right := 1 // product of all right
+	right := 1 // product all right of nums[i]
 	for i := size - 1; i >= 0; i-- {
 		res[i] *= right
 		right *= nums[i]
