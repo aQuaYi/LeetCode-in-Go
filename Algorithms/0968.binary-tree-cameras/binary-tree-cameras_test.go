3c0bc9be1f2fbf15201d2a77b3002f10452fd808
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-04-13 16:49:22 +0800

    968 added

---
+++ Algorithms/0968.binary-tree-cameras/binary-tree-cameras_test.go
@@ -0,0 +1,45 @@
+package problem0968
+
+import (
+	"testing"
+
+	"github.com/aQuaYi/LeetCode-in-Go/kit"
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	root []int
+	ans  int
+}{
+
+	{
+		[]int{0, 0, kit.NULL, 0, 0},
+		1,
+	},
+
+	{
+		[]int{0, 0, kit.NULL, 0, kit.NULL, 0, kit.NULL, kit.NULL, 0},
+		2,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_minCameraCover(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		root := kit.Ints2TreeNode(tc.root)
+		ast.Equal(tc.ans, minCameraCover(root), "输入:%v", tc)
+	}
+}
+
+func Benchmark_minCameraCover(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			root := kit.Ints2TreeNode(tc.root)
+			minCameraCover(root)
+		}
+	}
+}
