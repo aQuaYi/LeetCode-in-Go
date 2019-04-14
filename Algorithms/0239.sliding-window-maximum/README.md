b6e922bed1c7be813d82e315049a39d093cd215f
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-04-15 00:41:25 +0800

    239 wrong answer

--- Algorithms/0239.sliding-window-maximum/README.md
+++ Algorithms/0239.sliding-window-maximum/README.md
@@ -1,10 +1,10 @@
 # [239. Sliding Window Maximum](https://leetcode.com/problems/sliding-window-maximum/)
 
-## 题目
 Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
 
 For example,
-```
+
+```text
 Given nums = [1,3,-1,-3,5,3,6,7], and k = 3.
 
 Window position                Max
@@ -19,12 +19,8 @@ Window position                Max
 
 Therefore, return the max sliding window as [3,3,5,5,6,7].
 
-Note: 
-You may assume k is always valid, ie: 1 ≤ k ≤ input array's size for non-empty array.
+Note:
 
-Follow up:
-Could you solve it in linear time?
+- You may assume k is always valid, ie: 1 ≤ k ≤ input array's size for non-empty array.
 
-## 解题思路
-
-见程序注释
+**Follow up:** Could you solve it in linear time?
--- Algorithms/0239.sliding-window-maximum/sliding-window-maximum_test.go
+++ Algorithms/0239.sliding-window-maximum/sliding-window-maximum_test.go
@@ -15,9 +15,27 @@ var tcs = []struct {
 }{
 
 	{
+		[]int{7, 2, 4},
+		2,
+		[]int{7, 4},
+	},
+
+	{
 		[]int{},
 		0,
 		[]int{},
+	},
+
+	{
+		[]int{2, 1, 3, 4, 6, 3, 8, 9, 10, 12, 56},
+		4,
+		[]int{4, 6, 6, 8, 9, 10, 12, 56},
+	},
+
+	{
+		[]int{1, 3, -1, -3, 5, 3, 6, 7},
+		1,
+		[]int{1, 3, -1, -3, 5, 3, 6, 7},
 	},
 
 	{
@@ -38,27 +56,10 @@ func Test_maxSlidingWindow(t *testing.T) {
 	}
 }
 
-func Test_maxSlidingWindow2(t *testing.T) {
-	ast := assert.New(t)
-
-	for _, tc := range tcs {
-		fmt.Printf("~~%v~~\n", tc)
-		ast.Equal(tc.ans, maxSlidingWindow2(tc.nums, tc.k), "输入:%v", tc)
-	}
-}
-
 func Benchmark_maxSlidingWindow(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		for _, tc := range tcs {
 			maxSlidingWindow(tc.nums, tc.k)
-		}
-	}
-}
-
-func Benchmark_maxSlidingWindow2(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		for _, tc := range tcs {
-			maxSlidingWindow2(tc.nums, tc.k)
 		}
 	}
 }
