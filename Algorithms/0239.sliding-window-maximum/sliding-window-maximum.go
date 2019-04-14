2fd8aa2882713e829e1b427564545401caaf6439
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-04-15 00:41:25 +0800

    239 wrong answer

--- Algorithms/0239.sliding-window-maximum/sliding-window-maximum.go
+++ Algorithms/0239.sliding-window-maximum/sliding-window-maximum.go
@@ -1,59 +1,35 @@
 package problem0239
 
-func maxSlidingWindow(nums []int, k int) []int {
-	res := make([]int, 0, len(nums)-k+1)
-	window := make([]int, 0, k)
-	for i, v := range nums {
-		// 控制 window 窗口大小
-		if i > k-1 && window[0] == i-k {
-			window = window[1:]
-		}
-
-		// window 保持递减
-		j := len(window) - 1
-		for ; j >= 0; j-- {
-			if nums[window[j]] > v {
-				break
-			}
-		}
-		window = window[:j+1]
-		window = append(window, i)
-
-		// 获取 window 最大值
-		if i >= k-1 {
-			res = append(res, nums[window[0]])
-		}
-	}
-
-	return res
-}
-
-// 解法二
 // 参看 https://leetcode.com/problems/sliding-window-maximum/discuss/65881/O(n)-solution-in-Java-with-two-simple-pass-in-the-array
 // 例子
-// nums 1 3 3 -3 5 5 6 7
-// k = 3
-// left  1 3 3 | -3 5 5 | 6 7
-// right 3 3 -1|  5 5 3 | 7 7
-// res   3 3 5    5 6 7
-func maxSlidingWindow2(nums []int, k int) []int {
+// nums = [2,1,3,4,6,3,8,9,10,12,56], k=4
+// split   2, 1, 3, 4 | 6, 3,   8,  9 | 10, 12, 56  // every k number into a group
+// left    2, 2, 3, 4 | 6, 6,   8,  9 | 10, 12, 56
+// right   4, 4, 4, 4 | 9, 9,   9,  9 | 56, 56, 56
+// res[i] = max(right_max[i], left[i+w-1])
+// res     4, 6, 6, 8,| 9, 10, 12, 56
+func maxSlidingWindow(nums []int, k int) []int {
 	size := len(nums)
-	if size <= 1 {
+	if k <= 1 {
 		return nums
 	}
 
-	left, right := make([]int, size), make([]int, size)
-	left[0], right[size-1] = nums[0], nums[size-1]
+	g := k - 1
 
+	left := make([]int, size)
+	left[0] = nums[0]
 	for i := 1; i < size; i++ {
-		if i%k == 0 {
+		if i%g == 1 {
 			left[i] = nums[i]
 		} else {
 			left[i] = max(nums[i], left[i-1])
 		}
+	}
 
-		j := size - 1 - i
-		if (j+1)%k == 0 {
+	right := make([]int, size)
+	right[size-1] = nums[size-1]
+	for j := size - 2; j >= 0; j-- {
+		if j%g == 0 {
 			right[j] = nums[j]
 		} else {
 			right[j] = max(nums[j], right[j+1])
@@ -66,13 +42,11 @@ func maxSlidingWindow2(nums []int, k int) []int {
 	}
 
 	return res
-
 }
 
 func max(a, b int) int {
 	if a > b {
 		return a
 	}
-
 	return b
 }
