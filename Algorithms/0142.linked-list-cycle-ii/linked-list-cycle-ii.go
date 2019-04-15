66bc60008f753d5ba70fef2fa1c412b783628ce9
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-04-15 20:58:24 +0800

    141 accepted.

--- Algorithms/0142.linked-list-cycle-ii/linked-list-cycle-ii.go
+++ Algorithms/0142.linked-list-cycle-ii/linked-list-cycle-ii.go
@@ -6,6 +6,20 @@ import "github.com/aQuaYi/LeetCode-in-Go/kit"
 type ListNode = kit.ListNode
 
 func detectCycle(head *ListNode) *ListNode {
+	headPre := &ListNode{
+		Next: head,
+	}
+	return detect(headPre)
+}
 
+func detect(head *ListNode) *ListNode {
+	hasSeen := make(map[*ListNode]bool)
+	for head != nil && head.Next != nil {
+		if hasSeen[head.Next] {
+			return head.Next
+		}
+		hasSeen[head.Next] = true
+		head = head.Next
+	}
 	return nil
 }
--- Algorithms/0142.linked-list-cycle-ii/linked-list-cycle-ii_test.go
+++ Algorithms/0142.linked-list-cycle-ii/linked-list-cycle-ii_test.go
@@ -15,6 +15,11 @@ var tcs = []struct {
 }{
 
 	{
+		[]int{1, 2, 3},
+		-1,
+	},
+
+	{
 		[]int{3, 2, 0, -4},
 		1,
 	},
@@ -24,20 +29,17 @@ var tcs = []struct {
 		0,
 	},
 
-	{
-		[]int{1},
-		-1,
-	},
-
 	// 可以有多个 testcase
 }
 
 func Test_detectCycle(t *testing.T) {
 	ast := assert.New(t)
-
 	for _, tc := range tcs {
 		head := kit.Ints2ListWithCycle(tc.ints, tc.pos)
-		ans := head.GetNodeWith(tc.ints[tc.pos])
+		var ans *ListNode
+		if tc.pos >= 0 {
+			ans = head.GetNodeWith(tc.ints[tc.pos])
+		}
 		ast.Equal(ans, detectCycle(head), "输入:%v", tc)
 	}
 }
