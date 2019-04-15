d0071f866826c37bd094348e8e40739265b943fb
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-04-15 20:22:15 +0800

    141 accepted.

--- Algorithms/0141.linked-list-cycle/linked-list-cycle.go
+++ Algorithms/0141.linked-list-cycle/linked-list-cycle.go
@@ -6,10 +6,10 @@ import "github.com/aQuaYi/LeetCode-in-Go/kit"
 type ListNode = kit.ListNode
 
 func hasCycle(head *ListNode) bool {
-	if head == nil || head.Next == nil || head.Next.Next == nil {
+	if head == nil {
 		return false
 	}
-	slow, fast := head, head
+	slow, fast := head, head.Next
 	for fast != nil && fast.Next != nil && slow != fast {
 		slow, fast = slow.Next, fast.Next.Next
 	}
