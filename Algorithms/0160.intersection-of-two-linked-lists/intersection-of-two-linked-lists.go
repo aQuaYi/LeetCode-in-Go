ea3fbe5870a9363c1c8a0511b79d4583f1c7d19d
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-04-16 20:08:25 +0800

    160 accepted. 52ms

--- Algorithms/0160.intersection-of-two-linked-lists/intersection-of-two-linked-lists.go
+++ Algorithms/0160.intersection-of-two-linked-lists/intersection-of-two-linked-lists.go
@@ -6,5 +6,16 @@ import "github.com/aQuaYi/LeetCode-in-Go/kit"
 type ListNode = kit.ListNode
 
 func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	hasSeen := make(map[*ListNode]bool, 128)
+	for headA != nil {
+		hasSeen[headA] = true
+		headA = headA.Next
+	}
+	for headB != nil {
+		if hasSeen[headB] {
+			return headB
+		}
+		headB = headB.Next
+	}
 	return nil
 }
