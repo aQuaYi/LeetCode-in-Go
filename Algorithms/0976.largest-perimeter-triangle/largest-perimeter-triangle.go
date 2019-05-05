073c25b9b9ba2a6199b947a9d97c3ff6004ab6b1
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-05 21:14:33 +0800

    976 accepted. 28ms

--- Algorithms/0976.largest-perimeter-triangle/largest-perimeter-triangle.go
+++ Algorithms/0976.largest-perimeter-triangle/largest-perimeter-triangle.go
@@ -4,29 +4,14 @@ import "sort"
 
 func largestPerimeter(A []int) int {
 	size := len(A)
-	sort.Sort(sort.Reverse(sort.IntSlice(A)))
-	for i := 0; i < size; i++ {
-		a := A[i]
-		for j := i + 1; j < size; j++ {
-			b := A[j]
-			for k := j + 1; k < size; k++ {
-				c := A[k]
-				if a < b+c {
-					return a + b + c
-				}
-			}
+	sort.Ints(A)
+	a, b := A[size-1], A[size-2]
+	for i := size - 3; i >= 0; i-- {
+		c := A[i]
+		if a < b+c {
+			return a + b + c
 		}
+		a, b = b, c
 	}
 	return 0
-}
-
-func isOK(a, b, c int) bool {
-	return a+b > c
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
 }
