f7fd585cae6d4c9b75cdde7fc3d2725bbbbca829
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-05 21:47:14 +0800

    976 finish

--- Algorithms/0976.largest-perimeter-triangle/largest-perimeter-triangle.go
+++ Algorithms/0976.largest-perimeter-triangle/largest-perimeter-triangle.go
@@ -1,17 +1,50 @@
 package problem0976
 
-import "sort"
+import "container/heap"
 
 func largestPerimeter(A []int) int {
 	size := len(A)
-	sort.Ints(A)
-	a, b := A[size-1], A[size-2]
+	h := intHeap(A)
+
+	heap.Init(&h)
+
+	a := heap.Pop(&h).(int)
+	b := heap.Pop(&h).(int)
 	for i := size - 3; i >= 0; i-- {
-		c := A[i]
+		c := heap.Pop(&h).(int)
 		if a < b+c {
 			return a + b + c
 		}
 		a, b = b, c
 	}
 	return 0
+}
+
+// intHeap 实现了 heap 的接口
+type intHeap []int
+
+func (h intHeap) Len() int {
+	return len(h)
+}
+
+func (h intHeap) Less(i, j int) bool {
+	return h[i] > h[j] // NOTICE: Max is at the top
+}
+
+func (h intHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *intHeap) Push(x interface{}) {
+	// Push 使用 *h，是因为
+	// Push 增加了 h 的长度
+	// *h = append(*h, x.(int))
+}
+
+func (h *intHeap) Pop() interface{} {
+	// Pop 使用 *h ，是因为
+	// Pop 减短了 h 的长度
+	res := (*h)[len(*h)-1]
+	*h = (*h)[:len(*h)-1]
+	return res
 }
