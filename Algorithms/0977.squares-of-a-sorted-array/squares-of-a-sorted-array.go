feef9b1bfb8ba0f30d0aa55c3b8afa9098690ba4
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-06 20:00:28 +0800

    977 finish

--- Algorithms/0977.squares-of-a-sorted-array/squares-of-a-sorted-array.go
+++ Algorithms/0977.squares-of-a-sorted-array/squares-of-a-sorted-array.go
@@ -1,20 +1,15 @@
 package problem0977
 
-import "sort"
-
 func sortedSquares(A []int) []int {
-	r := sort.SearchInts(A, 0)
-	l := r - 1
 	size := len(A)
-	res := make([]int, 0, size)
-	for 0 <= l || r < size {
-		if r == size ||
-			(0 <= l && A[l]+A[r] > 0) {
-			res = append(res, A[l]*A[l])
-			l--
+	res := make([]int, size)
+	for l, r, i := 0, size-1, size-1; l <= r; i-- {
+		if A[l]+A[r] < 0 {
+			res[i] = A[l] * A[l]
+			l++
 		} else {
-			res = append(res, A[r]*A[r])
-			r++
+			res[i] = A[r] * A[r]
+			r--
 		}
 	}
 	return res
