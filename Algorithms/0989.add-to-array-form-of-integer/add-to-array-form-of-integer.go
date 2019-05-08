656b8a888cdba002ce95397f3bb63bd306b4b55e
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-08 21:05:27 +0800

    989 wrong answer

--- Algorithms/0989.add-to-array-form-of-integer/add-to-array-form-of-integer.go
+++ Algorithms/0989.add-to-array-form-of-integer/add-to-array-form-of-integer.go
@@ -1,6 +1,27 @@
 package problem0989
 
-func addToArrayForm(A []int, K int) []int {
+import "strconv"
 
-	return nil
+func addToArrayForm(A []int, K int) []int {
+	a := slice2num(A)
+	a += K
+	return num2Slice(a)
+}
+
+func slice2num(A []int) int {
+	res := 0
+	for _, v := range A {
+		res = res*10 + v
+	}
+	return res
+}
+
+func num2Slice(n int) []int {
+	s := strconv.Itoa(n)
+	bytes := []byte(s)
+	res := make([]int, len(bytes))
+	for i, b := range bytes {
+		res[i] = int(b - '0')
+	}
+	return res
 }
