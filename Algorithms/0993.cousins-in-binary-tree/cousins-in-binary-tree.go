74ab6a1c7493555ebed61c75ca895cee95099e9a
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-09 20:08:28 +0800

    993 accepted. 0ms

--- Algorithms/0993.cousins-in-binary-tree/cousins-in-binary-tree.go
+++ Algorithms/0993.cousins-in-binary-tree/cousins-in-binary-tree.go
@@ -8,6 +8,33 @@ import (
 type TreeNode = kit.TreeNode
 
 func isCousins(root *TreeNode, x int, y int) bool {
+	px, dx := find(root, x)
+	py, dy := find(root, y)
+	return dx == dy && px != py
+}
 
-	return false
+// root's parent is nil
+func find(root *TreeNode, x int) (*TreeNode, int) {
+	if root == nil {
+		return nil, -1
+	}
+	if root.Val == x {
+		return nil, 0
+	}
+
+	p, d := find(root.Left, x)
+	if d == 0 {
+		return root, 1
+	} else if d > 0 {
+		return p, d + 1
+	}
+
+	p, d = find(root.Right, x)
+	if d == 0 {
+		return root, 1
+	} else if d > 0 {
+		return p, d + 1
+	}
+
+	return nil, -1
 }
