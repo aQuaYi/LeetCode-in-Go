fe52c784cae0fd71cdc2d63366acb3c64b495401
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-10 10:50:00 +0800

    994 added

---
+++ Algorithms/0994.rotting-oranges/rotting-oranges.go
@@ -0,0 +1,6 @@
+package problem0994
+
+func orangesRotting(grid [][]int) int {
+
+	return 0
+}
---
+++ Algorithms/0994.rotting-oranges/rotting-oranges_test.go
@@ -0,0 +1,47 @@
+package problem0994
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	grid [][]int
+	ans  int
+}{
+
+	{
+		[][]int{{2, 1, 1}, {1, 1, 0}, {0, 1, 1}},
+		4,
+	},
+
+	{
+		[][]int{{2, 1, 1}, {0, 1, 1}, {1, 0, 1}},
+		-1,
+	},
+
+	{
+		[][]int{{0, 2}},
+		0,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_orangesRotting(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		ast.Equal(tc.ans, orangesRotting(tc.grid), "输入:%v", tc)
+	}
+}
+
+func Benchmark_orangesRotting(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			orangesRotting(tc.grid)
+		}
+	}
+}
