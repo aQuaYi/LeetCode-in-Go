e70bcf3392de931dc8579d648963d6129dd29d84
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-12 21:05:02 +0800

    972 重构失败

--- Algorithms/0972.equal-rational-numbers/equal-rational-numbers.go
+++ Algorithms/0972.equal-rational-numbers/equal-rational-numbers.go
@@ -6,49 +6,16 @@ import (
 )
 
 func isRationalEqual(S string, T string) bool {
-	S = normalize(S)
-	sn, sr := parse2(S)
-	sn, sr = simplify2(sn, sr)
-	s, sr := convert2(sn, sr)
+	si, sn, sr := parse(S)
+	sn, sr = simplify(sn, sr)
+	s, sr := convert(si, sn, sr)
 
-	T = normalize(T)
-	tn, tr := parse2(T)
-	tn, tr = simplify2(tn, tr)
-	t, tr := convert2(tn, tr)
+	ti, tn, tr := parse(T)
+	tn, tr = simplify(tn, tr)
+	t, tr := convert(ti, tn, tr)
 
-	return s == t && sr == tr
-}
-
-// 1.2(3) -> 0.00012(3) -> 00012(3)
-func normalize(s string) string {
-	if !strings.Contains(s, ".") {
-		s += "."
-	}
-	if strings.HasSuffix(s, ".") {
-		s += "0"
-	}
-	dot := strings.Index(s, ".")
-	if dot < 4 {
-		s = strings.Repeat("0", 4-dot) + s
-	}
-	return strings.Replace("1"+s, ".", "", 1)
-}
-
-func parse2(s string) (string, string) {
-	if !strings.Contains(s, "(") {
-		return s, ""
-	}
-
-	i := strings.Index(s, "(")
-
-	nonRepeat := s[:i]
-	repeat := s[i+1 : len(s)-1]
-
-	if repeat == "0" {
-		repeat = ""
-	}
-
-	return nonRepeat, repeat
+	return s == t &&
+		sr == tr
 }
 
 func parse(s string) (string, string, string) {
@@ -66,7 +33,7 @@ func parse(s string) (string, string, string) {
 	l := strings.Index(fraction, "(")
 	if l == -1 {
 		if fraction == "0" {
-			return integer, "", ""
+			fraction = ""
 		}
 		return integer, fraction, ""
 	}
@@ -83,34 +50,6 @@ func parse(s string) (string, string, string) {
 	}
 
 	return integer, nonRepeat, repeat
-}
-
-func simplify2(nonRepeat, repeat string) (string, string) {
-	if repeat == "" {
-		return nonRepeat, repeat
-	}
-
-	if repeat == strings.Repeat(repeat[:1], len(repeat)) {
-		repeat = repeat[:1]
-	}
-
-	for repeat[:len(repeat)/2] == repeat[len(repeat)/2:] {
-		repeat = repeat[:len(repeat)/2]
-	}
-
-	for strings.HasSuffix(nonRepeat, repeat) {
-		nonRepeat = nonRepeat[:len(nonRepeat)-len(repeat)]
-	}
-
-	for i := 1; i < len(repeat); i++ {
-		if strings.HasSuffix(nonRepeat, repeat[i:]) {
-			repeat = repeat[i:] + repeat[:i]
-			nonRepeat = nonRepeat[:len(nonRepeat)-len(repeat)+i]
-			break
-		}
-	}
-
-	return nonRepeat, repeat
 }
 
 func simplify(nonRepeat, repeat string) (string, string) {
@@ -139,15 +78,6 @@ func simplify(nonRepeat, repeat string) (string, string) {
 	}
 
 	return nonRepeat, repeat
-}
-
-func convert2(nonRepeat, repeat string) (int, string) {
-	i, _ := strconv.Atoi(nonRepeat)
-	if repeat == "9" {
-		i++
-		repeat = ""
-	}
-	return i, repeat
 }
 
 func convert(integer, nonRepeat, repeat string) (int, string) {
