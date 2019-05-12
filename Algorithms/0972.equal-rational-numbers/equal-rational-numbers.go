9b533a5bae1498e0a125aaed8b0135b3b868dad3
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-12 18:29:04 +0800

    972 test pass

--- Algorithms/0972.equal-rational-numbers/equal-rational-numbers.go
+++ Algorithms/0972.equal-rational-numbers/equal-rational-numbers.go
@@ -6,22 +6,16 @@ import (
 )
 
 func isRationalEqual(S string, T string) bool {
-	is, ns, rs := parse(S)
-	it, nt, rt := parse(T)
+	si, sn, sr := parse(S)
+	sn, sr = simplify(sn, sr)
+	s, sr := convert(si, sn, sr)
 
-	// if rs == "" && rt == "" {
-	// 	return is == it && ns == nt
-	// }
-
-	ns, rs = simplify(ns, rs)
-	nt, rt = simplify(nt, rt)
-
-	s, ns, rs := convert(is, ns, rs)
-	t, nt, rt := convert(it, nt, rt)
+	ti, tn, tr := parse(T)
+	tn, tr = simplify(tn, tr)
+	t, tr := convert(ti, tn, tr)
 
 	return s == t &&
-		ns == nt &&
-		rs == rt
+		sr == tr
 }
 
 func parse(s string) (string, string, string) {
@@ -31,13 +25,16 @@ func parse(s string) (string, string, string) {
 
 	dot := strings.Index(s, ".")
 	if dot == -1 {
-		return s, "0", ""
+		return s, "", ""
 	}
 
 	integer, fraction := s[:dot], s[dot+1:]
 
 	l := strings.Index(fraction, "(")
 	if l == -1 {
+		if fraction == "0" {
+			fraction = ""
+		}
 		return integer, fraction, ""
 	}
 
@@ -48,8 +45,8 @@ func parse(s string) (string, string, string) {
 		repeat = ""
 	}
 
-	if repeat == "" && nonRepeat == "" {
-		nonRepeat = "0"
+	if repeat == "" && nonRepeat == "0" {
+		nonRepeat = ""
 	}
 
 	return integer, nonRepeat, repeat
@@ -71,7 +68,7 @@ func simplify(nonRepeat, repeat string) (string, string) {
 	for i := 1; i < len(repeat); i++ {
 		if strings.HasSuffix(nonRepeat, repeat[i:]) {
 			repeat = repeat[i:] + repeat[:i]
-			nonRepeat = nonRepeat[:len(nonRepeat)-i]
+			nonRepeat = nonRepeat[:len(nonRepeat)-len(repeat)+i]
 			break
 		}
 	}
@@ -79,12 +76,18 @@ func simplify(nonRepeat, repeat string) (string, string) {
 	return nonRepeat, repeat
 }
 
-func convert(integer, nonRepeat, repeat string) (int, string, string) {
+func convert(integer, nonRepeat, repeat string) (int, string) {
 	i, _ := strconv.Atoi(integer)
-	if nonRepeat == "" && repeat == "9" {
+	for j := len(nonRepeat); j > 0; j-- {
+		i *= 10
+	}
+	if nonRepeat != "" {
+		n, _ := strconv.Atoi(nonRepeat)
+		i += n
+	}
+	if repeat == "9" {
 		i++
 		repeat = ""
-		nonRepeat = "0"
 	}
-	return i, nonRepeat, repeat
+	return i, repeat
 }
