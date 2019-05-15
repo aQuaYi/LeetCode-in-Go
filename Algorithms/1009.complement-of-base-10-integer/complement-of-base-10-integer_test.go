81cb194e34c16818d6c8a7dad69501ec493262a2
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-15 21:21:15 +0800

    1009 added

---
+++ Algorithms/1009.complement-of-base-10-integer/complement-of-base-10-integer_test.go
@@ -0,0 +1,47 @@
+package problem1009
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	N   int
+	ans int
+}{
+
+	{
+		5,
+		2,
+	},
+
+	{
+		7,
+		0,
+	},
+
+	{
+		10,
+		5,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_bitwiseComplement(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		ast.Equal(tc.ans, bitwiseComplement(tc.N), "输入:%v", tc)
+	}
+}
+
+func Benchmark_bitwiseComplement(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			bitwiseComplement(tc.N)
+		}
+	}
+}
