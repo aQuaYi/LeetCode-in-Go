a478242d20d7231253e11cb3c8df533cef517676
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-15 21:55:25 +0800

    1009 accepted. 0ms

--- Algorithms/1009.complement-of-base-10-integer/complement-of-base-10-integer.go
+++ Algorithms/1009.complement-of-base-10-integer/complement-of-base-10-integer.go
@@ -1,6 +1,15 @@
 package problem1009
 
 func bitwiseComplement(N int) int {
+	b := bit(N)
+	return (1<<b - 1) ^ N
+}
 
-	return 0
+func bit(N int) uint {
+	b := uint(0)
+	for N > 0 {
+		b++
+		N >>= 1
+	}
+	return b
 }
