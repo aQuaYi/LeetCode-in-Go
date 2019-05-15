a27e74ce208dbcf5db7d28349481d5532e386475
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-16 00:33:30 +0800

    1009 添加了小注释

--- Algorithms/1009.complement-of-base-10-integer/complement-of-base-10-integer.go
+++ Algorithms/1009.complement-of-base-10-integer/complement-of-base-10-integer.go
@@ -1,11 +1,14 @@
 package problem1009
 
 func bitwiseComplement(N int) int {
-	x := N | 1
+	x := N
+	x |= 1 // when x is 0
 	x |= x >> 1
 	x |= x >> 2
 	x |= x >> 4
 	x |= x >> 8
 	x |= x >> 16
+	// if N is 000101011101
+	// then  x 000111111111
 	return x ^ N
 }
