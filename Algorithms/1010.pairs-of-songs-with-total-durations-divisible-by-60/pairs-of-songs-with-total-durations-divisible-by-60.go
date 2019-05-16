fdcbc9a9759e6e54767c89717af729dc925293ae
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-16 21:54:53 +0800

    1010 test pass

--- Algorithms/1010.pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.go
+++ Algorithms/1010.pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.go
@@ -1,6 +1,12 @@
 package problem1010
 
 func numPairsDivisibleBy60(time []int) int {
-
-	return 0
+	rec := [60]int{}
+	res := 0
+	for _, t := range time {
+		t %= 60
+		res += rec[(60-t)%60]
+		rec[t]++
+	}
+	return res
 }
