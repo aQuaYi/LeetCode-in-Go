cda737f6f6c82de6fa58f1d3bf18257037b7132d
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-17 20:33:39 +0800

    971 added

---
+++ Algorithms/0971.flip-binary-tree-to-match-preorder-traversal/flip-binary-tree-to-match-preorder-traversal.go
@@ -0,0 +1,19 @@
+package problem0971
+
+import "github.com/aQuaYi/LeetCode-in-Go/kit"
+
+// TreeNode is pre-defined.
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+type TreeNode = kit.TreeNode
+
+func flipMatchVoyage(root *TreeNode, voyage []int) []int {
+
+	return nil
+}
