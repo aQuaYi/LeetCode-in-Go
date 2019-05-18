a6ff1599748f00f7f26424db66032c971aeb4c8c
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-18 19:42:29 +0800

    971 test fail

--- Algorithms/0971.flip-binary-tree-to-match-preorder-traversal/flip-binary-tree-to-match-preorder-traversal.go
+++ Algorithms/0971.flip-binary-tree-to-match-preorder-traversal/flip-binary-tree-to-match-preorder-traversal.go
@@ -14,6 +14,50 @@ import "github.com/aQuaYi/LeetCode-in-Go/kit"
 type TreeNode = kit.TreeNode
 
 func flipMatchVoyage(root *TreeNode, voyage []int) []int {
+	res := make([]int, 0, len(voyage))
+	if rescur(root, voyage, &res) {
+		return res
+	}
+	return []int{-1}
+}
 
-	return nil
+func rescur(root *TreeNode, voyage []int, res *[]int) bool {
+	if root == nil {
+		if len(voyage) == 0 {
+			return true
+		}
+		return false
+	}
+
+	if len(voyage) == 0 || root.Val != voyage[0] {
+		return false
+	}
+
+	if len(voyage) < 2 && root.Left != nil {
+		return false
+	}
+
+	if voyage[1] != root.Left.Val {
+		*res = append(*res, root.Val)
+		root.Left, root.Right = root.Right, root.Left
+	}
+
+	right := 0
+	if root.Right != nil {
+		right = root.Right.Val
+	}
+
+	l, r := split(voyage, right)
+
+	return rescur(root.Left, l, res) && rescur(root.Right, r, res)
+}
+
+func split(voyage []int, right int) ([]int, []int) {
+	begin := 1
+	for ; begin < len(voyage); begin++ {
+		if voyage[begin] == right {
+			break
+		}
+	}
+	return voyage[:begin], voyage[begin:]
 }
