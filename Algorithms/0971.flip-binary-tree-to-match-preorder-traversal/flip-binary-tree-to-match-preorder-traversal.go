e0a3a4d0e53d5e1523c39d6db0dcdb1532a3b54f
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-18 20:58:15 +0800

    971 finish

--- Algorithms/0971.flip-binary-tree-to-match-preorder-traversal/flip-binary-tree-to-match-preorder-traversal.go
+++ Algorithms/0971.flip-binary-tree-to-match-preorder-traversal/flip-binary-tree-to-match-preorder-traversal.go
@@ -15,46 +15,24 @@ type TreeNode = kit.TreeNode
 
 func flipMatchVoyage(root *TreeNode, voyage []int) []int {
 	res := make([]int, 0, len(voyage))
-	if rescur(root, voyage, &res) {
+	index := 0
+	var dfs func(*TreeNode) bool
+	dfs = func(node *TreeNode) bool {
+		if node == nil {
+			return true
+		}
+		if node.Val != voyage[index] {
+			return false
+		}
+		index++
+		if node.Left != nil && node.Left.Val != voyage[index] {
+			res = append(res, node.Val)
+			return dfs(node.Right) && dfs(node.Left)
+		}
+		return dfs(node.Left) && dfs(node.Right)
+	}
+	if dfs(root) {
 		return res
 	}
 	return []int{-1}
-}
-
-func rescur(root *TreeNode, voyage []int, res *[]int) bool {
-	if len(voyage) == 0 || root.Val != voyage[0] {
-		return false
-	}
-
-	if root.Left == nil && root.Right == nil {
-		return true
-	}
-
-	if root.Left == nil {
-		return rescur(root.Right, voyage[1:], res)
-	}
-
-	if root.Right == nil {
-		return rescur(root.Left, voyage[1:], res)
-	}
-
-	if voyage[1] != root.Left.Val {
-		*res = append(*res, root.Val)
-		root.Left, root.Right = root.Right, root.Left
-	}
-
-	right := root.Right.Val
-	l, r := split(voyage, right)
-
-	return rescur(root.Left, l, res) && rescur(root.Right, r, res)
-}
-
-func split(voyage []int, right int) ([]int, []int) {
-	begin := 1
-	for ; begin < len(voyage); begin++ {
-		if voyage[begin] == right {
-			break
-		}
-	}
-	return voyage[1:begin], voyage[begin:]
 }
