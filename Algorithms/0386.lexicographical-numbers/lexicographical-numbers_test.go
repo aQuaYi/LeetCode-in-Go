2606f1dc1c2b340e7e6d3b512c54e7fedf65dd8e
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-19 13:30:23 +0800

    386 added

--- Algorithms/0386.lexicographical-numbers/lexicographical-numbers_test.go
+++ Algorithms/0386.lexicographical-numbers/lexicographical-numbers_test.go
@@ -8,9 +8,14 @@ import (
 
 // tcs is testcase slice
 var tcs = []struct {
-	n   int
+	max int
 	ans []int
 }{
+
+	{
+		120,
+		[]int{1, 10, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 11, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 12, 120, 13, 14, 15, 16, 17, 18, 19, 2, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 3, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 4, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 5, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 6, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 7, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 8, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 9, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99},
+	},
 
 	{
 		13,
@@ -24,14 +29,14 @@ func Test_lexicalOrder(t *testing.T) {
 	ast := assert.New(t)
 
 	for _, tc := range tcs {
-		ast.Equal(tc.ans, lexicalOrder(tc.n), "输入:%v", tc)
+		ast.Equal(tc.ans, lexicalOrder(tc.max), "输入:%v", tc)
 	}
 }
 
 func Benchmark_lexicalOrder(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		for _, tc := range tcs {
-			lexicalOrder(tc.n)
+			lexicalOrder(tc.max)
 		}
 	}
 }
