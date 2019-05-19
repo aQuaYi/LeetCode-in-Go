57e402b35f36246467f87393b1a79c16e3f6697c
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-19 13:48:27 +0800

    386 accepted.

--- Algorithms/0386.lexicographical-numbers/lexicographical-numbers.go
+++ Algorithms/0386.lexicographical-numbers/lexicographical-numbers.go
@@ -1,6 +1,18 @@
 package problem0386
 
 func lexicalOrder(max int) []int {
+	res := make([]int, 0, max)
 
-	return nil
+	var dfs func(int)
+	dfs = func(begin int) {
+		limit := (begin + 10) / 10 * 10
+		for begin <= max && begin < limit {
+			res = append(res, begin)
+			dfs(begin * 10)
+			begin++
+		}
+	}
+
+	dfs(1)
+	return res
 }
