d812cfea47580cc3edb6c8d75ebd52e9c8001776
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-19 14:29:08 +0800

    912 finish

--- Algorithms/0912.sort-an-array/sort-an-array.go
+++ Algorithms/0912.sort-an-array/sort-an-array.go
@@ -1,8 +1,30 @@
 package problem0912
 
-import "sort"
-
 func sortArray(nums []int) []int {
-	sort.Ints(nums)
+	quick3way(nums, 0, len(nums)-1)
 	return nums
+}
+
+// 3 way quick sort
+func quick3way(nums []int, lo, hi int) {
+	if lo >= hi {
+		return
+	}
+	lt, i, gt := lo, lo+1, hi
+	v := nums[lo]
+	for i <= gt {
+		switch {
+		case nums[i] < v:
+			nums[lt], nums[i] = nums[i], nums[lt]
+			lt++
+			i++
+		case nums[i] > v:
+			nums[i], nums[gt] = nums[gt], nums[i]
+			gt--
+		default:
+			i++
+		}
+	}
+	quick3way(nums, lo, lt-1)
+	quick3way(nums, gt+1, hi)
 }
