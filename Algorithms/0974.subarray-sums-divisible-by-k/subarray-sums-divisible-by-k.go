caac3f5456e421d20d36b16dc89913b285e680e5
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-20 21:12:28 +0800

    974 finish

--- Algorithms/0974.subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.go
+++ Algorithms/0974.subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.go
@@ -1,6 +1,13 @@
 package problem0974
 
 func subarraysDivByK(A []int, K int) int {
-
-	return 0
+	count := [10001]int{}
+	count[0] = 1
+	prefix, res := 0, 0
+	for _, a := range A {
+		prefix = (prefix + a%K + K) % K
+		res += count[prefix] // 减去相同的 prefix，剩下的就都是 K 的倍数
+		count[prefix]++
+	}
+	return res
 }
