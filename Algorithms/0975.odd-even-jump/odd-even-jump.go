a84350c25f57deef20badfdc74eb9ae213289903
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-21 22:37:14 +0800

    975 accepted. 52ms

--- Algorithms/0975.odd-even-jump/odd-even-jump.go
+++ Algorithms/0975.odd-even-jump/odd-even-jump.go
@@ -1,69 +1,69 @@
 package problem0975
 
+import "sort"
+
+// ref: https://leetcode.com/problems/odd-even-jump/discuss/217981/JavaC%2B%2BPython-DP-idea-Using-TreeMap-or-Stack
 func oddEvenJumps(A []int) int {
-	odds := make(map[int]bool, len(A))
-	evens := make(map[int]bool, len(A))
+	n := len(A)
+
+	AI := make([][2]int, n)
+	for i, a := range A {
+		AI[i] = [2]int{a, i}
+	}
+
+	sort.Slice(AI, func(i int, j int) bool {
+		if AI[i][0] == AI[j][0] {
+			return AI[i][1] < AI[j][1]
+		}
+		return AI[i][0] < AI[j][0]
+	})
+
+	nextHigher := make([]int, n)
+	stack := make([]int, 0, n)
+	for _, ai := range AI {
+		i := ai[1]
+		for len(stack) > 0 && stack[len(stack)-1] < i {
+			pop := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			nextHigher[pop] = i
+		}
+		stack = append(stack, i)
+	}
+
+	sort.Slice(AI, func(i int, j int) bool {
+		if AI[i][0] == AI[j][0] {
+			return AI[i][1] < AI[j][1]
+		}
+		return AI[i][0] > AI[j][0]
+	})
+
+	nextLower := make([]int, n)
+	stack = make([]int, 0, n)
+	for _, ai := range AI {
+		i := ai[1]
+		for len(stack) > 0 && stack[len(stack)-1] < i {
+			pop := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			nextLower[pop] = i
+		}
+		stack = append(stack, i)
+	}
+
+	higher := make([]int, n)
+	lower := make([]int, n)
+	higher[n-1] = 1
+	lower[n-1] = 1
+	for i := n - 2; i >= 0; i-- {
+		higher[i] = lower[nextHigher[i]]
+		lower[i] = higher[nextLower[i]]
+	}
+	return sum(higher)
+}
+
+func sum(A []int) int {
 	res := 0
-	for i := 0; i < len(A); i++ {
-		oddJump(A, i, odds, evens, &res)
+	for _, a := range A {
+		res += a
 	}
 	return res
-}
-
-func oddJump(A []int, i int, odds, evens map[int]bool, res *int) bool {
-	if len(A)-1 == i || odds[i] {
-		*res++
-		return true
-	}
-	if success, ok := odds[i]; ok && !success {
-		return false
-	}
-	j := oddNumberedJumps(A, i)
-	if j == -1 {
-		odds[j] = false
-		return false
-	}
-	odds[i] = evenJump(A, j, odds, evens, res)
-	return odds[i]
-}
-
-func evenJump(A []int, i int, odds, evens map[int]bool, res *int) bool {
-	if len(A)-1 == i || evens[i] {
-		*res++
-		return true
-	}
-	if success, ok := evens[i]; ok && !success {
-		return false
-	}
-	j := evenNumberedJumps(A, i)
-	if j == -1 {
-		evens[j] = false
-		return false
-	}
-	evens[i] = oddJump(A, j, odds, evens, res)
-	return evens[i]
-}
-
-func oddNumberedJumps(A []int, i int) int {
-	minNum := 100000
-	index := -1
-	for j := len(A) - 1; j > i; j-- {
-		if A[i] <= A[j] && minNum >= A[j] {
-			minNum = A[j]
-			index = j
-		}
-	}
-	return index
-}
-
-func evenNumberedJumps(A []int, i int) int {
-	maxNum := -1
-	index := -1
-	for j := len(A) - 1; j > i; j-- {
-		if A[i] >= A[j] && maxNum <= A[j] {
-			maxNum = A[j]
-			index = j
-		}
-	}
-	return index
 }
