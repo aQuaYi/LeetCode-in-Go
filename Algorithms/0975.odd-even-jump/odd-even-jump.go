e3626689a77f6f13d66b1e6414d124917482a704
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-21 23:02:23 +0800

    975 finish. 36ms

--- Algorithms/0975.odd-even-jump/odd-even-jump.go
+++ Algorithms/0975.odd-even-jump/odd-even-jump.go
@@ -4,58 +4,53 @@ import "sort"
 
 // ref: https://leetcode.com/problems/odd-even-jump/discuss/217981/JavaC%2B%2BPython-DP-idea-Using-TreeMap-or-Stack
 func oddEvenJumps(A []int) int {
-	n := len(A)
+	size := len(A)
 
-	AI := make([][2]int, n)
-	for i, a := range A {
-		AI[i] = [2]int{a, i}
+	indexs := make([]int, size)
+	for i := range indexs {
+		indexs[i] = i
 	}
 
-	sort.Slice(AI, func(i int, j int) bool {
-		if AI[i][0] == AI[j][0] {
-			return AI[i][1] < AI[j][1]
+	sort.Slice(indexs, func(i int, j int) bool {
+		if A[indexs[i]] == A[indexs[j]] {
+			return indexs[i] < indexs[j]
 		}
-		return AI[i][0] < AI[j][0]
+		return A[indexs[i]] < A[indexs[j]]
 	})
 
-	nextHigher := make([]int, n)
-	stack := make([]int, 0, n)
-	for _, ai := range AI {
-		i := ai[1]
-		for len(stack) > 0 && stack[len(stack)-1] < i {
+	nextHigher := make([]int, size)
+	stack := make([]int, 0, size)
+	for _, j := range indexs {
+		for len(stack) > 0 && stack[len(stack)-1] < j {
 			pop := stack[len(stack)-1]
 			stack = stack[:len(stack)-1]
-			nextHigher[pop] = i
+			nextHigher[pop] = j
 		}
-		stack = append(stack, i)
+		stack = append(stack, j)
 	}
 
-	sort.Slice(AI, func(i int, j int) bool {
-		if AI[i][0] == AI[j][0] {
-			return AI[i][1] < AI[j][1]
+	sort.Slice(indexs, func(i int, j int) bool {
+		if A[indexs[i]] == A[indexs[j]] {
+			return indexs[i] < indexs[j]
 		}
-		return AI[i][0] > AI[j][0]
+		return A[indexs[i]] > A[indexs[j]]
 	})
 
-	nextLower := make([]int, n)
-	stack = make([]int, 0, n)
-	for _, ai := range AI {
-		i := ai[1]
-		for len(stack) > 0 && stack[len(stack)-1] < i {
+	nextLower := make([]int, size)
+	stack = make([]int, 0, size)
+	for _, j := range indexs {
+		for len(stack) > 0 && stack[len(stack)-1] < j {
 			pop := stack[len(stack)-1]
 			stack = stack[:len(stack)-1]
-			nextLower[pop] = i
+			nextLower[pop] = j
 		}
-		stack = append(stack, i)
+		stack = append(stack, j)
 	}
 
-	higher := make([]int, n)
-	lower := make([]int, n)
-	higher[n-1] = 1
-	lower[n-1] = 1
-	for i := n - 2; i >= 0; i-- {
-		higher[i] = lower[nextHigher[i]]
-		lower[i] = higher[nextLower[i]]
+	higher, lower := make([]int, size), make([]int, size)
+	higher[size-1], lower[size-1] = 1, 1
+	for i := size - 2; i >= 0; i-- {
+		higher[i], lower[i] = lower[nextHigher[i]], higher[nextLower[i]]
 	}
 	return sum(higher)
 }
