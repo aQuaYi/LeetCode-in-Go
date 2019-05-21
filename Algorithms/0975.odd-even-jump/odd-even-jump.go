0dafb0cf74b92218b8e6bcabd6a1f5f048949fca
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-21 21:44:01 +0800

    975 accepted. 3944 ms, faster than 20.83%

--- Algorithms/0975.odd-even-jump/odd-even-jump.go
+++ Algorithms/0975.odd-even-jump/odd-even-jump.go
@@ -1,6 +1,69 @@
 package problem0975
 
 func oddEvenJumps(A []int) int {
+	odds := make(map[int]bool, len(A))
+	evens := make(map[int]bool, len(A))
+	res := 0
+	for i := 0; i < len(A); i++ {
+		oddJump(A, i, odds, evens, &res)
+	}
+	return res
+}
 
-	return 0
+func oddJump(A []int, i int, odds, evens map[int]bool, res *int) bool {
+	if len(A)-1 == i || odds[i] {
+		*res++
+		return true
+	}
+	if success, ok := odds[i]; !success && ok {
+		return false
+	}
+	j := oddNumberedJumps(A, i)
+	if j == -1 {
+		odds[j] = false
+		return false
+	}
+	odds[i] = evenJump(A, j, odds, evens, res)
+	return odds[i]
+}
+
+func evenJump(A []int, i int, odds, evens map[int]bool, res *int) bool {
+	if len(A)-1 == i || evens[i] {
+		*res++
+		return true
+	}
+	if success, ok := evens[i]; !success && ok {
+		return false
+	}
+	j := evenNumberedJumps(A, i)
+	if j == -1 {
+		evens[j] = false
+		return false
+	}
+	evens[i] = oddJump(A, j, odds, evens, res)
+	return evens[i]
+}
+
+func oddNumberedJumps(A []int, i int) int {
+	minNum := 100000
+	index := -1
+	for j := len(A) - 1; j > i; j-- {
+		if A[i] <= A[j] && minNum >= A[j] {
+			minNum = A[j]
+			index = j
+		}
+	}
+	return index
+}
+
+func evenNumberedJumps(A []int, i int) int {
+	maxNum := -1
+	index := -1
+	for j := len(A) - 1; j > i; j-- {
+		if A[i] >= A[j] && maxNum <= A[j] {
+			maxNum = A[j]
+			index = j
+		}
+	}
+	return index
 }
