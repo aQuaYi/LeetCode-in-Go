ac523f2a798e0ce2cb42351b73f2f0d8e141c70b
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-22 20:00:42 +0800

    1013 wrong answer

--- Algorithms/1013.partition-array-into-three-parts-with-equal-sum/partition-array-into-three-parts-with-equal-sum.go
+++ Algorithms/1013.partition-array-into-three-parts-with-equal-sum/partition-array-into-three-parts-with-equal-sum.go
@@ -1,13 +1,30 @@
 package problem1013
 
+import "sort"
+
 func canThreePartsEqualSum(A []int) bool {
-
-}
-
-func sum(A []int) int {
-	sum := 0
-	for _, a := range A {
-		sum += a
+	n := len(A)
+	sums := make([]int, n)
+	sums[0] = A[0]
+	for i := 1; i < n; i++ {
+		sums[i] = sums[i-1] + A[i]
 	}
-	return sum
+	sum := sums[n-1]
+	if sum%3 != 0 {
+		return false
+	}
+
+	sort.Ints(sums)
+
+	i := sort.SearchInts(sums, sum/3)
+	if i == n || sums[i] != sum/3 {
+		return false
+	}
+
+	j := sort.SearchInts(sums, sum/3*2)
+	if j == n || sums[j] != sum/3*2 {
+		return false
+	}
+
+	return i < j
 }
