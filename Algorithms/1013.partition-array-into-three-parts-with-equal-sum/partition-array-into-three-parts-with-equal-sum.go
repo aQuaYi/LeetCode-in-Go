fd2d41bb158cee033c7b7ffbb62305f7eebb3336
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-22 19:46:08 +0800

    1013

--- Algorithms/1013.partition-array-into-three-parts-with-equal-sum/partition-array-into-three-parts-with-equal-sum.go
+++ Algorithms/1013.partition-array-into-three-parts-with-equal-sum/partition-array-into-three-parts-with-equal-sum.go
@@ -1,6 +1,18 @@
 package problem1013
 
 func canThreePartsEqualSum(A []int) bool {
+	s := sum(A)
+	if s%3 != 0 {
+		return false
+	}
 
 	return false
+}
+
+func sum(A []int) int {
+	sum := 0
+	for _, a := range A {
+		sum += a
+	}
+	return sum
 }
