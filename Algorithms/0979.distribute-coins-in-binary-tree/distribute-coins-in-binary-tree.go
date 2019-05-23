b9eb2d671f1f96d722a02c028532d177c69cdebe
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-23 20:49:59 +0800

    979 finish

--- Algorithms/0979.distribute-coins-in-binary-tree/distribute-coins-in-binary-tree.go
+++ Algorithms/0979.distribute-coins-in-binary-tree/distribute-coins-in-binary-tree.go
@@ -6,6 +6,23 @@ import "github.com/aQuaYi/LeetCode-in-Go/kit"
 type TreeNode = kit.TreeNode
 
 func distributeCoins(root *TreeNode) int {
+	res := 0
+	dfs(root, &res)
+	return res
+}
 
-	return 0
+func dfs(node *TreeNode, res *int) int {
+	if node == nil {
+		return 0
+	}
+	l, r := dfs(node.Left, res), dfs(node.Right, res)
+	*res += abs(l) + abs(r)
+	return l + r + node.Val - 1
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
 }
