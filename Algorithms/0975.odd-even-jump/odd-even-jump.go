c93dfb52672904e020d183dcc2daeadfcee498d8
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-23 09:18:52 +0800

    975 improved.

--- Algorithms/0975.odd-even-jump/odd-even-jump.go
+++ Algorithms/0975.odd-even-jump/odd-even-jump.go
@@ -18,34 +18,11 @@ func oddEvenJumps(A []int) int {
 		return A[indexs[i]] < A[indexs[j]]
 	})
 
-	nextHigher := make([]int, size)
-	stack := make([]int, 0, size)
-	for _, j := range indexs {
-		for len(stack) > 0 && stack[len(stack)-1] < j {
-			pop := stack[len(stack)-1]
-			nextHigher[pop] = j
-			stack = stack[:len(stack)-1]
-		}
-		stack = append(stack, j)
-	}
+	nextHigher := nextIndex(indexs)
 
-	sort.Slice(indexs, func(i int, j int) bool {
-		if A[indexs[i]] == A[indexs[j]] {
-			return indexs[i] < indexs[j]
-		}
-		return A[indexs[i]] > A[indexs[j]]
-	})
+	ascToDes(A, indexs)
 
-	nextLower := make([]int, size)
-	stack = make([]int, 0, size)
-	for _, j := range indexs {
-		for len(stack) > 0 && stack[len(stack)-1] < j {
-			pop := stack[len(stack)-1]
-			nextLower[pop] = j
-			stack = stack[:len(stack)-1]
-		}
-		stack = append(stack, j)
-	}
+	nextLower := nextIndex(indexs)
 
 	higher, lower := make([]int, size), make([]int, size)
 	higher[size-1], lower[size-1] = 1, 1
@@ -53,6 +30,46 @@ func oddEvenJumps(A []int) int {
 		higher[i], lower[i] = lower[nextHigher[i]], higher[nextLower[i]]
 	}
 	return sum(higher)
+}
+
+func nextIndex(indexs []int) []int {
+	size := len(indexs)
+	res := make([]int, size)
+	stack := make([]int, 0, size)
+	for _, j := range indexs {
+		for len(stack) > 0 && stack[len(stack)-1] < j {
+			pop := stack[len(stack)-1]
+			res[pop] = j
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, j)
+	}
+	return res
+}
+
+func ascToDes(A, indexs []int) {
+	i, size := 0, len(A)
+	for i+1 < size {
+		if A[indexs[i]] != A[indexs[i+1]] {
+			i++
+			continue
+		}
+		a, j := A[indexs[i]], i+1
+		for j+1 < size && A[indexs[j+1]] == a {
+			j++
+		}
+		reverse(indexs, i, j)
+		i = j + 1
+	}
+	reverse(indexs, 0, size-1)
+}
+
+func reverse(A []int, i, j int) {
+	for i < j {
+		A[i], A[j] = A[j], A[i]
+		i++
+		j--
+	}
 }
 
 func sum(A []int) int {
