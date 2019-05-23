7a07770038066d23596bce109a24041a3268a3b6
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-23 20:11:36 +0800

    979 added

---
+++ Algorithms/0979.distribute-coins-in-binary-tree/distribute-coins-in-binary-tree.go
@@ -0,0 +1,13 @@
+package problem0979
+
+import (
+	"github.com/aQuaYi/Data-Structure-and-Algorithms-in-Go/kit"
+)
+
+// TreeNode is pre-defined...
+type TreeNode = kit.TreeNode
+
+func distributeCoins(root *TreeNode) int {
+
+	return 0
+}
