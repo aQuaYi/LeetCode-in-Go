91b9f47235120d7fe9547ffdf1c9ed385bde6499
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-24 21:16:49 +0800

    978 accepted. 64ms

--- Algorithms/0978.longest-turbulent-subarray/longest-turbulent-subarray.go
+++ Algorithms/0978.longest-turbulent-subarray/longest-turbulent-subarray.go
@@ -3,29 +3,20 @@ package problem0978
 func maxTurbulenceSize(A []int) int {
 	size := len(A)
 	greater, less := make([]int, size), make([]int, size)
-	for i := 0; i < size; i++ {
-		greater[i] = 1
-		less[i] = 1
-	}
 
+	res := 0
 	for i := 1; i < size; i++ {
 		switch {
 		case A[i] > A[i-1]:
 			greater[i] = less[i-1] + 1
+			res = max(res, greater[i])
 		case A[i] < A[i-1]:
 			less[i] = greater[i-1] + 1
+			res = max(res, less[i])
 		}
 	}
 
-	return max(maxOf(greater), maxOf(less))
-}
-
-func maxOf(A []int) int {
-	res := A[0]
-	for i := 1; i < len(A); i++ {
-		res = max(res, A[i])
-	}
-	return res
+	return res + 1
 }
 
 func max(a, b int) int {
