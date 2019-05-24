06be0c1414633090300bb1244b23cc946aadadd5
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-24 20:54:09 +0800

    978 added

---
+++ Algorithms/0978.longest-turbulent-subarray/longest-turbulent-subarray_test.go
@@ -0,0 +1,34 @@
+package problem0978
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	A []int
+	ans int
+}{
+
+
+
+	// 可以有多个 testcase
+}
+
+func Test_maxTurbulenceSize(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		ast.Equal(tc.ans, maxTurbulenceSize(tc.A), "输入:%v", tc)
+	}
+}
+
+func Benchmark_maxTurbulenceSize(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			maxTurbulenceSize(tc.A)
+		}
+	}
+}
