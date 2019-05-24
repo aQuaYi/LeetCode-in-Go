92aac8b682ba2e629dbe8deb1bc5db301330d42d
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-24 20:55:03 +0800

    978 added

--- Algorithms/0978.longest-turbulent-subarray/longest-turbulent-subarray_test.go
+++ Algorithms/0978.longest-turbulent-subarray/longest-turbulent-subarray_test.go
@@ -8,11 +8,24 @@ import (
 
 // tcs is testcase slice
 var tcs = []struct {
-	A []int
+	A   []int
 	ans int
 }{
 
+	{
+		[]int{9, 4, 2, 10, 7, 8, 8, 1, 9},
+		5,
+	},
 
+	{
+		[]int{4, 8, 12, 16},
+		2,
+	},
+
+	{
+		[]int{100},
+		1,
+	},
 
 	// 可以有多个 testcase
 }
