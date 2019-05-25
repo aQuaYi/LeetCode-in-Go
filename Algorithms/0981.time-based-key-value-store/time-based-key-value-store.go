9cbe8f592cbacebb0bc08a686c2e2e2013a81b55
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-25 15:35:57 +0800

    981 accepted. 532ms

--- Algorithms/0981.time-based-key-value-store/time-based-key-value-store.go
+++ Algorithms/0981.time-based-key-value-store/time-based-key-value-store.go
@@ -20,21 +20,21 @@ func Constructor() TimeMap {
 // Note 3:
 // The timestamps for all TimeMap.set operations are strictly increasing.
 func (m *TimeMap) Set(key string, value string, timestamp int) {
+	if _, ok := m.times[key]; !ok {
+		m.times[key] = make([]int, 1, 1024)
+		m.strings[key] = make([]string, 1, 1024)
+	}
 	m.times[key] = append(m.times[key], timestamp)
 	m.strings[key] = append(m.strings[key], value)
 }
 
 // Get the value of key
 func (m *TimeMap) Get(key string, timestamp int) string {
-	if len(m.times[key]) == 0 ||
-		timestamp < m.times[key][0] {
-		return ""
-	}
-	i := sort.SearchInts(m.times[key], timestamp)
-	if i == len(m.times[key]) ||
-		timestamp < m.times[key][i] {
-		i--
-	}
+	times := m.times[key]
+	i := sort.Search(len(times), func(i int) bool {
+		return timestamp < times[i]
+	})
+	i--
 	return m.strings[key][i]
 }
 
