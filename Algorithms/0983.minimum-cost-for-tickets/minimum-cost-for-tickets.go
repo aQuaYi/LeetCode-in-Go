0229d00f884df4ff27c554b25ffb884d3f2255c8
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-28 20:30:41 +0800

    983 doing

--- Algorithms/0983.minimum-cost-for-tickets/minimum-cost-for-tickets.go
+++ Algorithms/0983.minimum-cost-for-tickets/minimum-cost-for-tickets.go
@@ -1,6 +1,46 @@
 package problem0983
 
 func mincostTickets(days []int, costs []int) int {
+	size := len(days)
+	dp := [366]int{}
+	endDay := days[size-1]
 
-	return 0
+	ds := days
+	for i, c := 1, costs[0]; i <= endDay; i++ {
+		if i == ds[0] {
+			dp[i] = dp[i-1] + c
+			ds = ds[1:]
+			continue
+		}
+		dp[i] = dp[i-1]
+	}
+
+	ds = days
+	for i, c := 7, costs[1]; i <= endDay; i++ {
+		if i == ds[0] {
+			dp[i] = min(dp[i], dp[i-1]+c)
+			ds = ds[1:]
+			continue
+		}
+		dp[i] = dp[i-1]
+	}
+
+	ds = days
+	for i, c := 30, costs[2]; i <= endDay; i++ {
+		if i == ds[0] {
+			dp[i] = min(dp[i], dp[i-1]+c)
+			ds = ds[1:]
+			continue
+		}
+		dp[i] = dp[i-1]
+	}
+
+	return dp[endDay]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
