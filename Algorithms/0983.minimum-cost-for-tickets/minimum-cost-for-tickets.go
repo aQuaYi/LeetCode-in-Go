4799ebf8803ff39532982112c39f39917f470d49
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-28 21:43:19 +0800

    983 finish

--- Algorithms/0983.minimum-cost-for-tickets/minimum-cost-for-tickets.go
+++ Algorithms/0983.minimum-cost-for-tickets/minimum-cost-for-tickets.go
@@ -1,33 +1,37 @@
 package problem0983
 
-var intervals = []int{1, 7, 30}
-
+// ref: https://leetcode.com/problems/minimum-cost-for-tickets/discuss/226659/Two-DP-solutions-with-pictures
 func mincostTickets(days []int, costs []int) int {
-	size := len(days)
-	endDay := days[size-1]
+	c1, c7, c30 := costs[0], costs[1], costs[2]
+	endDay := days[len(days)-1]
 
 	dp := [366]int{}
-	for i := 1; i <= endDay; i++ {
-		dp[i] = 365000
-	}
 
-	for i := 1; i <= endDay; i++ {
-		if i != days[0] {
-			dp[i] = dp[i-1]
+	for d, i := 1, 0; d <= endDay; d++ {
+		if d == days[i] {
+			dp[d] = min(dp[d-1]+c1, dp[max(0, d-7)]+c7, dp[max(0, d-30)]+c30)
+			i++
 			continue
 		}
-		for j := 0; j < 3 && i-intervals[j] >= 0; j++ {
-			dp[i] = min(dp[i], dp[i-intervals[j]]+costs[j])
-		}
-		days = days[1:]
+		dp[d] = dp[d-1]
 	}
 
 	return dp[endDay]
 }
 
-func min(a, b int) int {
-	if a < b {
+func max(a, b int) int {
+	if a > b {
 		return a
 	}
 	return b
+}
+
+func min(a, b, c int) int {
+	if a <= b && a <= c {
+		return a
+	}
+	if b <= a && b <= c {
+		return b
+	}
+	return c
 }
