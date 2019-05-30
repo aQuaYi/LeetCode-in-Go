e20d3fea0c5040681f2bc85efdb8d9c5824ba342
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-30 19:49:26 +0800

    983 100% coverage

--- Algorithms/0983.minimum-cost-for-tickets/minimum-cost-for-tickets_test.go
+++ Algorithms/0983.minimum-cost-for-tickets/minimum-cost-for-tickets_test.go
@@ -49,3 +49,42 @@ func Benchmark_mincostTickets(b *testing.B) {
 		}
 	}
 }
+
+func Test_min(t *testing.T) {
+	type args struct {
+		a int
+		b int
+		c int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+
+		{
+			"a 最小",
+			args{a: 0, b: 1, c: 2},
+			0,
+		},
+
+		{
+			"b 最小",
+			args{a: 1, b: 0, c: 2},
+			0,
+		},
+
+		{
+			"c 最小",
+			args{a: 2, b: 1, c: 0},
+			0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := min(tt.args.a, tt.args.b, tt.args.c); got != tt.want {
+				t.Errorf("min() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
