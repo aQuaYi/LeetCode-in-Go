087a6253d9cfa235bab1fa8a335130c2dbe70bdb
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-30 22:09:21 +0800

    987 wrong answer

--- Algorithms/0987.vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.go
+++ Algorithms/0987.vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.go
@@ -1,11 +1,57 @@
 package problem0987
 
-import "github.com/aQuaYi/LeetCode-in-Go/kit"
+import (
+	"sort"
+
+	"github.com/aQuaYi/LeetCode-in-Go/kit"
+)
 
 // TreeNode is pre-defined...
 type TreeNode = kit.TreeNode
 
 func verticalTraversal(root *TreeNode) [][]int {
+	ds := make([]data, 0, 1024)
+	dfs(root, 0, 0, &ds)
 
-	return nil
+	sort.Slice(ds, func(i int, j int) bool {
+		if ds[i].x == ds[j].x {
+			return ds[i].y > ds[j].y
+		}
+		return ds[i].x < ds[j].x
+	})
+
+	res := make([][]int, 0, len(ds))
+
+	x := ds[0].x
+	values := make([]int, 0, 10)
+	for _, d := range ds {
+		if x == d.x {
+			values = append(values, d.value)
+		} else {
+			x = d.x
+			res = append(res, values)
+			values = make([]int, 1, 10)
+			values[0] = d.value
+		}
+	}
+	res = append(res, values)
+
+	return res
+}
+
+type data struct {
+	value, x, y int
+}
+
+func dfs(node *TreeNode, x, y int, dataSlice *[]data) {
+	if node == nil {
+		return
+	}
+	*dataSlice = append(*dataSlice, data{
+		value: node.Val,
+		x:     x,
+		y:     y,
+	})
+	dfs(node.Left, x-1, y-1, dataSlice)
+	dfs(node.Right, x+1, y-1, dataSlice)
 }
