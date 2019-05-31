444eb789aa255a7615563bad65979157a66348a1
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-31 19:52:44 +0800

    988 added

---
+++ Algorithms/0988.smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.go
@@ -0,0 +1,11 @@
+package problem0988
+
+import "github.com/aQuaYi/LeetCode-in-Go/kit"
+
+// TreeNode is pre-defined...
+type TreeNode = kit.TreeNode
+
+func smallestFromLeaf(root *TreeNode) string {
+
+	return ""
+}
