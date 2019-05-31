588b00c4291c60940dfce6f7a1cf7fb56aedc7b2
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-05-31 20:00:16 +0800

    988 accepted. 8ms, faster than 16.36%

--- Algorithms/0988.smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.go
+++ Algorithms/0988.smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.go
@@ -6,6 +6,33 @@ import "github.com/aQuaYi/LeetCode-in-Go/kit"
 type TreeNode = kit.TreeNode
 
 func smallestFromLeaf(root *TreeNode) string {
+	res := string('z' + 1)
+	dfs(root, "", &res)
+	return res
+}
 
-	return ""
+func dfs(node *TreeNode, s string, res *string) {
+	s = string('a'+node.Val) + s
+
+	if node.Left == nil && node.Right == nil {
+		*res = min(*res, s)
+		return
+	}
+
+	if node.Left != nil {
+		dfs(node.Left, s, res)
+	}
+
+	if node.Right != nil {
+		dfs(node.Right, s, res)
+	}
+
+	return
+}
+
+func min(a, b string) string {
+	if a < b {
+		return a
+	}
+	return b
 }
