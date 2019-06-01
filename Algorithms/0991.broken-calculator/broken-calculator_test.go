39fc253aaeded0d1a63e758e1bdbf74d479d6172
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-01 16:26:28 +0800

    991 added

---
+++ Algorithms/0991.broken-calculator/broken-calculator_test.go
@@ -0,0 +1,57 @@
+package problem0991
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	X   int
+	Y   int
+	ans int
+}{
+
+	{
+		2,
+		3,
+		2,
+	},
+
+	{
+		5,
+		8,
+		2,
+	},
+
+	{
+		3,
+		1,
+		3,
+	},
+
+	{
+		1024,
+		1,
+		1023,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_brokenCalc(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		ast.Equal(tc.ans, brokenCalc(tc.X, tc.Y), "输入:%v", tc)
+	}
+}
+
+func Benchmark_brokenCalc(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			brokenCalc(tc.X, tc.Y)
+		}
+	}
+}
