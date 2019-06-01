85b4db691810157073b643bb748c00aa2714978c
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-01 18:51:10 +0800

    992 finish

--- Algorithms/0992.subarrays-with-k-different-integers/subarrays-with-k-different-integers.go
+++ Algorithms/0992.subarrays-with-k-different-integers/subarrays-with-k-different-integers.go
@@ -1,61 +1,28 @@
 package problem0992
 
+// ref: https://leetcode.com/problems/subarrays-with-k-different-integers/discuss/234482/JavaC%2B%2BPython-Sliding-Window-with-Video
 func subarraysWithKDistinct(A []int, K int) int {
-	size := len(A)
+	return atMostK(A, K) - atMostK(A, K-1)
+}
+
+func atMostK(A []int, K int) int {
+	count := [20001]int{}
 	res := 0
-	for i := 0; i < size; i++ {
-		for j := i + K; j <= size; j++ {
-			if count(A[i:j]) == K {
-				res++
-			}
+	for l, r := 0, 0; r < len(A); r++ {
+		if count[A[r]] == 0 {
+			K--
 		}
+		count[A[r]]++
+		for K < 0 {
+			count[A[l]]--
+			if count[A[l]] == 0 {
+				K++
+			}
+			l++
+		}
+		// the number of distinct elements in any A[i:r+1] (l<=i<=r)
+		// is NOT more than K
+		res += r + 1 - l
 	}
 	return res
 }
-
-func count(A []int) int {
-	m := make(map[int]bool, len(A))
-	for _, a := range A {
-		m[a] = true
-	}
-	return len(m)
-}
-
-// func subarraysWithKDistinct(A []int, K int) int {
-// 	counts, k := [20001]int{}, 0
-
-// 	size := len(A)
-// 	l, r := 0, -1
-// 	res := 0
-// 	for r+1 < size {
-// 		c := 0
-// 		for k <= K && r+1 < size {
-// 			if k == K && counts[A[r+1]] == 0 {
-// 				break
-// 			}
-// 			r++
-// 			if counts[A[r]] == 0 {
-// 				k++
-// 			}
-// 			counts[A[r]]++
-// 			if k == K {
-// 				c++
-// 			}
-// 		}
-
-// 		if c > 1 {
-// 			c--
-// 		}
-
-// 		for k == K {
-// 			res += c
-// 			counts[A[l]]--
-// 			if counts[A[l]] == 0 {
-// 				k--
-// 			}
-// 			l++
-// 		}
-// 	}
-
-// 	return res
-// }
