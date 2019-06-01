8f99ef3fa74457362a93c1ff6c2612130c51a172
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-01 16:35:10 +0800

    991 wrong answer

--- Algorithms/0991.broken-calculator/broken-calculator.go
+++ Algorithms/0991.broken-calculator/broken-calculator.go
@@ -1,6 +1,17 @@
 package problem0991
 
 func brokenCalc(X int, Y int) int {
+	if X > Y {
+		return X - Y
+	}
+
+	if Y%2 == 0 {
+		return brokenCalc(X, Y/2) + 1
+	}
+
+	if X < Y {
+		return brokenCalc(X*2, Y) + 1
+	}
 
 	return 0
 }
