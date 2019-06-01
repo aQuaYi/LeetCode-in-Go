f21208d1a4f2a1e3cbf2944ef3b1a6d60c27d6a0
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-01 17:11:58 +0800

    992 added

---
+++ Algorithms/0992.subarrays-with-k-different-integers/subarrays-with-k-different-integers.go
@@ -0,0 +1,6 @@
+package problem0992
+
+func subarraysWithKDistinct(A []int, K int) int {
+
+	return 0
+}
---
+++ Algorithms/0992.subarrays-with-k-different-integers/subarrays-with-k-different-integers_test.go
@@ -0,0 +1,45 @@
+package problem0992
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	A   []int
+	K   int
+	ans int
+}{
+
+	{
+		[]int{1, 2, 1, 2, 3},
+		2,
+		7,
+	},
+
+	{
+		[]int{1, 2, 1, 3, 4},
+		3,
+		3,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_subarraysWithKDistinct(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		ast.Equal(tc.ans, subarraysWithKDistinct(tc.A, tc.K), "输入:%v", tc)
+	}
+}
+
+func Benchmark_subarraysWithKDistinct(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			subarraysWithKDistinct(tc.A, tc.K)
+		}
+	}
+}
