df2eb6aca3079cb8502ddfdb9a7fce7c2cd07e6c
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-01 17:48:41 +0800

    992 test fail

--- Algorithms/0992.subarrays-with-k-different-integers/subarrays-with-k-different-integers.go
+++ Algorithms/0992.subarrays-with-k-different-integers/subarrays-with-k-different-integers.go
@@ -7,16 +7,23 @@ func subarraysWithKDistinct(A []int, K int) int {
 	l, r := 0, -1
 	res := 0
 	for r+1 < size {
-		for k < K && r+1 < size {
+		c := 0
+		for k <= K && r+1 < size {
+			if k == K && counts[A[r+1]] == 0 {
+				break
+			}
 			r++
 			if counts[A[r]] == 0 {
 				k++
+				if k == K {
+					c++
+				}
 			}
 			counts[A[r]]++
 		}
 
 		for k == K {
-			res++
+			res += c
 			counts[A[l]]--
 			if counts[A[l]] == 0 {
 				k--
