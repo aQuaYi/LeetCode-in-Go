7a6cd0a33dd6893f3edfc99ecb601002d3fd194e
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-02 20:57:48 +0800

    996 added

---
+++ Algorithms/0996.number-of-squareful-arrays/number-of-squareful-arrays_test.go
@@ -0,0 +1,41 @@
+package problem0996
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	A   []int
+	ans int
+}{
+
+	{
+		[]int{1, 17, 8},
+		2,
+	},
+
+	{
+		[]int{2, 2, 2},
+		1,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_numSquarefulPerms(t *testing.T) {
+	ast := assert.New(t)
+	for _, tc := range tcs {
+		ast.Equal(tc.ans, numSquarefulPerms(tc.A), "输入:%v", tc)
+	}
+}
+
+func Benchmark_numSquarefulPerms(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			numSquarefulPerms(tc.A)
+		}
+	}
+}
