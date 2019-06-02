ef34c9b3055a1cd3e65256558351db87fa58cc4b
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-02 12:36:45 +0800

    995 accepted. 96ms, faster than 95.65%

--- Algorithms/0995.minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.go
+++ Algorithms/0995.minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.go
@@ -2,18 +2,23 @@ package problem0995
 
 func minKBitFlips(A []int, K int) int {
 	size := len(A)
+	queue := make([]int, 0, size)
+	flag := 1
 	res := 0
 	for i := 0; i < size; i++ {
-		if A[i] == 1 {
+		if len(queue) > 0 && i == queue[0] {
+			flag ^= 1
+			queue = queue[1:]
+		}
+		if A[i] == flag {
 			continue
 		}
 		if i+K > size {
 			return -1
 		}
+		queue = append(queue, i+K)
 		res++
-		for j := i; j < i+K; j++ {
-			A[j] ^= 1 // flip
-		}
+		flag ^= 1
 	}
 
 	return res
