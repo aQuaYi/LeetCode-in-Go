bf24ab907b25f77d42f177b05b69c09a82601f80
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-02 10:36:27 +0800

    998 added

---
+++ Algorithms/0998.maximum-binary-tree-ii/maximum-binary-tree-ii.go
@@ -0,0 +1,11 @@
+package problem0998
+
+import "github.com/aQuaYi/LeetCode-in-Go/kit"
+
+// TreeNode is pre-defined...
+type TreeNode = kit.TreeNode
+
+func insertIntoMaxTree(root *TreeNode, val int) *TreeNode {
+
+	return nil
+}
