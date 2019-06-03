0b6fe32837400982e0573ee5f95d4ee62af6b054
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-03 20:30:41 +0800

    1000 doing

--- Algorithms/1000.minimum-cost-to-merge-stones/minimum-cost-to-merge-stones.go
+++ Algorithms/1000.minimum-cost-to-merge-stones/minimum-cost-to-merge-stones.go
@@ -1,6 +1,30 @@
 package problem1000
 
 func mergeStones(stones []int, K int) int {
+	size := len(stones)
+	if !isPossible(size, K) {
+		return -1
+	}
 
-	return 0
+	dp := [31][31]int{}
+	for i := 1; i <= size; i++ {
+		for j := i; j < i+K && j <= size; j++ {
+			dp[i][j] = dp[i][j-1] + stones[j]
+		}
+	}
+
+	for i := 1; i <= size; i++ {
+		for j := i + K - 1; j <= size; j += K {
+
+		}
+	}
+
+	return dp[1][size]
+}
+
+func isPossible(size, K int) bool {
+	for size >= K {
+		size = size/K + size%K
+	}
+	return size == 1
 }
