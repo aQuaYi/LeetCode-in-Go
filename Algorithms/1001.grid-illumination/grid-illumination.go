20de8dfe4bbb62897d29280b2468e20435985129
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-04 22:44:57 +0800

    1001 done

--- Algorithms/1001.grid-illumination/grid-illumination.go
+++ Algorithms/1001.grid-illumination/grid-illumination.go
@@ -10,16 +10,7 @@ func gridIllumination(N int, lamps [][]int, queries [][]int) []int {
 	res := make([]int, 0, len(queries))
 	for _, cell := range queries {
 		x, y := cell[0], cell[1]
-		if !l.isIlluminated(x, y) {
-			res = append(res, 0)
-			continue
-		}
-		res = append(res, 1)
-		for dx := -1; dx <= 1; dx++ {
-			for dy := -1; dy <= 1; dy++ {
-				l.turnOff(x+dx, y+dy)
-			}
-		}
+		res = append(res, l.checkIlluminated(x, y))
 	}
 
 	return res
@@ -27,16 +18,18 @@ func gridIllumination(N int, lamps [][]int, queries [][]int) []int {
 
 type lights struct {
 	h, v, ad, dd map[int]int
-	hasLamp      map[int]bool
+	isOn         map[int]bool
+	n            int
 }
 
 func newLights(N int) *lights {
 	return &lights{
-		h:       make(map[int]int, 1024),
-		v:       make(map[int]int, 1024),
-		ad:      make(map[int]int, 1024),
-		dd:      make(map[int]int, 1024),
-		hasLamp: make(map[int]bool, 1024),
+		h:    make(map[int]int, 1024),
+		v:    make(map[int]int, 1024),
+		ad:   make(map[int]int, 1024),
+		dd:   make(map[int]int, 1024),
+		isOn: make(map[int]bool, 1024),
+		n:    N,
 	}
 }
 
@@ -45,23 +38,52 @@ func (l *lights) turnOn(x, y int) {
 	l.v[y]++
 	l.ad[x+y]++
 	l.dd[x-y]++
-	l.hasLamp[x<<32+y] = true
+	l.isOn[x<<32+y] = true
 }
 
 func (l *lights) turnOff(x, y int) {
-	if !l.hasLamp[x<<32+y] {
+	if x < 0 || l.n <= x ||
+		y < 0 || l.n <= y ||
+		!l.isOn[x<<32+y] {
 		return
 	}
-	l.hasLamp[x<<32+y] = false
+	l.isOn[x<<32+y] = false
 	l.h[x]--
 	l.v[y]--
 	l.ad[x+y]--
 	l.dd[x-y]--
 }
 
-func (l *lights) isIlluminated(x, y int) bool {
-	return l.h[x] > 0 ||
-		l.v[y] > 0 ||
-		l.ad[x+y] > 0 ||
-		l.dd[x-y] > 0
+func (l *lights) checkIlluminated(x, y int) int {
+	res := 0
+
+	if l.h[x] > 0 {
+		res = 1
+		l.turnOff(x, y-1)
+		l.turnOff(x, y+1)
+	}
+
+	if l.v[y] > 0 {
+		res = 1
+		l.turnOff(x-1, y)
+		l.turnOff(x+1, y)
+	}
+
+	if l.ad[x+y] > 0 {
+		res = 1
+		l.turnOff(x-1, y+1)
+		l.turnOff(x+1, y-1)
+	}
+
+	if l.dd[x-y] > 0 {
+		res = 1
+		l.turnOff(x-1, y-1)
+		l.turnOff(x+1, y+1)
+	}
+
+	if res == 1 {
+		l.turnOff(x, y)
+	}
+
+	return res
 }
