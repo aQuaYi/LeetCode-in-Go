6a23cd828cbce5040f1cc6536d48013e7e431254
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-06 22:14:41 +0800

    1004 done

--- Algorithms/1004.max-consecutive-ones-iii/max-consecutive-ones-iii.go
+++ Algorithms/1004.max-consecutive-ones-iii/max-consecutive-ones-iii.go
@@ -1,72 +1,16 @@
 package problem1004
 
+// ref: https://leetcode.com/problems/max-consecutive-ones-iii/discuss/247564/JavaC%2B%2BPython-Sliding-Window
 func longestOnes(A []int, K int) int {
-	A = append(A, 0)
-	size := len(A)
-
-	i := 0
-	for i < size && A[i] == 0 {
-		i++
-	}
-
-	ones, zeros := gen(A[i:])
-
-	res := 0
-	i, j := 0, 0
-	one := 0
-	for j < len(zeros) {
-		if zeros[j] > K {
-			res = max(res, one+ones[j]+K)
-			for i < j && K < zeros[j] {
-				K += zeros[i]
-				one -= ones[i] + zeros[i]
-				i++
-			}
+	left, right := 0, 0
+	for right = range A {
+		K -= 1 ^ A[right] // 每次遇到 0 就会消耗一次 K
+		if K < 0 {        // K 不够用的时候，就从左边回血
+			K += 1 ^ A[left]
+			left++
 		}
-
-		if K >= zeros[j] {
-			K -= zeros[j]
-			one += ones[j] + zeros[j]
-			res = max(res, one)
-		}
-
-		j++
 	}
-
-	return min(res, size-1)
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-
-func gen(A []int) ([]int, []int) {
-	ones, zeros := make([]int, 0, len(A)), make([]int, 0, len(A))
-	size := len(A)
-	a, i := 0, 0
-	for i < len(A) {
-		a = i
-		for i < size && A[i] == 1 {
-			i++
-		}
-		ones = append(ones, i-a)
-		a = i
-		for i < size && A[i] == 0 {
-			i++
-		}
-		zeros = append(zeros, i-a)
-	}
-	ones = append(ones, 0)
-	zeros = append(zeros, len(A))
-	return ones, zeros
+	// A[left:right+1] 并不是真的 max-length-sliding-windows
+	// 把 right-left+1 当作是 A[:right+1] 中出现过的最长子数组的长度，更合适
+	return right - left + 1
 }
