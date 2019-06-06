dd043bdf52d6705dc959b63e970defc4ee19cf41
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-06 20:13:12 +0800

    1004 准备换个思路

--- Algorithms/1004.max-consecutive-ones-iii/max-consecutive-ones-iii.go
+++ Algorithms/1004.max-consecutive-ones-iii/max-consecutive-ones-iii.go
@@ -1,6 +1,39 @@
 package problem1004
 
 func longestOnes(A []int, K int) int {
+	A = append(A, 0)
+	size := len(A)
+	indexs := make([]int, 0, size)
+	if A[0] == 1 {
+		indexs = append(indexs, 0)
+	}
+	for i := 1; i < size; i++ {
+		if (A[i-1] == 0 && A[i] == 1) ||
+			(A[i-1] == 1 && A[i] == 0) {
+			indexs = append(indexs, i)
+		}
+	}
 
-	return 0
+	res := 0
+	for i := 0; i < len(indexs); i += 2 {
+		k := K
+		for j := 2; j < size && k > 0; j += 2 {
+
+		}
+	}
+	return min(res, size-1)
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
