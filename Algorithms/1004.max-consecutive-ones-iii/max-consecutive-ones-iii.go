78f9d9bc52dceb87fa2a75f631ea71e80bee3517
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-06 20:39:30 +0800

    1004 doing

--- Algorithms/1004.max-consecutive-ones-iii/max-consecutive-ones-iii.go
+++ Algorithms/1004.max-consecutive-ones-iii/max-consecutive-ones-iii.go
@@ -3,24 +3,16 @@ package problem1004
 func longestOnes(A []int, K int) int {
 	A = append(A, 0)
 	size := len(A)
-	indexs := make([]int, 0, size)
-	if A[0] == 1 {
-		indexs = append(indexs, 0)
+
+	i := 0
+	for i < size && A[i] == 0 {
+		i++
 	}
-	for i := 1; i < size; i++ {
-		if (A[i-1] == 0 && A[i] == 1) ||
-			(A[i-1] == 1 && A[i] == 0) {
-			indexs = append(indexs, i)
-		}
-	}
+
+	ones, zeros := gen(A[i:])
 
 	res := 0
-	for i := 0; i < len(indexs); i += 2 {
-		k := K
-		for j := 2; j < size && k > 0; j += 2 {
 
-		}
-	}
 	return min(res, size-1)
 }
 
@@ -36,4 +28,22 @@ func min(a, b int) int {
 		return a
 	}
 	return b
+}
+
+func gen(A []int) ([]int, []int) {
+	ones, zeros := make([]int, 0, len(A)), make([]int, 0, len(A))
+	a, i := 0, 0
+	for i < len(A) {
+		a = i
+		for i < size && A[i] == 1 {
+			i++
+		}
+		ones = append(ones, i-a)
+		a = i
+		for i < size && A[i] == 0 {
+			i++
+		}
+		zeros = append(zeros, i-a)
+	}
+	return ones, zeros
 }
