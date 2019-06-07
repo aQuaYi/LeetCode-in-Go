00823ccc7c8faa19b464f3372ece5f742f5ba4b7
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-07 16:39:10 +0800

    1008 added

--- Algorithms/1007.minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.go
+++ Algorithms/1007.minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.go
@@ -1,43 +1,27 @@
 package problem1007
 
 func minDominoRotations(A []int, B []int) int {
-	a, b := A[0], B[0]
-	ca, cb := 1, 1
-	for i := 1; i < len(A); i++ {
-		if A[i] != a && A[i] != b &&
-			B[i] != a && B[i] != b {
-			return -1
+	if res, ok := check(A, B, A[0]); ok {
+		return res
+	}
+	if res, ok := check(A, B, B[0]); ok {
+		return res
+	}
+	return -1
+}
+
+func check(A, B []int, flag int) (int, bool) {
+	a, b := 0, 0
+	i, n := 0, len(A)
+	for ; i < n && (A[i] == flag || B[i] == flag); i++ {
+		if A[i] != flag { // A needs a swap to be all flag
+			a++
 		}
-		if A[i] == a || B[i] == a {
-			ca++
-		}
-		if A[i] == b || B[i] == b {
-			cb++
+		if B[i] != flag { // B needs a swap to be all flag
+			b++
 		}
 	}
-
-	if ca != len(A) && cb != len(A) {
-		return -1
-	}
-
-	flag := a
-	if ca < cb {
-		flag = b
-	}
-
-	ca, cb = 0, 0
-	for i := 0; i < len(A); i++ {
-		if A[i] == flag && B[i] == flag {
-			continue
-		}
-		if A[i] == flag {
-			cb++
-		} else {
-			ca++
-		}
-	}
-
-	return min(ca, cb)
+	return min(a, b), i == n
 }
 
 func min(a, b int) int {
---
+++ Algorithms/1008.construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.go
@@ -0,0 +1,11 @@
+package problem1008
+
+import "github.com/aQuaYi/LeetCode-in-Go/kit"
+
+// TreeNode is pre-defined...
+type TreeNode = kit.TreeNode
+
+func bstFromPreorder(preorder []int) *TreeNode {
+
+	return nil
+}
