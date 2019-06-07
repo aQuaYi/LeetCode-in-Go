87bbfc633fe310b820066b5a736f8929f0f02b8a
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-07 16:16:46 +0800

    1007 accepted. 128ms, faster than 46.85%

--- Algorithms/1007.minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.go
+++ Algorithms/1007.minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.go
@@ -1,6 +1,48 @@
 package problem1007
 
 func minDominoRotations(A []int, B []int) int {
+	a, b := A[0], B[0]
+	ca, cb := 1, 1
+	for i := 1; i < len(A); i++ {
+		if A[i] != a && A[i] != b &&
+			B[i] != a && B[i] != b {
+			return -1
+		}
+		if A[i] == a || B[i] == a {
+			ca++
+		}
+		if A[i] == b || B[i] == b {
+			cb++
+		}
+	}
 
-	return 0
+	if ca != len(A) && cb != len(A) {
+		return -1
+	}
+
+	flag := a
+	if ca < cb {
+		flag = b
+	}
+
+	ca, cb = 0, 0
+	for i := 0; i < len(A); i++ {
+		if A[i] == flag && B[i] == flag {
+			continue
+		}
+		if A[i] == flag {
+			cb++
+		} else {
+			ca++
+		}
+	}
+
+	return min(ca, cb)
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
