bb1e1abf2fe11bf8ee224fc35244db39ed0b7b96
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-08 21:56:39 +0800

    1011 accepted. 48ms, faster than 25%.

--- Algorithms/1011.capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.go
+++ Algorithms/1011.capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.go
@@ -1,6 +1,39 @@
 package problem1011
 
-func shipWithinDays(weights []int, D int) int {
+import "sort"
 
-	return 0
+func shipWithinDays(weights []int, D int) int {
+	n := len(weights)
+	sum := make([]int, n+1)
+	maxWeight := 0
+	for i := 1; i <= n; i++ {
+		sum[i] = sum[i-1] + weights[i-1]
+		maxWeight = max(maxWeight, weights[i-1])
+	}
+
+	check := func(cap int) bool {
+		if cap < maxWeight {
+			return false
+		}
+		i, j, d := 0, 0, 0
+		for ; d < D && j < n; j++ {
+			if sum[j+1]-sum[i] <= cap {
+				continue
+			}
+			i = j
+			d++
+		}
+		return d+1 <= D && sum[n]-sum[i] <= cap
+	}
+
+	res := sort.Search(sum[n], check)
+
+	return res
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
