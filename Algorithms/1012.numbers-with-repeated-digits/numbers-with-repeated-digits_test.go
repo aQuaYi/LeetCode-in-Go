17e021651c5bdc0e425627d02197e9fcb5308859
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-09 12:25:24 +0800

    1012 added

---
+++ Algorithms/1012.numbers-with-repeated-digits/numbers-with-repeated-digits_test.go
@@ -0,0 +1,47 @@
+package problem1012
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	N   int
+	ans int
+}{
+
+	{
+		20,
+		1,
+	},
+
+	{
+		100,
+		10,
+	},
+
+	{
+		1000,
+		262,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_numDupDigitsAtMostN(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		ast.Equal(tc.ans, numDupDigitsAtMostN(tc.N), "输入:%v", tc)
+	}
+}
+
+func Benchmark_numDupDigitsAtMostN(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			numDupDigitsAtMostN(tc.N)
+		}
+	}
+}
