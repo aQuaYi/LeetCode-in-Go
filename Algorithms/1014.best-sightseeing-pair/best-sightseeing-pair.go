4d800e5ab9b40a70000fd91cf2bc5ff99af9733a
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-10 20:39:36 +0800

    1014 accepted. 52ms

--- Algorithms/1014.best-sightseeing-pair/best-sightseeing-pair.go
+++ Algorithms/1014.best-sightseeing-pair/best-sightseeing-pair.go
@@ -1,6 +1,20 @@
 package problem1014
 
 func maxScoreSightseeingPair(A []int) int {
+	Ai, i := A[0], 0
+	res := 0
+	for j := 1; j < len(A); j++ {
+		res = max(res, Ai+A[j]+i-j)
+		if Ai+i < A[j]+j {
+			Ai, i = A[j], j
+		}
+	}
+	return res
+}
 
-	return 0
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
