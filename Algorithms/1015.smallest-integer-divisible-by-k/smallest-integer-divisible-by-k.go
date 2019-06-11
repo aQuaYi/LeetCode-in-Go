aadd6483ccbe414def65609773dea27c7462f72c
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-11 20:09:23 +0800

    1015 wrong answer

--- Algorithms/1015.smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.go
+++ Algorithms/1015.smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.go
@@ -1,6 +1,14 @@
 package problem1015
 
 func smallestRepunitDivByK(K int) int {
-
-	return 0
+	if K%2 == 0 {
+		return -1
+	}
+	res := 1
+	N := 1
+	for N%K != 0 {
+		N = N*10 + 1
+		res++
+	}
+	return res
 }
