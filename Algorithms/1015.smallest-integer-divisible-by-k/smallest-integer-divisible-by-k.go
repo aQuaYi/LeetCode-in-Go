922ebb59be517c53a8e0c76e12a8396566f99894
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-11 21:22:29 +0800

    1015 accepted. 40ms, faster than 9.68%

--- Algorithms/1015.smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.go
+++ Algorithms/1015.smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.go
@@ -1,14 +1,33 @@
 package problem1015
 
 func smallestRepunitDivByK(K int) int {
-	if K%2 == 0 {
+	if K == 1 {
+		return 1
+	}
+
+	if K%2 == 0 || K%5 == 0 {
 		return -1
 	}
-	res := 1
-	N := 1
-	for N%K != 0 {
-		N = N*10 + 1
-		res++
+
+	e := 0
+	m := mod(e, K)
+	for m != 0 {
+		e++
+		m = (m + mod(e, K)) % K
 	}
-	return res
+	return e + 1
+}
+
+// return 10^n%K
+func mod(n, K int) int {
+	if n == 0 {
+		return 1
+	}
+
+	m := mod(n/2, K)
+	m = m * m % K
+	if n&1 == 1 {
+		m = m * 10 % K
+	}
+	return m
 }
