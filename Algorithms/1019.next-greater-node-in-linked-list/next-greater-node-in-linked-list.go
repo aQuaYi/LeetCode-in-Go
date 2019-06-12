e1e09ff78729594bd796b95a5e40269ce6c7ce80
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-12 17:05:07 +0800

    1019 prepared.

---
+++ Algorithms/1019.next-greater-node-in-linked-list/next-greater-node-in-linked-list.go
@@ -0,0 +1,11 @@
+package problem1019
+
+import "github.com/aQuaYi/LeetCode-in-Go/kit"
+
+// ListNode is pre-defined...
+type ListNode = kit.ListNode
+
+func nextLargerNodes(head *ListNode) []int {
+
+	return nil
+}
