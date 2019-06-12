3230e9b04c466b0507f6d15a947d32f1ba4dc19d
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-12 17:14:28 +0800

    1019 accepted. 1488 ms, faster than 23.49%

--- Algorithms/1019.next-greater-node-in-linked-list/next-greater-node-in-linked-list.go
+++ Algorithms/1019.next-greater-node-in-linked-list/next-greater-node-in-linked-list.go
@@ -6,6 +6,21 @@ import "github.com/aQuaYi/LeetCode-in-Go/kit"
 type ListNode = kit.ListNode
 
 func nextLargerNodes(head *ListNode) []int {
+	res := make([]int, 0, 10000)
+	for head != nil {
+		res = append(res, larger(head))
+		head = head.Next
+	}
+	return res
+}
 
-	return nil
+func larger(head *ListNode) int {
+	val, next := head.Val, head.Next
+	for next != nil {
+		if val < next.Val {
+			return next.Val
+		}
+		next = next.Next
+	}
+	return 0
 }
