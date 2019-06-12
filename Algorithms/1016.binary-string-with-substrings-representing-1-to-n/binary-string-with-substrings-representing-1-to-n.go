bb034bf23a986d7ee43f4010e5cab4ad89882580
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-12 11:02:56 +0800

    1016 done

--- Algorithms/1016.binary-string-with-substrings-representing-1-to-n/binary-string-with-substrings-representing-1-to-n.go
+++ Algorithms/1016.binary-string-with-substrings-representing-1-to-n/binary-string-with-substrings-representing-1-to-n.go
@@ -1,11 +1,33 @@
 package problem1016
 
-func queryString(S string, N int) bool {
+import "strings"
 
-	return false
+func queryString(S string, N int) bool {
+	end := N >> 1
+	for N >= end {
+		if !strings.Contains(S, binaryString(N)) {
+			return false
+		}
+		N--
+	}
+	return true
 }
 
-func int2Binary(n int) string {
+func binaryString(n int) string {
+	B := make([]byte, 0, 30)
+	for n > 0 {
+		B = append(B, byte(n&1)+'0')
+		n >>= 1
+	}
+	swap(B)
+	return string(B)
+}
 
-	return
+func swap(B []byte) {
+	i, j := 0, len(B)-1
+	for i < j {
+		B[i], B[j] = B[j], B[i]
+		i++
+		j--
+	}
 }
