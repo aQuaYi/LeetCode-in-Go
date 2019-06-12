9bbc4fe7c765d2db18d74a8b8b19d5cd2ef6a98d
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-12 11:32:49 +0800

    1017 wrong answer

--- Algorithms/1017.convert-to-base-2/convert-to-base-2.go
+++ Algorithms/1017.convert-to-base-2/convert-to-base-2.go
@@ -1,6 +1,37 @@
 package problem1017
 
 func baseNeg2(N int) string {
+	B := make([]byte, 0, 30)
+	for N > 0 {
+		switch N & 3 {
+		case 0:
+			B = append(B, '0', '0')
+		case 1:
+			B = append(B, '1', '0')
+		case 2:
+			B = append(B, '0', '1')
+			N += 4
+		case 3:
+			B = append(B, '1', '1')
+			N += 4
+		}
+		N >>= 2
+	}
 
-	return ""
+	swap(B)
+
+	if B[0] == '0' { // no lead 0 except for N is 0
+		B = B[1:]
+	}
+
+	return string(B)
+}
+
+func swap(B []byte) {
+	i, j := 0, len(B)-1
+	for i < j {
+		B[i], B[j] = B[j], B[i]
+		i++
+		j--
+	}
 }
