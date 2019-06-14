b354f6d91f90f13936e4add06062fe8965c720ab
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-14 20:27:21 +0800

    1024 wrong answer

--- Algorithms/1024.video-stitching/video-stitching.go
+++ Algorithms/1024.video-stitching/video-stitching.go
@@ -1,6 +1,36 @@
 package problem1024
 
-func videoStitching(clips [][]int, T int) int {
+import "sort"
 
-	return 0
+func videoStitching(clips [][]int, T int) int {
+	sort.Slice(clips, func(i int, j int) bool {
+		if clips[i][0] == clips[j][0] {
+			return clips[i][1] > clips[j][1]
+		}
+		return clips[i][0] < clips[j][0]
+	})
+
+	l, r := clips[0][0], clips[0][1]
+	if l != 0 {
+		return -1
+	}
+
+	res := 1
+	for i := 1; i < len(clips); i++ {
+		if r < clips[i][0] {
+			return -1
+		}
+		if r < clips[i][1] {
+			r = clips[i][1]
+			res++
+			if r >= T {
+				break
+			}
+		}
+	}
+
+	if r < T {
+		return -1
+	}
+	return res
 }
