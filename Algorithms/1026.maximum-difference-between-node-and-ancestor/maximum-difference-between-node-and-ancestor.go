5bad173ef12926b6378ef95d893d9013ee2eeb96
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-15 19:35:49 +0800

    1026 prepared

---
+++ Algorithms/1026.maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.go
@@ -0,0 +1,11 @@
+package problem1026
+
+import "github.com/aQuaYi/LeetCode-in-Go/kit"
+
+// TreeNode is pre-defined...
+type TreeNode = kit.TreeNode
+
+func maxAncestorDiff(root *TreeNode) int {
+
+	return 0
+}
