b7610e9029cf0480ec82d6fa63166a7b2196246b
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-16 15:11:21 +0800

    1027 整理好了

--- Algorithms/1027.longest-arithmetic-sequence/longest-arithmetic-sequence.go
+++ Algorithms/1027.longest-arithmetic-sequence/longest-arithmetic-sequence.go
@@ -2,49 +2,30 @@ package problem1027
 
 func longestArithSeqLength(A []int) int {
 	n := len(A)
-	exist, same := make(map[int][]int, n), make(map[int]int, n)
-	res := 0
+
+	indexes := make(map[int][]int, n)
 	for i := 0; i < n; i++ {
-		if _, ok := exist[A[i]]; ok {
-			exist[A[i]] = append(exist[A[i]], i)
-		} else {
-			exist[A[i]] = []int{i}
-		}
-		same[A[i]]++
-		if same[A[i]] > res {
-			res = same[A[i]]
-		}
+		indexes[A[i]] = append(indexes[A[i]], i)
 	}
 
+	// diff :=0
+
+	var dfs func(int, int) int
+	dfs = func(diff, j int) int {
+		for _, k := range indexes[A[j]+diff] {
+			if j < k {
+				return 1 + dfs(diff, k)
+			}
+		}
+
+		return 0
+	}
+
+	res := 0
 	for i := 0; i < n; i++ {
 		for j := i + 1; j < n; j++ {
 			diff := A[j] - A[i]
-			if diff != 0 {
-				p, index := 2, j
-				for {
-					if indexes, ok := exist[A[index]+diff]; ok {
-						breakFlag := true
-						for k := 0; k < len(indexes); k++ {
-							if indexes[k] > index {
-								p++
-								index = indexes[k]
-								breakFlag = false
-								break
-							}
-						}
-
-						if breakFlag {
-							break
-						}
-					} else {
-						break
-					}
-				}
-
-				if p > res {
-					res = p
-				}
-			}
+			res = max(res, dfs(diff, j)+2)
 		}
 	}
 
