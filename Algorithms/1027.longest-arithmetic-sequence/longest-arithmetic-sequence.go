09f6d01cbb0c67dc1c9c9031673c6432652c6e58
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-16 15:21:39 +0800

    1027 done

--- Algorithms/1027.longest-arithmetic-sequence/longest-arithmetic-sequence.go
+++ Algorithms/1027.longest-arithmetic-sequence/longest-arithmetic-sequence.go
@@ -8,16 +8,13 @@ func longestArithSeqLength(A []int) int {
 		indexes[A[i]] = append(indexes[A[i]], i)
 	}
 
-	// diff :=0
-
 	var dfs func(int, int) int
-	dfs = func(diff, j int) int {
+	dfs = func(j, diff int) int {
 		for _, k := range indexes[A[j]+diff] {
 			if j < k {
-				return 1 + dfs(diff, k)
+				return 1 + dfs(k, diff)
 			}
 		}
-
 		return 0
 	}
 
@@ -25,7 +22,7 @@ func longestArithSeqLength(A []int) int {
 	for i := 0; i < n; i++ {
 		for j := i + 1; j < n; j++ {
 			diff := A[j] - A[i]
-			res = max(res, dfs(diff, j)+2)
+			res = max(res, dfs(j, diff)+2)
 		}
 	}
 
