ee8ba7e7318f6c71cd08c658ec3c221f7c16e43c
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-17 19:42:36 +0800

    1028 prepared

---
+++ Algorithms/1028.recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.go
@@ -0,0 +1,13 @@
+package problem1028
+
+import (
+	"github.com/aQuaYi/LeetCode-in-Go/kit"
+)
+
+// TreeNode is pre-defined...
+type TreeNode = kit.TreeNode
+
+func recoverFromPreorder(S string) *TreeNode {
+
+	return nil
+}
