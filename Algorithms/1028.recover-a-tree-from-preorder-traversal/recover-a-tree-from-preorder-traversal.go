5b5f7bacae400c8e1c356b5b011f75dfdcacf036
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-17 20:11:50 +0800

    1028 index out of range

--- Algorithms/1028.recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.go
+++ Algorithms/1028.recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.go
@@ -1,6 +1,8 @@
 package problem1028
 
 import (
+	"strconv"
+
 	"github.com/aQuaYi/LeetCode-in-Go/kit"
 )
 
@@ -8,6 +10,44 @@ import (
 type TreeNode = kit.TreeNode
 
 func recoverFromPreorder(S string) *TreeNode {
+	m := make(map[int][]*TreeNode, 1000)
 
-	return nil
+	for i := 0; i < len(S); i++ {
+		if S[i] == '-' {
+			v := S[:i]
+			S = S[i:]
+			val, _ := strconv.Atoi(v)
+			m[0] = append(m[0], &TreeNode{
+				Val: val,
+			})
+		}
+	}
+
+	level, val := 0, 0
+	for S != "" {
+		level, val, S = next(S)
+		node := &TreeNode{Val: val}
+		m[level] = append(m[level], node)
+		p := m[level-1][len(m[level-1])-1]
+		if p.Left == nil {
+			p.Left = node
+		} else {
+			p.Right = node
+		}
+	}
+
+	return m[0][0]
+}
+
+func next(S string) (int, int, string) {
+	level := 0
+	for S[level] == '-' {
+		level++
+	}
+	end := level
+	for end < len(S) && S[end] != '-' {
+		end++
+	}
+	value, _ := strconv.Atoi(S[level:end])
+	return level, value, S[end:]
 }
