7ddc47c1fa3b501e332eec3d8cc0bd495d555384
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-18 21:39:21 +0800

    1031 accepted. 4ms, faster than 60.71%

--- Algorithms/1031.maximum-sum-of-two-non-overlapping-subarrays/maximum-sum-of-two-non-overlapping-subarrays_test.go
+++ Algorithms/1031.maximum-sum-of-two-non-overlapping-subarrays/maximum-sum-of-two-non-overlapping-subarrays_test.go
@@ -1,6 +1,7 @@
 package problem1031
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -13,6 +14,13 @@ var tcs = []struct {
 	M   int
 	ans int
 }{
+
+	{
+		[]int{4, 5, 14, 16, 16, 20, 7, 13, 8, 15},
+		3,
+		5,
+		109,
+	},
 
 	{
 		[]int{0, 6, 5, 2, 2, 5, 1, 9, 4},
@@ -51,5 +59,38 @@ func Benchmark_maxSumTwoNoOverlap(b *testing.B) {
 		for _, tc := range tcs {
 			maxSumTwoNoOverlap(tc.A, tc.L, tc.M)
 		}
+	}
+}
+
+func Test_sums(t *testing.T) {
+	type args struct {
+		A []int
+		l int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []*entry
+	}{
+		{
+			"test",
+			args{
+				A: []int{0, 1, 2, 3},
+				l: 3,
+			},
+			[]*entry{
+				&entry{6, 1, 3},
+				&entry{3, 0, 2},
+			},
+		},
+
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sums(tt.args.A, tt.args.l); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("sums() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
