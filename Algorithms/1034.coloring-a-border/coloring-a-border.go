162a1a3163e0fde08abe2b0441128fb83bc6e6f4
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-19 22:07:41 +0800

    1034 done

--- Algorithms/1034.coloring-a-border/coloring-a-border.go
+++ Algorithms/1034.coloring-a-border/coloring-a-border.go
@@ -7,48 +7,46 @@ func colorBorder(grid [][]int, x0 int, y0 int, color int) [][]int {
 	m, n := len(grid), len(grid[0])
 	c := grid[x0][y0]
 
-	points := make([][2]int, 1, 1024)
-	points[0] = [2]int{x0, y0}
-	for len(points) > 0 {
-		size := len(points)
-		for i := 0; i < size; i++ {
-			p := points[i]
-			grid[p[0]][p[1]] = -c
-			for i := 0; i < 4; i++ {
-				x, y := p[0]+dx[i], p[1]+dy[i]
-				if 0 <= x && x < m &&
-					0 <= y && y < n &&
-					grid[x][y] == c {
-					points = append(points, [2]int{x, y})
-				}
-			}
-		}
-		points = points[size:]
-	}
-
 	isBorder := func(x, y int) bool {
 		return x == 0 || x == m-1 ||
 			y == 0 || y == n-1 ||
-			grid[x][y-1] != -c ||
-			grid[x][y+1] != -c ||
-			grid[x+1][y] != -c ||
-			grid[x-1][y] != -c
-	}
-	res := make([][]int, m)
-	for i := 0; i < m; i++ {
-		res[i] = make([]int, n)
-		for j := 0; j < n; j++ {
-			if grid[i][j] > 0 {
-				res[i][j] = grid[i][j]
-				continue
-			}
-			if isBorder(i, j) {
-				res[i][j] = color
-			} else {
-				res[i][j] = c
-			}
-		}
+			grid[x][y-1] != c ||
+			grid[x][y+1] != c ||
+			grid[x-1][y] != c ||
+			grid[x+1][y] != c
 	}
 
-	return res
+	connected := make([][2]int, 1, 1024)
+	hasSeen := [51][51]bool{}
+
+	connected[0] = [2]int{x0, y0}
+	hasSeen[x0][y0] = true
+
+	borders := make([][2]int, 0, 1024)
+	for len(connected) > 0 {
+		size := len(connected)
+		for i := 0; i < size; i++ {
+			p := connected[i]
+			if isBorder(p[0], p[1]) {
+				borders = append(borders, p)
+			}
+			for k := 0; k < 4; k++ {
+				x, y := p[0]+dx[k], p[1]+dy[k]
+				if 0 <= x && x < m &&
+					0 <= y && y < n &&
+					grid[x][y] == c &&
+					!hasSeen[x][y] {
+					connected = append(connected, [2]int{x, y})
+					hasSeen[x][y] = true
+				}
+			}
+		}
+		connected = connected[size:]
+	}
+
+	for _, b := range borders {
+		grid[b[0]][b[1]] = color
+	}
+
+	return grid
 }
