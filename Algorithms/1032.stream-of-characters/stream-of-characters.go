ba36a795c229742cb828c5fc8272883c80273f86
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-20 21:00:46 +0800

    1032 继续解决中

--- Algorithms/1032.stream-of-characters/stream-of-characters.go
+++ Algorithms/1032.stream-of-characters/stream-of-characters.go
@@ -1,40 +1,19 @@
 package problem1032
 
+import "strings"
+
 // StreamChecker check letters
 type StreamChecker struct {
-	dic  [][]*char
-	time int
-}
-
-type char struct {
-	index, queryTime int // alphabet order
-	isEnd            bool
-	next             *char
-}
-
-func makeChars(word string) *char {
-	n := len(word)
-	head := &char{
-		index: int(word[0] - 'a'),
-	}
-	end := head
-	for i := 1; i < n; i++ {
-		end.next = &char{
-			index: int(word[i] - 'a'),
-		}
-		end = end.next
-	}
-	end.isEnd = true
-	end.next = head
-	return head
+	dic [][]string
+	sb  strings.Builder
 }
 
 // Constructor returns StreamChecker
 func Constructor(words []string) StreamChecker {
-	dic := make([][]*char, 26)
+	dic := make([][]string, 26)
 	for _, word := range words {
-		l := makeChars(word)
-		dic[l.index] = append(dic[l.index], l)
+		index := int(word[len(word)-1] - 'a')
+		dic[index] = append(dic[index], word)
 	}
 	return StreamChecker{
 		dic: dic,
@@ -43,33 +22,9 @@ func Constructor(words []string) StreamChecker {
 
 // Query returns true if letter in words
 func (sc *StreamChecker) Query(letter byte) bool {
-	time := sc.time
-	sc.time++
+	sc.sb.WriteByte(letter)
 	index := int(letter - 'a')
-
-	chars := sc.dic[index]
-	sc.dic[index] = make([]*char, 0, len(chars))
-
-	res := false
-	for _, c := range chars {
-		c.queryTime = time
-		res = res || (c.isEnd && c.check())
-		c = c.next
-		sc.dic[c.index] = append(sc.dic[c.index], c)
-	}
+	words := sc.dic[index]
 
 	return res
-}
-
-func (c *char) check() bool {
-	head := c.next
-	qt := head.queryTime
-	for !head.isEnd {
-		head = head.next
-		qt++
-		if qt != head.queryTime {
-			return false
-		}
-	}
-	return true
 }
