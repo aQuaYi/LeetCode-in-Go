9530e3f989e89f26badabbc5003e0a8b56d4fe20
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-20 21:37:02 +0800

    1032 accepted, 328 ms, faster than 60.38%

--- Algorithms/1032.stream-of-characters/stream-of-characters.go
+++ Algorithms/1032.stream-of-characters/stream-of-characters.go
@@ -1,30 +1,45 @@
 package problem1032
 
-import "strings"
-
 // StreamChecker check letters
 type StreamChecker struct {
-	dic [][]string
-	sb  strings.Builder
+	dic    [][]string
+	querys []byte
 }
 
 // Constructor returns StreamChecker
 func Constructor(words []string) StreamChecker {
 	dic := make([][]string, 26)
+	maxLen := 0
 	for _, word := range words {
-		index := int(word[len(word)-1] - 'a')
+		Len := len(word)
+		maxLen = max(maxLen, Len)
+		index := int(word[Len-1] - 'a')
 		dic[index] = append(dic[index], word)
 	}
 	return StreamChecker{
-		dic: dic,
+		dic:    dic,
+		querys: make([]byte, 0, 40001),
 	}
 }
 
 // Query returns true if letter in words
 func (sc *StreamChecker) Query(letter byte) bool {
-	sc.sb.WriteByte(letter)
+	sc.querys = append(sc.querys, letter)
 	index := int(letter - 'a')
 	words := sc.dic[index]
+	for _, w := range words {
+		lq, lw := len(sc.querys), len(w)
+		if lq-lw >= 0 && w == string(sc.querys[lq-lw:]) {
+			return true
+		}
+	}
 
-	return res
+	return false
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
