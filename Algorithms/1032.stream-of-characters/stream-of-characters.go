3cdb561b92a5c6fe73f015e7290136b725060104
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-20 20:45:55 +0800

    1032 错误理解了题意

--- Algorithms/1032.stream-of-characters/stream-of-characters.go
+++ Algorithms/1032.stream-of-characters/stream-of-characters.go
@@ -2,16 +2,38 @@ package problem1032
 
 // StreamChecker check letters
 type StreamChecker struct {
-	dic []bool
+	dic [][]*char
+}
+
+type char struct {
+	index int // alphabet order
+	isEnd bool
+	next  *char
+}
+
+func makeLetters(word string) *char {
+	n := len(word)
+	head := &char{
+		index: int(word[0] - 'a'),
+	}
+	end := head
+	for i := 1; i < n; i++ {
+		end.next = &char{
+			index: int(word[i] - 'a'),
+		}
+		end = end.next
+	}
+	end.isEnd = true
+	end.next = head
+	return head
 }
 
 // Constructor returns StreamChecker
 func Constructor(words []string) StreamChecker {
-	dic := make([]bool, 26)
+	dic := make([][]*char, 26)
 	for _, word := range words {
-		for _, r := range word {
-			dic[int(r-'a')] = true
-		}
+		l := makeLetters(word)
+		dic[l.index] = append(dic[l.index], l)
 	}
 	return StreamChecker{
 		dic: dic,
@@ -19,8 +41,20 @@ func Constructor(words []string) StreamChecker {
 }
 
 // Query returns true if letter in words
-func (c *StreamChecker) Query(letter byte) bool {
-	return c.dic[letter-'a']
+func (sc *StreamChecker) Query(letter byte) bool {
+	index := int(letter - 'a')
+
+	chars := sc.dic[index]
+	sc.dic[index] = make([]*char, 0, len(chars))
+
+	res := false
+	for _, c := range chars {
+		res = res || c.isEnd
+		c = c.next
+		sc.dic[c.index] = append(sc.dic[c.index], c)
+	}
+
+	return res
 }
 
 /**
