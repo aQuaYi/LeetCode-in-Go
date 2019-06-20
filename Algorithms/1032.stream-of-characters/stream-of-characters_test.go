33422522fe0ca1c26d82169b2d68b157e42cc805
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-20 20:45:55 +0800

    1032 错误理解了题意

--- Algorithms/1032.stream-of-characters/stream-of-characters_test.go
+++ Algorithms/1032.stream-of-characters/stream-of-characters_test.go
@@ -15,12 +15,12 @@ var tcs = []struct {
 	// 可以有多个 testcase
 }
 
-func Test_Constructor(t *testing.T) {
+func Test_Case(t *testing.T) {
 	ast := assert.New(t)
-
+	//
 	words := []string{"cd", "f", "kl"}
 	streamChecker := Constructor(words)
-
+	//
 	ast.False(streamChecker.Query('a')) // return false
 	ast.False(streamChecker.Query('b')) // return false
 	ast.False(streamChecker.Query('c')) // return false
@@ -33,4 +33,64 @@ func Test_Constructor(t *testing.T) {
 	ast.False(streamChecker.Query('j')) // return false
 	ast.False(streamChecker.Query('k')) // return false
 	ast.True(streamChecker.Query('l'))  // return true, because 'kl' is in the wordlist
+}
+
+func Test_Case_2(t *testing.T) {
+	ast := assert.New(t)
+	//
+	words := []string{"cd"}
+	streamChecker := Constructor(words)
+	//
+	ast.False(streamChecker.Query('c')) // return false
+	ast.True(streamChecker.Query('d'))  // return true, because 'cd' is in the wordlist
+	ast.False(streamChecker.Query('c')) // return false
+}
+
+func Test_Case_3(t *testing.T) {
+	ast := assert.New(t)
+	//
+	words := []string{"cd"}
+	streamChecker := Constructor(words)
+	//
+	ast.False(streamChecker.Query('c')) // return false
+	ast.True(streamChecker.Query('d'))  // return true, because 'cd' is in the wordlist
+	ast.False(streamChecker.Query('d')) // return false
+}
+
+func Test_Case_4(t *testing.T) {
+	ast := assert.New(t)
+	//
+	words := []string{"cd"}
+	streamChecker := Constructor(words)
+	//
+	ast.False(streamChecker.Query('c')) // return false
+	ast.True(streamChecker.Query('d'))  // return true, because 'cd' is in the wordlist
+	ast.False(streamChecker.Query('c')) // return false
+	ast.True(streamChecker.Query('d'))  // return true, because 'cd' is in the wordlist
+}
+
+func Test_Case_5(t *testing.T) {
+	ast := assert.New(t)
+	//
+	words := []string{"cd", "cdd"}
+	streamChecker := Constructor(words)
+	//
+	ast.False(streamChecker.Query('c')) // return false
+	ast.True(streamChecker.Query('d'))  // return true, because 'cd' is in the wordlist
+	ast.True(streamChecker.Query('d'))  // return true, because 'cd' is in the wordlist
+}
+
+func Test_Case_6(t *testing.T) {
+	ast := assert.New(t)
+	//
+	words := []string{"abaa", "abaab", "aabbb", "bab", "ab"}
+	streamChecker := Constructor(words)
+
+	querys := []byte{'a', 'a', 'b', 'b', 'b', 'a', 'a', 'b', 'b', 'a', 'a', 'a', 'a', 'b', 'a', 'b', 'b', 'b', 'a', 'b', 'b', 'b', 'a', 'a', 'a', 'a', 'a', 'b', 'a', 'b', 'b', 'b', 'a', 'a', 'b', 'b', 'b', 'a', 'b', 'a'}
+	expects := []bool{false, false, true, false, true, false, false, true, false, false, false, false, false, true, false, true, false, false, false, true, false, false, false, false, false, false, false, true, false, true, false, false, false, false, true, false, true, false, true, false}
+
+	ast.Equal(len(querys), len(expects))
+	for i, q := range querys {
+		ast.Equal(expects[i], streamChecker.Query(q), "%d:%s", i, string(querys[:i+1]))
+	}
 }
