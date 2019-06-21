165b45d672f9a790bfac72cb00c8ea7452d0c41d
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-21 21:49:02 +0800

    1035 accepted. 4ms

--- Algorithms/1035.uncrossed-lines/uncrossed-lines.go
+++ Algorithms/1035.uncrossed-lines/uncrossed-lines.go
@@ -1,6 +1,26 @@
 package problem1035
 
+// ref: https://leetcode.com/problems/uncrossed-lines/discuss/282842/JavaC%2B%2BPython-DP-The-Longest-Common-Subsequence
 func maxUncrossedLines(A []int, B []int) int {
+	m, n := len(A), len(B)
+	dp := make([][]int, m+1)
+	dp[0] = make([]int, n+1)
+	for i := 1; i <= m; i++ {
+		dp[i] = make([]int, n+1)
+		for j := 1; j <= n; j++ {
+			if A[i-1] == B[j-1] {
+				dp[i][j] = 1 + dp[i-1][j-1]
+			} else {
+				dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+			}
+		}
+	}
+	return dp[m][n]
+}
 
-	return 0
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
