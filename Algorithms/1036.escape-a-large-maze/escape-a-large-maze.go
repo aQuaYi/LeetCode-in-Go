4b8a34ac320b4cf59256210f776dd023a288d5a5
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-22 11:50:24 +0800

    1036 doing

--- Algorithms/1036.escape-a-large-maze/escape-a-large-maze.go
+++ Algorithms/1036.escape-a-large-maze/escape-a-large-maze.go
@@ -1,6 +1,23 @@
 package problem1036
 
 func isEscapePossible(blocked [][]int, source []int, target []int) bool {
+	isBlocked := make(map[int]bool, len(blocked))
+	for _, b := range blocked {
+		x, y := b[0], b[1]
+		isBlocked[x<<32+y] = true
+	}
+	hasSeen := make(map[int]bool, len(blocked))
+
+	// tx, ty := target[0], target[1]
+
+	// var dfs func(int, int) bool
+	// dfs = func(x, y int) bool {
+	// 	if x == tx && y == ty {
+	// 		return true
+	// 	}
+
+	// }
+	// dfs()
 
 	return false
 }
