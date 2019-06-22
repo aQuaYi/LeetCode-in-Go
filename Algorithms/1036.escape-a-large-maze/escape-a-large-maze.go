4f2798f7bd845e01b6f86e0169034c9476aa5c86
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-22 16:26:25 +0800

    1036 accepted. 152 ms, faster than 79.17%

--- Algorithms/1036.escape-a-large-maze/escape-a-large-maze.go
+++ Algorithms/1036.escape-a-large-maze/escape-a-large-maze.go
@@ -1,23 +1,59 @@
 package problem1036
 
+var dx = []int{0, 0, 1, -1}
+var dy = []int{1, -1, 0, 0}
+
+// M is length and width of the large maze
+const M = 1e6
+
+// MAX 代表了 blocked 能够围起来的最大数量
+// 题目里面的说了 len(blocked)<=200
+// 这样的话，能够围起来的点，不会超过 20000 个
+const MAX = 20000
+
 func isEscapePossible(blocked [][]int, source []int, target []int) bool {
 	isBlocked := make(map[int]bool, len(blocked))
 	for _, b := range blocked {
 		x, y := b[0], b[1]
 		isBlocked[x<<32+y] = true
 	}
-	hasSeen := make(map[int]bool, len(blocked))
 
-	// tx, ty := target[0], target[1]
+	isOpen := func(source, target []int) bool {
+		tx, ty := target[0], target[1]
+		//
+		points := make([][]int, 0, MAX)
+		hasSeen := make(map[int]bool, MAX)
+		//
+		sx, sy := source[0], source[1]
+		points, hasSeen[sx<<32+sy] = append(points, source), true
+		//
+		count := 0
+		for len(points) > 0 {
+			size := len(points)
+			for i := 0; i < size; i++ {
+				px, py := points[i][0], points[i][1]
+				for k := 0; k < 4; k++ {
+					x, y := px+dx[k], py+dy[k]
+					if x == tx && y == ty {
+						return true
+					}
+					p := x<<32 + y
+					if 0 <= x && x < M &&
+						0 <= y && y < M &&
+						!isBlocked[p] &&
+						!hasSeen[p] {
+						points, hasSeen[p] = append(points, []int{x, y}), true
+					}
+				}
+			}
+			points = points[size:]
+			count += size
+			if count >= MAX {
+				return true
+			}
+		}
+		return false
+	}
 
-	// var dfs func(int, int) bool
-	// dfs = func(x, y int) bool {
-	// 	if x == tx && y == ty {
-	// 		return true
-	// 	}
-
-	// }
-	// dfs()
-
-	return false
+	return isOpen(source, target) && isOpen(target, source)
 }
