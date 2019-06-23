98cf678e24c619ae0e8b1358d4d7daede39a661a
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-23 15:30:30 +0800

    1039 错误的答案

--- Algorithms/1039.minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.go
+++ Algorithms/1039.minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.go
@@ -1,26 +1,25 @@
 package problem1039
 
 func minScoreTriangulation(A []int) int {
-	A = move(A)
-	return sum(A)
-}
-
-func sum(A []int) int {
-	sum, n := 0, len(A)
-	a := A[0]
-	for i := 1; i+1 < n; i++ {
-		sum += a * A[i] * A[i+1]
-	}
-	return sum
-}
-
-func move(A []int) []int {
-	a, index := A[0], 0
 	n := len(A)
-	for i := 1; i < n; i++ {
-		if a > A[i] {
-			a, index = A[i], i
+	A = append(A, A[0])
+	dp := [51][51]int{}
+
+	for r := 2; r <= n; r++ {
+		for i := 1; i+r-1 <= n; i++ {
+			j := i + r - 1
+			dp[i][j] = 1<<63 - 1
+			for k := i; k <= j; k++ {
+				dp[i][j] = min(dp[i][j], dp[i][k]+dp[k+1][j]+A[i-1]*A[k]*A[j])
+			}
 		}
 	}
-	return append(A, A[:index]...)[index:]
+	return dp[1][n]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
