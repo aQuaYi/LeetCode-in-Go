1513c49ee0c389b3e0bf0c2ac7aa349542ac69e9
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-23 15:49:59 +0800

    1039 done

--- Algorithms/1039.minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.go
+++ Algorithms/1039.minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.go
@@ -2,19 +2,17 @@ package problem1039
 
 func minScoreTriangulation(A []int) int {
 	n := len(A)
-	A = append(A, A[0])
-	dp := [51][51]int{}
-
-	for r := 2; r <= n; r++ {
-		for i := 1; i+r-1 <= n; i++ {
-			j := i + r - 1
-			dp[i][j] = 1<<63 - 1
-			for k := i; k <= j; k++ {
-				dp[i][j] = min(dp[i][j], dp[i][k]+dp[k+1][j]+A[i-1]*A[k]*A[j])
+	dp := [50][50]int{}
+	for d := 2; d < n; d++ {
+		for i := 0; i+d < n; i++ {
+			k := i + d
+			dp[i][k] = 1 << 30
+			for j := i + 1; j < k; j++ {
+				dp[i][k] = min(dp[i][k], dp[i][j]+dp[j][k]+A[i]*A[j]*A[k])
 			}
 		}
 	}
-	return dp[1][n]
+	return dp[0][n-1]
 }
 
 func min(a, b int) int {
