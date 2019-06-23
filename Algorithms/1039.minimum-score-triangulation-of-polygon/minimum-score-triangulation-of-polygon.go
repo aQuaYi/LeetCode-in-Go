26e52cf9b13e613ee59be81dc69f4c1a234823ca
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-23 14:14:00 +0800

    1039 错误的解法

--- Algorithms/1039.minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.go
+++ Algorithms/1039.minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.go
@@ -1,6 +1,42 @@
 package problem1039
 
 func minScoreTriangulation(A []int) int {
+	if len(A) < 3 {
+		return 0
+	}
+	B := move(A)
+	a := sum(A) + minScoreTriangulation(next(A))
+	b := sum(B) + minScoreTriangulation(next(B))
+	return min(a, b)
+}
 
-return 0
+func sum(A []int) int {
+	sum, n := 0, len(A)
+	for i := 0; i+2 < n; i += 2 {
+		sum += A[i] * A[i+1] * A[i+2]
+	}
+	if n%3 != 2 && n != 3 {
+		sum += A[n-2] * A[n-1] * A[0]
+	}
+	return sum
+}
+
+func next(A []int) []int {
+	n := len(A)
+	res := make([]int, 0, (n+1)/2)
+	for i := 0; i < n; i += 2 {
+		res = append(res, A[i])
+	}
+	return res
+}
+
+func move(A []int) []int {
+	return append(A, A[0])[1:]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
