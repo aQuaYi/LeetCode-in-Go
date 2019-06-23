7e8cc3190eaa8985905e2731e0c4be15118d605a
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-23 15:09:18 +0800

    1039 开始尝试动态规划解法

--- Algorithms/1039.minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.go
+++ Algorithms/1039.minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.go
@@ -1,42 +1,26 @@
 package problem1039
 
 func minScoreTriangulation(A []int) int {
-	if len(A) < 3 {
-		return 0
-	}
-	B := move(A)
-	a := sum(A) + minScoreTriangulation(next(A))
-	b := sum(B) + minScoreTriangulation(next(B))
-	return min(a, b)
+	A = move(A)
+	return sum(A)
 }
 
 func sum(A []int) int {
 	sum, n := 0, len(A)
-	for i := 0; i+2 < n; i += 2 {
-		sum += A[i] * A[i+1] * A[i+2]
-	}
-	if n%3 != 2 && n != 3 {
-		sum += A[n-2] * A[n-1] * A[0]
+	a := A[0]
+	for i := 1; i+1 < n; i++ {
+		sum += a * A[i] * A[i+1]
 	}
 	return sum
 }
 
-func next(A []int) []int {
-	n := len(A)
-	res := make([]int, 0, (n+1)/2)
-	for i := 0; i < n; i += 2 {
-		res = append(res, A[i])
-	}
-	return res
-}
-
 func move(A []int) []int {
-	return append(A, A[0])[1:]
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
+	a, index := A[0], 0
+	n := len(A)
+	for i := 1; i < n; i++ {
+		if a > A[i] {
+			a, index = A[i], i
+		}
 	}
-	return b
+	return append(A, A[:index]...)[index:]
 }
