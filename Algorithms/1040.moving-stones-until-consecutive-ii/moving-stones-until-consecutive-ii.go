22518b28fd5d6e2844b94fa0f98697c7e2d207e5
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-24 23:09:16 +0800

    1040 done

--- Algorithms/1040.moving-stones-until-consecutive-ii/moving-stones-until-consecutive-ii.go
+++ Algorithms/1040.moving-stones-until-consecutive-ii/moving-stones-until-consecutive-ii.go
@@ -4,18 +4,22 @@ import "sort"
 
 func numMovesStonesII(stones []int) []int {
 	sort.Ints(stones)
-	return []int{minMove(stones), maxMove(stones)}
+	return []int{low(stones), high(stones)}
 }
 
-func maxMove(s []int) int {
+func high(s []int) int {
 	n := len(s)
 	return max(s[n-1]-s[1], s[n-2]-s[0]) - n + 2
 }
 
-func minMove(s []int) int {
+func low(s []int) int {
 	n := len(s)
-	width := 0
-	i, j := 0, 1
+	// corner case
+	if (s[n-2]-s[0] == n-2 && s[n-1]-s[n-2] > 2) ||
+		(s[n-1]-s[1] == n-2 && s[1]-s[0] > 2) {
+		return 2
+	}
+	width, i, j := 0, 0, 1
 	for ; j < n; j++ {
 		if s[j]-s[i] < n {
 			continue
@@ -24,6 +28,8 @@ func minMove(s []int) int {
 		i = j
 	}
 	width = max(width, j-i)
+	// finally, all stone move into maxWidth windows
+	// so need move n-width stones
 	return n - width
 }
 
