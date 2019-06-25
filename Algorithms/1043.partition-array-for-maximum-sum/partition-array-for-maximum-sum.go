e9b161f7764d59227fefb3ba69d302f74dd67e16
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-25 15:36:03 +0800

    1043 done

--- Algorithms/1043.partition-array-for-maximum-sum/partition-array-for-maximum-sum.go
+++ Algorithms/1043.partition-array-for-maximum-sum/partition-array-for-maximum-sum.go
@@ -3,19 +3,17 @@ package problem1043
 // ref: https://leetcode.com/problems/partition-array-for-maximum-sum/discuss/290863/JavaC%2B%2BPython-DP
 func maxSumAfterPartitioning(A []int, K int) int {
 	n := len(A)
-	dp := make([]int, n)
-	for i := 0; i < n; i++ {
-		curMax := 0
-		for k := 1; k <= K && 0 <= i-k+1; k++ {
-			curMax = max(curMax, A[i-k+1])
-			cur := curMax * k
-			if i >= k {
-				cur += dp[i-k]
-			}
-			dp[i] = max(dp[i], cur)
+	dp := make([]int, n+1)
+	// A's length grows up from 1 to n
+	for l := 1; l <= n; l++ {
+		m := 0 // max value of last k item in A[:l]
+		for k := 1; k <= K && 0 <= l-k; k++ {
+			m = max(m, A[l-k])
+			sum := dp[l-k] + m*k
+			dp[l] = max(dp[l], sum)
 		}
 	}
-	return dp[n-1]
+	return dp[n]
 }
 
 func max(a, b int) int {
