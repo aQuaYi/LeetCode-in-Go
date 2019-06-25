716b785b10511b5b0c6f7c7e4e7799edfa122409
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-25 15:15:57 +0800

    1043 copy 了答案

--- Algorithms/1043.partition-array-for-maximum-sum/partition-array-for-maximum-sum.go
+++ Algorithms/1043.partition-array-for-maximum-sum/partition-array-for-maximum-sum.go
@@ -1,57 +1,25 @@
 package problem1043
 
+// ref: https://leetcode.com/problems/partition-array-for-maximum-sum/discuss/290863/JavaC%2B%2BPython-DP
 func maxSumAfterPartitioning(A []int, K int) int {
-	i := indexOfMax(A)
 	n := len(A)
-	if n <= K {
-		return n * A[i]
-	}
-	L, R := max(0, i-K+1), min(n-1, i+K-1) // [L,R]
-	mL, mR, mSum := L, R, 0
-	for l := L; l <= i && l+K-1 <= R; l++ {
-		r := l + K - 1
-		sum := 0
-		for j := L; j <= R; j++ {
-			if l <= j && j <= r {
-				continue
+	dp := make([]int, n)
+	for i := 0; i < n; i++ {
+		curMax := 0
+		for k := 1; k <= K && 0 <= i-k+1; k++ {
+			curMax = max(curMax, A[i-k+1])
+			cur := curMax * k
+			if i >= k {
+				cur += dp[i-k]
 			}
-			sum += A[j]
-		}
-		if mSum < sum {
-			mSum, mL, mR = sum, l, r
+			dp[i] = max(dp[i], cur)
 		}
 	}
-	left := 0
-	if mL > 0 {
-		left = maxSumAfterPartitioning(A[:mL], K)
-	}
-
-	right := 0
-	if mR < n-1 {
-		right = maxSumAfterPartitioning(A[mR+1:], K)
-	}
-	return A[i]*K + left + right
-}
-
-func indexOfMax(A []int) int {
-	a, i := A[0], 0
-	for j := 1; j < len(A); j++ {
-		if a < A[j] {
-			a, i = A[j], j
-		}
-	}
-	return i
+	return dp[n-1]
 }
 
 func max(a, b int) int {
 	if a > b {
-		return a
-	}
-	return b
-}
-
-func min(a, b int) int {
-	if a < b {
 		return a
 	}
 	return b
