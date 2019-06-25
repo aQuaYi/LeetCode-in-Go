3f45a632148bda0eb4a9b96f2ed2842d6b71a09f
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-25 14:32:59 +0800

    1043 prepared

---
+++ Algorithms/1043.partition-array-for-maximum-sum/partition-array-for-maximum-sum.go
@@ -0,0 +1,6 @@
+package problem1043
+
+func maxSumAfterPartitioning(A []int, K int) int {
+
+return 0
+}
---
+++ Algorithms/1043.partition-array-for-maximum-sum/partition-array-for-maximum-sum_test.go
@@ -0,0 +1,39 @@
+package problem1043
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	A   []int
+	K   int
+	ans int
+}{
+
+	{
+		[]int{1, 15, 7, 9, 2, 5, 10},
+		3,
+		84,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_maxSumAfterPartitioning(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		ast.Equal(tc.ans, maxSumAfterPartitioning(tc.A, tc.K), "输入:%v", tc)
+	}
+}
+
+func Benchmark_maxSumAfterPartitioning(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			maxSumAfterPartitioning(tc.A, tc.K)
+		}
+	}
+}
