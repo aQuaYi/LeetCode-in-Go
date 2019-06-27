135d9faa8c57b86301e11e6a13b574dd46f59ed1
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-27 17:29:58 +0800

    1030 copy the fastest answer

--- Algorithms/1030.matrix-cells-in-distance-order/matrix-cells-in-distance-order.go
+++ Algorithms/1030.matrix-cells-in-distance-order/matrix-cells-in-distance-order.go
@@ -1,29 +1,31 @@
 package problem1030
 
-var dr = []int{-1, 0, 0, 1}
-var dc = []int{0, -1, 1, 0}
+func abs(n int) int {
+	y := n >> 63
+	return (n ^ y) - y
+}
+
+var tmp map[int][][]int = make(map[int][][]int)
+var result [10000][]int
 
 func allCellsDistOrder(R int, C int, r0 int, c0 int) [][]int {
-	RC := R * C
-	res := make([][]int, 1, RC)
-	hasSeen := [10000]bool{}
-	res[0], hasSeen[r0*100+c0] = []int{r0, c0}, true
 
-	b, e := 0, len(res)
-	for b < e {
-		for i := b; i < e; i++ {
-			pr, pc := res[i][0], res[i][1]
-			for k := 0; k < 4; k++ {
-				r, c := pr+dr[k], pc+dc[k]
-				if 0 <= r && r < R &&
-					0 <= c && c < C &&
-					!hasSeen[r*100+c] {
-					res, hasSeen[r*100+c] = append(res, []int{r, c}), true
-				}
-			}
+	for indexR := 0; indexR < R; indexR++ {
+		for indexC := 0; indexC < C; indexC++ {
+			index := abs(indexR-r0) + abs(indexC-c0)
+			tmp[index] = append(tmp[index], []int{indexR, indexC})
 		}
-		b, e = e, len(res)
+	}
+	indexResult := 0
+	for index := 0; index <= R+C; index++ {
+		val := tmp[index]
+		num := len(val)
+		end := indexResult + num
+		copy(result[indexResult:end], val)
+		indexResult = end
+
+		delete(tmp, index)
 	}
 
-	return res
+	return result[:indexResult]
 }
