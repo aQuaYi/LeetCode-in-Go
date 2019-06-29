70f154f39b6ea124afcbc0305ff153437981f025
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-29 20:14:34 +0800

    1037 done

--- Algorithms/1037.valid-boomerang/valid-boomerang.go
+++ Algorithms/1037.valid-boomerang/valid-boomerang.go
@@ -1,27 +1,8 @@
 package problem1037
 
-func isBoomerang(points [][]int) bool {
-	return isAllDistinct(points) && !isLine(points)
-}
-
-func isAllDistinct(points [][]int) bool {
-	for i := 0; i < len(points); i++ {
-		for j := i + 1; j < len(points); j++ {
-			if isEqual(points[i], points[j]) {
-				return false
-			}
-		}
-	}
-	return true
-}
-
-func isEqual(p, q []int) bool {
-	return p[0] == q[0] && p[1] == q[1]
-}
-
-func isLine(points [][]int) bool {
-	a, b, c := points[0], points[1], points[2]
-	h0, w0 := a[1]-b[1], a[0]-b[0]
-	h1, w1 := a[1]-c[1], a[0]-c[0]
-	return h0*w1 == h1*w0
+func isBoomerang(p [][]int) bool {
+	x0, y0 := p[0][0], p[0][1]
+	x1, y1 := p[1][0], p[1][1]
+	x2, y2 := p[2][0], p[2][1]
+	return (x0-x2)*(y1-y2) != (y0-y2)*(x1-x2)
 }
