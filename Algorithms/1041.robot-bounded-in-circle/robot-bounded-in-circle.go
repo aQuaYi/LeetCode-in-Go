235c02810ae485dc06f9c24deb2cdb7b79169260
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-06-30 19:22:16 +0800

    1041 wrong answer

--- Algorithms/1041.robot-bounded-in-circle/robot-bounded-in-circle.go
+++ Algorithms/1041.robot-bounded-in-circle/robot-bounded-in-circle.go
@@ -1,16 +1,60 @@
 package problem1041
 
 func isRobotBounded(instructions string) bool {
-	g, l, r := 0, 0, 0
-	for _, b := range instructions {
-		switch b {
-		case 'G':
-			g++
-		case 'L':
-			l++
-		case 'R':
-			r++
-		}
+	r := newRobot()
+	for _, i := range instructions {
+		r.receive(i)
 	}
-	return g == 0 || (l-r)%4 != 0
+	return (r.x == 0 && r.y == 0) ||
+		(r.d != north)
+}
+
+type direction int
+
+const (
+	north direction = iota
+	east
+	south
+	west
+)
+
+type robot struct {
+	d    direction
+	x, y int
+}
+
+func newRobot() *robot {
+	return &robot{
+		d: north,
+	}
+}
+
+func (r *robot) receive(instruction rune) {
+	if instruction == 'G' {
+		r.move()
+	} else {
+		r.turn(instruction)
+	}
+}
+
+func (r *robot) move() {
+	switch r.d {
+	case north:
+		r.y++
+	case east:
+		r.x++
+	case south:
+		r.y--
+	case west:
+		r.x--
+	}
+}
+
+func (r *robot) turn(instruction rune) {
+	if instruction == 'R' {
+		r.d++
+	} else {
+		r.d--
+	}
+	r.d = r.d % 4
 }
