8043950e726e6defc66f2c7fc92ce4980ea0a094
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-01 20:43:52 +0800

    1042 added

---
+++ Algorithms/1042.flower-planting-with-no-adjacent/flower-planting-with-no-adjacent.go
@@ -0,0 +1,6 @@
+package problem1042
+
+func gardenNoAdj(N int, paths [][]int) []int {
+
+return nil
+}
---
+++ Algorithms/1042.flower-planting-with-no-adjacent/flower-planting-with-no-adjacent_test.go
@@ -0,0 +1,51 @@
+package problem1042
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	N     int
+	paths [][]int
+	ans   []int
+}{
+
+	{
+		3,
+		[][]int{{1, 2}, {2, 3}, {3, 1}},
+		[]int{1, 2, 3},
+	},
+
+	{
+		4,
+		[][]int{{1, 2}, {3, 4}},
+		[]int{1, 2, 1, 2},
+	},
+
+	{
+		4,
+		[][]int{{1, 2}, {2, 3}, {3, 4}, {4, 1}, {1, 3}, {2, 4}},
+		[]int{1, 2, 3, 4},
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_gardenNoAdj(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		ast.Equal(tc.ans, gardenNoAdj(tc.N, tc.paths), "输入:%v", tc)
+	}
+}
+
+func Benchmark_gardenNoAdj(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			gardenNoAdj(tc.N, tc.paths)
+		}
+	}
+}
