a1c491fadaf1be0e75b3ef4bd402778d09bb25e6
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-02 20:18:57 +0800

    1046 accepted.

---
+++ Algorithms/1046.last-stone-weight/last-stone-weight.go
@@ -0,0 +1,45 @@
+package problem1046
+
+import "container/heap"
+
+func lastStoneWeight(stones []int) int {
+	pq := PQ(stones)
+	heap.Init(&pq)
+	for len(pq) >= 2 {
+		a, b := heap.Pop(&pq).(int), heap.Pop(&pq).(int)
+		if a == b {
+			continue
+		}
+		heap.Push(&pq, a-b)
+	}
+	if len(pq) == 0 {
+		return 0
+	}
+	return pq[0]
+}
+
+// PQ implements heap.Interface and holds entries.
+type PQ []int
+
+func (pq PQ) Len() int { return len(pq) }
+
+func (pq PQ) Less(i, j int) bool {
+	return pq[i] > pq[j]
+}
+
+func (pq PQ) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+
+// Push 往 pq 中放 entry
+func (pq *PQ) Push(x interface{}) {
+	temp := x.(int)
+	*pq = append(*pq, temp)
+}
+
+// Pop 从 pq 中取出最优先的 entry
+func (pq *PQ) Pop() interface{} {
+	temp := (*pq)[len(*pq)-1]
+	*pq = (*pq)[0 : len(*pq)-1]
+	return temp
+}
---
+++ Algorithms/1046.last-stone-weight/last-stone-weight_test.go
@@ -0,0 +1,37 @@
+package problem1046
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	stones []int
+	ans    int
+}{
+
+	{
+		[]int{2, 7, 4, 1, 8, 1},
+		1,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_lastStoneWeight(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		ast.Equal(tc.ans, lastStoneWeight(tc.stones), "输入:%v", tc)
+	}
+}
+
+func Benchmark_lastStoneWeight(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			lastStoneWeight(tc.stones)
+		}
+	}
+}
