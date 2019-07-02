34d54dcb9bfbf28966f9bd4084e5ff6da025c718
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-02 20:22:02 +0800

    1046 done

--- Algorithms/1046.last-stone-weight/last-stone-weight.go
+++ Algorithms/1046.last-stone-weight/last-stone-weight.go
@@ -23,21 +23,17 @@ type PQ []int
 
 func (pq PQ) Len() int { return len(pq) }
 
-func (pq PQ) Less(i, j int) bool {
-	return pq[i] > pq[j]
-}
+func (pq PQ) Less(i, j int) bool { return pq[i] > pq[j] }
 
-func (pq PQ) Swap(i, j int) {
-	pq[i], pq[j] = pq[j], pq[i]
-}
+func (pq PQ) Swap(i, j int) { pq[i], pq[j] = pq[j], pq[i] }
 
-// Push 往 pq 中放 entry
+// Push 往 pq 中放 int
 func (pq *PQ) Push(x interface{}) {
 	temp := x.(int)
 	*pq = append(*pq, temp)
 }
 
-// Pop 从 pq 中取出最优先的 entry
+// Pop 从 pq 中取出最优先的 int
 func (pq *PQ) Pop() interface{} {
 	temp := (*pq)[len(*pq)-1]
 	*pq = (*pq)[0 : len(*pq)-1]
