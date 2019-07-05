484a11e831202669403fa8683da6c07a59b6dc41
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-05 22:12:48 +0800

    1049 wrong answer

--- Algorithms/1049.last-stone-weight-ii/last-stone-weight-ii.go
+++ Algorithms/1049.last-stone-weight-ii/last-stone-weight-ii.go
@@ -1,6 +1,30 @@
 package problem1049
 
 func lastStoneWeightII(stones []int) int {
+	n := len(stones)
+	dp := [32][32]int{}
 
-return 0
+	for width := 1; width <= n; width++ {
+		for i := 1; i+width-1 <= n; i++ {
+			j := i + width - 1
+			dp[i][j] = min(abs(stones[i-1]-dp[i+1][j]),
+				abs(stones[j-1]-dp[i][j-1]))
+		}
+	}
+
+	return dp[1][n]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
 }
