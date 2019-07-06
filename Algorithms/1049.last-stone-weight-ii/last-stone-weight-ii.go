d2e69ba54b956a921171ca4d1ceda3adcaf12080
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-06 12:19:51 +0800

    1049 accepted.

--- Algorithms/1049.last-stone-weight-ii/last-stone-weight-ii.go
+++ Algorithms/1049.last-stone-weight-ii/last-stone-weight-ii.go
@@ -1,30 +1,27 @@
 package problem1049
 
 func lastStoneWeightII(stones []int) int {
-	n := len(stones)
-	dp := [32][32]int{}
-
-	for width := 1; width <= n; width++ {
-		for i := 1; i+width-1 <= n; i++ {
-			j := i + width - 1
-			dp[i][j] = 3000
-
+	dp := [3001]bool{}
+	dp[0] = true
+	sum := 0
+	for _, s := range stones {
+		sum += s
+		for i := sum; i >= s; i-- {
+			// 把所有可能的 sum 组合都记录下来
+			dp[i] = dp[i] || dp[i-s]
 		}
 	}
-
-	return dp[1][n]
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
+	// 此时 sum 是 sum(stones)
+	// stones 分成两组后，重量分别是 part 和 sum-part
+	// 答案是 sum-part - part
+	// 最好的情况是 part = sum/2
+	// 如果不行的话，就看看 part-- 行不行
+	// 以此类推直到 part = 0
+	part := sum / 2
+	for ; part >= 0; part-- {
+		if dp[part] {
+			break
+		}
 	}
-	return b
-}
-
-func abs(a int) int {
-	if a < 0 {
-		return -a
-	}
-	return a
+	return sum - part - part
 }
