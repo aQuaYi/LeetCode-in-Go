32854f26becc9623911747e3e7c9e6d8b00abbdb
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-07 14:15:58 +0800

    1051 done

--- Algorithms/1051.height-checker/height-checker.go
+++ Algorithms/1051.height-checker/height-checker.go
@@ -1,6 +1,16 @@
 package problem1051
 
-func heightChecker(heights []int) int {
+import "sort"
 
-return 0
+func heightChecker(heights []int) int {
+	sorted := make([]int, len(heights))
+	copy(sorted, heights)
+	sort.Ints(sorted)
+	res := 0
+	for i := 0; i < len(heights); i++ {
+		if heights[i] != sorted[i] {
+			res++
+		}
+	}
+	return res
 }
