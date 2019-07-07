52f1ede64b716a93c56addb45ca9597fe3fcfc74
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-07 14:51:27 +0800

    1052 暂时觉得不用 dp

--- Algorithms/1052.grumpy-bookstore-owner/grumpy-bookstore-owner.go
+++ Algorithms/1052.grumpy-bookstore-owner/grumpy-bookstore-owner.go
@@ -1,6 +1,21 @@
 package problem1052
 
 func maxSatisfied(customers []int, grumpy []int, X int) int {
+	n := len(customers)
+	s := make([]int, n+1)
+	u := make([]int, n+1)
+	sum := 0
+	for i := 1; i <= n; i++ {
+		sum += customers[i-1]
+		u[i] = u[i-1] + customers[i-1]*grumpy[i-1]
+		s[i] = sum - u[i]
+	}
+	return 0
+}
 
-return 0
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
