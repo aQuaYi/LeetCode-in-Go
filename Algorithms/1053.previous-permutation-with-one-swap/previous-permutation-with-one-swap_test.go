43b8994e80aaf7e28cc8bae24ab03696240ba49c
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-08 19:48:34 +0800

    1053 added

---
+++ Algorithms/1053.previous-permutation-with-one-swap/previous-permutation-with-one-swap_test.go
@@ -0,0 +1,52 @@
+package problem1053
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	A   []int
+	ans []int
+}{
+
+	{
+		[]int{3, 2, 1},
+		[]int{3, 1, 2},
+	},
+
+	{
+		[]int{1, 1, 5},
+		[]int{1, 1, 5},
+	},
+
+	{
+		[]int{1, 9, 4, 6, 7},
+		[]int{1, 7, 4, 6, 9},
+	},
+
+	{
+		[]int{3, 1, 1, 3},
+		[]int{1, 3, 1, 3},
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_prevPermOpt1(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		ast.Equal(tc.ans, prevPermOpt1(tc.A), "输入:%v", tc)
+	}
+}
+
+func Benchmark_prevPermOpt1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			prevPermOpt1(tc.A)
+		}
+	}
+}
