8448fd961de772b1c4dc2aadab5e16795fd5f1d1
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-08 19:57:21 +0800

    1053 wrong answer

--- Algorithms/1053.previous-permutation-with-one-swap/previous-permutation-with-one-swap.go
+++ Algorithms/1053.previous-permutation-with-one-swap/previous-permutation-with-one-swap.go
@@ -1,6 +1,23 @@
 package problem1053
 
 func prevPermOpt1(A []int) []int {
+	n := len(A)
+	stack, top := make([]int, n), 0
+	stack[top] = 0
 
-return nil
+	for i := 1; i < n; i++ {
+		if A[i] <= A[stack[top]] {
+			top++
+			stack[top] = i
+			continue
+		}
+		for top-1 >= 0 && A[i] > A[stack[top-1]] {
+			top--
+		}
+		j := stack[top]
+		A[i], A[j] = A[j], A[i]
+		break
+	}
+
+	return A
 }
