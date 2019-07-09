9f8a5ef81ca37603940ff0323d0d95bf4bd34305
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-09 20:26:28 +0800

    1054 wrong answer

--- Algorithms/1054.distant-barcodes/distant-barcodes.go
+++ Algorithms/1054.distant-barcodes/distant-barcodes.go
@@ -1,6 +1,16 @@
 package problem1054
 
-func rearrangeBarcodes(barcodes []int) []int {
-
-return nil
+func rearrangeBarcodes(A []int) []int {
+	n := len(A)
+	for i := 1; i < n; i++ {
+		if A[i] != A[i-1] {
+			continue
+		}
+		j := n - 1
+		for A[j] == A[i] {
+			j--
+		}
+		A[i], A[j] = A[j], A[i]
+	}
+	return A
 }
