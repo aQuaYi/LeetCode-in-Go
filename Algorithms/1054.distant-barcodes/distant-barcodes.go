d5188e9ca270504038e0660e3d26c11c1638652b
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-09 21:22:07 +0800

    1054 still wrong

--- Algorithms/1054.distant-barcodes/distant-barcodes.go
+++ Algorithms/1054.distant-barcodes/distant-barcodes.go
@@ -1,12 +1,27 @@
 package problem1054
 
-import "sort"
-
 func rearrangeBarcodes(A []int) []int {
 	n := len(A)
-	sort.Ints(A)
-	for i, j := 1, (n-1)/2*2; i < j; i, j = i+2, j-2 {
-		A[i], A[j] = A[j], A[i]
+
+	// odds in left, evens in right
+	i, j := 0, n-1
+	for i < j {
+		if A[i]%2 == 0 {
+			A[i], A[j] = A[j], A[i]
+			j--
+		} else {
+			i++
+		}
 	}
+	i, j = 0, n/2*2-1
+	if A[n/2]%2 == 1 {
+		i, j = 1, n-1
+	}
+	for i < j {
+		A[i], A[j] = A[j], A[i]
+		i += 2
+		j -= 2
+	}
+
 	return A
 }
