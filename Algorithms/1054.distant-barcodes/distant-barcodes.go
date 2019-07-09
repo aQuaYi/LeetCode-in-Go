d8d930b0581111b93aef52a43f9bf34194337aef
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-09 23:11:55 +0800

    1054 done

--- Algorithms/1054.distant-barcodes/distant-barcodes.go
+++ Algorithms/1054.distant-barcodes/distant-barcodes.go
@@ -1,31 +1,30 @@
 package problem1054
 
+import "sort"
+
 func rearrangeBarcodes(A []int) []int {
 	n := len(A)
 
 	count := [10001]int{}
-	max, maxA := 0, 0
 	for _, a := range A {
 		count[a]++
-		if max < count[a] {
-			max, maxA = count[a], a
-		}
 	}
+
+	sort.Slice(A, func(i int, j int) bool {
+		if count[A[i]] == count[A[j]] {
+			return A[i] < A[j]
+		}
+		return count[A[i]] > count[A[j]]
+	})
 
 	res := make([]int, n)
-	for i := 0; i < max*2; i += 2 {
-		res[i] = maxA
-	}
-
-	i := 1
+	i := 0
 	for _, a := range A {
-		if a == maxA {
-			continue
-		}
-		for res[i] != 0 {
-			i++
-		}
 		res[i] = a
+		i += 2
+		if i >= n {
+			i = 1
+		}
 	}
 
 	return res
