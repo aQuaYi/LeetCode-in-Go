6ac4e2d9dace66cfe55d52635d49a686d6341bc6
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-10 19:38:55 +0800

    1071 wrong answer

--- Algorithms/1071.greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.go
+++ Algorithms/1071.greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.go
@@ -1,6 +1,25 @@
 package problem1071
 
-func gcdOfStrings(str1 string, str2 string) string {
+import "strings"
 
-	return ""
+func gcdOfStrings(s1, s2 string) string {
+	if len(s1) < len(s2) {
+		s1, s2 = s2, s1
+	}
+	l1, l2 := len(s1), len(s2)
+	max := l2 / 2
+	res := ""
+	for i := 1; i <= max; i++ {
+		if l2%i != 0 || l1%i != 0 {
+			continue
+		}
+		tmp := s2[:l2/i]
+		if s2 == strings.Repeat(tmp, i) &&
+			s1 == strings.Repeat(tmp, l1/i) {
+			res = tmp
+			break
+		}
+	}
+
+	return res
 }
