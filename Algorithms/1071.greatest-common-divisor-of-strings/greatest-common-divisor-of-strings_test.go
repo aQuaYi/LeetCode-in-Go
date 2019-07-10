79f78c98d25b1d3777a293e0dfc8acec16d044ed
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-10 19:28:07 +0800

    1071 added

---
+++ Algorithms/1071.greatest-common-divisor-of-strings/greatest-common-divisor-of-strings_test.go
@@ -0,0 +1,51 @@
+package problem1071
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	str1 string
+	str2 string
+	ans  string
+}{
+
+	{
+		"ABCABC",
+		"ABC",
+		"ABC",
+	},
+
+	{
+		"ABABAB",
+		"ABAB",
+		"AB",
+	},
+
+	{
+		"LEET",
+		"CODE",
+		"",
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_gcdOfStrings(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		ast.Equal(tc.ans, gcdOfStrings(tc.str1, tc.str2), "输入:%v", tc)
+	}
+}
+
+func Benchmark_gcdOfStrings(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			gcdOfStrings(tc.str1, tc.str2)
+		}
+	}
+}
