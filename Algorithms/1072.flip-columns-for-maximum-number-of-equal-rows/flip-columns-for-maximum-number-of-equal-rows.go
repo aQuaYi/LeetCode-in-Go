a65a739bff34473c82f7a2a179dd49d850bd366b
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-11 19:56:04 +0800

    1072 added

---
+++ Algorithms/1072.flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.go
@@ -0,0 +1,6 @@
+package problem1072
+
+func maxEqualRowsAfterFlips(matrix [][]int) int {
+
+	return 0
+}
---
+++ Algorithms/1072.flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows_test.go
@@ -0,0 +1,47 @@
+package problem1072
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	matrix [][]int
+	ans    int
+}{
+
+	{
+		[][]int{{0, 1}, {1, 1}},
+		1,
+	},
+
+	{
+		[][]int{{0, 1}, {1, 0}},
+		2,
+	},
+
+	{
+		[][]int{{0, 0, 0}, {0, 0, 1}, {1, 1, 0}},
+		2,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_maxEqualRowsAfterFlips(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		ast.Equal(tc.ans, maxEqualRowsAfterFlips(tc.matrix), "输入:%v", tc)
+	}
+}
+
+func Benchmark_maxEqualRowsAfterFlips(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			maxEqualRowsAfterFlips(tc.matrix)
+		}
+	}
+}
