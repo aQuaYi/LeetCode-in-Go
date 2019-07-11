87af2466992d6ceb0629fe424eecaa41b4ac1a79
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-11 20:30:56 +0800

    1072 accepted. 200 ms, faster than 52.83%

--- Algorithms/1072.flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.go
+++ Algorithms/1072.flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.go
@@ -1,6 +1,37 @@
 package problem1072
 
-func maxEqualRowsAfterFlips(matrix [][]int) int {
+import "strings"
 
-	return 0
+func maxEqualRowsAfterFlips(matrix [][]int) int {
+	count := make(map[string]int, 300)
+	xor := make(map[string]string, 300)
+	for _, m := range matrix {
+		a, b := convert(m)
+		count[a]++
+		xor[a] = b
+	}
+
+	res := 0
+
+	for a, c := range count {
+		res = max(res, c+count[xor[a]])
+	}
+
+	return res
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func convert(nums []int) (string, string) {
+	var x, xor strings.Builder
+	for _, n := range nums {
+		x.WriteByte(byte(n + '0'))
+		xor.WriteByte(byte(1 - n + '0'))
+	}
+	return x.String(), xor.String()
 }
