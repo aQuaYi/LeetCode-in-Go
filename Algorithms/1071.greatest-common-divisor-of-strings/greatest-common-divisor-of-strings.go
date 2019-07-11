e594f214d0f36f06d3207436f350d806196b3c6b
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-11 09:12:17 +0800

    1071 fixed

--- Algorithms/1071.greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.go
+++ Algorithms/1071.greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.go
@@ -3,19 +3,34 @@ package problem1071
 import "strings"
 
 func gcdOfStrings(s1, s2 string) string {
-	if len(s1) > len(s2) {
-		return gcd(s1, s2)
+	l1, l2 := len(s1), len(s2)
+	d := gcd(max(l1, l2), min(l1, l2))
+	p := s2[:d]
+	if s1 == strings.Repeat(p, l1/d) &&
+		s2 == strings.Repeat(p, l2/d) {
+		return p
 	}
-	return gcd(s2, s1)
+	return ""
 }
 
-func gcd(s1, s2 string) string {
-	l1, l2 := len(s1), len(s2)
-	if l1%l2 == 0 {
-		if s1 == strings.Repeat(s2, l1/l2) {
-			return s2
-		}
-		return ""
+// a >= b
+func gcd(a, b int) int {
+	if b == 0 {
+		return a
 	}
-	return gcd(s2, s1[l1/l2*l2:])
+	return gcd(b, a%b)
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
