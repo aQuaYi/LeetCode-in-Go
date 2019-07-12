d93d753a3263607847a7aa7bc29653425d7b8fb5
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-12 17:14:37 +0800

    1078 accepted.

--- Algorithms/1078.occurrences-after-bigram/occurrences-after-bigram.go
+++ Algorithms/1078.occurrences-after-bigram/occurrences-after-bigram.go
@@ -1,6 +1,16 @@
 package problem1078
 
-func findOcurrences(text string, first string, second string) []string {
+import "strings"
 
-	return nil
+func findOcurrences(text string, first string, second string) []string {
+	words := strings.Split(text, " ")
+	n := len(words)
+	res := make([]string, 0, n)
+	for i := 0; i+2 < n; i++ {
+		if words[i] == first &&
+			words[i+1] == second {
+			res = append(res, words[i+2])
+		}
+	}
+	return res
 }
