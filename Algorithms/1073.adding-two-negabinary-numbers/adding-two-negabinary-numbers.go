7bd4c59dc38386985d44687b13e2fb4a01ec9c97
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-12 20:23:45 +0800

    1073 wrong answer

--- Algorithms/1073.adding-two-negabinary-numbers/adding-two-negabinary-numbers.go
+++ Algorithms/1073.adding-two-negabinary-numbers/adding-two-negabinary-numbers.go
@@ -1,6 +1,46 @@
 package problem1073
 
-func addNegabinary(arr1 []int, arr2 []int) []int {
+func addNegabinary(A, B []int) []int {
+	A, B = reverse(A), reverse(B)
+	if len(A) < len(B) {
+		A, B = B, A
+	}
+	res := make([]int, 0, len(A)+2)
+	carry := 0
+	n := len(B)
+	for i, a := range A {
+		res = append(res, a)
+		if i < n {
+			res[i] += B[i]
+		}
+		res[i] += carry
+		carry = 0
+		switch res[i] {
+		case -1:
+			res[i], carry = 1, 1
+		case 2:
+			res[i], carry = 0, -1
+		case 3:
+			res[i], carry = 1, -1
+		}
+	}
 
-	return nil
+	switch carry {
+	case 1:
+		res = append(res, 1)
+	case -1:
+		res = append(res, 1, 1)
+	}
+
+	return reverse(res)
+}
+
+func reverse(A []int) []int {
+	i, j := 0, len(A)-1
+	for i < j {
+		A[i], A[j] = A[j], A[i]
+		i++
+		j--
+	}
+	return A
 }
