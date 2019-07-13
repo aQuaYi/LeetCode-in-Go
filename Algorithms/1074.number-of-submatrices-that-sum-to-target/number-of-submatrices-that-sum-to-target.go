0ea28bc5c136e0871581dc90640b20c054a46e9f
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-13 23:01:51 +0800

    1074 done

--- Algorithms/1074.number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.go
+++ Algorithms/1074.number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.go
@@ -1,27 +1,25 @@
 package problem1074
 
-func numSubmatrixSumTarget(A [][]int, target int) int {
-	m, n := len(A), len(A[0])
+func numSubmatrixSumTarget(M [][]int, target int) int {
+	m, n := len(M), len(M[0])
 
-	for i := 0; i < m; i++ {
-		for j := 1; j < n; j++ {
-			A[i][j] += A[i][j-1]
+	S := [301][301]int{}
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			S[i][j] = S[i][j-1] + M[i-1][j-1]
 		}
 	}
 
 	res := 0
+
 	for y1 := 0; y1 < n; y1++ {
-		for y2 := y1; y2 < n; y2++ {
-			count := make(map[int]int, n)
-			count[0] = 1
-			cur := 0
-			for x := 0; x < m; x++ {
-				cur += A[x][y2]
-				if y1-1 >= 0 {
-					cur -= A[x][y1-1]
-				}
-				res += count[cur-target]
-				count[cur]++
+		for y2 := y1 + 1; y2 <= n; y2++ {
+			count := make(map[int]int, m)
+			sum := 0
+			for x := 0; x <= m; x++ {
+				sum += S[x][y2] - S[x][y1]
+				res += count[sum-target]
+				count[sum]++
 			}
 		}
 	}
