e3484c47ff849e25712a34dc64a7c1f8ff969976
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-13 21:47:55 +0800

    1074 accepted. 2700 ms, faster than 37.93%

--- Algorithms/1074.number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.go
+++ Algorithms/1074.number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.go
@@ -1,6 +1,31 @@
 package problem1074
 
-func numSubmatrixSumTarget(matrix [][]int, target int) int {
+func numSubmatrixSumTarget(M [][]int, target int) int {
+	m, n := len(M), len(M[0])
 
-return 0
+	sums := [301][301]int{}
+	for i := 1; i <= m; i++ {
+		t := 0
+		for j := 1; j <= n; j++ {
+			t += M[i-1][j-1]
+			sums[i][j] = t + sums[i-1][j]
+		}
+	}
+
+	res := 0
+
+	for x1 := 0; x1 < m; x1++ {
+		for x2 := x1 + 1; x2 <= m; x2++ {
+			for y1 := 0; y1 < n; y1++ {
+				for y2 := y1 + 1; y2 <= n; y2++ {
+					s := sums[x2][y2] - sums[x1][y2] - sums[x2][y1] + sums[x1][y1]
+					if s == target {
+						res++
+					}
+				}
+			}
+		}
+	}
+
+	return res
 }
