f4bfba15646046fd74c940488c519d994eb483e9
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-14 12:26:20 +0800

    1079 done

--- Algorithms/1079.letter-tile-possibilities/letter-tile-possibilities.go
+++ Algorithms/1079.letter-tile-possibilities/letter-tile-possibilities.go
@@ -1,6 +1,25 @@
 package problem1079
 
 func numTilePossibilities(tiles string) int {
+	count := [26]int{}
+	for _, t := range tiles {
+		count[t-'A']++
+	}
 
-return 0
+	var dfs func() int
+	dfs = func() int {
+		sum := 0
+		for i := 0; i < 26; i++ {
+			if count[i] == 0 {
+				continue
+			}
+			sum++
+			count[i]--
+			sum += dfs()
+			count[i]++
+		}
+		return sum
+	}
+
+	return dfs()
 }
