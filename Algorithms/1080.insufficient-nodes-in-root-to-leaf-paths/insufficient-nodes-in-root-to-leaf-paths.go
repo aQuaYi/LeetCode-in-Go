dd06f83e0b18b85bfaf82dd2183c75c8a09e3166
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-14 16:45:22 +0800

    1080 done

--- Algorithms/1080.insufficient-nodes-in-root-to-leaf-paths/insufficient-nodes-in-root-to-leaf-paths.go
+++ Algorithms/1080.insufficient-nodes-in-root-to-leaf-paths/insufficient-nodes-in-root-to-leaf-paths.go
@@ -1,8 +1,6 @@
 package problem1080
 
 import (
-	"math"
-
 	"github.com/aQuaYi/LeetCode-in-Go/kit"
 )
 
@@ -15,41 +13,20 @@ import (
 type TreeNode = kit.TreeNode
 
 func sufficientSubset(root *TreeNode, limit int) *TreeNode {
-	if dfs(root, 0, limit) < limit {
+	if root == nil {
+		return nil
+	}
+	if root.Left == nil && root.Right == nil {
+		if root.Val < limit {
+			return nil
+		}
+		return root
+	}
+	limit -= root.Val
+	root.Left = sufficientSubset(root.Left, limit)
+	root.Right = sufficientSubset(root.Right, limit)
+	if root.Left == root.Right { // both nil
 		return nil
 	}
 	return root
-}
-
-func dfs(node *TreeNode, pre, limit int) int {
-	if node.Left == nil && node.Right == nil {
-		return node.Val
-	}
-
-	pre += node.Val
-
-	l, r := math.MinInt64, math.MinInt64
-
-	if node.Left != nil {
-		l = dfs(node.Left, pre, limit)
-		if pre+l < limit {
-			node.Left = nil
-		}
-	}
-
-	if node.Right != nil {
-		r = dfs(node.Right, pre, limit)
-		if pre+r < limit {
-			node.Right = nil
-		}
-	}
-
-	return max(l, r) + node.Val
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
 }
