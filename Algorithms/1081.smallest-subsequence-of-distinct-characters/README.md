8cb45ddcb020895d7db80e650c88addc5c89c900
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-14 17:07:28 +0800

    1081 added

---
+++ Algorithms/1081.smallest-subsequence-of-distinct-characters/README.md
@@ -0,0 +1,36 @@
+# [1081. Smallest Subsequence of Distinct Characters](https://leetcode.com/problems/smallest-subsequence-of-distinct-characters/)
+
+Return the lexicographically smallest subsequence of text that contains all the distinct characters of text exactly once.
+
+Example 1:
+
+```text
+Input: "cdadabcc"
+Output: "adbc"
+```
+
+Example 2:
+
+```text
+Input: "abcd"
+Output: "abcd"
+```
+
+Example 3:
+
+```text
+Input: "ecbacba"
+Output: "eacb"
+```
+
+Example 4:
+
+```text
+Input: "leetcode"
+Output: "letcod"
+```
+
+Note:
+
+1. `1 <= text.length <= 1000`
+1. `text consists of lowercase English letters.`
---
+++ Algorithms/1081.smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters_test.go
@@ -0,0 +1,52 @@
+package problem1081
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	text string
+	ans  string
+}{
+
+	{
+		"cdadabcc",
+		"adbc",
+	},
+
+	{
+		"abcd",
+		"abcd",
+	},
+
+	{
+		"ecbacba",
+		"eacb",
+	},
+
+	{
+		"leetcode",
+		"letcod",
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_smallestSubsequence(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		ast.Equal(tc.ans, smallestSubsequence(tc.text), "输入:%v", tc)
+	}
+}
+
+func Benchmark_smallestSubsequence(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			smallestSubsequence(tc.text)
+		}
+	}
+}
