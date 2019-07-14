dc3fd4e898653c5db045c4dc4d240c27bc50497c
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-14 11:26:40 +0800

    1108 done

--- Algorithms/1108.defanging-an-ip-address/defanging-an-ip-address_test.go
+++ Algorithms/1108.defanging-an-ip-address/defanging-an-ip-address_test.go
@@ -6,44 +6,37 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var tcs = []struct{
-	addr string
-	ans string
+// tcs is testcase slice
+var tcs = []struct {
+	address string
+	ans     string
 }{
-	{"1.1.1.1", "1[.]1[.]1[.]1"},
-	{"255.100.50.0", "255[.]100[.]50[.]0"},
+
+	{
+		"1.1.1.1",
+		"1[.]1[.]1[.]1",
+	},
+
+	{
+		"255.100.50.0",
+		"255[.]100[.]50[.]0",
+	},
+
+	// 可以有多个 testcase
 }
 
 func Test_defangIPaddr(t *testing.T) {
 	ast := assert.New(t)
 
 	for _, tc := range tcs {
-		ast.Equal(tc.ans, defangIPaddr(tc.addr), "输入：%v", tc)
+		ast.Equal(tc.ans, defangIPaddr(tc.address), "输入:%v", tc)
 	}
 }
 
-// Benchmark_defangIPaddr-8   	 5000000	       235 ns/op
 func Benchmark_defangIPaddr(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		for _, tc := range tcs {
-			defangIPaddr(tc.addr)
-		}
-	}
-}
-
-func Test_defangIPaddr2(t *testing.T) {
-	ast := assert.New(t)
-
-	for _, tc := range tcs {
-		ast.Equal(tc.ans, defangIPaddr2(tc.addr), "输入：%v", tc)
-	}
-}
-
-// Benchmark_defangIPaddr2-8   	 2000000	       709 ns/op
-func Benchmark_defangIPaddr2(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		for _, tc := range tcs {
-			defangIPaddr2(tc.addr)
+			defangIPaddr(tc.address)
 		}
 	}
 }
