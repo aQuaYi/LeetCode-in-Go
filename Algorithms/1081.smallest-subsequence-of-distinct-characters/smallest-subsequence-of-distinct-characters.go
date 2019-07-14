af1abe71d3c7baa6e08b91264fe892860b1fbfc6
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-14 17:31:07 +0800

    1081 wrong answer

--- Algorithms/1081.smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.go
+++ Algorithms/1081.smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.go
@@ -1,6 +1,45 @@
 package problem1081
 
-func smallestSubsequence(text string) string {
+import (
+	"sort"
+	"strings"
+)
 
-	return ""
+func smallestSubsequence(text string) string {
+	rec := make([][]int, 26)
+	for i, b := range text {
+		c := b - 'a'
+		rec[c] = append(rec[c], i)
+	}
+
+	letters := make([]*letter, 0, 26)
+	for i := 0; i < 26; i++ {
+		if len(rec[i]) == 0 {
+			continue
+		}
+		letters = append(letters, &letter{
+			char:   byte(i + 'a'),
+			indexs: rec[i],
+		})
+	}
+
+	sort.Slice(letters, func(i int, j int) bool {
+		return isLess(letters[i], letters[j])
+	})
+
+	var sb strings.Builder
+	for _, l := range letters {
+		sb.WriteByte(l.char)
+	}
+
+	return sb.String()
+}
+
+type letter struct {
+	char   byte
+	indexs []int
+}
+
+func isLess(a, b *letter) bool {
+	return a.char < b.char && a.indexs[0] < b.indexs[len(b.indexs)-1]
 }
