4c1ba0b77da04a877b1b40b727fb3e061514fbd5
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-15 20:00:15 +0800

    1089 wrong answer

--- Algorithms/1089.duplicate-zeros/duplicate-zeros.go
+++ Algorithms/1089.duplicate-zeros/duplicate-zeros.go
@@ -1,4 +1,27 @@
 package problem1089
 
-func duplicateZeros(arr []int) {
+func duplicateZeros(A []int) {
+	n := len(A)
+	//
+	i, count := 0, 0
+	for i+count < n {
+		if A[i] == 0 {
+			count++
+		}
+		i++
+	}
+	i--
+	//
+	j := n - 1
+	for count > 0 {
+		if A[i] == 0 {
+			A[j-1], A[j] = 0, 0
+			j -= 2
+			count--
+		} else {
+			A[j] = A[i]
+			j--
+		}
+		i--
+	}
 }
