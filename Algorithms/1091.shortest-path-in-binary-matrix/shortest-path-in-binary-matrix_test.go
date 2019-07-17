8d7758787029ca3457a8b4f82a43e6c8fb2335d1
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-17 21:49:05 +0800

    1091 added

---
+++ Algorithms/1091.shortest-path-in-binary-matrix/shortest-path-in-binary-matrix_test.go
@@ -0,0 +1,42 @@
+package problem1091
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	grid [][]int
+	ans  int
+}{
+
+	{
+		[][]int{{0, 1}, {1, 0}},
+		2,
+	},
+
+	{
+		[][]int{{0, 0, 0}, {1, 1, 0}, {1, 1, 0}},
+		4,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_shortestPathBinaryMatrix(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		ast.Equal(tc.ans, shortestPathBinaryMatrix(tc.grid), "输入:%v", tc)
+	}
+}
+
+func Benchmark_shortestPathBinaryMatrix(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			shortestPathBinaryMatrix(tc.grid)
+		}
+	}
+}
