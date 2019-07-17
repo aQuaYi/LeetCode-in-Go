5767c20404ae7c9af59175a0587bb91ce3fe5f40
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-17 22:11:17 +0800

    1091 done

--- Algorithms/1091.shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.go
+++ Algorithms/1091.shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.go
@@ -1,6 +1,41 @@
 package problem1091
 
-func shortestPathBinaryMatrix(grid [][]int) int {
+var dx = []int{-1, -1, -1, 0, 1, 1, 1, 0}
+var dy = []int{-1, 0, 1, 1, 1, 0, -1, -1}
 
-return 0
+func shortestPathBinaryMatrix(grid [][]int) int {
+	if grid[0][0] == 1 {
+		return -1
+	}
+
+	N := len(grid)
+
+	cells := make([][2]int, 1, N*N)
+	grid[0][0] = 1
+	length := 1
+
+	for len(cells) > 0 {
+		size := len(cells)
+		for s := 0; s < size; s++ {
+			c := cells[s]
+			x, y := c[0], c[1]
+			if x == N-1 && y == N-1 {
+				return length
+			}
+			for k := 0; k < 8; k++ {
+				i, j := x+dx[k], y+dy[k]
+				if i < 0 || N <= i ||
+					j < 0 || N <= j ||
+					grid[i][j] == 1 {
+					continue
+				}
+				cells = append(cells, [2]int{i, j})
+				grid[i][j] = 1
+			}
+		}
+		length++
+		cells = cells[size:]
+	}
+
+	return -1
 }
