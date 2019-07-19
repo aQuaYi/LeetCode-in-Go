153abfdf408726f942588bdb52625e450138a0c9
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-19 20:24:00 +0800

    1092 added

---
+++ Algorithms/1092.shortest-common-supersequence/shortest-common-supersequence_test.go
@@ -0,0 +1,45 @@
+package problem1092
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	str1 string
+	str2 string
+	ans  string
+}{
+
+	{
+		"abaa",
+		"aaaa",
+		"aabaa",
+	},
+
+	{
+		"abac",
+		"cab",
+		"cabac",
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_shortestCommonSupersequence(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		ast.Equal(tc.ans, shortestCommonSupersequence(tc.str1, tc.str2), "输入:%v", tc)
+	}
+}
+
+func Benchmark_shortestCommonSupersequence(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			shortestCommonSupersequence(tc.str1, tc.str2)
+		}
+	}
+}
