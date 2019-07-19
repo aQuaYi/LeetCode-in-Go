9b16293fb7270f7396d6fa4cfff345739bc3cf84
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-19 23:00:43 +0800

    1092 accepted

--- Algorithms/1092.shortest-common-supersequence/shortest-common-supersequence.go
+++ Algorithms/1092.shortest-common-supersequence/shortest-common-supersequence.go
@@ -1,73 +1,64 @@
 package problem1092
 
-func shortestCommonSupersequence(A string, B string) string {
+import "strings"
+
+func shortestCommonSupersequence(A, B string) string {
 	m, n := len(A), len(B)
+	// 解题思路，
+	//   先求出 A 和 B 的 LCS，
+	//   然后，在 LCS 上缺少的字母
+	// 利用 dp 求解 LCS Z ,
+	// dp[i][j]=k 表示 A[:i] 与 B[:j] 的 LCS 的长度为 k
+	// 在递归过程中，会出现三种情况:
+	//   1. A[i]=B[j]， 则 dp[i][j]= dp[i-1][j-1]+1
+	//   2. A[i]!=B[j] 且 Z[k]!= A[i]，则
+	//   3.
 
 	dp := [1001][1001]int{}
+	b := [1001][1001]int{}
 
-	if A[0] == B[0] {
-		dp[0][0] = 1
-	} else {
-		dp[0][0] = 2
-	}
-
-	for i := 0; i < m; i++ {
-		for j := 0; j < n; j++ {
-			cur := i + j + 2
-			if i > 0 && cur > dp[i-1][j]+1 {
-				cur = dp[i-1][j] + 1
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if A[i-1] == B[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+				b[i][j] = 1
+			} else if dp[i-1][j] >= dp[i][j-1] {
+				dp[i][j] = dp[i-1][j]
+				b[i][j] = 2
+			} else {
+				dp[i][j] = dp[i][j-1]
+				b[i][j] = 3
 			}
-			if j > 0 && cur > dp[i][j-1]+1 {
-				cur = dp[i][j-1] + 1
-			}
-
-			if A[i] == B[j] {
-				if i > 0 && j > 0 && cur > dp[i-1][j-1]+1 {
-					cur = dp[i-1][j-1] + 1
-				} else if i == 0 {
-					cur = 1 + j
-				} else if j == 0 {
-					cur = 1 + i
-				}
-			}
-
-			dp[i][j] = cur
 		}
 	}
 
-	m, n = m-1, n-1
-	ans := make([]byte, 0, dp[m][n])
-	for m >= 0 && n >= 0 {
-		if m > 0 && dp[m][n] == dp[m-1][n]+1 {
-			ans = append(ans, A[m])
-			m--
-		} else if n > 0 && dp[m][n] == dp[m][n-1]+1 {
-			ans = append(ans, B[n])
-			n--
-		} else if m > 0 && n > 0 && dp[m][n] == dp[m-1][n-1]+1 {
-			ans = append(ans, A[m])
-			m--
-			n--
-		} else if m == 0 && n+2 == dp[m][n] {
-			ans = append(ans, A[m])
-			m--
-		} else if n == 0 && m+2 == dp[m][n] {
-			ans = append(ans, B[n])
-			n--
-		} else if m+n+1 == dp[m][n] {
-			ans = append(ans, A[m])
-			m--
-			n--
+	var sb strings.Builder
+	var super func(int, int)
+	super = func(i, j int) {
+		if i == 0 {
+			sb.WriteString(B[:j])
+			return
+		}
+
+		if j == 0 {
+			sb.WriteString(A[:i])
+			return
+		}
+
+		switch b[i][j] {
+		case 1:
+			super(i-1, j-1)
+			sb.WriteByte(A[i-1])
+		case 2:
+			super(i-1, j)
+			sb.WriteByte(A[i-1])
+		case 3:
+			super(i, j-1)
+			sb.WriteByte(B[j-1])
 		}
 	}
-	for ; m >= 0; m-- {
-		ans = append(ans, A[m])
-	}
-	for ; n >= 0; n-- {
-		ans = append(ans, B[n])
-	}
-	for i, j := 0, len(ans)-1; i < j; i, j = i+1, j-1 {
-		ans[i], ans[j] = ans[j], ans[i]
-	}
-	return string(ans)
+
+	super(m, n)
+
+	return sb.String()
 }
