6f5e57ef40a4f9ad9d40b8675624d5b8fd89924f
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-20 11:50:29 +0800

    1094 done

--- Algorithms/1094.car-pooling/car-pooling.go
+++ Algorithms/1094.car-pooling/car-pooling.go
@@ -1,6 +1,19 @@
 package problem1094
 
 func carPooling(trips [][]int, capacity int) bool {
+	location := [1001]int{}
+	for _, t := range trips {
+		num, start, end := t[0], t[1], t[2]
+		location[start] += num
+		location[end] -= num
+	}
 
-	return false
+	p := 0
+	for _, c := range location {
+		p += c
+		if p > capacity {
+			return false
+		}
+	}
+	return true
 }
