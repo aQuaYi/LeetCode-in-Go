58baa8e596df81a0fd446c21943b355e0ec1bb8f
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-21 12:57:16 +0800

    1095 wrong answer

--- Algorithms/1095.find-in-mountain-array/find-in-mountain-array.go
+++ Algorithms/1095.find-in-mountain-array/find-in-mountain-array.go
@@ -9,9 +9,68 @@ package problem1095
  * func (this *MountainArray) get(index int) int {}
  * func (this *MountainArray) length() int {}
  */
-func findInMountainArray(target int, mountainArr *MountainArray) int {
+func findInMountainArray(target int, ma *MountainArray) int {
+	n := ma.length()
+	lo, hi := 1, n-2
+	for lo < hi {
+		mid := (lo + hi - 1) / 2
+		if ma.get(mid-1) < ma.get(mid) {
+			lo = mid + 1
+		} else {
+			hi = mid - 1
+		}
+	}
+	high := lo - 1
+	mh := ma.get(high)
 
-	return 0
+	if mh < target {
+		return -1
+	}
+
+	if mh == target {
+		return high
+	}
+
+	m0 := ma.get(0)
+	if m0 == target {
+		return 0
+	}
+
+	if m0 < target {
+		lo, hi := 1, high-1
+		for lo < hi {
+			mid := (lo + hi) / 2
+			mm := ma.get(mid)
+			if mm == target {
+				return mid
+			} else if mm < target {
+				lo = mid + 1
+			} else {
+				hi = mid - 1
+			}
+		}
+		return -1
+	}
+
+	me := ma.get(n - 1)
+	if me == target {
+		return n - 1
+	}
+
+	lo, hi = high+1, n-2
+	for lo < hi {
+		mid := (lo + hi) / 2
+		mm := ma.get(mid)
+		if mm == target {
+			return mid
+		} else if mm > target {
+			lo = mid + 1
+		} else {
+			hi = mid - 1
+		}
+	}
+	return -1
+
 }
 
 // MountainArray is required
