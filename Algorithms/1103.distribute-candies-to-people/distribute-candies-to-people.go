8852746d67ab706147b067a246f7c4ad1c993b2b
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-22 17:27:23 +0800

    1103 done

--- Algorithms/1103.distribute-candies-to-people/distribute-candies-to-people.go
+++ Algorithms/1103.distribute-candies-to-people/distribute-candies-to-people.go
@@ -1,6 +1,27 @@
 package problem1103
 
-func distributeCandies(candies int, num int) []int {
+import "math"
 
-	return nil
+func distributeCandies(candies int, num int) []int {
+	res := make([]int, num)
+	l := length(candies)
+	for i := 0; i < num && i < l; i++ {
+		res[i] = candy(i, num, l)
+	}
+	r := candies - l*(l+1)/2
+	res[l%num] += r
+	return res
+}
+
+func length(candies int) int {
+	delta := float64(1 + 8*candies)
+	l := int((math.Sqrt(delta) - 1) / 2)
+	return l
+}
+
+func candy(i, d, l int) int {
+	a1 := i + 1
+	n := (l-a1)/d + 1
+	an := a1 + (n-1)*d
+	return n * (a1 + an) / 2
 }
