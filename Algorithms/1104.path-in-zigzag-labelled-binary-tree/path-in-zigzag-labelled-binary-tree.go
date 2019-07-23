dfc133d8f425de0810d9937a4574d83e872f44e9
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-23 20:41:28 +0800

    1104 add commit

--- Algorithms/1104.path-in-zigzag-labelled-binary-tree/path-in-zigzag-labelled-binary-tree.go
+++ Algorithms/1104.path-in-zigzag-labelled-binary-tree/path-in-zigzag-labelled-binary-tree.go
@@ -5,6 +5,7 @@ import "math"
 func pathInZigZagTree(label int) []int {
 	index := math.Floor(math.Log2(float64(label)))
 	t := int(math.Pow(2, index))
+	// sum is max+min in the above level
 	i, sum := int(index), t+t/2-1
 	res := make([]int, i+1)
 	for i >= 0 {
