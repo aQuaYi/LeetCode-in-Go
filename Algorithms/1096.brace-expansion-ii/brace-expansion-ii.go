f29b16a9768a81edc3a849554ff175d873a39642
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-25 00:31:10 +0800

    1096 accepted. 12ms

--- Algorithms/1096.brace-expansion-ii/brace-expansion-ii.go
+++ Algorithms/1096.brace-expansion-ii/brace-expansion-ii.go
@@ -1,6 +1,124 @@
 package problem1096
 
-func braceExpansionII(expression string) []string {
+import (
+	"sort"
+	"strings"
+)
 
-	return nil
+var pres = []string{"a{", "b{", "c{", "d{", "e{", "f{", "g{", "h{", "i{", "j{", "k{", "l{", "m{", "n{", "o{", "p{", "q{", "r{", "s{", "t{", "u{", "v{", "w{", "x{", "y{", "z{", "}{", "}a", "}b", "}c", "}d", "}e", "}f", "}g", "}h", "}i", "}j", "}k", "}l", "}m", "}n", "}o", "}p", "}q", "}r", "}s", "}t", "}u", "}v", "}w", "}x", "}y", "}z"}
+var nows = []string{"a*{", "b*{", "c*{", "d*{", "e*{", "f*{", "g*{", "h*{", "i*{", "j*{", "k*{", "l*{", "m*{", "n*{", "o*{", "p*{", "q*{", "r*{", "s*{", "t*{", "u*{", "v*{", "w*{", "x*{", "y*{", "z*{", "}*{", "}*a", "}*b", "}*c", "}*d", "}*e", "}*f", "}*g", "}*h", "}*i", "}*j", "}*k", "}*l", "}*m", "}*n", "}*o", "}*p", "}*q", "}*r", "}*s", "}*t", "}*u", "}*v", "}*w", "}*x", "}*y", "}*z"}
+
+type optFn func([]string, []string) []string
+
+func braceExpansionII(exp string) []string {
+	exp = outBrace(exp)
+
+	for i, p := range pres {
+		exp = strings.Replace(exp, p, nows[i], -1)
+	}
+
+	return add(exp)
+}
+
+func add(exp string) []string {
+	exp = outBrace(exp)
+
+	if !strings.ContainsRune(exp, '{') {
+		return strings.Split(exp, ",")
+	}
+
+	count := 0
+	bytes := []byte(exp)
+	for i, b := range bytes {
+		switch b {
+		case '{':
+			count++
+		case '}':
+			count--
+		case ',':
+			if count == 0 {
+				bytes[i] = '@'
+			}
+		}
+	}
+	exp = string(bytes)
+	strs := strings.Split(exp, "@")
+	res := []string{}
+	for _, s := range strs {
+		res = merge(res, mul(s))
+	}
+	return unique(res)
+}
+
+func mul(exp string) []string {
+	exp = outBrace(exp)
+	count := 0
+	bytes := []byte(exp)
+	for i, b := range bytes {
+		switch b {
+		case '{':
+			count++
+		case '}':
+			count--
+		case '*':
+			if count == 0 {
+				bytes[i] = '@'
+			}
+		}
+	}
+	exp = string(bytes)
+	strs := strings.Split(exp, "@")
+	res := []string{""}
+	for _, s := range strs {
+		res = multiply(res, add(s))
+	}
+	return unique(res)
+}
+
+func outBrace(exp string) string {
+	n := len(exp)
+	if exp[0] != '{' ||
+		exp[n-1] != '}' {
+		return exp
+	}
+	count := 1
+	i := 1
+	for ; i < n && count > 0; i++ {
+		if exp[i] == '{' {
+			count++
+		} else if exp[i] == '}' {
+			count--
+		}
+	}
+	if i == n {
+		return exp[1 : n-1]
+	}
+	return exp
+}
+
+func merge(A, B []string) []string {
+	return append(A, B...)
+}
+
+func multiply(A, B []string) []string {
+	res := make([]string, 0, len(A)*len(B))
+	for _, a := range A {
+		for _, b := range B {
+			res = append(res, a+b)
+		}
+	}
+	return res
+}
+
+func unique(A []string) []string {
+	sort.Strings(A)
+	i, j, n := 0, 0, len(A)
+	for j < n {
+		if A[i] != A[j] {
+			i++
+			A[i] = A[j]
+		}
+		j++
+	}
+	return A[:i+1]
 }
