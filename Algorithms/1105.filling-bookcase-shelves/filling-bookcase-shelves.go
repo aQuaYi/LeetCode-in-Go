5ad2c1d1bc2e8fda3ee417409c81f4da4f4513fe
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-26 23:07:52 +0800

    1105 done

--- Algorithms/1105.filling-bookcase-shelves/filling-bookcase-shelves.go
+++ Algorithms/1105.filling-bookcase-shelves/filling-bookcase-shelves.go
@@ -1,6 +1,37 @@
 package problem1105
 
-func minHeightShelves(books [][]int, shelfWidth int) int {
+func minHeightShelves(books [][]int, width int) int {
+	m := len(books)
+	dp := make([]int, m+1)
 
-	return 0
+	for i := 1; i <= m; i++ {
+		dp[i] = 1000000
+		k := 1
+		w, h := books[i-k][0], books[i-k][1]
+		for w <= width {
+			dp[i] = min(dp[i], dp[i-k]+h)
+			k++
+			if i-k < 0 {
+				break
+			}
+			w += books[i-k][0]
+			h = max(h, books[i-k][1])
+		}
+	}
+
+	return dp[m]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
