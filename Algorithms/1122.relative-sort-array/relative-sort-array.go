12ae2d2ef1a7c0ff21536523f5588274be83b08b
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-27 11:54:03 +0800

    1122 done

--- Algorithms/1122.relative-sort-array/relative-sort-array.go
+++ Algorithms/1122.relative-sort-array/relative-sort-array.go
@@ -1,5 +1,24 @@
 package problem1122
 
-func relativeSortArray(A1, A2 []int) []int {
-	return nil
+func relativeSortArray(A, B []int) []int {
+	count := [1001]int{}
+	for _, a := range A {
+		count[a]++
+	}
+
+	res := make([]int, 0, len(A))
+	for _, b := range B {
+		for count[b] > 0 {
+			res = append(res, b)
+			count[b]--
+		}
+	}
+	for i := 0; i < 1001; i++ {
+		for count[i] > 0 {
+			res = append(res, i)
+			count[i]--
+		}
+	}
+
+	return res
 }
