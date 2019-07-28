3204c901ee9ef4bb2446c483c404f46e5c6b496d
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-28 19:34:40 +0800

    1109 done

--- Algorithms/1109.corporate-flight-bookings/corporate-flight-bookings.go
+++ Algorithms/1109.corporate-flight-bookings/corporate-flight-bookings.go
@@ -1,13 +1,16 @@
 package problem1109
 
 func corpFlightBookings(bookings [][]int, n int) []int {
-	res := make([]int, n)
+	res := make([]int, n+1)
 	for _, b := range bookings {
 		i, j, k := b[0], b[1], b[2]
-		for m := i - 1; m < j; m++ {
-			res[m] += k
-		}
+		res[i-1] += k
+		res[j] -= k
 	}
 
-	return res
+	for i := 1; i < n; i++ {
+		res[i] += res[i-1]
+	}
+
+	return res[:n]
 }
