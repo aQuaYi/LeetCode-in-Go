2a4c1fa2179b00d73cdaf96d7ae871a785b90208
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-28 19:21:18 +0800

    1109 1864 ms, faster than 34.18%

--- Algorithms/1109.corporate-flight-bookings/corporate-flight-bookings.go
+++ Algorithms/1109.corporate-flight-bookings/corporate-flight-bookings.go
@@ -1,6 +1,13 @@
 package problem1109
 
 func corpFlightBookings(bookings [][]int, n int) []int {
+	res := make([]int, n)
+	for _, b := range bookings {
+		i, j, k := b[0], b[1], b[2]
+		for m := i - 1; m < j; m++ {
+			res[m] += k
+		}
+	}
 
-	return nil
+	return res
 }
