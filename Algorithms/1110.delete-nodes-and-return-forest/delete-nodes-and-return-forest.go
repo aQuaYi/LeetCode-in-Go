78c8ecdd16acb70b3e156337b99ee3ee6adea7d6
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-29 20:14:23 +0800

    1110 added

---
+++ Algorithms/1110.delete-nodes-and-return-forest/delete-nodes-and-return-forest.go
@@ -0,0 +1,11 @@
+package problem1110
+
+import "github.com/aQuaYi/LeetCode-in-Go/kit"
+
+// TreeNode is pre-defined
+type TreeNode = kit.TreeNode
+
+func delNodes(root *TreeNode, delete []int) []*TreeNode {
+
+	return nil
+}
