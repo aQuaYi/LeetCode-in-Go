c1f0b80a9e48ec05a817a17d3fc1d93138265862
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-29 20:48:59 +0800

    1072 coverage 100%

--- Algorithms/1072.flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows_test.go
+++ Algorithms/1072.flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows_test.go
@@ -62,3 +62,39 @@ func Benchmark_maxEqualRowsAfterFlips(b *testing.B) {
 		}
 	}
 }
+
+func Test_max(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"a is bigger",
+			args{
+				a: 1,
+				b: 0,
+			},
+			1,
+		},
+		{
+			"b is bigger",
+			args{
+				a: 1,
+				b: 2,
+			},
+			2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := max(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("max() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
