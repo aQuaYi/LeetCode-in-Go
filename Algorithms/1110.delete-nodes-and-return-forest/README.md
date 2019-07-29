4185ccb68812cb7b5428a0e466e5abea3cf090b0
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-29 20:14:23 +0800

    1110 added

---
+++ Algorithms/1110.delete-nodes-and-return-forest/README.md
@@ -0,0 +1,23 @@
+# [1110. Delete Nodes And Return Forest](https://leetcode.com/problems/delete-nodes-and-return-forest/)
+
+Given the root of a binary tree, each node in the tree has a distinct value.
+
+After deleting all nodes with a value in to_delete, we are left with a forest (a disjoint union of trees).
+
+Return the roots of the trees in the remaining forest.  You may return the result in any order.
+
+Example 1:
+
+![1](1.png)
+
+```text
+Input: root = [1,2,3,4,5,6,7], to_delete = [3,5]
+Output: [[1,2,null,4],[6],[7]]
+```
+
+Constraints:
+
+- The number of nodes in the given tree is at most 1000.
+- Each node has a distinct value between 1 and 1000.
+- to_delete.length <= 1000
+- to_delete contains distinct values between 1 and 1000.
---
+++ Algorithms/1110.delete-nodes-and-return-forest/delete-nodes-and-return-forest_test.go
@@ -0,0 +1,47 @@
+package problem1110
+
+import (
+	"testing"
+
+	"github.com/aQuaYi/LeetCode-in-Go/kit"
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	root   []int
+	delete []int
+	ans    [][]int
+}{
+
+	{
+		[]int{1, 2, 3, 4, 5, 6, 7},
+		[]int{3, 5},
+		[][]int{{1, 2, kit.NULL, 4}, {6}, {7}},
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_delNodes(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		root := kit.Ints2TreeNode(tc.root)
+		tmps := delNodes(root, tc.delete)
+		ans := make([][]int, len(tmps))
+		for i, t := range tmps {
+			ans[i] = kit.Tree2ints(t)
+		}
+		ast.Equal(tc.ans, ans, "输入:%v", tc)
+	}
+}
+
+func Benchmark_delNodes(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			root := kit.Ints2TreeNode(tc.root)
+			delNodes(root, tc.delete)
+		}
+	}
+}
