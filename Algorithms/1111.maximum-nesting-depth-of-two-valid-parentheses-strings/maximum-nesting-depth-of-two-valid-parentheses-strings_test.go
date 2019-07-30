edc18e9bc416ffd67caa9ab2073dc9a04991dd34
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-30 16:51:08 +0800

    1111 added

---
+++ Algorithms/1111.maximum-nesting-depth-of-two-valid-parentheses-strings/maximum-nesting-depth-of-two-valid-parentheses-strings_test.go
@@ -0,0 +1,42 @@
+package problem1111
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	seq string
+	ans []int
+}{
+
+	{
+		"(()())",
+		[]int{0, 1, 1, 1, 1, 0},
+	},
+
+	{
+		"()(())()",
+		[]int{0, 0, 0, 1, 1, 0, 1, 1},
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_maxDepthAfterSplit(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		ast.Equal(tc.ans, maxDepthAfterSplit(tc.seq), "输入:%v", tc)
+	}
+}
+
+func Benchmark_maxDepthAfterSplit(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			maxDepthAfterSplit(tc.seq)
+		}
+	}
+}
