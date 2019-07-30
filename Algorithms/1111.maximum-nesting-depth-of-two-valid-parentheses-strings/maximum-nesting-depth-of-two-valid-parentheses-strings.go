8d2949e586bb27db8cf8064c7c7fadecd5020ec4
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-30 17:01:55 +0800

    1111 accepted. 36 ms, faster than 36.36%

--- Algorithms/1111.maximum-nesting-depth-of-two-valid-parentheses-strings/maximum-nesting-depth-of-two-valid-parentheses-strings.go
+++ Algorithms/1111.maximum-nesting-depth-of-two-valid-parentheses-strings/maximum-nesting-depth-of-two-valid-parentheses-strings.go
@@ -1,6 +1,18 @@
 package problem1111
 
 func maxDepthAfterSplit(seq string) []int {
+	res := make([]int, len(seq))
+	stack, top := make([]int, len(seq)), -1
+	for i, r := range seq {
+		if r == ')' {
+			res[i] = res[stack[top]]
+			top--
+			continue
+		}
+		top++
+		stack[top] = i
+		res[i] = top % 2
+	}
 
-return nil
+	return res
 }
