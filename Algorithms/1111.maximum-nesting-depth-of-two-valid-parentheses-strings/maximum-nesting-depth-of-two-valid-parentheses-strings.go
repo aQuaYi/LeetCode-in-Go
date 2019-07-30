353992250a67ca6cc506a0ef6380c30295b2628f
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-30 17:09:26 +0800

    1111 done

--- Algorithms/1111.maximum-nesting-depth-of-two-valid-parentheses-strings/maximum-nesting-depth-of-two-valid-parentheses-strings.go
+++ Algorithms/1111.maximum-nesting-depth-of-two-valid-parentheses-strings/maximum-nesting-depth-of-two-valid-parentheses-strings.go
@@ -4,15 +4,13 @@ func maxDepthAfterSplit(seq string) []int {
 	res := make([]int, len(seq))
 	stack, top := make([]int, len(seq)), -1
 	for i, r := range seq {
-		if r == ')' {
+		if r == '(' {
+			top++
+			stack[top], res[i] = i, top%2
+		} else {
 			res[i] = res[stack[top]]
 			top--
-			continue
 		}
-		top++
-		stack[top] = i
-		res[i] = top % 2
 	}
-
 	return res
 }
