55e2727fc2826cb08b0d968e31a6f1b985b6a2e2
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-31 16:35:49 +0800

    1123 accepted. 8 ms, faster than 91.30%

--- Algorithms/1123.lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.go
+++ Algorithms/1123.lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.go
@@ -11,6 +11,29 @@ import "github.com/aQuaYi/LeetCode-in-Go/kit"
 type TreeNode = kit.TreeNode
 
 func lcaDeepestLeaves(root *TreeNode) *TreeNode {
+	res, _ := dfs(root)
+	return res
+}
 
-	return nil
+func dfs(node *TreeNode) (*TreeNode, int) {
+	if node == nil {
+		return nil, 0
+	}
+
+	if node.Left == nil && node.Right == nil {
+		return node, 1
+	}
+
+	l, ld := dfs(node.Left)
+	r, rd := dfs(node.Right)
+
+	if ld == rd {
+		return node, ld + 1
+	}
+
+	if ld > rd {
+		return l, ld + 1
+	}
+
+	return r, rd + 1
 }
