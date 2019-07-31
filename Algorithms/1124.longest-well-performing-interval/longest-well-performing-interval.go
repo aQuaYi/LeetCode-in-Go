9858326ffc2e5c37db6c7fb7ef38bcf2fbf8cdec
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-31 17:26:20 +0800

    1124 doing

--- Algorithms/1124.longest-well-performing-interval/longest-well-performing-interval.go
+++ Algorithms/1124.longest-well-performing-interval/longest-well-performing-interval.go
@@ -1,6 +1,34 @@
 package problem1124
 
 func longestWPI(hours []int) int {
+	res := 0
 
-	return 0
+	rec := make(map[int]int, len(hours)+1)
+	rec[0] = -1
+	rec[-1] = -1
+
+	cur := 0
+	for i, h := range hours {
+		if h > 8 {
+			cur++
+		} else {
+			cur--
+		}
+		res = max(res, cur)
+		if j, ok := rec[cur-1]; ok {
+			res = max(res, i-j)
+		}
+		if _, ok := rec[cur]; !ok {
+			rec[cur] = i
+		}
+	}
+
+	return res
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
