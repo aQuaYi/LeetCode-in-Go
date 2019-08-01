2c991821784f8523c0e9607032d0d185dceb74f5
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-01 17:26:53 +0800

    90 add benchmark test

--- Algorithms/0090.subsets-ii/subsets-ii.go
+++ Algorithms/0090.subsets-ii/subsets-ii.go
@@ -2,27 +2,25 @@ package problem0090
 
 import "sort"
 
-func subsetsWithDup(nums []int) [][]int {
+func subsetsWithDup(A []int) [][]int {
 	res := [][]int{}
-	sort.Ints(nums)
+	sort.Ints(A)
 
 	var dfs func(int, []int)
-	dfs = func(idx int, temp []int) {
+	dfs = func(index int, temp []int) {
 		t := make([]int, len(temp))
 		copy(t, temp)
-		// 没有以上两行，答案就是错的
-		// 因为temp的底层数组在递归过程中，不停地修改
-		// 程序结束时，temp的底层数组的值，全部是 nums 的最大值。
 		res = append(res, t)
 
-		for i := idx; i < len(nums); i++ {
-			if i == idx || nums[i] != nums[i-1] {
-				dfs(i+1, append(temp, nums[i]))
+		for i := index; i < len(A); i++ {
+			if i == index ||
+				A[i] != A[i-1] { // 避免重复答案
+				dfs(i+1, append(temp, A[i]))
 			}
 		}
 	}
 
-	temp := make([]int, 0, len(nums))
+	temp := make([]int, 0, len(A))
 	dfs(0, temp)
 
 	return res
