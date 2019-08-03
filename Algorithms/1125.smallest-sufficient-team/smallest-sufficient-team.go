ba7ec1d3c69fb62744e9fc66688eaa340f806255
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-03 20:06:39 +0800

    1125  开始重构

--- Algorithms/1125.smallest-sufficient-team/smallest-sufficient-team.go
+++ Algorithms/1125.smallest-sufficient-team/smallest-sufficient-team.go
@@ -2,35 +2,45 @@ package problem1125
 
 import "math"
 
-func smallestSufficientTeam(reqs []string, peoples [][]string) []int {
-	n := len(reqs)
-	key := make(map[string]uint8, n)
-	for i, r := range reqs {
-		key[r] = uint8(i)
+var workers []int
+var skillCands [][]int
+var dp map[int][]int
+
+func smallestSufficientTeam(reqSkills []string, people [][]string) []int {
+	index := make(map[string]int)
+	for i, skill := range reqSkills {
+		index[skill] = i
 	}
-
-	dp := make(map[int][]int, int(math.Pow(2, float64(n))))
-	dp[0] = []int{}[:0:0]
-
-	for i, person := range peoples {
-		personSkills := 0
-		for _, s := range person {
-			if index, ok := key[s]; ok {
-				personSkills |= 1 << index
-			}
-		}
-		for teamSkills, team := range dp {
-			m := len(team) + 1
-			withPerson := teamSkills | personSkills
-			if withPerson == teamSkills {
-				continue
-			}
-			oldTeam, ok := dp[withPerson]
-			if !ok || len(oldTeam) > m {
-				dp[withPerson] = append(team, i)[:m:m]
+	workers = make([]int, len(people))
+	skillCands = make([][]int, len(reqSkills))
+	for i, p := range people {
+		for _, skill := range p {
+			if _, ok := index[skill]; ok {
+				workers[i] |= (1 << uint(index[skill]))
+				skillCands[index[skill]] = append(skillCands[index[skill]], i)
 			}
 		}
 	}
+	dp = map[int][]int{0: {}}
+	return dfs(1<<uint(len(reqSkills)) - 1)
+}
 
-	return dp[1<<uint(n)-1]
+func dfs(skillsWanted int) []int {
+	if v, ok := dp[skillsWanted]; ok {
+		return v
+	}
+	smallestTeamSize := 61
+	res := []int{}
+	targetSkill := int(math.Log2(float64(skillsWanted & -skillsWanted)))
+	for _, p := range skillCands[targetSkill] {
+		candSkillWanted := skillsWanted &^ workers[p]
+		pre := dfs(candSkillWanted)
+		pre = append(pre, p)
+		if len(pre) < smallestTeamSize {
+			smallestTeamSize = len(pre)
+			res = pre
+		}
+	}
+	dp[skillsWanted] = res
+	return res
 }
