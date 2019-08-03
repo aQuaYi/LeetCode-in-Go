b6daaabc422e6476e56cef7d781c8c5a8b6e7965
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-03 09:12:23 +0800

    1106 accepted.

--- Algorithms/1106.parsing-a-boolean-expression/parsing-a-boolean-expression.go
+++ Algorithms/1106.parsing-a-boolean-expression/parsing-a-boolean-expression.go
@@ -1,6 +1,67 @@
 package problem1106
 
-func parseBoolExpr(expression string) bool {
+import "strings"
 
+func parseBoolExpr(exp string) bool {
+	if exp == "t" {
+		return true
+	}
+	if exp == "f" {
+		return false
+	}
+	n := len(exp)
+	symbol, exp := exp[0], exp[2:n-1]
+	subs := split(exp)
+	switch symbol {
+	case '&':
+		return and(subs)
+	case '|':
+		return or(subs)
+	default:
+		return not(exp)
+	}
+}
+
+func split(exp string) []string {
+	count := 0
+	isUnbracketed := func() bool {
+		return count == 0
+	}
+	bytes := []byte(exp)
+	for i, b := range bytes {
+		switch b {
+		case '(':
+			count++
+		case ')':
+			count--
+		case ',':
+			if isUnbracketed() {
+				bytes[i] = '@'
+			}
+		}
+	}
+	exp = string(bytes)
+	return strings.Split(exp, "@")
+}
+
+func or(exps []string) bool {
+	for _, e := range exps {
+		if parseBoolExpr(e) {
+			return true
+		}
+	}
 	return false
+}
+
+func and(exps []string) bool {
+	for _, e := range exps {
+		if !parseBoolExpr(e) {
+			return false
+		}
+	}
+	return true
+}
+
+func not(exp string) bool {
+	return !parseBoolExpr(exp)
 }
