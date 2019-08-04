f5d5ca6a1bedc52c3412787f90b456302eb9dc53
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-04 16:56:02 +0800

    1130 done

--- Algorithms/1130.minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.go
+++ Algorithms/1130.minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.go
@@ -1,47 +1,30 @@
 package problem1130
 
+import "math"
+
+// ref: https://leetcode.com/problems/minimum-cost-tree-from-leaf-values/discuss/339959/One-Pass-O(N)-Time-and-Space
 func mctFromLeafValues(A []int) int {
 	n := len(A)
 
-	maxs := [40][40]int{}
-	var maxVal func(int, int) int
-	maxVal = func(i, j int) int {
-		if maxs[i][j] != 0 {
-			return maxs[i][j]
+	stack, top, pop := make([]int, n+1), 0, 0
+	stack[top] = math.MaxInt64
+
+	res := 0
+	for _, a := range A {
+		for stack[top] <= a {
+			pop, top = stack[top], top-1
+			res += pop * min(stack[top], a)
 		}
-		if i == j {
-			maxs[i][i] = A[i]
-			return A[i]
-		}
-		res := max(A[i], maxVal(i+1, j))
-		maxs[i][j] = res
-		return res
+		top++
+		stack[top] = a
 	}
 
-	for i := 0; i < n; i++ {
-		maxs[i][i] = A[i]
-	}
-	dp := [40][40]int{}
-	for width := 2; width <= n; width++ {
-		for i := 0; i+width-1 < n; i++ {
-			j := i + width - 1
-			left := A[i]*maxVal(i+1, j) + dp[i+1][j]
-			right := A[j]*maxVal(i, j-1) + dp[i][j-1]
-			dp[i][j] = min(left, right)
-			for k := i + 1; k < j-1; k++ {
-				dp[i][j] = min(dp[i][j], dp[i][k]+dp[k+1][j]+maxVal(i, k)*maxVal(k+1, j))
-			}
-		}
+	for top >= 2 {
+		pop, top = stack[top], top-1
+		res += pop * stack[top]
 	}
 
-	return dp[0][n-1]
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
+	return res
 }
 
 func min(a, b int) int {
