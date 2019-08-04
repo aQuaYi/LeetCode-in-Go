ce1f9e8f3cd303e09de4aaaab3904cfe66090239
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-04 09:55:49 +0800

    1129 wrong answer

--- Algorithms/1129.shortest-path-with-alternating-colors/shortest-path-with-alternating-colors.go
+++ Algorithms/1129.shortest-path-with-alternating-colors/shortest-path-with-alternating-colors.go
@@ -1,15 +1,15 @@
 package problem1129
 
 func shortestAlternatingPaths(n int, reds [][]int, blues [][]int) []int {
-	R := convert(n, reds)
-	B := convert(n, blues)
+	isRed := convert(n, reds)
+	isBlue := convert(n, blues)
 
 	res := make([]int, n)
 	for i := range res {
 		res[i] = n
 	}
 
-	bfs := func(g []int, isRed, isBlue [][]bool) {
+	bfs := func(g []int, isCur, isNext [][]bool) {
 		hasSeen := make([]bool, n)
 		count := 0
 		for len(g) > 0 {
@@ -19,25 +19,35 @@ func shortestAlternatingPaths(n int, reds [][]int, blues [][]int) []int {
 				res[i] = min(res[i], count)
 				hasSeen[i] = true
 				for j := 0; j < n; j++ {
-					if hasSeen[j] || !isRed[i][j] {
+					if hasSeen[j] || !isCur[i][j] {
 						continue
 					}
 					g = append(g, j)
 				}
 			}
 			g = g[size:]
-			isRed, isBlue = isBlue, isRed
+			isCur, isNext = isNext, isCur
+			count++
 		}
 	}
 
-	bfs([]int{0}, R, B)
-	bfs([]int{0}, B, R)
+	bfs([]int{0}, isRed, isBlue)
+	bfs([]int{0}, isBlue, isRed)
+
+	for i := range res {
+		if res[i] == n {
+			res[i] = -1
+		}
+	}
 
 	return res
 }
 
 func convert(n int, edges [][]int) [][]bool {
 	res := make([][]bool, n)
+	for i := range res {
+		res[i] = make([]bool, n)
+	}
 	for _, e := range edges {
 		i, j := e[0], e[1]
 		res[i][j] = true
