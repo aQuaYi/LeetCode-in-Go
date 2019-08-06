f2655754f36611a489615f0c76e876857ed02461
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-06 20:24:57 +0800

    1131 done

--- Algorithms/1131.maximum-of-absolute-value-expression/maximum-of-absolute-value-expression.go
+++ Algorithms/1131.maximum-of-absolute-value-expression/maximum-of-absolute-value-expression.go
@@ -1,28 +1,34 @@
 package problem1131
 
+// ref: https://leetcode.com/problems/maximum-of-absolute-value-expression/discuss/340070/topic
+
 func maxAbsValExpr(A, B []int) int {
+	Q := [2]int{-1, 1}
+
 	n := len(A)
 	res := 0
-	for i := 1; i < n; i++ {
-		for j := 0; j < i; j++ {
-			res = max(
-				res,
-				abs(A[i]-A[j])+abs(B[i]-B[j])+i-j,
-			)
+	for _, a := range Q {
+		for _, b := range Q {
+			low := a*A[0] + b*B[0]
+			for i := 1; i < n; i++ {
+				cur := a*A[i] + b*B[i] + i
+				res = max(res, cur-low)
+				low = min(low, cur)
+			}
 		}
 	}
 	return res
 }
 
-func abs(a int) int {
-	if a < 0 {
-		return -a
-	}
-	return a
-}
-
 func max(a, b int) int {
 	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
 		return a
 	}
 	return b
