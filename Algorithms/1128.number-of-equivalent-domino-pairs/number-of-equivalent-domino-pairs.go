881b78c317c54b321565a6d2cacfe3222ea6e075
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-06 17:20:21 +0800

    1128 done

--- Algorithms/1128.number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.go
+++ Algorithms/1128.number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.go
@@ -1,6 +1,20 @@
 package problem1128
 
 func numEquivDominoPairs(dominoes [][]int) int {
+	res := 0
+	count := [100]int{}
+	for _, domino := range dominoes {
+		d := mapping(domino)
+		res += count[d]
+		count[d]++
+	}
+	return res
+}
 
-	return 0
+func mapping(A []int) int {
+	a, b := A[0], A[1]
+	if a < b {
+		return a*10 + b
+	}
+	return b*10 + a
 }
