ea6a4bd93737b045f1d27072cab281d8309eb45c
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-06 19:58:09 +0800

    1131 2784 ms, faster than 12.82%

--- Algorithms/1131.maximum-of-absolute-value-expression/maximum-of-absolute-value-expression.go
+++ Algorithms/1131.maximum-of-absolute-value-expression/maximum-of-absolute-value-expression.go
@@ -1,6 +1,29 @@
 package problem1131
 
 func maxAbsValExpr(A, B []int) int {
+	n := len(A)
+	res := 0
+	for i := 1; i < n; i++ {
+		for j := 0; j < i; j++ {
+			res = max(
+				res,
+				abs(A[i]-A[j])+abs(B[i]-B[j])+i-j,
+			)
+		}
+	}
+	return res
+}
 
-	return 0
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
