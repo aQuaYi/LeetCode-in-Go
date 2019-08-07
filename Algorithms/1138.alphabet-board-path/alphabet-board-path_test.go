cd2f94f563a14832aecec9db66dc18e2eb86fb14
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-07 22:47:22 +0800

    1138 done

--- Algorithms/1138.alphabet-board-path/alphabet-board-path_test.go
+++ Algorithms/1138.alphabet-board-path/alphabet-board-path_test.go
@@ -14,7 +14,7 @@ var tcs = []struct {
 
 	{
 		"dfdsfkjdfleiwllsgoidkjflsdfjdfierwsd",
-		"RRR!LLLD!RRRU!DDD!LLLUU!D!RRRRU!LU!LLLD!RD!RRRUU!LD!LDDD!LUU!!RRD!LLUU!RRRD!LU!U!LLLDD!RRRRU!LLLL!RD!RRD!UUU!LLLD!RRRR!LU!LLLD!RRR!RU!LLDDD!D!RU!UUU!",
+		"RRR!LLLD!URRR!DDD!LLLUU!D!URRRR!LU!LLLD!DR!UURRR!LD!LDDD!LUU!!DRR!LLUU!DRRR!LU!U!LLLDD!URRRR!LLLL!DR!DRR!UUU!LLLD!RRRR!LU!LLLD!RRR!UR!LLDDD!D!UR!UUU!",
 	},
 
 	{
@@ -24,27 +24,27 @@ var tcs = []struct {
 
 	{
 		"zdz",
-		"DDDDD!URRRUUUU!LLLDDDDD!",
+		"DDDDD!UUUUURRR!LLLDDDDD!",
 	},
 
 	{
 		"zb",
-		"DDDDD!URUUUU!",
+		"DDDDD!UUUUUR!",
 	},
 
 	{
 		"lazy",
-		"RDD!LUU!DDDDD!URRRR!",
+		"DDR!LUU!DDDDD!URRRR!",
 	},
 
 	{
 		"leet",
-		"RDD!RRRUU!!DDD!",
+		"DDR!UURRR!!DDD!",
 	},
 
 	{
 		"code",
-		"RR!RRDD!LUU!R!",
+		"RR!DDRR!LUU!R!",
 	},
 
 	// 可以有多个 testcase
@@ -119,88 +119,6 @@ func Test_coordinate(t *testing.T) {
 			}
 			if gotY != tt.wantY {
 				t.Errorf("coordinate() gotY = %v, want %v", gotY, tt.wantY)
-			}
-		})
-	}
-}
-
-func Test_move(t *testing.T) {
-	type args struct {
-		x1 int
-		y1 int
-		x2 int
-		y2 int
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		{
-			name: "原地移动",
-			args: args{
-				x1: 1,
-				y1: 1,
-				x2: 1,
-				y2: 1,
-			},
-			want: "",
-		},
-		{
-			name: "a to l",
-			args: args{
-				x1: 0,
-				y1: 0,
-				x2: 2,
-				y2: 1,
-			},
-			want: "RDD",
-		},
-		{
-			name: "f to j",
-			args: args{
-				x1: 1,
-				y1: 0,
-				x2: 1,
-				y2: 4,
-			},
-			want: "RRRR",
-		},
-		{
-			name: "j to u",
-			args: args{
-				x1: 1,
-				y1: 4,
-				x2: 4,
-				y2: 0,
-			},
-			want: "LLLLDDD",
-		},
-		{
-			name: "y to z",
-			args: args{
-				x1: 4,
-				y1: 4,
-				x2: 5,
-				y2: 0,
-			},
-			want: "LLLLD",
-		},
-		{
-			name: "z to n",
-			args: args{
-				x1: 5,
-				y1: 0,
-				x2: 2,
-				y2: 3,
-			},
-			want: "URRRUU",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := move(tt.args.x1, tt.args.y1, tt.args.x2, tt.args.y2); got != tt.want {
-				t.Errorf("move() = %v, want %v", got, tt.want)
 			}
 		})
 	}
