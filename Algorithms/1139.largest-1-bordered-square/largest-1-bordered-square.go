d96acaf68add7e98b094fbdd01d23a9d2a8e8b16
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-08 16:21:25 +0800

    1139 wrong answer

--- Algorithms/1139.largest-1-bordered-square/largest-1-bordered-square.go
+++ Algorithms/1139.largest-1-bordered-square/largest-1-bordered-square.go
@@ -1,6 +1,45 @@
 package problem1139
 
 func largest1BorderedSquare(grid [][]int) int {
+	m, n := len(grid), len(grid[0])
 
-	return 0
+	isBorderOk := func(x, y, dx, dy, w int) bool {
+		xMax := x + w
+		for x < xMax {
+			if grid[x][y] == 0 {
+				return false
+			}
+			x += dx
+			y += dy
+		}
+		return true
+	}
+
+	isOk := func(x, y, w int) bool {
+		return isBorderOk(x, y, 0, 1, w) &&
+			isBorderOk(x, y, 1, 0, w) &&
+			isBorderOk(x+w-1, y, 0, 1, w) &&
+			isBorderOk(x, y+w-1, 1, 0, w)
+	}
+
+	res := 0
+	for i := 0; i+res <= m; i++ {
+		for j := 0; j+res <= n; j++ {
+			w := min(m-i, n-j)
+			for w > res {
+				if isOk(i, j, w) {
+					res = w
+				}
+			}
+		}
+	}
+
+	return res * res
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
