e810909a5823acf4fdf865e76b77b2afafb5fa67
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-08 15:59:40 +0800

    1139 added

---
+++ Algorithms/1139.largest-1-bordered-square/largest-1-bordered-square_test.go
@@ -0,0 +1,42 @@
+package problem1139
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	grid [][]int
+	ans  int
+}{
+
+	{
+		[][]int{{1, 1, 1}, {1, 0, 1}, {1, 1, 1}},
+		9,
+	},
+
+	{
+		[][]int{{1, 1, 0, 0}},
+		1,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_largest1BorderedSquare(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		ast.Equal(tc.ans, largest1BorderedSquare(tc.grid), "输入:%v", tc)
+	}
+}
+
+func Benchmark_largest1BorderedSquare(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			largest1BorderedSquare(tc.grid)
+		}
+	}
+}
