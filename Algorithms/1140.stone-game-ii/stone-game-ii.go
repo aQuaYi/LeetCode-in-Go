85b677dcaa649f24e51e2e83f5e962339ed57aca
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-08 17:14:12 +0800

    1140 Time Limit Exceed

--- Algorithms/1140.stone-game-ii/stone-game-ii.go
+++ Algorithms/1140.stone-game-ii/stone-game-ii.go
@@ -1,6 +1,33 @@
 package problem1140
 
-func stoneGameII(piles []int) int {
+func stoneGameII(A []int) int {
+	n := len(A)
+	sum := make([]int, n+1)
+	for i := 0; i < n; i++ {
+		sum[i+1] = sum[i] + A[i]
+	}
 
-	return 0
+	var cur func(i, M int) int
+	cur = func(i, M int) int {
+		if i >= n {
+			return 0
+		}
+		res := 0
+		for x := 1; x <= 2*M; x++ {
+			res = max(
+				res,
+				sum[n]-sum[i]-cur(i+x, max(M, x)),
+			)
+		}
+		return res
+	}
+
+	return cur(0, 1)
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
