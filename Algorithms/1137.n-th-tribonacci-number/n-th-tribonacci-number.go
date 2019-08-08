49692ee43f4472f61e97d2ce025a69b66e39755f
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-08 22:19:37 +0800

    1137 done

--- Algorithms/1137.n-th-tribonacci-number/n-th-tribonacci-number.go
+++ Algorithms/1137.n-th-tribonacci-number/n-th-tribonacci-number.go
@@ -1,20 +1,13 @@
 package problem1137
 
 func tribonacci(n int) int {
-	mem := make([]int, n+1)
-	mem[0] = 0
-	mem[1] = 1
-	mem[2] = 1
-
-	var t func(int) int
-	t = func(n int) int {
-		if n == 0 || mem[n] > 0 {
-			return mem[n]
-		}
-		res := t(n-1) + t(n-2) + t(n-3)
-		mem[n] = res
-		return res
+	if n < 2 {
+		return n
 	}
-
-	return t(n)
+	a, b, c := 0, 1, 1
+	for n > 2 {
+		a, b, c = b, c, a+b+c
+		n--
+	}
+	return c
 }
