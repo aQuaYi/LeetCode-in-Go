b8bc971cbcd037e770e5e1c69bf730e97df9a7d5
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-09 20:59:47 +0800

    1143 accepted.

--- Algorithms/1143.longest-common-subsequence/longest-common-subsequence.go
+++ Algorithms/1143.longest-common-subsequence/longest-common-subsequence.go
@@ -1,6 +1,23 @@
 package problem1143
 
 func longestCommonSubsequence(A, B string) int {
+	m, n := len(A), len(B)
+	dp := [1001][1001]int{}
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if A[i-1] == B[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+			}
+		}
+	}
+	return dp[m][n]
+}
 
-	return 0
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
