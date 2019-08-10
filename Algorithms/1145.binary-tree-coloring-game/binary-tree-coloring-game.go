ececfe752b28a960f8721f5506a80af0f13494c3
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-10 19:03:53 +0800

    1145 added

---
+++ Algorithms/1145.binary-tree-coloring-game/binary-tree-coloring-game.go
@@ -0,0 +1,16 @@
+package problem1145
+
+import "github.com/aQuaYi/LeetCode-in-Go/kit"
+
+// TreeNode is pre-defined...
+// type TreeNode struct {
+//     Val int
+//     Left *TreeNode
+//     Right *TreeNode
+// }
+type TreeNode = kit.TreeNode
+
+func btreeGameWinningMove(root *TreeNode, n int, x int) bool {
+
+	return false
+}
