91086ec2c1420a6266304801e7dee33afa09c94c
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-10 19:24:36 +0800

    1145 wrong answer

--- Algorithms/1145.binary-tree-coloring-game/binary-tree-coloring-game.go
+++ Algorithms/1145.binary-tree-coloring-game/binary-tree-coloring-game.go
@@ -1,6 +1,8 @@
 package problem1145
 
-import "github.com/aQuaYi/LeetCode-in-Go/kit"
+import (
+	"github.com/aQuaYi/LeetCode-in-Go/kit"
+)
 
 // TreeNode is pre-defined...
 // type TreeNode struct {
@@ -11,6 +13,19 @@ import "github.com/aQuaYi/LeetCode-in-Go/kit"
 type TreeNode = kit.TreeNode
 
 func btreeGameWinningMove(root *TreeNode, n int, x int) bool {
+	if x == root.Val {
+		return count(root.Left, x, true) != count(root.Right, x, true)
+	}
+	return count(root, x, false) <= n/2
+}
 
-	return false
+// count node and its children
+func count(node *TreeNode, x int, ok bool) int {
+	if node == nil {
+		return 0
+	}
+	if ok || node.Val == x {
+		return 1 + count(node.Left, x, true) + count(node.Right, x, true)
+	}
+	return count(node.Left, x, ok) + count(node.Right, x, ok)
 }
