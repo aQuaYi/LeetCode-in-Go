296851e54ae4523dd29f07b10da1dfb593bb07c8
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-10 19:44:53 +0800

    1145 done

--- Algorithms/1145.binary-tree-coloring-game/binary-tree-coloring-game.go
+++ Algorithms/1145.binary-tree-coloring-game/binary-tree-coloring-game.go
@@ -13,19 +13,28 @@ import (
 type TreeNode = kit.TreeNode
 
 func btreeGameWinningMove(root *TreeNode, n int, x int) bool {
-	if x == root.Val {
-		return count(root.Left, x, true) != count(root.Right, x, true)
+	node := find(root, x)
+	l, r := count(node.Left), count(node.Right)
+	p := n - 1 - l - r
+	n /= 2
+	return l > n || r > n || p > n
+}
+
+func find(node *TreeNode, x int) *TreeNode {
+	if node == nil || node.Val == x {
+		return node
 	}
-	return count(root, x, false) <= n/2
+	l, r := find(node.Left, x), find(node.Right, x)
+	if l == nil {
+		return r
+	}
+	return l
 }
 
 // count node and its children
-func count(node *TreeNode, x int, ok bool) int {
+func count(node *TreeNode) int {
 	if node == nil {
 		return 0
 	}
-	if ok || node.Val == x {
-		return 1 + count(node.Left, x, true) + count(node.Right, x, true)
-	}
-	return count(node.Left, x, ok) + count(node.Right, x, ok)
+	return 1 + count(node.Left) + count(node.Right)
 }
