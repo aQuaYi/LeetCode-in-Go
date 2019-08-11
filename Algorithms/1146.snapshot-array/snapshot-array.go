f3d0896a6d5c6b94e7c30cf231483618cd0215ff
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-11 12:46:20 +0800

    1147 accepted.

--- Algorithms/1146.snapshot-array/snapshot-array.go
+++ Algorithms/1146.snapshot-array/snapshot-array.go
@@ -2,43 +2,42 @@ package problem1146
 
 // SnapshotArray can snap a array
 type SnapshotArray struct {
-	count int
-	rec   []map[int]int
+	snaps   []map[int]int
+	current map[int]int
+	id      int
 }
 
 // Constructor resturn s a SnapshotArray
 func Constructor(length int) SnapshotArray {
-	rec := make([]map[int]int, length)
-	for i := range rec {
-		m := make(map[int]int, 32)
-		rec[i] = m
-	}
+	s := make([]map[int]int, 0, 128)
+	c := make(map[int]int, 32)
 	return SnapshotArray{
-		rec: rec,
+		snaps:   s,
+		current: c,
+		id:      0,
 	}
 }
 
 // Set val in index
 func (sa *SnapshotArray) Set(index int, val int) {
-	sa.rec[index][sa.count] = val
+	sa.current[index] = val
 }
 
 // Snap make snapshot
 func (sa *SnapshotArray) Snap() int {
-	sa.count++
-	return sa.count - 1
+	sa.snaps = append(sa.snaps, sa.current)
+	sa.current = make(map[int]int)
+	sa.id++
+	return sa.id - 1
 }
 
 // Get returns val in the snap
 func (sa *SnapshotArray) Get(index int, snap int) int {
-	rec := sa.rec[index]
-	res, ok := rec[snap]
-	for !ok && snap > 0 {
-		snap--
-		res, ok = rec[snap]
+	for id := snap; id >= 0; id-- {
+		history := sa.snaps[id]
+		if t, ok := history[index]; ok {
+			return t
+		}
 	}
-	if ok {
-		return res
-	}
-	return 0
+	return 0 //default value
 }
