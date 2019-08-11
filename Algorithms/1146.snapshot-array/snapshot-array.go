8631b19ab98ef3f3b75525ad7e1b34586bfa7227
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-11 11:35:30 +0800

    1146 forget to inital rec

--- Algorithms/1146.snapshot-array/snapshot-array.go
+++ Algorithms/1146.snapshot-array/snapshot-array.go
@@ -2,37 +2,35 @@ package problem1146
 
 // SnapshotArray can snap a array
 type SnapshotArray struct {
-	array []int
-	snaps [][]int
+	size      int
+	snapCount int
+	rec       map[int]map[int]int
 }
 
 // Constructor resturn s a SnapshotArray
 func Constructor(length int) SnapshotArray {
 	return SnapshotArray{
-		array: make([]int, length),
+		size: length,
 	}
 }
 
 // Set val in index
 func (sa *SnapshotArray) Set(index int, val int) {
-	sa.array[index] = val
+	m, ok := sa.rec[sa.snapCount]
+	if !ok {
+		m = make(map[int]int, sa.size)
+		sa.rec[sa.snapCount] = m
+	}
+	m[index] = val
 }
 
 // Snap make snapshot
 func (sa *SnapshotArray) Snap() int {
-	sa.snaps = append(sa.snaps, append(sa.array[:0:0], sa.array...))
-	return len(sa.snaps) - 1
+	sa.snapCount++
+	return sa.snapCount - 1
 }
 
 // Get returns val in the snap
 func (sa *SnapshotArray) Get(index int, snapID int) int {
-	return sa.snaps[snapID][index]
+	return sa.rec[snapID][index]
 }
-
-/**
- * Your SnapshotArray object will be instantiated and called as such:
- * obj := Constructor(length);
- * obj.Set(index,val);
- * param_2 := obj.Snap();
- * param_3 := obj.Get(index,snap_id);
- */
