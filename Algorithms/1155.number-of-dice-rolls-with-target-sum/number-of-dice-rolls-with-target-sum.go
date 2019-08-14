7266d71a08ec7fca38e25df4733b84f08ab0cf07
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-14 22:53:48 +0800

    1155 done

--- Algorithms/1155.number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.go
+++ Algorithms/1155.number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.go
@@ -3,27 +3,18 @@ package problem1155
 const mod = 1e9 + 7
 
 func numRollsToTarget(d int, f int, target int) int {
-	rec := [1001]int{}
+	dp := [31][1001]int{}
+	dp[0][0] = 1
 
-	var dp func(int, int) int
-	dp = func(d, target int) int {
-		if d == 0 && target == 0 {
-			return 1
+	for i := 1; i <= d; i++ {
+		max := i * f
+		for j := i; j <= target && j <= max; j++ {
+			for k := 1; k <= f && k <= j; k++ {
+				dp[i][j] += dp[i-1][j-k]
+			}
+			dp[i][j] %= mod
 		}
-		if d == 0 || target < 0 {
-			return 0
-		}
-		if rec[target] > 0 {
-			return rec[target]
-		}
-		res := 0
-		for i := 1; i <= f; i++ {
-			res += dp(d-1, target-i)
-		}
-		res %= mod
-		rec[target] = res
-		return res
 	}
 
-	return dp(d, target)
+	return dp[d][target]
 }
