08233fef15abb3781e6a3af8bdd4dd2538e094b2
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-19 21:06:11 +0800

    1160 done

--- Algorithms/1160.find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.go
+++ Algorithms/1160.find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.go
@@ -1,6 +1,32 @@
 package problem1160
 
 func countCharacters(words []string, chars string) int {
+	char := parse(chars)
+	res := 0
+	for _, word := range words {
+		res += count(char, word)
+	}
+	return res
+}
 
-	return 0
+func parse(s string) []int {
+	res := make([]int, 26)
+	for _, r := range s {
+		res[r-'a']++
+	}
+	return res
+}
+
+func count(char []int, word string) int {
+	res := 0
+	w := make([]int, 26)
+	for _, r := range word {
+		b := r - 'a'
+		w[b]++
+		if w[b] > char[b] {
+			return 0
+		}
+		res++
+	}
+	return res
 }
