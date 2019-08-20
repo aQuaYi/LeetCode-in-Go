2df72936ed065170cf557f7a6b21b521e13636e7
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-20 17:15:49 +0800

    1161 accepted. 152 ms, faster than 13.16%

--- Algorithms/1161.maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.go
+++ Algorithms/1161.maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.go
@@ -1,6 +1,10 @@
 package problem1161
 
-import "github.com/aQuaYi/LeetCode-in-Go/kit"
+import (
+	"math"
+
+	"github.com/aQuaYi/LeetCode-in-Go/kit"
+)
 
 // TreeNode is pre-defined...
 // type TreeNode struct {
@@ -11,6 +15,34 @@ import "github.com/aQuaYi/LeetCode-in-Go/kit"
 type TreeNode = kit.TreeNode
 
 func maxLevelSum(root *TreeNode) int {
+	res, maxSum := 0, math.MinInt64
+	queue := make([]*TreeNode, 1, 1<<12)
+	queue[0] = root
 
-	return 0
+	// BFS
+	level := 0
+	for len(queue) > 0 {
+		size := len(queue)
+		level++
+		sum := 0
+		for i := 0; i < size; i++ {
+			node := queue[i]
+			sum += node.Val
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+
+		if maxSum < sum {
+			maxSum = sum
+			res = level
+		}
+
+		queue = queue[size:]
+	}
+
+	return res
 }
