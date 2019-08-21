0d330563e7e0210f9ed6de5022dade3f20a28afd
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-21 20:43:50 +0800

    1162 wrong answer

--- Algorithms/1162.as-far-from-land-as-possible/as-far-from-land-as-possible.go
+++ Algorithms/1162.as-far-from-land-as-possible/as-far-from-land-as-possible.go
@@ -1,6 +1,42 @@
 package problem1162
 
-func maxDistance(grid [][]int) int {
+var dx = []int{-1, 1, 0, 0}
+var dy = []int{0, 0, -1, 1}
 
-	return 0
+func maxDistance(grid [][]int) int {
+	n := len(grid)
+	hasSeen := [128][128]bool{}
+
+	queue := make([][2]int, 0, n)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			if grid[i][j] == 1 {
+				queue = append(queue, [2]int{i, j})
+				hasSeen[i][j] = true
+			}
+		}
+	}
+
+	dist := -1
+	for len(queue) > 0 {
+		dist++
+		size := len(queue)
+		for s := 0; s < size; s++ {
+			g := queue[s]
+			x, y := g[0], g[1]
+			for k := 0; k < 4; k++ {
+				i, j := x+dx[k], y+dy[k]
+				if i < 0 || n <= i ||
+					j < 0 || n <= j ||
+					hasSeen[i][j] {
+					continue
+				}
+				queue = append(queue, [2]int{i, j})
+				hasSeen[i][j] = true
+			}
+		}
+		queue = queue[size:]
+	}
+
+	return dist
 }
