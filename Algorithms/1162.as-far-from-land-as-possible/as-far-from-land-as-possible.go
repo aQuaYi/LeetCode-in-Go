c481f296f917dd126b99cea53dd4f341245aca55
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-21 20:55:36 +0800

    1162 done

--- Algorithms/1162.as-far-from-land-as-possible/as-far-from-land-as-possible.go
+++ Algorithms/1162.as-far-from-land-as-possible/as-far-from-land-as-possible.go
@@ -5,41 +5,40 @@ var dy = []int{0, 0, -1, 1}
 
 func maxDistance(grid [][]int) int {
 	n := len(grid)
-	hasSeen := [128][128]bool{}
 
-	queue := make([][2]int, 0, n)
+	lands := make([][2]int, 0, n)
 	for i := 0; i < n; i++ {
 		for j := 0; j < n; j++ {
 			if grid[i][j] == 1 {
-				queue = append(queue, [2]int{i, j})
-				hasSeen[i][j] = true
+				lands = append(lands, [2]int{i, j})
 			}
 		}
 	}
 
-	if len(queue) == n*n {
+	if len(lands) == n*n {
 		return -1
 	}
 
 	dist := -1
-	for len(queue) > 0 {
+	// BFS
+	for len(lands) > 0 {
 		dist++
-		size := len(queue)
+		size := len(lands)
 		for s := 0; s < size; s++ {
-			g := queue[s]
+			g := lands[s]
 			x, y := g[0], g[1]
 			for k := 0; k < 4; k++ {
 				i, j := x+dx[k], y+dy[k]
 				if i < 0 || n <= i ||
 					j < 0 || n <= j ||
-					hasSeen[i][j] {
+					grid[i][j] == 1 {
 					continue
 				}
-				queue = append(queue, [2]int{i, j})
-				hasSeen[i][j] = true
+				lands = append(lands, [2]int{i, j})
+				grid[i][j] = 1
 			}
 		}
-		queue = queue[size:]
+		lands = lands[size:]
 	}
 
 	return dist
