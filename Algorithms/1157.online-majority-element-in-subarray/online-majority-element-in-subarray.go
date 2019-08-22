03c23792f4133fbc94b4a3e2e0735f4c7b897739
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-22 17:06:54 +0800

    1157 time limit exceeded

--- Algorithms/1157.online-majority-element-in-subarray/online-majority-element-in-subarray.go
+++ Algorithms/1157.online-majority-element-in-subarray/online-majority-element-in-subarray.go
@@ -2,18 +2,24 @@ package problem1157
 
 // MajorityChecker is ..
 type MajorityChecker struct {
+	arr []int
 }
 
 // Constructor is ...
 func Constructor(arr []int) MajorityChecker {
-
-	return MajorityChecker{}
+	return MajorityChecker{arr: arr}
 }
 
 // Query is ...
 func (mc *MajorityChecker) Query(left int, right int, threshold int) int {
-
-	return 0
+	count := make(map[int]int, threshold)
+	for i := left; i <= right; i++ {
+		count[mc.arr[i]]++
+		if count[mc.arr[i]] >= threshold {
+			return mc.arr[i]
+		}
+	}
+	return -1
 }
 
 /**
