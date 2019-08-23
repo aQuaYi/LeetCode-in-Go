53107fc6cc73412bc367e360eb9816b95ce354e9
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-23 16:17:57 +0800

    1163 wrong answer

--- Algorithms/1163.last-substring-in-lexicographical-order/last-substring-in-lexicographical-order.go
+++ Algorithms/1163.last-substring-in-lexicographical-order/last-substring-in-lexicographical-order.go
@@ -1,6 +1,14 @@
 package problem1163
 
-func lastSubstring(s string) string {
+import "strings"
 
-	return ""
+func lastSubstring(s string) string {
+	var index int
+	for i := 'z'; i >= 'a'; i-- {
+		index = strings.Index(s, string(i))
+		if index != -1 {
+			break
+		}
+	}
+	return s[index:]
 }
