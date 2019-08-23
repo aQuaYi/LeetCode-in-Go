3fa689594f45b2a6476f2c4da4a2f478f40424b4
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-23 22:36:14 +0800

    1163 done

--- Algorithms/1163.last-substring-in-lexicographical-order/last-substring-in-lexicographical-order.go
+++ Algorithms/1163.last-substring-in-lexicographical-order/last-substring-in-lexicographical-order.go
@@ -1,42 +1,31 @@
 package problem1163
 
-import "strings"
-
 func lastSubstring(s string) string {
-	sub := make([]byte, 1, 128)
-	sub[0] = 'z'
-
-	reduce := func() {
-		sub[len(sub)-1]--
-	}
-
-	extend := func() {
-		sub = append(sub, sub[0])
-	}
-
-	// operating string is expensive
-	// so I have to make this function a little more responsibility
-	// check and change the sub
-	containsDouble := func() bool {
-		sub = append(sub, sub...)
-		if strings.Contains(s, string(sub)) {
-			return true
+	last, n := 0, len(s)
+	for i := 1; i < n; i++ {
+		if s[i-1] == s[i] {
+			// all of s[last:i+1] are the same letter.
+			// 'i' has no chance to be the new 'last'
+			// the reason is
+			// 1. s[i+1] > s[i], 'i+1' is the new 'last'
+			// 2. s[i+1] < s[i], 'last' is still 'last'
+			// 3. s[i+1] = s[i], 'i+1' is the new 'i'
+			//    like above, need a different letter to make decision
+			continue
 		}
-		sub = sub[:len(sub)/2]
-		return false
-	}
-
-	count := strings.Count(s, string(sub))
-	for count != 1 {
-		if count == 0 {
-			reduce()
-		} else if !containsDouble() {
-			extend()
+		for l := 0; i+l < n; l++ {
+			if s[last+l] < s[i+l] {
+				// s[i:] is bigger than s[last:]
+				// new begin is here
+				last = i
+				break
+			}
+			if s[last+l] > s[i+l] {
+				// s[last:] is bigger than s[i:]
+				// no need to compare continue
+				break
+			}
 		}
-		count = strings.Count(s, string(sub))
 	}
-
-	index := strings.Index(s, string(sub))
-
-	return s[index:]
+	return s[last:]
 }
