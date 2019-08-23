abd712e3eab8180386b4c590dbe2cb0a2549d801
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-23 16:10:45 +0800

    1163 added

---
+++ Algorithms/1163.last-substring-in-lexicographical-order/README.md
@@ -0,0 +1,23 @@
+# [1163. Last Substring in Lexicographical Order](https://leetcode.com/problems/last-substring-in-lexicographical-order/)
+
+Given a string s, return the last substring of s in lexicographical order.
+
+Example 1:
+
+```text
+Input: "abab"
+Output: "bab"
+Explanation: The substrings are ["a", "ab", "aba", "abab", "b", "ba", "bab"]. The lexicographically maximum substring is "bab".
+```
+
+Example 2:
+
+```text
+Input: "leetcode"
+Output: "tcode"
+```
+
+Note:
+
+- `1 <= s.length <= 4 * 10^5`
+- `s contains only lowercase English letters.`
---
+++ Algorithms/1163.last-substring-in-lexicographical-order/last-substring-in-lexicographical-order_test.go
@@ -0,0 +1,42 @@
+package problem1163
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	s   string
+	ans string
+}{
+
+	{
+		"abab",
+		"bab",
+	},
+
+	{
+		"leetcode",
+		"tcode",
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_lastSubstring(t *testing.T) {
+	a := assert.New(t)
+
+	for _, tc := range tcs {
+		a.Equal(tc.ans, lastSubstring(tc.s), "输入:%v", tc)
+	}
+}
+
+func Benchmark_lastSubstring(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			lastSubstring(tc.s)
+		}
+	}
+}
