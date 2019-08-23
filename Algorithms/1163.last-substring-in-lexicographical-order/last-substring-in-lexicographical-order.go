7ac0031894f58fbbd1bd6cf951a3b4375c4707ef
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-23 16:57:28 +0800

    1163 wrong answer

--- Algorithms/1163.last-substring-in-lexicographical-order/last-substring-in-lexicographical-order.go
+++ Algorithms/1163.last-substring-in-lexicographical-order/last-substring-in-lexicographical-order.go
@@ -2,13 +2,38 @@ package problem1163
 
 import "strings"
 
+// replace mark
+const (
+	MARK   = "@"
+	DOUBLE = "@@"
+)
+
 func lastSubstring(s string) string {
-	var index int
-	for i := 'z'; i >= 'a'; i-- {
-		index = strings.Index(s, string(i))
-		if index != -1 {
-			break
+	old := s
+	m, k := "", 'z'
+	c := 0
+	mark := m + string(k)
+	count := strings.Count(s, mark)
+	for count != 1 {
+		if count == 0 {
+			k--
+		} else {
+			s = strings.Replace(s, mark, MARK, -1)
+			c++
+			for strings.Contains(s, DOUBLE) {
+				s = strings.Replace(s, DOUBLE, MARK, -1)
+				c *= 2
+			}
+			m, k = MARK, 'z'
 		}
+		mark = m + string(k)
+		count = strings.Count(s, mark)
 	}
-	return s[index:]
+
+	index := strings.Index(s, mark)
+	s = s[:index]
+	count = strings.Count(s, MARK)
+	index += (c - 2) * count
+
+	return old[index:]
 }
