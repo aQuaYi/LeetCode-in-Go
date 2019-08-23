cca95a97bdde63abb07f51f7a8acacb9fecff9b4
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-23 20:56:06 +0800

    1163 done

--- Algorithms/1163.last-substring-in-lexicographical-order/last-substring-in-lexicographical-order.go
+++ Algorithms/1163.last-substring-in-lexicographical-order/last-substring-in-lexicographical-order.go
@@ -3,7 +3,7 @@ package problem1163
 import "strings"
 
 func lastSubstring(s string) string {
-	sub := make([]byte, 1, 64)
+	sub := make([]byte, 1, 128)
 	sub[0] = 'z'
 
 	reduce := func() {
@@ -11,29 +11,26 @@ func lastSubstring(s string) string {
 	}
 
 	extend := func() {
-		sub = append(sub, sub[len(sub)-1])
+		sub = append(sub, sub[0])
 	}
 
-	double := func() string {
+	// operating string is expensive
+	// so I have to make this function a little more responsibility
+	// check and change the sub
+	containsDouble := func() bool {
 		sub = append(sub, sub...)
-		return string(sub)
-	}
-
-	half := func() {
+		if strings.Contains(s, string(sub)) {
+			return true
+		}
 		sub = sub[:len(sub)/2]
+		return false
 	}
 
 	count := strings.Count(s, string(sub))
 	for count != 1 {
 		if count == 0 {
 			reduce()
-		} else {
-			for strings.Contains(s, double()) {
-			}
-			half()
-			if strings.HasSuffix(s, string(sub)) {
-				break
-			}
+		} else if !containsDouble() {
 			extend()
 		}
 		count = strings.Count(s, string(sub))
