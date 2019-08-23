be48ca9672749972ef7644596e610f711c16174e
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-23 17:29:19 +0800

    1163 done

--- Algorithms/1163.last-substring-in-lexicographical-order/last-substring-in-lexicographical-order.go
+++ Algorithms/1163.last-substring-in-lexicographical-order/last-substring-in-lexicographical-order.go
@@ -3,28 +3,43 @@ package problem1163
 import "strings"
 
 func lastSubstring(s string) string {
-	m, k := "", 'z'
-	mark := m + string(k)
-	count := strings.Count(s, mark)
-	for count != 1 {
-		if count == 0 {
-			k--
-		} else {
-			double := mark + mark
-			for strings.Contains(s, double) {
-				mark = double
-				double = mark + mark
-			}
-			if strings.HasSuffix(s, mark) {
-				break
-			}
-			m = mark
-		}
-		mark = m + string(k)
-		count = strings.Count(s, mark)
+	sub := make([]byte, 1, 64)
+	sub[0] = 'z'
+
+	reduce := func() {
+		sub[len(sub)-1]--
 	}
 
-	index := strings.Index(s, mark)
+	extend := func() {
+		sub = append(sub, sub[len(sub)-1])
+	}
+
+	double := func() string {
+		sub = append(sub, sub...)
+		return string(sub)
+	}
+
+	half := func() {
+		sub = sub[:len(sub)/2]
+	}
+
+	count := strings.Count(s, string(sub))
+	for count != 1 {
+		if count == 0 {
+			reduce()
+		} else {
+			for strings.Contains(s, double()) {
+			}
+			half()
+			if strings.HasSuffix(s, string(sub)) {
+				break
+			}
+			extend()
+		}
+		count = strings.Count(s, string(sub))
+	}
+
+	index := strings.Index(s, string(sub))
 
 	return s[index:]
 }
