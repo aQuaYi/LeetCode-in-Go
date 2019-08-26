798dcee2592615c528dfe7c2a56b857ee9956bc7
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-26 17:14:45 +0800

    1170 accepted.

--- Algorithms/1170.compare-strings-by-frequency-of-the-smallest-character/compare-strings-by-frequency-of-the-smallest-character.go
+++ Algorithms/1170.compare-strings-by-frequency-of-the-smallest-character/compare-strings-by-frequency-of-the-smallest-character.go
@@ -1,6 +1,32 @@
 package problem1170
 
-func numSmallerByFrequency(queries []string, words []string) []int {
+import "sort"
 
-	return nil
+func numSmallerByFrequency(queries []string, words []string) []int {
+	n := len(words)
+	ws := make([]int, n)
+	for i, w := range words {
+		ws[i] = f(w)
+	}
+	sort.Ints(ws)
+
+	res := make([]int, len(queries))
+	for i, q := range queries {
+		fq := f(q)
+		res[i] = n - sort.Search(n, func(i int) bool { return fq < ws[i] })
+	}
+
+	return res
+}
+
+func f(s string) int {
+	count := [26]int{}
+	for _, b := range s {
+		count[b-'a']++
+	}
+	i := 0
+	for count[i] == 0 {
+		i++
+	}
+	return count[i]
 }
