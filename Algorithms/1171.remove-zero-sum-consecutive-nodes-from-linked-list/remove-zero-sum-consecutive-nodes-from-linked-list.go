0a0162b2a994552197513e9b8c93947664b09101
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-08-28 14:16:26 +0800

    1171 added

---
+++ Algorithms/1171.remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.go
@@ -0,0 +1,11 @@
+package problem1171
+
+import "github.com/aQuaYi/LeetCode-in-Go/kit"
+
+// ListNode is ...
+type ListNode = kit.ListNode
+
+func removeZeroSumSublists(head *ListNode) *ListNode {
+
+	return nil
+}
