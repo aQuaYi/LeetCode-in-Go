512c1099ea8ea76335cecd65fca754814d1711da
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-09-02 20:02:23 +0800

    1176 accepted. 28 ms, faster than 83.33%

--- Algorithms/1176.diet-plan-performance/diet-plan-performance.go
+++ Algorithms/1176.diet-plan-performance/diet-plan-performance.go
@@ -1,6 +1,28 @@
 package problem1176
 
 func dietPlanPerformance(calories []int, k int, lower int, upper int) int {
+	point := func(t int) int {
+		if t < lower {
+			return -1
+		}
+		if t > upper {
+			return 1
+		}
+		return 0
+	}
 
-	return 0
+	T := 0
+	res := 0
+	for i, c := range calories {
+		T += c
+		if i < k-1 {
+			continue
+		}
+		if i-k >= 0 {
+			T -= calories[i-k]
+		}
+		res += point(T)
+	}
+
+	return res
 }
