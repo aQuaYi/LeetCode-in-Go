06fb896434bba3f360bc937d4692e51bdd18b035
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-09-02 15:55:50 +0800

    1175 done

--- Algorithms/1175.prime-arrangements/prime-arrangements.go
+++ Algorithms/1175.prime-arrangements/prime-arrangements.go
@@ -1,8 +1,25 @@
 package problem1175
 
+import "sort"
+
 const mod = 1e9 + 7
 
-func numPrimeArrangements(n int) int {
+var primes = []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}
 
-	return 0
+func numPrimeArrangements(n int) int {
+	c := count(n)
+	return factorial(c) * factorial(n-c) % mod
+}
+
+func count(n int) int {
+	return sort.Search(25, func(i int) bool {
+		return primes[i] > n
+	})
+}
+
+func factorial(n int) int {
+	if n == 1 || n == 0 {
+		return 1
+	}
+	return n * factorial(n-1) % mod
 }
