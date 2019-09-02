bb4fb1ed70be5e35393964b46f763936f9101776
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-09-02 16:05:44 +0800

    78 improve

--- Algorithms/0078.subsets/subsets.go
+++ Algorithms/0078.subsets/subsets.go
@@ -1,33 +1,11 @@
 package problem0078
 
-import (
-	"math"
-)
-
 func subsets(nums []int) [][]int {
-	res := make([][]int, 0, resSize(nums))
-	rec(nums, []int{}, &res)
-	return res
-}
-
-func resSize(nums []int) int {
-	return int(math.Pow(2, float64(len(nums))))
-}
-
-func rec(nums, temp []int, res *[][]int) {
-	size := len(nums)
-	if size == 0 {
-		*res = append(*res, temp)
-		return
+	res := make([][]int, 1, 1024)
+	for _, n := range nums {
+		for _, r := range res {
+			res = append(res, append([]int{n}, r...))
+		}
 	}
-
-	// 对于 last 来说，要么存在于某个子集中，要么不存在
-	nums, last := nums[:size-1], nums[size-1]
-
-	rec(nums, temp, res)
-
-	withLast := make([]int, 1, 1+len(temp))
-	withLast[0] = last
-	withLast = append(withLast, temp...)
-	rec(nums, withLast, res)
+	return res
 }
