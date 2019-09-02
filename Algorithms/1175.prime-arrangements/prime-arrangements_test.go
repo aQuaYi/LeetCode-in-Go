b784f44b667f46a1dd03df943faed872de40cd91
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-09-02 15:43:16 +0800

    1175 added

---
+++ Algorithms/1175.prime-arrangements/prime-arrangements_test.go
@@ -0,0 +1,42 @@
+package problem1175
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	n   int
+	ans int
+}{
+
+	{
+		5,
+		12,
+	},
+
+	{
+		100,
+		682289015,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_numPrimeArrangements(t *testing.T) {
+	a := assert.New(t)
+
+	for _, tc := range tcs {
+		a.Equal(tc.ans, numPrimeArrangements(tc.n), "输入:%v", tc)
+	}
+}
+
+func Benchmark_numPrimeArrangements(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			numPrimeArrangements(tc.n)
+		}
+	}
+}
