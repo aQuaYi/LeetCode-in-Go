0eaa5d47ae7c8c197a1d51859a9a71a73705e1e3
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-09-03 23:32:01 +0800

    1177 undone

--- Algorithms/1177.can-make-palindrome-from-substring/can-make-palindrome-from-substring.go
+++ Algorithms/1177.can-make-palindrome-from-substring/can-make-palindrome-from-substring.go
@@ -1,5 +1,7 @@
 package problem1177
 
+// TODO: unaccepted. the biggest test case spent 4500 ms. Limit Time Exceeded.
+
 func canMakePaliQueries(s string, queries [][]int) []bool {
 	n := len(queries)
 
@@ -11,21 +13,37 @@ func canMakePaliQueries(s string, queries [][]int) []bool {
 	}
 
 	res := make([]bool, n)
-
+	hasSeen := make(map[int]bool, n)
 	for i, q := range queries {
 		lo, hi, k := q[0], q[1], q[2]
-		remains := bites(cnt[hi+1] ^ cnt[lo])
-		res[i] = remains/2 <= k
+		if k >= 13 {
+			res[i] = true
+			continue
+		}
+		m := mark(lo, hi, k)
+		r, ok := hasSeen[m]
+		if ok {
+			res[i] = r
+			continue
+		}
+		remains := bits(cnt[hi+1] ^ cnt[lo])
+		r = remains/2 <= k
+		res[i] = r
+		hasSeen[m] = r
 	}
 
 	return res
 }
 
-func bites(n int) int {
+func bits(n int) int {
 	res := 0
 	for n > 0 {
 		res += n & 1
 		n >>= 1
 	}
 	return res
+}
+
+func mark(lo, hi, k int) int {
+	return lo<<22 + hi<<4 + k
 }
