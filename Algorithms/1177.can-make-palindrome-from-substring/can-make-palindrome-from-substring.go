96115bed2a9eb6b202d32b796b7ed49c2024421a
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-09-03 20:10:44 +0800

    1177 doing

--- Algorithms/1177.can-make-palindrome-from-substring/can-make-palindrome-from-substring.go
+++ Algorithms/1177.can-make-palindrome-from-substring/can-make-palindrome-from-substring.go
@@ -1,6 +1,23 @@
 package problem1177
 
 func canMakePaliQueries(s string, queries [][]int) []bool {
+	count := [26][]int{}
+	for i, letter := range s {
+		b := letter - 'a'
+		count[b] = append(count[b], i)
+	}
+
+	check := func(q []int) bool {
+		left, right, k := q[0], q[1], q[2]
+		k++
+		for i := left; i <= right; i++ {
+			// TODO:
+			if k < 0 {
+				return false
+			}
+		}
+		return true
+	}
 
 	return nil
 }
