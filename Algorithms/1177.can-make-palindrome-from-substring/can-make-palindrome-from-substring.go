9b833394ac70be41ea7a9a48c41b7009fc14b06a
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-09-03 22:12:49 +0800

    1177 LIMIT TIME EXCEEDED

--- Algorithms/1177.can-make-palindrome-from-substring/can-make-palindrome-from-substring.go
+++ Algorithms/1177.can-make-palindrome-from-substring/can-make-palindrome-from-substring.go
@@ -3,36 +3,40 @@ package problem1177
 import "sort"
 
 func canMakePaliQueries(s string, queries [][]int) []bool {
-	count := [26][]int{}
+	indexesOf := [26][]int{}
 	for i, letter := range s {
 		b := letter - 'a'
-		count[b] = append(count[b], i)
+		indexesOf[b] = append(indexesOf[b], i)
 	}
 
-	check := func(q []int) bool {
-		left, right, k := q[0], q[1]+1, q[2]+1
-		hasSeen := [26]bool{}
-		for i := left; i < right; i++ {
-			b := s[i] - 'a'
-			if hasSeen[b] {
-				continue
-			}
-			hasSeen[b] = true
-			cs := count[b]
-			l := sort.SearchInts(cs, left)
-			r := sort.SearchInts(cs, right)
-			k -= (r - l) % 2
-			if k < 0 {
-				return false
-			}
+	count := func(left, right int) int {
+		single := 0
+		for _, indexes := range indexesOf {
+			l := sort.SearchInts(indexes, left)
+			r := sort.SearchInts(indexes, right+1)
+			single += (r - l) % 2
 		}
-		return true
+		return single / 2
 	}
+
 	n := len(queries)
+	needs := make(map[int]int, n)
 	res := make([]bool, n)
 	for i := 0; i < n; i++ {
-		res[i] = check(queries[i])
+		q := queries[i]
+		left, right, k := q[0], q[1], q[2]
+		m := mark(left, right)
+		need, ok := needs[m]
+		if !ok {
+			need = count(left, right)
+			needs[m] = need
+		}
+		res[i] = k >= need
 	}
 
 	return res
+}
+
+func mark(left, right int) int {
+	return left<<18 + right
 }
