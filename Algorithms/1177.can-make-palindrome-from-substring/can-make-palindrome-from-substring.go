64bcd09d565b34e7fceff38a4b92fd14a93b4616
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-09-03 21:27:22 +0800

    1177 wrong answer. 错误地理解了题意。

--- Algorithms/1177.can-make-palindrome-from-substring/can-make-palindrome-from-substring.go
+++ Algorithms/1177.can-make-palindrome-from-substring/can-make-palindrome-from-substring.go
@@ -1,5 +1,7 @@
 package problem1177
 
+import "sort"
+
 func canMakePaliQueries(s string, queries [][]int) []bool {
 	count := [26][]int{}
 	for i, letter := range s {
@@ -8,16 +10,29 @@ func canMakePaliQueries(s string, queries [][]int) []bool {
 	}
 
 	check := func(q []int) bool {
-		left, right, k := q[0], q[1], q[2]
-		k++
-		for i := left; i <= right; i++ {
-			// TODO:
+		left, right, k := q[0], q[1]+1, q[2]+1
+		hasSeen := [26]bool{}
+		for i := left; i < right; i++ {
+			b := s[i] - 'a'
+			if hasSeen[b] {
+				continue
+			}
+			hasSeen[b] = true
+			cs := count[b]
+			l := sort.SearchInts(cs, left)
+			r := sort.SearchInts(cs, right)
+			k -= (r - l) % 2
 			if k < 0 {
 				return false
 			}
 		}
 		return true
 	}
+	n := len(queries)
+	res := make([]bool, n)
+	for i := 0; i < n; i++ {
+		res[i] = check(queries[i])
+	}
 
-	return nil
+	return res
 }
