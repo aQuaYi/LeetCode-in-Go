817e269fb7eb8187473bd4c3a227fd65d5309d4d
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-09-03 22:58:21 +0800

    1177 Time Limit Exceeded.

--- Algorithms/1177.can-make-palindrome-from-substring/can-make-palindrome-from-substring.go
+++ Algorithms/1177.can-make-palindrome-from-substring/can-make-palindrome-from-substring.go
@@ -3,27 +3,29 @@ package problem1177
 func canMakePaliQueries(s string, queries [][]int) []bool {
 	n := len(queries)
 
-	count := make([][26]int, 1, n+1)
-	c := [26]int{}
-	for _, letter := range s {
-		b := letter - 'a'
-		c[b]++
-		count = append(count, c)
+	cnt := make([]int, 1, n+1)
+	c := 0
+	for _, l := range s {
+		c ^= 1 << uint(l-'a')
+		cnt = append(cnt, c)
 	}
 
 	res := make([]bool, n)
 
 	for i, q := range queries {
 		lo, hi, k := q[0], q[1], q[2]
-		single := 0
-		for j := 0; j < 26; j++ {
-			single += (count[hi+1][j] - count[lo][j]) % 2
-			// if single/2 > k {
-			// 	break
-			// }
-		}
-		res[i] = single/2 <= k
+		remains := bites(cnt[hi+1] ^ cnt[lo])
+		res[i] = remains/2 <= k
 	}
 
+	return res
+}
+
+func bites(n int) int {
+	res := 0
+	for n > 0 {
+		res += n & 1
+		n >>= 1
+	}
 	return res
 }
