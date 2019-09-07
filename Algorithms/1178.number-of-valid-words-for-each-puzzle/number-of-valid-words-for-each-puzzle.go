97547a2449cc9297b2b6ccccf89fbbbb916d274b
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-09-07 14:06:42 +0800

    1178 Limit Time Exceeded

--- Algorithms/1178.number-of-valid-words-for-each-puzzle/number-of-valid-words-for-each-puzzle.go
+++ Algorithms/1178.number-of-valid-words-for-each-puzzle/number-of-valid-words-for-each-puzzle.go
@@ -1,49 +1,28 @@
 package problem1178
 
 func findNumOfValidWords(words []string, puzzles []string) []int {
-	m, n := len(words), len(puzzles)
-
-	ws := make([]word, m)
-	for i, w := range words {
-		ws[i] = makeWord(w)
+	count := make(map[int]int, len(words))
+	for _, w := range words {
+		count[mask(w)]++
 	}
 
-	ps := make([]word, n)
+	res := make([]int, len(puzzles))
+
 	for i, p := range puzzles {
-		ps[i] = makeWord(p)
-	}
-
-	res := make([]int, n)
-
-	for i, p := range ps {
-		for _, w := range ws {
-			res[i] += valid(p, w)
+		first := 1 << uint(p[0]-'a')
+		pm := mask(p)
+		for wm, c := range count {
+			if first&wm != 0 &&
+				pm&wm == wm {
+				res[i] += c
+			}
 		}
 	}
 
 	return res
 }
 
-func valid(p, w word) int {
-	if p.first&w.total != 0 &&
-		p.total&w.total == w.total {
-		return 1
-	}
-	return 0
-}
-
-type word struct {
-	first, total int
-}
-
-func makeWord(w string) word {
-	return word{
-		first: 1 << uint(w[0]-'a'),
-		total: convert(w),
-	}
-}
-
-func convert(w string) int {
+func mask(w string) int {
 	res := 0
 	for _, l := range w {
 		res |= 1 << uint(l-'a')
