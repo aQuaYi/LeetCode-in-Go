e6cdd97e634e807cd8fa1002c3885a6c22265a5f
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-09-07 12:59:05 +0800

    1178 Limit Time Exceeded

--- Algorithms/1178.number-of-valid-words-for-each-puzzle/number-of-valid-words-for-each-puzzle.go
+++ Algorithms/1178.number-of-valid-words-for-each-puzzle/number-of-valid-words-for-each-puzzle.go
@@ -1,6 +1,46 @@
 package problem1178
 
-func findNumOfValidWords(words []string, puzzles []string) []int {
+import "strings"
 
-	return []int{1, 1, 3, 2, 4, 0}
+func findNumOfValidWords(words []string, puzzles []string) []int {
+	m := len(words)
+	lss := make([][]string, m)
+	for i, w := range words {
+		lss[i] = unique(w)
+	}
+
+	n := len(puzzles)
+	res := make([]int, n)
+	for i, p := range puzzles {
+		for j, w := range words {
+			res[i] += valid(p, w, lss[j])
+		}
+	}
+
+	return res
+}
+
+func unique(w string) []string {
+	n := len(w)
+	lm := make(map[string]bool, n)
+	for i := 0; i < n; i++ {
+		lm[w[i:i+1]] = true
+	}
+	ls := make([]string, 0, n)
+	for l := range lm {
+		ls = append(ls, l)
+	}
+	return ls
+}
+
+func valid(p, w string, ls []string) int {
+	if !strings.Contains(w, p[:1]) {
+		return 0
+	}
+	for _, l := range ls {
+		if !strings.Contains(p, l) {
+			return 0
+		}
+	}
+	return 1
 }
