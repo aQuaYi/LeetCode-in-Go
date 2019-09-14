9a17aa4f756e6bf47eb7b003218d342fcf64041d
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-09-14 18:47:47 +0800

    1184 done

--- Algorithms/1184.distance-between-bus-stops/distance-between-bus-stops.go
+++ Algorithms/1184.distance-between-bus-stops/distance-between-bus-stops.go
@@ -1,6 +1,24 @@
 package problem1184
 
 func distanceBetweenBusStops(distance []int, start int, destination int) int {
+	if start > destination {
+		start, destination = destination, start
+	}
 
-	return 0
+	total, part := 0, 0
+	for i, d := range distance {
+		total += d
+		if start <= i && i < destination {
+			part += d
+		}
+	}
+
+	return min(part, total-part)
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
