3bc6882780ea022a64ea765a4baa65cd048f76a8
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-09-16 20:24:56 +0800

    1189 done

--- Algorithms/1189.maximum-number-of-balloons/maximum-number-of-balloons.go
+++ Algorithms/1189.maximum-number-of-balloons/maximum-number-of-balloons.go
@@ -1,5 +1,29 @@
 package problem1189
 
 func maxNumberOfBalloons(text string) int {
-	return 0
+	b, a, l, o, n := 0, 0, 0, 0, 0
+	for _, r := range text {
+		switch r {
+		case 'b':
+			b++
+		case 'a':
+			a++
+		case 'l':
+			l++
+		case 'o':
+			o++
+		case 'n':
+			n++
+		}
+	}
+	res := min(b, min(a, n))
+	res = min(res, min(l/2, o/2))
+	return res
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
