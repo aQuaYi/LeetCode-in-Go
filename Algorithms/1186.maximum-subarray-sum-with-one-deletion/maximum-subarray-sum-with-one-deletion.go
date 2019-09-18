b5dcdf593e630c4ba746b0fd6dffda16e19dfc89
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-09-18 17:27:04 +0800

    1156 wrong answer

--- Algorithms/1186.maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.go
+++ Algorithms/1186.maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.go
@@ -1,6 +1,28 @@
 package problem1186
 
-func maximumSum(arr []int) int {
+func maximumSum(A []int) int {
+	res, maxA := 0, -1000000
+	last, cur := 0, 0
+	for _, a := range A {
+		if a < 0 {
+			res = max(res, last+cur)
+			last, cur = cur, 0
+		}else{
+			cur += a
+		}
+		maxA= max(maxA, a)
+	}
 
-	return 0
+	if maxA<=0 {
+			return maxA
+	}
+
+	return max(res, last+cur)
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
