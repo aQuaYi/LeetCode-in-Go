384072b171551715bc77d265f9882f6cebc488a7
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-09-18 19:55:23 +0800

    1186 done

--- Algorithms/1186.maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.go
+++ Algorithms/1186.maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.go
@@ -1,34 +1,25 @@
 package problem1186
 
+// ref: https://leetcode.com/problems/maximum-subarray-sum-with-one-deletion/discuss/377397/Intuitive-Java-Solution-With-Explanation
+
 func maximumSum(A []int) int {
-	sum, curMin := 0, 1000000
-	res, maxA := 0, -1000000
-	for _, a := range A {
-		sum += a
-		curMin = min(curMin, a)
-
-		if sum-min(curMin, 0) <= 0 {
-			sum = 0
-			curMin = 1000000
-		} else {
-			res = max(res, sum-min(curMin, 0))
-		}
-
-		maxA = max(maxA, a)
+	n := len(A)
+	maxRightIs := make([]int, n)
+	maxLeftIs := make([]int, n)
+	maxRightIs[0] = A[0]
+	maxLeftIs[n-1] = A[n-1]
+	res := A[0]
+	for i := 1; i < n; i++ {
+		maxRightIs[i] = max(A[i], maxRightIs[i-1]+A[i])
+		j := n - i - 1
+		maxLeftIs[j] = max(A[j], A[j]+maxLeftIs[j+1])
+		// 最大的子数组，也有可能是仅由一段组成
+		res = max(res, maxRightIs[i])
 	}
-
-	if maxA <= 0 {
-		return maxA
+	for i := 1; i < n-1; i++ {
+		res = max(res, maxRightIs[i-1]+maxLeftIs[i+1])
 	}
-
-return max(res, sum-min(curMin,0))
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
+	return res
 }
 
 func max(a, b int) int {
