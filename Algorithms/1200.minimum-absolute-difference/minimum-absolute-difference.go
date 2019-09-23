e594a066f05aac229b930b4b4c7b15a7de88f1fa
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-09-23 16:27:14 +0800

    1200 accepted. 248ms

--- Algorithms/1200.minimum-absolute-difference/minimum-absolute-difference.go
+++ Algorithms/1200.minimum-absolute-difference/minimum-absolute-difference.go
@@ -1,6 +1,36 @@
 package problem1200
 
-func minimumAbsDifference(arr []int) [][]int {
+import (
+	"math"
+	"sort"
+)
 
-	return nil
+func minimumAbsDifference(A []int) [][]int {
+	sort.Ints(A)
+	n := len(A)
+	isExist := make(map[int]bool, n)
+
+	minDiff := math.MaxInt64
+	for i := 1; i < n; i++ {
+		minDiff = min(minDiff, A[i]-A[i-1])
+		isExist[A[i]] = true
+	}
+
+	res := make([][]int, 0, n)
+
+	for i := 0; i < n-1; i++ {
+		if !isExist[A[i]+minDiff] {
+			continue
+		}
+		res = append(res, []int{A[i], A[i] + minDiff})
+	}
+
+	return res
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
