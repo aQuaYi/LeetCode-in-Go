508035d98ad382b3b3f5df0b5bd26383aedf50db
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-09-28 21:44:38 +0800

    修改 union find

--- routine/union-find/union-find.go
+++ routine/union-find/union-find.go
@@ -2,27 +2,27 @@ package unionfind
 
 // leetcode 中需要 union 的集合都太小了，最普通的集合算法，就够用了
 
-type union struct {
+type unionFind struct {
 	parent []int
 }
 
-func newUnion(size int) *union {
+func newUnionFind(size int) *unionFind {
 	parent := make([]int, size)
 	for i := range parent {
 		parent[i] = i
 	}
-	return &union{
+	return &unionFind{
 		parent: parent,
 	}
 }
 
-func (u *union) find(i int) int {
+func (u *unionFind) find(i int) int {
 	if u.parent[i] != i {
 		u.parent[i] = u.find(u.parent[i])
 	}
 	return u.parent[i]
 }
 
-func (u *union) unite(x, y int) {
+func (u *unionFind) union(x, y int) {
 	u.parent[u.find(x)] = u.find(y)
 }
