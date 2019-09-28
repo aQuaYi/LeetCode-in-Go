ac7e5bbc9c3cce220bfe77035d764b2c453e950c
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-09-28 21:03:39 +0800

    1202 accepted.  124 ms, faster than 61.54%

---
+++ Algorithms/1202.smallest-string-with-swaps/smallest-string-with-swaps_test.go
@@ -0,0 +1,51 @@
+package problem1202
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	s     string
+	pairs [][]int
+	ans   string
+}{
+
+	// {
+	// 	"dcab",
+	// 	[][]int{{0, 3}, {1, 2}},
+	// 	"bacd",
+	// },
+
+	{
+		"dcab",
+		[][]int{{0, 3}, {1, 2}, {0, 2}},
+		"abcd",
+	},
+
+	{
+		"cba",
+		[][]int{{0, 1}, {1, 2}},
+		"abc",
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_smallestStringWithSwaps(t *testing.T) {
+	a := assert.New(t)
+
+	for _, tc := range tcs {
+		a.Equal(tc.ans, smallestStringWithSwaps(tc.s, tc.pairs), "输入:%v", tc)
+	}
+}
+
+func Benchmark_smallestStringWithSwaps(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			smallestStringWithSwaps(tc.s, tc.pairs)
+		}
+	}
+}
