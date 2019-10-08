de56887ca6337470c9384081d745cbc8998037e7
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-10-08 20:05:10 +0800

    1207 done

--- Algorithms/1207.unique-number-of-occurrences/unique-number-of-occurrences.go
+++ Algorithms/1207.unique-number-of-occurrences/unique-number-of-occurrences.go
@@ -1,6 +1,16 @@
 package problem1207
 
-func uniqueOccurrences(arr []int) bool {
-
-	return false
+func uniqueOccurrences(A []int) bool {
+	count := make(map[int]int, len(A))
+	for _, a := range A {
+		count[a]++
+	}
+	hasSeen := make(map[int]bool, len(count))
+	for _, c := range count {
+		if hasSeen[c] {
+			return false
+		}
+		hasSeen[c] = true
+	}
+	return true
 }
