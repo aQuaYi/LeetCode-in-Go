d383f6bda3f87645f47c22bf6933683d1c9ef548
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-10-08 19:58:42 +0800

    1207 added

---
+++ Algorithms/1207.unique-number-of-occurrences/unique-number-of-occurrences.go
@@ -0,0 +1,6 @@
+package problem1207
+
+func uniqueOccurrences(arr []int) bool {
+
+	return false
+}
---
+++ Algorithms/1207.unique-number-of-occurrences/unique-number-of-occurrences_test.go
@@ -0,0 +1,47 @@
+package problem1207
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	arr []int
+	ans bool
+}{
+
+	{
+		[]int{1, 2, 2, 1, 1, 3},
+		true,
+	},
+
+	{
+		[]int{1, 2},
+		false,
+	},
+
+	{
+		[]int{-3, 0, 1, -3, 1, 1, 1, -3, 10, 0},
+		true,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_uniqueOccurrences(t *testing.T) {
+	a := assert.New(t)
+
+	for _, tc := range tcs {
+		a.Equal(tc.ans, uniqueOccurrences(tc.arr), "输入:%v", tc)
+	}
+}
+
+func Benchmark_uniqueOccurrences(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			uniqueOccurrences(tc.arr)
+		}
+	}
+}
