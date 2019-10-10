3cd95302bcf0e79d7ba65025b544f7c09b4241da
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-10-10 21:08:21 +0800

    1217 added

--- Algorithms/1217.play-with-chips/play-with-chips.go
+++ Algorithms/1217.play-with-chips/play-with-chips.go
@@ -1,6 +1,20 @@
 package problem1217
 
 func minCostToMoveChips(chips []int) int {
+	odd, even := 0, 0
+	for _, p := range chips {
+		if p%2 == 0 {
+			even++
+		} else {
+			odd++
+		}
+	}
+	return min(odd, even)
+}
 
-	return 0
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
