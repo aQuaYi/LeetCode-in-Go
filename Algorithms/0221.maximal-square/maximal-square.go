e40b51beed07f0b7a57a150bf0ada1731f294885
Liang, Kui <liang.kui@hotmail.com>
2020-04-27 16:30:17 +0800

    opt 0221 solution

--- Algorithms/0221.maximal-square/maximal-square.go
+++ Algorithms/0221.maximal-square/maximal-square.go
@@ -13,32 +13,15 @@ func maximalSquare(matrix [][]byte) int {
 	maxEdge := 0
 	// dp[i][j] == 以 (i,j) 点为右下角点的符合题意的最大正方形的边长
 	// TODO: 由于 dp[i][j] 只与上，左上，左的数据有关，可以把 dp 压缩成一维的
-	dp := make([][]int, m)
+	dp := make([][]int, m+1)
 	for i := range dp {
-		dp[i] = make([]int, n)
-		if matrix[i][0] == '1' {
-			dp[i][0] = 1
-			maxEdge = 1
-		}
-	}
-	for j := 1; j < n; j++ {
-		if matrix[0][j] == '1' {
-			dp[0][j] = 1
-			maxEdge = 1
-		}
+		dp[i] = make([]int, n+1)
 	}
 
-	for i := 1; i < m; i++ {
-		for j := 1; j < n; j++ {
-			if matrix[i][j] == '1' {
-				dp[i][j] = 1 +
-					min(
-						dp[i-1][j-1],
-						min(
-							dp[i-1][j],
-							dp[i][j-1],
-						),
-					)
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if matrix[i-1][j-1] == '1' {
+				dp[i][j] = 1 + min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1]))
 				maxEdge = max(maxEdge, dp[i][j])
 			}
 		}
